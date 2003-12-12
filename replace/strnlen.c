6983667336b01a583378eb657e2690e63f3f7097
Alan Robertson <alanr@unix.sh>
2003-12-12 18:23:28 +0000

    Added a real replacement strnlen function. Should actually be pretty fast too...
    CVS patchset: 2251
    CVS date: 2003/12/12 18:23:28
    
    Mercurial revision: 7d552e3daf90fdef4c5520e2a8969a524d847fc2

---
+++ replace/strnlen.c
@@ -0,0 +1,31 @@
+#include <portability.h>
+#include <string.h>
+/*
+ * Copyright (C) 2003 Alan Robertson <alanr@unix.sh>
+ * This software licensed under the GNU LGPL.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+size_t
+strnlen(const char *s, size_t maxlen)
+{
+	const char * eospos;
+
+	eospos = memchr(s, (int)'\0', maxlen);
+
+	return (eospos == NULL ? maxlen : (size_t)(eospos-s));
+}
