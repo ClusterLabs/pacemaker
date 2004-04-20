59a9fac619e9becb1af767d62f2314792074af7d
Matthew Soffen <matt@soffen.com>
2004-04-20 20:17:47 +0000

    Added portability.h include
    CVS patchset: 2893
    CVS date: 2004/04/20 20:17:47
    
    Mercurial revision: 5e287e39a858acdaf9fd7679bf0c721489cc34f7

--- crm/crmd/fsa.c
+++ crm/crmd/fsa.c
@@ -15,6 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include <portability.h>
 #include <crm/crm.h>
 #include <crmd_fsa.h>
 #include <fsa_matrix.h>
