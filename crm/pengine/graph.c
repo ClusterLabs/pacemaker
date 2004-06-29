6801d1f2494aecee03d2fc6c0613a051ede1d814
Matthew Soffen <matt@soffen.com>
2004-06-29 16:22:04 +0000

    sys/param.h needs to be included before any project headers
    CVS patchset: 3275
    CVS date: 2004/06/29 16:22:04
    
    Mercurial revision: 6bf55776ba0ce0799af2018b9a2bf8462fae34fb

--- crm/pengine/graph.c
+++ crm/pengine/graph.c
@@ -1,4 +1,4 @@
-/* $Id: graph.c,v 1.7 2004/06/28 08:29:20 andrew Exp $ */
+/* $Id: graph.c,v 1.8 2004/06/29 16:22:04 msoffen Exp $ */
 /* 
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
  * 
@@ -16,6 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include <sys/param.h>
 #include <crm/crm.h>
 #include <crm/cib.h>
 #include <crm/msg_xml.h>
