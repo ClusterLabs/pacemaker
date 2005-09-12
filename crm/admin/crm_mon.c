55bf998e1aa48002ba70838cadeca497869f9ca0
Guochun Shi <gshi@ncsa.uiuc.edu>
2005-09-12 01:42:47 +0000

    I believe it is trying to print an integer :)
    CVS patchset: 7521
    CVS date: 2005/09/12 01:42:47
    
    Mercurial revision: 5aa3eab1ee1ceb93e65f5d17ae480e1ebb508c00

--- crm/admin/crm_mon.c
+++ crm/admin/crm_mon.c
@@ -1,4 +1,4 @@
-/* $Id: crm_mon.c,v 1.8 2005/09/11 20:56:56 andrew Exp $ */
+/* $Id: crm_mon.c,v 1.9 2005/09/12 01:42:47 gshi Exp $ */
 
 /* 
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
@@ -234,7 +234,7 @@ mon_timer_popped(gpointer data)
 			wait_for_refresh(0, "Not connected: ", 2*interval);
 			return FALSE;
 		}
-		crm_err("Channel fd: %p", cib_conn->cmds->inputfd(cib_conn));
+		crm_err("Channel fd: %d", cib_conn->cmds->inputfd(cib_conn));
 	}
 	if(as_console) { blank_screen(); }
 	rc = cib_conn->cmds->query(cib_conn, NULL, NULL, options);
