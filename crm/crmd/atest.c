c8012807fc4a7a280af0eea5828f0bff0b9b6d12
Andrew Beekhof <andrew@beekhof.net>
2006-02-02 09:03:27 +0000

    Time XML copying
    CVS patchset: 8558
    CVS date: 2006/02/02 09:03:27
    
    Mercurial revision: 7697f9dfa7bbf72ed6541f604b9b535a7521e604

--- crm/crmd/atest.c
+++ crm/crmd/atest.c
@@ -1,4 +1,4 @@
-/* $Id: atest.c,v 1.6 2005/06/14 11:09:03 davidlee Exp $ */
+/* $Id: atest.c,v 1.7 2006/02/02 09:03:27 andrew Exp $ */
 /* 
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
  * 
@@ -62,6 +62,8 @@ gboolean process_atest_message(
 
 GMainLoop*  mainloop = NULL;
 
+#define OPTARGS	"V?f:"
+
 int
 main(int argc, char ** argv)
 {
@@ -72,7 +74,7 @@ main(int argc, char ** argv)
 	
 	crm_log_init(crm_system_name);
 	set_crm_log_level(LOG_DEBUG_5-1);
-	
+
 	a_cib = string2xml("<cib_fragment section=\"status\"><cib timestamp=\"1107940665\" generated=\"true\" cib_feature_revision=\"1\" debug_source=\"do_lrm_query\"><configuration timestamp=\"1107940665\"><crm_config/><nodes/><resources/><constraints/></configuration><status timestamp=\"1107940665\"><node_state replace=\"lrm\" id=\"f67904e0-4dfc-4db1-83a2-e930fc1d20f4\" uname=\"c001n09\"><lrm><lrm_resources/><lrm_agents><lrm_agent class=\"stonith\" type=\"apcmaster\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"apcsmart\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"baytech\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"external\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"ibmhmc\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"meatware\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"null\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"nw_rpc100s\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"rcd_serial\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"riloe\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"rps10\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"ssh\" version=\"1\"/><lrm_agent class=\"stonith\" type=\"wti_nps\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"portblock\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"ldirectord\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"db2\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"apache\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"Xinetd\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"WinPopup\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"WAS\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"ServeRAID\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"SendArp\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"Raid1\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"OCF\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"MailTo\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"LinuxSCSI\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"LVM\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"IPv6addr\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"IPsrcaddr\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"IPaddr2\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"IPaddr\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"ICP\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"Filesystem\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"Delay\" version=\"1\"/><lrm_agent class=\"heartbeat\" type=\"AudibleAlarm\" version=\"1\"/></lrm_agents><metatdata/></lrm></node_state></status></cib></cib_fragment>");
 	/* createEmptyCib(); */
 	cl_log_message(LOG_DEBUG, (HA_Message*)a_cib);
@@ -98,24 +100,78 @@ main(int argc, char ** argv)
 	g_main_run(mainloop);
 	
 #else
+	int flag = 0;
+	FILE *xml_strm = NULL;
+	const char *xml_file = "default.xml";
+	crm_data_t *input = NULL;
+	crm_data_t *output = NULL;
+	int start = 0, length = 0, time_diff_ms = 0;
+	longclock_t time_start = 0;
+	longclock_t time_stop = 0;
+	longclock_t time_diff = 0;
+
 	crm_log_init(crm_system_name);
-	set_crm_log_level(LOG_DEBUG_5-1);
+	set_crm_log_level(LOG_DEBUG);
 
-	/* read local config file */
-    crm_debug("Enabling coredumps");
-    if(cl_set_corerootdir(HA_COREDIR) < 0){
-	    cl_perror("cannot set corerootdir");
-    }
-    if(cl_enable_coredumps(1) != 0) {
-	    crm_err("Cannot enable coredumps");
-    }
-    if(cl_cdtocoredir() != 0) {
-	    crm_err("Cannot cd to coredump dir");
-    }
+	while (1) {
+		flag = getopt(argc, argv, OPTARGS);
+		if (flag == -1)
+			break;
 
-    crm_warn("Calling abort()");
-    abort();
-    crm_err("We just dumped core");
+		switch(flag) {
+			case 'f':
+				xml_file = optarg;
+				break;
+			case 'V':
+				cl_log_enable_stderr(TRUE);
+				alter_debug(DEBUG_INC);
+				break;				
+			default:
+				printf("Argument code 0%o (%c)"
+				       " is not (?yet?) supported\n",
+				       flag, flag);
+				break;
+		}
+	}
+	
+	xml_strm = fopen(xml_file, "r");
+
+	start  = ftell(xml_strm);
+	fseek(xml_strm, 0L, SEEK_END);
+	length = ftell(xml_strm);
+	fseek(xml_strm, 0L, start);
+	
+	if(xml_strm != NULL) {
+		crm_debug("Reading: %s", xml_file);
+		input = file2xml(xml_strm);
+	} else {
+		cl_perror("File not found: %s", xml_file);
+	}
+
+	time_start = time_longclock();
+	output = copy_xml(input);
+	time_stop = time_longclock();
+	
+	time_diff = sub_longclock(time_stop, time_start);
+	time_diff_ms = longclockto_ms(time_diff);
+	crm_warn("Copy %s (%d bytes): %dms", xml_file, length, time_diff_ms);
+
+	time_start = time_longclock();
+	free_xml(output);
+	time_stop = time_longclock();
+
+	time_diff = sub_longclock(time_stop, time_start);
+	time_diff_ms = longclockto_ms(time_diff);
+	crm_warn("Free %s (%d bytes): %dms", xml_file, length, time_diff_ms);
+
+	time_start = time_longclock();
+	write_xml_file(input, "/tmp/foo.xml");
+	time_stop = time_longclock();
+
+	time_diff = sub_longclock(time_stop, time_start);
+	time_diff_ms = longclockto_ms(time_diff);
+	crm_warn("Write %s (%d bytes): %dms", xml_file, length, time_diff_ms);
+	
 #endif
     return 0;
 }
