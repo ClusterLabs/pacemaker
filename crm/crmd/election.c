97387c06baa2856e3c8decc71063f0c536aefd35
Andrew Beekhof <andrew@beekhof.net>
2007-01-05 10:53:41 +0100

    Backed out changeset c38fb26a9497ab221688bfbfe6639eebcf29b0dd crm_str_eq is the correct function to use, but there needed to be an extra argument
    Mercurial revision: b0175a87cc42de5e658b6c8386d9e84801c08a62

--- crm/crmd/election.c
+++ crm/crmd/election.c
@@ -232,7 +232,7 @@ do_election_count_vote(long long action,
 	crm_debug("Election %d, owner: %s", election_id, election_owner);
 	
 	/* update the list of nodes that have voted */
-	if(safe_str_eq(fsa_our_uuid, election_owner)) {
+	if(crm_str_eq(fsa_our_uuid, election_owner)) {
 		if(election_id == current_election_id) {
 			char *uname_copy = NULL;
 			char *op_copy = crm_strdup(op);
@@ -248,15 +248,15 @@ do_election_count_vote(long long action,
 		}
 			
 	} else {
-		CRM_CHECK(safe_str_neq(op, CRM_OP_NOVOTE), return I_NULL);
+		CRM_CHECK(crm_str_neq(op, CRM_OP_NOVOTE), return I_NULL);
 	}
 	
-	if(vote_from == NULL || safe_str_eq(vote_from, fsa_our_uname)) {
+	if(vote_from == NULL || crm_str_eq(vote_from, fsa_our_uname)) {
 		/* don't count our own vote */
 		crm_info("Election ignore: our %s (%s)", op,crm_str(vote_from));
 		return I_NULL;
 
-	} else if(safe_str_eq(op, CRM_OP_NOVOTE)) {
+	} else if(crm_str_eq(op, CRM_OP_NOVOTE)) {
 		crm_info("Election ignore: no-vote from %s", vote_from);
 		return I_NULL;
 	}
