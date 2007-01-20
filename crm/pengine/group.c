98a5956f817f475c09df7086a10eaf6a669b05b7
Andrew Beekhof <andrew@beekhof.net>
2007-01-20 12:55:56 +0100

    PE: Fix group recovery Given g(a,b,c), if b is stopping or starting it implies c should also, not a.
    Mercurial revision: 4c8a98e52705c0e23ca7d946ed40f721640b2d38

--- crm/pengine/group.c
+++ crm/pengine/group.c
@@ -226,12 +226,12 @@ void group_internal_constraints(resource_t *rsc, pe_working_set_t *data_set)
 			order_start_start(
 				last_rsc, child_rsc, pe_order_implies_right);
 			order_stop_stop(
-				child_rsc, last_rsc, pe_order_implies_right);
+				last_rsc, child_rsc, pe_order_implies_right);
 
 		} else {
 			custom_action_order(
 				child_rsc, stop_key(child_rsc), NULL,
-				this_rsc, stopped_key(this_rsc), NULL,
+				this_rsc,  stopped_key(this_rsc), NULL,
 				pe_order_optional, data_set);
 
 			order_start_start(
@@ -240,7 +240,7 @@ void group_internal_constraints(resource_t *rsc, pe_working_set_t *data_set)
 			/* recovery */
 			custom_action_order(
 				child_rsc, stop_key(child_rsc), NULL,
-				this_rsc, stopped_key(this_rsc), NULL,
+				this_rsc,  stopped_key(this_rsc), NULL,
 				pe_order_implies_right, data_set);
 			order_start_start(
 				this_rsc, child_rsc, pe_order_implies_right);
