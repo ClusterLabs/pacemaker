9a2e58db7380fb17691530ccf3c019444d9b571d
Alan Robertson <alanr@unix.sh>
2007-05-17 07:24:08 -0600

    Fixed a tengine compile error. Please don't forget to compile things with gcc before pushing upstream.
    Mercurial revision: 45ca592555ec16efc4e2d339c10bf10d21b44fbe

--- crm/tengine/events.c
+++ crm/tengine/events.c
@@ -335,7 +335,7 @@ match_graph_event(int action_id, crm_data_t *event, const char *event_node,
 			break;
 		default:
 			action->failed = TRUE;
-			crm_err("Unsupported action result: %d", crm_str(op_status));
+			crm_err("Unsupported action result: %d", op_status);
 	}
 
 	/* stop this event's timer if it had one */
