7c8a9259b2e75e54ab23003730f237cd04d1b5c5
Andrew Beekhof <andrew@beekhof.net>
2007-05-22 14:42:22 +0200

    ais: Send and receive messages as well as node and crm membership information
    Mercurial revision: a85a484c24f064e548ed87387ed7391f0dddfa97

--- crm/ais/plugin.c
+++ crm/ais/plugin.c
@@ -47,12 +47,25 @@
 
 typedef struct crm_ais_msg_s
 {
-		int   id;
-		int   size;
-		char *data;
+		int		id;
+		int		type;
+
+		int		host_size;
+		const char     *host;
+
+		int		size;
+		const char     *data;
 } AIS_Message;
 
-extern totempg_groups_handle openais_group_handle;
+static struct totempg_group crm_group[] = {
+        {
+		.group		= "CRM",
+		.group_len	= 3
+	},
+};
+
+totempg_groups_handle crm_group_handle;
+extern poll_handle aisexec_poll_handle;
 
 static void crm_confchg_fn (
 	enum totem_configuration_type configuration_type,
@@ -61,7 +74,15 @@ static void crm_confchg_fn (
 	unsigned int *joined_list, int joined_list_entries,
 	struct memb_ring_id *ring_id);
 
+static void global_confchg_fn (
+	enum totem_configuration_type configuration_type,
+	unsigned int *member_list, int member_list_entries,
+	unsigned int *left_list, int left_list_entries,
+	unsigned int *joined_list, int joined_list_entries,
+	struct memb_ring_id *ring_id);
+
 static int crm_exec_init_fn (struct objdb_iface_ver0 *objdb);
+static int crm_config_init_fn(struct objdb_iface_ver0 *objdb);
 
 static int crm_lib_init_fn (void *conn);
 
@@ -92,6 +113,13 @@ static struct openais_exec_handler crm_exec_service[] =
 	}
 };
 
+static void crm_exec_dump_fn(void) 
+{
+	ENTER("");
+	crm_err("here");
+	LEAVE("");
+}
+
 /*
  * Exports the interface for the service
  */
@@ -107,7 +135,13 @@ struct openais_service_handler crm_service_handler = {
 	.exec_init_fn		= crm_exec_init_fn,
 	.exec_service		= crm_exec_service,
 	.exec_service_count	= sizeof (crm_exec_service) / sizeof (struct openais_exec_handler),
-	.confchg_fn		= crm_confchg_fn,
+	.config_init_fn		= crm_config_init_fn,
+	.confchg_fn		= global_confchg_fn,
+	.exec_dump_fn		= crm_exec_dump_fn,
+/* 	void (*sync_init) (void); */
+/* 	int (*sync_process) (void); */
+/* 	void (*sync_activate) (void); */
+/* 	void (*sync_abort) (void); */
 };
 
 /*
@@ -140,6 +174,9 @@ static struct lcr_comp crm_comp_ver0 = {
 
 static struct openais_service_handler *crm_get_handler_ver0 (void)
 {
+	ENTER("");
+	crm_err("here");
+	LEAVE("");
 	return (&crm_service_handler);
 }
 
@@ -150,8 +187,14 @@ __attribute__ ((constructor)) static void register_this_component (void) {
 }
 
 /* IMPL */
+static int crm_config_init_fn(struct objdb_iface_ver0 *objdb)
+{
+	ENTER("");
+	LEAVE("");
+	return 0;
+}
 
-static int send_cluster_msg(const char *data) 
+static int send_cluster_msg(int type, const char *host, const char *data) 
 {
 	int rc = 0;
 	struct iovec iovec;
@@ -159,21 +202,92 @@ static int send_cluster_msg(const char *data)
 	static int msg_id = 0;
 
 	ENTER("");
-	ais_msg.id = msg_id++;
-	ais_msg.size = 0;
+	if(data == NULL) {
+		rc = -1;
+		goto bail;
+	}
+
+	msg_id++;
+	
+	ais_msg.type = type;
+	ais_msg.id = msg_id;
+	
+	ais_msg.size = strlen(data);
 	ais_msg.data = data;
+
+	if(host) {
+		ais_msg.host_size = strlen(host);
+		ais_msg.host = host;
+	} else {
+		ais_msg.host_size = 0;
+		ais_msg.host = NULL;
+	}
 	
 	iovec.iov_base = (char *)&ais_msg;
 	iovec.iov_len = sizeof (AIS_Message);
 
 	rc = totempg_groups_mcast_joined (
-		openais_group_handle, &iovec, 1, TOTEMPG_SAFE);
-	
+		crm_group_handle, &iovec, 1, TOTEMPG_SAFE);
+  bail:
 	CRM_CHECK(rc == 0,
 		  crm_err("Message not sent (%d): %s", rc, crm_str(data)));
 
 	LEAVE("");
 	return rc;	
+}
+
+static int send_cluster_xml(int type, const char *host, crm_data_t *xml) 
+{
+	int rc = 0;
+	char *data = dump_xml_unformatted(xml);
+	CRM_CHECK(data != NULL, return -1);
+	
+	rc = send_cluster_msg(type, host, data);
+	crm_free(data);
+	return rc;
+}
+
+static void crm_deliver_fn (
+	unsigned int nodeid,
+	struct iovec *iovec,
+	int iov_len,
+	int endian_conversion_required)
+{
+	void *data = NULL;
+	AIS_Message *ais_msg;
+
+	ENTER("iov_len: %d", iov_len);
+	if (iov_len > 1) {
+		int i = 0;
+		int pos = 0;
+		crm_err("Combining multiple iovec entries");
+		for (i = 0; i < iov_len; i++) {
+			crm_realloc(data, pos+iovec[i].iov_len+1);
+			memcpy (data+pos, iovec[i].iov_base, iovec[i].iov_len);
+			pos += iovec[i].iov_len;
+		}
+		ais_msg = data;
+		
+	} else {
+		ais_msg = iovec[0].iov_base;
+	}
+
+	if (endian_conversion_required) {
+		crm_info("Performing endian conversion...");
+		ais_msg->id = swab32 (ais_msg->id);
+		ais_msg->type = swab32 (ais_msg->type);
+		ais_msg->size = swab32 (ais_msg->size);
+		ais_msg->host_size = swab32 (ais_msg->host_size);
+	}
+	crm_info("dump");
+
+	crm_info("Msg (id=%d, type=%d, host=%s, size=%d): %s",
+		 ais_msg->id, ais_msg->type,
+		 ais_msg->host?ais_msg->host:"<all>",
+		 ais_msg->size, crm_str(ais_msg->data));
+
+	crm_free(data);
+	LEAVE("");
 }
 
 static int crm_exec_init_fn (struct objdb_iface_ver0 *objdb)
@@ -187,11 +301,35 @@ static int crm_exec_init_fn (struct objdb_iface_ver0 *objdb)
 	set_crm_log_level(LOG_DEBUG);
 	crm_info("CRM-AIS Plugin: Initialized");
 
-	send_cluster_msg(NULL);
+	totempg_groups_initialize(
+		&crm_group_handle, crm_deliver_fn, crm_confchg_fn);
+	totempg_groups_join(crm_group_handle, crm_group, 1);
+	
+	crm_info("CRM Group: Initialized");
+	send_cluster_msg(0, NULL, "I'm alive!");
 	
 	LEAVE("");
 	return 0;
 }
+
+static void ais_pint_node() 
+{
+}
+
+static void global_confchg_fn (
+	enum totem_configuration_type configuration_type,
+	unsigned int *member_list, int member_list_entries,
+	unsigned int *left_list, int left_list_entries,
+	unsigned int *joined_list, int joined_list_entries,
+	struct memb_ring_id *ring_id)
+{
+	ENTER("");
+	
+	send_cluster_msg(0, "this_host", "Global config changed");
+	
+	LEAVE("");
+}
+
 static void crm_confchg_fn (
 	enum totem_configuration_type configuration_type,
 	unsigned int *member_list, int member_list_entries,
@@ -200,7 +338,7 @@ static void crm_confchg_fn (
 	struct memb_ring_id *ring_id)
 {
 	ENTER("");
-	send_cluster_msg(NULL);
+	send_cluster_msg(0, NULL, "CRM config changed");
 	LEAVE("");
 }
 
