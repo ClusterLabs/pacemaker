93b77147a4ee66ee537ceaf0dca23a7a332f7576
Andrew Beekhof <andrew@beekhof.net>
2007-06-05 11:00:39 +0200

    PE: Indicate orphan status when displayuing resources
    Mercurial revision: 2de18b7471aec7a917ff2cee5e8743c566129f14

--- lib/crm/pengine/native.c
+++ lib/crm/pengine/native.c
@@ -260,17 +260,19 @@ native_print(
 	if((options & pe_print_rsconly) || g_list_length(rsc->running_on) > 1) {
 		const char *desc = NULL;
 		desc = crm_element_value(rsc->xml, XML_ATTR_DESC);
-		status_print("%s%s\t(%s%s%s:%s)%s%s",
+		status_print("%s%s\t(%s%s%s:%s%s)%s%s",
 			     pre_text?pre_text:"", rsc->id,
 			     prov?prov:"", prov?"::":"",
 			     class, crm_element_value(rsc->xml, XML_ATTR_TYPE),
+			     rsc->orphan?" ORPHANED":"",
 			     desc?": ":"", desc?desc:"");
 
 	} else {
-		status_print("%s%s\t(%s%s%s:%s):\t%s %s%s%s",
+		status_print("%s%s\t(%s%s%s:%s%s):\t%s %s%s%s",
 			     pre_text?pre_text:"", rsc->id,
 			     prov?prov:"", prov?"::":"",
 			     class, crm_element_value(rsc->xml, XML_ATTR_TYPE),
+			     rsc->orphan?" ORPHANED":"",
 			     (rsc->variant!=pe_native)?"":role2text(rsc->role),
 			     (rsc->variant!=pe_native)?"":node!=NULL?node->details->uname:"",
 			     rsc->is_managed?"":" (unmanaged)", rsc->failed?" FAILED":"");
