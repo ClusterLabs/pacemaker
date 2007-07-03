98fe7c49988cb3d551766b2fe141e075860d65cb
Andrew Beekhof <andrew@beekhof.net>
2007-07-03 14:00:39 +0200

    CRM: Allow cd-ing to the the core directory to be optional
    Mercurial revision: baad1401ee08ebca99432addd17b4046d4465689

--- crm/cib/cibmon.c
+++ crm/cib/cibmon.c
@@ -95,7 +95,7 @@ main(int argc, char **argv)
 	};
 #endif
 
-	crm_log_init(crm_system_name);
+	crm_log_init(crm_system_name, TRUE);
 
 	G_main_add_SignalHandler(
 		G_PRIORITY_HIGH, SIGTERM, cibmon_shutdown, NULL, NULL);
