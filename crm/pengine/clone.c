556ae5b2c44ed8532a2d73bda27745c1e2bb83c2
Andrew Beekhof <andrew@beekhof.net>
2007-10-16 16:55:00 +0200

    Low: PE: Note-to-self comment for later
    Mercurial revision: 123825b2d84150007a2ee1e59693c8cdd0d2b643

--- crm/pengine/clone.c
+++ crm/pengine/clone.c
@@ -232,6 +232,7 @@ color_instance(resource_t *rsc, pe_working_set_t *data_set)
 		if(local_node) {
 		    local_node->count++;
 		} else if(rsc->is_managed) {
+		    /* what to do? we can't enforce per-node limits in this case */
 		    crm_config_err("%s not found in %s (list=%d)",
 				   chosen->details->id, rsc->parent->id,
 				   g_list_length(rsc->parent->allowed_nodes));
