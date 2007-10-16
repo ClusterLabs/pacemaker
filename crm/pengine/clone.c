07eac1b1c2bcc781b4ecabee26415eb3bc096c76
Andrew Beekhof <andrew@beekhof.net>
2007-10-16 12:43:02 +0200

    High: PE: Relax an assumption regarding clones that is not true when they are unmanaged
    Mercurial revision: 6ffc9e9827cb0027af0b5c7a690a034ca556722b

--- crm/pengine/clone.c
+++ crm/pengine/clone.c
@@ -229,13 +229,13 @@ color_instance(resource_t *rsc, pe_working_set_t *data_set)
 		local_node = pe_find_node_id(
 			rsc->parent->allowed_nodes, chosen->details->id);
 
-		if(local_node == NULL) {
-			crm_err("%s not found in %s (list=%d)",
-				chosen->details->id, rsc->parent->id,
-				g_list_length(rsc->parent->allowed_nodes));
+		if(local_node) {
+		    local_node->count++;
+		} else if(rsc->is_managed) {
+		    crm_config_err("%s not found in %s (list=%d)",
+				   chosen->details->id, rsc->parent->id,
+				   g_list_length(rsc->parent->allowed_nodes));
 		}
-		CRM_ASSERT(local_node);
-		local_node->count++;
 	}
 
 	return chosen;
