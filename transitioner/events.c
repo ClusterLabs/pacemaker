fe910210bd8d795c0fdd434284dc25eca70a8971
Andrew Beekhof <andrew@beekhof.net>
2008-04-11 11:00:40 +0200

    High: TE: Use the correct offset when updating failcount
    Mercurial revision: 39ca646060276b34e3cb7c3ec171013b4c4bbb31

--- transitioner/events.c
+++ transitioner/events.c
@@ -250,7 +250,7 @@ update_failcount(crm_data_t *event, const char *event_node, int rc)
 
 	if(safe_str_eq(task, CRMD_ACTION_START)) {
 	    interval = 1;
-	    value = failed_stop_offset;
+	    value = failed_start_offset;
 
 	} else if(safe_str_eq(task, CRMD_ACTION_STOP)) {
 	    interval = 1;
@@ -264,8 +264,8 @@ update_failcount(crm_data_t *event, const char *event_node, int rc)
 	if(interval > 0) {
 		int call_id = 0;
 		attr_name = crm_concat("fail-count", rsc_id, '-');
-		crm_warn("Updating failcount for %s on %s after failed %s: rc=%d",
-			 rsc_id, on_uuid, task, rc);
+		crm_warn("Updating failcount (%s) for %s on %s after failed %s: rc=%d",
+			 value, rsc_id, on_uuid, task, rc);
 
 		call_id = update_attr(te_cib_conn, cib_inhibit_notify, XML_CIB_TAG_STATUS,
 			    on_uuid, NULL,NULL, attr_name, value, FALSE);
