82ab57565a78b2bcc706b75b7203f0d0560b86be
Andrew Beekhof <andrew@beekhof.net>
2008-04-30 16:56:07 +0200

    High: TE: Parse the transition key correctly
    Mercurial revision: 9a9fcea0e2c4a8ed95326dfbeaf4aaa8f14b918d

--- lib/crm/common/utils.c
+++ lib/crm/common/utils.c
@@ -1076,7 +1076,7 @@ decode_transition_key(
 	CRM_CHECK(transition_id != NULL, return FALSE);
 	
 	crm_malloc0(*uuid, strlen(key));
-	res = sscanf(key, "%d:%d:%d:%s", transition_id, action_id, target_rc, *uuid);
+	res = sscanf(key, "%d:%d:%d:%s", action_id, transition_id, target_rc, *uuid);
 	switch(res) {
 	    case 4:
 		/* Post Pacemaker 0.6 */
@@ -1084,7 +1084,7 @@ decode_transition_key(
 	    case 2:
 		/* Until Pacemaker 0.6 */
 		*target_rc = -1;
-		res = sscanf(key, "%d:%d:%s", transition_id, action_id, *uuid);
+		res = sscanf(key, "%d:%d:%s", action_id, transition_id, *uuid);
 		CRM_CHECK(res == 3, done = FALSE);
 		break;
 	    case 1:
