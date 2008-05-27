bc2d4c64845e89278f95eb93cb9eeb6f039e0025
Andrew Beekhof <andrew@beekhof.net>
2008-05-27 10:43:42 +0200

    Medium: cib: New call option 'cib_can_create' which can be passed to modify actions - allows the object to be created if it doesn't exist yet
    Mercurial revision: 6e4aec646d2bc7a915e4d7d6cabd99a8862f5205

--- include/crm/cib.h
+++ include/crm/cib.h
@@ -54,6 +54,7 @@ enum cib_call_options {
 	cib_verbose         = 0x00000001,
 	cib_xpath           = 0x00000002,
 	cib_multiple        = 0x00000004,
+	cib_can_create      = 0x00000008,
 	cib_discard_reply   = 0x00000010,
 	cib_scope_local     = 0x00000100,
 	cib_sync_call       = 0x00001000,
