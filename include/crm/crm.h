4234f23e6767a7a23b144b977e5c4de84b52634b
Andrew Beekhof <andrew@beekhof.net>
2008-06-04 13:35:47 +0200

    Medium: Core: Update the CRM feature set to 3.0
    Mercurial revision: f86ef2f0bef91d6790340db21a5f539d14fd0a73

--- include/crm/crm.h
+++ include/crm/crm.h
@@ -33,7 +33,7 @@
 
 #include <libxml/tree.h> 
 
-#define CRM_FEATURE_SET		"2.1"
+#define CRM_FEATURE_SET		"3.0"
 #define MINIMUM_SCHEMA_VERSION	"pacemaker-0.7"
 #define LATEST_SCHEMA_VERSION	"pacemaker-"DTD_VERSION
 
