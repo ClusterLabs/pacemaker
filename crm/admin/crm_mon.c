a2551ba4e8c8baf3da7640b518d765262ff57115
Andrew Beekhof <andrew@beekhof.net>
2008-06-12 16:07:51 +0200

    Medium: Admin: crm_mon - Cleanup on exit patch based on the work of Satomi Taniguchi
    Mercurial revision: 7c94c535d945035c557d7b533528a71fc9e8454b

--- crm/admin/crm_mon.c
+++ crm/admin/crm_mon.c
@@ -66,6 +66,7 @@ int print_html_status(crm_data_t *cib, const char *filename, gboolean web_cgi);
 void make_daemon(gboolean daemonize, const char *pidfile);
 gboolean mon_timer_popped(gpointer data);
 void mon_update(const HA_Message*, int, int, crm_data_t*,void*);
+void clean_up(void);
 
 char *xml_file = NULL;
 char *as_html_file = NULL;
@@ -83,6 +84,32 @@ cib_t *cib_conn = NULL;
 int failed_connections = 0;
 gboolean one_shot = FALSE;
 gboolean has_warnings = FALSE;
+
+/*
+ * Non-mainloop signal handler.
+ */
+static void
+mon_shutdown_wrapper(int nsig)
+{
+    clean_up();
+    exit(0);
+}
+
+/*
+ * Mainloop signal handler.
+ */
+static gboolean
+mon_shutdown(int nsig, gpointer unused)
+{
+    clean_up();
+    if (mainloop && g_main_is_running(mainloop)) {
+	g_main_quit(mainloop);
+	
+    } else {
+	exit(0);
+    }
+    return FALSE;
+}
 
 #if CURSES_ENABLED
 #  define print_as(fmt...) if(as_console) {	\
@@ -157,6 +184,7 @@ main(int argc, char **argv)
 				daemonize = TRUE;
 				break;
 			case 'p':
+				crm_free(pid_file);
 				pid_file = crm_strdup(optarg);
 				break;
 			case 'X':
@@ -204,6 +232,10 @@ main(int argc, char **argv)
 	if (argerr) {
 		usage(crm_system_name, LSB_EXIT_GENERIC);
 	}
+
+	/* Set signal callback function. */
+	signal(SIGTERM, mon_shutdown_wrapper);
+	signal(SIGINT, mon_shutdown_wrapper);
 
 	if(as_html_file == NULL && !web_cgi && !simple_status) {
 #if CURSES_ENABLED
@@ -261,7 +293,13 @@ main(int argc, char **argv)
 	}
 
 	mon_timer_popped(NULL);
+
+	G_main_add_SignalHandler(
+		G_PRIORITY_HIGH, SIGTERM, mon_shutdown, NULL, NULL);
+	G_main_add_SignalHandler(
+		G_PRIORITY_HIGH, SIGINT, mon_shutdown, NULL, NULL);
 	g_main_run(mainloop);
+	g_main_destroy(mainloop);
 	return_to_orig_privs();
 	
 	crm_info("Exiting %s", crm_system_name);	
@@ -322,6 +360,7 @@ mon_timer_popped(gpointer data)
 
 		} else if (simple_status || one_shot) {
 			fprintf(stdout, "Critical: Unable to connect to the CIB\n");
+			clean_up();
 			exit(2);
 		} else {
 			failed_connections++;
@@ -363,26 +402,32 @@ mon_update(const HA_Message *msg, int call_id, int rc,
 		if(as_html_file || web_cgi) {
 			if (print_html_status(cib, as_html_file, web_cgi) != 0) {
 				fprintf(stderr, "Critical: Unable to output html file\n");
+				clean_up();
 				exit(2);
 			}
 		} else if (simple_status) {
 			print_simple_status(cib);
 			if (has_warnings) {
+				clean_up();
 				exit(1); 
 			}
 		} else {
 			print_status(cib);
 		}
 		if(one_shot) {
+			clean_up();
 			exit(LSB_EXIT_OK);
 		}
 		
 			
 	} else if(simple_status) {
 		fprintf(stderr, "Critical: query failed: %s", cib_error2string(rc));
+		clean_up();
 		exit(2);
+		
 	} else if(one_shot) {
 		fprintf(stderr, "Query failed: %s", cib_error2string(rc));
+		clean_up();
 		exit(LSB_EXIT_OK);
 
 	} else {
@@ -792,6 +837,7 @@ usage(const char *cmd, int exit_status)
 
 	fflush(stream);
 
+	clean_up();
 	exit(exit_status);
 }
 
@@ -810,8 +856,10 @@ make_daemon(gboolean daemonize, const char *pidfile)
 		fprintf(stderr, "%s: could not start daemon\n",
 			crm_system_name);
 		perror("fork");
+		clean_up();
 		exit(LSB_EXIT_GENERIC);
 	} else if (pid > 0) {
+		clean_up();
 		exit(LSB_EXIT_OK);
 	}
 	
@@ -819,6 +867,7 @@ make_daemon(gboolean daemonize, const char *pidfile)
 		pid = cl_read_pidfile(pidfile);
 		fprintf(stderr, "%s: already running [pid %ld].\n",
 			crm_system_name, pid);
+		clean_up();
 		exit(LSB_EXIT_OK);
 	}
 	
@@ -829,4 +878,31 @@ make_daemon(gboolean daemonize, const char *pidfile)
 	(void)open(devnull, O_RDONLY);		/* Stdin:  fd 0 */
 	(void)open(devnull, O_WRONLY);		/* Stdout: fd 1 */
 	(void)open(devnull, O_WRONLY);		/* Stderr: fd 2 */
+}
+
+/*
+ * De-init ncurses, signoff from the CIB and deallocate memory.
+ */
+void clean_up(void)
+{
+#if CURSES_ENABLED
+    if(as_console) {
+	as_console = FALSE;
+	echo();
+	nocbreak();
+	endwin();
+    }
+#endif
+    
+    if (cib_conn != NULL) {
+	cib_conn->cmds->signoff(cib_conn);
+	cib_delete(cib_conn);
+	cib_conn = NULL;
+    }
+
+    crm_free(as_html_file);
+    crm_free(xml_file);
+    crm_free(pid_file);
+    
+    return;
 }
