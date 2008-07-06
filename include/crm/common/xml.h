16a7cb11847638497d7aea9b227c30dc26889284
Andrew Beekhof <andrew@beekhof.net>
2008-07-06 17:06:39 +0200

    Low: Core: Streamline crm_xml_add and expand_plus_plus
    Mercurial revision: fe9da71a94d567664673048c884626fbf2fbed2c

--- include/crm/common/xml.h
+++ include/crm/common/xml.h
@@ -36,7 +36,9 @@ typedef xmlNode crm_data_t;
 #define CRM_BZ2_BLOCKS		4
 #define CRM_BZ2_WORK		20
 #define CRM_BZ2_THRESHOLD	10240
-    
+
+#define XML_PARANOIA_CHECKS 0
+
 extern gboolean add_message_xml(
 	xmlNode *msg, const char *field, xmlNode *xml);
 extern xmlNode *get_message_xml(xmlNode *msg, const char *field);
@@ -120,6 +122,9 @@ extern xmlNode *create_xml_node(xmlNode *parent, const char *name);
  *
  */
 extern const char *crm_xml_add(
+	xmlNode *node, const char *name, const char *value);
+
+extern const char *crm_xml_replace(
 	xmlNode *node, const char *name, const char *value);
 
 extern const char *crm_xml_add_int(
