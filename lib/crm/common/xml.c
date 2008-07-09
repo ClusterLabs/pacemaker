39bae288ec1a469705261bca56e4caa7234acbb1
Andrew Beekhof <andrew@beekhof.net>
2008-07-09 10:21:33 +0200

    Low: Core: crm_element_value - Remove a layer of indirection
    Mercurial revision: 8386947bbe0ea5fb23f29b549c8c2712c02a8d81

--- lib/crm/common/xml.c
+++ lib/crm/common/xml.c
@@ -1267,7 +1267,7 @@ crm_element_value_int(xmlNode *data, const char *name, int *dest)
     const char *value = crm_element_value(data, name);
     CRM_CHECK(dest != NULL, return -1);
     if(value) {
-	*dest = crm_parse_int(value, NULL);
+	*dest = crm_int_helper(value, NULL);
 	return 0;
     }
     return -1;
