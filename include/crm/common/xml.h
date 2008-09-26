01ef0fc453a47fbad55435a889f967b3d3b5b4dc
Andrew Beekhof <andrew@beekhof.net>
2008-09-26 11:58:14 +0200

    Low: Core: Make crm_element_value a non-inline function again as some compilers complain it causes the code to grow :/
    Mercurial revision: a76f04827620eee1cacce2e2cf791565d9a5114a

--- include/crm/common/xml.h
+++ include/crm/common/xml.h
@@ -196,27 +196,7 @@ static inline const char *crm_element_name(const xmlNode *data)
     return (data ? (const char *)data->name : NULL);
 }
 
-static inline const char *
-crm_element_value(xmlNode *data, const char *name)
-{
-    xmlAttr *attr = NULL;
-    
-    if(data == NULL) {
-	crm_err("Couldn't find %s in NULL", name?name:"<null>");
-	return NULL;
-
-    } else if(name == NULL) {
-	crm_err("Couldn't find NULL in %s", crm_element_name(data));
-	return NULL;
-    }
-    
-    attr = xmlHasProp(data, (const xmlChar*)name);
-    if(attr == NULL || attr->children == NULL) {
-	return NULL;
-    }
-    return (const char*)attr->children->content;
-}
-
+extern const char *crm_element_value(xmlNode *data, const char *name);
 
 extern void xml_validate(const xmlNode *root);
 
--- lib/common/xml.c
+++ lib/common/xml.c
@@ -2565,3 +2565,24 @@ get_xpath_object(const char *xpath, xmlNode *xml_obj, int error_level)
     }
     return result;
 }
+
+const char *
+crm_element_value(xmlNode *data, const char *name)
+{
+    xmlAttr *attr = NULL;
+    
+    if(data == NULL) {
+	crm_err("Couldn't find %s in NULL", name?name:"<null>");
+	return NULL;
+
+    } else if(name == NULL) {
+	crm_err("Couldn't find NULL in %s", crm_element_name(data));
+	return NULL;
+    }
+    
+    attr = xmlHasProp(data, (const xmlChar*)name);
+    if(attr == NULL || attr->children == NULL) {
+	return NULL;
+    }
+    return (const char*)attr->children->content;
+}
