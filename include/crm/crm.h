528c4f80f15e24692efb797cfe7730a6f46dc4a7
Andrew Beekhof <andrew@beekhof.net>
2008-09-26 10:54:02 +0200

    Low: Core: Update the minimum schema version to 1.0
    Mercurial revision: e43da64ca84503eb3b317103703ed66550dba511

--- include/crm/crm.h
+++ include/crm/crm.h
@@ -32,7 +32,7 @@
 #include <libxml/tree.h> 
 
 #define CRM_FEATURE_SET		"3.0"
-#define MINIMUM_SCHEMA_VERSION	"pacemaker-0.7"
+#define MINIMUM_SCHEMA_VERSION	"pacemaker-1.0"
 #define LATEST_SCHEMA_VERSION	"pacemaker-"DTD_VERSION
 
 #define EOS		'\0'
