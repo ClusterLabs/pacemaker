22d92973bf874cf7048fcf0b3fc4de7e96b45c7d
Andrew Beekhof <andrew@beekhof.net>
2008-12-10 15:23:22 +0100

    Low: Core: Include the errno when logging the result of strerror()
    Mercurial revision: ace6a460547baec5810383c268f87e5c4e1a562b

--- include/crm/crm.h
+++ include/crm/crm.h
@@ -295,7 +295,7 @@ typedef GList* GListPtr;
 #define crm_debug_6(fmt, args...) do_crm_log_unlikely(LOG_DEBUG_6, fmt , ##args)
 #define crm_perror(level, fmt, args...) do {				\
 	const char *err = strerror(errno);				\
-	do_crm_log_always(level, fmt ": %s", ##args, err);		\
+	do_crm_log_always(level, fmt ": %s (%d)", ##args, err, errno);	\
     } while(0)
 
 #include <crm/common/util.h>
