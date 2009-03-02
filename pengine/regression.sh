4d26a45289c0be52016ab767f08f4c1028578a21
Andrew Beekhof <andrew@beekhof.net>
2009-03-02 16:02:57 +0100

    Low: PE: Add new regression test
    Mercurial revision: 2f5bf9b8aa9ef3be15791f118d8fbe61b3b41177

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -211,6 +211,7 @@ do_test inc11 "Primitive colocation with clones"
 do_test inc12 "Clone shutdown" 
 do_test cloned-group "Make sure only the correct number of cloned groups are started"
 do_test clone-no-shuffle "Dont prioritize allocation of instances that must be moved"
+do_test clone-max-zero "Orphan processing with clone-max=0"
 
 echo ""
 do_test master-0 "Stopped -> Slave"
