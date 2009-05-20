883be4ea73f82b10032876b777b91f2d41cabe35
Andrew Beekhof <andrew@beekhof.net>
2009-05-20 14:17:26 +0200

    High: cib: Fix huge memory leak affecting heartbeat-based clusters
    Mercurial revision: e87d635ead44e0aa32cf456de5e93b674531f2a7

--- cib/callbacks.c
+++ cib/callbacks.c
@@ -1064,6 +1064,7 @@ cib_ha_peer_callback(HA_Message * msg, void* private_data)
 {
     xmlNode *xml = convert_ha_message(NULL, msg, __FUNCTION__);
     cib_peer_callback(xml, private_data);
+    free_xml(xml);
 }
 
 void
