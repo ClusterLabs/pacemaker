b0ba01f61086f073be69db3e6beb0914642f79d9
Lars Marowsky-Bree <lmb@suse.de>
2009-08-25 17:55:38 +0200

    Low: pengine: always provide notify, ordered, and interleave values to RAs.
    This allows more validation to be performed by the resource agents.
    
    Mercurial revision: 35972fcf5cb0a72f43e7568d82b7ce0d0bad6180

--- pengine/graph.c
+++ pengine/graph.c
@@ -543,6 +543,9 @@ action2xml(action_t *action, gboolean as_input)
 			XML_RSC_ATTR_INCARNATION_NODEMAX,
 			XML_RSC_ATTR_MASTER_MAX,
 			XML_RSC_ATTR_MASTER_NODEMAX,
+			XML_RSC_ATTR_NOTIFY,
+			XML_RSC_ATTR_INTERLEAVE,
+			XML_RSC_ATTR_ORDERED
 		};
 		
 		for(lpc = 0; lpc < DIMOF(meta_list); lpc++) {
