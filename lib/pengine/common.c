5557ab21add29faaf22ef24c5540fa3ff153b863
Andrew Beekhof <andrew@beekhof.net>
2009-10-15 11:59:51 +0200

    Low: PE: Suppress debug messages concerning the stonith-complete action being unknown
    Mercurial revision: 24990738da2bb032f0af7b18a068e4d2ca377948

--- lib/pengine/common.c
+++ lib/pengine/common.c
@@ -253,6 +253,8 @@ text2task(const char *task)
 		return no_action;	
 	} else if(safe_str_eq(task, "stonith_up")) {
 		return no_action;	
+	} else if(safe_str_eq(task, "stonith_complete")) {
+		return no_action;	
 	} else if(safe_str_eq(task, "all_stopped")) {
 		return no_action;	
 	} 
