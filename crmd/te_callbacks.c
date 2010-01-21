098b64e92895ac278e445cb5b7be7441e15786fa
Andrew Beekhof <andrew@beekhof.net>
2010-01-21 23:14:06 +0100

    Low: crmd: Log text version of fencing error codes
    Mercurial revision: 7b1030936625086f18c4ea86da302c8d8921de97

--- crmd/te_callbacks.c
+++ crmd/te_callbacks.c
@@ -350,7 +350,7 @@ tengine_stonith_callback(
 
 	CRM_CHECK(userdata != NULL, return);
 	crm_log_xml_info(output, "StonithOp");
-	crm_info("Stonith operation %d/%s result=%d", call_id, (char*)userdata, rc);
+	crm_info("Stonith operation %d/%s: %s (%d)", call_id, (char*)userdata, stonith_error2string(rc), rc);
 
 	if(AM_I_DC == FALSE) {
 	    return;
