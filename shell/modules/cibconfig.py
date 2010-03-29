327eac2e02604595f1deb9bea1b38c85a73a95c6
Dejan Muhamedagic <dejan@hello-penguin.com>
2010-03-29 15:52:07 +0200

    Dev: Shell: fix regression introduced in c7bb6cb8da24 (bad cib passed to crm_verify)
    Mercurial revision: 43547fa8cb8351e4326c6abd640d606e8f0d1495

--- shell/modules/cibconfig.py
+++ shell/modules/cibconfig.py
@@ -217,7 +217,7 @@ class CibObjectSet(object):
         See below for specific implementations.
         '''
         pass
-    def verify2(self):
+    def semantic_check(self):
         '''
         Test objects for sanity. This is about semantics.
         '''
