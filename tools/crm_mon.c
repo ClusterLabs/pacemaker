3067fb32784c612b41537af3a81c10fb582d1b97
Keisuke MORI <kskmori@intellilink.co.jp>
2010-03-29 17:51:53 +0900

    High: Tools: crm_mon - fix memory leaks exposed by valgrind
    Mercurial revision: 0425e7daa4caf38de1ff912065f268751d24aa42

--- tools/crm_mon.c
+++ tools/crm_mon.c
@@ -1617,7 +1617,7 @@ static void handle_rsc_op(xmlNode *rsc_op)
 			
     if(parse_op_key(id, &rsc, &task, &interval) == FALSE) {
 	crm_err("Invalid event detected for %s", id);
-	return;
+	goto bail;
     }
 
     while(n != NULL && safe_str_neq(XML_CIB_TAG_STATE, TYPE(n))) {
@@ -1630,7 +1630,7 @@ static void handle_rsc_op(xmlNode *rsc_op)
     }
     if(node == NULL) {
 	crm_err("No node detected for event %s (%s)", magic, id);
-	return;
+	goto bail;
     }
 
     /* look up where we expected it to be? */
@@ -1659,6 +1659,10 @@ static void handle_rsc_op(xmlNode *rsc_op)
     if(notify && external_agent) {
 	send_custom_trap(node, rsc, task, target_rc, rc, status, desc);
     }
+  bail:
+    crm_free(update_te_uuid);
+    crm_free(rsc);
+    crm_free(task);
 }
 
 void
@@ -1724,6 +1728,8 @@ crm_diff_update(const char *event, xmlNode *msg)
 		xmlNode *rsc_op = getXpathResult(xpathObj, lpc);
 		handle_rsc_op(rsc_op);
 	    }
+	}
+	if (xpathObj) {
 	    xmlXPathFreeObject(xpathObj);
 	}
     }
