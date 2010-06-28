c871cadd9a1cdeeac4343bfb88c2af44e1b52519
NAKAHIRA Kazutomo <nakahira.kazutomo@oss.ntt.co.jp>
2010-06-28 14:34:15 +0900

    Low: tools: crm_node - Enable the -A parameter, the short form of --openais
    Mercurial revision: 1e3e1af24be2bf2a15905f04c549a5d87aa49d86

--- tools/ccm_epoche.c
+++ tools/ccm_epoche.c
@@ -383,7 +383,7 @@ main(int argc, char ** argv)
 
     crm_peer_init();
     crm_log_init(NULL, LOG_WARNING, FALSE, FALSE, argc, argv, FALSE);
-    crm_set_options("?V$qepHR:ifl", "command [options]", long_options,
+    crm_set_options("?V$qepHAR:ifl", "command [options]", long_options,
 		    "Tool for displaying low-level node information");
 	
     while (flag >= 0) {
