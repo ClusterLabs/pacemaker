9ecb77d7451a91e6d4d56c19bcb928ef51372dbe
Andrew Beekhof <andrew@beekhof.net>
2010-09-10 08:15:29 +0200

    Medium: cib: Bug lf#2469 - Clean up disconnected remote connections
    Mercurial revision: 22651433eb94d18270eba3533b3531de5a02b4d0

--- lib/cib/cib_remote.c
+++ lib/cib/cib_remote.c
@@ -311,6 +311,9 @@ void
 cib_remote_connection_destroy(gpointer user_data)
 {
     crm_err("Connection destroyed");
+#ifdef HAVE_GNUTLS_GNUTLS_H
+    cib_tls_close(user_data);
+#endif
     return;
 }
 
