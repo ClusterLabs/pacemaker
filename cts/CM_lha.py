24695d3cd10b820b5e04aea2f6e3a0e5d3a9f4b6
Andrew Beekhof <andrew@beekhof.net>
2010-09-20 14:33:43 +0200

    Low: cts: Always supply a timeout for lrmadmin operations
    Mercurial revision: fd664835726a948226abd7c36a37c8aa66434e60

--- cts/CM_lha.py
+++ cts/CM_lha.py
@@ -77,7 +77,8 @@ class crm_lha(ClusterManager):
             "QuorumCmd"      : "crm_node -H -q",
             "ParitionCmd"    : "crm_node -H -p",
             "CibQuery"       : "cibadmin -Ql",
-            "ExecuteRscOp"   : "lrmadmin -n %s -E %s %s 0 %d EVERYTIME 2>&1",
+            # 300,000 == 5 minutes
+            "ExecuteRscOp"   : "lrmadmin -n %s -E %s %s 300000 %d EVERYTIME 2>&1",
             "CIBfile"        : "%s:"+CTSvars.CRM_CONFIG_DIR+"/cib.xml",
             "TmpDir"         : "/tmp",
 
