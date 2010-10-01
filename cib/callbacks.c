a90030be98a15874c331168ad9ec23e0b93f1fcc
Andrew Beekhof <andrew@beekhof.net>
2010-10-01 16:54:02 +0200

    Medium: cib: Another minor optimization
    Mercurial revision: 0104bddb202b245bf4821ed0111b67a82df8dbd5

--- cib/callbacks.c
+++ cib/callbacks.c
@@ -750,7 +750,6 @@ cib_construct_reply(xmlNode *request, xmlNode *output, int rc)
 {
 	int lpc = 0;
 	xmlNode *reply = NULL;
-	
 	const char *name = NULL;
 	const char *value = NULL;
 	const char *names[] = {
@@ -759,12 +758,13 @@ cib_construct_reply(xmlNode *request, xmlNode *output, int rc)
 		F_CIB_CLIENTID,
 		F_CIB_CALLOPTS
 	};
+	static int max = DIMOF(names);
 
 	crm_debug_4("Creating a basic reply");
 	reply = create_xml_node(NULL, "cib-reply");
 	crm_xml_add(reply, F_TYPE, T_CIB);
 
-	for(lpc = 0; lpc < DIMOF(names); lpc++) {
+	for(lpc = 0; lpc < max; lpc++) {
 		name = names[lpc];
 		value = crm_element_value(request, name);
 		crm_xml_add(reply, name, value);
