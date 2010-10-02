9dc30ec8e02a851656cbe94f47659375ae8c0a6c
Andrew Beekhof <andrew@beekhof.net>
2010-10-02 12:42:12 +0200

    Low: mcp: Allow callgrind to be enabled dynamically
    Mercurial revision: 378bb3ca7a0b7c957c04250eede8c669de7d92d2

--- mcp/pacemaker.c
+++ mcp/pacemaker.c
@@ -221,15 +221,29 @@ start_child(pcmk_child_t *child)
     uid_t uid = 0;
     struct rlimit oflimits;
     gboolean use_valgrind = FALSE;
+    gboolean use_callgrind = FALSE;
     const char *devnull = "/dev/null";
-    const char *env_valgrind = getenv("HA_VALGRIND_ENABLED");
+    const char *env_valgrind = getenv("PCMK_valgrind_enabled");
+    const char *env_callgrind = getenv("PCMK_callgrind_enabled");
     
     if(child->command == NULL) {
 	crm_info("Nothing to do for child \"%s\"", child->name);
 	return TRUE;
     }
     
-    if(env_valgrind == NULL) {
+    if(env_callgrind == NULL) {
+	use_callgrind = FALSE;
+	use_valgrind = FALSE;
+
+    } else if(crm_is_true(env_callgrind)) {
+	use_callgrind = TRUE;
+	use_valgrind = FALSE;
+
+    } else if(strstr(env_callgrind, child->name)) {
+	use_callgrind = TRUE;
+	use_valgrind = FALSE;
+
+    } else if(env_valgrind == NULL) {
 	use_valgrind = FALSE;
 
     } else if(crm_is_true(env_valgrind)) {
@@ -238,7 +252,7 @@ start_child(pcmk_child_t *child)
     } else if(strstr(env_valgrind, child->name)) {
 	use_valgrind = TRUE;
     }
-
+    
     if(use_valgrind && strlen(VALGRIND_BIN) == 0) {
 	crm_warn("Cannot enable valgrind for %s:"
 		 " The location of the valgrind binary is unknown", child->name);
@@ -262,18 +276,18 @@ start_child(pcmk_child_t *child)
 
 	/* Setup the two alternate arg arrarys */ 
 	opts_vgrind[0] = crm_strdup(VALGRIND_BIN);
-#if SUPPORT_PROFILING
-	opts_vgrind[1] = crm_strdup("--tool=callgrind");
-	opts_vgrind[2] = crm_strdup("--callgrind-out-file="CRM_STATE_DIR"/callgrind.%p");
-	opts_vgrind[3] = crm_strdup(child->command);
-	opts_vgrind[4] = NULL;
-#else
-	opts_vgrind[1] = crm_strdup(child->command);
-	opts_vgrind[2] = NULL;
-	opts_vgrind[3] = NULL;
-	opts_vgrind[4] = NULL;
-#endif
-	opts_default[0] = opts_vgrind[2];
+	if(use_callgrind) {
+	    opts_vgrind[1] = crm_strdup("--tool=callgrind");
+	    opts_vgrind[2] = crm_strdup("--callgrind-out-file="CRM_STATE_DIR"/callgrind.%p");
+	    opts_vgrind[3] = crm_strdup(child->command);
+	    opts_vgrind[4] = NULL;
+	} else {
+	    opts_vgrind[1] = crm_strdup(child->command);
+	    opts_vgrind[2] = NULL;
+	    opts_vgrind[3] = NULL;
+	    opts_vgrind[4] = NULL;
+	}
+	opts_default[0] = crm_strdup(child->command);;
 	
 #if 0
 	/* Dont set the group for now - it prevents connection to the cluster */
