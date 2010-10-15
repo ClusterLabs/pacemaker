bf501cc554f22cce940624e6f742104398da2395
Andrew Beekhof <andrew@beekhof.net>
2010-10-15 18:58:37 +0200

    Low: crmd: Determin the target rc for an action once
    Mercurial revision: 83d85b3a9787e1b18e84f18c74979cd7f94619d5

--- crmd/te_actions.c
+++ crmd/te_actions.c
@@ -254,7 +254,7 @@ cib_action_update(crm_action_t *action, int status, int op_rc)
 	const char *target_uuid = crm_element_value(action->xml, XML_LRM_ATTR_TARGET_UUID);
 
 	int call_options = cib_quorum_override|cib_scope_local;
-
+	int target_rc = get_target_rc(action);
 
 	if(status == LRM_OP_PENDING) {
 	    crm_debug("%s %d: Recording pending operation %s on %s",
@@ -308,10 +308,10 @@ cib_action_update(crm_action_t *action, int status, int op_rc)
 	op = convert_graph_action(NULL, action, status, op_rc);
 	op->call_id = -1;
 	op->user_data = generate_transition_key(
-	    transition_graph->id, action->id, get_target_rc(action), te_uuid);
+	    transition_graph->id, action->id, target_rc, te_uuid);
 	
 	xml_op = create_operation_update(
-	    rsc, op, CRM_FEATURE_SET, get_target_rc(action), __FUNCTION__, LOG_INFO);
+	    rsc, op, CRM_FEATURE_SET, target_rc, __FUNCTION__, LOG_INFO);
 	free_lrm_op(op);
 	
 	crm_debug_3("Updating CIB with \"%s\" (%s): %s %s on %s",
