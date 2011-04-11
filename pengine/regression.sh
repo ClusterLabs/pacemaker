f7f220879c18a03c9b5fd7ced6033e5ba58cf6a4
Andrew Beekhof <andrew@beekhof.net>
2011-04-11 12:38:42 +0200

    Low: PE: Bug lf#2581 - Avoid group restart when clone (re)starts on an unrelated node (regression test)
    Mercurial revision: cbacd51553eeb981698a93c93aad66232f786a18

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -286,6 +286,7 @@ do_test bug-lf-2508 "Correctly reconstruct the status of anonymous cloned groups
 do_test bug-lf-2544 "Balanced clone placement"
 do_test bug-lf-2445 "Redistribute clones with node-max > 1 and stickiness = 0"
 do_test bug-lf-2574 "Avoid clone shuffle"
+do_test bug-lf-2581 "Avoid group restart due to unrelated clone (re)start"
 
 echo ""
 do_test master-0 "Stopped -> Slave"
