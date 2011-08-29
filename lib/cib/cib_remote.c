dd3abaedc3def1fb4c57cefb9f29ec5bebe45177
Andrew Beekhof <andrew@beekhof.net>
2011-08-29 18:35:47 +1000

    High: cib: Call gnutls_bye() and shutdown() when disconnecting from remote TLS connections
    Mercurial revision: 63e01d70dea7291e708cb6e8fc43926e3897a1db

--- lib/cib/cib_remote.c
+++ lib/cib/cib_remote.c
@@ -159,13 +159,18 @@ static int
 cib_tls_close(cib_t *cib)
 {
     cib_remote_opaque_t *private = cib->variant_opaque;
+    shutdown (private->command.socket, SHUT_RDWR);     /* no more receptions */
+    shutdown (private->callback.socket, SHUT_RDWR);     /* no more receptions */
     close(private->command.socket);
     close(private->callback.socket);
+
 #ifdef HAVE_GNUTLS_GNUTLS_H
     if(private->command.encrypted) {
+	gnutls_bye(*(private->command.session), GNUTLS_SHUT_RDWR);
 	gnutls_deinit(*(private->command.session));
 	gnutls_free(private->command.session);
 
+	gnutls_bye(*(private->callback.session), GNUTLS_SHUT_RDWR);
 	gnutls_deinit(*(private->callback.session));
 	gnutls_free(private->callback.session);
 
