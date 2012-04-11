e6059370db3c5bddf3f692e043a7d0e119a256fd
unknown <yamauchi@.intellilink.co.jp>
2012-04-11 11:24:42 +0900

    Sorry....The patch whick fell out.

--- crmd/tengine.c
+++ crmd/tengine.c
@@ -204,6 +204,7 @@ do_te_invoke(long long action,
 
         if (is_timer_started(transition_timer)) {
             crm_debug("The transitioner wait for a transition timer");
+            return;
         }
 
         CRM_CHECK(graph_data != NULL,
