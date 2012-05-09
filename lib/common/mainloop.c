82bc7b0d6ee82bd9d219f4ed9f78fbf928f45716
David Vossel <dvossel@redhat.com>
2012-05-09 10:17:06 +1000

    High: IPC: Check for failures when creating the server

--- lib/common/mainloop.c
+++ lib/common/mainloop.c
@@ -412,6 +412,7 @@ pick_ipc_type(enum qb_ipc_type requested)
 qb_ipcs_service_t *mainloop_add_ipc_server(
     const char *name, enum qb_ipc_type type, struct qb_ipcs_service_handlers *callbacks) 
 {
+    int rc = 0;
     qb_ipcs_service_t* server = NULL;
 
     if(gio_map == NULL) {
@@ -420,7 +421,12 @@ qb_ipcs_service_t *mainloop_add_ipc_server(
 
     server = qb_ipcs_create(name, 0, pick_ipc_type(type), callbacks);
     qb_ipcs_poll_handlers_set(server, &gio_poll_funcs);
-    qb_ipcs_run(server);
+
+    rc = qb_ipcs_run(server);
+    if (rc < 0) {
+        crm_err("Could not start %s IPC server: %s (%d)", name, strerror(rc), rc);
+        return NULL;
+    }
 
     return server;
 }
