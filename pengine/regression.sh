54726b75f83fe3ce73de4f20e757fef2fdbf60c2
David Vossel <dvossel@redhat.com>
2012-06-11 11:38:32 -0500

    Low: pengine: cl#5069 - Regression tests for on-fail=ignore

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -425,6 +425,8 @@ do_test bug-5025-3 "Automatically clean up failcount after resource config chang
 do_test monitor-onfail-restart "bug-5058 - Monitor failure with on-fail set to restart"
 do_test monitor-onfail-stop    "bug-5058 - Monitor failure wiht on-fail set to stop"
 do_test bug-5059 "No need to restart p_stateful1:*"
+do_test bug-5069-op-enabled  "Test on-fail=ignore with failure when monitor is enabled."
+do_test bug-5069-op-disabled "Test on-fail-ignore with failure when monitor is disabled."
 
 echo ""
 do_test systemhealth1  "System Health ()               #1"
