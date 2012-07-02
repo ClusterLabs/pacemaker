02e388ecbeb222165e159fe77d3c61c669d9a0d8
Andrew Beekhof <andrew@beekhof.net>
2012-07-03 06:48:33 +1000

    Revert "Low: Build: Mark private functions as internal"
    This reverts commit 61fa54d399676de9121b67adfba7476cfdaa6c4e.

--- include/crm_internal.h
+++ include/crm_internal.h
@@ -32,12 +32,12 @@
 #  include <crm/lrmd.h>
 
 /* Dynamic loading of libraries */
-G_GNUC_INTERNAL void *find_library_function(void **handle, const char *lib, const char *fn, int fatal);
-G_GNUC_INTERNAL void *convert_const_pointer(const void *ptr);
+void *find_library_function(void **handle, const char *lib, const char *fn, int fatal);
+void *convert_const_pointer(const void *ptr);
 
 /* For ACLs */
-G_GNUC_INTERNAL char *uid2username(uid_t uid);
-G_GNUC_INTERNAL void determine_request_user(char *user, xmlNode * request, const char *field);
+char *uid2username(uid_t uid);
+void determine_request_user(char *user, xmlNode * request, const char *field);
 
 #  if ENABLE_ACL
 static inline gboolean
@@ -86,10 +86,10 @@ struct crm_option {
     long flags;
 };
 
-G_GNUC_INTERNAL void crm_set_options(const char *short_options, const char *usage, struct crm_option *long_options, const char *app_desc);
-G_GNUC_INTERNAL int crm_get_option(int argc, char **argv, int *index);
-G_GNUC_INTERNAL int crm_get_option_long(int argc, char **argv, int *index, const char **longname);
-G_GNUC_INTERNAL void crm_help(char cmd, int exit_code);
+void crm_set_options(const char *short_options, const char *usage, struct crm_option *long_options, const char *app_desc);
+int crm_get_option(int argc, char **argv, int *index);
+int crm_get_option_long(int argc, char **argv, int *index, const char **longname);
+void crm_help(char cmd, int exit_code);
 
 /* Cluster Option Processing */
 typedef struct pe_cluster_option_s {
@@ -106,29 +106,27 @@ typedef struct pe_cluster_option_s {
 
 } pe_cluster_option;
 
-G_GNUC_INTERNAL const char *cluster_option(
-    GHashTable * options, gboolean(*validate) (const char *),
-    const char *name, const char *old_name, const char *def_value);
+const char *cluster_option(GHashTable * options, gboolean(*validate) (const char *),
+                           const char *name, const char *old_name, const char *def_value);
 
-G_GNUC_INTERNAL const char *get_cluster_pref(GHashTable * options, pe_cluster_option * option_list, int len, const char *name);
+const char *get_cluster_pref(GHashTable * options, pe_cluster_option * option_list, int len, const char *name);
 
-G_GNUC_INTERNAL void config_metadata(const char *name, const char *version, const char *desc_short,
+void config_metadata(const char *name, const char *version, const char *desc_short,
                      const char *desc_long, pe_cluster_option * option_list, int len);
 
-G_GNUC_INTERNAL void verify_all_options(GHashTable * options, pe_cluster_option * option_list, int len);
-G_GNUC_INTERNAL gboolean check_time(const char *value);
-G_GNUC_INTERNAL gboolean check_timer(const char *value);
-G_GNUC_INTERNAL gboolean check_boolean(const char *value);
-G_GNUC_INTERNAL gboolean check_number(const char *value);
+void verify_all_options(GHashTable * options, pe_cluster_option * option_list, int len);
+gboolean check_time(const char *value);
+gboolean check_timer(const char *value);
+gboolean check_boolean(const char *value);
+gboolean check_number(const char *value);
 
 /* Shared PE/crmd functionality */
-G_GNUC_INTERNAL void filter_action_parameters(xmlNode * param_set, const char *version);
-G_GNUC_INTERNAL void filter_reload_parameters(xmlNode * param_set, const char *restart_string);
+void filter_action_parameters(xmlNode * param_set, const char *version);
+void filter_reload_parameters(xmlNode * param_set, const char *restart_string);
 
 /* Resource operation updates */
-G_GNUC_INTERNAL xmlNode *create_operation_update(
-    xmlNode * parent, lrmd_event_data_t *event, const char *caller_version,
-    int target_rc, const char *origin, int level);
+xmlNode *create_operation_update(xmlNode * parent, lrmd_event_data_t *event, const char *caller_version,
+                                 int target_rc, const char *origin, int level);
 
 /* char2score */
 extern int node_score_red;
@@ -144,18 +142,18 @@ crm_strlen_zero(const char *s)
     return !s || *s == '\0';
 }
 
-G_GNUC_INTERNAL char *generate_series_filename(const char *directory, const char *series, int sequence, gboolean bzip);
-G_GNUC_INTERNAL int get_last_sequence(const char *directory, const char *series);
-G_GNUC_INTERNAL void write_last_sequence(const char *directory, const char *series, int sequence, int max);
+char *generate_series_filename(const char *directory, const char *series, int sequence, gboolean bzip);
+int get_last_sequence(const char *directory, const char *series);
+void write_last_sequence(const char *directory, const char *series, int sequence, int max);
 
-G_GNUC_INTERNAL void crm_make_daemon(const char *name, gboolean daemonize, const char *pidfile);
-G_GNUC_INTERNAL gboolean crm_is_writable(const char *dir, const char *file, const char *user, const char *group, gboolean need_both);
+void crm_make_daemon(const char *name, gboolean daemonize, const char *pidfile);
+gboolean crm_is_writable(const char *dir, const char *file, const char *user, const char *group, gboolean need_both);
 
-G_GNUC_INTERNAL long long crm_int_helper(const char *text, char **end_text);
-G_GNUC_INTERNAL char *crm_concat(const char *prefix, const char *suffix, char join);
-G_GNUC_INTERNAL char *generate_hash_key(const char *crm_msg_reference, const char *sys);
-G_GNUC_INTERNAL xmlNode *crm_recv_remote_msg(void *session, gboolean encrypted);
-G_GNUC_INTERNAL void crm_send_remote_msg(void *session, xmlNode * msg, gboolean encrypted);
+long long crm_int_helper(const char *text, char **end_text);
+char *crm_concat(const char *prefix, const char *suffix, char join);
+char *generate_hash_key(const char *crm_msg_reference, const char *sys);
+xmlNode *crm_recv_remote_msg(void *session, gboolean encrypted);
+void crm_send_remote_msg(void *session, xmlNode * msg, gboolean encrypted);
 
 #  define crm_config_err(fmt...) { crm_config_error = TRUE; crm_err(fmt); }
 #  define crm_config_warn(fmt...) { crm_config_warning = TRUE; crm_warn(fmt); }
