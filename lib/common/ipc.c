b431c18ccf32e3fd7f807416bc97016f6d739882
Andrew Beekhof <andrew@beekhof.net>
2012-07-24 09:58:08 +1000

    High: ipc: Prevent use-of-NULL when closing connections in the client

--- lib/common/ipc.c
+++ lib/common/ipc.c
@@ -337,11 +337,14 @@ crm_ipc_connect(crm_ipc_t *client)
 void
 crm_ipc_close(crm_ipc_t *client) 
 {
-    crm_trace("Disconnecting %s IPC connection %p (%p.%d)", client->name, client, client->ipc);
-    if(client && client->ipc) {
-        qb_ipcc_connection_t *ipc = client->ipc;
-        client->ipc = NULL;
-        qb_ipcc_disconnect(ipc);
+    if(client) {
+        crm_trace("Disconnecting %s IPC connection %p (%p.%d)", client->name, client, client->ipc);
+
+        if(client->ipc) {
+            qb_ipcc_connection_t *ipc = client->ipc;
+            client->ipc = NULL;
+            qb_ipcc_disconnect(ipc);
+        }
     }
 }
 
