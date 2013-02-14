e12cf4022e966828ab1ea1354b011db1b7632402
Andrew Beekhof <andrew@beekhof.net>
2013-02-14 12:57:20 +1100

    Fix: Date/time: Bug cl#5118 - Correctly convert seconds-since-epoch to the current time
    Minutes were incorrectly calculated resulting in an invalid number of
    seconds.  Callers of crm_time_as_string() such as crm_resource using
    the --lifetime option were affected.

--- lib/common/iso8601.c
+++ lib/common/iso8601.c
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (C) 2005 Andrew Beekhof <andrew@beekhof.net>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -238,7 +238,7 @@ crm_time_get_sec(int sec, uint * h, uint * m, uint * s)
     hours = seconds / (60 * 60);
     seconds -= 60 * 60 * hours;
 
-    minutes = seconds / (60 * 60);
+    minutes = seconds / (60);
     seconds -= 60 * minutes;
 
     crm_trace("%d == %.2d:%.2d:%.2d", sec, hours, minutes, seconds);
