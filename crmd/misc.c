fb042a80c929e213fe7be3abe9a92f38cab43132
Andrew Beekhof <andrew@beekhof.net>
2013-02-19 16:28:12 +1100

    Log: A_LOG actions to reach the file logs

--- crmd/misc.c
+++ crmd/misc.c
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -34,7 +34,7 @@ do_log(long long action,
     unsigned log_type = LOG_TRACE;
 
     if (action & A_LOG) {
-        log_type = LOG_DEBUG;
+        log_type = LOG_INFO;
     } else if (action & A_WARN) {
         log_type = LOG_WARNING;
     } else if (action & A_ERROR) {
