be35371af33a1131161ce0d67097d4f1feea6294
Andrew Beekhof <andrew@beekhof.net>
2013-02-22 14:10:31 +1100

    Fix: crmd: Process join ack's in S_INTEGRATION
    Now that join_offer_one is supported, valid ack's requested during
    S_FINALIZE_JOIN could arrive after we've dropped back to S_INTEGRATION

--- crmd/fsa_matrix.h
+++ crmd/fsa_matrix.h
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -1122,7 +1122,7 @@ const long long crmd_fsa_actions[MAXINPUT][MAXSTATE] = {
     {
      /* S_IDLE               ==> */ A_ERROR | A_TE_HALT | A_DC_JOIN_OFFER_ALL,
      /* S_ELECTION           ==> */ A_LOG,
-     /* S_INTEGRATION        ==> */ A_LOG,
+     /* S_INTEGRATION        ==> */ A_LOG | A_CL_JOIN_RESULT  | A_DC_JOIN_PROCESS_ACK,
      /* S_FINALIZE_JOIN      ==> */ A_CL_JOIN_RESULT | A_DC_JOIN_PROCESS_ACK,
      /* S_NOT_DC             ==> */ A_ERROR | A_CL_JOIN_ANNOUNCE,
      /* S_POLICY_ENGINE      ==> */ A_ERROR | A_TE_HALT | A_DC_JOIN_OFFER_ALL,
