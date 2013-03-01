dc5f117c61c1ae5ec2888ca54649595a72de60e4
Kazunori INOUE <inouekazu@intellilink.co.jp>
2013-03-01 14:15:28 +0900

    Low: Bump the log of unset DC to info

--- crmd/utils.c
+++ crmd/utils.c
@@ -983,7 +983,7 @@ update_dc(xmlNode * msg)
         crm_info("Set DC to %s (%s)", crm_str(fsa_our_dc), crm_str(fsa_our_dc_version));
 
     } else if (last_dc != NULL) {
-        crm_debug("Unset DC. Was %s", crm_str(last_dc));
+        crm_info("Unset DC. Was %s", crm_str(last_dc));
     }
 
     free(last_dc);
