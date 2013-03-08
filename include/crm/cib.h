4474c3142575c47c248c9db2bb7ebc0e9853c082
Andrew Beekhof <andrew@beekhof.net>
2013-03-08 12:43:05 +1100

    Build: Bump library versions as appropriate

--- include/crm/cib.h
+++ include/crm/cib.h
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -175,6 +175,6 @@ void remove_cib_op_callback(int call_id, gboolean all_callbacks);
     } while(0)
 #  include <crm/cib/util.h>
 
-#  define CIB_LIBRARY "libcib.so.2"
+#  define CIB_LIBRARY "libcib.so.3"
 
 #endif
