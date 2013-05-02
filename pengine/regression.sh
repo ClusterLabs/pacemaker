31af7e1293c170523d768ab779e8740944f230ab
Andrew Beekhof <andrew@beekhof.net>
2013-05-02 14:27:59 +1000

    Test: cl#5154 - Do not expire failures when on-fail=block is present

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -435,6 +435,7 @@ do_test bug-5025-2 "Make sure clear failcount action isn't set when config does 
 do_test bug-5025-3 "Automatically clean up failcount after resource config change with restart"
 do_test bug-5025-4 "Clear failcount when last failure is a start op and rsc attributes changed."
 do_test failcount "Ensure failcounts are correctly expired"
+do_test failcount-block "Ensure failcounts are not expired when on-fail=block is present"
 do_test monitor-onfail-restart "bug-5058 - Monitor failure with on-fail set to restart"
 do_test monitor-onfail-stop    "bug-5058 - Monitor failure wiht on-fail set to stop"
 do_test bug-5059 "No need to restart p_stateful1:*"
