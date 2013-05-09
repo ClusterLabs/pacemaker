62545b8a202a5fef0aad187de096b0f938f4f832
Gao,Yan <ygao@suse.com>
2013-05-09 22:35:01 +0800

    Fix: crmd: Do not record pending delete operations in the CIB
    Otherwise, it would get stuck in an endless transition loop.

--- crmd/te_actions.c
+++ crmd/te_actions.c
@@ -464,7 +464,9 @@ te_rsc_command(crm_graph_t * graph, crm_action_t * action)
     }
 
     value = crm_meta_value(action->params, XML_OP_ATTR_PENDING);
-    if (crm_is_true(value) && safe_str_neq(task, CRMD_ACTION_CANCEL)) {
+    if (crm_is_true(value)
+        && safe_str_neq(task, CRMD_ACTION_CANCEL)
+        && safe_str_neq(task, CRMD_ACTION_DELETE)) {
         /* write a "pending" entry to the CIB, inhibit notification */
         crm_debug("Recording pending op %s in the CIB", task_uuid);
         cib_action_update(action, PCMK_LRM_OP_PENDING, PCMK_EXECRA_STATUS_UNKNOWN);
