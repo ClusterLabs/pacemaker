2e10db86f64edebe8da6e2c60bd73711fffe007b
Andrew Beekhof <andrew@beekhof.net>
2013-05-10 14:40:33 +1000

    Fix: crmd: Ensure pending and lost actions have values for last-run and last-rc-change

--- lib/transition/unpack.c
+++ lib/transition/unpack.c
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -166,10 +166,10 @@ unpack_graph(xmlNode * xml_graph, const char *reference)
   <synapse>
   <action_set>
   <rsc_op id="2"
-  ... 
+  ...
   <inputs>
   <rsc_op id="2"
-  ... 
+  ...
 */
     crm_graph_t *new_graph = NULL;
     const char *t_id = NULL;
@@ -312,6 +312,8 @@ convert_graph_action(xmlNode * resource, crm_action_t * action, int status, int 
 
     op->rc = rc;
     op->op_status = status;
+    op->t_run = time(NULL);
+    op->t_rcchange = op->t_run;
 
     op->params = g_hash_table_new_full(crm_str_hash, g_str_equal,
                                        g_hash_destroy_str, g_hash_destroy_str);
