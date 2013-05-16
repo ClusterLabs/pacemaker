9ce3edb98598f777adf22f2aecf9b9ddcf857883
Andrew Beekhof <andrew@beekhof.net>
2013-05-16 13:29:58 +1000

    Log: crmd: Attempt to display the node on which changes occurred

--- crmd/te_utils.c
+++ crmd/te_utils.c
@@ -338,9 +338,21 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
         int diff_del_updates = 0;
         int diff_del_epoch = 0;
         int diff_del_admin_epoch = 0;
+
+        const char *uname = "";
+        xmlNode *search = reason;
         xmlNode *diff = get_xpath_object("//" F_CIB_UPDATE_RESULT "//diff", reason, LOG_DEBUG_2);
 
         magic = crm_element_value(reason, XML_ATTR_TRANSITION_MAGIC);
+
+        while(search) {
+            const char *kind = TYPE(search);
+            if(safe_str_eq(XML_CIB_TAG_STATE, kind)
+               || safe_str_eq(XML_CIB_TAG_NODE, kind)) {
+                uname = get_uname(ID(search));
+            }
+            search = search->parent;
+        }
 
         if (diff) {
             cib_diff_version_details(diff,
@@ -348,22 +360,22 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                                      &diff_del_admin_epoch, &diff_del_epoch, &diff_del_updates);
             if (crm_str_eq(TYPE(reason), XML_CIB_TAG_NVPAIR, TRUE)) {
                 crm_info
-                    ("%s:%d - Triggered transition abort (complete=%d, tag=%s, id=%s, name=%s, value=%s, magic=%s, cib=%d.%d.%d) : %s",
-                     fn, line, transition_graph->complete, TYPE(reason), ID(reason), NAME(reason),
+                    ("%s:%d - Triggered transition abort (complete=%d, node=%s, tag=%s, id=%s, name=%s, value=%s, magic=%s, cib=%d.%d.%d) : %s",
+                     fn, line, transition_graph->complete, uname, TYPE(reason), ID(reason), NAME(reason),
                      VALUE(reason), magic ? magic : "NA", diff_add_admin_epoch, diff_add_epoch,
                      diff_add_updates, abort_text);
             } else {
                 crm_info
-                    ("%s:%d - Triggered transition abort (complete=%d, tag=%s, id=%s, magic=%s, cib=%d.%d.%d) : %s",
-                     fn, line, transition_graph->complete, TYPE(reason), ID(reason),
+                    ("%s:%d - Triggered transition abort (complete=%d, node=%s, tag=%s, id=%s, magic=%s, cib=%d.%d.%d) : %s",
+                     fn, line, transition_graph->complete, uname, TYPE(reason), ID(reason),
                      magic ? magic : "NA", diff_add_admin_epoch, diff_add_epoch, diff_add_updates,
                      abort_text);
             }
 
         } else {
             crm_info
-                ("%s:%d - Triggered transition abort (complete=%d, tag=%s, id=%s, magic=%s) : %s",
-                 fn, line, transition_graph->complete, TYPE(reason), ID(reason),
+                ("%s:%d - Triggered transition abort (complete=%d, node=%s, tag=%s, id=%s, magic=%s) : %s",
+                 fn, line, transition_graph->complete, uname, TYPE(reason), ID(reason),
                  magic ? magic : "NA", abort_text);
         }
 
