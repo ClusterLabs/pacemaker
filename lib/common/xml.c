3eb78af5fa5085da061c3faeb9ad9e796ea8f69a
Andrew Beekhof <andrew@beekhof.net>
2013-05-21 09:33:27 +1000

    Fix: Core: CID#1023875 - Dereference before null check

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -1025,12 +1025,13 @@ static inline void
 dump_xml_attr(xmlAttrPtr attr, int options, char **buffer, int *offset, int *max)
 {
     char *p_value = NULL;
-    const char *p_name = (const char *)attr->name;
+    const char *p_name = NULL;
 
     if (attr == NULL || attr->children == NULL) {
         return;
     }
 
+    p_name = (const char *)attr->name;
     p_value = crm_xml_escape((const char *)attr->children->content);
     buffer_print(*buffer, *max, *offset, " %s=\"%s\"", p_name, p_value);
     free(p_value);
