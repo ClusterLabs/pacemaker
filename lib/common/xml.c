08fe1ed71aff96d1edd2ee078df1efe43220da76
Andrew Beekhof <andrew@beekhof.net>
2013-05-21 10:41:50 +1000

    Fix: Core: CID#1023861 - Dereference after null check

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -139,7 +139,7 @@ insert_prefix(int options, char **buffer, int *offset, int *max, int depth)
     if (options & xml_log_option_formatted) {
         size_t spaces = 2 * depth;
 
-        if (spaces >= ((*max) - (*offset))) {
+        if ((*buffer) == NULL || spaces >= ((*max) - (*offset))) {
             (*max) = QB_MAX(CHUNK_SIZE, (*max) * 2);
             (*buffer) = realloc((*buffer), (*max) + 1);
         }
