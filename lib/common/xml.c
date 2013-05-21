632309d13615e0a1a9a1a10ac332f88ffdc5e860
Andrew Beekhof <andrew@beekhof.net>
2013-05-21 14:05:33 +1000

    Fix: Core: CID#739448 - Dereference before null check

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -2449,34 +2449,33 @@ validate_with_dtd(xmlDocPtr doc, gboolean to_logs, const char *dtd_file)
     CRM_CHECK(dtd_file != NULL, return FALSE);
 
     dtd = xmlParseDTD(NULL, (const xmlChar *)dtd_file);
-    CRM_CHECK(dtd != NULL, crm_err("Could not find/parse %s", dtd_file);
-              goto cleanup);
+    if(dtd == NULL) {
+        crm_err("Could not locate/parse DTD: %s", dtd_file);
+        return TRUE;
+    }
 
     cvp = xmlNewValidCtxt();
-    CRM_CHECK(cvp != NULL, goto cleanup);
+    if(cvp) {
+        if (to_logs) {
+            cvp->userData = (void *)LOG_ERR;
+            cvp->error = (xmlValidityErrorFunc) xml_log;
+            cvp->warning = (xmlValidityWarningFunc) xml_log;
+        } else {
+            cvp->userData = (void *)stderr;
+            cvp->error = (xmlValidityErrorFunc) fprintf;
+            cvp->warning = (xmlValidityWarningFunc) fprintf;
+        }
 
-    if (to_logs) {
-        cvp->userData = (void *)LOG_ERR;
-        cvp->error = (xmlValidityErrorFunc) xml_log;
-        cvp->warning = (xmlValidityWarningFunc) xml_log;
-    } else {
-        cvp->userData = (void *)stderr;
-        cvp->error = (xmlValidityErrorFunc) fprintf;
-        cvp->warning = (xmlValidityWarningFunc) fprintf;
-    }
-
-    if (!xmlValidateDtd(cvp, doc, dtd)) {
-        valid = FALSE;
-    }
-
-  cleanup:
-    if (cvp) {
+        if (!xmlValidateDtd(cvp, doc, dtd)) {
+            valid = FALSE;
+        }
         xmlFreeValidCtxt(cvp);
-    }
-    if (dtd) {
-        xmlFreeDtd(dtd);
+
+    } else {
+        crm_err("Internal error: No valid context");
     }
 
+    xmlFreeDtd(dtd);
     return valid;
 }
 
