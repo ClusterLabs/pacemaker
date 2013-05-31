9ff33ea63979b5613d8098c9917806e1fe554928
Andrew Beekhof <andrew@beekhof.net>
2013-05-31 14:31:28 +1000

    Test: tools: Overhaul regression tests

--- tools/regression.sh
+++ tools/regression.sh
@@ -6,28 +6,30 @@ num_errors=0
 num_passed=0
 GREP_OPTIONS=
 
-function assert() {
-    rc=$1; shift
+function test_assert() {
     target=$1; shift
-    app=$1; shift
-    msg=$1; shift
     cib=$1; shift
+    app=`echo "$cmd" | sed 's/\ .*//'`
+    printf "* Running: $app - $desc\n" 1>&2
 
-    if [ x$cib = x0 ]; then
-	: nothing
-    else
+    printf "=#=#=#= Begin test: $desc =#=#=#=\n"
+    eval $VALGRIND_CMD $cmd 2>&1
+    rc=$?
+
+    if [ x$cib != x0 ]; then
 	cibadmin -Q
     fi
 
+    printf "=#=#=#= End test: $desc - `crm_error $rc` ($rc) =#=#=#=\n"
+
     if [ $rc -ne $target ]; then
 	num_errors=`expr $num_errors + 1`
-	printf "* Failed (rc=%.3d): %-14s - %s\n" $rc $app "$msg"
-	printf "* Failed (rc=%.3d): %-14s - %s\n" $rc $app "$msg" 1>&2
+	printf "* Failed (rc=%.3d): %-14s - %s\n" $rc $app "$desc"
+	printf "* Failed (rc=%.3d): %-14s - %s\n" $rc $app "$desc" 1>&2
 	return
 	exit 1
     else
-	printf "* Passed: %-14s - %s\n" $app "$msg"
-	printf "* Passed: %-14s - %s\n" $app "$msg" 1>&2
+	printf "* Passed: %-14s - %s\n" $app "$desc"
 
 	num_passed=`expr $num_passed + 1`
     fi
@@ -66,182 +68,263 @@ function test_tools() {
     export CIB_shadow_dir=$test_home
     $VALGRIND_CMD crm_shadow --batch --force --create-empty $shadow  2>&1
     export CIB_shadow=$shadow
-    $VALGRIND_CMD cibadmin -Q 2>&1
 
-    $VALGRIND_CMD cibadmin -E 2>&1
-    assert $? 22 cibadmin "Require --force for CIB erasure"
+    desc="Validate CIB"
+    cmd="cibadmin -Q"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -E --force
-    assert $? 0 cibadmin "Allow CIB erasure with --force"
+    desc="Require --force for CIB erasure"
+    cmd="cibadmin -E"
+    test_assert 22
 
-    $VALGRIND_CMD cibadmin -Q > /tmp/$$.existing.xml
-    assert $? 0 cibadmin "Query CIB"
+    desc="Allow CIB erasure with --force"
+    cmd="cibadmin -E --force"
+    test_assert 0
 
-    $VALGRIND_CMD crm_attribute -n cluster-delay -v 60s
-    assert $? 0 crm_attribute "Set cluster option"
+    desc="Query CIB"
+    cmd="cibadmin -Q > /tmp/$$.existing.xml"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -Q -o crm_config | grep cib-bootstrap-options-cluster-delay
-    assert $? 0 cibadmin "Query new cluster option"
+    desc="Set cluster option"
+    cmd="crm_attribute -n cluster-delay -v 60s"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -Q -o crm_config > /tmp/$$.opt.xml
-    assert $? 0 cibadmin "Query cluster options"
+    desc="Query new cluster option"
+    cmd="cibadmin -Q -o crm_config | grep cib-bootstrap-options-cluster-delay"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -D -o crm_config --xml-text '<nvpair id="cib-bootstrap-options-cluster-delay"/>'
-    assert $? 0 cibadmin "Delete nvpair"
+    desc="Query cluster options"
+    cmd="cibadmin -Q -o crm_config > /tmp/$$.opt.xml"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -C -o crm_config --xml-file /tmp/$$.opt.xml 2>&1
-    assert $? 76 cibadmin "Create operaton should fail with: -76, The object already exists"
+    desc="Set no-quorum policy"
+    cmd="crm_attribute -n no-quorum-policy -v ignore"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -M -o crm_config --xml-file /tmp/$$.opt.xml
-    assert $? 0 cibadmin "Modify cluster options section"
+    desc="Delete nvpair"
+    cmd="cibadmin -D -o crm_config --xml-text '<nvpair id=\"cib-bootstrap-options-cluster-delay\"/>'"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -Q -o crm_config | grep cib-bootstrap-options-cluster-delay
-    assert $? 0 cibadmin "Query updated cluster option"
+    desc="Create operaton should fail"
+    cmd="cibadmin -C -o crm_config --xml-file /tmp/$$.opt.xml"
+    test_assert 76
 
-    $VALGRIND_CMD crm_attribute -n cluster-delay -v 40s -s duplicate
-    assert $? 0 crm_attribute "Set duplicate cluster option"
+    desc="Modify cluster options section"
+    cmd="cibadmin -M -o crm_config --xml-file /tmp/$$.opt.xml"
+    test_assert 0
 
-    $VALGRIND_CMD crm_attribute -n cluster-delay -v 30s
-    assert $? 234 crm_attribute "Setting multiply defined cluster option should fail with -216, Could not set cluster option"
+    desc="Query updated cluster option"
+    cmd="cibadmin -Q -o crm_config | grep cib-bootstrap-options-cluster-delay"
+    test_assert 0
 
-    $VALGRIND_CMD crm_attribute -n cluster-delay -v 30s -s duplicate
-    assert $? 0 crm_attribute "Set cluster option with -s"
+    desc="Set duplicate cluster option"
+    cmd="crm_attribute -n cluster-delay -v 40s -s duplicate"
+    test_assert 0
 
-    $VALGRIND_CMD crm_attribute -n cluster-delay -D -i cib-bootstrap-options-cluster-delay
-    assert $? 0 crm_attribute "Delete cluster option with -i"
+    desc="Setting multiply defined cluster option should fail"
+    cmd="crm_attribute -n cluster-delay -v 30s"
+    test_assert 76
 
-    $VALGRIND_CMD cibadmin -C -o nodes --xml-text '<node id="clusterNode-UUID" uname="clusterNode-UNAME" type="member">'
-    assert $? 0 cibadmin "Create node entry"
+    desc="Set cluster option with -s"
+    cmd="crm_attribute -n cluster-delay -v 30s -s duplicate"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -C -o status --xml-text '<node_state id="clusterNode-UUID" uname="clusterNode-UNAME"/>'
-    assert $? 0 cibadmin "Create node status entry"
+    desc="Delete cluster option with -i"
+    cmd="crm_attribute -n cluster-delay -D -i cib-bootstrap-options-cluster-delay"
+    test_assert 0
 
-    $VALGRIND_CMD crm_attribute -n ram -v 1024M -U clusterNode-UNAME -t nodes
-    assert $? 0 crm_attribute "Create node attribute"
+    desc="Create node1 and bring it online"
+    cmd="crm_simulate --live-check --in-place --node-up=node1"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -Q -o nodes | grep clusterNode-UUID-ram
-    assert $? 0 cibadmin "Query new node attribute"
+    desc="Create node attribute"
+    cmd="crm_attribute -n ram -v 1024M -U node1 -t nodes"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -Q | cibadmin -5 -p 2>&1 > /dev/null
-    assert $? 0 cibadmin "Digest calculation"
+    desc="Query new node attribute"
+    cmd="cibadmin -Q -o nodes | grep node1-ram"
+    test_assert 0
+
+    desc="Digest calculation"
+    cmd="cibadmin -Q | cibadmin -5 -p 2>&1 > /dev/null"
+    test_assert 0
 
     # This update will fail because it has version numbers
-    $VALGRIND_CMD cibadmin -R --xml-file /tmp/$$.existing.xml 2>&1
-    assert $? 205 cibadmin "Replace operation should fail with: 205, Update was older than existing configuration"
+    desc="Replace operation should fail"
+    cmd="cibadmin -R --xml-file /tmp/$$.existing.xml"
+    test_assert 205
 
-    crm_standby -N clusterNode-UNAME -G
-    assert $? 0 crm_standby "Default standby value"
+    desc="Default standby value"
+    cmd="crm_standby -N node1 -G"
+    test_assert 0
+ 
+    desc="Set standby status"
+    cmd="crm_standby -N node1 -v true"
+    test_assert 0
+ 
+    desc="Query standby value"
+    cmd="crm_standby -N node1 -G"
+    test_assert 0
+ 
+    desc="Delete standby value"
+    cmd="crm_standby -N node1 -D"
+    test_assert 0
 
-    crm_standby -N clusterNode-UNAME -v true
-    assert $? 0 crm_standby "Set standby status"
+    desc="Create a resource"
+    cmd="cibadmin -C -o resources --xml-text '<primitive id=\"dummy\" class=\"ocf\" provider=\"pacemaker\" type=\"Dummy\"/>'"
+    test_assert 0
 
-    crm_standby -N clusterNode-UNAME -G
-    assert $? 0 crm_standby "Query standby value"
+    desc="Create a resource meta attribute"
+    cmd="crm_resource -r dummy --meta -p is-managed -v false"
+    test_assert 0
 
-    crm_standby -N clusterNode-UNAME -D 2>&1
-    assert $? 0 crm_standby "Delete standby value"
+    desc="Query a resource meta attribute"
+    cmd="crm_resource -r dummy --meta -g is-managed"
+    test_assert 0
 
-    $VALGRIND_CMD cibadmin -C -o resources --xml-text '<primitive id="dummy" class="ocf" provider="pacemaker" type="Dummy"/>'
-    assert $? 0 cibadmin "Create a resource"
+    desc="Remove a resource meta attribute"
+    cmd="crm_resource -r dummy --meta -d is-managed"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy --meta -p is-managed -v false
-    assert $? 0 crm_resource "Create a resource meta attribute"
+    desc="Create a resource attribute"
+    cmd="crm_resource -r dummy -p delay -v 10s"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy --meta -g is-managed
-    assert $? 0 crm_resource "Query a resource meta attribute"
+    desc="List the configured resources"
+    cmd="crm_resource -L"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy --meta -d is-managed
-    assert $? 0 crm_resource "Remove a resource meta attribute"
+    desc="Set a resource's fail-count"
+    cmd="crm_failcount -r dummy -v 10 -N node1"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -p delay -v 10s
-    assert $? 0 crm_resource "Create a resource attribute"
+    desc="Require a destination when migrating a resource that is stopped"
+    cmd="crm_resource -r dummy -M"
+    test_assert 234
 
-    $VALGRIND_CMD crm_resource -L
-    assert $? 0 crm_resource "List the configured resources"
+    desc="Don't support migration to non-existant locations"
+    cmd="crm_resource -r dummy -M -N i.dont.exist"
+    test_assert 250
 
-    crm_failcount -r dummy -v 10 -N clusterNode-UNAME 2>&1
-    assert $? 0 crm_resource "Set a resource's fail-count"
+    desc="Create a fencing resource"
+    cmd="cibadmin -C -o resources --xml-text '<primitive id=\"Fence\" class=\"stonith\" type=\"fence_true\"/>'"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -M 2>&1
-    assert $? 234 crm_resource "Require a destination when migrating a resource that is stopped"
+    desc="Bring resources online"
+    cmd="crm_simulate --live-check --in-place -S"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -M -N i.dont.exist 2>&1
-    assert $? 250 crm_resource "Don't support migration to non-existant locations"
+    desc="Try to move a resource to its existing location"
+    cmd="crm_resource -r dummy --move --host node1"
+    test_assert 234
 
-    $VALGRIND_CMD crm_resource -r dummy -M -N clusterNode-UNAME
-    assert $? 0 crm_resource "Migrate a resource"
+    desc="Move a resource from its existing location"
+    cmd="crm_resource -r dummy --move"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -U 1>&2
-    assert $? 0 crm_resource "Un-migrate a resource"
+    desc="Clear out constraints generated by --move"
+    cmd="crm_resource -r dummy --clear"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -G granted -d false
-    assert $? 0 crm_ticket "Default ticket granted state"
+    desc="Default ticket granted state"
+    cmd="crm_ticket -t ticketA -G granted -d false"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -r --force
-    assert $? 0 crm_ticket "Set ticket granted state"
+    desc="Set ticket granted state"
+    cmd="crm_ticket -t ticketA -r --force"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -G granted
-    assert $? 0 crm_ticket "Query ticket granted state"
+    desc="Query ticket granted state"
+    cmd="crm_ticket -t ticketA -G granted"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -D granted --force
-    assert $? 0 crm_ticket "Delete ticket granted state"
+    desc="Delete ticket granted state"
+    cmd="crm_ticket -t ticketA -D granted --force"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -s
-    assert $? 0 crm_ticket "Make a ticket standby"
+    desc="Make a ticket standby"
+    cmd="crm_ticket -t ticketA -s"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -G standby
-    assert $? 0 crm_ticket "Query ticket standby state"
+    desc="Query ticket standby state"
+    cmd="crm_ticket -t ticketA -G standby"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -a
-    assert $? 0 crm_ticket "Activate a ticket"
+    desc="Activate a ticket"
+    cmd="crm_ticket -t ticketA -a"
+    test_assert 0
 
-    $VALGRIND_CMD crm_ticket -t ticketA -D standby
-    assert $? 0 crm_ticket "Delete ticket standby state"
+    desc="Delete ticket standby state"
+    cmd="crm_ticket -t ticketA -D standby"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -B -N node1 2>&1
-    assert $? 250 crm_resource "Ban a resource on unknown node"
+    desc="Ban a resource on unknown node"
+    cmd="crm_resource -r dummy -B -N host1"
+    test_assert 250
 
-    $VALGRIND_CMD crm_simulate --live-check --in-place --node-up=node1 --node-up=node2 --node-up=node3
-    assert $? 0 crm_simulate "Create three nodes and bring them online"
+    desc="Create two more nodes and bring them online"
+    cmd="crm_simulate --live-check --in-place --node-up=node2 --node-up=node3"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -B -N node1 2>&1
-    assert $? 0 crm_resource "Ban dummy from node1"
+    desc="Ban dummy from node1"
+    cmd="crm_resource -r dummy -B -N node1"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -B -N node2 2>&1
-    assert $? 0 crm_resource "Ban dummy from node2"
+    desc="Ban dummy from node2"
+    cmd="crm_resource -r dummy -B -N node2"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -M -N node1 2>&1
-    assert $? 0 crm_resource "Move dummy to node1"
+    desc="Relocate resources due to ban"
+    cmd="crm_simulate --live-check --in-place -S"
+    test_assert 0
 
-    $VALGRIND_CMD crm_resource -r dummy -U -N node2
-    assert $? 0 crm_resource "Clear implicit constraints for dummy on node2"
+    desc="Move dummy to node1"
+    cmd="crm_resource -r dummy -M -N node1"
+    test_assert 0
+
+    desc="Clear implicit constraints for dummy on node2"
+    cmd="crm_resource -r dummy -U -N node2"
+    test_assert 0
  }
 
 function test_date() {
-#    $VALGRIND_CMD cibadmin -Q
     for y in 06 07 08 09 10 11 12 13 14 15 16 17 18; do
-	$VALGRIND_CMD iso8601 -d "20$y-W01-7 00Z"
-	$VALGRIND_CMD iso8601 -d "20$y-W01-7 00Z" -W -E "20$y-W01-7 00:00:00Z"
-	assert $? 0 iso8601 "20$y-W01-7" 0
-	$VALGRIND_CMD iso8601 -d "20$y-W01-1 00Z"
-	$VALGRIND_CMD iso8601 -d "20$y-W01-1 00Z" -W -E "20$y-W01-1 00:00:00Z"
-	assert $? 0 iso8601 "20$y-W01-1" 0
+	desc="20$y-W01-7"
+	cmd="iso8601 -d '20$y-W01-7 00Z'"
+	test_assert 0 0
+
+	desc="20$y-W01-7 - round-trip"
+	cmd="iso8601 -d '20$y-W01-7 00Z' -W -E '20$y-W01-7 00:00:00Z'"
+	test_assert 0 0
+
+	desc="20$y-W01-1"
+	cmd="iso8601 -d '20$y-W01-1 00Z'"
+	test_assert 0 0
+
+	desc="20$y-W01-1 - round-trip"
+	cmd="iso8601 -d '20$y-W01-1 00Z' -W -E '20$y-W01-1 00:00:00Z'"
+	test_assert 0 0
     done
 
-    $VALGRIND_CMD iso8601 -d "2009-W53-7 00:00:00Z" -W -E "2009-W53-7 00:00:00Z"
-    assert $? 0 iso8601 "2009-W53-07" 0
+    desc="2009-W53-07"
+    cmd="iso8601 -d '2009-W53-7 00:00:00Z' -W -E '2009-W53-7 00:00:00Z'"
+    test_assert 0 0
 
-    $VALGRIND_CMD iso8601 -d "2009-01-31 00:00:00Z" -D "P1M" -E "2009-02-28 00:00:00Z"
-    assert $? 0 iso8601 "2009-01-31 + 1 Month" 0
+    desc="2009-01-31 + 1 Month"
+    cmd="iso8601 -d '2009-01-31 00:00:00Z' -D P1M -E '2009-02-28 00:00:00Z'"
+    test_assert 0 0
 
-    $VALGRIND_CMD iso8601 -d "2009-01-31 00:00:00Z" -D "P2M" -E "2009-03-31 00:00:00Z"
-    assert $? 0 iso8601 "2009-01-31 + 2 Months" 0
+    desc="2009-01-31 + 2 Months"
+    cmd="iso8601 -d '2009-01-31 00:00:00Z' -D P2M -E '2009-03-31 00:00:00Z'"
+    test_assert 0 0
 
-    $VALGRIND_CMD iso8601 -d "2009-01-31 00:00:00Z" -D "P3M" -E "2009-04-30 00:00:00Z"
-    assert $? 0 iso8601 "2009-01-31 + 3 Months" 0
+    desc="2009-01-31 + 3 Months"
+    cmd="iso8601 -d '2009-01-31 00:00:00Z' -D P3M -E '2009-04-30 00:00:00Z'"
+    test_assert 0 0
 
-    $VALGRIND_CMD iso8601 -d "2009-03-31 00:00:00Z" -D "P-1M" -E "2009-02-28 00:00:00Z"
-    assert $? 0 iso8601 "2009-03-31 - 1 Month" 0
+    desc="2009-03-31 - 1 Month"
+    cmd="iso8601 -d '2009-03-31 00:00:00Z' -D P-1M -E '2009-02-28 00:00:00Z'"
+    test_assert 0 0
  }
 
 echo "Testing dates"
@@ -256,19 +339,23 @@ if [ $do_save = 1 ]; then
     cp $test_home/regression.out $test_home/regression.exp
 fi
 
+failed=0
+
+echo -e "\n\nResults"
+diff -wu $test_home/regression.exp $test_home/regression.out
+if [ $? != 0 ]; then
+    failed=1
+fi
+
+echo -e "\n\nSummary"
 grep -e "^*" $test_home/regression.out
 
 if [ $num_errors != 0 ]; then
     echo $num_errors tests failed
-    diff -wu $test_home/regression.exp $test_home/regression.out
     exit 1
-fi
-
-diff -wu $test_home/regression.exp $test_home/regression.out
-if [ $? != 0 ]; then
+elif [ $failed = 1 ]; then
     echo $num_passed tests passed but diff failed
     exit 2
-
 else
     echo $num_passed tests passed
     exit 0
