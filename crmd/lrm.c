84ca685d4d9cab5c9606a29b187082d31ec8488b
Andrew Beekhof <andrew@beekhof.net>
2013-08-01 20:20:46 +1000

    Fix: crmd: Prevent crash by passing log arguments in the correct order

--- crmd/lrm.c
+++ crmd/lrm.c
@@ -340,7 +340,7 @@ lrm_state_verify_stopped(lrm_state_t * lrm_state, enum crmd_fsa_state cur_state,
             lrm_state->pending_ops, stop_recurring_actions, lrm_state);
 
         crm_notice("Stopped %u recurring operations at %s (%u ops remaining)",
-                   g_hash_table_size(lrm_state->pending_ops), removed, when);
+                   g_hash_table_size(lrm_state->pending_ops), when, removed);
     }
 
     if (lrm_state->pending_ops) {
