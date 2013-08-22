5d63bac1297d89448139202a1e72980262dc1d6e
Takatoshi MATSUO <matsuo.tak@gmail.com>
2013-08-22 16:31:22 +0900

    Low: tools: remove unnecessary function of crm_mon

--- tools/crm_mon.c
+++ tools/crm_mon.c
@@ -719,37 +719,6 @@ main(int argc, char **argv)
     return 0;                   /* never reached */
 }
 
-void
-wait_for_refresh(int offset, const char *prefix, int msec)
-{
-    int lpc = msec / 1000;
-    struct timespec sleept = { 1, 0 };
-
-    if (as_console == FALSE) {
-        timer_id = g_timeout_add(msec, mon_timer_popped, NULL);
-        return;
-    }
-
-    crm_notice("%sRefresh in %ds...", prefix ? prefix : "", lpc);
-    while (lpc > 0) {
-#if CURSES_ENABLED
-        move(offset, 0);
-/* 		printw("%sRefresh in \033[01;32m%ds\033[00m...", prefix?prefix:"", lpc); */
-        printw("%sRefresh in %ds...\n", prefix ? prefix : "", lpc);
-        clrtoeol();
-        refresh();
-#endif
-        lpc--;
-        if (lpc == 0) {
-            timer_id = g_timeout_add(1000, mon_timer_popped, NULL);
-        } else {
-            if (nanosleep(&sleept, NULL) != 0) {
-                return;
-            }
-        }
-    }
-}
-
 #define mon_warn(fmt...) do {			\
 	if (!has_warnings) {			\
 	    print_as("Warning:");		\
