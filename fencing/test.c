3d1417f0265e3d055706342e19d7478551c4e2df
David Vossel <dvossel@redhat.com>
2013-09-30 21:35:25 -0500

    Low: fencing: Update return codes for regression tests

--- fencing/test.c
+++ fencing/test.c
@@ -427,7 +427,7 @@ test_async_fence_timeout(int check_event)
     int rc = 0;
 
     if (check_event) {
-        if (callback_rc != -EHOSTUNREACH) {
+        if (callback_rc != -ENODEV) {
             mainloop_test_done(FALSE);
         } else {
             mainloop_test_done(TRUE);
