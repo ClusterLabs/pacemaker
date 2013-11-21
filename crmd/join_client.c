ba43cc0d70a33899f9cf25fb6c106ba75d3704f1
Andrew Beekhof <andrew@beekhof.net>
2013-11-21 14:35:06 +1100

    Fix: crmd: Avoid deleting the 'shutdown' attribute
    Always supply a value so that any changes can get written to the DC
    rather than locally (because our peers may not know about the
    cib_mixed_update flag yet).
    
    This ensures we can always shut down.

--- crmd/join_client.c
+++ crmd/join_client.c
@@ -264,7 +264,7 @@ do_cl_join_finalize_respond(long long action,
             /* Just in case attrd was still around too */
             if (is_not_set(fsa_input_register, R_SHUTDOWN)) {
                 update_attrd(fsa_our_uname, "terminate", NULL, NULL, FALSE);
-                update_attrd(fsa_our_uname, XML_CIB_ATTR_SHUTDOWN, NULL, NULL, FALSE);
+                update_attrd(fsa_our_uname, XML_CIB_ATTR_SHUTDOWN, "0", NULL, FALSE);
             }
         }
 
