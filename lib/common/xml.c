cc84d5e8f2b09acaf33a4114e70961291f0394bd
Andrew Beekhof <andrew@beekhof.net>
2013-12-11 12:09:43 +1100

    Fix: xml: Prevent use-after-free of ID values

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -751,7 +751,7 @@ __subtract_xml_object(xmlNode * target, xmlNode * patch)
     xmlNode *target_child = NULL;
     xmlAttrPtr xIter = NULL;
 
-    const char *id = NULL;
+    char *id = NULL;
     const char *name = NULL;
     const char *value = NULL;
 
@@ -765,17 +765,18 @@ __subtract_xml_object(xmlNode * target, xmlNode * patch)
         subtract_xml_comment(target->parent, target, patch, &dummy);
     }
 
-    id = ID(target);
     name = crm_element_name(target);
     CRM_CHECK(name != NULL, return);
     CRM_CHECK(safe_str_eq(crm_element_name(target), crm_element_name(patch)), return);
     CRM_CHECK(safe_str_eq(ID(target), ID(patch)), return);
 
     /* check for XML_DIFF_MARKER in a child */
+    id = crm_element_value_copy(target, XML_ATTR_ID);
     value = crm_element_value(patch, XML_DIFF_MARKER);
     if (value != NULL && strcmp(value, "removed:top") == 0) {
         crm_trace("We are the root of the deletion: %s.id=%s", name, id);
         free_xml(target);
+        free(id);
         return;
     }
 
@@ -800,6 +801,7 @@ __subtract_xml_object(xmlNode * target, xmlNode * patch)
 
         __subtract_xml_object(target_child, patch_child);
     }
+    free(id);
 }
 
 static void
