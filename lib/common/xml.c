b879934c00fb34748d090db0247721c8726f1add
Andrew Beekhof <andrew@beekhof.net>
2013-12-17 11:31:58 +1100

    Fix: xml: Mark children of created nodes as also created

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -226,12 +226,17 @@ crm_node_dirty(xmlNode *xml)
 static void
 crm_node_created(xmlNode *xml) 
 {
+    xmlNode *cIter = NULL;
     xml_private_t *p = xml->_private;
 
     if(p && TRACKING_CHANGES(xml)) {
         if(is_not_set(p->flags, xpf_created)) {
             p->flags |= xpf_created;
             crm_node_dirty(xml);
+        }
+
+        for (cIter = __xml_first_child(xml); cIter != NULL; cIter = __xml_next(cIter)) {
+           crm_node_created(cIter);
         }
     }
 }
