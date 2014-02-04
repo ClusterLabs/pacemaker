6d0eaa01fe3646336036b00f312915f8e8c5ba30
Andrew Beekhof <andrew@beekhof.net>
2014-02-05 10:56:51 +1100

    Refactor: dbus: Use native function for adding arguments to messages
    (cherry picked from commit bab144c83e90785c7df01423d3b239cbd4a2cd55)

--- lib/services/pcmk-dbus.h
+++ lib/services/pcmk-dbus.h
@@ -5,7 +5,6 @@ void pcmk_dbus_disconnect(DBusConnection *connection);
 bool pcmk_dbus_send(DBusMessage *msg, DBusConnection *connection,
                     void(*done)(DBusPendingCall *pending, void *user_data), void *user_data);
 DBusMessage *pcmk_dbus_send_recv(DBusMessage *msg, DBusConnection *connection, DBusError *error);
-bool pcmk_dbus_append_arg(DBusMessage *msg, int dtype, const void *value);
 bool pcmk_dbus_type_check(DBusMessage *msg, DBusMessageIter *field, int expected, const char *function, int line);
 char *pcmk_dbus_get_property(DBusConnection *connection, const char *target, const char *obj, const gchar * iface, const char *name);
 
