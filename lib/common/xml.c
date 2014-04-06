921734f34e0202387eb8af0ead8bb8c3777c7f84
Andrew Beekhof <andrew@beekhof.net>
2014-04-07 08:33:02 +1000

    Fix: cib: Determine the best value of validate-with if one is not supplied

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -5266,29 +5266,45 @@ validate_xml_verbose(xmlNode * xml_blob)
 gboolean
 validate_xml(xmlNode * xml_blob, const char *validation, gboolean to_logs)
 {
-    int lpc = 0;
+    int version = 0;
 
     if (validation == NULL) {
         validation = crm_element_value(xml_blob, XML_ATTR_VALIDATION);
     }
 
     if (validation == NULL) {
+        int lpc = 0;
+        bool valid = FALSE;
+
         validation = crm_element_value(xml_blob, "ignore-dtd");
         if (crm_is_true(validation)) {
-            validation = "none";
-        } else {
-            validation = "pacemaker-1.0";
+            /* Legacy compatibilty */
+            crm_xml_add(xml_blob, XML_ATTR_VALIDATION, "none");
+            return TRUE;
         }
+
+        /* Work it out */
+        for (lpc = 0; lpc < all_schemas; lpc++) {
+            if(validate_with(xml_blob, lpc, FALSE)) {
+                valid = TRUE;
+                crm_xml_add(xml_blob, XML_ATTR_VALIDATION, known_schemas[lpc].name);
+                crm_info("XML validated against %s", known_schemas[lpc].name);
+                if(known_schemas[lpc].after_transform == 0) {
+                    break;
+                }
+            }
+        }
+
+        return valid;
     }
 
+    version = get_schema_version(validation);
     if (strcmp(validation, "none") == 0) {
         return TRUE;
-    }
 
-    for (; lpc < all_schemas; lpc++) {
-        if (known_schemas[lpc].name && strcmp(validation, known_schemas[lpc].name) == 0) {
-            return validate_with(xml_blob, lpc, to_logs);
-        }
+    } else if(version < all_schemas) {
+        return validate_with(xml_blob, version, to_logs);
+
     }
 
     crm_err("Unknown validator: %s", validation);
