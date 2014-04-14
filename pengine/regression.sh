30ea303ba7078271e6d7d01dab9e408c07a78c10
Gao,Yan <ygao@suse.com>
2014-04-14 20:48:42 +0800

    Test: pengine: Ignore failure-timeout only if the failed operation has on-fail="block"

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -486,6 +486,7 @@ do_test bug-5059 "No need to restart p_stateful1:*"
 do_test bug-5069-op-enabled  "Test on-fail=ignore with failure when monitor is enabled."
 do_test bug-5069-op-disabled "Test on-fail-ignore with failure when monitor is disabled."
 do_test obsolete-lrm-resource "cl#5115 - Do not use obsolete lrm_resource sections"
+do_test expire-non-blocked-failure "Ignore failure-timeout only if the failed operation has on-fail=block"
 
 do_test ignore_stonith_rsc_order1 "cl#5056- Ignore order constraint between stonith and non-stonith rsc."
 do_test ignore_stonith_rsc_order2 "cl#5056- Ignore order constraint with group rsc containing mixed stonith and non-stonith."
