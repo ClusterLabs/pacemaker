0e2f2156fde48e76ac3aabfca898c5567122a71c
Andrew Beekhof <andrew@beekhof.net>
2014-05-28 16:48:06 +1000

    Fix: attrd: Ensure attribute dampening is always observed

--- attrd/commands.c
+++ attrd/commands.c
@@ -464,6 +464,15 @@ attrd_peer_update(crm_node_t *peer, xmlNode *xml, bool filter)
 
     a->changed |= changed;
 
+    if(changed) {
+        if(a->timer) {
+            crm_trace("Delayed write out (%dms) for %s", a->timeout_ms, a->id);
+            mainloop_timer_start(a->timer);
+        } else {
+            write_or_elect_attribute(a);
+        }
+    }
+
     /* this only involves cluster nodes. */
     if(v->nodeid == 0 && (v->is_remote == FALSE)) {
         if(crm_element_value_int(xml, F_ATTRD_HOST_ID, (int*)&v->nodeid) == 0) {
@@ -474,15 +483,6 @@ attrd_peer_update(crm_node_t *peer, xmlNode *xml, bool filter)
                 write_attributes(FALSE, TRUE);
                 return;
             }
-        }
-    }
-
-    if(changed) {
-        if(a->timer) {
-            crm_trace("Delayed write out (%dms) for %s", a->timeout_ms, a->id);
-            mainloop_timer_start(a->timer);
-        } else {
-            write_or_elect_attribute(a);
         }
     }
 }
