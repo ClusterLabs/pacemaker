5e5dde9a16677e19dfe22068273ed87fa71ca678
Andrew Beekhof <andrew@beekhof.net>
2014-08-15 09:40:35 +1000

    Fix: solaris: Correctly replace the linux swab functions

--- lib/common/remote.c
+++ lib/common/remote.c
@@ -67,25 +67,25 @@ const int anon_tls_kx_order[] = {
  * casts are necessary for constants, because we never know how for sure
  * how U/UL/ULL map to __u16, __u32, __u64. At least not in a portable way.
  */
-#define ___swab16(x) ((__u16)(                                  \
-        (((__u16)(x) & (__u16)0x00ffU) << 8) |                  \
-        (((__u16)(x) & (__u16)0xff00U) >> 8)))
+#define __swab16(x) ((uint16_t)(                                      \
+        (((uint16_t)(x) & (uint16_t)0x00ffU) << 8) |                  \
+        (((uint16_t)(x) & (uint16_t)0xff00U) >> 8)))
 
-#define ___swab32(x) ((__u32)(                                  \
-        (((__u32)(x) & (__u32)0x000000ffUL) << 24) |            \
-        (((__u32)(x) & (__u32)0x0000ff00UL) <<  8) |            \
-        (((__u32)(x) & (__u32)0x00ff0000UL) >>  8) |            \
-        (((__u32)(x) & (__u32)0xff000000UL) >> 24)))
+#define __swab32(x) ((uint32_t)(                                      \
+        (((uint32_t)(x) & (uint32_t)0x000000ffUL) << 24) |            \
+        (((uint32_t)(x) & (uint32_t)0x0000ff00UL) <<  8) |            \
+        (((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) |            \
+        (((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24)))
 
-#define ___swab64(x) ((__u64)(                                  \
-        (((__u64)(x) & (__u64)0x00000000000000ffULL) << 56) |   \
-        (((__u64)(x) & (__u64)0x000000000000ff00ULL) << 40) |   \
-        (((__u64)(x) & (__u64)0x0000000000ff0000ULL) << 24) |   \
-        (((__u64)(x) & (__u64)0x00000000ff000000ULL) <<  8) |   \
-        (((__u64)(x) & (__u64)0x000000ff00000000ULL) >>  8) |   \
-        (((__u64)(x) & (__u64)0x0000ff0000000000ULL) >> 24) |   \
-        (((__u64)(x) & (__u64)0x00ff000000000000ULL) >> 40) |   \
-        (((__u64)(x) & (__u64)0xff00000000000000ULL) >> 56)))
+#define __swab64(x) ((uint64_t)(                                      \
+        (((uint64_t)(x) & (uint64_t)0x00000000000000ffULL) << 56) |   \
+        (((uint64_t)(x) & (uint64_t)0x000000000000ff00ULL) << 40) |   \
+        (((uint64_t)(x) & (uint64_t)0x0000000000ff0000ULL) << 24) |   \
+        (((uint64_t)(x) & (uint64_t)0x00000000ff000000ULL) <<  8) |   \
+        (((uint64_t)(x) & (uint64_t)0x000000ff00000000ULL) >>  8) |   \
+        (((uint64_t)(x) & (uint64_t)0x0000ff0000000000ULL) >> 24) |   \
+        (((uint64_t)(x) & (uint64_t)0x00ff000000000000ULL) >> 40) |   \
+        (((uint64_t)(x) & (uint64_t)0xff00000000000000ULL) >> 56)))
 #endif
 
 #define REMOTE_MSG_VERSION 1
