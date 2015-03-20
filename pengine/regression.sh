5cdb4bb9ed361b2399632d111cba821ba00a6b71
David Vossel <dvossel@redhat.com>
2015-03-20 17:11:40 -0400

    Low: pengine: isolation regression tests

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -799,4 +799,7 @@ do_test resource-discovery      "Exercises resource-discovery location constrain
 do_test rsc-discovery-per-node  "Disable resource discovery per node"
 
 echo ""
+do_test isolation-start-all "Start docker isolated resources."
+
+echo ""
 test_results
