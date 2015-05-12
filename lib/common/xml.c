caac728b227bb14b541d32274ea80083e16d21ab
Gao,Yan <ygao@suse.com>
2015-05-13 01:31:14 +0200

    Fix: xml: Do not dump deleted attributes
    Dumping deleted attributes would introduce digest mis-match

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -3408,9 +3408,15 @@ dump_xml_attr(xmlAttrPtr attr, int options, char **buffer, int *offset, int *max
 {
     char *p_value = NULL;
     const char *p_name = NULL;
+    xml_private_t *p = NULL;
 
     CRM_ASSERT(buffer != NULL);
     if (attr == NULL || attr->children == NULL) {
+        return;
+    }
+
+    p = attr->_private;
+    if (is_set(p->flags, xpf_deleted)) {
         return;
     }
 
