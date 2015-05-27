54c8cb75f473e448c7a38d3fe953fae0f973e5de
Gao,Yan <ygao@suse.com>
2015-05-27 16:42:06 +0200

    Fix: dbus: Use operation timeout for dbus reply timeout
    The reply timeout of dbus defaults to 25 seconds. That's not sufficient
    in case the system is under high load. This commit uses operation timeout
    for dbus reply timeout instead.

--- lib/services/upstart.c
+++ lib/services/upstart.c
@@ -77,7 +77,7 @@ upstart_cleanup(void)
 }
 
 static gboolean
-upstart_job_by_name(const gchar * arg_name, gchar ** out_unit)
+upstart_job_by_name(const gchar * arg_name, gchar ** out_unit, int timeout)
 {
 /*
   com.ubuntu.Upstart0_6.GetJobByName (in String name, out ObjectPath job)
@@ -97,7 +97,7 @@ upstart_job_by_name(const gchar * arg_name, gchar ** out_unit)
 
     dbus_error_init(&error);
     CRM_LOG_ASSERT(dbus_message_append_args(msg, DBUS_TYPE_STRING, &arg_name, DBUS_TYPE_INVALID));
-    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error);
+    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error, timeout);
     dbus_message_unref(msg);
 
     if(error.name) {
@@ -190,7 +190,7 @@ upstart_job_listall(void)
                                        method); // method name
     CRM_ASSERT(msg != NULL);
 
-    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error);
+    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error, DBUS_TIMEOUT_USE_DEFAULT);
     dbus_message_unref(msg);
 
     if(error.name) {
@@ -246,11 +246,11 @@ upstart_job_listall(void)
 gboolean
 upstart_job_exists(const char *name)
 {
-    return upstart_job_by_name(name, NULL);
+    return upstart_job_by_name(name, NULL, DBUS_TIMEOUT_USE_DEFAULT);
 }
 
 static char *
-get_first_instance(const gchar * job)
+get_first_instance(const gchar * job, int timeout)
 {
     char *instance = NULL;
     const char *method = "GetAllInstances";
@@ -268,7 +268,7 @@ get_first_instance(const gchar * job)
     CRM_ASSERT(msg != NULL);
 
     dbus_message_append_args(msg, DBUS_TYPE_INVALID);
-    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error);
+    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error, timeout);
     dbus_message_unref(msg);
 
     if(error.name) {
@@ -451,7 +451,7 @@ upstart_job_exec(svc_action_t * op, gboolean synchronous)
         goto cleanup;
     }
 
-    if(!upstart_job_by_name(op->agent, &job)) {
+    if(!upstart_job_by_name(op->agent, &job, op->timeout)) {
         crm_debug("Could not obtain job named '%s' to %s", op->agent, action);
         if (!g_strcmp0(action, "stop")) {
             op->rc = PCMK_OCF_OK;
@@ -465,7 +465,7 @@ upstart_job_exec(svc_action_t * op, gboolean synchronous)
 
     if (safe_str_eq(op->action, "monitor") || safe_str_eq(action, "status")) {
 
-        char *path = get_first_instance(job);
+        char *path = get_first_instance(job, op->timeout);
 
         op->rc = PCMK_OCF_NOT_RUNNING;
         if(path) {
@@ -473,7 +473,7 @@ upstart_job_exec(svc_action_t * op, gboolean synchronous)
             char *state = pcmk_dbus_get_property(
                 upstart_proxy, BUS_NAME, path, UPSTART_06_API ".Instance", "state",
                 op->synchronous?NULL:upstart_job_check, op,
-                op->synchronous?NULL:&pending);
+                op->synchronous?NULL:&pending, op->timeout);
 
             free(job);
             free(path);
@@ -523,7 +523,7 @@ upstart_job_exec(svc_action_t * op, gboolean synchronous)
     CRM_LOG_ASSERT(dbus_message_append_args(msg, DBUS_TYPE_BOOLEAN, &arg_wait, DBUS_TYPE_INVALID));
 
     if (op->synchronous == FALSE) {
-        DBusPendingCall* pending = pcmk_dbus_send(msg, upstart_proxy, upstart_async_dispatch, op);
+        DBusPendingCall* pending = pcmk_dbus_send(msg, upstart_proxy, upstart_async_dispatch, op, op->timeout);
         free(job);
 
         if(pending) {
@@ -535,7 +535,7 @@ upstart_job_exec(svc_action_t * op, gboolean synchronous)
     }
 
     dbus_error_init(&error);
-    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error);
+    reply = pcmk_dbus_send_recv(msg, upstart_proxy, &error, op->timeout);
 
     if(error.name) {
         if(!upstart_mask_error(op, error.name)) {
