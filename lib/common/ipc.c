06aa3e5372027c7e3f3ebe1fcb0db8a652c07e2b
Andrew Beekhof <andrew@beekhof.net>
2015-06-12 09:55:59 +1000

    Log: Core: Re-instate two log messages as debug

--- lib/common/ipc.c
+++ lib/common/ipc.c
@@ -799,11 +799,13 @@ crm_ipc_connect(crm_ipc_t * client)
     client->ipc = qb_ipcc_connect(client->name, client->buf_size);
 
     if (client->ipc == NULL) {
+        crm_debug("Could not establish %s connection: %s (%d)", client->name, pcmk_strerror(errno), errno);
         return FALSE;
     }
 
     client->pfd.fd = crm_ipc_get_fd(client);
     if (client->pfd.fd < 0) {
+        crm_debug("Could not obtain file descriptor for %s connection: %s (%d)", client->name, pcmk_strerror(errno), errno);
         return FALSE;
     }
 
