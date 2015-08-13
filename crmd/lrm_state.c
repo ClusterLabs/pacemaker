ClusterLabs/pacemaker

commit 446a1005a8773471e03c777808c9f4a1991c6b0a

Fix: Set the attribute from remote node.
The characteristic by attrd_updater carried out in the node that is
remote by way of proxy is not surely set.
This patch sets the attribute from remote node definitely.

crmd/lrm_state.c
@@ -534,7 +534,20 @@ remote_proxy_cb(lrmd_t *lrmd, void *userdata, xmlNode *msg)
             }
 
         } else if(is_set(flags, crm_ipc_proxied)) {
-            int rc = crm_ipc_send(proxy->ipc, request, flags, 5000, NULL);
+            const char *type = crm_element_value(request, F_TYPE);
+            const char *host = NULL;
+            remote_proxy_t *proxy = g_hash_table_lookup(proxy_table, session);
+            int rc;
+
+            if (safe_str_eq(type, T_ATTRD)) {
+                host = crm_element_value_copy(request, F_ATTRD_HOST);
+                if (host == NULL) {
+                    crm_xml_add(request, F_ATTRD_HOST, proxy->node_name);
+                    crm_xml_add_int(request, F_ATTRD_HOST_ID, get_local_nodeid(0));
+                }
+            }
+
+            rc = crm_ipc_send(proxy->ipc, request, flags, 5000, NULL);
 
             if(rc < 0) {
                 xmlNode *op_reply = create_xml_node(NULL, "nack");

commit e5ad5ff003a52c253bbbefc44b6bd91f48a7f187

Fix: crmd: Resolve memory leak in remote_proxy_cb()
Resolve memory leak introduced by 446a100

crmd/lrm_state.c
@@ -535,15 +535,12 @@ remote_proxy_cb(lrmd_t *lrmd, void *userdata, xmlNode *msg)
 
         } else if(is_set(flags, crm_ipc_proxied)) {
             const char *type = crm_element_value(request, F_TYPE);
-            const char *host = NULL;
             int rc;
 
-            if (safe_str_eq(type, T_ATTRD)) {
-                host = crm_element_value_copy(request, F_ATTRD_HOST);
-                if (host == NULL) {
-                    crm_xml_add(request, F_ATTRD_HOST, proxy->node_name);
-                    crm_xml_add_int(request, F_ATTRD_HOST_ID, get_local_nodeid(0));
-                }
+            if (safe_str_eq(type, T_ATTRD)
+                && crm_element_value(request, F_ATTRD_HOST) == NULL) {
+                crm_xml_add(request, F_ATTRD_HOST, proxy->node_name);
+                crm_xml_add_int(request, F_ATTRD_HOST_ID, get_local_nodeid(0));
             }
 
             rc = crm_ipc_send(proxy->ipc, request, flags, 5000, NULL);