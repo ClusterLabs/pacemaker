9ca92ff4c036f477fad151c30524e99ec8d5a636
Jan Pokorný <jpokorny@redhat.com>
2015-09-11 22:21:12 +0200

    Med: libcib: find_nvpair_attr_delegate: check buffer overflow
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- lib/cib/cib_attrs.c
+++ lib/cib/cib_attrs.c
@@ -43,6 +43,13 @@
 	}					\
     } while(0)
 
+/* could also check for possible truncation */
+#define attr_snprintf(_str, _offset, _limit, ...) do {              \
+    _offset += snprintf(_str + _offset,                             \
+                        (_limit > _offset) ? _limit - _offset : 0,  \
+                        __VA_ARGS__);                               \
+    } while(0)
+
 extern int
 find_nvpair_attr_delegate(cib_t * the_cib, const char *attr, const char *section,
                           const char *node_uuid, const char *attr_set_type, const char *set_name,
@@ -88,10 +95,10 @@ find_nvpair_attr_delegate(cib_t * the_cib, const char *attr, const char *section
     xpath_string = calloc(1, xpath_max);
     CRM_CHECK(xpath_string != NULL, return -ENOMEM);
 
-    offset += snprintf(xpath_string + offset, xpath_max - offset, "%.128s", get_object_path(section));
+    attr_snprintf(xpath_string, offset, xpath_max, "%.128s", get_object_path(section));
 
     if (safe_str_eq(node_type, XML_CIB_TAG_TICKETS)) {
-        offset += snprintf(xpath_string + offset, xpath_max - offset, "//%s", node_type);
+        attr_snprintf(xpath_string, offset, xpath_max, "//%s", node_type);
 
     } else if (node_uuid) {
         const char *node_type = XML_CIB_TAG_NODE;
@@ -100,31 +107,29 @@ find_nvpair_attr_delegate(cib_t * the_cib, const char *attr, const char *section
             node_type = XML_CIB_TAG_STATE;
             set_type = XML_TAG_TRANSIENT_NODEATTRS;
         }
-        offset +=
-            snprintf(xpath_string + offset, xpath_max - offset, "//%s[@id='%s']", node_type,
-                     node_uuid);
+        attr_snprintf(xpath_string, offset, xpath_max, "//%s[@id='%s']", node_type,
+                      node_uuid);
     }
 
     if (set_name) {
-        offset +=
-            snprintf(xpath_string + offset, xpath_max - offset, "//%s[@id='%.128s']", set_type,
-                     set_name);
+        attr_snprintf(xpath_string, offset, xpath_max, "//%s[@id='%.128s']", set_type,
+                      set_name);
     } else {
-        offset += snprintf(xpath_string + offset, xpath_max - offset, "//%s", set_type);
+        attr_snprintf(xpath_string, offset, xpath_max, "//%s", set_type);
     }
 
-    offset += snprintf(xpath_string + offset, xpath_max - offset, "//nvpair[");
+    attr_snprintf(xpath_string, offset, xpath_max, "//nvpair[");
     if (attr_id) {
-        offset += snprintf(xpath_string + offset, xpath_max - offset, "@id='%s'", attr_id);
+        attr_snprintf(xpath_string, offset, xpath_max, "@id='%s'", attr_id);
     }
 
     if (attr_name) {
         if (attr_id) {
-            offset += snprintf(xpath_string + offset, xpath_max - offset, " and ");
+            attr_snprintf(xpath_string, offset, xpath_max, " and ");
         }
-        offset += snprintf(xpath_string + offset, xpath_max - offset, "@name='%.128s'", attr_name);
+        attr_snprintf(xpath_string, offset, xpath_max, "@name='%.128s'", attr_name);
     }
-    offset += snprintf(xpath_string + offset, xpath_max - offset, "]");
+    attr_snprintf(xpath_string, offset, xpath_max, "]");
     CRM_LOG_ASSERT(offset > 0);
 
     rc = cib_internal_op(the_cib, CIB_OP_QUERY, NULL, xpath_string, NULL, &xml_search,
