bcced6cf8fbdca7bb91121989e30b29ca5351f5a
Hideo Yamauchi <renayama19661014@ybb.ne.jp>
2016-02-18 11:22:24 +0900

    Fix: pacemakerd: Correction of the stonithd process judgment of pacemakerd.

--- mcp/pacemaker.c
+++ mcp/pacemaker.c
@@ -699,11 +699,18 @@ check_active_before_startup_processes(gpointer user_data)
                 continue;
             } else if (start_seq != pcmk_children[lpc].start_seq) {
                 continue;
-            } else if (crm_pid_active(pcmk_children[lpc].pid, pcmk_children[lpc].name) != 1) {
-                crm_notice("Process %s terminated (pid=%d)",
-                           pcmk_children[lpc].name, pcmk_children[lpc].pid);
-                pcmk_process_exit(&(pcmk_children[lpc]));
-                continue;
+            } else {
+                const char *name = pcmk_children[lpc].name;
+                if (pcmk_children[lpc].flag == crm_proc_stonith_ng) {
+                    name = "stonithd";
+                }
+
+                if (crm_pid_active(pcmk_children[lpc].pid, name) != 1) {
+                    crm_notice("Process %s terminated (pid=%d)",
+                           name, pcmk_children[lpc].pid);
+                    pcmk_process_exit(&(pcmk_children[lpc]));
+                    continue;
+                }
             }
             /* at least one of the processes found at startup
              * is still going, so keep this recurring timer around */
