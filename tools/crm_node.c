6ec3b1ba779b92356fa1dbf6821db223e28818ff
Chen Huajun <chjischj@163.com>
2016-03-09 11:54:21 +0800

    Update crm_node.c

--- tools/crm_node.c
+++ tools/crm_node.c
@@ -344,7 +344,7 @@ try_pacemaker(int command, enum cluster_type_e stack)
         case 'p':
             /* Go to pacemakerd */
             {
-                mainloop_io_t *ipc
+                mainloop_io_t *ipc;
                 GMainLoop *amainloop = g_main_loop_new(NULL, FALSE);
                 ipc = mainloop_add_ipc_client(CRM_SYSTEM_MCP, G_PRIORITY_DEFAULT, 0, NULL, &node_callbacks);
                 if (ipc != NULL) {
