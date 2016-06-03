ClusterLabs/pacemaker

commit 755fb18bf286852bdf7205ea9bbd2641fa151bc7

Fix: attrd, libcrmcommon: validate attrd requests better
This prevents potential attrd segfaults due to malformed client requests.
attrd_update_delegate() may now return -EINVAL if a required argument
is not supplied.

Fixes CLBZ#5272

attrd/commands.c
@@ -249,6 +249,10 @@ attrd_client_update(xmlNode *xml)
         regfree(r_patt);
         free(r_patt);
         return;
+
+    } else if (attr == NULL) {
+        crm_err("Update request did not specify attribute or regular expression");
+        return;
     }
 
     if (host == NULL) {
@@ -651,6 +655,7 @@ void
 attrd_peer_update(crm_node_t *peer, xmlNode *xml, const char *host, bool filter)
 {
     bool changed = FALSE;
+    attribute_t *a;
     attribute_value_t *v = NULL;
     int dampen = 0;
 
@@ -659,8 +664,12 @@ attrd_peer_update(crm_node_t *peer, xmlNode *xml, const char *host, bool filter)
     const char *value = crm_element_value(xml, F_ATTRD_VALUE);
     const char *dvalue = crm_element_value(xml, F_ATTRD_DAMPEN);
 
-    attribute_t *a = g_hash_table_lookup(attributes, attr);
+    if (attr == NULL) {
+        crm_warn("Peer update did not specify attribute");
+        return;
+    }
 
+    a = g_hash_table_lookup(attributes, attr);
     if(a == NULL) {
         if (op == NULL /* The xml children from an ATTRD_OP_SYNC_RESPONSE have no F_ATTRD_TASK */
             || safe_str_eq(op, ATTRD_OP_UPDATE)

lib/common/utils.c
@@ -1773,6 +1773,8 @@ attrd_update_delegate(crm_ipc_t * ipc, char command, const char *host, const cha
 {
     int rc = -ENOTCONN;
     int max = 5;
+    const char *task = NULL;
+    const char *name_as = NULL;
     xmlNode *update = create_xml_node(NULL, __FUNCTION__);
 
     static gboolean connected = TRUE;
@@ -1806,35 +1808,44 @@ attrd_update_delegate(crm_ipc_t * ipc, char command, const char *host, const cha
 
     switch (command) {
         case 'u':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_UPDATE);
-            crm_xml_add(update, F_ATTRD_REGEX, name);
+            task = ATTRD_OP_UPDATE;
+            name_as = F_ATTRD_REGEX;
             break;
         case 'D':
         case 'U':
         case 'v':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_UPDATE);
-            crm_xml_add(update, F_ATTRD_ATTRIBUTE, name);
+            task = ATTRD_OP_UPDATE;
+            name_as = F_ATTRD_ATTRIBUTE;
             break;
         case 'R':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_REFRESH);
+            task = ATTRD_OP_REFRESH;
             break;
         case 'B':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_UPDATE_BOTH);
-            crm_xml_add(update, F_ATTRD_ATTRIBUTE, name);
+            task = ATTRD_OP_UPDATE_BOTH;
+            name_as = F_ATTRD_ATTRIBUTE;
             break;
         case 'Y':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_UPDATE_DELAY);
-            crm_xml_add(update, F_ATTRD_ATTRIBUTE, name);
+            task = ATTRD_OP_UPDATE_DELAY;
+            name_as = F_ATTRD_ATTRIBUTE;
             break;
         case 'Q':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_QUERY);
-            crm_xml_add(update, F_ATTRD_ATTRIBUTE, name);
+            task = ATTRD_OP_QUERY;
+            name_as = F_ATTRD_ATTRIBUTE;
             break;
         case 'C':
-            crm_xml_add(update, F_ATTRD_TASK, ATTRD_OP_PEER_REMOVE);
+            task = ATTRD_OP_PEER_REMOVE;
             break;
     }
 
+    if (name_as != NULL) {
+        if (name == NULL) {
+            rc = -EINVAL;
+            goto done;
+        }
+        crm_xml_add(update, name_as, name);
+    }
+
+    crm_xml_add(update, F_ATTRD_TASK, task);
     crm_xml_add(update, F_ATTRD_VALUE, value);
     crm_xml_add(update, F_ATTRD_DAMPEN, dampen);
     crm_xml_add(update, F_ATTRD_SECTION, section);
@@ -1878,6 +1889,7 @@ attrd_update_delegate(crm_ipc_t * ipc, char command, const char *host, const cha
         }
     }
 
+done:
     free_xml(update);
     if (rc > 0) {
         crm_debug("Sent update: %s=%s for %s", name, value, host ? host : "localhost");

commit c51f5d768c3f7eba600b9b78cbddb0447523fd20

Fix: attrd: don't leak memory when returning due to malformed update request
introduced by 755fb18b

attrd/commands.c
@@ -252,6 +252,9 @@ attrd_client_update(xmlNode *xml)
 
     } else if (attr == NULL) {
         crm_err("Update request did not specify attribute or regular expression");
+        free(key);
+        free(set);
+        free(host);
         return;
     }
 