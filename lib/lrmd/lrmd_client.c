ClusterLabs/pacemaker

commit 6424a647dd160d7a0c8e13a0908297df1a5b2c6f

Feature: Allow cluster and remote LRM API versions to diverge
Requiring the same version is not realistic.

Common examples will be rolling upgrades (where typically you'd upgrade
the cluster first) and guest introspection (where you may never upgrade
the guest software).

Normally you want the "server" to be the higher, however the
client/sever roles are turned on their head here since the cluster is
acting as the "client" connecting to pacemaker remote "server".

However we generally want pacemaker-remote to be as dumb as possible
which is consistent with how we'd do upgrades. So for this reason we
require that the remote node's API version be less-than or equal to that
of the cluster.

lib/lrmd/lrmd_client.c
@@ -109,6 +109,7 @@ typedef struct lrmd_private_s {
     /* Internal IPC proxy msg passing for remote guests */
     void (*proxy_callback)(lrmd_t *lrmd, void *userdata, xmlNode *msg);
     void *proxy_callback_userdata;
+    char *peer_version;
 } lrmd_private_t;
 
 static lrmd_list_t *
@@ -907,6 +908,7 @@ lrmd_handshake(lrmd_t * lrmd, const char *name)
         crm_err("Did not receive registration reply");
         rc = -EPROTO;
     } else {
+        const char *version = crm_element_value(reply, F_LRMD_PROTOCOL_VERSION);
         const char *msg_type = crm_element_value(reply, F_LRMD_OPERATION);
         const char *tmp_ticket = crm_element_value(reply, F_LRMD_CLIENTID);
 
@@ -914,7 +916,7 @@ lrmd_handshake(lrmd_t * lrmd, const char *name)
 
         if (rc == -EPROTO) {
             crm_err("LRMD protocol mismatch client version %s, server version %s",
-                LRMD_PROTOCOL_VERSION, crm_element_value(reply, F_LRMD_PROTOCOL_VERSION));
+                LRMD_PROTOCOL_VERSION, version);
             crm_log_xml_err(reply, "Protocol Error");
 
         } else if (safe_str_neq(msg_type, CRM_OP_REGISTER)) {
@@ -928,6 +930,7 @@ lrmd_handshake(lrmd_t * lrmd, const char *name)
         } else {
             crm_trace("Obtained registration token: %s", tmp_ticket);
             native->token = strdup(tmp_ticket);
+            native->peer_version = strdup(version?version:"1.0"); /* Included since 1.1 */
             rc = pcmk_ok;
         }
     }

include/crm/lrmd.h
@@ -35,7 +35,7 @@ typedef struct lrmd_key_value_s {
     struct lrmd_key_value_s *next;
 } lrmd_key_value_t;
 
-#define LRMD_PROTOCOL_VERSION "1.0"
+#define LRMD_PROTOCOL_VERSION "1.1"
 
 /* *INDENT-OFF* */
 #define DEFAULT_REMOTE_KEY_LOCATION "/etc/pacemaker/authkey"

lrmd/lrmd.c
@@ -1380,13 +1380,16 @@ process_lrmd_signon(crm_client_t * client, uint32_t id, xmlNode * request)
     const char *is_ipc_provider = crm_element_value(request, F_LRMD_IS_IPC_PROVIDER);
     const char *protocol_version = crm_element_value(request, F_LRMD_PROTOCOL_VERSION);
 
-    if (safe_str_neq(protocol_version, LRMD_PROTOCOL_VERSION)) {
+    if (compare_version(protocol_version, LRMD_PROTOCOL_VERSION) < 0) {
+        crm_err("Cluster API version must be greater than or equal to %s, not %s",
+                LRMD_PROTOCOL_VERSION, protocol_version);
         crm_xml_add_int(reply, F_LRMD_RC, -EPROTO);
         crm_xml_add(reply, F_LRMD_PROTOCOL_VERSION, LRMD_PROTOCOL_VERSION);
     }
 
     crm_xml_add(reply, F_LRMD_OPERATION, CRM_OP_REGISTER);
     crm_xml_add(reply, F_LRMD_CLIENTID, client->id);
+    crm_xml_add(reply, F_LRMD_PROTOCOL_VERSION, LRMD_PROTOCOL_VERSION);
     lrmd_server_send_reply(client, id, reply);
 
     if (crm_is_true(is_ipc_provider)) {

commit 27d58a580adb434ad3ef4571f090ce0d6be5b327

Fix: liblrmd: avoid memory leak when closing or deleting lrmd connections
6424a64 introduced a peer version to the private lrmd structure but never
freed it

lib/lrmd/lrmd_client.c
@@ -1412,6 +1412,9 @@ lrmd_api_disconnect(lrmd_t * lrmd)
 
     free(native->token);
     native->token = NULL;
+
+    free(native->peer_version);
+    native->peer_version = NULL;
     return 0;
 }
 
@@ -2217,6 +2220,8 @@ lrmd_api_delete(lrmd_t * lrmd)
 #endif
         free(native->remote_nodename);
         free(native->remote);
+        free(native->token);
+        free(native->peer_version);
     }
 
     free(lrmd->private);