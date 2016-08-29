dd169b2d5c82464d84f68855ddd09928d97a6d8c
Ken Gaillot <kgaillot@redhat.com>
2016-08-29 13:46:01 -0500

    Test: cts: add package docstring
    makes pylint happy

--- cts/__init__.py
+++ cts/__init__.py
@@ -1,2 +1,19 @@
-# This file is required for python packages.
-# It is intentionally empty.
+"""Python modules for Pacemaker's Cluster Test Suite (CTS)
+
+This package provides the following modules:
+
+CIB
+cib_xml
+CM_ais
+CM_lha
+CTSaudits
+CTS
+CTSscenarios
+CTStests
+CTSvars
+environment
+logging
+patterns
+remote
+watcher
+"""
