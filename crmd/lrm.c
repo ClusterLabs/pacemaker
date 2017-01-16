ClusterLabs/pacemaker

commit da486ed648cd98b5ed72af15de6884d2942c34f6

Fix: crmd: Reliably detect and act on reprobe operations from the policy engine

crmd/lrm.c
@@ -1357,7 +1357,7 @@ do_lrm_invoke(long long action,
     } else if (safe_str_eq(operation, CRM_OP_PROBED)) {
         update_attrd(lrm_state->node_name, CRM_OP_PROBED, XML_BOOLEAN_TRUE, user_name, is_remote_node);
 
-    } else if (safe_str_eq(crm_op, CRM_OP_REPROBE)) {
+    } else if (safe_str_eq(operation, CRM_OP_REPROBE) || safe_str_eq(crm_op, CRM_OP_REPROBE)) {
         GHashTableIter gIter;
         rsc_history_t *entry = NULL;
 
@@ -1377,7 +1377,8 @@ do_lrm_invoke(long long action,
          */
         update_attrd(lrm_state->node_name, CRM_OP_PROBED, NULL, user_name, is_remote_node);
 
-        if(strcmp(CRM_SYSTEM_CRMD, from_sys) != 0) {
+        if(strcmp(CRM_SYSTEM_TENGINE, from_sys) != 0
+           && strcmp(CRM_SYSTEM_TENGINE, from_sys) != 0) {
             xmlNode *reply = create_request(
                 CRM_OP_INVOKE_LRM, NULL,
                 from_host, from_sys, CRM_SYSTEM_LRMD, fsa_our_uuid);

commit abef9e8bdc0401168b513c90ccc3cb33d62c2379

Low: crmd: be more resilient when checking an LRM command's "from"
da486ed accidentally checked from_sys against CRM_SYSTEM_TENGINE twice.
Also, don't assume from_sys is not NULL.

crmd/lrm.c
@@ -1595,7 +1595,7 @@ do_lrm_invoke(long long action,
         fsa_cib_update(XML_CIB_TAG_STATUS, fragment, cib_quorum_override, rc, user_name);
         crm_info("Forced a local LRM refresh: call=%d", rc);
 
-        if(strcmp(CRM_SYSTEM_CRMD, from_sys) != 0) {
+        if (safe_str_neq(CRM_SYSTEM_CRMD, from_sys)) {
             xmlNode *reply = create_request(
                 CRM_OP_INVOKE_LRM, fragment,
                 from_host, from_sys, CRM_SYSTEM_LRMD, fsa_our_uuid);
@@ -1629,8 +1629,9 @@ do_lrm_invoke(long long action,
 
         force_reprobe(lrm_state, from_sys, from_host, user_name, is_remote_node);
 
-        if(strcmp(CRM_SYSTEM_TENGINE, from_sys) != 0
-           && strcmp(CRM_SYSTEM_TENGINE, from_sys) != 0) {
+        if (safe_str_neq(CRM_SYSTEM_PENGINE, from_sys)
+           && safe_str_neq(CRM_SYSTEM_TENGINE, from_sys)) {
+
             xmlNode *reply = create_request(
                 CRM_OP_INVOKE_LRM, NULL,
                 from_host, from_sys, CRM_SYSTEM_LRMD, fsa_our_uuid);