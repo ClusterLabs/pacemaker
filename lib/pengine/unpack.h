ClusterLabs/pacemaker

commit a773335936cfe7fe664c1c9ebabafd0908d19849

Medium: PE: Add support for failover domains
Mercurial revision: 6147d4f310aebc82b7cebeb032fa314a4485f42c

lib/pengine/unpack.h
@@ -25,6 +25,8 @@ extern gboolean unpack_config(xmlNode *config, pe_working_set_t *data_set);
 
 extern gboolean unpack_nodes(xmlNode *xml_nodes, pe_working_set_t *data_set);
 
+extern gboolean unpack_domains(xmlNode *xml_domains, pe_working_set_t *data_set);
+
 extern gboolean unpack_status(xmlNode *status, pe_working_set_t *data_set);
 
 extern gint sort_op_by_callid(gconstpointer a, gconstpointer b);

include/crm/msg_xml.h
@@ -114,6 +114,7 @@
 #define XML_CIB_TAG_STATUS       	"status"
 #define XML_CIB_TAG_RESOURCES		"resources"
 #define XML_CIB_TAG_NODES         	"nodes"
+#define XML_CIB_TAG_DOMAINS         	"domains"
 #define XML_CIB_TAG_CONSTRAINTS   	"constraints"
 #define XML_CIB_TAG_CRMCONFIG   	"crm_config"
 #define XML_CIB_TAG_OPCONFIG		"op_defaults"
@@ -121,6 +122,7 @@
 
 #define XML_CIB_TAG_STATE         	"node_state"
 #define XML_CIB_TAG_NODE          	"node"
+#define XML_CIB_TAG_DOMAIN          	"domain"
 #define XML_CIB_TAG_CONSTRAINT    	"constraint"
 #define XML_CIB_TAG_NVPAIR        	"nvpair"
 

include/crm/pengine/status.h
@@ -79,6 +79,7 @@ typedef struct pe_working_set_s
 		no_quorum_policy_t no_quorum_policy;
 
 		GHashTable *config_hash;
+		GHashTable *domains;
 		
 		GListPtr nodes;
 		GListPtr resources;

lib/cib/cib_utils.c
@@ -52,6 +52,7 @@ struct config_root_s known_paths[] = {
     { XML_CIB_TAG_CONFIGURATION,"/cib",               "//cib/configuration" },
     { XML_CIB_TAG_CRMCONFIG,    "/cib/configuration", "//cib/configuration/crm_config" },
     { XML_CIB_TAG_NODES,        "/cib/configuration", "//cib/configuration/nodes" },
+    { XML_CIB_TAG_DOMAINS,      "/cib/configuration", "//cib/configuration/domains" },
     { XML_CIB_TAG_RESOURCES,    "/cib/configuration", "//cib/configuration/resources" },
     { XML_CIB_TAG_CONSTRAINTS,  "/cib/configuration", "//cib/configuration/constraints" },
     { XML_CIB_TAG_OPCONFIG,	"/cib/configuration", "//cib/configuration/op_defaults" },

lib/pengine/status.c
@@ -68,6 +68,8 @@ cluster_status(pe_working_set_t *data_set)
 		XML_CIB_TAG_RESOURCES,   data_set->input);
 	xmlNode * cib_status      = get_object_root(
 		XML_CIB_TAG_STATUS,      data_set->input);
+	xmlNode * cib_domains     = get_object_root(
+		XML_CIB_TAG_DOMAINS,      data_set->input);
  	const char *value = crm_element_value(
 		data_set->input, XML_ATTR_HAVE_QUORUM);
 	
@@ -107,6 +109,7 @@ cluster_status(pe_working_set_t *data_set)
 	}
 	
  	unpack_nodes(cib_nodes, data_set);
+	unpack_domains(cib_domains, data_set);
  	unpack_resources(cib_resources, data_set);
  	unpack_status(cib_status, data_set);
 	
@@ -192,6 +195,9 @@ cleanup_calculations(pe_working_set_t *data_set)
 	crm_debug_3("deleting actions");
 	pe_free_actions(data_set->actions);
 
+	g_hash_table_destroy(data_set->domains);
+	data_set->domains = NULL;
+	
 	crm_debug_3("deleting nodes");
 	pe_free_nodes(data_set->nodes);
 	
@@ -205,7 +211,6 @@ cleanup_calculations(pe_working_set_t *data_set)
 	CRM_CHECK(data_set->placement_constraints == NULL, ;);
 	xmlCleanupParser();
 }
-
 
 void
 set_working_set_defaults(pe_working_set_t *data_set) 

lib/pengine/unpack.c
@@ -294,6 +294,67 @@ unpack_nodes(xmlNode * xml_nodes, pe_working_set_t *data_set)
 	return TRUE;
 }
 
+static void g_hash_destroy_node_list(gpointer data)
+{
+    GListPtr domain = data;
+    slist_destroy(node_t, node, domain, crm_free(node));
+}
+
+gboolean
+unpack_domains(xmlNode *xml_domains, pe_working_set_t *data_set)
+{
+    GListPtr domain = NULL;
+    const char *id     = NULL;
+
+    crm_info("Unpacking domains");
+    data_set->domains = g_hash_table_new_full(
+	g_str_hash, g_str_equal, g_hash_destroy_str, g_hash_destroy_node_list);
+    
+    xml_child_iter_filter(
+	xml_domains, xml_domain, XML_CIB_TAG_DOMAIN,
+
+	domain = NULL;
+	id = crm_element_value(xml_domain, XML_ATTR_ID);
+
+	xml_child_iter_filter(
+	    xml_domain, xml_node, XML_CIB_TAG_NODE,
+	    
+	    node_t *copy = NULL;
+	    node_t *node = NULL;
+	    const char *uname = crm_element_value(xml_node, "name");
+	    const char *score = crm_element_value(xml_node, XML_RULE_ATTR_SCORE);
+
+	    if(uname == NULL) {
+		crm_config_err("Invalid domain %s: Must specify id tag in <node>", id);
+		continue;
+	    }
+
+	    node = pe_find_node(data_set->nodes, uname);
+	    if(node == NULL) {
+		node = pe_find_node_id(data_set->nodes, uname);
+		continue;
+	    }
+	    if(node == NULL) {
+		crm_config_warn("Invalid domain %s: Node %s does not exist", id, uname);
+		continue;
+	    }
+
+	    copy = node_copy(node);
+	    copy->weight = char2score(score);
+	    crm_debug("Adding %s to domain %s with score %s", node->details->uname, id, score);
+
+	    domain = g_list_append(domain, copy);
+	    );
+
+	if(domain) {
+	    crm_debug("Created domain %s with %d members", id, g_list_length(domain));
+	    g_hash_table_replace(data_set->domains, crm_strdup(id), domain);
+	}
+	);
+  
+	return TRUE;
+}
+
 gboolean 
 unpack_resources(xmlNode * xml_resources, pe_working_set_t *data_set)
 {

pengine/constraints.c
@@ -312,43 +312,74 @@ gboolean
 unpack_rsc_location(xmlNode * xml_obj, pe_working_set_t *data_set)
 {
 	gboolean empty = TRUE;
+	rsc_to_node_t *location = NULL;
 	const char *id_lh   = crm_element_value(xml_obj, "rsc");
 	const char *id      = crm_element_value(xml_obj, XML_ATTR_ID);
 	resource_t *rsc_lh  = pe_find_resource(data_set->resources, id_lh);
 	const char *node    = crm_element_value(xml_obj, "node");
 	const char *score   = crm_element_value(xml_obj, XML_RULE_ATTR_SCORE);
+	const char *domain  = crm_element_value(xml_obj, XML_CIB_TAG_DOMAIN);
+	const char *role    = crm_element_value(xml_obj, XML_RULE_ATTR_ROLE);
 	
 	if(rsc_lh == NULL) {
 		/* only a warn as BSC adds the constraint then the resource */
 		crm_config_warn("No resource (con=%s, rsc=%s)", id, id_lh);
 		return FALSE;
 	}
+	
+	if(domain) {
+	    GListPtr nodes = g_hash_table_lookup(data_set->domains, domain);
 
-	if(node != NULL && score != NULL) {
+	    if(domain == NULL) {
+		crm_config_err("Invalid constraint %s: Domain %s does not exist",
+			       id, domain);
+		return FALSE;
+	    }
+
+	    location = rsc2node_new(id, rsc_lh,  0, NULL, data_set);
+	    location->node_list_rh = node_list_dup(nodes, FALSE, FALSE);
+
+	} else if(node != NULL && score != NULL) {
 	    int score_i = char2score(score);
 	    node_t *match = pe_find_node(data_set->nodes, node);
 
-	    if(match) {
-		rsc2node_new(id, rsc_lh, score_i, match, data_set);
-		return TRUE;
-	    } else {
+	    if(!match) {
 		return FALSE;
 	    }
-	}
-	
-	xml_child_iter_filter(
+	    location = rsc2node_new(id, rsc_lh, score_i, match, data_set);
+
+	} else {
+	    xml_child_iter_filter(
 		xml_obj, rule_xml, XML_TAG_RULE,
 		empty = FALSE;
 		crm_debug_2("Unpacking %s/%s", id, ID(rule_xml));
 		generate_location_rule(rsc_lh, rule_xml, data_set);
 		);
 
-	if(empty) {
+	    if(empty) {
 		crm_config_err("Invalid location constraint %s:"
 			      " rsc_location must contain at least one rule",
 			      ID(xml_obj));
+	    }
 	}
-	return TRUE;
+	
+	if(location && role) {
+	    if(text2role(role) == RSC_ROLE_UNKNOWN) {
+		pe_err("Invalid constraint %s: Bad role %s", id, role);
+		return FALSE;
+
+	    } else {
+		location->role_filter = text2role(role);
+		if(location->role_filter == RSC_ROLE_SLAVE) {
+		    /* Fold slave back into Started for simplicity
+		     * At the point Slave location constraints are evaluated,
+		     * all resources are still either stopped or started
+		     */  
+		    location->role_filter = RSC_ROLE_STARTED;
+		}
+	    }
+	}
+	return TRUE;	
 }
 
 static int

pengine/regression.sh
@@ -62,6 +62,9 @@ do_test target-1 "Target Role : master"
 do_test target-2 "Target Role : invalid"
 
 echo ""
+do_test domain "Failover domains"
+
+echo ""
 do_test date-1 "Dates" -t "2005-020"
 do_test date-2 "Date Spec - Pass" -t "2005-020T12:30"
 do_test date-3 "Date Spec - Fail" -t "2005-020T11:30"

commit a7bc1dc8a47d49605bdc9e5f9d881cb4c1a05242

Cleanup: remove extern reference to unpack_domains()
This is a remnant from experimental support for failover domains
(as in rgmanager cluster world order) introduced in a77333593, but
did not got purged during 800a12fa4.

lib/pengine/unpack.h
@@ -26,8 +26,6 @@ extern gboolean unpack_config(xmlNode * config, pe_working_set_t * data_set);
 
 extern gboolean unpack_nodes(xmlNode * xml_nodes, pe_working_set_t * data_set);
 
-extern gboolean unpack_domains(xmlNode * xml_domains, pe_working_set_t * data_set);
-
 extern gboolean unpack_tags(xmlNode * xml_tags, pe_working_set_t * data_set);
 
 extern gboolean unpack_status(xmlNode * status, pe_working_set_t * data_set);