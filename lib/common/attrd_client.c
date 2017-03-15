fb6c1eab6a430e4a5f24545683f93faba6fe627a
Ken Gaillot <kgaillot@redhat.com>
2017-03-15 17:50:23 -0500

    Refactor: libcrmcommon: functionize creating and sending an attrd operation

--- lib/common/attrd_client.c
+++ lib/common/attrd_client.c
@@ -18,19 +18,42 @@
 #include <crm/msg_xml.h>
 #include <crm/attrd.h>
 
-int
-attrd_update_delegate(crm_ipc_t *ipc, char command, const char *host,
-                      const char *name, const char *value, const char *section,
-                      const char *set, const char *dampen,
-                      const char *user_name, int options)
+/*!
+ * \internal
+ * \brief Create a generic attrd operation
+ *
+ * \param[in] user_name  If not NULL, ACL user to set for operation
+ *
+ * \return XML of attrd operation
+ */
+static xmlNode *
+create_attrd_op(const char *user_name)
+{
+    xmlNode *attrd_op = create_xml_node(NULL, __FUNCTION__);
+
+    crm_xml_add(attrd_op, F_TYPE, T_ATTRD);
+    crm_xml_add(attrd_op, F_ORIG, (crm_system_name? crm_system_name: "unknown"));
+#if ENABLE_ACL
+    crm_xml_add(attrd_op, F_ATTRD_USER, user_name);
+#endif
+
+    return attrd_op;
+}
+
+/*!
+ * \internal
+ * \brief Send an operation to attrd via IPC
+ *
+ * \param[in] ipc       Connection to attrd (or NULL to use a local connection)
+ * \param[in] attrd_op  XML of attrd operation to send
+ *
+ * \return pcmk_ok on success, -errno otherwise
+ */
+static int
+send_attrd_op(crm_ipc_t *ipc, xmlNode *attrd_op)
 {
     int rc = -ENOTCONN;
     int max = 5;
-    const char *task = NULL;
-    const char *name_as = NULL;
-    const char *display_host = (host ? host : "localhost");
-    const char *display_command = NULL; /* for commands without name/value */
-    xmlNode *update = create_xml_node(NULL, __FUNCTION__);
 
     static gboolean connected = TRUE;
     static crm_ipc_t *local_ipc = NULL;
@@ -46,6 +69,82 @@ attrd_update_delegate(crm_ipc_t *ipc, char command, const char *host,
         ipc = local_ipc;
     }
 
+    while (max > 0) {
+        if (connected == FALSE) {
+            crm_info("Connecting to cluster... %d retries remaining", max);
+            connected = crm_ipc_connect(ipc);
+        }
+
+        if (connected) {
+            rc = crm_ipc_send(ipc, attrd_op, flags, 0, NULL);
+        } else {
+            crm_perror(LOG_INFO, "Connection to cluster attribute manager failed");
+        }
+
+        if (ipc != local_ipc) {
+            break;
+
+        } else if (rc > 0) {
+            break;
+
+        } else if (rc == -EAGAIN || rc == -EALREADY) {
+            sleep(5 - max);
+            max--;
+
+        } else {
+            crm_ipc_close(ipc);
+            connected = FALSE;
+            sleep(5 - max);
+            max--;
+        }
+    }
+
+    if (rc > 0) {
+        rc = pcmk_ok;
+    }
+    return rc;
+}
+
+/*!
+ * \brief Send a request to attrd
+ *
+ * \param[in] ipc      Connection to attrd (or NULL to use a local connection)
+ * \param[in] command  A character indicating the type of attrd request:
+ *                     U or v: update attribute (or refresh if name is NULL)
+ *                     u: update attributes matching regular expression in name
+ *                     D: delete attribute (value must be NULL)
+ *                     R: refresh
+ *                     B: update both attribute and its dampening
+ *                     Y: update attribute dampening only
+ *                     Q: query attribute
+ *                     C: remove peer specified by host
+ *                     c: clear failure for resource specified by name
+ * \param[in] host     Affect only this host (or NULL for all hosts)
+ * \param[in] name     Name of attribute to affect (or resource, for 'c')
+ * \param[in] value    Attribute value to set
+ * \param[in] section  Status or nodes
+ * \param[in] set      ID of attribute set to use (or NULL to choose first)
+ * \param[in] dampen   Attribute dampening to use with B/Y, and U/v if creating
+ * \param[in] user_name ACL user to pass to attrd
+ * \param[in] options  Bitmask that may include:
+ *                     attrd_opt_remote: host is a Pacemaker Remote node
+ *                     attrd_opt_private: attribute is private (not kept in CIB)
+ *
+ * \return pcmk_ok if request was successfully submitted to attrd, else -errno
+ */
+int
+attrd_update_delegate(crm_ipc_t *ipc, char command, const char *host,
+                      const char *name, const char *value, const char *section,
+                      const char *set, const char *dampen,
+                      const char *user_name, int options)
+{
+    int rc = pcmk_ok;
+    const char *task = NULL;
+    const char *name_as = NULL;
+    const char *display_host = (host ? host : "localhost");
+    const char *display_command = NULL; /* for commands without name/value */
+    xmlNode *update = create_attrd_op(user_name);
+
     /* remap common aliases */
     if (safe_str_eq(section, "reboot")) {
         section = XML_CIB_TAG_STATUS;
@@ -53,9 +152,6 @@ attrd_update_delegate(crm_ipc_t *ipc, char command, const char *host,
     } else if (safe_str_eq(section, "forever")) {
         section = XML_CIB_TAG_NODES;
     }
-
-    crm_xml_add(update, F_TYPE, T_ATTRD);
-    crm_xml_add(update, F_ORIG, crm_system_name?crm_system_name:"unknown");
 
     if (name == NULL && command == 'U') {
         command = 'R';
@@ -116,47 +212,11 @@ attrd_update_delegate(crm_ipc_t *ipc, char command, const char *host,
     crm_xml_add(update, F_ATTRD_SET, set);
     crm_xml_add_int(update, F_ATTRD_IS_REMOTE, is_set(options, attrd_opt_remote));
     crm_xml_add_int(update, F_ATTRD_IS_PRIVATE, is_set(options, attrd_opt_private));
-#if ENABLE_ACL
-    if (user_name) {
-        crm_xml_add(update, F_ATTRD_USER, user_name);
-    }
-#endif
 
-    while (max > 0) {
-        if (connected == FALSE) {
-            crm_info("Connecting to cluster... %d retries remaining", max);
-            connected = crm_ipc_connect(ipc);
-        }
-
-        if (connected) {
-            rc = crm_ipc_send(ipc, update, flags, 0, NULL);
-        } else {
-            crm_perror(LOG_INFO, "Connection to cluster attribute manager failed");
-        }
-
-        if (ipc != local_ipc) {
-            break;
-
-        } else if (rc > 0) {
-            break;
-
-        } else if (rc == -EAGAIN || rc == -EALREADY) {
-            sleep(5 - max);
-            max--;
-
-        } else {
-            crm_ipc_close(ipc);
-            connected = FALSE;
-            sleep(5 - max);
-            max--;
-        }
-    }
+    rc = send_attrd_op(ipc, update);
 
 done:
     free_xml(update);
-    if (rc > 0) {
-        rc = pcmk_ok;
-    }
 
     if (command == 'c') {
         crm_debug("Asked attrd to clear failure of %s on %s: %s (%d)",
