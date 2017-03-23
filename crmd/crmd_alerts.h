47c98e49c51fbe08f0911d84802a2b15a72f6c47
Hideo Yamauchi <renayama19661014@ybb.ne.jp>
2017-03-23 21:51:59 +0900

    Low: crmd: Change of the file name.

--- crmd/crmd_alerts.h
+++ crmd/crmd_alerts.h
@@ -15,8 +15,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#ifndef CRMD_NOTIFY__H
-#  define CRMD_NOTIFY__H
+#ifndef CRMD_ALERT__H
+#  define CRMD_ALERT__H
 
 #  include <crm/crm.h>
 #  include <crm/cluster.h>
