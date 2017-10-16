ClusterLabs/pacemaker

commit 2558d76ffde83cc01a43fff222531710da6f84e5

Refactor: PE: Simplify the reload logic
By mandating that the reload occurs even if the resource eventually
stops, we can simplify the logic and schedule it when we detect the
missmatch.

pengine/native.c
@@ -48,6 +48,7 @@ void RecurringOp_Stopped(resource_t * rsc, action_t * start, node_t * node,
 void pe_post_notify(resource_t * rsc, node_t * node, action_t * op,
                     notify_data_t * n_data, pe_working_set_t * data_set);
 
+void ReloadRsc(resource_t * rsc, node_t *node, pe_working_set_t * data_set);
 gboolean DeleteRsc(resource_t * rsc, node_t * node, gboolean optional, pe_working_set_t * data_set);
 gboolean StopRsc(resource_t * rsc, node_t * next, gboolean optional, pe_working_set_t * data_set);
 gboolean StartRsc(resource_t * rsc, node_t * next, gboolean optional, pe_working_set_t * data_set);
@@ -771,6 +772,10 @@ RecurringOp(resource_t * rsc, action_t * start, node_t * node,
 
     if (node == NULL || is_set(rsc->flags, pe_rsc_managed)) {
         custom_action_order(rsc, start_key(rsc), NULL,
+                            NULL, strdup(key), mon,
+                            pe_order_implies_then | pe_order_runnable_left, data_set);
+
+        custom_action_order(rsc, reload_key(rsc), NULL,
                             NULL, strdup(key), mon,
                             pe_order_implies_then | pe_order_runnable_left, data_set);
 
@@ -2814,6 +2819,11 @@ native_create_probe(resource_t * rsc, node_t * node, action_t * complete,
                         top, generate_op_key(top->id, RSC_START, 0), NULL,
                         flags, data_set);
 
+    /* Before any reloads, if they exist */
+    custom_action_order(rsc, NULL, probe,
+                        top, reload_key(rsc), NULL,
+                        pe_order_optional, data_set);
+    
     if (node && node->details->shutdown == FALSE) {
         custom_action_order(rsc, NULL, probe,
                             rsc, generate_op_key(rsc->id, RSC_STOP, 0), NULL,
@@ -3135,92 +3145,54 @@ get_first_named_action(resource_t * rsc, const char *action, gboolean only_valid
     return a;
 }
 
-static void
-ReloadRsc(resource_t * rsc, action_t * stop, action_t * start, pe_working_set_t * data_set)
-{
-    action_t *action = NULL;
-    action_t *rewrite = NULL;
-
-    if (is_not_set(rsc->flags, pe_rsc_try_reload)) {
-        return;
-
-    } else if (is_not_set(stop->flags, pe_action_optional)) {
-        pe_rsc_trace(rsc, "%s: stop action", rsc->id);
-        return;
-
-    } else if (is_not_set(start->flags, pe_action_optional)) {
-        pe_rsc_trace(rsc, "%s: start action", rsc->id);
-        return;
-    }
-
-    pe_rsc_trace(rsc, "%s on %s", rsc->id, stop->node->details->uname);
-
-    action = get_first_named_action(rsc, RSC_PROMOTE, TRUE, NULL);
-    if (action && is_set(action->flags, pe_action_optional) == FALSE) {
-        update_action_flags(action, pe_action_pseudo);
-    }
-
-    action = get_first_named_action(rsc, RSC_DEMOTE, TRUE, NULL);
-    if (action && is_set(action->flags, pe_action_optional) == FALSE) {
-        rewrite = action;
-        update_action_flags(stop, pe_action_pseudo);
-
-    } else {
-        rewrite = start;
-    }
-
-    pe_rsc_info(rsc, "Rewriting %s of %s on %s as a reload",
-                rewrite->task, rsc->id, stop->node->details->uname);
-    set_bit(rsc->flags, pe_rsc_reload);
-    update_action_flags(rewrite, pe_action_optional | pe_action_clear);
-
-    free(rewrite->uuid);
-    free(rewrite->task);
-    rewrite->task = strdup("reload");
-    rewrite->uuid = generate_op_key(rsc->id, rewrite->task, 0);
-}
-
 void
-rsc_reload(resource_t * rsc, pe_working_set_t * data_set)
+ReloadRsc(resource_t * rsc, node_t *node, pe_working_set_t * data_set)
 {
     GListPtr gIter = NULL;
-    action_t *stop = NULL;
-    action_t *start = NULL;
-
-    if(is_set(rsc->flags, pe_rsc_munging)) {
-        return;
-    }
-    set_bit(rsc->flags, pe_rsc_munging);
+    action_t *other = NULL;
+    action_t *reload = NULL;
 
     if (rsc->children) {
         for (gIter = rsc->children; gIter != NULL; gIter = gIter->next) {
             resource_t *child_rsc = (resource_t *) gIter->data;
 
-            rsc_reload(child_rsc, data_set);
+            ReloadRsc(child_rsc, node, data_set);
         }
         return;
 
     } else if (rsc->variant > pe_native) {
+        /* Complex resource with no children */
+        return;
+
+    } else if (is_not_set(rsc->flags, pe_rsc_managed)) {
+        pe_rsc_trace(rsc, "%s: unmanaged", rsc->id);
+        return;
+
+    } else if (is_set(rsc->flags, pe_rsc_failed) || is_set(rsc->flags, pe_rsc_start_pending)) {
+        pe_rsc_trace(rsc, "%s: general resource state: flags=0x%.16llx", rsc->id, rsc->flags);
+        stop_action(rsc, node, FALSE); /* Force a full restart, overkill? */
+        return;
+
+    } else if (node == NULL) {
+        pe_rsc_trace(rsc, "%s: not active", rsc->id);
         return;
     }
 
     pe_rsc_trace(rsc, "Processing %s", rsc->id);
+    set_bit(rsc->flags, pe_rsc_reload);
+    
+    reload = custom_action(
+        rsc, reload_key(rsc), CRMD_ACTION_RELOAD, node, FALSE, TRUE, data_set);
 
-    stop =
-        get_first_named_action(rsc, RSC_STOP, TRUE,
-                               rsc->running_on ? rsc->running_on->data : NULL);
-    start = get_first_named_action(rsc, RSC_START, TRUE, NULL);
-
-    if (is_not_set(rsc->flags, pe_rsc_managed)
-        || is_set(rsc->flags, pe_rsc_failed)
-        || is_set(rsc->flags, pe_rsc_start_pending)
-        || rsc->next_role < RSC_ROLE_STARTED) {
-        pe_rsc_trace(rsc, "%s: general resource state: flags=0x%.16llx", rsc->id, rsc->flags);
-        return;
+    /* stop = stop_action(rsc, node, optional); */
+    other = get_first_named_action(rsc, RSC_STOP, TRUE, node);
+    if (other != NULL) {
+        order_actions(reload, other, pe_order_optional);
     }
 
-    if (stop != NULL && is_set(stop->flags, pe_action_optional) && is_set(rsc->flags, pe_rsc_try_reload)) {
-        ReloadRsc(rsc, stop, start, data_set);
+    other = get_first_named_action(rsc, RSC_DEMOTE, TRUE, node);
+    if (other != NULL) {
+        order_actions(reload, other, pe_order_optional);
     }
 }
 

include/crm/crm.h
@@ -145,6 +145,7 @@ extern char *crm_system_name;
 #  define CRMD_ACTION_DELETE		"delete"
 #  define CRMD_ACTION_CANCEL		"cancel"
 
+#  define CRMD_ACTION_RELOAD		"reload"
 #  define CRMD_ACTION_MIGRATE		"migrate_to"
 #  define CRMD_ACTION_MIGRATED		"migrate_from"
 

include/crm/pengine/internal.h
@@ -179,6 +179,7 @@ extern action_t *custom_action(resource_t * rsc, char *key, const char *task, no
 		rsc, stop_key(rsc), CRMD_ACTION_STOP, node,		\
 		optional, TRUE, data_set);
 
+#  define reload_key(rsc) generate_op_key(rsc->id, CRMD_ACTION_RELOAD, 0)
 #  define start_key(rsc) generate_op_key(rsc->id, CRMD_ACTION_START, 0)
 #  define start_action(rsc, node, optional) custom_action(		\
 		rsc, start_key(rsc), CRMD_ACTION_START, node,		\

pengine/allocate.c
@@ -36,6 +36,8 @@ CRM_TRACE_INIT_DATA(pe_allocate);
 
 void set_alloc_actions(pe_working_set_t * data_set);
 void migrate_reload_madness(pe_working_set_t * data_set);
+extern void ReloadRsc(resource_t * rsc, node_t *node, pe_working_set_t * data_set);
+extern gboolean DeleteRsc(resource_t * rsc, node_t * node, gboolean optional, pe_working_set_t * data_set);
 
 resource_alloc_functions_t resource_class_alloc_functions[] = {
     {
@@ -322,8 +324,7 @@ check_action_definition(resource_t * rsc, node_t * active_node, xmlNode * xml_op
 
 #if 0
             /* Always reload/restart the entire resource */
-            op = custom_action(rsc, start_key(rsc), RSC_START, NULL, FALSE, TRUE, data_set);
-            update_action_flags(op, pe_action_allow_reload_conversion);
+            ReloadRsc(rsc, data_set);
 #else
             /* Re-sending the recurring op is sufficient - the old one will be cancelled automatically */
             op = custom_action(rsc, key, task, active_node, TRUE, TRUE, data_set);
@@ -333,8 +334,8 @@ check_action_definition(resource_t * rsc, node_t * active_node, xmlNode * xml_op
         } else if (digest_restart && rsc->isolation_wrapper == NULL && (uber_parent(rsc))->isolation_wrapper == NULL) {
             pe_rsc_trace(rsc, "Reloading '%s' action for resource %s", task, rsc->id);
 
-            /* Allow this resource to reload - unless something else causes a full restart */
-            set_bit(rsc->flags, pe_rsc_try_reload);
+            /* Reload this resource */
+            ReloadRsc(rsc, active_node, data_set);
 
             /* Create these for now, it keeps the action IDs the same in the regression outputs */
             custom_action(rsc, key, task, NULL, TRUE, TRUE, data_set);
@@ -352,8 +353,6 @@ check_action_definition(resource_t * rsc, node_t * active_node, xmlNode * xml_op
     return did_change;
 }
 
-extern gboolean DeleteRsc(resource_t * rsc, node_t * node, gboolean optional,
-                          pe_working_set_t * data_set);
 
 static void
 check_actions_for(xmlNode * rsc_entry, resource_t * rsc, node_t * node, pe_working_set_t * data_set)
@@ -1989,7 +1988,6 @@ stage7(pe_working_set_t * data_set)
     for (gIter = data_set->resources; gIter != NULL; gIter = gIter->next) {
         resource_t *rsc = (resource_t *) gIter->data;
 
-        rsc_reload(rsc, data_set);
         LogActions(rsc, data_set, FALSE);
     }
     return TRUE;

pengine/allocate.h
@@ -155,7 +155,6 @@ extern gboolean expand_notification_data(notify_data_t * n_data, pe_working_set_
 extern void create_notifications(resource_t * rsc, notify_data_t * n_data,
                                  pe_working_set_t * data_set);
 extern void free_notification_data(notify_data_t * n_data);
-extern void rsc_reload(resource_t * rsc, pe_working_set_t * data_set);
 extern void rsc_stonith_ordering(resource_t * rsc, action_t * stonith_op,
                                  pe_working_set_t * data_set);
 

pengine/test10/bug-5025-1.exp
@@ -1,7 +1,7 @@
 <transition_graph cluster-delay="60s" stonith-timeout="60s" failed-stop-offset="INFINITY" failed-start-offset="INFINITY"  transition_id="0">
   <synapse id="0">
     <action_set>
-      <crm_event id="4" operation="clear_failcount" operation_key="A_clear_failcount_0" on_node="fc16-builder" on_node_uuid="fc16-builder">
+      <crm_event id="5" operation="clear_failcount" operation_key="A_clear_failcount_0" on_node="fc16-builder" on_node_uuid="fc16-builder">
         <primitive id="A" class="ocf" provider="pacemaker" type="Dummy"/>
         <attributes CRM_meta_timeout="20000" allow-migrate="1" />
       </crm_event>

pengine/test10/params-4.exp
@@ -1,7 +1,7 @@
 <transition_graph cluster-delay="3m" stonith-timeout="60s" failed-stop-offset="INFINITY" failed-start-offset="INFINITY"  transition_id="0">
    <synapse id="0">
      <action_set>
-      <rsc_op id="19" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
+      <rsc_op id="20" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
         <primitive id="DcIPaddr" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.20"/>
       </rsc_op>
@@ -10,7 +10,7 @@
    </synapse>
   <synapse id="1">
      <action_set>
-      <rsc_op id="15" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
+      <rsc_op id="16" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
         <primitive id="DcIPaddr" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.20"/>
        </rsc_op>
@@ -19,7 +19,7 @@
    </synapse>
   <synapse id="2">
      <action_set>
-      <rsc_op id="8" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
+      <rsc_op id="9" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
         <primitive id="DcIPaddr" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.20"/>
        </rsc_op>
@@ -35,13 +35,13 @@
      </action_set>
     <inputs>
       <trigger>
-        <rsc_op id="8" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3"/>
+        <rsc_op id="9" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3"/>
       </trigger>
       <trigger>
-        <rsc_op id="15" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193"/>
+        <rsc_op id="16" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193"/>
       </trigger>
       <trigger>
-        <rsc_op id="19" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b"/>
+        <rsc_op id="20" operation="monitor" operation_key="DcIPaddr_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b"/>
       </trigger>
     </inputs>
    </synapse>
@@ -60,7 +60,7 @@
    </synapse>
   <synapse id="5">
      <action_set>
-      <rsc_op id="16" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
+      <rsc_op id="17" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
         <primitive id="rsc_c001n08" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="21000"  ip="127.0.0.11"/>
        </rsc_op>
@@ -69,7 +69,7 @@
    </synapse>
   <synapse id="6">
      <action_set>
-      <rsc_op id="12" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
+      <rsc_op id="13" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
         <primitive id="rsc_c001n08" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="21000"  ip="127.0.0.11"/>
        </rsc_op>
@@ -78,7 +78,7 @@
    </synapse>
   <synapse id="7">
      <action_set>
-      <rsc_op id="9" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
+      <rsc_op id="10" operation="monitor" operation_key="rsc_c001n08_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
         <primitive id="rsc_c001n08" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="21000"  ip="127.0.0.11"/>
        </rsc_op>
@@ -96,7 +96,7 @@
    </synapse>
   <synapse id="9">
      <action_set>
-      <rsc_op id="28" operation="monitor" operation_key="rsc_c001n02_monitor_6000" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
+      <rsc_op id="29" operation="monitor" operation_key="rsc_c001n02_monitor_6000" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
         <primitive id="rsc_c001n02" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_interval="6000" CRM_meta_name="monitor" CRM_meta_timeout="20000"  ip="127.0.0.12"/>
        </rsc_op>
@@ -105,7 +105,7 @@
    </synapse>
   <synapse id="10">
      <action_set>
-      <rsc_op id="20" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
+      <rsc_op id="21" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
         <primitive id="rsc_c001n02" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.12"/>
        </rsc_op>
@@ -114,7 +114,7 @@
    </synapse>
   <synapse id="11">
      <action_set>
-      <rsc_op id="17" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
+      <rsc_op id="18" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
         <primitive id="rsc_c001n02" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.12"/>
        </rsc_op>
@@ -123,7 +123,7 @@
    </synapse>
   <synapse id="12">
      <action_set>
-      <rsc_op id="10" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
+      <rsc_op id="11" operation="monitor" operation_key="rsc_c001n02_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
         <primitive id="rsc_c001n02" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.12"/>
        </rsc_op>
@@ -141,7 +141,7 @@
    </synapse>
   <synapse id="14">
      <action_set>
-      <rsc_op id="21" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
+      <rsc_op id="22" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
         <primitive id="rsc_c001n03" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.13"/>
        </rsc_op>
@@ -150,7 +150,7 @@
    </synapse>
   <synapse id="15">
      <action_set>
-      <rsc_op id="13" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
+      <rsc_op id="14" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
         <primitive id="rsc_c001n03" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.13"/>
        </rsc_op>
@@ -159,7 +159,7 @@
    </synapse>
   <synapse id="16">
      <action_set>
-      <rsc_op id="11" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
+      <rsc_op id="12" operation="monitor" operation_key="rsc_c001n03_monitor_0" on_node="c001n01" on_node_uuid="de937e3d-0309-4b5d-b85c-f96edc1ed8e3">
         <primitive id="rsc_c001n03" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.13"/>
        </rsc_op>
@@ -168,7 +168,7 @@
    </synapse>
   <synapse id="17">
      <action_set>
-      <rsc_op id="22" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
+      <rsc_op id="23" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n08" on_node_uuid="6427cb5a-c7a5-4bdf-9892-a04ce56f4e6b">
         <primitive id="rsc_c001n01" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.14"/>
        </rsc_op>
@@ -177,7 +177,7 @@
    </synapse>
   <synapse id="18">
      <action_set>
-      <rsc_op id="18" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
+      <rsc_op id="19" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n03" on_node_uuid="5d9a8c11-8684-43ea-91.0.6e221530c193">
         <primitive id="rsc_c001n01" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.14"/>
        </rsc_op>
@@ -186,7 +186,7 @@
    </synapse>
   <synapse id="19">
      <action_set>
-      <rsc_op id="14" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
+      <rsc_op id="15" operation="monitor" operation_key="rsc_c001n01_monitor_0" on_node="c001n02" on_node_uuid="e9bdfde9-01b0-421f-acd8-8a65a53e775f">
         <primitive id="rsc_c001n01" class="ocf" provider="heartbeat" type="IPaddr"/>
         <attributes CRM_meta_op_target_rc="7" CRM_meta_timeout="20000"  ip="127.0.0.14"/>
        </rsc_op>

commit 4acd528347c94fa73ca74548081d97b45543e893

Fix: pengine: don't schedule reload and restart in same transition
2558d76f introduced a regression into 1.1.15 where a restart that becomes
required would not cancel a previously scheduled reload, so that both
actions would be scheduled.

This introduces a new ordering flag to indicate that if 'then' becomes
required, 'first' should become optional.

include/crm/pengine/status.h
@@ -443,6 +443,7 @@ enum pe_ordering {
     pe_order_anti_colocation       = 0x800000,
 
     pe_order_preserve              = 0x1000000, /* Hack for breaking user ordering constraints with container resources */
+    pe_order_then_cancels_first    = 0x2000000, // if 'then' becomes required, 'first' becomes optional
     pe_order_trace                 = 0x4000000, /* test marker */
 };
 /* *INDENT-ON* */

pengine/graph.c
@@ -546,6 +546,17 @@ update_action(action_t * then)
 
         clear_bit(changed, pe_graph_updated_first);
 
+        if (first->rsc && is_set(other->type, pe_order_then_cancels_first)
+            && is_not_set(then->flags, pe_action_optional)) {
+
+            /* 'then' is required, so we must abandon 'first'
+             * (e.g. a required stop cancels any reload).
+             * Only used with reload actions as 'first'.
+             */
+            set_bit(other->action->flags, pe_action_optional);
+            clear_bit(first->rsc->flags, pe_rsc_reload);
+        }
+
         if (first->rsc && then->rsc && (first->rsc != then->rsc)
             && (is_parent(then->rsc, first->rsc) == FALSE)) {
             first = rsc_expand_action(first);

pengine/native.c
@@ -79,8 +79,6 @@ gboolean (*rsc_action_matrix[RSC_ROLE_MAX][RSC_ROLE_MAX])(resource_t*,node_t*,gb
 };
 /* *INDENT-ON* */
 
-static action_t * get_first_named_action(resource_t * rsc, const char *action, gboolean only_valid, node_t * current);
-
 static gboolean
 native_choose_node(resource_t * rsc, node_t * prefer, pe_working_set_t * data_set)
 {
@@ -3250,42 +3248,10 @@ enum stack_activity {
     stack_middle = 4,
 };
 
-static action_t *
-get_first_named_action(resource_t * rsc, const char *action, gboolean only_valid, node_t * current)
-{
-    action_t *a = NULL;
-    GListPtr action_list = NULL;
-    char *key = generate_op_key(rsc->id, action, 0);
-
-    action_list = find_actions(rsc->actions, key, current);
-
-    if (action_list == NULL || action_list->data == NULL) {
-        crm_trace("%s: no %s action", rsc->id, action);
-        free(key);
-        return NULL;
-    }
-
-    a = action_list->data;
-    g_list_free(action_list);
-
-    if (only_valid && is_set(a->flags, pe_action_pseudo)) {
-        crm_trace("%s: pseudo", key);
-        a = NULL;
-
-    } else if (only_valid && is_not_set(a->flags, pe_action_runnable)) {
-        crm_trace("%s: runnable", key);
-        a = NULL;
-    }
-
-    free(key);
-    return a;
-}
-
 void
 ReloadRsc(resource_t * rsc, node_t *node, pe_working_set_t * data_set)
 {
     GListPtr gIter = NULL;
-    action_t *other = NULL;
     action_t *reload = NULL;
 
     if (rsc->children) {
@@ -3316,20 +3282,16 @@ ReloadRsc(resource_t * rsc, node_t *node, pe_working_set_t * data_set)
 
     pe_rsc_trace(rsc, "Processing %s", rsc->id);
     set_bit(rsc->flags, pe_rsc_reload);
-    
+
     reload = custom_action(
         rsc, reload_key(rsc), CRMD_ACTION_RELOAD, node, FALSE, TRUE, data_set);
 
-    /* stop = stop_action(rsc, node, optional); */
-    other = get_first_named_action(rsc, RSC_STOP, TRUE, node);
-    if (other != NULL) {
-        order_actions(reload, other, pe_order_optional);
-    }
-
-    other = get_first_named_action(rsc, RSC_DEMOTE, TRUE, node);
-    if (other != NULL) {
-        order_actions(reload, other, pe_order_optional);
-    }
+    custom_action_order(NULL, NULL, reload, rsc, stop_key(rsc), NULL,
+                        pe_order_optional|pe_order_then_cancels_first,
+                        data_set);
+    custom_action_order(NULL, NULL, reload, rsc, demote_key(rsc), NULL,
+                        pe_order_optional|pe_order_then_cancels_first,
+                        data_set);
 }
 
 void