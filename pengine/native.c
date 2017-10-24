cafc6b1adca133b18c6769ab7eb09a869dccdfb2
Andrew Beekhof <andrew@beekhof.net>
2017-10-24 13:20:56 +1100

    Fix: PE: Bare metal remotes _can_ run resources now and must be probed

--- pengine/native.c
+++ pengine/native.c
@@ -2803,7 +2803,7 @@ native_create_probe(resource_t * rsc, node_t * node, action_t * complete,
         if (safe_str_eq(class, PCMK_RESOURCE_CLASS_STONITH)) {
             pe_rsc_trace(rsc, "Skipping probe for %s on node %s, remote-nodes do not run stonith agents.", rsc->id, node->details->id);
             return FALSE;
-        } else if (rsc_contains_remote_node(data_set, rsc)) {
+        } else if (is_container_remote_node(node) && rsc_contains_remote_node(data_set, rsc)) {
             pe_rsc_trace(rsc, "Skipping probe for %s on node %s, remote-nodes can not run resources that contain connection resources.", rsc->id, node->details->id);
             return FALSE;
         } else if (rsc->is_remote_node) {
