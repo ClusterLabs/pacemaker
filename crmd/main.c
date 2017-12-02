f5edcfd5edccc7e44e9245cc5f6d93ed11837be5
Ken Gaillot <kgaillot@redhat.com>
2017-12-01 21:50:13 -0600

    Low: crmd: remove deprecation warning for legacy stacks
    should have been done with 7a9891f2

--- crmd/main.c
+++ crmd/main.c
@@ -124,26 +124,17 @@ main(int argc, char **argv)
 static void
 log_deprecation_warnings()
 {
-    enum cluster_type_e cluster_type = get_cluster_type();
-
-    if (cluster_type != pcmk_cluster_corosync) {
-        crm_warn("Support for cluster infrastructure %s "
-                 " is deprecated and will be removed in a future release",
-                 name_for_cluster_type(cluster_type));
-
-    }
-
 #if ENABLE_SNMP
     crm_warn("Compile-time support for crm_mon SNMP options"
              " is deprecated and will be removed in a future release"
              " (configure alerts instead)",
-             name_for_cluster_type(cluster_type));
+             name_for_cluster_type(get_cluster_type()));
 #endif
 #if ENABLE_ESMTP
     crm_warn("Compile-time support for crm_mon SMTP options"
              " is deprecated and will be removed in a future release"
              " (configure alerts instead)",
-             name_for_cluster_type(cluster_type));
+             name_for_cluster_type(get_cluster_type()));
 #endif
 
     if (getenv("LRMD_MAX_CHILDREN")) {
