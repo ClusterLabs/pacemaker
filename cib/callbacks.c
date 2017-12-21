87c5217e92e31065e3b2ec9a3e147c2e2d4d6fb0
Ken Gaillot <kgaillot@redhat.com>
2017-12-21 09:07:53 -0600

    Feature: cib,libcrmcommon: drop support for PCMK_legacy variable
    It was only used to select "CIB legacy mode" (v1 patchset format) on hosts that
    otherwise would use the current patchset format. We aren't dropping support for
    legacy mode itself, which will still be used in rolling upgrades from 1.1.11,
    but this will ensure that future rolling upgrades from >=2.0.0 clusters will
    never need legacy mode.

--- cib/callbacks.c
+++ cib/callbacks.c
@@ -70,27 +70,8 @@ int cib_process_command(xmlNode * request, xmlNode ** reply,
 gboolean cib_common_callback(qb_ipcs_connection_t * c, void *data, size_t size,
                              gboolean privileged);
 
-static gboolean cib_read_legacy_mode(void)
-{
-    static gboolean init = TRUE;
-    static gboolean legacy = FALSE;
-
-    if(init) {
-        init = FALSE;
-        legacy = daemon_option_enabled("cib", "legacy");
-        if(legacy) {
-            crm_notice("Enabled legacy mode");
-        }
-    }
-
-    return legacy;
-}
-
 gboolean cib_legacy_mode(void)
 {
-    if(cib_read_legacy_mode()) {
-        return TRUE;
-    }
     return legacy_mode;
 }
 
