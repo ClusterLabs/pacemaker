13410372807ed2eaa2fa0b8171d0ea183c80a15b
Ken Gaillot <kgaillot@redhat.com>
2018-01-10 15:46:16 -0600

    Refactor: libpe_status: remove unused warn-once values
    should have been done when corresponding functionality was removed

--- lib/pengine/unpack.h
+++ lib/pengine/unpack.h
@@ -97,10 +97,6 @@ extern gboolean determine_online_status(xmlNode * node_state, node_t * this_node
 
 enum pe_warn_once_e {
     pe_wo_blind         = 0x0001,
-    pe_wo_requires      = 0x0010,
-    pe_wo_rsc_failstick = 0x0200,
-    pe_wo_default_rscfs = 0x0400,
-    pe_wo_legacy_notifs = 0x0800,
 };
 
 extern uint32_t pe_wo;
