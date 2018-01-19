ClusterLabs/pacemaker

commit ed51cc97cb4f1ace1b284f1726bd5a9749a313ee

Refactor: crmd: remove dead code
With corosync 1 support dropped, we no longer need subsystem launch support.
This should have been done with 7a9891f2.

crmd/pengine.c
@@ -30,12 +30,36 @@
 #include <crmd_fsa.h>
 #include <crmd_messages.h>  /* register_fsa_error_adv */
 
+static mainloop_io_t *pe_subsystem = NULL;
 
-struct crm_subsystem_s *pe_subsystem = NULL;
-void do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void *user_data);
+/*!
+ * \internal
+ * \brief Close any PE connection and free associated memory
+ */
+void
+pe_subsystem_free(void)
+{
+    if (pe_subsystem) {
+        mainloop_del_ipc_client(pe_subsystem);
+        pe_subsystem = NULL;
+    }
+}
 
+/*!
+ * \internal
+ * \brief Save CIB query result to file, raising FSA error
+ *
+ * \param[in] msg        Ignored
+ * \param[in] call_id    Call ID of CIB query
+ * \param[in] rc         Return code of CIB query
+ * \param[in] output     Result of CIB query
+ * \param[in] user_data  Unique identifier for filename (will be freed)
+ *
+ * \note This is intended to be called after a PE connection fails.
+ */
 static void
-save_cib_contents(xmlNode * msg, int call_id, int rc, xmlNode * output, void *user_data)
+save_cib_contents(xmlNode *msg, int call_id, int rc, xmlNode *output,
+                  void *user_data)
 {
     char *id = user_data;
 
@@ -65,15 +89,21 @@ save_cib_contents(xmlNode * msg, int call_id, int rc, xmlNode * output, void *us
     }
 }
 
+/*!
+ * \internal
+ * \brief Respond to PE connection failure
+ *
+ * \param[in] user_data  Ignored
+ */
 static void
 pe_ipc_destroy(gpointer user_data)
 {
-    if (is_set(fsa_input_register, pe_subsystem->flag_required)) {
+    if (is_set(fsa_input_register, R_PE_REQUIRED)) {
         int rc = pcmk_ok;
         char *uuid_str = crm_generate_uuid();
 
         crm_crit("Connection to the Policy Engine failed "
-                 CRM_XS " pid=%d uuid=%s", pe_subsystem->pid, uuid_str);
+                 CRM_XS " uuid=%s", uuid_str);
 
         /*
          * The PE died...
@@ -92,15 +122,22 @@ pe_ipc_destroy(gpointer user_data)
         crm_info("Connection to the Policy Engine released");
     }
 
-    clear_bit(fsa_input_register, pe_subsystem->flag_connected);
-    pe_subsystem->pid = -1;
-    pe_subsystem->source = NULL;
-    pe_subsystem->client = NULL;
-
+    clear_bit(fsa_input_register, R_PE_CONNECTED);
+    pe_subsystem_free();
     mainloop_set_trigger(fsa_source);
     return;
 }
 
+/*!
+ * \internal
+ * \brief Handle message from PE connection
+ *
+ * \param[in] buffer    XML message (will be freed)
+ * \param[in] length    Ignored
+ * \param[in] userdata  Ignored
+ *
+ * \return 0
+ */
 static int
 pe_ipc_dispatch(const char *buffer, ssize_t length, gpointer userdata)
 {
@@ -109,55 +146,83 @@ pe_ipc_dispatch(const char *buffer, ssize_t length, gpointer userdata)
     if (msg) {
         route_message(C_IPC_MESSAGE, msg);
     }
-
     free_xml(msg);
     return 0;
 }
 
-/*	 A_PE_START, A_PE_STOP, A_TE_RESTART	*/
+/*!
+ * \internal
+ * \brief Make new connection to PE
+ *
+ * \return TRUE on success, FALSE otherwise
+ */
+static bool
+pe_subsystem_new()
+{
+    static struct ipc_client_callbacks pe_callbacks = {
+        .dispatch = pe_ipc_dispatch,
+        .destroy = pe_ipc_destroy
+    };
+
+    pe_subsystem = mainloop_add_ipc_client(CRM_SYSTEM_PENGINE,
+                                           G_PRIORITY_DEFAULT,
+                                           5 * 1024 * 1024 /* 5MB */,
+                                           NULL, &pe_callbacks);
+    return (pe_subsystem != NULL);
+}
+
+/*!
+ * \internal
+ * \brief Send an XML message to the PE
+ *
+ * \param[in] cmd  XML message to send
+ *
+ * \return pcmk_ok on success, -errno otherwise
+ */
+static int
+pe_subsystem_send(xmlNode *cmd)
+{
+    if (pe_subsystem) {
+        int sent = crm_ipc_send(mainloop_get_ipc_client(pe_subsystem), cmd,
+                                0, 0, NULL);
+
+        if (sent == 0) {
+            sent = -ENODATA;
+        } else if (sent > 0) {
+            sent = pcmk_ok;
+        }
+        return sent;
+    }
+    return -ENOTCONN;
+}
+
+static void do_pe_invoke_callback(xmlNode *msg, int call_id, int rc,
+                                  xmlNode *output, void *user_data);
+
+/*	 A_PE_START, A_PE_STOP, O_PE_RESTART	*/
 void
 do_pe_control(long long action,
               enum crmd_fsa_cause cause,
               enum crmd_fsa_state cur_state,
               enum crmd_fsa_input current_input, fsa_data_t * msg_data)
 {
-    struct crm_subsystem_s *this_subsys = pe_subsystem;
-
-    long long stop_actions = A_PE_STOP;
-    long long start_actions = A_PE_START;
-
-    static struct ipc_client_callbacks pe_callbacks = {
-        .dispatch = pe_ipc_dispatch,
-        .destroy = pe_ipc_destroy
-    };
-
-    if (action & stop_actions) {
-        clear_bit(fsa_input_register, pe_subsystem->flag_required);
-
-        mainloop_del_ipc_client(pe_subsystem->source);
-        pe_subsystem->source = NULL;
-
-        clear_bit(fsa_input_register, pe_subsystem->flag_connected);
+    if (action & A_PE_STOP) {
+        clear_bit(fsa_input_register, R_PE_REQUIRED);
+        pe_subsystem_free();
+        clear_bit(fsa_input_register, R_PE_CONNECTED);
     }
 
-    if ((action & start_actions) && (is_set(fsa_input_register, R_PE_CONNECTED) == FALSE)) {
+    if ((action & A_PE_START) && (is_set(fsa_input_register, R_PE_CONNECTED) == FALSE)) {
         if (cur_state != S_STOPPING) {
-            set_bit(fsa_input_register, pe_subsystem->flag_required);
-
-            pe_subsystem->source =
-                mainloop_add_ipc_client(CRM_SYSTEM_PENGINE, G_PRIORITY_DEFAULT,
-                                        5 * 1024 * 1024 /* 5MB */ , NULL, &pe_callbacks);
-
-            if (pe_subsystem->source == NULL) {
-                crm_warn("Setup of client connection failed, not adding channel to mainloop");
+            set_bit(fsa_input_register, R_PE_REQUIRED);
+            if (pe_subsystem_new()) {
+                set_bit(fsa_input_register, R_PE_CONNECTED);
+            } else {
+                crm_warn("Could not connect to Policy Engine");
                 register_fsa_error(C_FSA_INTERNAL, I_FAIL, NULL);
-                return;
             }
-
-            set_bit(fsa_input_register, pe_subsystem->flag_connected);
-
         } else {
-            crm_info("Ignoring request to start %s while shutting down", this_subsys->name);
+            crm_info("Ignoring request to connect to PE while shutting down");
         }
     }
 }
@@ -273,10 +338,9 @@ force_local_option(xmlNode *xml, const char *attr_name, const char *attr_value)
     freeXpathObject(xpathObj);
 }
 
-void
+static void
 do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void *user_data)
 {
-    int sent;
     xmlNode *cmd = NULL;
     pid_t watchdog = pcmk_locate_sbd();
 
@@ -330,12 +394,12 @@ do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void
     free(fsa_pe_ref);
     fsa_pe_ref = crm_element_value_copy(cmd, XML_ATTR_REFERENCE);
 
-    sent = crm_ipc_send(mainloop_get_ipc_client(pe_subsystem->source), cmd, 0, 0, NULL);
-    if (sent <= 0) {
-        crm_err("Could not contact the pengine: %d", sent);
+    rc = pe_subsystem_send(cmd);
+    if (rc < 0) {
+        crm_err("Could not contact the Policy Engine: %s " CRM_XS " rc=%d",
+                pcmk_strerror(rc), rc);
         register_fsa_error_adv(C_FSA_INTERNAL, I_ERROR, NULL, NULL, __FUNCTION__);
     }
-
     crm_debug("Invoking the PE: query=%d, ref=%s, seq=%llu, quorate=%d",
               fsa_pe_query, fsa_pe_ref, crm_peer_seq, fsa_has_quorum);
     free_xml(cmd);

crmd/Makefile.am
@@ -44,7 +44,7 @@ crmd_LDADD	= $(top_builddir)/lib/fencing/libstonithd.la		\
 
 crmd_SOURCES	= main.c crmd.c corosync.c crmd_metadata.c			\
 		fsa.c control.c messages.c membership.c callbacks.c attrd.c	\
-		election.c join_client.c join_dc.c subsystems.c throttle.c	\
+		election.c join_client.c join_dc.c throttle.c			\
 		cib.c pengine.c tengine.c lrm.c lrm_state.c remote_lrmd_ra.c	\
 		utils.c misc.c te_events.c te_actions.c te_utils.c te_callbacks.c
 

crmd/cib.c
@@ -30,9 +30,6 @@
 #include <crmd_fsa.h>
 #include <crmd_messages.h>
 
-
-struct crm_subsystem_s *cib_subsystem = NULL;
-
 int cib_retries = 0;
 
 static void

crmd/control.c
@@ -237,10 +237,7 @@ crmd_exit(int rc)
         attrd_ipc = NULL;
     }
 
-    if (pe_subsystem && pe_subsystem->client && pe_subsystem->client->ipcs) {
-        crm_trace("Disconnecting Policy Engine");
-        qb_ipcs_disconnect(pe_subsystem->client->ipcs);
-    }
+    pe_subsystem_free();
 
     if(stonith_api) {
         crm_trace("Disconnecting fencing API");
@@ -280,10 +277,6 @@ crmd_exit(int rc)
 
     clear_bit(fsa_input_register, R_MEMBERSHIP);
     g_list_free(fsa_message_queue); fsa_message_queue = NULL;
-
-    free(pe_subsystem); pe_subsystem = NULL;
-    free(te_subsystem); te_subsystem = NULL;
-    free(cib_subsystem); cib_subsystem = NULL;
 
     metadata_cache_fini();
 
@@ -531,48 +524,9 @@ do_startup(long long action,
         was_error = TRUE;
     }
 
-    /* set up the sub systems */
-    cib_subsystem = calloc(1, sizeof(struct crm_subsystem_s));
-    te_subsystem = calloc(1, sizeof(struct crm_subsystem_s));
-    pe_subsystem = calloc(1, sizeof(struct crm_subsystem_s));
-
-    if (cib_subsystem != NULL) {
-        cib_subsystem->pid = -1;
-        cib_subsystem->name = CRM_SYSTEM_CIB;
-        cib_subsystem->flag_connected = R_CIB_CONNECTED;
-        cib_subsystem->flag_required = R_CIB_REQUIRED;
-
-    } else {
-        was_error = TRUE;
-    }
-
-    if (te_subsystem != NULL) {
-        te_subsystem->pid = -1;
-        te_subsystem->name = CRM_SYSTEM_TENGINE;
-        te_subsystem->flag_connected = R_TE_CONNECTED;
-        te_subsystem->flag_required = R_TE_REQUIRED;
-
-    } else {
-        was_error = TRUE;
-    }
-
-    if (pe_subsystem != NULL) {
-        pe_subsystem->pid = -1;
-        pe_subsystem->path = CRM_DAEMON_DIR;
-        pe_subsystem->name = CRM_SYSTEM_PENGINE;
-        pe_subsystem->command = CRM_DAEMON_DIR "/" CRM_SYSTEM_PENGINE;
-        pe_subsystem->args = NULL;
-        pe_subsystem->flag_connected = R_PE_CONNECTED;
-        pe_subsystem->flag_required = R_PE_REQUIRED;
-
-    } else {
-        was_error = TRUE;
-    }
-
     if (was_error) {
         register_fsa_error(C_FSA_INTERNAL, I_ERROR, NULL);
     }
-
 }
 
 static int32_t
@@ -629,42 +583,15 @@ static int32_t
 crmd_ipc_closed(qb_ipcs_connection_t * c)
 {
     crm_client_t *client = crm_client_get(c);
-    struct crm_subsystem_s *the_subsystem = NULL;
 
-    if (client == NULL) {
-        return 0;
+    if (client) {
+        crm_trace("Disconnecting %sregistered client %s (%p/%p)",
+                  (client->userdata? "" : "un"), crm_client_name(client),
+                  c, client);
+        free(client->userdata);
+        crm_client_destroy(client);
+        trigger_fsa(fsa_source);
     }
-
-    crm_trace("Connection %p", c);
-
-    if (client->userdata == NULL) {
-        crm_trace("Client hadn't registered with us yet");
-
-    } else if (strcasecmp(CRM_SYSTEM_PENGINE, client->userdata) == 0) {
-        the_subsystem = pe_subsystem;
-
-    } else if (strcasecmp(CRM_SYSTEM_TENGINE, client->userdata) == 0) {
-        the_subsystem = te_subsystem;
-
-    } else if (strcasecmp(CRM_SYSTEM_CIB, client->userdata) == 0) {
-        the_subsystem = cib_subsystem;
-    }
-
-    if (the_subsystem != NULL) {
-        the_subsystem->source = NULL;
-        the_subsystem->client = NULL;
-        crm_info("Received HUP from %s:[%d]", the_subsystem->name, the_subsystem->pid);
-
-    } else {
-        /* else that was a transient client */
-        crm_trace("Received HUP from transient client");
-    }
-
-    crm_trace("Disconnecting client %s (%p)", crm_client_name(client), client);
-    free(client->userdata);
-    crm_client_destroy(client);
-
-    trigger_fsa(fsa_source);
     return 0;
 }
 

crmd/crmd.h
@@ -18,11 +18,6 @@
 #ifndef CRMD__H
 #  define CRMD__H
 
-#  define SYS_NAME     CRM_SYSTEM_CRMD
-#  define PID_FILE     WORKING_DIR "/"SYS_NAME".pid"
-#  define DAEMON_LOG   DEVEL_DIR"/"SYS_NAME".log"
-#  define DAEMON_DEBUG DEVEL_DIR"/"SYS_NAME".debug"
-
 extern GMainLoop *crmd_mainloop;
 extern bool no_quorum_suicide_escalation;
 

crmd/crmd_fsa.h
@@ -28,24 +28,6 @@
 #  include <crm/cluster/election.h>
 #  include <crm/common/ipcs.h>
 
-/* copy from struct client_child in heartbeat.h
- *
- * Plus a couple of other things
- */
-struct crm_subsystem_s {
-    pid_t pid;                  /* Process id of child process */
-    const char *name;           /* executable name */
-    const char *path;           /* Command location */
-    const char *command;        /* Command with path */
-    const char *args;           /* Command arguments */
-    crm_client_t *client;       /* Client connection object */
-
-    gboolean sent_kill;
-    mainloop_io_t *source;      /* How can we communicate with it */
-    long long flag_connected;   /*  */
-    long long flag_required;    /*  */
-};
-
 typedef struct fsa_timer_s fsa_timer_t;
 struct fsa_timer_s {
     guint source_id;            /* timer source id */
@@ -105,10 +87,6 @@ extern fsa_timer_t *recheck_timer;
 
 extern crm_trigger_t *fsa_source;
 extern crm_trigger_t *config_read;
-
-extern struct crm_subsystem_s *cib_subsystem;
-extern struct crm_subsystem_s *te_subsystem;
-extern struct crm_subsystem_s *pe_subsystem;
 
 /* these two should be moved elsewhere... */
 extern void do_update_cib_nodes(gboolean overwrite, const char *caller);

crmd/crmd_utils.h
@@ -78,8 +78,7 @@ gboolean is_timer_started(fsa_timer_t * timer);
 
 int crmd_exit(int rc);
 int crmd_fast_exit(int rc);
-gboolean stop_subsystem(struct crm_subsystem_s *centry, gboolean force_quit);
-gboolean start_subsystem(struct crm_subsystem_s *centry);
+void pe_subsystem_free(void);
 
 void fsa_dump_actions(long long action, const char *text);
 void fsa_dump_inputs(int log_level, const char *text, long long input_register);

crmd/fsa_proto.h
@@ -262,13 +262,6 @@ do_lrm_event(long long action,
              enum crmd_fsa_cause cause,
              enum crmd_fsa_state cur_state, enum crmd_fsa_input cur_input, fsa_data_t * msg_data);
 
-/*	A_PE_INVOKE	*/
-void
-
-do_pe_invoke(long long action,
-             enum crmd_fsa_cause cause,
-             enum crmd_fsa_state cur_state, enum crmd_fsa_input cur_input, fsa_data_t * msg_data);
-
 /*	A_TE_INVOKE, A_TE_CANCEL	*/
 void
 

commit de9897f49fefd324295ba97e8d8a87ba6feaffa7

Fix: crmd: Avoid double-removal of glib event source on stopping
ed51cc97c introduced an issue that'd cause reentrancy of
mainloop_del_fd() on stopping crmd, which would complain like:

"crit: GLib: Source ID 22 was not found when attempting to remove it"

```
 #0  0xb7f29cc9 in __kernel_vsyscall ()
 #1  0xb7b538e2 in raise () from /lib/libc.so.6
 #2  0xb7b54fd1 in abort () from /lib/libc.so.6
 #3  0xb7e51a1e in crm_abort (file=0xb7e83c9f "logging.c", function=0xb7e85b00 <__FUNCTION__.21612> "crm_glib_handler", line=73, assert_condition=0x63c6a0 "Source ID 22 was not found when attempting to remove it", do_core=1, do_fork=<optimized out>) at utils.c:689
 #4  0xb7e7257f in crm_glib_handler (log_domain=0xb7d93e8e "GLib", flags=G_LOG_LEVEL_CRITICAL, message=0x63c6a0 "Source ID 22 was not found when attempting to remove it", user_data=0x0) at logging.c:73
 #5  0xb7d5055c in g_logv () from /usr/lib/libglib-2.0.so.0
 #6  0xb7d506a5 in g_log () from /usr/lib/libglib-2.0.so.0
 #7  0xb7d484f6 in g_source_remove () from /usr/lib/libglib-2.0.so.0
 #8  0xb7e6f921 in mainloop_del_fd (client=0x61a110) at mainloop.c:862
 #9  0xb7e6f9b8 in mainloop_del_ipc_client (client=0x61a110) at mainloop.c:797
 #10 0x005069a8 in pe_subsystem_free () at pengine.c:43
 #11 pe_ipc_destroy (user_data=0x0) at pengine.c:126
 #12 0xb7e6dab4 in mainloop_gio_destroy (c=0x61a110) at mainloop.c:748
 #13 0xb7d46088 in ?? () from /usr/lib/libglib-2.0.so.0
 #14 0xb7d46b6a in ?? () from /usr/lib/libglib-2.0.so.0
 #15 0xb7d484cd in g_source_remove () from /usr/lib/libglib-2.0.so.0
 #16 0xb7e6f921 in mainloop_del_fd (client=0x61a110) at mainloop.c:862
 #17 0xb7e6f9b8 in mainloop_del_ipc_client (client=0x61a110) at mainloop.c:797
 #18 0x00506cac in pe_subsystem_free () at pengine.c:43
 #19 do_pe_control (action=2251799813685248, cause=C_FSA_INTERNAL, cur_state=S_STOPPING, current_input= I_STOP, msg_data=0x645600) at pengine.c:211

crmd/pengine.c
@@ -123,7 +123,7 @@ pe_ipc_destroy(gpointer user_data)
     }
 
     clear_bit(fsa_input_register, R_PE_CONNECTED);
-    pe_subsystem_free();
+    pe_subsystem = NULL;
     mainloop_set_trigger(fsa_source);
     return;
 }