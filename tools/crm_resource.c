ClusterLabs/pacemaker

commit 047a6611e26ad778312d0d57c8a8ea58aa21be11

Fix: crm_resource: Have cleanup operate only on failures
This more closely aligns with admin intent and we support the old
begaviour with the re-instated --refresh option

tools/crm_resource.c
@@ -180,6 +180,12 @@ static struct crm_option long_options[] = {
     {"validate",   0, 0, 0, "\t\tCall the validate-all action of the local given resource"},
     {
         "cleanup", 0, 0, 'C',
+        "\t\tDelete failed operations from a resource's history allowing its current state to be rechecked.\n"
+        "\t\t\t\tOptionally filtered by --resource, --node, --operation, and --interval (otherwise all).\n"
+        "\t\t\t\tUnless --force is specified, resource's group or clone (if any) will also be cleaned"
+    },
+    {
+        "refresh", 0, 0, 'R',
         "\t\tDelete resource's history (including failures) so its current state is rechecked.\n"
         "\t\t\t\tOptionally filtered by --resource, --node, --operation, and --interval (otherwise all).\n"
         "\t\t\t\tUnless --force is specified, resource's group or clone (if any) will also be cleaned"
@@ -324,7 +330,6 @@ static struct crm_option long_options[] = {
     {"un-migrate", 0, 0, 'U', NULL, pcmk_option_hidden},
     {"un-move",    0, 0, 'U', NULL, pcmk_option_hidden},
 
-    {"refresh",    0, 0, 'R', NULL, pcmk_option_hidden},
     {"reprobe",    0, 0, 'P', NULL, pcmk_option_hidden},
 
     {"-spacer-", 1, 0, '-', "\nExamples:", pcmk_option_paragraph},
@@ -350,6 +355,7 @@ static struct crm_option long_options[] = {
     {0, 0, 0, 0}
 };
 /* *INDENT-ON* */
+
 
 int
 main(int argc, char **argv)
@@ -379,6 +385,7 @@ main(int argc, char **argv)
     bool require_resource = TRUE; /* whether command requires that resource be specified */
     bool require_dataset = TRUE;  /* whether command requires populated dataset instance */
     bool require_crmd = FALSE;    /* whether command requires connection to CRMd */
+    bool just_errors = TRUE;      /* whether cleanup command deletes all history or just errors */
 
     int rc = pcmk_ok;
     int option_index = 0;
@@ -560,12 +567,20 @@ main(int argc, char **argv)
                 timeout_ms = crm_get_msec(optarg);
                 break;
 
-            case 'C':
             case 'R':
             case 'P':
                 crm_log_args(argc, argv);
                 require_resource = FALSE;
                 require_crmd = TRUE;
+                just_errors = FALSE;
+                rsc_cmd = 'C';
+                break;
+
+            case 'C':
+                crm_log_args(argc, argv);
+                require_resource = FALSE;
+                require_crmd = TRUE;
+                just_errors = TRUE;
                 rsc_cmd = 'C';
                 break;
 
@@ -1009,6 +1024,33 @@ main(int argc, char **argv)
     } else if (rsc_cmd == 'd') {
         /* coverity[var_deref_model] False positive */
         rc = cli_resource_delete_attribute(rsc_id, prop_set, prop_id, prop_name, cib_conn, &data_set);
+    } else if (rsc_cmd == 'C' && just_errors) {
+        crmd_replies_needed = 0;
+        for (xmlNode *xml_op = __xml_first_child(data_set.failed); xml_op != NULL;
+             xml_op = __xml_next(xml_op)) {
+
+            const char *node = crm_element_value(xml_op, XML_ATTR_UNAME);
+            const char *task = crm_element_value(xml_op, XML_LRM_ATTR_TASK);
+            const char *task_interval = crm_element_value(xml_op, XML_LRM_ATTR_INTERVAL);
+            const char *resource_name = crm_element_value(xml_op, XML_LRM_ATTR_RSCID);
+            resource_t *rsc = NULL;
+            if(resource_name == NULL) {
+                continue;
+            }
+
+            /* TODO: Filter by node, resource and operation if supplied */
+            rsc = pe_find_resource_with_flags(
+                data_set.resources, resource_name, pe_find_renamed | pe_find_current | pe_find_anon);
+            if(rsc) {
+                crm_debug("Erasing %s failure for %s (%s detected) on %s",
+                          task, rsc->id, resource_name, node);
+                rc = cli_resource_delete(crmd_channel, node, rsc, task, task_interval, &data_set);
+
+            } else {
+                rc = -ENODEV;
+            }
+        }
+
     } else if ((rsc_cmd == 'C') && (rsc_id)) {
         resource_t *rsc = pe_find_resource_with_flags(data_set.resources, rsc_id, pe_find_renamed | pe_find_current | pe_find_anon);
         if(do_force == FALSE) {

lib/pengine/unpack.c
@@ -2552,7 +2552,7 @@ unpack_rsc_migration_failure(resource_t *rsc, node_t *node, xmlNode *xml_op, pe_
 }
 
 static void
-record_failed_op(xmlNode *op, node_t* node, pe_working_set_t * data_set)
+record_failed_op(xmlNode *op, node_t* node, resource_t *rsc, pe_working_set_t * data_set)
 {
     xmlNode *xIter = NULL;
     const char *op_key = crm_element_value(op, XML_LRM_ATTR_TASK_KEY);
@@ -2573,6 +2573,7 @@ record_failed_op(xmlNode *op, node_t* node, pe_working_set_t * data_set)
 
     crm_trace("Adding entry %s on %s", op_key, node->details->uname);
     crm_xml_add(op, XML_ATTR_UNAME, node->details->uname);
+    crm_xml_add(op, XML_LRM_ATTR_RSCID, rsc->id);
     add_node_copy(data_set->failed, op);
 }
 
@@ -2612,7 +2613,7 @@ unpack_rsc_op_failure(resource_t * rsc, node_t * node, int rc, xmlNode * xml_op,
                  task, rsc->id, node->details->uname, services_ocf_exitcode_str(rc),
                  rc);
 
-        record_failed_op(xml_op, node, data_set);
+        record_failed_op(xml_op, node, rsc, data_set);
 
     } else {
         crm_trace("Processing failed op %s for %s on %s: %s (%d)",
@@ -3135,7 +3136,7 @@ unpack_rsc_op(resource_t * rsc, node_t * node, xmlNode * xml_op, xmlNode ** last
         /* Add them to the failed list to highlight them for the user */
         if ((node->details->shutdown == FALSE) || (node->details->online == TRUE)) {
             crm_trace("Remapping %d to %d", PCMK_OCF_DEGRADED, PCMK_OCF_OK);
-            record_failed_op(xml_op, node, data_set);
+            record_failed_op(xml_op, node, rsc, data_set);
         }
 
     } else if (rc == PCMK_OCF_DEGRADED_MASTER && safe_str_eq(task, CRMD_ACTION_STATUS)) {
@@ -3144,7 +3145,7 @@ unpack_rsc_op(resource_t * rsc, node_t * node, xmlNode * xml_op, xmlNode ** last
         /* Add them to the failed list to highlight them for the user */
         if ((node->details->shutdown == FALSE) || (node->details->online == TRUE)) {
             crm_trace("Remapping %d to %d", PCMK_OCF_DEGRADED_MASTER, PCMK_OCF_RUNNING_MASTER);
-            record_failed_op(xml_op, node, data_set);
+            record_failed_op(xml_op, node, rsc, data_set);
         }
     }
 
@@ -3250,7 +3251,7 @@ unpack_rsc_op(resource_t * rsc, node_t * node, xmlNode * xml_op, xmlNode ** last
                 crm_xml_add(xml_op, XML_ATTR_UNAME, node->details->uname);
                 set_bit(rsc->flags, pe_rsc_failure_ignored);
 
-                record_failed_op(xml_op, node, data_set);
+                record_failed_op(xml_op, node, rsc, data_set);
 
                 if (failure_strategy == action_fail_restart_container && *on_fail <= action_fail_recover) {
                     *on_fail = failure_strategy;

commit aecab580390f4facdc2968b4fa41f448125099f5

Fix: tools: crm_resource --cleanup couldn't match clone instances
regression introduced by 047a6611 + e3b825a7

tools/crm_resource.c
@@ -1101,16 +1101,24 @@ main(int argc, char **argv)
             const char *task_interval = crm_element_value(xml_op, XML_LRM_ATTR_INTERVAL);
             const char *resource_name = crm_element_value(xml_op, XML_LRM_ATTR_RSCID);
 
-            if(resource_name == NULL) {
+            if (resource_name == NULL) {
                 continue;
             } else if(host_uname && safe_str_neq(host_uname, node)) {
-                continue;
-            } else if(rsc_id && safe_str_neq(rsc_id, resource_name)) {
                 continue;
             } else if(operation && safe_str_neq(operation, task)) {
                 continue;
             } else if(interval && safe_str_neq(interval, task_interval)) {
                 continue;
+            }
+
+            if (rsc_id) {
+                resource_t *fail_rsc = pe_find_resource_with_flags(data_set.resources,
+                                                                   resource_name,
+                                                                   find_flags);
+
+                if (!fail_rsc || safe_str_neq(rsc->id, fail_rsc->id)) {
+                    continue;
+                }
             }
 
             crm_debug("Erasing %s failure for %s (%s detected) on %s",