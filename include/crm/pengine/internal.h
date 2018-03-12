ClusterLabs/pacemaker

commit 4b53b163f8dbc0b602b460de220fccd6b80d4b23

PE: Containers: Correctly find the right anonymous child resource running in a container

include/crm/pengine/internal.h
@@ -289,5 +289,7 @@ void pe_fence_node(pe_working_set_t * data_set, node_t * node, const char *reaso
 node_t *create_node(const char *id, const char *uname, const char *type, const char *score, pe_working_set_t * data_set);
 bool remote_id_conflict(const char *remote_name, pe_working_set_t *data);
 void common_print(resource_t * rsc, const char *pre_text, const char *name, node_t *node, long options, void *print_data);
+resource_t *find_container_child(const char *stem, resource_t * rsc, node_t *node);
+
 
 #endif

lib/pengine/container.c
@@ -578,6 +578,36 @@ container_active(resource_t * rsc, gboolean all)
     return TRUE;
 }
 
+resource_t *
+find_container_child(const char *stem, resource_t * rsc, node_t *node) 
+{
+    container_variant_data_t *container_data = NULL;
+    resource_t *parent = uber_parent(rsc);
+    CRM_ASSERT(parent->parent);
+
+    parent = parent->parent;
+    get_container_variant_data(container_data, parent);
+
+    if (is_not_set(rsc->flags, pe_rsc_unique)) {
+        for (GListPtr gIter = container_data->tuples; gIter != NULL; gIter = gIter->next) {
+            container_grouping_t *tuple = (container_grouping_t *)gIter->data;
+
+            CRM_ASSERT(tuple);
+            if(tuple->node->details == node->details) {
+                rsc = tuple->child;
+                break;
+            }
+        }
+    }
+
+    if (rsc && safe_str_neq(stem, rsc->id)) {
+        free(rsc->clone_name);
+        rsc->clone_name = strdup(stem);
+    }
+
+    return rsc;
+}
+
 static void
 container_print_xml(resource_t * rsc, const char *pre_text, long options, void *print_data)
 {

lib/pengine/unpack.c
@@ -1819,7 +1819,10 @@ unpack_find_resource(pe_working_set_t * data_set, node_t * node, const char *rsc
         parent = uber_parent(rsc);
     }
 
-    if (parent && parent->variant >= pe_clone) {
+    if(parent && parent->parent) {
+        rsc = find_container_child(rsc_id, rsc, node);
+
+    } else if (parent && parent->variant >= pe_clone) {
         if (is_not_set(parent->flags, pe_rsc_unique)) {
             char *base = clone_strip(rsc_id);
 

commit 800b217ceb22f8c65982d84d5816b613aa4c90d9

Fix: libpe_status: handle unique bundle children correctly
fixes segfault introduced by 4b53b163, when processing resource history for a
unique clone instance inside a bundle, and the particular instance isn't found
but the clone name is

include/crm/pengine/internal.h
@@ -305,7 +305,7 @@ node_t *pe_create_node(const char *id, const char *uname, const char *type,
                        const char *score, pe_working_set_t * data_set);
 bool remote_id_conflict(const char *remote_name, pe_working_set_t *data);
 void common_print(resource_t * rsc, const char *pre_text, const char *name, node_t *node, long options, void *print_data);
-resource_t *find_container_child(const char *stem, resource_t * rsc, node_t *node);
+resource_t *find_container_child(const resource_t *bundle, const node_t *node);
 bool container_fix_remote_addr(resource_t *rsc);
 const char *container_fix_remote_addr_in(resource_t *rsc, xmlNode *xml, const char *field);
 const char *pe_node_attribute_calculated(const pe_node_t *node,

lib/pengine/container.c
@@ -1173,34 +1173,32 @@ container_active(resource_t * rsc, gboolean all)
     return all;
 }
 
+/*!
+ * \internal
+ * \brief Find the container child corresponding to a given node
+ *
+ * \param[in] bundle  Top-level bundle resource
+ * \param[in] node    Node to search for
+ *
+ * \return Container child if found, NULL otherwise
+ */
 resource_t *
-find_container_child(const char *stem, resource_t * rsc, node_t *node) 
+find_container_child(const resource_t *bundle, const node_t *node)
 {
     container_variant_data_t *container_data = NULL;
-    resource_t *parent = uber_parent(rsc);
-    CRM_ASSERT(parent->parent);
+    CRM_ASSERT(bundle && node);
 
-    parent = parent->parent;
-    get_container_variant_data(container_data, parent);
+    get_container_variant_data(container_data, bundle);
+    for (GListPtr gIter = container_data->tuples; gIter != NULL;
+         gIter = gIter->next) {
+        container_grouping_t *tuple = (container_grouping_t *)gIter->data;
 
-    if (is_not_set(rsc->flags, pe_rsc_unique)) {
-        for (GListPtr gIter = container_data->tuples; gIter != NULL; gIter = gIter->next) {
-            container_grouping_t *tuple = (container_grouping_t *)gIter->data;
-
-            CRM_ASSERT(tuple);
-            if(tuple->node->details == node->details) {
-                rsc = tuple->child;
-                break;
-            }
+        CRM_ASSERT(tuple && tuple->node);
+        if (tuple->node->details == node->details) {
+            return tuple->child;
         }
     }
-
-    if (rsc && safe_str_neq(stem, rsc->id)) {
-        free(rsc->clone_name);
-        rsc->clone_name = strdup(stem);
-    }
-
-    return rsc;
+    return NULL;
 }
 
 static void

lib/pengine/unpack.c
@@ -1651,13 +1651,6 @@ find_anonymous_clone(pe_working_set_t * data_set, node_t * node, resource_t * pa
         pe_rsc_debug(parent, "Created orphan %s for %s: %s on %s", top->id, parent->id, rsc_id,
                      node->details->uname);
     }
-
-    if (safe_str_neq(rsc_id, rsc->id)) {
-        pe_rsc_debug(rsc, "Internally renamed %s on %s to %s%s",
-                    rsc_id, node->details->uname, rsc->id,
-                    is_set(rsc->flags, pe_rsc_orphan) ? " (ORPHAN)" : "");
-    }
-
     return rsc;
 }
 
@@ -1671,20 +1664,25 @@ unpack_find_resource(pe_working_set_t * data_set, node_t * node, const char *rsc
     crm_trace("looking for %s", rsc_id);
     rsc = pe_find_resource(data_set->resources, rsc_id);
 
-    /* no match */
     if (rsc == NULL) {
-        /* Even when clone-max=0, we still create a single :0 orphan to match against */
-        char *tmp = clone_zero(rsc_id);
-        resource_t *clone0 = pe_find_resource(data_set->resources, tmp);
+        /* If we didn't find the resource by its name in the operation history,
+         * check it again as a clone instance. Even when clone-max=0, we create
+         * a single :0 orphan to match against here.
+         */
+        char *clone0_id = clone_zero(rsc_id);
+        resource_t *clone0 = pe_find_resource(data_set->resources, clone0_id);
 
         if (clone0 && is_not_set(clone0->flags, pe_rsc_unique)) {
             rsc = clone0;
         } else {
-            crm_trace("%s is not known as %s either", rsc_id, tmp);
+            crm_trace("%s is not known as %s either", rsc_id, clone0_id);
         }
 
+        /* Grab the parent clone even if this a different unique instance,
+         * so we can remember the clone name, which will be the same.
+         */
         parent = uber_parent(clone0);
-        free(tmp);
+        free(clone0_id);
 
         crm_trace("%s not found: %s", rsc_id, parent ? parent->id : "orphan");
 
@@ -1697,24 +1695,28 @@ unpack_find_resource(pe_working_set_t * data_set, node_t * node, const char *rsc
         parent = uber_parent(rsc);
     }
 
-    if(parent && parent->parent) {
-        rsc = find_container_child(rsc_id, rsc, node);
+    if (pe_rsc_is_anon_clone(parent)) {
 
-    } else if (pe_rsc_is_clone(parent)) {
-        if (is_not_set(parent->flags, pe_rsc_unique)) {
+        if (parent && parent->parent) {
+            rsc = find_container_child(parent->parent, node);
+        } else {
             char *base = clone_strip(rsc_id);
 
             rsc = find_anonymous_clone(data_set, node, parent, base);
-            CRM_ASSERT(rsc != NULL);
             free(base);
-        }
-
-        if (rsc && safe_str_neq(rsc_id, rsc->id)) {
-            free(rsc->clone_name);
-            rsc->clone_name = strdup(rsc_id);
+            CRM_ASSERT(rsc != NULL);
         }
     }
 
+    if (rsc && safe_str_neq(rsc_id, rsc->id)
+        && safe_str_neq(rsc_id, rsc->clone_name)) {
+
+        free(rsc->clone_name);
+        rsc->clone_name = strdup(rsc_id);
+        pe_rsc_debug(rsc, "Internally renamed %s on %s to %s%s",
+                     rsc_id, node->details->uname, rsc->id,
+                     (is_set(rsc->flags, pe_rsc_orphan)? " (ORPHAN)" : ""));
+    }
     return rsc;
 }
 