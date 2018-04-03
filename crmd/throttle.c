ClusterLabs/pacemaker

commit 73711ce525614c374b6c97a317018a715619483e

Refactor: crmd,libcrmcommon: add conditional for procfs support

crmd/throttle.c
@@ -1,19 +1,8 @@
 /*
- * Copyright (C) 2013 Andrew Beekhof <andrew@beekhof.net>
+ * Copyright 2013-2018 Andrew Beekhof <andrew@beekhof.net>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * This source code is licensed under the GNU General Public License version 2
+ * or later (GPLv2+) WITHOUT ANY WARRANTY.
  */
 
 #include <crm_internal.h>
@@ -292,14 +281,11 @@ throttle_handle_load(float load, const char *desc, int cores)
 static enum throttle_state_e
 throttle_mode(void)
 {
+#if SUPPORT_PROCFS
     unsigned int cores;
     float load;
     float thresholds[4];
     enum throttle_state_e mode = throttle_none;
-
-#if defined(ON_BSD) || defined(ON_SOLARIS)
-    return throttle_none;
-#endif
 
     cores = crm_procfs_num_cores();
     if(throttle_cib_load(&load)) {
@@ -351,6 +337,7 @@ throttle_mode(void)
     } else if(mode & throttle_low) {
         return throttle_low;
     }
+#endif // SUPPORT_PROCFS
     return throttle_none;
 }
 

include/portability.h
@@ -1,25 +1,11 @@
+/*
+ * Copyright 2001-2018 Alan Robertson <alanr@unix.sh>
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
 #ifndef PORTABILITY_H
 #  define PORTABILITY_H
-
-/*
- * Copyright (C) 2001 Alan Robertson <alanr@unix.sh>
- * This software licensed under the GNU LGPL.
- *
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
 
 #  define	EOS			'\0'
 #  define	DIMOF(a)		((int) (sizeof(a)/sizeof(a[0])) )
@@ -72,6 +58,13 @@ size_t strnlen(const char *s, size_t maxlen);
 char *strndup(const char *str, size_t len);
 #  else
 #    	define USE_GNU
+#  endif
+
+// This test could be better, but it covers platforms of interest
+#  if defined(ON_BSD) || defined(ON_SOLARIS)
+#    define SUPPORT_PROCFS 0
+#  else
+#    define SUPPORT_PROCFS 1
 #  endif
 
 #  include <glib.h>

commit 54119be5fed8ea9d55b031a5167867ee34ad28ed

Low: crmd: restore compiling without procfs support
regression introduced in 73711ce

crmd/throttle.c
@@ -46,6 +46,7 @@ static float throttle_load_target = 0.0;
 static GHashTable *throttle_records = NULL;
 static mainloop_timer_t *throttle_timer = NULL;
 
+#if SUPPORT_PROCFS
 /*!
  * \internal
  * \brief Return name of /proc file containing the CIB deamon's load statistics
@@ -277,6 +278,7 @@ throttle_handle_load(float load, const char *desc, int cores)
 
     return throttle_check_thresholds(load, desc, thresholds);
 }
+#endif
 
 static enum throttle_state_e
 throttle_mode(void)