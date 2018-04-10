13a7c5be972fbd5c00fa0ad3b832720c966e80cf
Ken Gaillot <kgaillot@redhat.com>
2018-04-10 15:19:11 -0500

    Low: fencing: avoid memory leaks when freeing remote operation

--- fencing/remote.c
+++ fencing/remote.c
@@ -237,6 +237,7 @@ free_remote_op(gpointer data)
 
     free(op->id);
     free(op->action);
+    free(op->delegate);
     free(op->target);
     free(op->client_id);
     free(op->client_name);
@@ -254,6 +255,7 @@ free_remote_op(gpointer data)
         op->devices_list = NULL;
     }
     g_list_free_full(op->automatic_list, free);
+    g_list_free(op->duplicates);
     free(op);
 }
 
