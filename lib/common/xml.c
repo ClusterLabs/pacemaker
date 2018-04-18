599d6cc8dd535ecb658bb745275d72e7b00193e8
Ken Gaillot <kgaillot@redhat.com>
2018-04-17 19:15:21 -0500

    Build: libcrmcommon: correct license declaration in library files
    original intent

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -1,8 +1,8 @@
 /*
  * Copyright 2004-2018 Andrew Beekhof <andrew@beekhof.net>
  *
- * This source code is licensed under the GNU General Public License version 2
- * or later (GPLv2+) WITHOUT ANY WARRANTY.
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
  */
 
 #include <crm_internal.h>
