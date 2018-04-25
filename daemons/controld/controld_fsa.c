1f8ead4bcc51d887a6d5787448cbba3f801266cb
Ken Gaillot <kgaillot@redhat.com>
2018-04-24 20:22:07 -0500

    Refactor: controld: merge misc.c into controld_fsa.c

--- daemons/controld/controld_fsa.c
+++ daemons/controld/controld_fsa.c
@@ -125,6 +125,47 @@ static long long startup_actions =
     A_STARTUP | A_CIB_START | A_LRM_CONNECT | A_HA_CONNECT | A_READCONFIG |
     A_STARTED | A_CL_JOIN_QUERY;
 
+// A_LOG, A_WARN, A_ERROR
+void
+do_log(long long action, enum crmd_fsa_cause cause,
+       enum crmd_fsa_state cur_state,
+       enum crmd_fsa_input current_input, fsa_data_t *msg_data)
+{
+    unsigned log_type = LOG_TRACE;
+
+    if (action & A_LOG) {
+        log_type = LOG_INFO;
+    } else if (action & A_WARN) {
+        log_type = LOG_WARNING;
+    } else if (action & A_ERROR) {
+        log_type = LOG_ERR;
+    }
+
+    do_crm_log(log_type, "Input %s received in state %s from %s",
+               fsa_input2string(msg_data->fsa_input),
+               fsa_state2string(cur_state), msg_data->origin);
+
+    if (msg_data->data_type == fsa_dt_ha_msg) {
+        ha_msg_input_t *input = fsa_typed_data(msg_data->data_type);
+
+        crm_log_xml_debug(input->msg, __FUNCTION__);
+
+    } else if (msg_data->data_type == fsa_dt_xml) {
+        xmlNode *input = fsa_typed_data(msg_data->data_type);
+
+        crm_log_xml_debug(input, __FUNCTION__);
+
+    } else if (msg_data->data_type == fsa_dt_lrm) {
+        lrmd_event_data_t *input = fsa_typed_data(msg_data->data_type);
+
+        do_crm_log(log_type,
+                   "Resource %s: Call ID %d returned %d (%d)."
+                   "  New status if rc=0: %s",
+                   input->rsc_id, input->call_id, input->rc,
+                   input->op_status, (char *)input->user_data);
+    }
+}
+
 enum crmd_fsa_state
 s_crmd_fsa(enum crmd_fsa_cause cause)
 {
