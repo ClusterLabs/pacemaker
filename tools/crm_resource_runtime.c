ClusterLabs/pacemaker

commit 36e4b4906ca5f227841e80279fb0e7a6dc99de92

Feature: various: merge clone resource type and master/slave resource type
Rather than a separate resource type, master/slave clones are now represented
as clone resources with the "promotable" meta-attribute set to "true".
The old syntax remains supported for backward compatibility, but is deprecated.

This breaks API backward compatibility:

* enum pe_obj_types: the pe_master value has been removed,
  and the pe_container value has changed value

* libpe_status: get_resource_type() and get_resource_typename()
  are no longer available as part of the public API

tools/crm_resource_runtime.c
@@ -182,9 +182,8 @@ find_matching_attr_resource(resource_t * rsc, const char * rsc_id, const char * 
                     printf("Performing %s of '%s' for '%s' will not apply to its peers in '%s'\n", cmd, attr_name, rsc_id, rsc->parent->id);
                 }
                 break;
-            case pe_master:
-            case pe_clone:
 
+            case pe_clone:
                 rc = find_resource_attr(cib, XML_ATTR_ID, rsc_id, attr_set_type, attr_set, attr_id, attr_name, &local_attr_id);
                 free(local_attr_id);
 
@@ -844,7 +843,8 @@ cli_resource_check(cib_t * cib_conn, resource_t *rsc)
             printf("\n  * The configuration specifies that '%s' should remain stopped\n", parent->id);
             need_nl++;
 
-        } else if(parent->variant == pe_master && role == RSC_ROLE_SLAVE) {
+        } else if (is_set(parent->flags, pe_rsc_promotable)
+                   && (role == RSC_ROLE_SLAVE)) {
             printf("\n  * The configuration specifies that '%s' should not be promoted\n", parent->id);
             need_nl++;
         }
@@ -1763,21 +1763,22 @@ cli_resource_move(resource_t *rsc, const char *rsc_id, const char *host_name,
     node_t *dest = pe_find_node(data_set->nodes, host_name);
     bool cur_is_dest = FALSE;
 
-    if (scope_master && rsc->variant != pe_master) {
+    if (scope_master && is_set(rsc->flags, pe_rsc_promotable)) {
         resource_t *p = uber_parent(rsc);
-        if(p->variant == pe_master) {
+
+        if (is_set(p->flags, pe_rsc_promotable)) {
             CMD_ERR("Using parent '%s' for --move command instead of '%s'.", rsc->id, rsc_id);
             rsc_id = p->id;
             rsc = p;
 
         } else {
-            CMD_ERR("Ignoring '--master' option: not valid for %s resources.",
-                    get_resource_typename(rsc->variant));
+            CMD_ERR("Ignoring '--master' option: %s is not a promotable resource",
+                    rsc_id);
             scope_master = FALSE;
         }
     }
 
-    if(rsc->variant == pe_master) {
+    if (is_set(rsc->flags, pe_rsc_promotable)) {
         GListPtr iter = NULL;
 
         for(iter = rsc->children; iter; iter = iter->next) {

include/crm/compatibility.h
@@ -7,6 +7,8 @@
 #ifndef CRM_COMPATIBILITY__H
 #  define CRM_COMPATIBILITY__H
 
+#include <crm/msg_xml.h>
+
 /* Heartbeat-specific definitions. Support for heartbeat has been removed
  * entirely, so any code branches relying on these should be deleted.
  */
@@ -84,4 +86,48 @@ static int ais_fd_sync = -1;
 #define LOG_DEBUG_4  LOG_TRACE
 #define LOG_DEBUG_5  LOG_TRACE
 #define LOG_DEBUG_6  LOG_TRACE
+
+/* Clone terminology definitions */
+
+// These can no longer be used in a switch together
+#define pe_master pe_clone
+
+static inline enum pe_obj_types
+get_resource_type(const char *name)
+{
+    if (safe_str_eq(name, XML_CIB_TAG_RESOURCE)) {
+        return pe_native;
+
+    } else if (safe_str_eq(name, XML_CIB_TAG_GROUP)) {
+        return pe_group;
+
+    } else if (safe_str_eq(name, XML_CIB_TAG_INCARNATION)
+               || safe_str_eq(name, XML_CIB_TAG_MASTER)) {
+        return pe_clone;
+
+    } else if (safe_str_eq(name, XML_CIB_TAG_CONTAINER)) {
+        return pe_container;
+    }
+
+    return pe_unknown;
+}
+
+static inline const char *
+get_resource_typename(enum pe_obj_types type)
+{
+    switch (type) {
+        case pe_native:
+            return XML_CIB_TAG_RESOURCE;
+        case pe_group:
+            return XML_CIB_TAG_GROUP;
+        case pe_clone:
+            return XML_CIB_TAG_INCARNATION;
+        case pe_container:
+            return XML_CIB_TAG_CONTAINER;
+        case pe_unknown:
+            return "unknown";
+    }
+    return "<unknown>";
+}
+
 #endif

include/crm/msg_xml.h
@@ -194,7 +194,7 @@
 #  define XML_CIB_TAG_RESOURCE	  	"primitive"
 #  define XML_CIB_TAG_GROUP	  	"group"
 #  define XML_CIB_TAG_INCARNATION	"clone"
-#  define XML_CIB_TAG_MASTER		"master"
+#  define XML_CIB_TAG_MASTER        "master" // deprecated since 2.0.0
 #  define XML_CIB_TAG_CONTAINER		"bundle"
 
 #  define XML_CIB_TAG_RSC_TEMPLATE	"template"
@@ -207,6 +207,7 @@
 #  define XML_RSC_ATTR_INCARNATION_MAX	"clone-max"
 #  define XML_RSC_ATTR_INCARNATION_MIN	"clone-min"
 #  define XML_RSC_ATTR_INCARNATION_NODEMAX	"clone-node-max"
+#  define XML_RSC_ATTR_PROMOTABLE       "promotable"
 #  define XML_RSC_ATTR_MASTER_MAX	"master-max"
 #  define XML_RSC_ATTR_MASTER_NODEMAX	"master-node-max"
 #  define XML_RSC_ATTR_STATE		"clone-state"

include/crm/pengine/complex.h
@@ -32,12 +32,8 @@ enum pe_obj_types {
     pe_native = 0,
     pe_group = 1,
     pe_clone = 2,
-    pe_master = 3,
-    pe_container = 4,
+    pe_container = 3,
 };
-
-enum pe_obj_types get_resource_type(const char *name);
-const char *get_resource_typename(enum pe_obj_types type);
 
 typedef struct resource_object_functions_s {
     gboolean(*unpack) (resource_t *, pe_working_set_t *);

include/crm/pengine/internal.h
@@ -74,7 +74,6 @@ node_t *rsc_known_on(resource_t * rsc, GListPtr * list);
 gboolean native_unpack(resource_t * rsc, pe_working_set_t * data_set);
 gboolean group_unpack(resource_t * rsc, pe_working_set_t * data_set);
 gboolean clone_unpack(resource_t * rsc, pe_working_set_t * data_set);
-gboolean master_unpack(resource_t * rsc, pe_working_set_t * data_set);
 gboolean container_unpack(resource_t * rsc, pe_working_set_t * data_set);
 
 resource_t *native_find_rsc(resource_t * rsc, const char *id, node_t * node, int flags);

include/crm/pengine/status.h
@@ -187,6 +187,7 @@ struct node_s {
 #  define pe_rsc_notify                     0x00000010ULL
 #  define pe_rsc_unique                     0x00000020ULL
 #  define pe_rsc_fence_device               0x00000040ULL
+#  define pe_rsc_promotable                 0x00000080ULL
 
 #  define pe_rsc_provisional                0x00000100ULL
 #  define pe_rsc_allocating                 0x00000200ULL
@@ -485,7 +486,7 @@ pe_rsc_action_details_t *pe_rsc_action_details(pe_action_t *action);
 static inline bool
 pe_rsc_is_clone(resource_t *rsc)
 {
-    return rsc && ((rsc->variant == pe_clone) || (rsc->variant == pe_master));
+    return rsc && (rsc->variant == pe_clone);
 }
 
 /*!

lib/common/utils.c
@@ -1465,7 +1465,7 @@ crm_provider_required(const char *standard)
      * - this should probably be case-sensitive, but isn't,
      *   for backward compatibility
      * - it might be nice to keep standards' capabilities (supports provider,
-     *   master/slave, etc.) as structured data somewhere
+     *   can be promotable, etc.) as structured data somewhere
      */
     if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_OCF)) {
         return TRUE;

lib/pengine/clone.c
@@ -132,24 +132,6 @@ create_child_clone(resource_t * rsc, int sub_id, pe_working_set_t * data_set)
 }
 
 gboolean
-master_unpack(resource_t * rsc, pe_working_set_t * data_set)
-{
-    const char *master_max = g_hash_table_lookup(rsc->meta, XML_RSC_ATTR_MASTER_MAX);
-    const char *master_node_max = g_hash_table_lookup(rsc->meta, XML_RSC_ATTR_MASTER_NODEMAX);
-
-    g_hash_table_replace(rsc->meta, strdup("stateful"), strdup(XML_BOOLEAN_TRUE));
-    if (clone_unpack(rsc, data_set)) {
-        clone_variant_data_t *clone_data = NULL;
-
-        get_clone_variant_data(clone_data, rsc);
-        clone_data->master_max = crm_parse_int(master_max, "1");
-        clone_data->master_node_max = crm_parse_int(master_node_max, "1");
-        return TRUE;
-    }
-    return FALSE;
-}
-
-gboolean
 clone_unpack(resource_t * rsc, pe_working_set_t * data_set)
 {
     int lpc = 0;
@@ -166,6 +148,14 @@ clone_unpack(resource_t * rsc, pe_working_set_t * data_set)
 
     clone_data = calloc(1, sizeof(clone_variant_data_t));
     rsc->variant_opaque = clone_data;
+
+    if (is_set(rsc->flags, pe_rsc_promotable)) {
+        const char *master_max = g_hash_table_lookup(rsc->meta, XML_RSC_ATTR_MASTER_MAX);
+        const char *master_node_max = g_hash_table_lookup(rsc->meta, XML_RSC_ATTR_MASTER_NODEMAX);
+
+        clone_data->master_max = crm_parse_int(master_max, "1");
+        clone_data->master_node_max = crm_parse_int(master_node_max, "1");
+    }
 
     clone_data->active_clones = 0;
     clone_data->xml_obj_child = NULL;
@@ -194,6 +184,8 @@ clone_unpack(resource_t * rsc, pe_working_set_t * data_set)
     pe_rsc_trace(rsc, "\tClone node max: %d", clone_data->clone_node_max);
     pe_rsc_trace(rsc, "\tClone is unique: %s",
                  is_set(rsc->flags, pe_rsc_unique) ? "true" : "false");
+    pe_rsc_trace(rsc, "\tClone is promotable: %s",
+                 is_set(rsc->flags, pe_rsc_promotable) ? "true" : "false");
 
     // Clones may contain a single group or primitive
     for (a_child = __xml_first_child(xml_obj); a_child != NULL;
@@ -321,14 +313,13 @@ configured_role(resource_t * rsc)
 static void
 clone_print_xml(resource_t * rsc, const char *pre_text, long options, void *print_data)
 {
-    int is_master_slave = rsc->variant == pe_master ? 1 : 0;
     char *child_text = crm_concat(pre_text, "   ", ' ');
     const char *target_role = configured_role_str(rsc);
     GListPtr gIter = rsc->children;
 
     status_print("%s<clone ", pre_text);
     status_print("id=\"%s\" ", rsc->id);
-    status_print("multi_state=\"%s\" ", is_master_slave ? "true" : "false");
+    status_print("multi_state=\"%s\" ", is_set(rsc->flags, pe_rsc_promotable)? "true" : "false");
     status_print("unique=\"%s\" ", is_set(rsc->flags, pe_rsc_unique) ? "true" : "false");
     status_print("managed=\"%s\" ", is_set(rsc->flags, pe_rsc_managed) ? "true" : "false");
     status_print("failed=\"%s\" ", is_set(rsc->flags, pe_rsc_failed) ? "true" : "false");
@@ -407,7 +398,7 @@ clone_print(resource_t * rsc, const char *pre_text, long options, void *print_da
 
     child_text = crm_concat(pre_text, "   ", ' ');
 
-    if (rsc->variant == pe_master) {
+    if (is_set(rsc->flags, pe_rsc_promotable)) {
         type = "Master/Slave";
     }
 
@@ -518,7 +509,7 @@ clone_print(resource_t * rsc, const char *pre_text, long options, void *print_da
 	active_instances++;
     }
 
-    if(rsc->variant == pe_master) {
+    if (is_set(rsc->flags, pe_rsc_promotable)) {
         enum rsc_role_e role = configured_role(rsc);
 
         if(role == RSC_ROLE_SLAVE) {

commit da02750b81a1229c89e80f6bd07a7589f4380c84

Fix: tools: correctly check crm_resource --move for master role
regression introduced in 36e4b490

tools/crm_resource_runtime.c
@@ -1790,7 +1790,7 @@ cli_resource_move(resource_t *rsc, const char *rsc_id, const char *host_name,
     node_t *dest = pe_find_node(data_set->nodes, host_name);
     bool cur_is_dest = FALSE;
 
-    if (scope_master && is_set(rsc->flags, pe_rsc_promotable)) {
+    if (scope_master && is_not_set(rsc->flags, pe_rsc_promotable)) {
         resource_t *p = uber_parent(rsc);
 
         if (is_set(p->flags, pe_rsc_promotable)) {