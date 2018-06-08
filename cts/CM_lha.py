80a6780d8597df5732bc927248db9be27131bd27
Ken Gaillot <kgaillot@redhat.com>
2018-06-08 17:29:18 -0500

    Test: CTS: don't muck with iptables if not necessary
    It interferes with other firewall management software. The iptables commands to
    break connectivity are only used with experimental tests or the PacketLoss
    scenario currently, so only clean up iptables in those cases.

--- cts/CM_lha.py
+++ cts/CM_lha.py
@@ -118,7 +118,8 @@ class crm_lha(ClusterManager):
         self.partitions_expected = 1
         for node in self.Env["nodes"]:
             self.ShouldBeStatus[node] = ""
-            self.unisolate_node(node)
+            if self.Env["experimental-tests"]:
+                self.unisolate_node(node)
             self.StataCM(node)
 
     def test_node_CM(self, node):
