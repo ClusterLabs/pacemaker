61e23d8e0a3ab97e8deeca896bc64a943b16bf51
Jan Pokorný <jpokorny@redhat.com>
2018-06-14 22:51:08 +0200

    Low: xml.c: properly "staticize" or drop eligible functions

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -245,10 +245,7 @@ int get_attr_name(const char *input, size_t offset, size_t max);
 int get_attr_value(const char *input, size_t offset, size_t max);
 gboolean can_prune_leaf(xmlNode * xml_node);
 
-void diff_filter_context(int context, int upper_bound, int lower_bound,
-                         xmlNode * xml_node, xmlNode * parent);
-int in_upper_context(int depth, int context, xmlNode * xml_node);
-int add_xml_object(xmlNode * parent, xmlNode * target, xmlNode * update, gboolean as_diff);
+static int add_xml_object(xmlNode * parent, xmlNode * target, xmlNode * update, gboolean as_diff);
 
 static inline const char *
 crm_attr_value(const xmlAttr *attr)
@@ -4312,69 +4309,6 @@ can_prune_leaf(xmlNode * xml_node)
     return can_prune;
 }
 
-void
-diff_filter_context(int context, int upper_bound, int lower_bound,
-                    xmlNode * xml_node, xmlNode * parent)
-{
-    xmlNode *us = NULL;
-    xmlNode *child = NULL;
-    xmlAttrPtr pIter = NULL;
-    xmlNode *new_parent = parent;
-    const char *name = crm_element_name(xml_node);
-
-    CRM_CHECK(xml_node != NULL && name != NULL, return);
-
-    us = create_xml_node(parent, name);
-    for (pIter = crm_first_attr(xml_node); pIter != NULL; pIter = pIter->next) {
-        const char *p_name = (const char *)pIter->name;
-        const char *p_value = crm_attr_value(pIter);
-
-        lower_bound = context;
-        crm_xml_add(us, p_name, p_value);
-    }
-
-    if (lower_bound >= 0 || upper_bound >= 0) {
-        crm_xml_add(us, XML_ATTR_ID, ID(xml_node));
-        new_parent = us;
-
-    } else {
-        upper_bound = in_upper_context(0, context, xml_node);
-        if (upper_bound >= 0) {
-            crm_xml_add(us, XML_ATTR_ID, ID(xml_node));
-            new_parent = us;
-        } else {
-            free_xml(us);
-            us = NULL;
-        }
-    }
-
-    for (child = __xml_first_child(us); child != NULL; child = __xml_next(child)) {
-        diff_filter_context(context, upper_bound - 1, lower_bound - 1, child, new_parent);
-    }
-}
-
-int
-in_upper_context(int depth, int context, xmlNode * xml_node)
-{
-    if (context == 0) {
-        return 0;
-    }
-
-    if (xml_node->properties) {
-        return depth;
-
-    } else if (depth < context) {
-        xmlNode *child = NULL;
-
-        for (child = __xml_first_child(xml_node); child != NULL; child = __xml_next(child)) {
-            if (in_upper_context(depth + 1, context, child)) {
-                return depth;
-            }
-        }
-    }
-    return 0;
-}
-
 static xmlNode *
 find_xml_comment(xmlNode * root, xmlNode * search_comment, gboolean exact)
 {
@@ -4641,7 +4575,7 @@ add_xml_comment(xmlNode * parent, xmlNode * target, xmlNode * update)
     return 0;
 }
 
-int
+static int
 add_xml_object(xmlNode * parent, xmlNode * target, xmlNode * update, gboolean as_diff)
 {
     xmlNode *a_child = NULL;
