ClusterLabs/pacemaker

commit a69002b4961325227075fc3e0157ac6896ae67e0

Low: tools: improve stonith meta-data error messages
stonith__rhcs_metadata() now gives more detailed error log messages and return
codes.

This includes some refactoring: stonith_action_destroy() is exposed internally
as stonith__destroy_action() so it can be used elsewhere; a new internal
function stonith__action_result() is an accessor for an action's result, so the
result can be used elsewhere and so standard error output can be obtained; and
stonith_action_execute() has been renamed stonith__execute(), lost a couple of
arguments, and no longer destroys the action, so the caller can use the other
two functions to obtain the result and destroy the action.

lib/fencing/st_rhcs.c
@@ -76,27 +76,38 @@ stonith__list_rhcs_agents(stonith_key_value_t **devices)
 int
 stonith__rhcs_metadata(const char *agent, int timeout, char **output)
 {
-    int rc = 0;
     char *buffer = NULL;
     xmlNode *xml = NULL;
     xmlNode *actions = NULL;
     xmlXPathObject *xpathObj = NULL;
     stonith_action_t *action = stonith_action_create(agent, "metadata", NULL, 0,
                                                      5, NULL, NULL);
-    int exec_rc = stonith_action_execute(action, &rc, &buffer);
+    int rc = stonith__execute(action);
 
-    if ((exec_rc < 0) || (rc != 0) || (buffer == NULL)) {
-        crm_warn("Could not obtain metadata for %s", agent);
-        crm_debug("Query failed: %d %d: %s", exec_rc, rc, crm_str(buffer));
-        free(buffer);
-        return -EINVAL;
+    if (rc < 0) {
+        crm_warn("Could not execute metadata action for %s: %s "
+                 CRM_XS " rc=%d", agent, pcmk_strerror(rc), rc);
+        stonith__destroy_action(action);
+        return rc;
+    }
+
+    stonith__action_result(action, &rc, &buffer, NULL);
+    stonith__destroy_action(action);
+    if (rc < 0) {
+        crm_warn("Metadata action for %s failed: %s " CRM_XS "rc=%d",
+                 agent, pcmk_strerror(rc), rc);
+        return rc;
+    }
+
+    if (buffer == NULL) {
+        crm_warn("Metadata action for %s returned no data", agent);
+        return -ENODATA;
     }
 
     xml = string2xml(buffer);
     if (xml == NULL) {
         crm_warn("Metadata for %s is invalid", agent);
-        free(buffer);
-        return -EINVAL;
+        return -pcmk_err_schema_validation;
     }
 
     xpathObj = xpath_search(xml, "//actions");
@@ -129,11 +140,10 @@ stonith__rhcs_metadata(const char *agent, int timeout, char **output)
     }
     freeXpathObject(xpathObj);
 
-    free(buffer);
     buffer = dump_xml_formatted_with_text(xml);
     free_xml(xml);
     if (buffer == NULL) {
-        return -EINVAL;
+        return -pcmk_err_schema_validation;
     }
     if (output) {
         *output = buffer;

include/crm/fencing/internal.h
@@ -20,6 +20,9 @@ stonith_action_t *stonith_action_create(const char *agent,
                                         uint32_t victim_nodeid,
                                         int timeout,
                                         GHashTable * device_args, GHashTable * port_map);
+void stonith__destroy_action(stonith_action_t *action);
+void stonith__action_result(stonith_action_t *action, int *rc, char **output,
+                            char **error_output);
 
 GPid
 stonith_action_execute_async(stonith_action_t * action,
@@ -27,8 +30,7 @@ stonith_action_execute_async(stonith_action_t * action,
                              void (*done) (GPid pid, int rc, const char *output,
                                            gpointer user_data));
 
-int
- stonith_action_execute(stonith_action_t * action, int *agent_result, char **output);
+int stonith__execute(stonith_action_t *action);
 
 xmlNode *create_level_registration_xml(const char *node, const char *pattern,
                                        const char *attr, const char *value,

lib/fencing/st_client.c
@@ -602,15 +602,63 @@ stonith_action_clear_tracking_data(stonith_action_t * action)
     action->last_timeout_signo = 0;
 }
 
-static void
-stonith_action_destroy(stonith_action_t * action)
+/*!
+ * \internal
+ * \brief Free all memory used by a stonith action
+ *
+ * \param[in,out] action  Action to free
+ */
+void
+stonith__destroy_action(stonith_action_t *action)
 {
-    stonith_action_clear_tracking_data(action);
-    free(action->agent);
-    free(action->args);
-    free(action->action);
-    free(action->victim);
-    free(action);
+    if (action) {
+        stonith_action_clear_tracking_data(action);
+        free(action->agent);
+        free(action->args);
+        free(action->action);
+        free(action->victim);
+        free(action);
+    }
+}
+
+/*!
+ * \internal
+ * \brief Get the result of an executed stonith action
+ *
+ * \param[in,out] action        Executed action
+ * \param[out]    rc            Where to store result code (or NULL)
+ * \param[out]    output        Where to store standard output (or NULL)
+ * \param[out]    error_output  Where to store standard error output (or NULL)
+ *
+ * \note If output or error_output is not NULL, the caller is responsible for
+ *       freeing the memory.
+ */
+void
+stonith__action_result(stonith_action_t *action, int *rc, char **output,
+                       char **error_output)
+{
+    if (rc) {
+        *rc = pcmk_ok;
+    }
+    if (output) {
+        *output = NULL;
+    }
+    if (error_output) {
+        *error_output = NULL;
+    }
+    if (action != NULL) {
+        if (rc) {
+            *rc = action->rc;
+        }
+        if (output && action->output) {
+            *output = action->output;
+            action->output = NULL; // hand off memory management to caller
+        }
+        if (error_output && action->error) {
+            *error_output = action->error;
+            action->error = NULL; // hand off memory management to caller
+        }
+    }
 }
 
 #define FAILURE_MAX_RETRIES 2
@@ -764,7 +812,7 @@ stonith_action_async_done(mainloop_child_t * p, pid_t pid, int core, int signo, 
         action->done_cb(pid, action->rc, action->output, action->userdata);
     }
 
-    stonith_action_destroy(action);
+    stonith__destroy_action(action);
 }
 
 static int
@@ -870,6 +918,7 @@ internal_stonith_action_execute(stonith_action_t * action)
                    action->agent, pcmk_strerror(rc), rc);
     }
 
+    errno = 0;
     do {
         crm_debug("sending args");
         ret = write(p_write_fd, action->args + total, len - total);
@@ -1019,38 +1068,26 @@ stonith_action_execute_async(stonith_action_t * action,
     return rc < 0 ? rc : action->pid;
 }
 
+/*!
+ * \internal
+ * \brief Execute a stonith action
+ *
+ * \param[in,out] action  Action to execute
+ *
+ * \return pcmk_ok on success, -errno otherwise
+ */
 int
-stonith_action_execute(stonith_action_t * action, int *agent_result, char **output)
+stonith__execute(stonith_action_t *action)
 {
-    int rc = 0;
+    int rc = pcmk_ok;
 
-    if (!action) {
-        return -1;
-    }
+    CRM_CHECK(action != NULL, return -EINVAL);
 
+    // Keep trying until success, max retries, or timeout
     do {
         rc = internal_stonith_action_execute(action);
-        if (rc == pcmk_ok) {
-            /* success! */
-            break;
-        }
-        /* keep retrying while we have time left */
-    } while (update_remaining_timeout(action));
+    } while ((rc != pcmk_ok) && update_remaining_timeout(action));
 
-    if (rc) {
-        /* error */
-        return rc;
-    }
-
-    if (agent_result) {
-        *agent_result = action->rc;
-    }
-    if (output) {
-        *output = action->output;
-        action->output = NULL;  /* handed it off, do not free */
-    }
-
-    stonith_action_destroy(action);
     return rc;
 }
 

commit 1ca56cd0caea78b7cfe49904fdbd277ced737775

Low: libstonithd: avoid memory leak when getting meta-data
introduced by a69002b4

lib/fencing/st_rhcs.c
@@ -96,6 +96,7 @@ stonith__rhcs_metadata(const char *agent, int timeout, char **output)
     if (rc < 0) {
         crm_warn("Metadata action for %s failed: %s " CRM_XS "rc=%d",
                  agent, pcmk_strerror(rc), rc);
+        free(buffer);
         return rc;
     }
 
@@ -105,6 +106,8 @@ stonith__rhcs_metadata(const char *agent, int timeout, char **output)
     }
 
     xml = string2xml(buffer);
+    free(buffer);
+    buffer = NULL;
     if (xml == NULL) {
         crm_warn("Metadata for %s is invalid", agent);
         return -pcmk_err_schema_validation;