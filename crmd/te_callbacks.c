efb431fb69a1c456a7a3fa7ece90f528b2b25612
Ken Gaillot <kgaillot@redhat.com>
2018-06-29 17:38:24 -0500

    Low: controller: filter certain action timeouts from CIB

--- crmd/te_callbacks.c
+++ crmd/te_callbacks.c
@@ -958,7 +958,7 @@ action_timer_callback(gpointer data)
 
         if (timer->action->type != action_type_rsc) {
             send_update = FALSE;
-        } else if (safe_str_eq(task, RSC_CANCEL)) {
+        } else if (!controld_action_is_recordable(task)) {
             /* we don't need to update the CIB with these */
             send_update = FALSE;
         }
