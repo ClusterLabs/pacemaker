98ce833a093e1493343d52459ae2c3d7d5998150
Ken Gaillot <kgaillot@redhat.com>
2018-07-31 08:56:36 -0500

    Refactor: scheduler: add header for library use only
    This adds a new header for functions internal to libpe_status, so they can use
    G_GNUC_INTERNAL for efficiency.

---
+++ lib/pengine/pe_status_private.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018 Andrew Beekhof <andrew@beekhof.net>
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PE_STATUS_PRIVATE__H
+#  define PE_STATUS_PRIVATE__H
+
+/* This header is for the sole use of libpe_status, so that functions can be
+ * declared with G_GNUC_INTERNAL for efficiency.
+ */
+
+#endif  // PE_STATUS_PRIVATE__H
