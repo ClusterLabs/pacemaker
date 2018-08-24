19a1794d9186dbee8e6112cc8633be5a0ad8b530
Jan Pokorný <jpokorny@redhat.com>
2018-08-24 14:16:14 +0200

    Build: autogen.sh: get back to bare-bones, do not autotrigger configure
    It was needlessly bloated with an exteremly limited usefulness.
    
    Regarding the latter, it is not a customary practice (first
    project out of numerous ones I witnessed, actually), depends on
    an unpublished implementation detail of autoconf, and is also
    counter-productive when one changes something in configure.ac and
    only wants to test the configure options are available towards the
    user afterwards.
    
    Since a one-liner is not by any means anything of the originality,
    also drop the original license notice, which would then simply
    amount to another ballast, as the whole inventive content there
    was to be covered exists no more.  Instead, use the convention to
    associate file with the project and add an SPDX license tag
    referencing the project's default license as of the moment.
    
    Also regarding GREP_OPTIONS= workaround, note that it was in most
    likeliness needed only for very ancient autoconf versions, which
    got fixed there in 5f19cfb8 (meaning it's no longer needed for
    v2.63b+, which is already mandated by configure.ac proper
    [with AC_PREREQ(2.64)]).

--- autogen.sh
+++ autogen.sh
@@ -1,48 +1,6 @@
 #!/bin/sh
-#
-#	License: GNU General Public License (GPL)
-#	Copyright 2001 horms <horms@vergenet.net>
-#		(heavily mangled by alanr)
-#
-#	bootstrap: set up the project and get it ready to make
-#
-#	Basically, we run autoconf, automake and libtool in the
-#	right way to get things set up for this environment.
-#
-#	We also look and see if those tools are installed, and
-#	tell you where to get them if they're not.
-#
-#	Our goal is to not require dragging along anything
-#	more than we need.  If this doesn't work on your system,
-#	(i.e., your /bin/sh is broken) send us a patch.
-#
-#	This code loosely based on the corresponding named script in
-#	enlightenment, and also on the sort-of-standard autoconf
-#	bootstrap script.
+# Part of pacemaker project
+# SPDX-License-Identifier: GPL-2.0-or-later
 
-# Run this to generate all the initial makefiles, etc.
-
-# Unset GREP_OPTIONS as any coloring can mess up the AC_CONFIG_AUX_DIR matching patterns
-GREP_OPTIONS= autoreconf -visf || exit $?
-
-if [ -f config.log ]; then
-    echo Now re-running ./configure with the previous arguments
-    last=`grep --color=never "$.*configure" config.log | tail -n 1 | sed s:.*configure:./configure: | sed s:--no-create::`
-    echo "  $last"
-    eval $last
-
-elif [ "x$1" = xinit -a -e "`which rpm 2>/dev/null`" ]; then
-    cmd="./__configure.rpm"
-    rpm --eval %{configure} | grep -v program-prefix | sed 's/\t/    /' > $cmd
-    sh $cmd
-    make
-
-else
-    echo "Now run configure with any arguments (eg. --prefix) specific to your system"
-    if [ -e "`which rpm 2>/dev/null`" ]; then
-	echo "Suggested invocation:"
-	rpm --eval %{configure} | grep -v program-prefix
-    fi
-fi
-
-trap '' 0
+autoreconf -fisv || exit $?
+echo 'Now run ./configure and make'
