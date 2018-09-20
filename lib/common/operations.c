ClusterLabs/pacemaker

commit bdf697228f1bc478680f45b9c3d4031f70e2f170

Refactor: libcrmcommon,etc.: generic support for agent standard capabilities
This adds an enum for resource agent standard capabilities (such as requiring a
provider, or using status instead of monitor). A generic pcmk_get_ra_caps()
function replaces the now-deprecated crm_provider_required().

lib/common/operations.c
@@ -562,10 +562,7 @@ crm_op_needs_metadata(const char *rsc_class, const char *op)
 
     CRM_CHECK(rsc_class || op, return FALSE);
 
-    if (rsc_class
-        && strcmp(rsc_class, PCMK_RESOURCE_CLASS_OCF)
-        && strcmp(rsc_class, PCMK_RESOURCE_CLASS_STONITH)) {
-
+    if (is_set(pcmk_get_ra_caps(rsc_class), pcmk_ra_cap_params)) {
         /* Meta-data is only needed for resource classes that use parameters */
         return FALSE;
     }

daemons/execd/execd_commands.c
@@ -121,9 +121,7 @@ static const char *
 normalize_action_name(lrmd_rsc_t * rsc, const char *action)
 {
     if (safe_str_eq(action, "monitor") &&
-        (safe_str_eq(rsc->class, PCMK_RESOURCE_CLASS_LSB) ||
-         safe_str_eq(rsc->class, PCMK_RESOURCE_CLASS_SERVICE)
-         || safe_str_eq(rsc->class, PCMK_RESOURCE_CLASS_SYSTEMD))) {
+        is_set(pcmk_get_ra_caps(rsc->class), pcmk_ra_cap_status)) {
         return "status";
     }
     return action;

include/crm/common/util.h
@@ -117,11 +117,24 @@ xmlNode *crm_create_op_xml(xmlNode *parent, const char *prefix,
 #define CRM_DEFAULT_OP_TIMEOUT_S "20s"
 
 // Public resource agent functions (from agents.c)
+
+// Capabilities supported by a resource agent standard
+enum pcmk_ra_caps {
+    pcmk_ra_cap_none         = 0x000,
+    pcmk_ra_cap_provider     = 0x001, // Requires provider
+    pcmk_ra_cap_status       = 0x002, // Supports status instead of monitor
+    pcmk_ra_cap_params       = 0x004, // Supports parameters
+    pcmk_ra_cap_unique       = 0x008, // Supports unique clones
+    pcmk_ra_cap_promotable   = 0x010, // Supports promotable clones
+};
+
+uint32_t pcmk_get_ra_caps(const char *standard);
 char *crm_generate_ra_key(const char *standard, const char *provider,
                           const char *type);
-bool crm_provider_required(const char *standard);
 int crm_parse_agent_spec(const char *spec, char **standard, char **provider,
                          char **type);
+bool crm_provider_required(const char *standard); // deprecated
+
 
 int compare_version(const char *version1, const char *version2);
 

lib/common/agents.c
@@ -18,6 +18,52 @@
 #include <crm/crm.h>
 #include <crm/common/util.h>
 
+/*!
+ * \brief Get capabilities of a resource agent standard
+ *
+ * \param[in] standard  Standard name
+ *
+ * \return Bitmask of enum pcmk_ra_caps values
+ */
+uint32_t
+pcmk_get_ra_caps(const char *standard)
+{
+    /* @COMPAT This should probably be case-sensitive, but isn't,
+     * for backward compatibility.
+     */
+    if (standard == NULL) {
+        return pcmk_ra_cap_none;
+
+    } else if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_OCF)) {
+        return pcmk_ra_cap_provider | pcmk_ra_cap_params
+               | pcmk_ra_cap_unique | pcmk_ra_cap_promotable;
+
+    } else if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_STONITH)) {
+        /* @COMPAT Stonith resources can't really be unique clones, but we've
+         * allowed it in the past and have it in some scheduler regression tests
+         * (which were likely never used as real configurations).
+         *
+         * @TODO Remove pcmk_ra_cap_unique at the next major schema version
+         * bump, with a transform to remove globally-unique from the config.
+         */
+        return pcmk_ra_cap_params | pcmk_ra_cap_unique;
+
+    } else if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_SYSTEMD)
+               || !strcasecmp(standard, PCMK_RESOURCE_CLASS_SERVICE)
+               || !strcasecmp(standard, PCMK_RESOURCE_CLASS_LSB)
+               || !strcasecmp(standard, PCMK_RESOURCE_CLASS_UPSTART)) {
+
+        /* Since service can map to LSB, systemd, or upstart, these should
+         * have identical capabilities
+         */
+        return pcmk_ra_cap_status;
+
+    } else if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_NAGIOS)) {
+        return pcmk_ra_cap_params;
+    }
+    return pcmk_ra_cap_none;
+}
+
 char *
 crm_generate_ra_key(const char *standard, const char *provider,
                     const char *type)
@@ -33,6 +79,7 @@ crm_generate_ra_key(const char *standard, const char *provider,
 }
 
 /*!
+ * \deprecated
  * \brief Check whether a resource standard requires a provider to be specified
  *
  * \param[in] standard  Standard name
@@ -42,18 +89,7 @@ crm_generate_ra_key(const char *standard, const char *provider,
 bool
 crm_provider_required(const char *standard)
 {
-    CRM_CHECK(standard != NULL, return FALSE);
-
-    /* @TODO
-     * - this should probably be case-sensitive, but isn't,
-     *   for backward compatibility
-     * - it might be nice to keep standards' capabilities (supports provider,
-     *   can be promotable, etc.) as structured data somewhere
-     */
-    if (!strcasecmp(standard, PCMK_RESOURCE_CLASS_OCF)) {
-        return TRUE;
-    }
-    return FALSE;
+    return is_set(pcmk_get_ra_caps(standard), pcmk_ra_cap_provider);
 }
 
 /*!
@@ -89,7 +125,7 @@ crm_parse_agent_spec(const char *spec, char **standard, char **provider,
     *standard = strndup(spec, colon - spec);
     spec = colon + 1;
 
-    if (crm_provider_required(*standard)) {
+    if (is_set(pcmk_get_ra_caps(*standard), pcmk_ra_cap_provider)) {
         colon = strchr(spec, ':');
         if ((colon == NULL) || (colon == spec)) {
             free(*standard);

lib/lrmd/lrmd_client.c
@@ -1419,7 +1419,7 @@ lrmd_api_register_rsc(lrmd_t * lrmd,
     if (!class || !type || !rsc_id) {
         return -EINVAL;
     }
-    if (crm_provider_required(class) && !provider) {
+    if (is_set(pcmk_get_ra_caps(class), pcmk_ra_cap_provider) && !provider) {
         return -EINVAL;
     }
 
@@ -1522,7 +1522,8 @@ lrmd_api_get_rsc_info(lrmd_t * lrmd, const char *rsc_id, enum lrmd_call_options 
     if (!class || !type) {
         free_xml(output);
         return NULL;
-    } else if (crm_provider_required(class) && !provider) {
+    } else if (is_set(pcmk_get_ra_caps(class), pcmk_ra_cap_provider)
+               && !provider) {
         free_xml(output);
         return NULL;
     }

lib/pengine/native.c
@@ -542,7 +542,7 @@ common_print(resource_t * rsc, const char *pre_text, const char *name, node_t *n
     }
     offset += snprintf(buffer + offset, LINE_MAX - offset, "%s", name);
     offset += snprintf(buffer + offset, LINE_MAX - offset, "\t(%s", class);
-    if (crm_provider_required(class)) {
+    if (is_set(pcmk_get_ra_caps(class), pcmk_ra_cap_provider)) {
         const char *prov = crm_element_value(rsc->xml, XML_AGENT_ATTR_PROVIDER);
         offset += snprintf(buffer + offset, LINE_MAX - offset, "::%s", prov);
     }
@@ -823,7 +823,7 @@ get_rscs_brief(GListPtr rsc_list, GHashTable * rsc_table, GHashTable * active_ta
         }
 
         offset += snprintf(buffer + offset, LINE_MAX - offset, "%s", class);
-        if (crm_provider_required(class)) {
+        if (is_set(pcmk_get_ra_caps(class), pcmk_ra_cap_provider)) {
             const char *prov = crm_element_value(rsc->xml, XML_AGENT_ATTR_PROVIDER);
             offset += snprintf(buffer + offset, LINE_MAX - offset, "::%s", prov);
         }

lib/services/services.c
@@ -161,6 +161,7 @@ resources_action_create(const char *name, const char *standard,
                         GHashTable *params, enum svc_action_flags flags)
 {
     svc_action_t *op = NULL;
+    uint32_t ra_caps = 0;
 
     /*
      * Do some up front sanity checks before we go off and
@@ -176,9 +177,10 @@ resources_action_create(const char *name, const char *standard,
         crm_err("Cannot create operation for %s without resource class", name);
         goto return_error;
     }
+    ra_caps = pcmk_get_ra_caps(standard);
 
-    if (crm_provider_required(standard) && crm_strlen_zero(provider)) {
-        crm_err("Cannot create OCF operation for %s without provider", name);
+    if (is_set(ra_caps, pcmk_ra_cap_provider) && crm_strlen_zero(provider)) {
+        crm_err("Cannot create operation for %s without provider", name);
         goto return_error;
     }
 
@@ -207,27 +209,32 @@ resources_action_create(const char *name, const char *standard,
     op->flags = flags;
     op->id = generate_op_key(name, action, interval_ms);
 
-    if (safe_str_eq(action, "monitor")
-        && safe_str_eq(op->standard, PCMK_RESOURCE_CLASS_LSB)) {
-        action = "status";
+    if (is_set(ra_caps, pcmk_ra_cap_status) && safe_str_eq(action, "monitor")) {
+        op->action = strdup("status");
+    } else {
+        op->action = strdup(action);
     }
-    op->action = strdup(action);
 
-    if (crm_provider_required(op->standard)) {
+    if (is_set(ra_caps, pcmk_ra_cap_provider)) {
         op->provider = strdup(provider);
-        op->params = params;
-        params = NULL;
+    }
 
+    if (is_set(ra_caps, pcmk_ra_cap_params)) {
+        op->params = params;
+        params = NULL; // so we don't free them in this function
+    }
+
+    if (strcasecmp(op->standard, PCMK_RESOURCE_CLASS_OCF) == 0) {
         op->opaque->exec = crm_strdup_printf("%s/resource.d/%s/%s",
                                              OCF_ROOT_DIR, provider, agent);
         op->opaque->args[0] = strdup(op->opaque->exec);
-        op->opaque->args[1] = strdup(action);
+        op->opaque->args[1] = strdup(op->action);
 
     } else if (strcasecmp(op->standard, PCMK_RESOURCE_CLASS_LSB) == 0) {
         op->opaque->exec = services__lsb_agent_path(op->agent);
         op->opaque->args[0] = strdup(op->opaque->exec);
         op->opaque->args[1] = strdup(op->action);
-        op->opaque->args[2] = NULL;
+
 #if SUPPORT_SYSTEMD
     } else if (strcasecmp(op->standard, PCMK_RESOURCE_CLASS_SYSTEMD) == 0) {
         op->opaque->exec = strdup("systemd-dbus");
@@ -238,24 +245,21 @@ resources_action_create(const char *name, const char *standard,
 #endif
 #if SUPPORT_NAGIOS
     } else if (strcasecmp(op->standard, PCMK_RESOURCE_CLASS_NAGIOS) == 0) {
-        int index = 0;
-
         op->opaque->exec = dup_file_path(op->agent, NAGIOS_PLUGIN_DIR);
         op->opaque->args[0] = strdup(op->opaque->exec);
-        index = 1;
 
         if (safe_str_eq(op->action, "monitor") && (op->interval_ms == 0)) {
             /* Invoke --version for a nagios probe */
-            op->opaque->args[index] = strdup("--version");
-            index++;
+            op->opaque->args[1] = strdup("--version");
 
-        } else if (params) {
+        } else if (op->params) {
             GHashTableIter iter;
             char *key = NULL;
             char *value = NULL;
+            int index = 1;
             static int args_size = sizeof(op->opaque->args) / sizeof(char *);
 
-            g_hash_table_iter_init(&iter, params);
+            g_hash_table_iter_init(&iter, op->params);
 
             while (g_hash_table_iter_next(&iter, (gpointer *) & key, (gpointer *) & value) &&
                    index <= args_size - 3) {
@@ -267,12 +271,16 @@ resources_action_create(const char *name, const char *standard,
                 op->opaque->args[index++] = strdup(value);
             }
         }
-        op->opaque->args[index] = NULL;
+
+        // Nagios actions don't need to keep the parameters
+        if (op->params != NULL) {
+            g_hash_table_destroy(op->params);
+            op->params = NULL;
+        }
 #endif
     } else {
         crm_err("Unknown resource standard: %s", op->standard);
-        services_action_free(op);
-        op = NULL;
+        goto return_error;
     }
 
     if(params) {
@@ -1009,7 +1017,7 @@ resources_list_standards(void)
 GList *
 resources_list_providers(const char *standard)
 {
-    if (crm_provider_required(standard)) {
+    if (is_set(pcmk_get_ra_caps(standard), pcmk_ra_cap_provider)) {
         return resources_os_list_ocf_providers();
     }
 

tools/fake_transition.c
@@ -279,7 +279,8 @@ inject_resource(xmlNode * cib_node, const char *resource, const char *rclass, co
         fprintf(stderr, "Invalid class for %s: %s\n", resource, rclass);
         return NULL;
 
-    } else if (crm_provider_required(rclass) && (rprovider == NULL)) {
+    } else if (is_set(pcmk_get_ra_caps(rclass), pcmk_ra_cap_provider)
+                && (rprovider == NULL)) {
         fprintf(stderr, "Please specify the provider for resource %s\n", resource);
         return NULL;
     }

commit 15b9e93fb7b630a2e618130331eabe3fc5ead988

High: libcrmcommon: properly check whether resource supports parameters
This fixes a regression introduced in bdf69722 (not released) that broke
reloads.

lib/common/operations.c
@@ -562,7 +562,8 @@ crm_op_needs_metadata(const char *rsc_class, const char *op)
 
     CRM_CHECK(rsc_class || op, return FALSE);
 
-    if (is_set(pcmk_get_ra_caps(rsc_class), pcmk_ra_cap_params)) {
+    if (rsc_class
+        && is_not_set(pcmk_get_ra_caps(rsc_class), pcmk_ra_cap_params)) {
         /* Meta-data is only needed for resource classes that use parameters */
         return FALSE;
     }