9c993a7cca76da81448263fdecd78aad2badd06f
Ken Gaillot <kgaillot@redhat.com>
2018-11-16 09:48:43 -0600

    Test: CTS: don't require nodes to be specified if only listing tests
    Also, correct a couple of misspellings, avoid a redundant call to
    validate(), and call validate() before using the node list in discover().

--- cts/environment.py
+++ cts/environment.py
@@ -65,8 +65,9 @@ class Environment(object):
         self.target = "localhost"
 
         self.parse_args(args)
-        self.discover()
-        self.validate()
+        if self["ListTests"] == 0:
+            self.validate()
+            self.discover()
 
     def SeedRandom(self, seed=None):
         if not seed:
@@ -195,7 +196,7 @@ class Environment(object):
             self["at-boot"] = atboot
 
     def detect_ip_offset(self):
-        # Try to determin an offset for IPaddr resources
+        # Try to determine an offset for IPaddr resources
         if self["CIBResource"] and not "IPBase" in self.data:
             network=self.rsh(self.target, "ip addr | grep inet | grep -v -e link -e inet6 -e '/32' -e ' lo' | awk '{print $2}'", stdout=1).strip()
             self["IPBase"] = self.rsh(self.target, "nmap -sn -n %s | grep 'scan report' | awk '{print $NF}' | sed 's:(::' | sed 's:)::' | sort -V | tail -n 1" % network, stdout=1).strip()
@@ -242,8 +243,6 @@ class Environment(object):
         self.detect_syslog()
         self.detect_at_boot()
         self.detect_ip_offset()
-
-        self.validate()
 
     def parse_args(self, args):
         skipthis=None
