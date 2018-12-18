ClusterLabs/pacemaker

commit bf69beb3cda98cb1c68b099fe706d5cf2fb6188e

Low: scheduler: avoid container stop on wrong node
Previously, a failed resource with an on-fail of restart-container would lead
to a stop of the container on the failed resource's node. However, a bundle's
connection can run on a different node than its container, so that this could
be the wrong node. Now, remember that the container needs to be stopped, and
stop it once its location is known.

lib/pengine/unpack.c
@@ -1140,6 +1140,21 @@ unpack_status(xmlNode * status, pe_working_set_t * data_set)
     // Now catch any nodes we didn't see
     unpack_node_loop(status, is_set(data_set->flags, pe_flag_stonith_enabled), data_set);
 
+    /* Now that we know where resources are, we can schedule stops of containers
+     * with failed bundle connections
+     */
+    if (data_set->stop_needed != NULL) {
+        for (GList *item = data_set->stop_needed; item; item = item->next) {
+            pe_resource_t *container = item->data;
+            pe_node_t *node = pe__current_node(container);
+
+            if (node) {
+                stop_action(container, node, FALSE);
+            }
+        }
+        g_list_free(data_set->stop_needed);
+    }
+
     for (GListPtr gIter = data_set->nodes; gIter != NULL; gIter = gIter->next) {
         node_t *this_node = gIter->data;
 
@@ -1927,7 +1942,15 @@ process_rsc_state(resource_t * rsc, node_t * node,
         case action_fail_restart_container:
             set_bit(rsc->flags, pe_rsc_failed);
 
-            if (rsc->container) {
+            if (rsc->container && pe_rsc_is_bundled(rsc)) {
+                /* A bundle's remote connection can run on a different node than
+                 * the bundle's container. We don't necessarily know where the
+                 * container is running yet, so remember it and add a stop
+                 * action for it later.
+                 */
+                data_set->stop_needed = g_list_prepend(data_set->stop_needed,
+                                                       rsc->container);
+            } else if (rsc->container) {
                 stop_action(rsc->container, node, FALSE);
             } else if (rsc->role != RSC_ROLE_STOPPED && rsc->role != RSC_ROLE_UNKNOWN) {
                 stop_action(rsc, node, FALSE);

include/crm/pengine/status.h
@@ -134,6 +134,7 @@ struct pe_working_set_s {
     int disabled_resources;
 
     GList *param_check; // History entries that need to be checked
+    GList *stop_needed; // Containers that need stop actions
 };
 
 enum pe_check_parameters {

lib/pengine/failcounts.c
@@ -299,7 +299,19 @@ pe_get_failcount(node_t *node, resource_t *rsc, time_t *last_failure,
         }
     }
 
-    if (is_set(flags, pe_fc_fillers) && rsc->fillers) {
+    /* We never want the fail counts of a bundle container's fillers to
+     * count towards the container's fail count.
+     *
+     * Most importantly, a Pacemaker Remote connection to a bundle container
+     * is a filler of the container, but can reside on a different node than the
+     * container itself. Counting its fail count on its node towards the
+     * container's fail count on that node could lead to attempting to stop the
+     * container on the wrong node.
+     */
+
+    if (is_set(flags, pe_fc_fillers) && rsc->fillers
+        && !pe_rsc_is_bundled(rsc)) {
+
         GListPtr gIter = NULL;
 
         for (gIter = rsc->fillers; gIter != NULL; gIter = gIter->next) {

lib/pengine/status.c
@@ -277,6 +277,7 @@ cleanup_calculations(pe_working_set_t * data_set)
     pe_free_nodes(data_set->nodes);
 
     pe__free_param_checks(data_set);
+    g_list_free(data_set->stop_needed);
     free_xml(data_set->graph);
     crm_time_free(data_set->now);
     free_xml(data_set->input);

commit 5f75a663a1b5ea13a4cef114dc13169b01b2b29a

Fix: libpe_status: avoid double free of stop_needed list
regression in 2.0.1-rc1 introduced by bf69beb3

lib/pengine/unpack.c
@@ -1153,6 +1153,7 @@ unpack_status(xmlNode * status, pe_working_set_t * data_set)
             }
         }
         g_list_free(data_set->stop_needed);
+        data_set->stop_needed = NULL;
     }
 
     for (GListPtr gIter = data_set->nodes; gIter != NULL; gIter = gIter->next) {