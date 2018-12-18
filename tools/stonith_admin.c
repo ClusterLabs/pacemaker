ClusterLabs/pacemaker

commit cc4c8411be9809b76a86cd81024a614460e0caa1

High: pacemaker-fenced: fix easily plausible NULL pointer dereference
Previously, making pacemaker-fenced segfault was as easy as:

  stonith_admin -R whatever

i.e., without "-a AGENT" specification, resulting in:

  <st_device_id id="whatever" st_origin="create_device_registration_xml">
    <attributes/>
  </st_device_id>

stonith operation message internally.

Note that the respective CLI interface is relevant to "stand-alone"
modes of execution with pacemaker-fenced, as opposed to full-fledged
pacemaker run, but the danger of accidental misuse is imminent
regardless.

Also note that going forward, we would be better off declaring what
the valid messages like this are, reusing already existing RelaxNG
schema based infrastructure.  Amongst others, it would also alleviate
the needs for otherwise extremely hefty reverse engineering, and
allow to follow the compatibility guarantees easily.  It would
also allow us to gradually drop clumsy ad-hoc parse-time checks
when the validation gets moved over to the sending side (when
this circumstance is somehow indicated to the sender).

tools/stonith_admin.c
@@ -14,10 +14,11 @@
 #include <unistd.h>
 #include <sys/utsname.h>
 
-#include <stdlib.h>
-#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <crm/crm.h>
 #include <crm/msg_xml.h>
@@ -457,6 +458,7 @@ main(int argc, char **argv)
     int no_connect = 0;
     int tolerance = 0;
     int as_nodeid = FALSE;
+    bool required_agent = false;
 
     char *name = NULL;
     char *value = NULL;
@@ -496,6 +498,7 @@ main(int argc, char **argv)
             case 'I':
             case 'K':
                 no_connect = 1;
+                required_agent = true;
                 /* fall through */
             case 'L':
                 action = flag;
@@ -509,8 +512,10 @@ main(int argc, char **argv)
             case 'b':
                 broadcast = 1;
                 break;
-            case 'Q':
             case 'R':
+                required_agent = true;
+                /* fall through */
+            case 'Q':
             case 'D':
             case 's':
                 action = flag;
@@ -530,6 +535,7 @@ main(int argc, char **argv)
             case 'M':
                 no_connect = 1;
                 action = flag;
+                required_agent = true;
                 break;
             case 't':
                 timeout = crm_atoi(optarg, NULL);
@@ -601,11 +607,12 @@ main(int argc, char **argv)
         }
     }
 
-    if (optind > argc) {
+    if (optind > argc || action == 0) {
         ++argerr;
     }
 
-    if (action == 0) {
+    if (required_agent && agent == NULL) {
+        printf("Please specify an agent to query using -a,--agent [value]\n");
         ++argerr;
     }
 
@@ -701,11 +708,7 @@ main(int argc, char **argv)
             rc = handle_level(st, target, fence_level, devices, action == 'r');
             break;
         case 'M':
-            if (agent == NULL) {
-                printf("Please specify an agent to query using -a,--agent [value]\n");
-                exit_code = CRM_EX_USAGE;
-                goto done;
-            } else {
+            {
                 char *buffer = NULL;
 
                 rc = st->cmds->metadata(st, st_opt_sync_call, agent, NULL, &buffer, timeout);
@@ -749,11 +752,6 @@ main(int argc, char **argv)
                                 verbose, cleanup, broadcast);
             break;
         case 'K':
-            if (agent == NULL) {
-                printf("Please specify an agent to validate with --agent\n");
-                exit_code = CRM_EX_USAGE;
-                goto done;
-            }
             device = (devices? devices->key : NULL);
             rc = validate(st, agent, device, params, timeout, quiet);
             break;

daemons/fenced/fenced_commands.c
@@ -900,13 +900,19 @@ xml2device_params(const char *name, xmlNode *dev)
 static stonith_device_t *
 build_device_from_xml(xmlNode * msg)
 {
-    const char *value = NULL;
+    const char *value;
     xmlNode *dev = get_xpath_object("//" F_STONITH_DEVICE, msg, LOG_ERR);
     stonith_device_t *device = NULL;
+    char *agent = crm_element_value_copy(dev, "agent");
+
+    CRM_CHECK(agent != NULL, return device);
 
     device = calloc(1, sizeof(stonith_device_t));
+
+    CRM_CHECK(device != NULL, {free(agent); return device;});
+
     device->id = crm_element_value_copy(dev, XML_ATTR_ID);
-    device->agent = crm_element_value_copy(dev, "agent");
+    device->agent = agent;
     device->namespace = crm_element_value_copy(dev, "namespace");
     device->params = xml2device_params(device->id, dev);
 
@@ -1163,6 +1169,8 @@ stonith_device_register(xmlNode * msg, const char **desc, gboolean from_cib)
 {
     stonith_device_t *dup = NULL;
     stonith_device_t *device = build_device_from_xml(msg);
+
+    CRM_CHECK(device != NULL, return -ENOMEM);
 
     dup = device_has_duplicate(device);
     if (dup) {

daemons/fenced/pacemaker-fenced.c
@@ -637,6 +637,7 @@ static void cib_device_update(resource_t *rsc, pe_working_set_t *data_set)
 
     } else {
         /* Our node is allowed, so update the device information */
+        int rc;
         xmlNode *data;
         GHashTableIter gIter;
         stonith_key_value_t *params = NULL;
@@ -663,9 +664,9 @@ static void cib_device_update(resource_t *rsc, pe_working_set_t *data_set)
         remove = FALSE;
         data = create_device_registration_xml(rsc_name(rsc), st_namespace_any,
                                               agent, params, rsc_provides);
-        stonith_device_register(data, NULL, TRUE);
-
         stonith_key_value_freeall(params, 1, 1);
+        rc = stonith_device_register(data, NULL, TRUE);
+        CRM_ASSERT(rc == pcmk_ok);
         free_xml(data);
     }
 
@@ -1469,6 +1470,7 @@ main(int argc, char **argv)
     init_topology_list();
 
     if(stonith_watchdog_timeout_ms > 0) {
+        int rc;
         xmlNode *xml;
         stonith_key_value_t *params = NULL;
 
@@ -1477,10 +1479,13 @@ main(int argc, char **argv)
         xml = create_device_registration_xml("watchdog", st_namespace_internal,
                                              STONITH_WATCHDOG_AGENT, params,
                                              NULL);
-        stonith_device_register(xml, NULL, FALSE);
-
         stonith_key_value_freeall(params, 1, 1);
+        rc = stonith_device_register(xml, NULL, FALSE);
         free_xml(xml);
+        if (rc != pcmk_ok) {
+            crm_crit("Cannot register watchdog pseudo fence agent");
+            crm_exit(CRM_EX_FATAL);
+        }
     }
 
     stonith_ipc_server_init(&ipcs, &ipc_callbacks);

commit 7a749048e5462430c38a0b233b8e0e51f346f98e

Fix: tools: stonith_admin -I doesn't require an agent
regression since cc4c8411b (2.0.1-rc1)

tools/stonith_admin.c
@@ -495,14 +495,17 @@ main(int argc, char **argv)
             case '?':
                 crm_help(flag, CRM_EX_OK);
                 break;
-            case 'I':
+
             case 'K':
-                no_connect = 1;
                 required_agent = true;
+                /* fall through */
+            case 'I':
+                no_connect = 1;
                 /* fall through */
             case 'L':
                 action = flag;
                 break;
+
             case 'q':
                 quiet = 1;
                 break;