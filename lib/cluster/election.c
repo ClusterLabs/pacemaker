9b0a0f29e8ae07a9a2808900b9490e29a5fc67e3
Ken Gaillot <kgaillot@redhat.com>
2019-01-24 17:48:10 -0600

    Low: libcrmcluster: *really* write only one election storm black box
    464d481 didn't do what it thought it did

--- lib/cluster/election.c
+++ lib/cluster/election.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2018 Andrew Beekhof <andrew@beekhof.net>
+ * Copyright 2004-2019 Andrew Beekhof <andrew@beekhof.net>
  *
  * This source code is licensed under the GNU Lesser General Public License
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
@@ -644,6 +644,7 @@ election_count_vote(election_t *e, xmlNode *message, bool can_win)
                  * write a blackbox on every Nth occurrence.
                  */
                 crm_write_blackbox(0, NULL);
+                wrote_blackbox = TRUE;
             }
         }
     }
