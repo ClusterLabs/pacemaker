10334f628cd3891a68da414521d9cc6af509fdf6
Chris Lumens <clumens@redhat.com>
2019-01-31 14:19:38 -0500

    Build: libp-i: Move and rename source files.
    All the source files for libpacemaker-internal are now in their own
    directory under lib/.  In addition, the files have a pcmki_ prefix now
    to make it easier to tell in trace logs where messages originated.
    Includes are also now under include/pcmki/ and have been renamed
    similarly.  There is now also a top-level include/pacemaker-internal.h
    that includes all those files so the user doesn't have to know which to
    include.

---
+++ include/pacemaker-internal.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 Chris Lumens <clumens@redhat.com>
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PACEMAKER_INTERNAL__H
+#  define PACEMAKER_INTERNAL__H
+
+#  include <pcmki/pcmki_sched_allocate.h>
+#  include <pcmki/pcmki_sched_notif.h>
+#  include <pcmki/pcmki_sched_utils.h>
+
+#endif
