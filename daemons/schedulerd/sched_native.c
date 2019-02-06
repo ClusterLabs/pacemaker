b86b40d3858fd364687e4f41b5e233fc2aa7bc1b
Ken Gaillot <kgaillot@redhat.com>
2019-02-05 19:09:49 -0600

    Test: scheduler: sort nodes when creating constraints
    We want to keep action ID numbers the same for regression test output
    comparisons, so convert the allowed nodes hash table to a list before
    creating actions while creating native constraints. Sort the list only
    if printing to stdout, so the live cluster doesn't take the performance hit.

--- daemons/schedulerd/sched_native.c
+++ daemons/schedulerd/sched_native.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2018 Andrew Beekhof <andrew@beekhof.net>
+ * Copyright 2004-2019 Andrew Beekhof <andrew@beekhof.net>
  *
  * This source code is licensed under the GNU General Public License version 2
  * or later (GPLv2+) WITHOUT ANY WARRANTY.
@@ -1331,6 +1331,35 @@ rsc_avoids_remote_nodes(resource_t *rsc)
     }
 }
 
+/*!
+ * \internal
+ * \brief Return allowed nodes as (possibly sorted) list
+ *
+ * Convert a resource's hash table of allowed nodes to a list. If printing to
+ * stdout, sort the list, to keep action ID numbers consistent for regression
+ * test output (while avoiding the performance hit on a live cluster).
+ *
+ * \param[in] rsc       Resource to check for allowed nodes
+ * \param[in] data_set  Cluster working set
+ *
+ * \return List of resource's allowed nodes
+ * \note Callers should take care not to rely on the list being sorted.
+ */
+static GList *
+allowed_nodes_as_list(pe_resource_t *rsc, pe_working_set_t *data_set)
+{
+    GList *allowed_nodes = NULL;
+
+    if (rsc->allowed_nodes) {
+        allowed_nodes = g_hash_table_get_values(rsc->allowed_nodes);
+    }
+
+    if (is_set(data_set->flags, pe_flag_stdout)) {
+        allowed_nodes = g_list_sort(allowed_nodes, sort_node_uname);
+    }
+    return allowed_nodes;
+}
+
 void
 native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
 {
@@ -1338,7 +1367,17 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
 
     resource_t *top = uber_parent(rsc);
     int type = pe_order_optional | pe_order_implies_then | pe_order_restart;
-    gboolean is_stonith = is_set(rsc->flags, pe_rsc_fence_device);
+    GList *allowed_nodes = NULL;
+
+    // Whether resource requires unfencing
+    bool check_unfencing = is_not_set(rsc->flags, pe_rsc_fence_device)
+                           && is_set(data_set->flags, pe_flag_enable_unfencing)
+                           && is_set(rsc->flags, pe_rsc_needs_unfencing);
+
+    // Whether a non-default placement strategy is used
+    bool check_utilization = is_set(rsc->flags, pe_rsc_managed)
+                             && (g_hash_table_size(rsc->utilization) > 0)
+                             && safe_str_neq(data_set->placement_strategy, "default");
 
     custom_action_order(rsc, generate_op_key(rsc->id, RSC_STOP, 0), NULL,
                         rsc, generate_op_key(rsc->id, RSC_START, 0), NULL, type, data_set);
@@ -1353,16 +1392,16 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
                             pe_order_runnable_left, data_set);
     }
 
-    if (is_stonith == FALSE
-        && is_set(data_set->flags, pe_flag_enable_unfencing)
-        && is_set(rsc->flags, pe_rsc_needs_unfencing)) {
-        /* Check if the node needs to be unfenced first */
-        node_t *node = NULL;
-        GHashTableIter iter;
+    if (check_unfencing || check_utilization || rsc->container) {
+        allowed_nodes = allowed_nodes_as_list(rsc, data_set);
+    }
 
-        g_hash_table_iter_init(&iter, rsc->allowed_nodes);
-        while (g_hash_table_iter_next(&iter, NULL, (void **)&node)) {
-            action_t *unfence = pe_fence_op(node, "on", TRUE, NULL, data_set);
+    if (check_unfencing) {
+        /* Check if the node needs to be unfenced first */
+
+        for (GList *item = allowed_nodes; item; item = item->next) {
+            pe_node_t *node = item->data;
+            pe_action_t *unfence = pe_fence_op(node, "on", TRUE, NULL, data_set);
 
             crm_debug("Ordering any stops of %s before %s, and any starts after",
                       rsc->id, unfence->uuid);
@@ -1395,13 +1434,11 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
 
     if (is_not_set(rsc->flags, pe_rsc_managed)) {
         pe_rsc_trace(rsc, "Skipping fencing constraints for unmanaged resource: %s", rsc->id);
+        g_list_free(allowed_nodes);
         return;
     }
 
-    if (g_hash_table_size(rsc->utilization) > 0
-        && safe_str_neq(data_set->placement_strategy, "default")) {
-        GHashTableIter iter;
-        node_t *next = NULL;
+    if (check_utilization) {
         GListPtr gIter = NULL;
 
         pe_rsc_trace(rsc, "Creating utilization constraints for %s - strategy: %s",
@@ -1422,8 +1459,8 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
                                 NULL, load_stopped_task, load_stopped, pe_order_load, data_set);
         }
 
-        g_hash_table_iter_init(&iter, rsc->allowed_nodes);
-        while (g_hash_table_iter_next(&iter, NULL, (void **)&next)) {
+        for (GList *item = allowed_nodes; item; item = item->next) {
+            pe_node_t *next = item->data;
             char *load_stopped_task = crm_concat(LOAD_STOPPED, next->details->uname, '_');
             action_t *load_stopped = get_pseudo_op(load_stopped_task, data_set);
 
@@ -1483,10 +1520,9 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
             /* Force the resource on the Pacemaker Remote node instead of
              * colocating the resource with the container resource.
              */
-            GHashTableIter iter;
-            node_t *node = NULL;
-            g_hash_table_iter_init(&iter, rsc->allowed_nodes);
-            while (g_hash_table_iter_next(&iter, NULL, (void **)&node)) {
+            for (GList *item = allowed_nodes; item; item = item->next) {
+                pe_node_t *node = item->data;
+
                 if (node->details->remote_rsc != remote_rsc) {
                     node->weight = -INFINITY;
                 }
@@ -1520,11 +1556,12 @@ native_internal_constraints(resource_t * rsc, pe_working_set_t * data_set)
         }
     }
 
-    if (rsc->is_remote_node || is_stonith) {
+    if (rsc->is_remote_node || is_set(rsc->flags, pe_rsc_fence_device)) {
         /* don't allow remote nodes to run stonith devices
          * or remote connection resources.*/
         rsc_avoids_remote_nodes(rsc);
     }
+    g_list_free(allowed_nodes);
 }
 
 void
