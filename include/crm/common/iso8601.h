b368355b0d113525fafde922af468ec36359fe80
Ken Gaillot <kgaillot@redhat.com>
2019-03-15 17:45:25 -0500

    Refactor: libpe_status: straighten out header dependencies
    Previously, crm/pengine/status.h and crm/pengine/complex.h had
    interdependencies such that status.h could be included alone or before
    complex.h, but complex.h could only be included after status.h.
    
    22d066c1 included complex.h alone from compatibility.h, which broke
    compatibility.h (obviously, no one's using it, which is a good thing anyway).
    
    Commit 85334374 tried to get around the dependency by including status.h in
    complex.h, but that was a mistake. Thankfully it did not break any usage that
    was not already broken.
    
    This commit rectifies the situation by adding a new header, pe_types.h, with
    the type definitions and enums from both complex.h and status.h, which are
    reduced to including the new header and declaring various functions.
    
    All of this should make the ABI compliance checker happier, as well as being
    more correct.

--- include/crm/common/iso8601.h
+++ include/crm/common/iso8601.h
@@ -1,19 +1,10 @@
-/* 
- * Copyright (C) 2005 Andrew Beekhof <andrew@beekhof.net>
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/*
+ * Copyright 2005-2019 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
  */
 
 #ifndef CRM_COMMON_ISO8601
@@ -36,7 +27,8 @@ extern "C" {
 
 #  include <time.h>
 #  include <ctype.h>
-#  include <stdbool.h>
+#  include <stdint.h>   // uint32_t
+#  include <stdbool.h>  // bool
 
 typedef struct crm_time_s crm_time_t;
 
--- include/crm/pengine/complex.h
+++ include/crm/pengine/complex.h
@@ -1,5 +1,7 @@
 /*
- * Copyright 2004-2019 Andrew Beekhof <andrew@beekhof.net>
+ * Copyright 2004-2019 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
  *
  * This source code is licensed under the GNU Lesser General Public License
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
@@ -12,31 +14,9 @@
 extern "C" {
 #endif
 
-#include <glib.h>                   // gboolean, GList, GHashTable
+#include <glib.h>                   // gboolean, GHashTable
 #include <libxml/tree.h>            // xmlNode
-#include <crm/pengine/status.h>
-
-enum pe_obj_types {
-    pe_unknown = -1,
-    pe_native = 0,
-    pe_group = 1,
-    pe_clone = 2,
-    pe_container = 3,
-};
-
-typedef struct resource_object_functions_s {
-    gboolean (*unpack) (pe_resource_t*, pe_working_set_t*);
-    pe_resource_t *(*find_rsc) (pe_resource_t *parent, const char *search,
-                                const pe_node_t *node, int flags);
-    /* parameter result must be free'd */
-    char *(*parameter) (pe_resource_t*, pe_node_t*, gboolean, const char*,
-                        pe_working_set_t*);
-    void (*print) (pe_resource_t*, const char*, long, void*);
-    gboolean (*active) (pe_resource_t*, gboolean);
-    enum rsc_role_e (*state) (const pe_resource_t*, gboolean);
-    pe_node_t *(*location) (const pe_resource_t*, GList**, int);
-    void (*free) (pe_resource_t*);
-} resource_object_functions_t;
+#include <crm/pengine/pe_types.h>   // pe_node_t, pe_resource_t, etc.
 
 extern resource_object_functions_t resource_class_functions[];
 void get_meta_attributes(GHashTable * meta_hash, pe_resource_t *rsc,
@@ -48,8 +28,6 @@ void get_rsc_attributes(GHashTable *meta_hash, pe_resource_t *rsc,
 void pe_get_versioned_attributes(xmlNode *meta_hash, pe_resource_t *rsc,
                                  pe_node_t *node, pe_working_set_t *data_set);
 #endif
-
-typedef struct resource_alloc_functions_s resource_alloc_functions_t;
 
 gboolean is_parent(pe_resource_t *child, pe_resource_t *rsc);
 pe_resource_t *uber_parent(pe_resource_t *rsc);
