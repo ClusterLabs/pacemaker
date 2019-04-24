ClusterLabs/pacemaker

commit 14c94ddcf650051564b993f7cf2f76503b34c9d6

Refactor: libcrmcommon,crm_mon: make crm_signal() return previous handler
... so crm_mon can use it instead of signal(), which is considered non-portable
for this purpose.

While we're at it, improve crm_signal() log messages, and move crm_mon's
handling of HAVE_SIGHANDLER_T to mainloop.h where it can be of more use.

include/crm/common/mainloop.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2018 the Pacemaker project contributors
+ * Copyright 2009-2019 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -20,6 +20,7 @@ extern "C" {
  * \ingroup core
  */
 
+#  include <signal.h> // sighandler_t
 #  include <glib.h>
 
 enum mainloop_child_flags {
@@ -43,7 +44,11 @@ void mainloop_trigger_complete(crm_trigger_t * trig);
 
 gboolean mainloop_destroy_trigger(crm_trigger_t * source);
 
-gboolean crm_signal(int sig, void (*dispatch) (int sig));
+#  ifndef HAVE_SIGHANDLER_T
+typedef void (*sighandler_t)(int);
+#  endif
+
+sighandler_t crm_signal(int sig, sighandler_t dispatch);
 
 gboolean mainloop_add_signal(int sig, void (*dispatch) (int sig));
 

lib/common/mainloop.c
@@ -321,18 +321,20 @@ static GSourceFuncs crm_signal_funcs = {
  * \param[in] sig       Signal number to register handler for
  * \param[in] dispatch  Signal handler
  *
+ * \return The previous value of the signal handler, or SIG_ERR on error
  * \note The dispatch function must be async-safe.
  */
-gboolean
-crm_signal(int sig, void (*dispatch) (int sig))
+sighandler_t
+crm_signal(int sig, sighandler_t dispatch)
 {
     sigset_t mask;
     struct sigaction sa;
     struct sigaction old;
 
     if (sigemptyset(&mask) < 0) {
-        crm_perror(LOG_ERR, "Call to sigemptyset failed");
-        return FALSE;
+        crm_err("Could not set handler for signal %d: %s",
+                sig, pcmk_strerror(errno));
+        return SIG_ERR;
     }
 
     memset(&sa, 0, sizeof(struct sigaction));
@@ -341,11 +343,11 @@ crm_signal(int sig, void (*dispatch) (int sig))
     sa.sa_mask = mask;
 
     if (sigaction(sig, &sa, &old) < 0) {
-        crm_perror(LOG_ERR, "Could not install signal handler for signal %d", sig);
-        return FALSE;
+        crm_err("Could not set handler for signal %d: %s",
+                sig, pcmk_strerror(errno));
+        return SIG_ERR;
     }
-
-    return TRUE;
+    return old.sa_handler;
 }
 
 static void
@@ -406,7 +408,7 @@ mainloop_add_signal(int sig, void (*dispatch) (int sig))
     crm_signals[sig]->handler = dispatch;
     crm_signals[sig]->signal = sig;
 
-    if (crm_signal(sig, mainloop_signal_handler) == FALSE) {
+    if (crm_signal(sig, mainloop_signal_handler) == SIG_ERR) {
         mainloop_destroy_signal_entry(sig);
         return FALSE;
     }
@@ -431,7 +433,7 @@ mainloop_destroy_signal(int sig)
         crm_err("Signal %d is out of range", sig);
         return FALSE;
 
-    } else if (crm_signal(sig, NULL) == FALSE) {
+    } else if (crm_signal(sig, NULL) == SIG_ERR) {
         crm_perror(LOG_ERR, "Could not uninstall signal handler for signal %d", sig);
         return FALSE;
 

tools/crm_mon.c
@@ -22,6 +22,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <libgen.h>
+#include <signal.h>
 #include <sys/utsname.h>
 
 #include <crm/msg_xml.h>
@@ -256,15 +257,9 @@ mon_shutdown(int nsig)
     clean_up(CRM_EX_OK);
 }
 
-#if ON_DARWIN
-#  define sighandler_t sig_t
-#endif
-
 #if CURSES_ENABLED
-#  ifndef HAVE_SIGHANDLER_T
-typedef void (*sighandler_t) (int);
-#  endif
 static sighandler_t ncurses_winch_handler;
+
 static void
 mon_winresize(int nsig)
 {
@@ -925,7 +920,7 @@ main(int argc, char **argv)
     mainloop_add_signal(SIGINT, mon_shutdown);
 #if CURSES_ENABLED
     if (output_format == mon_output_console) {
-        ncurses_winch_handler = signal(SIGWINCH, mon_winresize);
+        ncurses_winch_handler = crm_signal(SIGWINCH, mon_winresize);
         if (ncurses_winch_handler == SIG_DFL ||
             ncurses_winch_handler == SIG_IGN || ncurses_winch_handler == SIG_ERR)
             ncurses_winch_handler = NULL;

commit f23e84252eb7e366fbc2bb7bb3a17d10a55e1ab9

Refactor: libcrmcommon: restore crm_signal() signature
14c94ddc mistakenly assumed crm_signal() was private API. Since it's actually
public, create a new crm_signal_handler() function and make crm_signal() a
deprecated wrapper for it, to preserve public API backward compatibility.

daemons/based/cibmon.c
@@ -1,20 +1,10 @@
-
-/* 
- * Copyright (C) 2004 Andrew Beekhof <andrew@beekhof.net>
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/*
+ * Copyright 2004-2019 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU General Public License version 2
+ * or later (GPLv2+) WITHOUT ANY WARRANTY.
  */
 
 #include <crm_internal.h>
@@ -87,7 +77,7 @@ main(int argc, char **argv)
 
     crm_log_cli_init("cibmon");
 
-    crm_signal(SIGTERM, cibmon_shutdown);
+    crm_signal_handler(SIGTERM, cibmon_shutdown);
 
     while (1) {
 #ifdef HAVE_GETOPT_H

include/crm/common/mainloop.h
@@ -48,7 +48,8 @@ gboolean mainloop_destroy_trigger(crm_trigger_t * source);
 typedef void (*sighandler_t)(int);
 #  endif
 
-sighandler_t crm_signal(int sig, sighandler_t dispatch);
+sighandler_t crm_signal_handler(int sig, sighandler_t dispatch);
+gboolean crm_signal(int sig, void (*dispatch) (int sig)); // deprecated
 
 gboolean mainloop_add_signal(int sig, void (*dispatch) (int sig));
 

lib/common/logging.c
@@ -401,11 +401,11 @@ crm_control_blackbox(int nsig, bool enable)
         crm_notice("Initiated blackbox recorder: %s", blackbox_file_prefix);
 
         /* Save to disk on abnormal termination */
-        crm_signal(SIGSEGV, crm_trigger_blackbox);
-        crm_signal(SIGABRT, crm_trigger_blackbox);
-        crm_signal(SIGILL,  crm_trigger_blackbox);
-        crm_signal(SIGBUS,  crm_trigger_blackbox);
-        crm_signal(SIGFPE,  crm_trigger_blackbox);
+        crm_signal_handler(SIGSEGV, crm_trigger_blackbox);
+        crm_signal_handler(SIGABRT, crm_trigger_blackbox);
+        crm_signal_handler(SIGILL,  crm_trigger_blackbox);
+        crm_signal_handler(SIGBUS,  crm_trigger_blackbox);
+        crm_signal_handler(SIGFPE,  crm_trigger_blackbox);
 
         crm_update_callsites();
 
@@ -497,7 +497,7 @@ crm_write_blackbox(int nsig, struct qb_log_callsite *cs)
             /* Do as little as possible, just try to get what we have out
              * We logged the filename when the blackbox was enabled
              */
-            crm_signal(nsig, SIG_DFL);
+            crm_signal_handler(nsig, SIG_DFL);
             qb_log_blackbox_write_to_file((const char *)blackbox_file_prefix);
             qb_log_ctl(QB_LOG_BLACKBOX, QB_LOG_CONF_ENABLED, QB_FALSE);
             raise(nsig);

lib/common/mainloop.c
@@ -325,7 +325,7 @@ static GSourceFuncs crm_signal_funcs = {
  * \note The dispatch function must be async-safe.
  */
 sighandler_t
-crm_signal(int sig, sighandler_t dispatch)
+crm_signal_handler(int sig, sighandler_t dispatch)
 {
     sigset_t mask;
     struct sigaction sa;
@@ -348,6 +348,16 @@ crm_signal(int sig, sighandler_t dispatch)
         return SIG_ERR;
     }
     return old.sa_handler;
+}
+
+/*
+ * \brief Use crm_signal_handler() instead
+ * \deprecated
+ */
+gboolean
+crm_signal(int sig, void (*dispatch) (int sig))
+{
+    return crm_signal_handler(sig, dispatch) != SIG_ERR;
 }
 
 static void
@@ -408,7 +418,7 @@ mainloop_add_signal(int sig, void (*dispatch) (int sig))
     crm_signals[sig]->handler = dispatch;
     crm_signals[sig]->signal = sig;
 
-    if (crm_signal(sig, mainloop_signal_handler) == SIG_ERR) {
+    if (crm_signal_handler(sig, mainloop_signal_handler) == SIG_ERR) {
         mainloop_destroy_signal_entry(sig);
         return FALSE;
     }
@@ -433,7 +443,7 @@ mainloop_destroy_signal(int sig)
         crm_err("Signal %d is out of range", sig);
         return FALSE;
 
-    } else if (crm_signal(sig, NULL) == SIG_ERR) {
+    } else if (crm_signal_handler(sig, NULL) == SIG_ERR) {
         crm_perror(LOG_ERR, "Could not uninstall signal handler for signal %d", sig);
         return FALSE;
 

tools/crm_mon.c
@@ -569,7 +569,7 @@ refresh:
 }
 #endif
 
-// Basically crm_signal(SIGCHLD, SIG_IGN) plus the SA_NOCLDWAIT flag
+// Basically crm_signal_handler(SIGCHLD, SIG_IGN) plus the SA_NOCLDWAIT flag
 static void
 avoid_zombies()
 {
@@ -936,7 +936,7 @@ main(int argc, char **argv)
     mainloop_add_signal(SIGINT, mon_shutdown);
 #if CURSES_ENABLED
     if (output_format == mon_output_console) {
-        ncurses_winch_handler = crm_signal(SIGWINCH, mon_winresize);
+        ncurses_winch_handler = crm_signal_handler(SIGWINCH, mon_winresize);
         if (ncurses_winch_handler == SIG_DFL ||
             ncurses_winch_handler == SIG_IGN || ncurses_winch_handler == SIG_ERR)
             ncurses_winch_handler = NULL;