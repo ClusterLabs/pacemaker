c46477fe38bdede01a070183052e5fa76f3631ef
Jan Pokorný <jpokorny@redhat.com>
2019-06-06 14:47:40 +0200

    Maint: introduce mock/experimental skeletons, now for cib (based) daemon
    [this is a rebase from 2.0, occurrences of "based" mean "cib", but they
    are not changed everywhere, especially not in the name of the mocked
    daemon, for easy comparability amongst the branches]
    
    This is meant for multiple purposes (non-exhaustive):
    
    - simulating scenarios that are rather hard to achieve in practice
      (overwhelming number of nodes or artificially capped resources)
    
    - robustness testing (will other daemons survive intermittently, yet
      frequently missing responses to the based queries?  to what extent
      do they rely on some causality relations like notify-after-write?
      what if a chance of continuously changed CIB contents causing
      troubles?)
    
    - anything not fitting the former two categories
    
    Another option would be to start from a full-fledged daemon, cutting it
    to pieces self-contained, patchable pieces, but since in case of based,
    it has grown rather organically as a monolith, it would be substantially
    more hassle than start grab the most vital pieces and start with a clean
    room.  The aim is to keep additional functionality on top of said
    skeleton, and let the additional functionality stay apart in opt-in
    loosly coupled modules.  We shall see how this works out.

---
+++ maint/mocked/based.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * Licensed under the GNU General Public License version 2 or later (GPLv2+).
+ */
+
+#pragma once
+
+#include <stdlib.h>  /* size_t */
+#include <stdbool.h>  /* bool */
+
+#include <crm/common/ipcs.h>  /* crm_client_t */
+
+
+struct module_s;
+
+typedef struct mock_based_context_s {
+    size_t modules_cnt;
+    struct module_s** modules;
+} mock_based_context_t;
+
+
+typedef int (*mock_based_argparse_hook)(mock_based_context_t *,
+                                        bool, int,
+                                        const char *[]);
+
+typedef void (*mock_based_destroy_hook)(struct module_s *);
+
+/* specialized callbacks... */
+
+typedef struct mock_based_hooks_s {
+    /* generic ones */
+    mock_based_argparse_hook argparse;
+    mock_based_destroy_hook destroy;
+
+    /* specialized callbacks... */
+} mock_based_hooks_t;
+
+typedef struct module_s {
+    char shortopt;
+    mock_based_hooks_t hooks;
+    void *priv;
+} module_t;
+
+size_t mock_based_register_module(module_t mod);
