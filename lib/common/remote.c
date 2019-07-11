ClusterLabs/pacemaker

commit 5bded362470480b37da7c6ef2db9e1ef6b0da891

Low: libcrmcommon: improve TLS session initialization failure handling
Refactor crm_create_anon_tls_session() and create_psk_tls_session() into
a single new function pcmk__new_tls_session(). On GnuTLS API errors, log a
useful error message and fail, rather than attempt to use a NULL or
insufficiently initialized session object.

lib/common/remote.c
@@ -166,61 +166,80 @@ crm_initiate_client_tls_handshake(crm_remote_t * remote, int timeout_ms)
     return rc;
 }
 
-void *
-crm_create_anon_tls_session(int csock, int type /* GNUTLS_SERVER, GNUTLS_CLIENT */ ,
-                            void *credentials)
+/*!
+ * \internal
+ * \brief Initialize a new TLS session
+ *
+ * \param[in] csock       Connected socket for TLS session
+ * \param[in] conn_type   GNUTLS_SERVER or GNUTLS_CLIENT
+ * \param[in] cred_type   GNUTLS_CRD_ANON or GNUTLS_CRD_PSK
+ * \param[in] credentials TLS session credentials
+ *
+ * \return Pointer to newly created session object, or NULL on error
+ */
+gnutls_session_t *
+pcmk__new_tls_session(int csock, unsigned int conn_type,
+                      gnutls_credentials_type_t cred_type, void *credentials)
 {
-    gnutls_session_t *session = gnutls_malloc(sizeof(gnutls_session_t));
-
-    gnutls_init(session, type);
+    int rc = GNUTLS_E_SUCCESS;
 #  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
-/*      http://www.manpagez.com/info/gnutls/gnutls-2.10.4/gnutls_81.php#Echo-Server-with-anonymous-authentication */
-    gnutls_priority_set_direct(*session, "NORMAL:+ANON-DH", NULL);
-/*	gnutls_priority_set_direct (*session, "NONE:+VERS-TLS-ALL:+CIPHER-ALL:+MAC-ALL:+SIGN-ALL:+COMP-ALL:+ANON-DH", NULL); */
-#  else
-    gnutls_set_default_priority(*session);
-    gnutls_kx_set_priority(*session, anon_tls_kx_order);
+    const char *prio = NULL;
 #  endif
-    gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) GINT_TO_POINTER(csock));
-    switch (type) {
-        case GNUTLS_SERVER:
-            gnutls_credentials_set(*session, GNUTLS_CRD_ANON,
-                                   (gnutls_anon_server_credentials_t) credentials);
-            break;
-        case GNUTLS_CLIENT:
-            gnutls_credentials_set(*session, GNUTLS_CRD_ANON,
-                                   (gnutls_anon_client_credentials_t) credentials);
-            break;
+    gnutls_session_t *session = NULL;
+
+#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
+    if (cred_type == GNUTLS_CRD_ANON) {
+        // http://www.manpagez.com/info/gnutls/gnutls-2.10.4/gnutls_81.php#Echo-Server-with-anonymous-authentication
+        prio = "NORMAL:+ANON-DH";
+    } else {
+        prio = "NORMAL:+DHE-PSK:+PSK";
+    }
+#  endif
+
+    session = gnutls_malloc(sizeof(gnutls_session_t));
+    if (session == NULL) {
+        rc = GNUTLS_E_MEMORY_ERROR;
+        goto error;
     }
 
-    return session;
-}
-
-void *
-create_psk_tls_session(int csock, int type /* GNUTLS_SERVER, GNUTLS_CLIENT */ , void *credentials)
-{
-    gnutls_session_t *session = gnutls_malloc(sizeof(gnutls_session_t));
-
-    gnutls_init(session, type);
-#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
-    gnutls_priority_set_direct(*session, "NORMAL:+DHE-PSK:+PSK", NULL);
-#  else
-    gnutls_set_default_priority(*session);
-    gnutls_kx_set_priority(*session, psk_tls_kx_order);
-#  endif
-    gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) GINT_TO_POINTER(csock));
-    switch (type) {
-        case GNUTLS_SERVER:
-            gnutls_credentials_set(*session, GNUTLS_CRD_PSK,
-                                   (gnutls_psk_server_credentials_t) credentials);
-            break;
-        case GNUTLS_CLIENT:
-            gnutls_credentials_set(*session, GNUTLS_CRD_PSK,
-                                   (gnutls_psk_client_credentials_t) credentials);
-            break;
+    rc = gnutls_init(session, conn_type);
+    if (rc != GNUTLS_E_SUCCESS) {
+        goto error;
     }
 
+#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
+    /* @TODO On the server side, it would be more efficient to cache the
+     * priority with gnutls_priority_init2() and set it with
+     * gnutls_priority_set() for all sessions.
+     */
+    rc = gnutls_priority_set_direct(*session, prio, NULL);
+    if (rc != GNUTLS_E_SUCCESS) {
+        goto error;
+    }
+#  else
+    gnutls_set_default_priority(*session);
+    gnutls_kx_set_priority(*session, (cred_type == GNUTLS_CRD_ANON)? anon_tls_kx_order : psk_tls_kx_order);
+#  endif
+
+    gnutls_transport_set_ptr(*session,
+                             (gnutls_transport_ptr_t) GINT_TO_POINTER(csock));
+
+    rc = gnutls_credentials_set(*session, cred_type, credentials);
+    if (rc != GNUTLS_E_SUCCESS) {
+        goto error;
+    }
     return session;
+
+error:
+    crm_err("Could not initialize %s TLS %s session: %s "
+            CRM_XS " rc=%d priority='%s'",
+            (cred_type == GNUTLS_CRD_ANON)? "anonymous" : "PSK",
+            (conn_type == GNUTLS_SERVER)? "server" : "client",
+            gnutls_strerror(rc), rc, prio);
+    if (session != NULL) {
+        gnutls_free(session);
+    }
+    return NULL;
 }
 
 static int

cib/remote.c
@@ -329,11 +329,11 @@ cib_remote_listen(gpointer data)
         new_client->kind = CRM_CLIENT_TLS;
 
         /* create gnutls session for the server socket */
-        new_client->remote->tls_session =
-            crm_create_anon_tls_session(csock, GNUTLS_SERVER, anon_cred_s);
-
+        new_client->remote->tls_session = pcmk__new_tls_session(csock,
+                                                                GNUTLS_SERVER,
+                                                                GNUTLS_CRD_ANON,
+                                                                anon_cred_s);
         if (new_client->remote->tls_session == NULL) {
-            crm_err("TLS session creation failed");
             close(csock);
             return TRUE;
         }

include/crm/common/remote_internal.h
@@ -26,7 +26,11 @@ int crm_remote_accept(int ssock);
 void crm_sockaddr2str(void *sa, char *s);
 
 #  ifdef HAVE_GNUTLS_GNUTLS_H
+#    include <gnutls/gnutls.h>
 
+gnutls_session_t *pcmk__new_tls_session(int csock, unsigned int conn_type,
+                                        gnutls_credentials_type_t cred_type,
+                                        void *credentials);
 /*!
  * \internal
  * \brief Initiate the client handshake after establishing the tcp socket
@@ -36,28 +40,6 @@ void crm_sockaddr2str(void *sa, char *s);
  *        until the timeout period is reached.
  */
 int crm_initiate_client_tls_handshake(crm_remote_t *remote, int timeout_ms);
-
-/*!
- * \internal
- * \brief Create client or server session for anon DH encryption credentials
- *
- * \param[in] credentials  gnutls_anon_server_credentials_t or gnutls_anon_client_credentials_t
- *
- * \return Pointer to new session object on success, NULL otherwise
- */
-void *crm_create_anon_tls_session(int sock, int type, void *credentials);
-
-/*!
- * \internal
- * \brief Create client or server session for PSK credentials
- *
- * \param[in] sock         Socket the session will use for transport
- * \param[in] type         GNUTLS_SERVER or GNUTLS_CLIENT
- * \param[in] credentials  gnutls_psk_server_credentials_t or gnutls_psk_client_credentials_t
- *
- * \return Pointer to new session object on success, NULL otherwise
- */
-void *create_psk_tls_session(int csock, int type, void *credentials);
 
 #  endif    // HAVE_GNUTLS_GNUTLS_H
 #endif      // PCMK__REMOTE__H

lib/cib/cib_remote.c
@@ -239,7 +239,13 @@ cib_tls_signon(cib_t * cib, crm_remote_t * connection, gboolean event_channel)
         }
 
         /* bind the socket to GnuTls lib */
-        connection->tls_session = crm_create_anon_tls_session(sock, GNUTLS_CLIENT, anon_cred_c);
+        connection->tls_session = pcmk__new_tls_session(sock, GNUTLS_CLIENT,
+                                                        GNUTLS_CRD_ANON,
+                                                        anon_cred_c);
+        if (connection->tls_session == NULL) {
+            cib_tls_close(cib);
+            return -1;
+        }
 
         if (crm_initiate_client_tls_handshake(connection, DEFAULT_CLIENT_HANDSHAKE_TIMEOUT) != 0) {
             crm_err("Session creation for %s:%d failed", private->server, private->port);

lib/lrmd/lrmd_client.c
@@ -1164,7 +1164,14 @@ lrmd_tcp_connect_cb(void *userdata, int sock)
     gnutls_psk_set_client_credentials(native->psk_cred_c, DEFAULT_REMOTE_USERNAME, &psk_key, GNUTLS_PSK_KEY_RAW);
     gnutls_free(psk_key.data);
 
-    native->remote->tls_session = create_psk_tls_session(sock, GNUTLS_CLIENT, native->psk_cred_c);
+    native->remote->tls_session = pcmk__new_tls_session(sock, GNUTLS_CLIENT,
+                                                        GNUTLS_CRD_PSK,
+                                                        native->psk_cred_c);
+    if (native->remote->tls_session == NULL) {
+        lrmd_tls_connection_destroy(lrmd);
+        report_async_connection_result(lrmd, -EPROTO);
+        return;
+    }
 
     if (crm_initiate_client_tls_handshake(native->remote, LRMD_CLIENT_HANDSHAKE_TIMEOUT) != 0) {
         crm_warn("Disconnecting after TLS handshake with remote LRMD %s:%d failed",
@@ -1245,7 +1252,13 @@ lrmd_tls_connect(lrmd_t * lrmd, int *fd)
     gnutls_psk_set_client_credentials(native->psk_cred_c, DEFAULT_REMOTE_USERNAME, &psk_key, GNUTLS_PSK_KEY_RAW);
     gnutls_free(psk_key.data);
 
-    native->remote->tls_session = create_psk_tls_session(sock, GNUTLS_CLIENT, native->psk_cred_c);
+    native->remote->tls_session = pcmk__new_tls_session(sock, GNUTLS_CLIENT,
+                                                        GNUTLS_CRD_PSK,
+                                                        native->psk_cred_c);
+    if (native->remote->tls_session == NULL) {
+        lrmd_tls_connection_destroy(lrmd);
+        return -EPROTO;
+    }
 
     if (crm_initiate_client_tls_handshake(native->remote, LRMD_CLIENT_HANDSHAKE_TIMEOUT) != 0) {
         crm_err("Session creation for %s:%d failed", native->server, native->port);

lrmd/tls_backend.c
@@ -39,6 +39,8 @@
 
 #ifdef HAVE_GNUTLS_GNUTLS_H
 
+#  include <gnutls/gnutls.h>
+
 // Hidden in liblrmd
 extern int lrmd_tls_set_key(gnutls_datum_t *key);
 
@@ -223,9 +225,9 @@ lrmd_remote_listen(gpointer data)
         return TRUE;
     }
 
-    session = create_psk_tls_session(csock, GNUTLS_SERVER, psk_cred_s);
+    session = pcmk__new_tls_session(csock, GNUTLS_SERVER, GNUTLS_CRD_PSK,
+                                    psk_cred_s);
     if (session == NULL) {
-        crm_err("TLS session creation failed");
         close(csock);
         return TRUE;
     }

commit 99a83b172544102ec32585514e5808585f2ce31c

Feature: remote: allow run-time configurable TLS priorities
This also restores compilability with GnuTLS <2.1.7 (not that anyone is still
using that ...), unintentionally broken in 5bded36 (1.1.20).

lib/common/remote.c
@@ -237,17 +237,25 @@ pcmk__new_tls_session(int csock, unsigned int conn_type,
 {
     int rc = GNUTLS_E_SUCCESS;
 #  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
-    const char *prio = NULL;
+    const char *prio_base = NULL;
+    char *prio = NULL;
 #  endif
     gnutls_session_t *session = NULL;
 
 #  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
-    if (cred_type == GNUTLS_CRD_ANON) {
-        // http://www.manpagez.com/info/gnutls/gnutls-2.10.4/gnutls_81.php#Echo-Server-with-anonymous-authentication
-        prio = PCMK_GNUTLS_PRIORITIES ":+ANON-DH";
-    } else {
-        prio = PCMK_GNUTLS_PRIORITIES ":+DHE-PSK:+PSK";
+    /* Determine list of acceptable ciphers, etc. Pacemaker always adds the
+     * values required for its functionality.
+     *
+     * For an example of anonymous authentication, see:
+     * http://www.manpagez.com/info/gnutls/gnutls-2.10.4/gnutls_81.php#Echo-Server-with-anonymous-authentication
+     */
+
+    prio_base = getenv("PCMK_tls_priorities");
+    if (prio_base == NULL) {
+        prio_base = PCMK_GNUTLS_PRIORITIES;
     }
+    prio = crm_strdup_printf("%s:%s", prio_base,
+                             (cred_type == GNUTLS_CRD_ANON)? "+ANON-DH" : "+DHE-PSK:+PSK");
 #  endif
 
     session = gnutls_malloc(sizeof(gnutls_session_t));
@@ -285,6 +293,9 @@ pcmk__new_tls_session(int csock, unsigned int conn_type,
     if (rc != GNUTLS_E_SUCCESS) {
         goto error;
     }
+#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
+    free(prio);
+#  endif
     return session;
 
 error:
@@ -292,7 +303,16 @@ error:
             CRM_XS " rc=%d priority='%s'",
             (cred_type == GNUTLS_CRD_ANON)? "anonymous" : "PSK",
             (conn_type == GNUTLS_SERVER)? "server" : "client",
-            gnutls_strerror(rc), rc, prio);
+            gnutls_strerror(rc), rc,
+#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
+            prio
+#  else
+            "default"
+#  endif
+            );
+#  ifdef HAVE_GNUTLS_PRIORITY_SET_DIRECT
+    free(prio);
+#  endif
     if (session != NULL) {
         gnutls_free(session);
     }

mcp/pacemaker.sysconfig
@@ -101,6 +101,15 @@
 # value must be the same on all nodes. The default is "3121".
 # PCMK_remote_port=3121
 
+# Use these GnuTLS cipher priorities for TLS connections. See:
+#
+#   https://gnutls.org/manual/html_node/Priority-Strings.html
+#
+# Pacemaker will append ":+ANON-DH" for remote CIB access (when enabled) and
+# ":+DHE-PSK:+PSK" for Pacemaker Remote connections, as they are required for
+# the respective functionality.
+# PCMK_tls_priorities="NORMAL"
+
 # Set bounds on the bit length of the prime number generated for Diffie-Hellman
 # parameters needed by TLS connections. The default is not to set any bounds.
 #