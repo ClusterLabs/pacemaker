ClusterLabs/pacemaker

commit 8c4f20583f17004d60dd13e2a073edc35549b9b0

Build: ensure "make clean" cleans up ancillary files generated by GNUmakefile

GNUmakefile
@@ -83,6 +83,8 @@ export:
 	    echo "`date`: Using existing tarball: $(TARFILE)";			\
 	fi
 
+export-clean:
+	-rm -f $(abs_builddir)/$(PACKAGE)-*.tar.gz
 
 ## RPM-related targets
 
@@ -185,6 +187,10 @@ $(RPM_SPEC_DIR)/$(PACKAGE).spec: rpm/pacemaker.spec.in
 
 .PHONY: $(PACKAGE).spec
 $(PACKAGE).spec: $(RPM_SPEC_DIR)/$(PACKAGE).spec
+
+.PHONY: spec-clean
+spec-clean:
+	-rm -f $(RPM_SPEC_DIR)/$(PACKAGE).spec
 
 .PHONY: srpm
 srpm:	export srpm-clean $(RPM_SPEC_DIR)/$(PACKAGE).spec
@@ -403,3 +409,9 @@ gnulib-update:
 	maint/gnulib/gnulib-tool \
 	  --source-base=lib/gnu --lgpl=2 --no-vc-files --no-conditional-dependencies \
 	  $(GNU_MODS_AVOID:%=--avoid %) --import $(GNU_MODS)
+
+# The toplevel "clean" targets are generated from Makefile.am, not this file.
+# We can't use autotools' CLEANFILES, clean-local, etc. here. Instead, we
+# define this target, which Makefile.am can call from clean-local.
+ancillary-clean: export-clean spec-clean srpm-clean mock-clean coverity-clean
+	-rm -f $(TARFILE)

Makefile.am
@@ -90,7 +90,10 @@ install-exec-local:
 # Use chown because the user/group may not exist
 
 clean-generic:
-	rm -f $(TARFILE) *.tar.bz2 *.sed
+	-rm -f *.tar.bz2 *.sed
+
+clean-local:
+	$(MAKE) $(AM_MAKEFLAGS) ancillary-clean
 
 dist-clean-local:
 	rm -f autoconf automake autoheader

commit 500b75e64253d8784fd210c9fc7d07ebbee11524

Build: makefiles: ensure clean works in a VPATH build
8c4f2058 broke it

GNUmakefile
@@ -10,6 +10,11 @@
 default: build
 .PHONY: default
 
+# The toplevel "clean" targets are generated from Makefile.am, not this file.
+# We can't use autotools' CLEANFILES, clean-local, etc. here. Instead, we
+# define this target, which Makefile.am can use as a dependency of clean-local.
+EXTRA_CLEAN_TARGETS	= ancillary-clean
+
 -include Makefile
 
 # The main purpose of this GNUmakefile is that its targets can be invoked
@@ -410,8 +415,5 @@ gnulib-update:
 	  --source-base=lib/gnu --lgpl=2 --no-vc-files --no-conditional-dependencies \
 	  $(GNU_MODS_AVOID:%=--avoid %) --import $(GNU_MODS)
 
-# The toplevel "clean" targets are generated from Makefile.am, not this file.
-# We can't use autotools' CLEANFILES, clean-local, etc. here. Instead, we
-# define this target, which Makefile.am can call from clean-local.
 ancillary-clean: export-clean spec-clean srpm-clean mock-clean coverity-clean
 	-rm -f $(TARFILE)

Makefile.am
@@ -106,8 +106,13 @@ install-exec-local:
 clean-generic:
 	-rm -f *.tar.bz2 *.sed
 
-clean-local:
-	$(MAKE) $(AM_MAKEFLAGS) ancillary-clean
+# In a normal build, this file is included by GNUmakefile, which serves as the
+# "real" makefile. But in a VPATH build, GNUmakefile won't exist in the build
+# tree, and this file will be the "real" makefile. EXTRA_CLEAN_TARGETS handles
+# both cases: GNUmakefile defines it before including this file, so the
+# clean-local target can clean up files created by GNUmakefile targets.
+# If this file is used alone, the variable will be undefined.
+clean-local: $(EXTRA_CLEAN_TARGETS)
 
 distclean-local:
 	-rm -rf libltdl autom4te.cache