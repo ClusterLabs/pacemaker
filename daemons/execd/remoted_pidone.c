2552e54a3f0e7b2fff00475c883666daabf4fc3e
Ken Gaillot <kgaillot@redhat.com>
2019-09-24 16:01:31 -0500

    Feature: Pacemaker Remote: allow environment variable file for PID 1
    Environment variables can be set in bundle containers via the extra options
    configured to pass to the container launcher. However, this does not allow
    environment variables whose value might differ by host (such as
    PCMK_remote_address).
    
    Now, when pacemaker-remoted runs as PID 1, it will look for a file
    /etc/pacemaker/pcmk-init.env, and if it exists, parse name/value pairs from it
    using a shell-like syntax, and set those in the environment. Users can mount
    a file on the container host as /etc/pacemaker/pcmk-init.env inside the
    container, via the bundle's storage-mapping, and the contents of the file can
    vary by host.

--- daemons/execd/remoted_pidone.c
+++ daemons/execd/remoted_pidone.c
@@ -10,6 +10,7 @@
 #include <crm_internal.h>
 
 #include <stdio.h>
+#include <ctype.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -57,6 +58,138 @@ static struct {
     { SIGINT,  sigdone },
 };
 
+/*!
+ * \internal
+ * \brief Check a line of text for a valid environment variable name
+ *
+ * \param[in]  line  Text to check
+ * \param[out] first  First character of valid name if found, NULL otherwise
+ * \param[out] last   Last character of valid name if found, NULL otherwise
+ *
+ * \return TRUE if valid name found, FALSE otherwise
+ * \note It's reasonable to impose limitations on environment variable names
+ *       beyond what C or setenv() does: We only allow names that contain only
+ *       [a-zA-Z0-9_] characters and do not start with a digit.
+ */
+static bool
+find_env_var_name(char *line, char **first, char **last)
+{
+    // Skip leading whitespace
+    *first = line;
+    while (isspace(**first)) {
+        ++*first;
+    }
+
+    if (isalpha(**first) || (**first == '_')) { // Valid first character
+        *last = *first;
+        while (isalnum(*(*last + 1)) || (*(*last + 1) == '_')) {
+            ++*last;
+        }
+        return TRUE;
+    }
+
+    *first = *last = NULL;
+    return FALSE;
+}
+
+static void
+load_env_vars(const char *filename)
+{
+    /* We haven't forked or initialized logging yet, so don't leave any file
+     * descriptors open, and don't log -- silently ignore errors.
+     */
+    FILE *fp = fopen(filename, "r");
+
+    if (fp != NULL) {
+        char line[LINE_MAX] = { '\0', };
+
+        while (fgets(line, LINE_MAX, fp) != NULL) {
+            char *name = NULL;
+            char *end = NULL;
+            char *value = NULL;
+            char *quote = NULL;
+
+            // Look for valid name immediately followed by equals sign
+            if (find_env_var_name(line, &name, &end) && (*++end == '=')) {
+
+                // Null-terminate name, and advance beyond equals sign
+                *end++ = '\0';
+
+                // Check whether value is quoted
+                if ((*end == '\'') || (*end == '"')) {
+                    quote = end++;
+                }
+                value = end;
+
+                if (quote) {
+                    /* Value is remaining characters up to next non-backslashed
+                     * matching quote character.
+                     */
+                    while (((*end != *quote) || (*(end - 1) == '\\'))
+                           && (*end != '\0')) {
+                        end++;
+                    }
+                    if (*end == *quote) {
+                        // Null-terminate value, and advance beyond close quote
+                        *end++ = '\0';
+                    } else {
+                        // Matching closing quote wasn't found
+                        value = NULL;
+                    }
+
+                } else {
+                    /* Value is remaining characters up to next non-backslashed
+                     * whitespace.
+                     */
+                    while ((!isspace(*end) || (*(end - 1) == '\\'))
+                           && (*end != '\0')) {
+                        ++end;
+                    }
+
+                    if (end == (line + LINE_MAX - 1)) {
+                        // Line was too long
+                        value = NULL;
+                    }
+                    // Do NOT null-terminate value (yet)
+                }
+
+                /* We have a valid name and value, and end is now the character
+                 * after the closing quote or the first whitespace after the
+                 * unquoted value. Make sure the rest of the line is just
+                 * whitespace or a comment.
+                 */
+                if (value) {
+                    char *value_end = end;
+
+                    while (isspace(*end) && (*end != '\n')) {
+                        ++end;
+                    }
+                    if ((*end == '\n') || (*end == '#')) {
+                        if (quote == NULL) {
+                            // Now we can null-terminate an unquoted value
+                            *value_end = '\0';
+                        }
+
+                        // Don't overwrite (bundle options take precedence)
+                        setenv(name, value, 0);
+
+                    } else {
+                        value = NULL;
+                    }
+                }
+            }
+
+            if ((value == NULL) && (strchr(line, '\n') == NULL)) {
+                // Eat remainder of line beyond LINE_MAX
+                if (fscanf(fp, "%*[^\n]\n") == EOF) {
+                    value = NULL; // Don't care, make compiler happy
+                }
+            }
+        }
+        fclose(fp);
+    }
+}
+
 void
 remoted_spawn_pidone(int argc, char **argv, char **envp)
 {
@@ -65,6 +198,14 @@ remoted_spawn_pidone(int argc, char **argv, char **envp)
     if (getpid() != 1) {
         return;
     }
+
+    /* When a container is launched, it may be given specific environment
+     * variables, which for Pacemaker bundles are given in the bundle
+     * configuration. However, that does not allow for host-specific values.
+     * To allow for that, look for a special file containing a shell-like syntax
+     * of name/value pairs, and export those into the environment.
+     */
+    load_env_vars("/etc/pacemaker/pcmk-init.env");
 
     /* Containers can be expected to have /var/log, but they may not have
      * /var/log/pacemaker, so use a different default if no value has been
@@ -93,7 +234,7 @@ remoted_spawn_pidone(int argc, char **argv, char **envp)
     /* Parent becomes the reaper of zombie processes */
     /* Safe to initialize logging now if needed */
 
-#ifdef HAVE___PROGNAME
+#  ifdef HAVE___PROGNAME
     /* Differentiate ourselves in the 'ps' output */
     {
         char *p;
@@ -126,7 +267,7 @@ remoted_spawn_pidone(int argc, char **argv, char **envp)
         }
         argv[1] = NULL;
     }
-#endif /* HAVE___PROGNAME */
+#  endif // HAVE___PROGNAME
 
     while (1) {
         int sig;
