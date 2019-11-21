ClusterLabs/pacemaker

commit 5a094ef8bf0260cb4636ded9a063d4930e7b9090

Low: libcrmcommon,tools: handle NULL values better in time handling
Checking for NULL is unnecessary in most cases, but does have one significant
effect: previously, an invalid ISO 8601 duration used as an interval in the CIB
would cause a segmentation fault and core dump; now, an error will be logged
and 0 will be used.

lib/common/iso8601.c
@@ -63,10 +63,16 @@ static crm_time_t *parse_date(const char *date_str);
 gboolean check_for_ordinal(const char *str);
 
 static crm_time_t *
-crm_get_utc_time(crm_time_t * dt)
+crm_get_utc_time(crm_time_t *dt)
 {
-    crm_time_t *utc = crm_time_new_undefined();
+    crm_time_t *utc = NULL;
 
+    if (dt == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    utc = crm_time_new_undefined();
     utc->years = dt->years;
     utc->days = dt->days;
     utc->seconds = dt->seconds;
@@ -308,7 +314,14 @@ crm_time_get_seconds(crm_time_t * dt)
     crm_time_t *utc = NULL;
     long long in_seconds = 0;
 
+    if (dt == NULL) {
+        return 0;
+    }
+
     utc = crm_get_utc_time(dt);
+    if (utc == NULL) {
+        return 0;
+    }
 
     for (lpc = 1; lpc < utc->years; lpc++) {
         int dmax = year_days(lpc);
@@ -340,7 +353,7 @@ crm_time_get_seconds(crm_time_t * dt)
 long long
 crm_time_get_seconds_since_epoch(crm_time_t * dt)
 {
-    return crm_time_get_seconds(dt) - EPOCH_SECONDS;
+    return (dt == NULL)? 0 : (crm_time_get_seconds(dt) - EPOCH_SECONDS);
 }
 
 int
@@ -1250,12 +1263,19 @@ crm_time_add(crm_time_t * dt, crm_time_t * value)
     crm_time_t *utc = NULL;
     crm_time_t *answer = NULL;
 
-    CRM_CHECK(dt != NULL && value != NULL, return NULL);
+    if ((dt == NULL) || (value == NULL)) {
+        errno = EINVAL;
+        return NULL;
+    }
 
     answer = crm_time_new_undefined();
     crm_time_set(answer, dt);
 
     utc = crm_get_utc_time(value);
+    if (utc == NULL) {
+        crm_time_free(answer);
+        return NULL;
+    }
 
     answer->years += utc->years;
     crm_time_add_months(answer, utc->months);
@@ -1272,10 +1292,21 @@ crm_time_calculate_duration(crm_time_t * dt, crm_time_t * value)
     crm_time_t *utc = NULL;
     crm_time_t *answer = NULL;
 
-    CRM_CHECK(dt != NULL && value != NULL, return NULL);
+    if ((dt == NULL) || (value == NULL)) {
+        errno = EINVAL;
+        return NULL;
+    }
 
     utc = crm_get_utc_time(value);
+    if (utc == NULL) {
+        return NULL;
+    }
+
     answer = crm_get_utc_time(dt);
+    if (answer == NULL) {
+        crm_time_free(utc);
+        return NULL;
+    }
     answer->duration = TRUE;
 
     answer->years -= utc->years;
@@ -1295,12 +1326,18 @@ crm_time_subtract(crm_time_t * dt, crm_time_t * value)
     crm_time_t *utc = NULL;
     crm_time_t *answer = NULL;
 
-    CRM_CHECK(dt != NULL && value != NULL, return NULL);
+    if ((dt == NULL) || (value == NULL)) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    utc = crm_get_utc_time(value);
+    if (utc == NULL) {
+        return NULL;
+    }
 
     answer = crm_time_new_undefined();
     crm_time_set(answer, dt);
-    utc = crm_get_utc_time(value);
-
     answer->years -= utc->years;
     if(utc->months != 0) {
         crm_time_add_months(answer, -utc->months);
@@ -1340,22 +1377,19 @@ crm_time_check(crm_time_t * dt)
     }
 
 int
-crm_time_compare(crm_time_t * a, crm_time_t * b)
+crm_time_compare(crm_time_t *a, crm_time_t *b)
 {
     int rc = 0;
-    crm_time_t *t1 = NULL;
-    crm_time_t *t2 = NULL;
+    crm_time_t *t1 = crm_get_utc_time(a);
+    crm_time_t *t2 = crm_get_utc_time(b);
 
-    if (a == NULL && b == NULL) {
+    if ((t1 == NULL) && (t2 == NULL)) {
         return 0;
-    } else if (a == NULL) {
+    } else if (t1 == NULL) {
         return -1;
-    } else if (b == NULL) {
+    } else if (t2 == NULL) {
         return 1;
     }
-
-    t1 = crm_get_utc_time(a);
-    t2 = crm_get_utc_time(b);
 
     do_cmp_field(t1, t2, years);
     do_cmp_field(t1, t2, days);

tools/iso8601.c
@@ -189,6 +189,11 @@ main(int argc, char **argv)
     if (date_time && duration) {
         crm_time_t *later = crm_time_add(date_time, duration);
 
+        if (later == NULL) {
+            fprintf(stderr, "Unable to calculate ending time of %s plus %s",
+                    date_time_s, duration_s);
+            crm_exit(CRM_EX_SOFTWARE);
+        }
         crm_time_log(LOG_TRACE, "Duration ends at", later,
                      crm_time_ordinal | crm_time_log_date | crm_time_log_timeofday);
         crm_time_log(-1, "Duration ends at", later,

lib/common/utils.c
@@ -556,8 +556,13 @@ crm_parse_interval_spec(const char *input)
     } else {
         crm_time_t *period_s = crm_time_parse_duration(input);
 
-        msec = 1000 * crm_time_get_seconds(period_s);
-        crm_time_free(period_s);
+        if (period_s) {
+            msec = 1000 * crm_time_get_seconds(period_s);
+            crm_time_free(period_s);
+        } else {
+            // Reason why not valid has already been logged
+            crm_warn("Using 0 instead of '%s'", input);
+        }
     }
 
     return (msec <= 0)? 0 : ((msec >= G_MAXUINT)? G_MAXUINT : (guint) msec);

tools/crm_resource_ban.c
@@ -36,6 +36,14 @@ parse_cli_lifetime(const char *input)
 
     now = crm_time_new(NULL);
     later = crm_time_add(now, duration);
+    if (later == NULL) {
+        CMD_ERR("Unable to add %s to current time", move_lifetime);
+        CMD_ERR("Please report to " PACKAGE_BUGREPORT " as possible bug");
+        crm_time_free(now);
+        crm_time_free(duration);
+        return NULL;
+    }
+
     crm_time_log(LOG_INFO, "now     ", now,
                  crm_time_log_date | crm_time_log_timeofday | crm_time_log_with_timezone);
     crm_time_log(LOG_INFO, "later   ", later,

commit ff1bae1bf41b0fb132860ed855a0a3f620262a0e

Low: libcrmcommon: memory leak when comparing time with invalid specification
introduced in 5a094ef8 (2.0.3-rc1)

lib/common/iso8601.c
@@ -1386,16 +1386,16 @@ crm_time_compare(crm_time_t *a, crm_time_t *b)
     crm_time_t *t2 = crm_get_utc_time(b);
 
     if ((t1 == NULL) && (t2 == NULL)) {
-        return 0;
+        rc = 0;
     } else if (t1 == NULL) {
-        return -1;
+        rc = -1;
     } else if (t2 == NULL) {
-        return 1;
+        rc = 1;
+    } else {
+        do_cmp_field(t1, t2, years);
+        do_cmp_field(t1, t2, days);
+        do_cmp_field(t1, t2, seconds);
     }
-
-    do_cmp_field(t1, t2, years);
-    do_cmp_field(t1, t2, days);
-    do_cmp_field(t1, t2, seconds);
 
     crm_time_free(t1);
     crm_time_free(t2);