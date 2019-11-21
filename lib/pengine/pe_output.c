b2a913539337ca6f8bb4ab43ca3eb05e253313ab
Chris Lumens <clumens@redhat.com>
2019-11-21 11:39:10 -0500

    Refactor: pengine, tools: Move cluster summary messages to pengine.
    Also, add entries for the log formatters.

--- lib/pengine/pe_output.c
+++ lib/pengine/pe_output.c
@@ -11,6 +11,25 @@
 #include <crm/common/iso8601_internal.h>
 #include <crm/pengine/internal.h>
 
+#define s_if_plural(i) (((i) == 1)? "" : "s")
+
+static char *
+last_changed_string(const char *last_written, const char *user,
+                    const char *client, const char *origin) {
+    if (last_written != NULL || user != NULL || client != NULL || origin != NULL) {
+        return crm_strdup_printf("%s%s%s%s%s%s%s",
+                                 last_written ? last_written : "",
+                                 user ? " by " : "",
+                                 user ? user : "",
+                                 client ? " via " : "",
+                                 client ? client : "",
+                                 origin ? " on " : "",
+                                 origin ? origin : "");
+    } else {
+        return strdup("");
+    }
+}
+
 char *
 pe__node_display_name(node_t *node, bool print_detail)
 {
@@ -145,6 +164,373 @@ pe__ban_xml(pcmk__output_t *out, va_list args) {
                (pcmkXmlStr) (location->role_filter == RSC_ROLE_MASTER ? "true" : "false"));
 
     free(weight_s);
+    return 0;
+}
+
+int
+pe__cluster_counts_html(pcmk__output_t *out, va_list args) {
+    xmlNodePtr nodes_node = pcmk__output_create_xml_node(out, "li");
+    xmlNodePtr resources_node = pcmk__output_create_xml_node(out, "li");
+
+    unsigned int nnodes = va_arg(args, unsigned int);
+    unsigned int nresources = va_arg(args, unsigned int);
+    unsigned int ndisabled = va_arg(args, unsigned int);
+    unsigned int nblocked = va_arg(args, unsigned int);
+
+    char *nnodes_str = crm_strdup_printf("%d node%s configured", nnodes, s_if_plural(nnodes));
+
+    pcmk_create_html_node(nodes_node, "span", NULL, NULL, nnodes_str);
+    free(nnodes_str);
+
+    if (ndisabled && nblocked) {
+        char *s = crm_strdup_printf("%d resource instance%s configured (%d ",
+                                    nresources, s_if_plural(nresources),
+                                    ndisabled);
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, s);
+        free(s);
+
+        pcmk_create_html_node(resources_node, "span", NULL, "bold", "DISABLED");
+
+        s = crm_strdup_printf(", %d ", nblocked);
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, s);
+        free(s);
+
+        pcmk_create_html_node(resources_node, "span", NULL, "bold", "BLOCKED");
+        pcmk_create_html_node(resources_node, "span", NULL, NULL,
+                              " from further action due to failure)");
+    } else if (ndisabled && !nblocked) {
+        char *s = crm_strdup_printf("%d resource instance%s configured (%d ",
+                                    nresources, s_if_plural(nresources),
+                                    ndisabled);
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, s);
+        free(s);
+
+        pcmk_create_html_node(resources_node, "span", NULL, "bold", "DISABLED");
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, ")");
+    } else if (!ndisabled && nblocked) {
+        char *s = crm_strdup_printf("%d resource instance%s configured (%d ",
+                                    nresources, s_if_plural(nresources),
+                                    nblocked);
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, s);
+        free(s);
+
+        pcmk_create_html_node(resources_node, "span", NULL, "bold", "BLOCKED");
+        pcmk_create_html_node(resources_node, "span", NULL, NULL,
+                              " from further action due to failure)");
+    } else {
+        char *s = crm_strdup_printf("%d resource instance%s configured",
+                                    nresources, s_if_plural(nresources));
+        pcmk_create_html_node(resources_node, "span", NULL, NULL, s);
+        free(s);
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_counts_text(pcmk__output_t *out, va_list args) {
+    unsigned int nnodes = va_arg(args, unsigned int);
+    unsigned int nresources = va_arg(args, unsigned int);
+    unsigned int ndisabled = va_arg(args, unsigned int);
+    unsigned int nblocked = va_arg(args, unsigned int);
+
+    out->list_item(out, NULL, "%d node%s configured", nnodes, s_if_plural(nnodes));
+
+    if (ndisabled && nblocked) {
+        out->list_item(out, NULL, "%d resource instance%s configured "
+                                  "(%d DISABLED, %d BLOCKED from "
+                                  "further action due to failure)",
+                       nresources, s_if_plural(nresources), ndisabled,
+                       nblocked);
+    } else if (ndisabled && !nblocked) {
+        out->list_item(out, NULL, "%d resource instance%s configured "
+                                  "(%d DISABLED)",
+                       nresources, s_if_plural(nresources), ndisabled);
+    } else if (!ndisabled && nblocked) {
+        out->list_item(out, NULL, "%d resource instance%s configured "
+                                  "(%d BLOCKED from further action "
+                                  "due to failure)",
+                       nresources, s_if_plural(nresources), nblocked);
+    } else {
+        out->list_item(out, NULL, "%d resource instance%s configured",
+                       nresources, s_if_plural(nresources));
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_counts_xml(pcmk__output_t *out, va_list args) {
+    xmlNodePtr nodes_node = pcmk__output_create_xml_node(out, "nodes_configured");
+    xmlNodePtr resources_node = pcmk__output_create_xml_node(out, "resources_configured");
+
+    unsigned int nnodes = va_arg(args, unsigned int);
+    unsigned int nresources = va_arg(args, unsigned int);
+    unsigned int ndisabled = va_arg(args, unsigned int);
+    unsigned int nblocked = va_arg(args, unsigned int);
+
+    char *s = crm_itoa(nnodes);
+    xmlSetProp(nodes_node, (pcmkXmlStr) "number", (pcmkXmlStr) s);
+    free(s);
+
+    s = crm_itoa(nresources);
+    xmlSetProp(resources_node, (pcmkXmlStr) "number", (pcmkXmlStr) s);
+    free(s);
+
+    s = crm_itoa(ndisabled);
+    xmlSetProp(resources_node, (pcmkXmlStr) "disabled", (pcmkXmlStr) s);
+    free(s);
+
+    s = crm_itoa(nblocked);
+    xmlSetProp(resources_node, (pcmkXmlStr) "blocked", (pcmkXmlStr) s);
+    free(s);
+
+    return 0;
+}
+
+int
+pe__cluster_dc_html(pcmk__output_t *out, va_list args) {
+    xmlNodePtr node = pcmk__output_create_xml_node(out, "li");
+
+    node_t *dc = va_arg(args, node_t *);
+    const char *quorum = va_arg(args, const char *);
+    const char *dc_version_s = va_arg(args, const char *);
+    const char *dc_name = va_arg(args, const char *);
+
+    pcmk_create_html_node(node, "span", NULL, "bold", "Current DC: ");
+
+    if (dc) {
+        if (crm_is_true(quorum)) {
+            char *buf = crm_strdup_printf("%s (version %s) - partition with quorum",
+                                          dc_name, dc_version_s ? dc_version_s : "unknown");
+            pcmk_create_html_node(node, "span", NULL, NULL, buf);
+            free(buf);
+        } else {
+            char *buf = crm_strdup_printf("%s (version %s) - partition",
+                                          dc_name, dc_version_s ? dc_version_s : "unknown");
+            pcmk_create_html_node(node, "span", NULL, NULL, buf);
+            free(buf);
+
+            pcmk_create_html_node(node, "span", NULL, "warning", "WITHOUT");
+            pcmk_create_html_node(node, "span", NULL, NULL, "quorum");
+        }
+    } else {
+        pcmk_create_html_node(node ,"span", NULL, "warning", "NONE");
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_dc_text(pcmk__output_t *out, va_list args) {
+    node_t *dc = va_arg(args, node_t *);
+    const char *quorum = va_arg(args, const char *);
+    const char *dc_version_s = va_arg(args, const char *);
+    const char *dc_name = va_arg(args, const char *);
+
+    if (dc) {
+        out->list_item(out, "Current DC", "%s (version %s) - partition %s quorum",
+                       dc_name, dc_version_s ? dc_version_s : "unknown",
+                       crm_is_true(quorum) ? "with" : "WITHOUT");
+    } else {
+        out->list_item(out, "Current DC", "NONE");
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_dc_xml(pcmk__output_t *out, va_list args) {
+    xmlNodePtr node = pcmk__output_create_xml_node(out, "current_dc");
+
+    node_t *dc = va_arg(args, node_t *);
+    const char *quorum = va_arg(args, const char *);
+    const char *dc_version_s = va_arg(args, const char *);
+
+    if (dc) {
+        xmlSetProp(node, (pcmkXmlStr) "present", (pcmkXmlStr) "true");
+        xmlSetProp(node, (pcmkXmlStr) "version", (pcmkXmlStr) (dc_version_s ? dc_version_s : ""));
+        xmlSetProp(node, (pcmkXmlStr) "name", (pcmkXmlStr) dc->details->uname);
+        xmlSetProp(node, (pcmkXmlStr) "id", (pcmkXmlStr) dc->details->id);
+        xmlSetProp(node, (pcmkXmlStr) "with_quorum", (pcmkXmlStr) (crm_is_true(quorum) ? "true" : "false"));
+    } else {
+        xmlSetProp(node, (pcmkXmlStr) "present", (pcmkXmlStr) "false");
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_options_html(pcmk__output_t *out, va_list args) {
+    pe_working_set_t *data_set = va_arg(args, pe_working_set_t *);
+
+    out->list_item(out, NULL, "STONITH of failed nodes %s",
+                   is_set(data_set->flags, pe_flag_stonith_enabled) ? "enabled" : "disabled");
+
+    out->list_item(out, NULL, "Cluster is %s",
+                   is_set(data_set->flags, pe_flag_symmetric_cluster) ? "symmetric" : "asymmetric");
+
+    switch (data_set->no_quorum_policy) {
+        case no_quorum_freeze:
+            out->list_item(out, NULL, "No Quorum policy: Freeze resources");
+            break;
+
+        case no_quorum_stop:
+            out->list_item(out, NULL, "No Quorum policy: Stop ALL resources");
+            break;
+
+        case no_quorum_ignore:
+            out->list_item(out, NULL, "No Quorum policy: Ignore");
+            break;
+
+        case no_quorum_suicide:
+            out->list_item(out, NULL, "No Quorum policy: Suicide");
+            break;
+    }
+
+    if (is_set(data_set->flags, pe_flag_maintenance_mode)) {
+        xmlNodePtr node = pcmk__output_create_xml_node(out, "li");
+
+        pcmk_create_html_node(node, "span", NULL, "bold", "DISABLED");
+        pcmk_create_html_node(node, "span", NULL, NULL,
+                              " (the cluster will not attempt to start, stop, or recover services)");
+    } else {
+        out->list_item(out, NULL, "Resource management enabled");
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_options_text(pcmk__output_t *out, va_list args) {
+    pe_working_set_t *data_set = va_arg(args, pe_working_set_t *);
+
+    if (is_set(data_set->flags, pe_flag_maintenance_mode)) {
+        fprintf(out->dest, "\n              *** Resource management is DISABLED ***");
+        fprintf(out->dest, "\n  The cluster will not attempt to start, stop or recover services");
+        fprintf(out->dest, "\n");
+    }
+
+    return 0;
+}
+
+int
+pe__cluster_options_xml(pcmk__output_t *out, va_list args) {
+    xmlNodePtr node = pcmk__output_create_xml_node(out, "cluster_options");
+    pe_working_set_t *data_set = va_arg(args, pe_working_set_t *);
+
+    xmlSetProp(node, (pcmkXmlStr) "stonith-enabled",
+               (pcmkXmlStr) (is_set(data_set->flags, pe_flag_stonith_enabled) ? "true" : "false"));
+    xmlSetProp(node, (pcmkXmlStr) "symmetric-cluster",
+               (pcmkXmlStr) (is_set(data_set->flags, pe_flag_symmetric_cluster) ? "true" : "false"));
+
+    switch (data_set->no_quorum_policy) {
+        case no_quorum_freeze:
+            xmlSetProp(node, (pcmkXmlStr) "no-quorum-policy", (pcmkXmlStr) "freeze");
+            break;
+
+        case no_quorum_stop:
+            xmlSetProp(node, (pcmkXmlStr) "no-quorum-policy", (pcmkXmlStr) "stop");
+            break;
+
+        case no_quorum_ignore:
+            xmlSetProp(node, (pcmkXmlStr) "no-quorum-policy", (pcmkXmlStr) "ignore");
+            break;
+
+        case no_quorum_suicide:
+            xmlSetProp(node, (pcmkXmlStr) "no-quorum-policy", (pcmkXmlStr) "suicide");
+            break;
+    }
+
+    xmlSetProp(node, (pcmkXmlStr) "maintenance-mode",
+               (pcmkXmlStr) (is_set(data_set->flags, pe_flag_maintenance_mode) ? "true" : "false"));
+
+    return 0;
+}
+
+int
+pe__cluster_stack_html(pcmk__output_t *out, va_list args) {
+    xmlNodePtr node = pcmk__output_create_xml_node(out, "li");
+    const char *stack_s = va_arg(args, const char *);
+
+    pcmk_create_html_node(node, "span", NULL, "bold", "Stack: ");
+    pcmk_create_html_node(node, "span", NULL, NULL, stack_s);
+
+    return 0;
+}
+
+int
+pe__cluster_stack_text(pcmk__output_t *out, va_list args) {
+    const char *stack_s = va_arg(args, const char *);
+    out->list_item(out, "Stack", "%s", stack_s);
+    return 0;
+}
+
+int
+pe__cluster_stack_xml(pcmk__output_t *out, va_list args) {
+    xmlNodePtr node = pcmk__output_create_xml_node(out, "stack");
+    const char *stack_s = va_arg(args, const char *);
+
+    xmlSetProp(node, (pcmkXmlStr) "type", (pcmkXmlStr) stack_s);
+
+    return 0;
+}
+
+int
+pe__cluster_times_html(pcmk__output_t *out, va_list args) {
+    xmlNodePtr updated_node = pcmk__output_create_xml_node(out, "li");
+    xmlNodePtr changed_node = pcmk__output_create_xml_node(out, "li");
+
+    const char *last_written = va_arg(args, const char *);
+    const char *user = va_arg(args, const char *);
+    const char *client = va_arg(args, const char *);
+    const char *origin = va_arg(args, const char *);
+
+    char *buf = last_changed_string(last_written, user, client, origin);
+
+    pcmk_create_html_node(updated_node, "span", NULL, "bold", "Last updated: ");
+    pcmk_create_html_node(updated_node, "span", NULL, NULL, crm_now_string(NULL));
+
+    pcmk_create_html_node(changed_node, "span", NULL, "bold", "Last change: ");
+    pcmk_create_html_node(changed_node, "span", NULL, NULL, buf);
+
+    free(buf);
+    return 0;
+}
+
+int
+pe__cluster_times_xml(pcmk__output_t *out, va_list args) {
+    xmlNodePtr updated_node = pcmk__output_create_xml_node(out, "last_update");
+    xmlNodePtr changed_node = pcmk__output_create_xml_node(out, "last_change");
+
+    const char *last_written = va_arg(args, const char *);
+    const char *user = va_arg(args, const char *);
+    const char *client = va_arg(args, const char *);
+    const char *origin = va_arg(args, const char *);
+
+    xmlSetProp(updated_node, (pcmkXmlStr) "time", (pcmkXmlStr) crm_now_string(NULL));
+
+    xmlSetProp(changed_node, (pcmkXmlStr) "time", (pcmkXmlStr) (last_written ? last_written : ""));
+    xmlSetProp(changed_node, (pcmkXmlStr) "user", (pcmkXmlStr) (user ? user : ""));
+    xmlSetProp(changed_node, (pcmkXmlStr) "client", (pcmkXmlStr) (client ? client : ""));
+    xmlSetProp(changed_node, (pcmkXmlStr) "origin", (pcmkXmlStr) (origin ? origin : ""));
+
+    return 0;
+}
+
+int
+pe__cluster_times_text(pcmk__output_t *out, va_list args) {
+    const char *last_written = va_arg(args, const char *);
+    const char *user = va_arg(args, const char *);
+    const char *client = va_arg(args, const char *);
+    const char *origin = va_arg(args, const char *);
+
+    char *buf = last_changed_string(last_written, user, client, origin);
+
+    out->list_item(out, "Last updated", "%s", crm_now_string(NULL));
+    out->list_item(out, "Last change", " %s", buf);
+
+    free(buf);
     return 0;
 }
 
@@ -294,6 +680,26 @@ static pcmk__message_entry_t fmt_functions[] = {
     { "clone", "html",  pe__clone_html },
     { "clone", "text",  pe__clone_text },
     { "clone", "log",  pe__clone_text },
+    { "cluster-counts", "html", pe__cluster_counts_html },
+    { "cluster-counts", "log", pe__cluster_counts_text },
+    { "cluster-counts", "text", pe__cluster_counts_text },
+    { "cluster-counts", "xml", pe__cluster_counts_xml },
+    { "cluster-dc", "html", pe__cluster_dc_html },
+    { "cluster-dc", "log", pe__cluster_dc_text },
+    { "cluster-dc", "text", pe__cluster_dc_text },
+    { "cluster-dc", "xml", pe__cluster_dc_xml },
+    { "cluster-options", "html", pe__cluster_options_html },
+    { "cluster-options", "log", pe__cluster_options_text },
+    { "cluster-options", "text", pe__cluster_options_text },
+    { "cluster-options", "xml", pe__cluster_options_xml },
+    { "cluster-stack", "html", pe__cluster_stack_html },
+    { "cluster-stack", "log", pe__cluster_stack_text },
+    { "cluster-stack", "text", pe__cluster_stack_text },
+    { "cluster-stack", "xml", pe__cluster_stack_xml },
+    { "cluster-times", "html", pe__cluster_times_html },
+    { "cluster-times", "log", pe__cluster_times_text },
+    { "cluster-times", "text", pe__cluster_times_text },
+    { "cluster-times", "xml", pe__cluster_times_xml },
     { "group", "xml",  pe__group_xml },
     { "group", "html",  pe__group_html },
     { "group", "text",  pe__group_text },
