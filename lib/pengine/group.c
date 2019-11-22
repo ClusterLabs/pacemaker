7cfcf5977c15cab2762e521e2bebddeddf60ef8c
Chris Lumens <clumens@redhat.com>
2019-11-22 16:14:18 -0500

    Fix: pengine: Move definition of status_print to pengine.

--- lib/pengine/group.c
+++ lib/pengine/group.c
@@ -9,11 +9,11 @@
 
 #include <crm_internal.h>
 
-#include <crm/common/curses_internal.h>
 #include <crm/pengine/rules.h>
 #include <crm/pengine/status.h>
 #include <crm/pengine/internal.h>
 #include <crm/msg_xml.h>
+#include <pe_status_private.h>
 
 #define VARIANT_GROUP 1
 #include "./variant.h"
