d3b70deb3b5baf27fb3b110f6858f772cd887755
Ken Gaillot <kgaillot@redhat.com>
2020-01-23 12:19:09 -0600

    Log: libcrmcommon: improve messages for IPC hello creation

--- lib/common/ipc.c
+++ lib/common/ipc.c
@@ -1680,23 +1680,35 @@ create_hello_message(const char *uuid,
     xmlNode *hello_node = NULL;
     xmlNode *hello = NULL;
 
-    if (uuid == NULL || strlen(uuid) == 0
-        || client_name == NULL || strlen(client_name) == 0
-        || major_version == NULL || strlen(major_version) == 0
-        || minor_version == NULL || strlen(minor_version) == 0) {
-        crm_err("Missing fields, Hello message will not be valid.");
+    if (crm_strlen_zero(uuid) || crm_strlen_zero(client_name)
+        || crm_strlen_zero(major_version) || crm_strlen_zero(minor_version)) {
+        crm_err("Could not create IPC hello message from %s (UUID %s): "
+                "missing information",
+                client_name? client_name : "unknown client",
+                uuid? uuid : "unknown");
         return NULL;
     }
 
     hello_node = create_xml_node(NULL, XML_TAG_OPTIONS);
+    if (hello_node == NULL) {
+        crm_err("Could not create IPC hello message from %s (UUID %s): "
+                "Message data creation failed", client_name, uuid);
+        return NULL;
+    }
+
     crm_xml_add(hello_node, "major_version", major_version);
     crm_xml_add(hello_node, "minor_version", minor_version);
     crm_xml_add(hello_node, "client_name", client_name);
     crm_xml_add(hello_node, "client_uuid", uuid);
 
-    crm_trace("creating hello message");
     hello = create_request(CRM_OP_HELLO, hello_node, NULL, NULL, client_name, uuid);
+    if (hello == NULL) {
+        crm_err("Could not create IPC hello message from %s (UUID %s): "
+                "Request creation failed", client_name, uuid);
+        return NULL;
+    }
     free_xml(hello_node);
 
+    crm_trace("Created hello message from %s (UUID %s)", client_name, uuid);
     return hello;
 }
