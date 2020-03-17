eea560ceb9f60bb6a832c7f286e3d63163645e24
Chris Lumens <clumens@redhat.com>
2020-03-17 15:12:15 -0400

    Refactor: libs: Move no_quorum_policy_t under PCMK__NO_COMPAT.
    It's not being used anywhere, though.

--- include/crm/pengine/pe_types.h
+++ include/crm/pengine/pe_types.h
@@ -509,7 +509,6 @@ typedef struct pe_action_wrapper_s {
 typedef struct pe_node_s node_t;                     //!< \deprecated Use pe_node_t instead
 typedef struct pe_resource_s resource_t;             //!< \deprecated Use pe_resource_t instead
 typedef struct pe_ticket_s ticket_t;                 //!< \deprecated Use pe_ticket_t instead
-typedef enum pe_quorum_policy no_quorum_policy_t;    //!< \deprecated Use enum pe_quorum_policy instead
 
 #ifndef PCMK__NO_COMPAT
 /* Everything here is deprecated and kept only for public API backward
@@ -520,6 +519,8 @@ typedef enum pe_quorum_policy no_quorum_policy_t;    //!< \deprecated Use enum p
 typedef struct pe_action_s action_t;
 //!< \deprecated Use pe_action_wrapper_t instead
 typedef struct pe_action_wrapper_s action_wrapper_t;
+//!< \deprecated Use enum pe_quorum_policy instead
+typedef enum pe_quorum_policy no_quorum_policy_t;
 //!< \deprecated Use pe_tag_t instead
 typedef struct pe_tag_s tag_t;
 
