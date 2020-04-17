ddb22e7d964ef5bed05b51ec8d4f0c4098d97ed2
Klaus Wenninger <klaus.wenninger@aon.at>
2020-04-17 15:45:01 +0200

    Build: improve checking headers for C++ fitness
    Build with each of the headers packaged separately to find issues
    easier and to prevent issues possibly covered by guard-macros.

--- maint/testcc_helper.cc
+++ maint/testcc_helper.cc
@@ -7,17 +7,6 @@
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
  */
 
-#include <crm/crm.h>
-#include <crm/cib.h>
-#include <crm/cluster.h>
-#include <crm/cluster/election.h>
-#include <crm/lrmd.h>
-#include <crm/msg_xml.h>
-#include <crm/services.h>
-#include <crm/stonith-ng.h>
-#include <crm/pengine/status.h>
-#include <crm/pengine/rules.h>
-
 int main()
 {
     return 0;
