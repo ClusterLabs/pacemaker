ClusterLabs/pacemaker

commit fdd8ea5ce8173b1e15e3a8c6eaf25dd413e22862

Build: GNUmakefile: use same archive name for make dist and make export
... as was intended according to the pre-existing comment.

Avoid using the DIST_ARCHIVES make variable, which is undocumented and internal
to automake (and removed in the automake-NG fork). Instead, change the distdir
name so that make dist automatically uses the desired tarball name.

Since this changes the source name for the spec file, we have to use the
short commit hash in the spec file now.

GNUmakefile
@@ -13,10 +13,18 @@ default: $(shell test ! -e configure && echo init) $(shell test -e configure && 
 
 PACKAGE		?= pacemaker
 
-# Force 'make dist' to be consistent with 'make export'
-distdir			= $(PACKAGE)-$(TAG)
+# This Makefile can create 2 types of distributions:
+#
+# - "make dist" is automake's native functionality, based on the various
+#   dist/nodist make variables; it always uses the current sources
+#
+# - "make export" is a custom target based on git archive and relevant entries
+#   from .gitattributes; it defaults to current sources but can use any git tag
+#
+# Both types use the TARFILE name for the result, though they generate
+# different contents.
+distdir			= $(PACKAGE)-$(SHORTTAG)
 TARFILE			= $(PACKAGE)-$(SHORTTAG).tar.gz
-DIST_ARCHIVES		= $(TARFILE)
 
 RPM_ROOT	= $(shell pwd)
 RPM_OPTS	= --define "_sourcedir $(RPM_ROOT)" 	\
@@ -186,7 +194,7 @@ srpm-%:	export $(PACKAGE)-%.spec
 		echo $(COUNT) > $(BUILD_COUNTER);				\
 	fi
 	sed -e 's/global\ specversion\ .*/global\ specversion\ $(SPECVERSION)/' \
-	    -e 's/global\ commit\ .*/global\ commit\ $(TAG)/' \
+	    -e 's/global\ commit\ .*/global\ commit\ $(SHORTTAG)/' \
 	    -e 's/global\ commit_abbrev\ .*/global\ commit_abbrev\ $(SHORTTAG_ABBREV)/' \
 	    -i $(PACKAGE).spec
 	$(call rpmbuild-with,$(WITH),-bs --define "dist .$*" $(RPM_OPTS),$(PACKAGE).spec)

commit 71a283bf8eb308e6262140ab5042f5bb4d3e2441

Build: rpm: define spec commit correctly for release candidate tags
fdd8ea5 (in Pacemaker 2.0.3) broke RPM generation for release candidates.

That commit was part of a larger project to get "make dist" working, and made
"make dist" and "make export" use the same archive file name. To get this to
work, it made the rpm spec use SHORTTAG instead of TAG as the commit.

This was fine for untagged releases, but for tagged releases, distribution
naming should use the version number alone, while the spec commit should use
the entire tag.

The solution is to use separate variables for distribution naming and
the spec commit.

GNUmakefile
@@ -36,14 +36,26 @@ PACKAGE		?= pacemaker
 # Definitions that specify what various targets will apply to
 
 COMMIT  ?= HEAD
+
+# TAG defaults to DIST when not in a git checkout (e.g. from a distribution),
+# the tag name if COMMIT is tagged, and the full commit ID otherwise.
 TAG     ?= $(shell T=$$(git describe --tags --exact-match '$(COMMIT)' 2>/dev/null); \
 	     test -n "$${T}" && echo "$${T}" \
 	       || git log --pretty=format:%H -n 1 '$(COMMIT)' 2>/dev/null || echo DIST)
 lparen = (
 rparen = )
-SHORTTAG ?= $(shell case $(TAG) in Pacemaker-*|DIST$(rparen) echo '$(TAG)' | cut -c11-;; \
-	      *$(rparen) git log --pretty=format:%h -n 1 '$(TAG)';; esac)
-SHORTTAG_ABBREV = $(shell printf %s '$(SHORTTAG)' | wc -c)
+
+# SPEC_COMMIT is identical to TAG for DIST and tagged releases, otherwise it is
+# the short commit ID (which must be used in order for "make export" to use the
+# same archive name as "make dist")
+SPEC_COMMIT	?= $(shell						\
+		case $(TAG) in						\
+		    Pacemaker-*|DIST$(rparen)				\
+		        echo '$(TAG)' ;;				\
+		    *$(rparen)						\
+		        git log --pretty=format:%h -n 1 '$(TAG)';;	\
+		esac)
+SPEC_ABBREV	= $(shell printf %s '$(SPEC_COMMIT)' | wc -c)
 
 LAST_RC		?= $(shell test -e /Volumes || git tag -l | grep Pacemaker | sort -Vr | grep rc | head -n 1)
 ifneq ($(origin VERSION), undefined)
@@ -64,8 +76,20 @@ NEXT_RELEASE	?= $(shell echo $(LAST_RELEASE) | awk -F. '/[0-9]+\./{$$3+=1;OFS=".
 #
 # Both types use the TARFILE name for the result, though they generate
 # different contents.
-distdir			= $(PACKAGE)-$(SHORTTAG)
-TARFILE			= $(abs_builddir)/$(PACKAGE)-$(SHORTTAG).tar.gz
+# 
+# The directory is named pacemaker-DIST when not in a git checkout (e.g.
+# from a distribution itself), pacemaker-<version_part_of_tag> for tagged
+# commits, and pacemaker-<short_commit> otherwise.
+distdir		= $(PACKAGE)-$(shell						\
+		  case $(TAG) in						\
+			DIST$(rparen)						\
+				echo DIST;;					\
+			Pacemaker-*$(rparen)					\
+		  		echo '$(TAG)' | cut -c11-;;			\
+		  	*$(rparen)						\
+		  		git log --pretty=format:%h -n 1 '$(TAG)';;	\
+		  esac)
+TARFILE		= $(abs_builddir)/$(distdir).tar.gz
 
 .PHONY: init
 init:
@@ -185,8 +209,8 @@ $(RPM_SPEC_DIR)/$(PACKAGE).spec: spec-clean rpm/pacemaker.spec.in
 	fi | sed									\
 	    -e "s/^\(%global pcmkversion \).*/\1$$(echo $(LAST_RELEASE) | sed -e s:Pacemaker-:: -e s:-.*::)/" \
 	    -e 's/global\ specversion\ .*/global\ specversion\ $(SPECVERSION)/' 	\
-	    -e 's/global\ commit\ .*/global\ commit\ $(SHORTTAG)/'			\
-	    -e 's/global\ commit_abbrev\ .*/global\ commit_abbrev\ $(SHORTTAG_ABBREV)/' \
+	    -e 's/global\ commit\ .*/global\ commit\ $(SPEC_COMMIT)/'			\
+	    -e 's/global\ commit_abbrev\ .*/global\ commit_abbrev\ $(SPEC_ABBREV)/'	\
 	    -e "s/PACKAGE_DATE/$$(date +'%a %b %d %Y')/"				\
 	    -e "s/PACKAGE_VERSION/$$(git describe --tags $(TAG) | sed -e s:Pacemaker-:: -e s:-.*::)/"	\
 	    > "$@"