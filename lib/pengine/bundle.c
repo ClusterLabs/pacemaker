ClusterLabs/pacemaker

commit 037c8ece42dabc55f9ce19dc4b4c807ff70a7c9b

Fix: libpe_status: Don't filter in pe__bundle_replica_output_text.
This is now handled by the callers.  Attempting to do filtering here
results in segfaults for inactive resources which do not have a node
they are currently running on.

The same also applies to pe__bundle_replica_output_html.

lib/pengine/bundle.c
@@ -1562,8 +1562,8 @@ pe__bundle_xml(pcmk__output_t *out, va_list args)
 }
 
 static void
-pe__bundle_replica_output_html(pcmk__output_t *out, GListPtr only_show,
-                               pe__bundle_replica_t *replica, long options)
+pe__bundle_replica_output_html(pcmk__output_t *out, pe__bundle_replica_t *replica,
+                               long options)
 {
     pe_node_t *node = NULL;
     pe_resource_t *rsc = replica->child;
@@ -1587,10 +1587,7 @@ pe__bundle_replica_output_html(pcmk__output_t *out, GListPtr only_show,
                            replica->ipaddr);
     }
 
-    node = pe__current_node(replica->container);
-    if (pcmk__str_in_list(only_show, node->details->uname)) {
-        pe__common_output_html(out, rsc, buffer, node, options);
-    }
+    pe__common_output_html(out, rsc, buffer, node, options);
 }
 
 PCMK__OUTPUT_ARGS("bundle", "unsigned int", "struct pe_resource_t *", "GListPtr")
@@ -1650,7 +1647,7 @@ pe__bundle_html(pcmk__output_t *out, va_list args)
 
             out->end_list(out);
         } else {
-            pe__bundle_replica_output_html(out, only_show, replica, options);
+            pe__bundle_replica_output_html(out, replica, options);
         }
 
         pcmk__output_xml_pop_parent(out);
@@ -1661,8 +1658,8 @@ pe__bundle_html(pcmk__output_t *out, va_list args)
 }
 
 static void
-pe__bundle_replica_output_text(pcmk__output_t *out, GListPtr only_show,
-                               pe__bundle_replica_t *replica, long options)
+pe__bundle_replica_output_text(pcmk__output_t *out, pe__bundle_replica_t *replica,
+                               long options)
 {
     pe_node_t *node = NULL;
     pe_resource_t *rsc = replica->child;
@@ -1686,10 +1683,7 @@ pe__bundle_replica_output_text(pcmk__output_t *out, GListPtr only_show,
                            replica->ipaddr);
     }
 
-    node = pe__current_node(replica->container);
-    if (pcmk__str_in_list(only_show, node->details->uname)) {
-        pe__common_output_text(out, rsc, buffer, node, options);
-    }
+    pe__common_output_text(out, rsc, buffer, node, options);
 }
 
 PCMK__OUTPUT_ARGS("bundle", "unsigned int", "struct pe_resource_t *", "GListPtr")
@@ -1745,7 +1739,7 @@ pe__bundle_text(pcmk__output_t *out, va_list args)
 
             out->end_list(out);
         } else {
-            pe__bundle_replica_output_text(out, only_show, replica, options);
+            pe__bundle_replica_output_text(out, replica, options);
         }
     }
 

commit e5bb6f1fb9026b6da9c0aeef7d615fb675e9207b

Fix: scheduler: Add the node name back to bundle instances.
This regression was introduced by 037c8ece42dabc55f9ce19dc4b4c807ff70a7c9b.

cts/cli/regression.crm_mon.exp
@@ -623,8 +623,8 @@ Active Resources:
     * Started: [ cluster01 ]
   * Fencing	(stonith:fence_xvm):	 Started cluster01
   * Container bundle set: httpd-bundle [pcmk:http]:
-    * httpd-bundle-0 (192.168.122.131)	(ocf::heartbeat:apache):	 Started
-    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped
+    * httpd-bundle-0 (192.168.122.131)	(ocf::heartbeat:apache):	 Started cluster02
+    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped cluster01
 =#=#=#= End test: Text output of partially active resources - OK (0) =#=#=#=
 * Passed: crm_mon        - Text output of partially active resources
 =#=#=#= Begin test: XML output of partially active resources =#=#=#=
@@ -757,8 +757,8 @@ Full List of Resources:
     * Stopped: [ cluster02 ]
   * Fencing	(stonith:fence_xvm):	 Started cluster01
   * Container bundle set: httpd-bundle [pcmk:http]:
-    * httpd-bundle-0 (192.168.122.131)	(ocf::heartbeat:apache):	 Started
-    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped
+    * httpd-bundle-0 (192.168.122.131)	(ocf::heartbeat:apache):	 Started cluster02
+    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped cluster01
 =#=#=#= End test: Text output of partially active resources, with inactive resources - OK (0) =#=#=#=
 * Passed: crm_mon        - Text output of partially active resources, with inactive resources
 =#=#=#= Begin test: Text output of partially active resources, with inactive resources, filtered by node =#=#=#=
@@ -778,7 +778,7 @@ Full List of Resources:
     * Started: [ cluster01 ]
   * Fencing	(stonith:fence_xvm):	 Started cluster01
   * Container bundle set: httpd-bundle [pcmk:http]:
-    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped
+    * httpd-bundle-1 (192.168.122.132)	(ocf::heartbeat:apache):	 Stopped cluster01
 =#=#=#= End test: Text output of partially active resources, with inactive resources, filtered by node - OK (0) =#=#=#=
 * Passed: crm_mon        - Text output of partially active resources, with inactive resources, filtered by node
 =#=#=#= Begin test: Text output of partially active resources, filtered by node =#=#=#=

lib/pengine/bundle.c
@@ -1563,9 +1563,8 @@ pe__bundle_xml(pcmk__output_t *out, va_list args)
 
 static void
 pe__bundle_replica_output_html(pcmk__output_t *out, pe__bundle_replica_t *replica,
-                               long options)
+                               pe_node_t *node, long options)
 {
-    pe_node_t *node = NULL;
     pe_resource_t *rsc = replica->child;
 
     int offset = 0;
@@ -1647,7 +1646,12 @@ pe__bundle_html(pcmk__output_t *out, va_list args)
 
             out->end_list(out);
         } else {
-            pe__bundle_replica_output_html(out, replica, options);
+            if (pcmk__rsc_is_filtered(replica->container, only_show)) {
+                continue;
+            }
+
+            pe__bundle_replica_output_html(out, replica, pe__current_node(replica->container),
+                                           options);
         }
 
         pcmk__output_xml_pop_parent(out);
@@ -1659,9 +1663,8 @@ pe__bundle_html(pcmk__output_t *out, va_list args)
 
 static void
 pe__bundle_replica_output_text(pcmk__output_t *out, pe__bundle_replica_t *replica,
-                               long options)
+                               pe_node_t *node, long options)
 {
-    pe_node_t *node = NULL;
     pe_resource_t *rsc = replica->child;
 
     int offset = 0;
@@ -1739,7 +1742,12 @@ pe__bundle_text(pcmk__output_t *out, va_list args)
 
             out->end_list(out);
         } else {
-            pe__bundle_replica_output_text(out, replica, options);
+            if (pcmk__rsc_is_filtered(replica->container, only_show)) {
+                continue;
+            }
+
+            pe__bundle_replica_output_text(out, replica, pe__current_node(replica->container),
+                                           options);
         }
     }
 