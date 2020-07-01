ceb7eff9623e85d2e06a070d88947027ff048108
Ken Gaillot <kgaillot@redhat.com>
2020-07-01 17:08:29 -0500

    Test: libcrmcommon: add unit tests for flag-checking functions

---
+++ lib/common/tests/flags/pcmk_all_flags_set.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+static void
+all_set(void) {
+    g_assert(pcmk_all_flags_set(0x000, 0x003) == false);
+    g_assert(pcmk_all_flags_set(0x00f, 0x003) == true);
+    g_assert(pcmk_all_flags_set(0x00f, 0x010) == false);
+    g_assert(pcmk_all_flags_set(0x00f, 0x011) == false);
+}
+
+static void
+one_is_set(void) {
+    // pcmk_is_set() is a simple macro alias for pcmk_all_flags_set()
+    g_assert(pcmk_is_set(0x00f, 0x001) == true);
+    g_assert(pcmk_is_set(0x00f, 0x010) == false);
+}
+
+int
+main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_func("/common/flags/all_set/all_set", all_set);
+    g_test_add_func("/common/flags/all_set/is_set", one_is_set);
+    return g_test_run();
+}
