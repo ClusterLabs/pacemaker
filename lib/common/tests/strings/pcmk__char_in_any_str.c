41df12e2dd1cd04fb3b7ef57c0c1847c199180c9
Reid Wahl <nrwahl@protonmail.com>
2020-08-08 14:41:49 -0700

    Test: libcrmcommon: Create unit tests for pcmk__char_in_any_str()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

---
+++ lib/common/tests/strings/pcmk__char_in_any_str.c
@@ -0,0 +1,44 @@
+#include <glib.h>
+
+#include <crm_internal.h>
+
+static void
+empty_list(void)
+{
+    g_assert_false(pcmk__char_in_any_str('x', NULL));
+    g_assert_false(pcmk__char_in_any_str('\0', NULL));
+}
+
+static void
+null_char(void)
+{
+    g_assert_true(pcmk__char_in_any_str('\0', "xxx", "yyy", NULL));
+    g_assert_true(pcmk__char_in_any_str('\0', "", NULL));
+}
+
+static void
+in_list(void)
+{
+    g_assert_true(pcmk__char_in_any_str('x', "aaa", "bbb", "xxx", NULL));
+}
+
+static void
+not_in_list(void)
+{
+    g_assert_false(pcmk__char_in_any_str('x', "aaa", "bbb", NULL));
+    g_assert_false(pcmk__char_in_any_str('A', "aaa", "bbb", NULL));
+    g_assert_false(pcmk__char_in_any_str('x', "", NULL));
+}
+
+int main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_func("/common/strings/char_in_any_str/empty_list", empty_list);
+    g_test_add_func("/common/strings/char_in_any_str/null_char", null_char);
+    g_test_add_func("/common/strings/char_in_any_str/in", in_list);
+    g_test_add_func("/common/strings/char_in_any_str/not_in", not_in_list);
+
+    return g_test_run();
+}
+
