ClusterLabs/pacemaker

commit 18a7ee8d1a80be91966eddf7287e16f56ad229d3

Refactor: daemons, lib, tools: Rename pcmk__str_any_of.
This function is case-insensitive.  It makes sense to rename it to
illustrate that fact, plus free up the old name for a new function that
is case-sensitive.

daemons/controld/controld_execd.c
@@ -225,8 +225,8 @@ update_history_cache(lrm_state_t * lrm_state, lrmd_rsc_info_t * rsc, lrmd_event_
         }
         entry->last = lrmd_copy_event(op);
 
-        if (op->params && pcmk__str_any_of(op->op_type, CRMD_ACTION_START,
-                                          "reload", CRMD_ACTION_STATUS, NULL)) {
+        if (op->params && pcmk__strcase_any_of(op->op_type, CRMD_ACTION_START,
+                                               "reload", CRMD_ACTION_STATUS, NULL)) {
             if (entry->stop_params) {
                 g_hash_table_destroy(entry->stop_params);
             }
@@ -1590,7 +1590,7 @@ handle_reprobe_op(lrm_state_t *lrm_state, const char *from_sys,
     crm_notice("Forcing the status of all resources to be redetected");
     force_reprobe(lrm_state, from_sys, from_host, user_name, is_remote_node);
 
-    if (!pcmk__str_any_of(from_sys, CRM_SYSTEM_PENGINE, CRM_SYSTEM_TENGINE, NULL)) {
+    if (!pcmk__strcase_any_of(from_sys, CRM_SYSTEM_PENGINE, CRM_SYSTEM_TENGINE, NULL)) {
 
         xmlNode *reply = create_request(CRM_OP_INVOKE_LRM, NULL, from_host,
                                         from_sys, CRM_SYSTEM_LRMD,
@@ -1797,7 +1797,7 @@ do_lrm_invoke(long long action,
         update_attrd(lrm_state->node_name, CRM_OP_PROBED, XML_BOOLEAN_TRUE,
                      user_name, is_remote_node);
 
-    } else if (pcmk__str_any_of(CRM_OP_REPROBE, operation, crm_op, NULL)) {
+    } else if (pcmk__strcase_any_of(CRM_OP_REPROBE, operation, crm_op, NULL)) {
         handle_reprobe_op(lrm_state, from_sys, from_host, user_name,
                           is_remote_node);
 
@@ -1977,8 +1977,8 @@ construct_op(lrm_state_t *lrm_state, xmlNode *rsc_op, const char *rsc_id,
 
 #if ENABLE_VERSIONED_ATTRS
     if (lrm_state && !is_remote_lrmd_ra(NULL, NULL, rsc_id)
-        && !pcmk__str_any_of(op_type, CRMD_ACTION_METADATA, CRMD_ACTION_DELETE,
-                              NULL)) {
+        && !pcmk__strcase_any_of(op_type, CRMD_ACTION_METADATA, CRMD_ACTION_DELETE,
+                                 NULL)) {
         resolve_versioned_parameters(lrm_state, rsc_id, rsc_op, params);
     }
 #endif
@@ -2023,7 +2023,7 @@ construct_op(lrm_state_t *lrm_state, xmlNode *rsc_op, const char *rsc_id,
     op->user_data = strdup(transition);
 
     if (op->interval_ms != 0) {
-        if (pcmk__str_any_of(operation, CRMD_ACTION_START, CRMD_ACTION_STOP, NULL)) {
+        if (pcmk__strcase_any_of(operation, CRMD_ACTION_START, CRMD_ACTION_STOP, NULL)) {
             crm_err("Start and Stop actions cannot have an interval: %u",
                     op->interval_ms);
             op->interval_ms = 0;

daemons/attrd/attrd_commands.c
@@ -590,8 +590,8 @@ attrd_peer_message(crm_node_t *peer, xmlNode *xml)
 
     peer_won = attrd_check_for_new_writer(peer, xml);
 
-    if (pcmk__str_any_of(op, PCMK__ATTRD_CMD_UPDATE, PCMK__ATTRD_CMD_UPDATE_BOTH,
-                        PCMK__ATTRD_CMD_UPDATE_DELAY, NULL)) {
+    if (pcmk__strcase_any_of(op, PCMK__ATTRD_CMD_UPDATE, PCMK__ATTRD_CMD_UPDATE_BOTH,
+                             PCMK__ATTRD_CMD_UPDATE_DELAY, NULL)) {
         attrd_peer_update(peer, xml, host, FALSE);
 
     } else if (pcmk__str_eq(op, PCMK__ATTRD_CMD_SYNC, pcmk__str_casei)) {

daemons/based/based_callbacks.c
@@ -515,7 +515,7 @@ parse_local_options_v2(pcmk__client_t *cib_client, int call_type,
                        gboolean *process, gboolean *needs_forward)
 {
     if (cib_op_modifies(call_type)) {
-        if (pcmk__str_any_of(op, CIB_OP_MASTER, CIB_OP_SLAVE, NULL)) {
+        if (pcmk__strcase_any_of(op, CIB_OP_MASTER, CIB_OP_SLAVE, NULL)) {
             /* Always handle these locally */
             *process = TRUE;
             *needs_reply = FALSE;

daemons/based/based_common.c
@@ -40,8 +40,8 @@ cib_prepare_common(xmlNode * root, const char *section)
     if (root == NULL) {
         return NULL;
 
-    } else if (pcmk__str_any_of(crm_element_name(root), XML_TAG_FRAGMENT,
-                               F_CRM_DATA, F_CIB_CALLDATA, NULL)) {
+    } else if (pcmk__strcase_any_of(crm_element_name(root), XML_TAG_FRAGMENT,
+                                    F_CRM_DATA, F_CIB_CALLDATA, NULL)) {
         data = first_named_child(root, XML_TAG_CIB);
 
     } else {

daemons/controld/controld_based.c
@@ -160,7 +160,7 @@ int crmd_cib_smart_opt()
 bool
 controld_action_is_recordable(const char *action)
 {
-    return !pcmk__str_any_of(action, CRMD_ACTION_CANCEL, CRMD_ACTION_DELETE,
+    return !pcmk__strcase_any_of(action, CRMD_ACTION_CANCEL, CRMD_ACTION_DELETE,
                             CRMD_ACTION_NOTIFY, CRMD_ACTION_METADATA, NULL);
 }
 

daemons/controld/controld_messages.c
@@ -375,8 +375,8 @@ relay_message(xmlNode * msg, gboolean originated_locally)
             is_local = 0;
 
         } else if (is_for_crm) {
-            if (pcmk__str_any_of(task, CRM_OP_NODE_INFO,
-                                 PCMK__CONTROLD_CMD_NODES, NULL)) {
+            if (pcmk__strcase_any_of(task, CRM_OP_NODE_INFO,
+                                     PCMK__CONTROLD_CMD_NODES, NULL)) {
                 /* Node info requests do not specify a host, which is normally
                  * treated as "all hosts", because the whole point is that the
                  * client may not know the local node name. Always handle these
@@ -412,8 +412,8 @@ relay_message(xmlNode * msg, gboolean originated_locally)
             crm_trace("Route message %s locally as DC request", ref);
             return FALSE; // More to be done by caller
 
-        } else if (originated_locally && !pcmk__str_any_of(sys_from, CRM_SYSTEM_PENGINE,
-                                                           CRM_SYSTEM_TENGINE, NULL)) {
+        } else if (originated_locally && !pcmk__strcase_any_of(sys_from, CRM_SYSTEM_PENGINE,
+                                                               CRM_SYSTEM_TENGINE, NULL)) {
 
 #if SUPPORT_COROSYNC
             if (is_corosync_cluster()) {

daemons/controld/controld_remote_ra.c
@@ -415,7 +415,7 @@ retry_start_cmd_cb(gpointer data)
         return FALSE;
     }
     cmd = ra_data->cur_cmd;
-    if (!pcmk__str_any_of(cmd->action, "start", "migrate_from", NULL)) {
+    if (!pcmk__strcase_any_of(cmd->action, "start", "migrate_from", NULL)) {
         return FALSE;
     }
     update_remaining_timeout(cmd);
@@ -597,8 +597,8 @@ remote_lrm_op_callback(lrmd_event_data_t * op)
 
     /* Start actions and migrate from actions complete after connection
      * comes back to us. */
-    if (op->type == lrmd_event_connect && pcmk__str_any_of(cmd->action, "start",
-                                                          "migrate_from", NULL)) {
+    if (op->type == lrmd_event_connect && pcmk__strcase_any_of(cmd->action, "start",
+                                                               "migrate_from", NULL)) {
         if (op->connection_rc < 0) {
             update_remaining_timeout(cmd);
 
@@ -735,8 +735,8 @@ handle_remote_ra_start(lrm_state_t * lrm_state, remote_ra_cmd_t * cmd, int timeo
     int timeout_used = timeout_ms > MAX_START_TIMEOUT_MS ? MAX_START_TIMEOUT_MS : timeout_ms;
 
     for (tmp = cmd->params; tmp; tmp = tmp->next) {
-        if (pcmk__str_any_of(tmp->key, XML_RSC_ATTR_REMOTE_RA_ADDR,
-                            XML_RSC_ATTR_REMOTE_RA_SERVER, NULL)) {
+        if (pcmk__strcase_any_of(tmp->key, XML_RSC_ATTR_REMOTE_RA_ADDR,
+                                 XML_RSC_ATTR_REMOTE_RA_SERVER, NULL)) {
             server = tmp->value;
         } else if (pcmk__str_eq(tmp->key, XML_RSC_ATTR_REMOTE_RA_PORT, pcmk__str_casei)) {
             port = atoi(tmp->value);

daemons/controld/controld_te_actions.c
@@ -451,8 +451,8 @@ te_update_job_count(crm_action_t * action, int offset)
      * the connection resources */
     target = crm_element_value(action->xml, XML_LRM_ATTR_ROUTER_NODE);
 
-    if ((target == NULL) && pcmk__str_any_of(task, CRMD_ACTION_MIGRATE,
-                                            CRMD_ACTION_MIGRATED, NULL)) {
+    if ((target == NULL) && pcmk__strcase_any_of(task, CRMD_ACTION_MIGRATE,
+                                                 CRMD_ACTION_MIGRATED, NULL)) {
 
         const char *t1 = crm_meta_value(action->params, XML_LRM_ATTR_MIGRATE_SOURCE);
         const char *t2 = crm_meta_value(action->params, XML_LRM_ATTR_MIGRATE_TARGET);
@@ -498,7 +498,7 @@ te_should_perform_action_on(crm_graph_t * graph, crm_action_t * action, const ch
         return FALSE;
 
     } else if(graph->migration_limit > 0 && r->migrate_jobs >= graph->migration_limit) {
-        if (pcmk__str_any_of(task, CRMD_ACTION_MIGRATE, CRMD_ACTION_MIGRATED, NULL)) {
+        if (pcmk__strcase_any_of(task, CRMD_ACTION_MIGRATE, CRMD_ACTION_MIGRATED, NULL)) {
             crm_trace("Peer %s is over their migration job limit of %d (%d): deferring %s",
                       target, graph->migration_limit, r->migrate_jobs, id);
             return FALSE;
@@ -527,8 +527,8 @@ te_should_perform_action(crm_graph_t * graph, crm_action_t * action)
      * the connection resources */
     target = crm_element_value(action->xml, XML_LRM_ATTR_ROUTER_NODE);
 
-    if ((target == NULL) && pcmk__str_any_of(task, CRMD_ACTION_MIGRATE,
-                                            CRMD_ACTION_MIGRATED, NULL)) {
+    if ((target == NULL) && pcmk__strcase_any_of(task, CRMD_ACTION_MIGRATE,
+                                                 CRMD_ACTION_MIGRATED, NULL)) {
         target = crm_meta_value(action->params, XML_LRM_ATTR_MIGRATE_SOURCE);
         if(te_should_perform_action_on(graph, action, target) == FALSE) {
             return FALSE;

commit dbf550106cc460be0ec77abe6fc7ab5f0c3254d7

Fix: controller: recognize reprobe requests correctly again
pcmk__strcase_any_of() can't be used if any of the comparison strings (other
than the last) can be NULL. Regression since 18a7ee8d1 (unreleased).
This is the only call affected by the issue.

daemons/controld/controld_execd.c
@@ -1802,7 +1802,8 @@ do_lrm_invoke(long long action,
         update_attrd(lrm_state->node_name, CRM_OP_PROBED, XML_BOOLEAN_TRUE,
                      user_name, is_remote_node);
 
-    } else if (pcmk__strcase_any_of(CRM_OP_REPROBE, operation, crm_op, NULL)) {
+    } else if (pcmk__str_eq(crm_op, CRM_OP_REPROBE, pcmk__str_casei)
+               || pcmk__str_eq(operation, CRM_OP_REPROBE, pcmk__str_casei)) {
         handle_reprobe_op(lrm_state, from_sys, from_host, user_name,
                           is_remote_node);
 