ClusterLabs/pacemaker

commit 34cb03a04b579821b65ee8d174b633599f5cb1a5

High: fencer: restrict certain IPC requests to privileged users
This is a partial fix (along with two previous commits) for CVE-2020-25654.

The fencer IPC API allows clients to register fence devices.

If ACLs are enabled, this could allow an ACL-restricted user to bypass ACLs to
configure fencing. If the user is able to install executables to the standard
fencing agent locations, have arbitrary code executed as root (the standard
locations generally require root for write access, so that is unlikely to be an
issue).

If ACLs are not enabled, users in the haclient group have full access to the
CIB, which already gives them these capabilities, so there is no additional
exposure in that case.

This commit does not restrict unprivileged users from using other fencing API,
such as requesting actual fencing.

daemons/fenced/fenced_commands.c
@@ -2547,6 +2547,20 @@ handle_request(pcmk__client_t *client, uint32_t id, uint32_t flags,
     const char *op = crm_element_value(request, F_STONITH_OPERATION);
     const char *client_id = crm_element_value(request, F_STONITH_CLIENTID);
 
+    bool allowed = false;
+
+#if ENABLE_ACL
+    /* IPC commands related to fencing configuration may be done only by
+     * privileged users (i.e. root or hacluster) when ACLs are supported,
+     * because all other users should go through the CIB to have ACLs applied.
+     */
+    if (client != NULL) {
+        allowed = pcmk_is_set(client->flags, pcmk__client_privileged);
+    }
+#else
+    allowed = true;
+#endif
+
     crm_element_value_int(request, F_STONITH_CALLOPTS, &call_options);
 
     if (pcmk_is_set(call_options, st_opt_sync_call)) {
@@ -2705,27 +2719,43 @@ handle_request(pcmk__client_t *client, uint32_t id, uint32_t flags,
     } else if (pcmk__str_eq(op, STONITH_OP_DEVICE_ADD, pcmk__str_none)) {
         const char *device_id = NULL;
 
-        rc = stonith_device_register(request, &device_id, FALSE);
+        if (allowed) {
+            rc = stonith_device_register(request, &device_id, FALSE);
+        } else {
+            rc = -EACCES;
+        }
         do_stonith_notify_device(call_options, op, rc, device_id);
 
     } else if (pcmk__str_eq(op, STONITH_OP_DEVICE_DEL, pcmk__str_none)) {
         xmlNode *dev = get_xpath_object("//" F_STONITH_DEVICE, request, LOG_ERR);
         const char *device_id = crm_element_value(dev, XML_ATTR_ID);
 
-        rc = stonith_device_remove(device_id, FALSE);
+        if (allowed) {
+            rc = stonith_device_remove(device_id, FALSE);
+        } else {
+            rc = -EACCES;
+        }
         do_stonith_notify_device(call_options, op, rc, device_id);
 
     } else if (pcmk__str_eq(op, STONITH_OP_LEVEL_ADD, pcmk__str_none)) {
         char *device_id = NULL;
 
-        rc = stonith_level_register(request, &device_id);
+        if (allowed) {
+            rc = stonith_level_register(request, &device_id);
+        } else {
+            rc = -EACCES;
+        }
         do_stonith_notify_level(call_options, op, rc, device_id);
         free(device_id);
 
     } else if (pcmk__str_eq(op, STONITH_OP_LEVEL_DEL, pcmk__str_none)) {
         char *device_id = NULL;
 
-        rc = stonith_level_remove(request, &device_id);
+        if (allowed) {
+            rc = stonith_level_remove(request, &device_id);
+        } else {
+            rc = -EACCES;
+        }
         do_stonith_notify_level(call_options, op, rc, device_id);
 
     } else if(pcmk__str_eq(op, CRM_OP_RM_NODE_CACHE, pcmk__str_casei)) {
@@ -2744,6 +2774,11 @@ handle_request(pcmk__client_t *client, uint32_t id, uint32_t flags,
     }
 
   done:
+
+    if (rc == -EACCES) {
+        crm_warn("Rejecting IPC request '%s' from unprivileged client %s",
+                 crm_str(op), pcmk__client_name(client));
+    }
 
     /* Always reply unless the request is in process still.
      * If in progress, a reply will happen async after the request

commit a4f0cb1e9578fef2e0dd99986c743f65f38246c8

Low: fencer: allow peer configuration requests
34cb03a0 mistakenly disallowed fencing configuration requests made via peer CPG
(as opposed to client IPC, which is properly allowed or disallowed as
appropriate).

This has no practical effect since configuration requests are never made via
CPG (instead, the fencer loads configuration from CIB notifications, and the
executor makes client IPC requests for device starts and stops). However,
allowing them is more correct.

daemons/fenced/fenced_commands.c
@@ -2547,18 +2547,17 @@ handle_request(pcmk__client_t *client, uint32_t id, uint32_t flags,
     const char *op = crm_element_value(request, F_STONITH_OPERATION);
     const char *client_id = crm_element_value(request, F_STONITH_CLIENTID);
 
-    bool allowed = false;
-
 #if ENABLE_ACL
     /* IPC commands related to fencing configuration may be done only by
      * privileged users (i.e. root or hacluster) when ACLs are supported,
      * because all other users should go through the CIB to have ACLs applied.
+     *
+     * If no client was given, this is a peer request, which is always allowed.
      */
-    if (client != NULL) {
-        allowed = pcmk_is_set(client->flags, pcmk__client_privileged);
-    }
+    bool allowed = (client == NULL)
+                   || pcmk_is_set(client->flags, pcmk__client_privileged);
 #else
-    allowed = true;
+    bool allowed = true;
 #endif
 
     crm_element_value_int(request, F_STONITH_CALLOPTS, &call_options);