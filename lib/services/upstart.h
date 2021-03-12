f9117a443b7c903df8fd4a85a2b42c2e11b282e1
Ken Gaillot <kgaillot@redhat.com>
2021-03-12 11:29:45 -0600

    Doc: all: finish updating copyright notices to current guidelines
    No copyrights are affected, they are just declared consistently.
    
    For extra/alerts/alert_snmp.sh.sample, the original copyright was determined
    from git history.

--- lib/services/upstart.h
+++ lib/services/upstart.h
@@ -1,6 +1,9 @@
 /*
- * Copyright (C) 2010 Senko Rasic <senko.rasic@dobarkod.hr>
- * Copyright (c) 2010 Ante Karamatic <ivoks@init.hr>
+ * Copyright 2010 Senko Rasic <senko.rasic@dobarkod.hr>
+ * Copyright 2010 Ante Karamatic <ivoks@init.hr>
+ * Later changes copyright 2012-2021 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
  *
  * This source code is licensed under the GNU Lesser General Public License
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
