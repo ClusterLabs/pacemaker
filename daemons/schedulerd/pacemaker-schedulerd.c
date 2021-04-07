ClusterLabs/pacemaker

commit 74278d7b9629e0bf28f134f09791a7d90a30a874

Refactor: various: rename and document config warn/error globals
... per current style guidelines

daemons/schedulerd/pacemaker-schedulerd.c
@@ -11,6 +11,7 @@
 
 #include <crm/crm.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -87,8 +88,8 @@ process_pe_message(xmlNode *msg, xmlNode *xml_data, pcmk__client_t *sender)
         gboolean is_repoke = FALSE;
         gboolean process = TRUE;
 
-        crm_config_error = FALSE;
-        crm_config_warning = FALSE;
+        pcmk__config_error = false;
+        pcmk__config_warning = false;
 
         was_processing_error = FALSE;
         was_processing_warning = FALSE;
@@ -159,8 +160,8 @@ process_pe_message(xmlNode *msg, xmlNode *xml_data, pcmk__client_t *sender)
         crm_xml_add(reply, F_CRM_TGRAPH_INPUT, filename);
         crm_xml_add_int(reply, "graph-errors", was_processing_error);
         crm_xml_add_int(reply, "graph-warnings", was_processing_warning);
-        crm_xml_add_int(reply, "config-errors", crm_config_error);
-        crm_xml_add_int(reply, "config-warnings", crm_config_warning);
+        crm_xml_add_int(reply, "config-errors", pcmk__config_error);
+        crm_xml_add_int(reply, "config-warnings", pcmk__config_warning);
 
         if (pcmk__ipc_send_xml(sender, 0, reply,
                                crm_ipc_server_event) != pcmk_rc_ok) {

include/crm/common/logging.h
@@ -65,9 +65,14 @@ crm_extended_logging(int t, int e)
 #endif
 
 extern unsigned int crm_log_level;
-extern gboolean crm_config_error;
-extern gboolean crm_config_warning;
 extern unsigned int crm_trace_nonlog;
+
+/* These are set when a configuration issue is found, and turn on extra messages
+ * at the end of processing. They are set via wrapper functions and do not need
+ * to be set directly.
+ */
+extern bool pcmk__config_error;
+extern bool pcmk__config_warning;
 
 enum xml_log_options
 {

lib/common/utils.c
@@ -47,8 +47,8 @@
 
 CRM_TRACE_INIT_DATA(common);
 
-gboolean crm_config_error = FALSE;
-gboolean crm_config_warning = FALSE;
+bool pcmk__config_error = false;
+bool pcmk__config_warning = false;
 char *crm_system_name = NULL;
 
 int pcmk__score_red = 0;

lib/pacemaker/pcmk_sched_allocate.c
@@ -2893,7 +2893,7 @@ pcmk__log_transition_summary(const char *filename)
         crm_notice("Calculated transition %d, saving inputs in %s",
                    transition_id, filename);
     }
-    if (crm_config_error) {
+    if (pcmk__config_error) {
         crm_notice("Configuration errors found during scheduler processing,"
                    "  please run \"crm_verify -L\" to identify issues");
     }

tools/crm_verify.c
@@ -265,7 +265,7 @@ main(int argc, char **argv)
         cib_object = NULL;
 
     } else if (cli_config_update(&cib_object, NULL, FALSE) == FALSE) {
-        crm_config_error = TRUE;
+        pcmk__config_error = true;
         free_xml(cib_object);
         cib_object = NULL;
         fprintf(stderr, "The cluster will NOT be able to use this configuration.\n");
@@ -293,14 +293,14 @@ main(int argc, char **argv)
     }
     pe_free_working_set(data_set);
 
-    if (crm_config_error) {
+    if (pcmk__config_error) {
         fprintf(stderr, "Errors found during check: config not valid\n");
         if (verbose == FALSE) {
             fprintf(stderr, "  -V may provide more details\n");
         }
         rc = -pcmk_err_schema_validation;
 
-    } else if (crm_config_warning) {
+    } else if (pcmk__config_warning) {
         fprintf(stderr, "Warnings found during check: config may not be valid\n");
         if (verbose == FALSE) {
             fprintf(stderr, "  Use -V -V for more details\n");

include/crm/common/internal.h
@@ -97,13 +97,27 @@ pcmk__open_devnull(int flags)
 
 /* internal logging utilities */
 
+/*!
+ * \internal
+ * \brief Log a configuration error
+ *
+ * \param[in] fmt   printf(3)-style format string
+ * \param[in] ...   Arguments for format string
+ */
 #  define pcmk__config_err(fmt...) do {     \
-        crm_config_error = TRUE;            \
+        pcmk__config_error = true;          \
         crm_err(fmt);                       \
     } while (0)
 
+/*!
+ * \internal
+ * \brief Log a configuration warning
+ *
+ * \param[in] fmt   printf(3)-style format string
+ * \param[in] ...   Arguments for format string
+ */
 #  define pcmk__config_warn(fmt...) do {    \
-        crm_config_warning = TRUE;          \
+        pcmk__config_warning = true;        \
         crm_warn(fmt);                      \
     } while (0)
 

include/crm/pengine/internal.h
@@ -19,8 +19,16 @@
 #  define pe_rsc_debug(rsc, fmt, args...) crm_log_tag(LOG_DEBUG, rsc ? rsc->id : "<NULL>", fmt, ##args)
 #  define pe_rsc_trace(rsc, fmt, args...) crm_log_tag(LOG_TRACE, rsc ? rsc->id : "<NULL>", fmt, ##args)
 
-#  define pe_err(fmt...) { was_processing_error = TRUE; crm_config_error = TRUE; crm_err(fmt); }
-#  define pe_warn(fmt...) { was_processing_warning = TRUE; crm_config_warning = TRUE; crm_warn(fmt); }
+#  define pe_err(fmt...) do {           \
+        was_processing_error = TRUE;    \
+        pcmk__config_err(fmt);          \
+    } while (0)
+
+#  define pe_warn(fmt...) do {          \
+        was_processing_warning = TRUE;  \
+        pcmk__config_warn(fmt);         \
+    } while (0)
+
 #  define pe_proc_err(fmt...) { was_processing_error = TRUE; crm_err(fmt); }
 #  define pe_proc_warn(fmt...) { was_processing_warning = TRUE; crm_warn(fmt); }
 

commit 24281455b8ed619d8494ae021f7efaa613c3cf5a

API: libcrmcommon: deprecate crm_config_error and crm_config_warning
These globals were renamed and retyped in 74278d7b, which mistakenly considered
them internal API. Unfortunately that incompatible API change made it into the
2.0.5 release.

In practice no external code is likely to have used these, so it likely doesn't
matter, but the most appropriate course of action now seems to be to revert the
change and deprecate them instead.

daemons/schedulerd/pacemaker-schedulerd.c
@@ -47,8 +47,8 @@ void pengine_shutdown(int nsig);
 static void
 init_working_set(void)
 {
-    pcmk__config_error = false;
-    pcmk__config_warning = false;
+    crm_config_error = FALSE;
+    crm_config_warning = FALSE;
 
     was_processing_error = FALSE;
     was_processing_warning = FALSE;
@@ -161,8 +161,8 @@ handle_pecalc_op(xmlNode *msg, xmlNode *xml_data, pcmk__client_t *sender)
     crm_xml_add(reply, F_CRM_TGRAPH_INPUT, filename);
     crm_xml_add_int(reply, "graph-errors", was_processing_error);
     crm_xml_add_int(reply, "graph-warnings", was_processing_warning);
-    crm_xml_add_int(reply, "config-errors", pcmk__config_error);
-    crm_xml_add_int(reply, "config-warnings", pcmk__config_warning);
+    crm_xml_add_int(reply, "config-errors", crm_config_error);
+    crm_xml_add_int(reply, "config-warnings", crm_config_warning);
 
     if (pcmk__ipc_send_xml(sender, 0, reply,
                            crm_ipc_server_event) != pcmk_rc_ok) {

include/crm/common/logging.h
@@ -67,12 +67,19 @@ crm_extended_logging(int t, int e)
 extern unsigned int crm_log_level;
 extern unsigned int crm_trace_nonlog;
 
-/* These are set when a configuration issue is found, and turn on extra messages
- * at the end of processing. They are set via wrapper functions and do not need
- * to be set directly.
+/*! \deprecated Pacemaker library functions set this when a configuration
+ *              error is found, which turns on extra messages at the end of
+ *              processing. It should not be used directly and will be removed
+ *              from the public C API in a future release.
  */
-extern bool pcmk__config_error;
-extern bool pcmk__config_warning;
+extern gboolean crm_config_error;
+
+/*! \deprecated Pacemaker library functions set this when a configuration
+ *              warning is found, which turns on extra messages at the end of
+ *              processing. It should not be used directly and will be removed
+ *              from the public C API in a future release.
+ */
+extern gboolean crm_config_warning;
 
 enum xml_log_options
 {

include/crm/common/logging_internal.h
@@ -22,7 +22,7 @@ extern "C" {
  * \param[in] ...   Arguments for format string
  */
 #  define pcmk__config_err(fmt...) do {     \
-        pcmk__config_error = true;          \
+        crm_config_error = TRUE;            \
         crm_err(fmt);                       \
     } while (0)
 
@@ -34,7 +34,7 @@ extern "C" {
  * \param[in] ...   Arguments for format string
  */
 #  define pcmk__config_warn(fmt...) do {    \
-        pcmk__config_warning = true;        \
+        crm_config_warning = TRUE;          \
         crm_warn(fmt);                      \
     } while (0)
 

lib/common/utils.c
@@ -49,8 +49,8 @@
 
 CRM_TRACE_INIT_DATA(common);
 
-bool pcmk__config_error = false;
-bool pcmk__config_warning = false;
+gboolean crm_config_error = FALSE;
+gboolean crm_config_warning = FALSE;
 char *crm_system_name = NULL;
 
 int pcmk__score_red = 0;

lib/pacemaker/pcmk_sched_allocate.c
@@ -2933,7 +2933,7 @@ pcmk__log_transition_summary(const char *filename)
                    (filename == NULL)? "" : ", saving inputs in ",
                    (filename == NULL)? "" : filename);
     }
-    if (pcmk__config_error) {
+    if (crm_config_error) {
         crm_notice("Configuration errors found during scheduler processing,"
                    "  please run \"crm_verify -L\" to identify issues");
     }

tools/crm_verify.c
@@ -225,7 +225,7 @@ main(int argc, char **argv)
         cib_object = NULL;
 
     } else if (cli_config_update(&cib_object, NULL, FALSE) == FALSE) {
-        pcmk__config_error = true;
+        crm_config_error = TRUE;
         free_xml(cib_object);
         cib_object = NULL;
         out->err(out, "The cluster will NOT be able to use this configuration.\n"
@@ -254,7 +254,7 @@ main(int argc, char **argv)
     }
     pe_free_working_set(data_set);
 
-    if (pcmk__config_error) {
+    if (crm_config_error) {
         rc = pcmk_rc_schema_validation;
 
         if (args->verbosity > 0) {
@@ -265,7 +265,7 @@ main(int argc, char **argv)
                         "Errors found during check: config not valid\n-V may provide more details");
         }
 
-    } else if (pcmk__config_warning) {
+    } else if (crm_config_warning) {
         rc = pcmk_rc_schema_validation;
 
         if (args->verbosity > 0) {