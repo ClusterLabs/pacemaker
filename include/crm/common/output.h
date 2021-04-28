92a6299a0b819bf4b05a5d7de521d3d351cb7d89
Chris Lumens <clumens@redhat.com>
2021-04-28 13:52:36 -0400

    API: libcrmcommon: Add pcmk_show_opt_e.
    This enum is used to make minor modifications to sections of output.
    For instance, it can be used to indicate that inactive resources should
    be shown in addition to active resources.  This enum can take the place
    of pe_print_*.

--- include/crm/common/output.h
+++ include/crm/common/output.h
@@ -49,6 +49,23 @@ typedef enum {
                                      pcmk_section_operations | pcmk_section_fencing_all | pcmk_section_tickets | \
                                      pcmk_section_bans | pcmk_section_failures)
 
+/*!
+ * \brief Further modify the output of sections
+ */
+typedef enum {
+    pcmk_show_brief         = 1 << 0,
+    pcmk_show_clone_detail  = 1 << 1,
+    pcmk_show_node_id       = 1 << 2,
+    pcmk_show_implicit_rscs = 1 << 3,
+    pcmk_show_timing        = 1 << 4,
+    pcmk_show_inactive_rscs = 1 << 5,
+    pcmk_show_rscs_by_node  = 1 << 6,
+    pcmk_show_pending       = 1 << 7,
+    pcmk_show_rsc_only      = 1 << 8,
+} pcmk_show_opt_e;
+
+#define pcmk_show_details           (pcmk_show_clone_detail | pcmk_show_node_id | pcmk_show_implicit_rscs)
+
 #ifdef __cplusplus
 }
 #endif
