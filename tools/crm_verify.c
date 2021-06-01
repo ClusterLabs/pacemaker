ClusterLabs/pacemaker

commit ca94575116a02c1fc163687a0b46f47d61cefad9

Refactor: tools: Use glib for command line handling in crm_verify.

tools/crm_verify.c
@@ -9,6 +9,7 @@
 
 #include <crm_internal.h>
 #include <crm/crm.h>
+#include <crm/common/cmdline_internal.h>
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -27,6 +28,13 @@
 #include <crm/pengine/status.h>
 #include <pacemaker-internal.h>
 
+const char *SUMMARY = "Check a Pacemaker configuration for errors\n\n"
+                      "Check the well-formedness of a complete Pacemaker XML configuration,\n"
+                      "its conformance to the configured schema, and the presence of common\n"
+                      "misconfigurations. Problems reported as errors must be fixed before the\n"
+                      "cluster will work properly. It is left to the administrator to decide\n"
+                      "whether to fix problems reported as warnings.";
+
 struct {
     char *cib_save;
     gboolean use_live_cib;
@@ -37,159 +45,84 @@ struct {
 
 extern gboolean stage0(pe_working_set_t * data_set);
 
-static pcmk__cli_option_t long_options[] = {
-    // long option, argument type, storage, short option, description, flags
-    {
-        "help", no_argument, NULL, '?',
-        "\tThis text", pcmk__option_default
-    },
-    {
-        "version", no_argument, NULL, '$',
-        "\tVersion information", pcmk__option_default
-    },
-    {
-        "verbose", no_argument, NULL, 'V',
-        "\tSpecify multiple times to increase debug output\n",
-        pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nData sources:", pcmk__option_default
-    },
-    {
-        "live-check", no_argument, NULL, 'L',
-        "Check the configuration used by the running cluster\n",
-        pcmk__option_default
-    },
-    {
-        "xml-file", required_argument, NULL, 'x',
-        "Check the configuration in the named file", pcmk__option_default
-    },
-    {
-        "xml-text", required_argument, NULL, 'X',
-        "Check the configuration in the supplied string", pcmk__option_default
-    },
-    {
-        "xml-pipe", no_argument, NULL, 'p',
-        "Check the configuration piped in via stdin", pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nAdditional Options:", pcmk__option_default
-    },
-    {
-        "save-xml", required_argument, 0, 'S',
-        "Save verified XML to named file (most useful with -L)",
-        pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nExamples:", pcmk__option_paragraph
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "Check the consistency of the configuration in the running cluster:",
-        pcmk__option_paragraph
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        " crm_verify --live-check", pcmk__option_example
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "Check the consistency of the configuration in a given file and "
-            "produce verbose output:",
-        pcmk__option_paragraph
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        " crm_verify --xml-file file.xml --verbose", pcmk__option_example
-    },
-    {0, 0, 0, 0}
+static GOptionEntry data_entries[] = {
+    { "live-check", 'L', 0, G_OPTION_ARG_NONE,
+      &options.use_live_cib, "Check the configuration used by the running cluster",
+      NULL },
+    { "xml-file", 'x', 0, G_OPTION_ARG_FILENAME,
+      &options.xml_file, "Check the configuration in the named file",
+      "FILE" },
+    { "xml-pipe", 'p', 0, G_OPTION_ARG_NONE,
+      &options.xml_string, "Check the configuration piped in via stdin",
+      NULL },
+    { "xml-text", 'X', 0, G_OPTION_ARG_STRING,
+      &options.xml_string, "Check the configuration in the supplied string",
+      "XML" },
+
+    { NULL }
 };
+
+static GOptionEntry addl_entries[] = {
+    { "save-xml", 'S', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME,
+      &options.cib_save, "Save verified XML to named file (most useful with -L)",
+      "FILE" },
+
+    { NULL }
+};
+
+static GOptionContext *
+build_arg_context(pcmk__common_args_t *args) {
+    GOptionContext *context = NULL;
+
+    const char *description = "Examples:\n\n"
+                              "Check the consistency of the configuration in the running cluster:\n\n"
+                              "\tcrm_verify --live-check\n\n"
+                              "Check the consistency of the configuration in a given file and "
+                              "produce verbose output:\n\n"
+                              "\tcrm_verify --xml-file file.xml --verbose\n\n";
+
+    context = pcmk__build_arg_context(args, NULL, NULL, NULL);
+    g_option_context_set_description(context, description);
+
+    pcmk__add_arg_group(context, "data", "Data sources:",
+                        "Show data options", data_entries);
+    pcmk__add_arg_group(context, "additional", "Additional options:",
+                        "Show additional options", addl_entries);
+
+    return context;
+}
 
 int
 main(int argc, char **argv)
 {
     xmlNode *cib_object = NULL;
     xmlNode *status = NULL;
-    int argerr = 0;
-    int flag;
-    int option_index = 0;
 
     pe_working_set_t *data_set = NULL;
     cib_t *cib_conn = NULL;
+    const char *xml_tag = NULL;
+
     int rc = pcmk_rc_ok;
+    crm_exit_t exit_code = CRM_EX_OK;
 
     GError *error = NULL;
 
-    bool verbose = FALSE;
-    const char *xml_tag = NULL;
+    pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
+    gchar **processed_args = pcmk__cmdline_preproc(argv, "xSX");
+    GOptionContext *context = build_arg_context(args);
 
-    pcmk__cli_init_logging("crm_verify", 0);
-    pcmk__set_cli_options(NULL, "[options]", long_options,
-                          "check a Pacemaker configuration for errors\n\n"
-                          "Check the well-formedness of a complete Pacemaker "
-                          "XML configuration,\nits conformance to the "
-                          "configured schema, and the presence of common\n"
-                          "misconfigurations. Problems reported as errors "
-                          "must be fixed before the\ncluster will work "
-                          "properly. It is left to the administrator to decide"
-                          "\nwhether to fix problems reported as warnings.");
-
-    while (1) {
-        flag = pcmk__next_cli_option(argc, argv, &option_index, NULL);
-        if (flag == -1)
-            break;
-
-        switch (flag) {
-            case 'X':
-                crm_trace("Option %c => %s", flag, optarg);
-                options.xml_string = optarg;
-                break;
-            case 'x':
-                crm_trace("Option %c => %s", flag, optarg);
-                options.xml_file = optarg;
-                break;
-            case 'p':
-                options.xml_stdin = TRUE;
-                break;
-            case 'S':
-                options.cib_save = optarg;
-                break;
-            case 'V':
-                verbose = TRUE;
-                crm_bump_log_level(argc, argv);
-                break;
-            case 'L':
-                options.use_live_cib = TRUE;
-                break;
-            case '$':
-            case '?':
-                pcmk__cli_help(flag, CRM_EX_OK);
-                break;
-            default:
-                fprintf(stderr, "Option -%c is not yet supported\n", flag);
-                ++argerr;
-                break;
-        }
+    if (!g_option_context_parse_strv(context, &processed_args, &error)) {
+        exit_code = CRM_EX_USAGE;
+        goto done;
     }
 
-    if (optind < argc) {
-        printf("non-option ARGV-elements: ");
-        while (optind < argc) {
-            printf("%s ", argv[optind++]);
-        }
-        printf("\n");
-    }
+    pcmk__cli_init_logging("crm_verify", args->verbosity);
 
-    if (optind > argc) {
-        ++argerr;
-    }
-
-    if (argerr) {
-        crm_err("%d errors in option parsing", argerr);
-        pcmk__cli_help(flag, CRM_EX_USAGE);
+    if (args->version) {
+        g_strfreev(processed_args);
+        pcmk__free_arg_context(context);
+        /* FIXME:  When crm_verify is converted to use formatted output, this can go. */
+        pcmk__cli_help('v', CRM_EX_USAGE);
     }
 
     crm_info("=#=#=#=#= Getting XML =#=#=#=#=");
@@ -303,7 +236,7 @@ main(int argc, char **argv)
     if (pcmk__config_error) {
         rc = pcmk_rc_schema_validation;
 
-        if (verbose) {
+        if (args->verbosity > 0) {
             g_set_error(&error, PCMK__RC_ERROR, rc,
                         "Errors found during check: config not valid");
         } else {
@@ -314,7 +247,7 @@ main(int argc, char **argv)
     } else if (pcmk__config_warning) {
         rc = pcmk_rc_schema_validation;
 
-        if (verbose) {
+        if (args->verbosity > 0) {
             g_set_error(&error, PCMK__RC_ERROR, rc,
                         "Warnings found during check: config may not be valid");
         } else {
@@ -329,14 +262,17 @@ main(int argc, char **argv)
     }
 
   done:
-    if (error != NULL) {
-        fprintf(stderr, "%s: %s\n", g_get_prgname(), error->message);
-        g_clear_error(&error);
-    }
-
+    g_strfreev(processed_args);
+    pcmk__free_arg_context(context);
     free(options.cib_save);
     free(options.xml_file);
     free(options.xml_string);
 
-    crm_exit(pcmk_rc2exitc(rc));
+    pcmk__output_and_clear_error(error, NULL);
+
+    if (exit_code != CRM_EX_OK) {
+        crm_exit(exit_code);
+    } else {
+        crm_exit(pcmk_rc2exitc(rc));
+    }
 }

tools/Makefile.am
@@ -31,6 +31,7 @@ EXTRA_DIST		= crm_diff.8.inc \
 			  crm_resource.8.inc \
 			  crm_rule.8.inc \
 			  crm_simulate.8.inc \
+			  crm_verify.8.inc \
 			  crmadmin.8.inc \
 			  fix-manpages \
 			  stonith_admin.8.inc

tools/crm_verify.8.inc
@@ -1,0 +1,5 @@
+[=synopsis]
+crm_verify [<options>]
+
+/reported as warnings./
+.SH OPTIONS

commit 6e9d75ad9efa73875329bcdf09b750622a9bf87c

Fix: tools: get `crm_verify --xml-pipe/-p` working again
Broke with ca94575116.

It produces with a command like:

```
 # cibadmin -Q | crm_verify -p
 Segmentation fault (core dumped)
```

tools/crm_verify.c
@@ -54,7 +54,7 @@ static GOptionEntry data_entries[] = {
       &options.xml_file, "Check the configuration in the named file",
       "FILE" },
     { "xml-pipe", 'p', 0, G_OPTION_ARG_NONE,
-      &options.xml_string, "Check the configuration piped in via stdin",
+      &options.xml_stdin, "Check the configuration piped in via stdin",
       NULL },
     { "xml-text", 'X', 0, G_OPTION_ARG_STRING,
       &options.xml_string, "Check the configuration in the supplied string",