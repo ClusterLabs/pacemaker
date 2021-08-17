1a13fcc8cb6907c70897d9083dc72ce7f49aa218
Ken Gaillot <kgaillot@redhat.com>
2021-08-17 09:50:34 -0500

    Refactor: libpacemaker: separate graph logging functions into own source file
    ... to organize things better. No code is changed, other than to make one
    function static that could be.

--- include/pcmki/pcmki_transition.h
+++ include/pcmki/pcmki_transition.h
@@ -132,7 +132,6 @@ void print_graph(unsigned int log_level, crm_graph_t * graph);
 void print_action(int log_level, const char *prefix, crm_action_t * action);
 bool update_abort_priority(crm_graph_t * graph, int priority,
                            enum transition_action action, const char *abort_reason);
-const char *actiontype2text(action_type_e type);
 lrmd_event_data_t *convert_graph_action(xmlNode * resource, crm_action_t * action, int status,
                                         int rc);
 
