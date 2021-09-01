5941821e557136812ff22b4ecb2604fd2045680e
Chris Lumens <clumens@redhat.com>
2021-09-01 14:53:05 -0400

    Test: libcrmcommon: Add some very simple unit tests with mocked functions.
    The functions I'm testing here really don't do very much, which makes
    them good candidates to test out how the mocked up function stuff works.
    
    On the build side, each new test case needs to:
    
    * Link against libcrmcommon_test.la instead of libcrmcommon.la.
    * Pass the -Wl,--wrap= flag for each wrapped function that it uses.
    
    The new test cases are largely boilerplate, but that's just how unit
    tests go.  Each unit test:
    
    * Defines a __wrap_X function that takes the place of some library
      function.  The body of these functions is very simple, basically just
      returning a previously defined value with mock_type.  Remember that we
      are not testing the library function.  We are testing how a Pacemaker
      function responds to the library function.
    
    * Defines one more more test functions.  These are just like regular
      test functions, except we also need to tell __wrap_X what to return.
      A call to will_return enqueues a value that some future call of
      mock_type will return.  These need to be paired, basically.  And then
      we just call our Pacemaker function and check the result.

---
+++ lib/common/tests/utils/pcmk_hostname_test.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+#include "mock_private.h"
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include <sys/utsname.h>
+
+int
+__wrap_uname(struct utsname *buf)
+{
+    int retval = mock_type(int);
+
+    if (retval == 0) {
+        strcpy(buf->nodename, mock_type(char *));
+    }
+
+    return retval;
+}
+
+static void
+uname_succeeded_test(void **state)
+{
+    char *retval;
+
+    will_return(__wrap_uname, 0);                       // uname() return value
+    will_return(__wrap_uname, "somename");              // uname() buf->nodename
+
+    retval = pcmk_hostname();
+    assert_non_null(retval);
+    assert_string_equal("somename", retval);
+
+    free(retval);
+}
+
+static void
+uname_failed_test(void **state)
+{
+    char *retval;
+
+    will_return(__wrap_uname, -1);                      // uname() return value
+
+    retval = pcmk_hostname();
+    assert_null(retval);
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(uname_succeeded_test),
+        cmocka_unit_test(uname_failed_test),
+    };
+
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
