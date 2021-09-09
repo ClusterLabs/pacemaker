ClusterLabs/pacemaker

commit 71dd4924d2cb5053db1bd46bd863fd2da783b038

Refactor: fencer: functionize getting delegate from XML
This will make it easier to muck with return codes.

daemons/fenced/fenced_remote.c
@@ -457,6 +457,18 @@ handle_duplicates(remote_fencing_op_t * op, xmlNode * data, int rc)
     }
 }
 
+static char *
+delegate_from_xml(xmlNode *xml)
+{
+    xmlNode *match = get_xpath_object("//@" F_STONITH_DELEGATE, xml, LOG_NEVER);
+
+    if (match == NULL) {
+        return crm_element_value_copy(xml, F_ORIG);
+    } else {
+        return crm_element_value_copy(match, F_STONITH_DELEGATE);
+    }
+}
+
 /*!
  * \internal
  * \brief Finalize a remote operation.
@@ -504,13 +516,14 @@ remote_op_done(remote_fencing_op_t * op, xmlNode * data, int rc, int dup)
         goto remote_op_done_cleanup;
     }
 
-    if (!op->delegate && data && rc != -ENODEV && rc != -EHOSTUNREACH) {
-        xmlNode *ndata = get_xpath_object("//@" F_STONITH_DELEGATE, data,
-                                          LOG_NEVER);
-        if(ndata) {
-            op->delegate = crm_element_value_copy(ndata, F_STONITH_DELEGATE);
-        } else { 
-            op->delegate = crm_element_value_copy(data, F_ORIG);
+    if (op->delegate == NULL) {
+        switch (rc) {
+            case -ENODEV:
+            case -EHOSTUNREACH:
+                break;
+            default:
+                op->delegate = delegate_from_xml(data);
+                break;
         }
     }
 

commit 437b074cfb73e79376090b10e42e7503010e6e11

Low: fencer: avoid assertions in delegate_from_xml()
71dd4924 mistakenly dropped a (data != NULL) condition. The behavior ends up
the same, but only after a couple of assertions.

daemons/fenced/fenced_remote.c
@@ -516,7 +516,11 @@ remote_op_done(remote_fencing_op_t * op, xmlNode * data, int rc, int dup)
         goto remote_op_done_cleanup;
     }
 
-    if (op->delegate == NULL) {
+    if (data == NULL) {
+        data = create_xml_node(NULL, "remote-op");
+        local_data = data;
+
+    } else if (op->delegate == NULL) {
         switch (rc) {
             case -ENODEV:
             case -EHOSTUNREACH:
@@ -525,11 +529,6 @@ remote_op_done(remote_fencing_op_t * op, xmlNode * data, int rc, int dup)
                 op->delegate = delegate_from_xml(data);
                 break;
         }
-    }
-
-    if (data == NULL) {
-        data = create_xml_node(NULL, "remote-op");
-        local_data = data;
     }
 
     if(dup) {