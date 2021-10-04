c10dafc8557ffeb4cf24d8938b46ca00967701f9
Chris Lumens <clumens@redhat.com>
2021-10-04 14:13:33 -0400

    Refactor: daemons: Use glib for command line handling in pacemaker-schedulerd.

--- daemons/schedulerd/pacemaker-schedulerd.c
+++ daemons/schedulerd/pacemaker-schedulerd.c
@@ -22,13 +22,18 @@
 
 #include <libxml/parser.h>
 
+#include <crm/common/cmdline_internal.h>
 #include <crm/common/ipc_internal.h>
 #include <crm/common/mainloop.h>
 #include <crm/pengine/internal.h>
 #include <pacemaker-internal.h>
 #include <crm/msg_xml.h>
 
-#define OPTARGS	"hVc"
+#define SUMMARY "pacemaker-schedulerd - daemon for calculating a Pacemaker cluster's response to events"
+
+struct {
+    gchar **remainder;
+} options;
 
 static GMainLoop *mainloop = NULL;
 static qb_ipcs_service_t *ipcs = NULL;
@@ -296,68 +301,64 @@ struct qb_ipcs_service_handlers ipc_callbacks = {
     .connection_destroyed = pe_ipc_destroy
 };
 
-static pcmk__cli_option_t long_options[] = {
-    // long option, argument type, storage, short option, description, flags
-    {
-        "help", no_argument, NULL, '?',
-        "\tThis text", pcmk__option_default
-    },
-    {
-        "verbose", no_argument, NULL, 'V',
-        "\tIncrease debug output", pcmk__option_default
-    },
-    { 0, 0, 0, 0 }
-};
+static GOptionContext *
+build_arg_context(pcmk__common_args_t *args) {
+    GOptionContext *context = NULL;
+
+    GOptionEntry extra_prog_entries[] = {
+        { G_OPTION_REMAINING, 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING_ARRAY, &options.remainder,
+          NULL,
+          NULL },
+
+        { NULL }
+    };
+
+    context = pcmk__build_arg_context(args, NULL, NULL, NULL);
+    pcmk__add_main_args(context, extra_prog_entries);
+    return context;
+}
 
 int
 main(int argc, char **argv)
 {
-    int flag;
-    int index = 0;
-    int argerr = 0;
-
     int rc = pcmk_rc_ok;
 
     GError *error = NULL;
 
-    crm_log_preinit(NULL, argc, argv);
-    pcmk__set_cli_options(NULL, "[options]", long_options,
-                          "daemon for calculating a Pacemaker cluster's "
-                          "response to events");
+    pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
+    gchar **processed_args = pcmk__cmdline_preproc(argv, NULL);
+    GOptionContext *context = build_arg_context(args);
 
+    crm_log_preinit(NULL, argc, argv);
     mainloop_add_signal(SIGTERM, pengine_shutdown);
 
-    while (1) {
-        flag = pcmk__next_cli_option(argc, argv, &index, NULL);
-        if (flag == -1)
-            break;
-
-        switch (flag) {
-            case 'V':
-                crm_bump_log_level(argc, argv);
-                break;
-            case 'h':          /* Help message */
-                pcmk__cli_help('?', CRM_EX_OK);
-                break;
-            default:
-                ++argerr;
-                break;
-        }
-    }
-
-    if (argc - optind == 1 && pcmk__str_eq("metadata", argv[optind], pcmk__str_casei)) {
-        pe_metadata();
+    if (!g_option_context_parse_strv(context, &processed_args, &error)) {
+        exit_code = CRM_EX_USAGE;
         goto done;
     }
 
-    if (optind > argc) {
-        ++argerr;
+    if (options.remainder) {
+        if (g_strv_length(options.remainder) == 1 &&
+            pcmk__str_eq("metadata", options.remainder[0], pcmk__str_casei)) {
+            pe_metadata();
+            goto done;
+        } else {
+            exit_code = CRM_EX_USAGE;
+            g_set_error(&error, PCMK__EXITC_ERROR, exit_code,
+                        "Unsupported extra command line parameters");
+            goto done;
+        }
     }
 
-    if (argerr) {
-        pcmk__cli_help('?', CRM_EX_USAGE);
+    if (args->version) {
+        g_strfreev(options.remainder);
+        g_strfreev(processed_args);
+        pcmk__free_arg_context(context);
+        /* FIXME:  When pacemaker-schedulerd is converted to use formatted output, this can go. */
+        pcmk__cli_help('v', CRM_EX_USAGE);
     }
 
+    pcmk__cli_init_logging("pacemaker-schedulerd", args->verbosity);
     crm_log_init(NULL, LOG_INFO, TRUE, FALSE, argc, argv, FALSE);
     crm_notice("Starting Pacemaker scheduler");
 
@@ -396,6 +397,10 @@ main(int argc, char **argv)
     g_main_loop_run(mainloop);
 
 done:
+    g_strfreev(options.remainder);
+    g_strfreev(processed_args);
+    pcmk__free_arg_context(context);
+
     pcmk__output_and_clear_error(error, NULL);
     pengine_shutdown(0);
 }
