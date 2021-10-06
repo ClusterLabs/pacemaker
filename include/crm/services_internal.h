82c31ef4495a1470e2e9b92d581c92a46ce7ea3d
Ken Gaillot <kgaillot@redhat.com>
2021-10-06 16:25:44 -0500

    Refactor: libcrmservice: add internal APIs to "steal" stdout/stderr
    These will come in handy when we don't want to duplicate memory

--- include/crm/services_internal.h
+++ include/crm/services_internal.h
@@ -40,6 +40,8 @@ svc_action_t *services__create_resource_action(const char *name, const char *sta
                                       enum svc_action_flags flags);
 
 const char *services__exit_reason(svc_action_t *action);
+char *services__grab_stdout(svc_action_t *action);
+char *services__grab_stderr(svc_action_t *action);
 
 void services__set_result(svc_action_t *action, int agent_status,
                           enum pcmk_exec_status exec_status,
