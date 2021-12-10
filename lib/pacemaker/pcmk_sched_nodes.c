4cf7d64f694a5699ec913970a1b3984b498f40ef
Ken Gaillot <kgaillot@redhat.com>
2021-12-10 16:55:34 -0600

    Refactor: scheduler: best practices when comparing nodes
    Rename sort_node_weight() to compare_nodes() to better reflect its purpose,
    and improve comments and formatting.

--- lib/pacemaker/pcmk_sched_nodes.c
+++ lib/pacemaker/pcmk_sched_nodes.c
@@ -94,15 +94,25 @@ struct node_weight_s {
     pe_working_set_t *data_set;
 };
 
-/* return -1 if 'a' is more preferred
- * return  1 if 'b' is more preferred
+/*!
+ * \internal
+ * \brief Compare two nodes for allocation desirability
+ *
+ * Given two nodes, check which one is more preferred by allocation criteria
+ * such as node weight and utilization.
+ *
+ * \param[in] a     First node to compare
+ * \param[in] b     Second node to compare
+ * \param[in] data  Sort data (as struct node_weight_s *)
+ *
+ * \return -1 if \p a is preferred, +1 if \p b is preferred, or 0 if they are
+ *         equally preferred
  */
-
 static gint
-sort_node_weight(gconstpointer a, gconstpointer b, gpointer data)
+compare_nodes(gconstpointer a, gconstpointer b, gpointer data)
 {
-    const pe_node_t *node1 = (const pe_node_t *)a;
-    const pe_node_t *node2 = (const pe_node_t *)b;
+    const pe_node_t *node1 = (const pe_node_t *) a;
+    const pe_node_t *node2 = (const pe_node_t *) b;
     struct node_weight_s *nw = data;
 
     int node1_weight = 0;
@@ -117,36 +127,38 @@ sort_node_weight(gconstpointer a, gconstpointer b, gpointer data)
         return -1;
     }
 
-    node1_weight = node1->weight;
-    node2_weight = node2->weight;
+    // Compare node weights
 
-    if (!pcmk__node_available(node1)) {
-        node1_weight = -INFINITY;
-    }
-    if (!pcmk__node_available(node2)) {
-        node2_weight = -INFINITY;
-    }
+    node1_weight = pcmk__node_available(node1)? node1->weight : -INFINITY;
+    node2_weight = pcmk__node_available(node2)? node2->weight : -INFINITY;
 
     if (node1_weight > node2_weight) {
         crm_trace("%s (%d) > %s (%d) : weight",
-                  node1->details->uname, node1_weight, node2->details->uname, node2_weight);
+                  node1->details->uname, node1_weight, node2->details->uname,
+                  node2_weight);
         return -1;
     }
 
     if (node1_weight < node2_weight) {
         crm_trace("%s (%d) < %s (%d) : weight",
-                  node1->details->uname, node1_weight, node2->details->uname, node2_weight);
+                  node1->details->uname, node1_weight, node2->details->uname,
+                  node2_weight);
         return 1;
     }
 
     crm_trace("%s (%d) == %s (%d) : weight",
-              node1->details->uname, node1_weight, node2->details->uname, node2_weight);
+              node1->details->uname, node1_weight, node2->details->uname,
+              node2_weight);
 
-    if (pcmk__str_eq(nw->data_set->placement_strategy, "minimal", pcmk__str_casei)) {
+    // If appropriate, compare node utilization
+
+    if (pcmk__str_eq(nw->data_set->placement_strategy, "minimal",
+                     pcmk__str_casei)) {
         goto equal;
     }
 
-    if (pcmk__str_eq(nw->data_set->placement_strategy, "balanced", pcmk__str_casei)) {
+    if (pcmk__str_eq(nw->data_set->placement_strategy, "balanced",
+                     pcmk__str_casei)) {
         result = compare_capacity(node1, node2);
         if (result < 0) {
             crm_trace("%s > %s : capacity (%d)",
@@ -159,7 +171,8 @@ sort_node_weight(gconstpointer a, gconstpointer b, gpointer data)
         }
     }
 
-    /* now try to balance resources across the cluster */
+    // Compare number of allocated resources
+
     if (node1->details->num_resources < node2->details->num_resources) {
         crm_trace("%s (%d) > %s (%d) : resources",
                   node1->details->uname, node1->details->num_resources,
@@ -173,18 +186,24 @@ sort_node_weight(gconstpointer a, gconstpointer b, gpointer data)
         return 1;
     }
 
-    if (nw->active && nw->active->details == node1->details) {
-        crm_trace("%s (%d) > %s (%d) : active",
-                  node1->details->uname, node1->details->num_resources,
-                  node2->details->uname, node2->details->num_resources);
-        return -1;
-    } else if (nw->active && nw->active->details == node2->details) {
-        crm_trace("%s (%d) < %s (%d) : active",
-                  node1->details->uname, node1->details->num_resources,
-                  node2->details->uname, node2->details->num_resources);
-        return 1;
+    // Check whether one node is already running desired resource
+
+    if (nw->active != NULL) {
+        if (nw->active->details == node1->details) {
+            crm_trace("%s (%d) > %s (%d) : active",
+                      node1->details->uname, node1->details->num_resources,
+                      node2->details->uname, node2->details->num_resources);
+            return -1;
+        } else if (nw->active->details == node2->details) {
+            crm_trace("%s (%d) < %s (%d) : active",
+                      node1->details->uname, node1->details->num_resources,
+                      node2->details->uname, node2->details->num_resources);
+            return 1;
+        }
     }
-  equal:
+
+    // If all else is equal, prefer node with lowest-sorting name
+equal:
     crm_trace("%s = %s", node1->details->uname, node2->details->uname);
     return strcmp(node1->details->uname, node2->details->uname);
 }
@@ -195,7 +214,7 @@ sort_nodes_by_weight(GList *nodes, pe_node_t *active_node,
 {
     struct node_weight_s nw = { active_node, data_set };
 
-    return g_list_sort_with_data(nodes, sort_node_weight, &nw);
+    return g_list_sort_with_data(nodes, compare_nodes, &nw);
 }
 
 gboolean
