ClusterLabs/pacemaker

commit f52bc8e1ce3257dc75d9046fc77029e4b83dc5da

Mid: fenced: Added nanosecond field to operation completion data.

daemons/fenced/fenced_commands.c
@@ -2608,6 +2608,21 @@ bool fencing_peer_active(crm_node_t *peer)
     return FALSE;
 }
 
+void set_fencing_completed(remote_fencing_op_t * op)
+{
+#ifdef CLOCK_MONOTONIC
+    struct timespec tv;
+
+    clock_gettime(CLOCK_MONOTONIC, &tv);
+
+    op->completed = tv.tv_sec;
+    op->completed_nsec = tv.tv_nsec;
+#else
+    op->completed = time(NULL);
+    op->completed_nsec = 0L;
+#endif
+}
+
 /*!
  * \internal
  * \brief Determine if we need to use an alternate node to

daemons/fenced/fenced_history.c
@@ -149,7 +149,11 @@ op_time_sort(const void *a_voidp, const void *b_voidp)
     } else if (b_pending) {
         return 1;
     } else if ((*b)->completed == (*a)->completed) {
-        return 0;
+        if ((*b)->completed_nsec > (*a)->completed_nsec) {
+            return 1;
+        } else if ((*b)->completed_nsec == (*a)->completed_nsec) {
+            return 0;
+        }
     } else if ((*b)->completed > (*a)->completed) {
         return 1;
     }
@@ -230,6 +234,7 @@ stonith_xml_history_to_list(xmlNode *history)
         char *id = crm_element_value_copy(xml_op, F_STONITH_REMOTE_OP_ID);
         int state;
         long long completed;
+        long long completed_nsec = 0L;
 
         if (!id) {
             crm_warn("Malformed fencing history received from peer");
@@ -248,6 +253,8 @@ stonith_xml_history_to_list(xmlNode *history)
         op->client_name = crm_element_value_copy(xml_op, F_STONITH_CLIENTNAME);
         crm_element_value_ll(xml_op, F_STONITH_DATE, &completed);
         op->completed = (time_t) completed;
+        crm_element_value_ll(xml_op, F_STONITH_DATE_NSEC, &completed_nsec);
+        op->completed_nsec = completed_nsec;
         crm_element_value_int(xml_op, F_STONITH_STATE, &state);
         op->state = (enum op_state) state;
 
@@ -346,6 +353,7 @@ stonith_local_history_diff_and_merge(GHashTable *remote_history,
                 crm_xml_add(entry, F_STONITH_DELEGATE, op->delegate);
                 crm_xml_add(entry, F_STONITH_CLIENTNAME, op->client_name);
                 crm_xml_add_ll(entry, F_STONITH_DATE, op->completed);
+                crm_xml_add_ll(entry, F_STONITH_DATE_NSEC, op->completed_nsec);
                 crm_xml_add_int(entry, F_STONITH_STATE, op->state);
             }
     }
@@ -363,7 +371,7 @@ stonith_local_history_diff_and_merge(GHashTable *remote_history,
                 crm_warn("Failing pending operation %.8s originated by us but "
                          "known only from peer history", op->id);
                 op->state = st_failed;
-                op->completed = time(NULL);
+                set_fencing_completed(op);
                 /* use -EHOSTUNREACH to not introduce a new return-code that might
                    trigger unexpected results at other places and to prevent
                    remote_op_done from setting the delegate if not present

daemons/fenced/fenced_remote.c
@@ -490,7 +490,7 @@ remote_op_done(remote_fencing_op_t * op, xmlNode * data, int rc, int dup)
     xmlNode *local_data = NULL;
     gboolean op_merged = FALSE;
 
-    op->completed = time(NULL);
+    set_fencing_completed(op);
     clear_remote_op_timers(op);
     undo_op_remap(op);
 
@@ -976,7 +976,7 @@ stonith_manual_ack(xmlNode * msg, remote_fencing_op_t * op)
     xmlNode *dev = get_xpath_object("//@" F_STONITH_TARGET, msg, LOG_ERR);
 
     op->state = st_done;
-    op->completed = time(NULL);
+    set_fencing_completed(op);
     op->delegate = strdup("a human");
 
     crm_notice("Injecting manual confirmation that %s is safely off/down",

daemons/fenced/pacemaker-fenced.h
@@ -155,6 +155,9 @@ typedef struct remote_fencing_op_s {
      * completes, the duplicate operations will be closed out as well. */
     GList *duplicates;
 
+    /*! The point at which the remote operation completed(nsec) */
+    long long completed_nsec;
+
 } remote_fencing_op_t;
 
 /*!
@@ -257,6 +260,8 @@ int stonith_fence_history(xmlNode *msg, xmlNode **output,
 void stonith_fence_history_trim(void);
 
 bool fencing_peer_active(crm_node_t *peer);
+
+void set_fencing_completed(remote_fencing_op_t * op);
 
 int stonith_manual_ack(xmlNode * msg, remote_fencing_op_t * op);
 

include/crm/fencing/internal.h
@@ -135,6 +135,7 @@ void stonith__device_parameter_flags(uint32_t *device_flags,
 #  define F_STONITH_ORIGIN        "st_origin"
 #  define F_STONITH_HISTORY_LIST  "st_history"
 #  define F_STONITH_DATE          "st_date"
+#  define F_STONITH_DATE_NSEC     "st_date_nsec"
 #  define F_STONITH_STATE         "st_state"
 #  define F_STONITH_ACTIVE        "st_active"
 #  define F_STONITH_DIFFERENTIAL  "st_differential"

include/crm/stonith-ng.h
@@ -110,6 +110,7 @@ typedef struct stonith_history_s {
     int state;
     time_t completed;
     struct stonith_history_s *next;
+    long completed_nsec;
 } stonith_history_t;
 
 typedef struct stonith_s stonith_t;

lib/fencing/st_client.c
@@ -1190,6 +1190,7 @@ stonith_api_history(stonith_t * stonith, int call_options, const char *node,
              op = pcmk__xml_next(op)) {
             stonith_history_t *kvp;
             long long completed;
+            long long completed_nsec = 0L;
 
             kvp = calloc(1, sizeof(stonith_history_t));
             kvp->target = crm_element_value_copy(op, F_STONITH_TARGET);
@@ -1199,6 +1200,8 @@ stonith_api_history(stonith_t * stonith, int call_options, const char *node,
             kvp->client = crm_element_value_copy(op, F_STONITH_CLIENTNAME);
             crm_element_value_ll(op, F_STONITH_DATE, &completed);
             kvp->completed = (time_t) completed;
+            crm_element_value_ll(op, F_STONITH_DATE_NSEC, &completed_nsec);
+            kvp->completed_nsec = completed_nsec;
             crm_element_value_int(op, F_STONITH_STATE, &kvp->state);
 
             if (last) {
@@ -2599,7 +2602,8 @@ stonith__later_succeeded(stonith_history_t *event, stonith_history_t *top_histor
             pcmk__str_eq(event->target, prev_hp->target, pcmk__str_casei) &&
             pcmk__str_eq(event->action, prev_hp->action, pcmk__str_casei) &&
             pcmk__str_eq(event->delegate, prev_hp->delegate, pcmk__str_casei) &&
-            (event->completed < prev_hp->completed)) {
+            ((event->completed < prev_hp->completed) || 
+             ((event->completed == prev_hp->completed) && (event->completed_nsec < prev_hp->completed_nsec)))) {
             ret = TRUE;
             break;
         }
@@ -2625,13 +2629,15 @@ stonith__sort_history(stonith_history_t *history)
         tmp = hp->next;
         if ((hp->state == st_done) || (hp->state == st_failed)) {
             /* sort into new */
-            if ((!new) || (hp->completed > new->completed)) {
+            if ((!new) || (hp->completed > new->completed) || 
+                ((hp->completed == new->completed) && (hp->completed_nsec > new->completed_nsec))) {
                 hp->next = new;
                 new = hp;
             } else {
                 np = new;
                 do {
-                    if ((!np->next) || (hp->completed > np->next->completed)) {
+                    if ((!np->next) || (hp->completed > np->next->completed) ||
+                        ((hp->completed == np->next->completed) && (hp->completed_nsec > np->next->completed_nsec))) {
                         hp->next = np->next;
                         np->next = hp;
                         break;

commit 0339e89f3238b31df78b864dae8684b82c370741

Fix: fencer: get current time correctly
f52bc8e1ce (2.1.2) introduced a regression by using clock_gettime() with
CLOCK_MONOTONIC to get the current time. Use qb_util_timespec_from_epoch_get()
instead (which as of this writing uses clock_gettime() with CLOCK_REALTIME if
available, and falls back to gettimeofday() if not).

daemons/fenced/fenced_commands.c
@@ -2746,19 +2746,14 @@ bool fencing_peer_active(crm_node_t *peer)
     return FALSE;
 }
 
-void set_fencing_completed(remote_fencing_op_t * op)
+void
+set_fencing_completed(remote_fencing_op_t *op)
 {
-#ifdef CLOCK_MONOTONIC
     struct timespec tv;
 
-    clock_gettime(CLOCK_MONOTONIC, &tv);
-
+    qb_util_timespec_from_epoch_get(&tv);
     op->completed = tv.tv_sec;
     op->completed_nsec = tv.tv_nsec;
-#else
-    op->completed = time(NULL);
-    op->completed_nsec = 0L;
-#endif
 }
 
 /*!