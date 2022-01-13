ClusterLabs/pacemaker

commit 31c7fa8a3a9c72c05bafdac1841c1c0c5f003797

Refactor: controller,liblrmd,libpacemaker: functionize setting event result
lrmd_event_data_t is public API, so we can't refactor it to use
pcmk__action_result_t at this time.

However, we can create equivalent accessor functions for the result members.
This encourages setting agent exit status, execution status, and exit reason
together whenever possible, and will make it easier to refactor to use
pcmk__action_result_t in the future.

This is purely a refactor; no additional exit reasons are set as of this
commit.

lib/lrmd/lrmd_client.c
@@ -248,9 +248,8 @@ lrmd_free_event(lrmd_event_data_t *event)
     free((void *) event->rsc_id);
     free((void *) event->op_type);
     free((void *) event->user_data);
-    free((void *) event->output);
-    free((void *) event->exit_reason);
     free((void *) event->remote_nodename);
+    lrmd__reset_result(event);
     if (event->params != NULL) {
         g_hash_table_destroy(event->params);
     }
@@ -305,9 +304,11 @@ lrmd_dispatch_internal(lrmd_t * lrmd, xmlNode * msg)
 
         event.op_type = crm_element_value(msg, F_LRMD_RSC_ACTION);
         event.user_data = crm_element_value(msg, F_LRMD_RSC_USERDATA_STR);
+        event.type = lrmd_event_exec_complete;
+
+        // No need to duplicate the memory, so don't use setter functions
         event.output = crm_element_value(msg, F_LRMD_RSC_OUTPUT);
         event.exit_reason = crm_element_value(msg, F_LRMD_RSC_EXIT_REASON);
-        event.type = lrmd_event_exec_complete;
 
         event.params = xml2list(msg);
     } else if (pcmk__str_eq(type, LRMD_OP_NEW_CLIENT, pcmk__str_none)) {
@@ -2344,4 +2345,50 @@ lrmd_api_delete(lrmd_t * lrmd)
 
     free(lrmd->lrmd_private);
     free(lrmd);
+}
+
+/*!
+ * \internal
+ * \brief Set the result of an executor event
+ *
+ * \param[in,out] event        Executor event to set
+ * \param[in]     rc           OCF exit status of event
+ * \param[in]     op_status    Executor status of event
+ * \param[in]     exit_reason  Human-friendly description of event
+ */
+void
+lrmd__set_result(lrmd_event_data_t *event, enum ocf_exitcode rc, int op_status,
+                 const char *exit_reason)
+{
+    if (event == NULL) {
+        return;
+    }
+
+    event->rc = rc;
+    event->op_status = op_status;
+
+    if (!pcmk__str_eq(event->exit_reason, exit_reason, pcmk__str_none)) {
+        free((void *) event->exit_reason);
+        event->exit_reason = (exit_reason == NULL)? NULL : strdup(exit_reason);
+    }
+}
+
+/*!
+ * \internal
+ * \brief Clear an executor event's exit reason, output, and error output
+ *
+ * \param[in] event  Executor event to reset
+ */
+void
+lrmd__reset_result(lrmd_event_data_t *event)
+{
+    if (event == NULL) {
+        return;
+    }
+
+    free((void *) event->exit_reason);
+    event->exit_reason = NULL;
+
+    free((void *) event->output);
+    event->output = NULL;
 }

daemons/controld/controld_execd.c
@@ -20,6 +20,7 @@
 #include <crm/msg_xml.h>
 #include <crm/common/xml.h>
 #include <crm/pengine/rules.h>
+#include <crm/lrmd_internal.h>
 
 #include <pacemaker-internal.h>
 #include <pacemaker-controld.h>
@@ -273,8 +274,7 @@ send_task_ok_ack(lrm_state_t *lrm_state, ha_msg_input_t *input,
 {
     lrmd_event_data_t *op = construct_op(lrm_state, input->xml, rsc_id, task);
 
-    op->rc = PCMK_OCF_OK;
-    op->op_status = PCMK_EXEC_DONE;
+    lrmd__set_result(op, PCMK_OCF_OK, PCMK_EXEC_DONE, NULL);
     controld_ack_event_directly(ack_host, ack_sys, rsc, op, rsc_id);
     lrmd_free_event(op);
 }
@@ -868,16 +868,15 @@ controld_rc2event(lrmd_event_data_t *event, int rc)
 {
     switch (rc) {
         case pcmk_rc_ok:
-            event->rc = PCMK_OCF_OK;
-            event->op_status = PCMK_EXEC_DONE;
+            lrmd__set_result(event, PCMK_OCF_OK, PCMK_EXEC_DONE, NULL);
             break;
         case EACCES:
-            event->rc = PCMK_OCF_INSUFFICIENT_PRIV;
-            event->op_status = PCMK_EXEC_ERROR;
+            lrmd__set_result(event, PCMK_OCF_INSUFFICIENT_PRIV,
+                             PCMK_EXEC_ERROR, NULL);
             break;
         default:
-            event->rc = PCMK_OCF_UNKNOWN_ERROR;
-            event->op_status = PCMK_EXEC_ERROR;
+            lrmd__set_result(event, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_ERROR,
+                             NULL);
             break;
     }
 }
@@ -1384,8 +1383,7 @@ fake_op_status(lrm_state_t *lrm_state, lrmd_event_data_t *op, int op_status,
     op->call_id = get_fake_call_id(lrm_state, op->rsc_id);
     op->t_run = time(NULL);
     op->t_rcchange = op->t_run;
-    op->op_status = op_status;
-    op->rc = op_exitcode;
+    lrmd__set_result(op, op_exitcode, op_status, NULL);
 }
 
 static void
@@ -1536,10 +1534,10 @@ fail_lrm_resource(xmlNode *xml, lrm_state_t *lrm_state, const char *user_name,
 
     if (get_lrm_resource(lrm_state, xml_rsc, TRUE, &rsc) == pcmk_ok) {
         crm_info("Failing resource %s...", rsc->id);
-        op->exit_reason = strdup("Simulated failure");
+        lrmd__set_result(op, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_DONE,
+                         "Simulated failure");
         process_lrm_event(lrm_state, op, NULL, xml);
-        op->op_status = PCMK_EXEC_DONE;
-        op->rc = PCMK_OCF_OK;
+        op->rc = PCMK_OCF_OK; // The request to fail the resource succeeded
         lrmd_free_rsc_info(rsc);
 
     } else {
@@ -1709,12 +1707,13 @@ do_lrm_delete(ha_msg_input_t *input, lrm_state_t *lrm_state,
         lrmd_event_data_t *op = NULL;
 
         op = construct_op(lrm_state, input->xml, rsc->id, CRMD_ACTION_DELETE);
-        op->op_status = PCMK_EXEC_ERROR;
 
         if (cib_rc == EACCES) {
-            op->rc = PCMK_OCF_INSUFFICIENT_PRIV;
+            lrmd__set_result(op, PCMK_OCF_INSUFFICIENT_PRIV, PCMK_EXEC_ERROR,
+                             NULL);
         } else {
-            op->rc = PCMK_OCF_UNKNOWN_ERROR;
+            lrmd__set_result(op, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_ERROR,
+                             NULL);
         }
         controld_ack_event_directly(from_host, from_sys, NULL, op, rsc->id);
         lrmd_free_event(op);
@@ -1961,10 +1960,9 @@ construct_op(lrm_state_t *lrm_state, xmlNode *rsc_op, const char *rsc_id,
 
     op = lrmd_new_event(rsc_id, operation, 0);
     op->type = lrmd_event_exec_complete;
-    op->op_status = PCMK_EXEC_PENDING;
-    op->rc = -1;
     op->timeout = 0;
     op->start_delay = 0;
+    lrmd__set_result(op, -1, PCMK_EXEC_PENDING, NULL);
 
     if (rsc_op == NULL) {
         CRM_LOG_ASSERT(pcmk__str_eq(CRMD_ACTION_STOP, operation, pcmk__str_casei));
@@ -2212,8 +2210,7 @@ record_pending_op(const char *node_name, lrmd_rsc_info_t *rsc, lrmd_event_data_t
     }
 
     op->call_id = -1;
-    op->op_status = PCMK_EXEC_PENDING;
-    op->rc = PCMK_OCF_UNKNOWN;
+    lrmd__set_result(op, PCMK_OCF_UNKNOWN, PCMK_EXEC_PENDING, NULL);
 
     op->t_run = time(NULL);
     op->t_rcchange = op->t_run;
@@ -2310,8 +2307,7 @@ do_lrm_rsc_op(lrm_state_t *lrm_state, lrmd_rsc_info_t *rsc,
                    operation, rsc->id, fsa_state2string(fsa_state),
                    pcmk__btoa(pcmk_is_set(fsa_input_register, R_SHUTDOWN)));
 
-        op->rc = PCMK_OCF_UNKNOWN_ERROR;
-        op->op_status = PCMK_EXEC_INVALID;
+        lrmd__set_result(op, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_INVALID, NULL);
         controld_ack_event_directly(NULL, NULL, rsc, op, rsc->id);
         lrmd_free_event(op);
         free(op_id);
@@ -2371,8 +2367,7 @@ do_lrm_rsc_op(lrm_state_t *lrm_state, lrmd_rsc_info_t *rsc,
 
             crm_info("Faking confirmation of %s: execution postponed for over 5 minutes", op_id);
             decode_transition_key(op->user_data, NULL, NULL, NULL, &target_rc);
-            op->rc = target_rc;
-            op->op_status = PCMK_EXEC_DONE;
+            lrmd__set_result(op, target_rc, PCMK_EXEC_DONE, NULL);
             controld_ack_event_directly(NULL, NULL, rsc, op, rsc->id);
         }
 
@@ -2635,12 +2630,12 @@ process_lrm_event(lrm_state_t *lrm_state, lrmd_event_data_t *op,
     if (compare_version(fsa_our_dc_version, "3.2.0") < 0) {
         switch (op->op_status) {
             case PCMK_EXEC_NOT_CONNECTED:
-                op->op_status = PCMK_EXEC_ERROR;
-                op->rc = PCMK_OCF_CONNECTION_DIED;
+                lrmd__set_result(op, PCMK_OCF_CONNECTION_DIED,
+                                 PCMK_EXEC_ERROR, op->exit_reason);
                 break;
             case PCMK_EXEC_INVALID:
-                op->op_status = PCMK_EXEC_ERROR;
-                op->rc = CRM_DIRECT_NACK_RC;
+                lrmd__set_result(op, CRM_DIRECT_NACK_RC, PCMK_EXEC_ERROR,
+                                 op->exit_reason);
                 break;
             default:
                 break;

daemons/controld/controld_execd_state.c
@@ -76,8 +76,8 @@ fail_pending_op(gpointer key, gpointer value, gpointer user_data)
     event.user_data = op->user_data;
     event.timeout = 0;
     event.interval_ms = op->interval_ms;
-    event.rc = PCMK_OCF_UNKNOWN_ERROR;
-    event.op_status = PCMK_EXEC_NOT_CONNECTED;
+    lrmd__set_result(&event, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_NOT_CONNECTED,
+                     NULL);
     event.t_run = (unsigned int) op->start_time;
     event.t_rcchange = (unsigned int) op->start_time;
 

daemons/controld/controld_remote_ra.c
@@ -13,6 +13,7 @@
 #include <crm/msg_xml.h>
 #include <crm/common/xml_internal.h>
 #include <crm/lrmd.h>
+#include <crm/lrmd_internal.h>
 #include <crm/services.h>
 
 #include <pacemaker-controld.h>
@@ -361,12 +362,8 @@ report_remote_ra_result(remote_ra_cmd_t * cmd)
     op.t_run = (unsigned int) cmd->start_time;
     op.t_rcchange = (unsigned int) cmd->start_time;
 
-    /* Since op's lifetime is just this function, which doesn't free cmd,
-     * point to the exit reason directly rather than copy it.
-     */
-    op.rc = cmd->result.exit_status;
-    op.op_status = cmd->result.exec_status;
-    op.exit_reason = cmd->result.exit_reason;
+    lrmd__set_result(&op, cmd->result.exit_status, cmd->result.exec_status,
+                     cmd->result.exit_reason);
 
     if (cmd->reported_success && (cmd->result.exit_status != PCMK_OCF_OK)) {
         op.t_rcchange = (unsigned int) time(NULL);
@@ -401,6 +398,7 @@ report_remote_ra_result(remote_ra_cmd_t * cmd)
     if (op.params) {
         g_hash_table_destroy(op.params);
     }
+    lrmd__reset_result(&op);
 }
 
 static void
@@ -511,11 +509,10 @@ synthesize_lrmd_success(lrm_state_t *lrm_state, const char *rsc_id, const char *
     op.type = lrmd_event_exec_complete;
     op.rsc_id = rsc_id;
     op.op_type = op_type;
-    op.rc = PCMK_OCF_OK;
-    op.op_status = PCMK_EXEC_DONE;
     op.t_run = (unsigned int) time(NULL);
     op.t_rcchange = op.t_run;
     op.call_id = generate_callid();
+    lrmd__set_result(&op, PCMK_OCF_OK, PCMK_EXEC_DONE, NULL);
     process_lrm_event(lrm_state, &op, NULL, NULL);
 }
 

include/crm/lrmd_internal.h
@@ -35,6 +35,11 @@ int lrmd_send_resource_alert(lrmd_t *lrmd, GList *alert_list,
 int lrmd__remote_send_xml(pcmk__remote_t *session, xmlNode *msg, uint32_t id,
                           const char *msg_type);
 
+void lrmd__set_result(lrmd_event_data_t *event, enum ocf_exitcode rc,
+                      int op_status, const char *exit_reason);
+
+void lrmd__reset_result(lrmd_event_data_t *event);
+
 /* Shared functions for IPC proxy back end */
 
 typedef struct remote_proxy_s {

lib/pacemaker/pcmk_graph_consumer.c
@@ -16,6 +16,7 @@
 #include <crm/msg_xml.h>
 #include <crm/common/xml.h>
 #include <crm/common/xml_internal.h>
+#include <crm/lrmd_internal.h>
 #include <pacemaker-internal.h>
 
 
@@ -818,8 +819,7 @@ pcmk__event_from_graph_action(xmlNode *resource, crm_action_t *action,
     op = lrmd_new_event(ID(action_resource),
                         crm_element_value(action->xml, XML_LRM_ATTR_TASK),
                         action->interval_ms);
-    op->rc = rc;
-    op->op_status = status;
+    lrmd__set_result(op, rc, status, NULL);
     op->t_run = time(NULL);
     op->t_rcchange = op->t_run;
     op->params = pcmk__strkey_table(free, free);

lib/pacemaker/pcmk_sched_transition.c
@@ -24,6 +24,7 @@
 #include <crm/common/util.h>
 #include <crm/common/iso8601.h>
 #include <crm/common/xml_internal.h>
+#include <crm/lrmd_internal.h>
 #include <crm/pengine/status.h>
 #include <pacemaker-internal.h>
 
@@ -121,8 +122,7 @@ create_op(xmlNode *cib_resource, const char *task, guint interval_ms,
     xmlNode *xop = NULL;
 
     op = lrmd_new_event(ID(cib_resource), task, interval_ms);
-    op->rc = outcome;
-    op->op_status = PCMK_EXEC_DONE;
+    lrmd__set_result(op, outcome, PCMK_EXEC_DONE, NULL);
     op->params = NULL;          /* TODO: Fill me in */
     op->t_run = (unsigned int) time(NULL);
     op->t_rcchange = op->t_run;

lib/pacemaker/pcmk_sched_utils.c
@@ -11,6 +11,7 @@
 #include <crm/msg_xml.h>
 #include <crm/lrmd.h>       // lrmd_event_data_t
 #include <crm/common/xml_internal.h>
+#include <crm/lrmd_internal.h>
 #include <pacemaker-internal.h>
 #include <pacemaker.h>
 #include "libpacemaker_private.h"
@@ -612,8 +613,7 @@ pcmk__create_history_xml(xmlNode *parent, lrmd_event_data_t *op,
              * @TODO It might be better to keep the correct result here, and
              * ignore it in process_graph_event().
              */
-            op->op_status = PCMK_EXEC_DONE;
-            op->rc = 0;
+            lrmd__set_result(op, 0, PCMK_EXEC_DONE, NULL);
         }
 
     } else if (did_rsc_op_fail(op, target_rc)) {

commit ed8b2c86ab77aaa3d7fd688c049ad5e1b922a9c6

Fix: liblrmd: Avoid double-free during notify operation
This commit fixes a regression introduced by 31c7fa8a, causing a
double-free in notify operations. lrmd_dispatch_internal() assigns the
exit_reason string directly from an XML node to a new lrmd_event_data_t
object (without duplicating), and this string gets freed twice.

Free #1: pcmk__create_history_xml() (reached via callback) calls
lrmd__set_result(), which frees event.exit_reason and sets it to NULL.
Free #2: lrmd_ipc_dispatch() frees the XML node, which contains a
pointer to the exit_reason string just freed, after
lrmd_dispatch_internal() returns.

Prior to 31c7fa8a, pcmk__create_history_xml reset event.rc and
event.op_status but **not** event.exit_reason.

In this commit we simply make a copy of event.exit_reason in
lrmd_dispatch_internal() before the callback. This way we don't have to
worry about whatever happens in the callback, and we can continue to
unset the exit_reason alongside the rc and op_status. The added overhead
should be minimal.

This commit also makes a copy of output. That's not strictly necessary
but adds some futureproofing and allows us to call lrmd__reset_result()
at the end of lrmd_dispatch_internal().

Resolves: RHBZ#2039675

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/lrmd/lrmd_client.c
@@ -305,9 +305,10 @@ lrmd_dispatch_internal(lrmd_t * lrmd, xmlNode * msg)
         event.user_data = crm_element_value(msg, F_LRMD_RSC_USERDATA_STR);
         event.type = lrmd_event_exec_complete;
 
-        // No need to duplicate the memory, so don't use setter functions
-        event.output = crm_element_value(msg, F_LRMD_RSC_OUTPUT);
-        event.exit_reason = crm_element_value(msg, F_LRMD_RSC_EXIT_REASON);
+        /* output and exit_reason may be freed by a callback */
+        event.output = crm_element_value_copy(msg, F_LRMD_RSC_OUTPUT);
+        lrmd__set_result(&event, event.rc, event.op_status,
+                         crm_element_value(msg, F_LRMD_RSC_EXIT_REASON));
 
         event.params = xml2list(msg);
     } else if (pcmk__str_eq(type, LRMD_OP_NEW_CLIENT, pcmk__str_none)) {
@@ -324,6 +325,7 @@ lrmd_dispatch_internal(lrmd_t * lrmd, xmlNode * msg)
     if (event.params) {
         g_hash_table_destroy(event.params);
     }
+    lrmd__reset_result(&event);
 }
 
 // \return Always 0, to indicate that IPC mainloop source should be kept