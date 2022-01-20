b72eb7d80c6113e46a8b0257a9186d735fa56652
Chris Lumens <clumens@redhat.com>
2022-01-20 14:05:52 -0500

    Refactor: libpacemaker: Make pcmk__reduce_fence_history static.

--- include/pcmki/pcmki_fence.h
+++ include/pcmki/pcmki_fence.h
@@ -226,8 +226,7 @@ int pcmk__fence_validate(pcmk__output_t *out, stonith_t *st, const char *agent,
                          unsigned int timeout);
 
 /**
- * \brief Fetch STONITH history, optionally reducing it as described by
- *        pcmk__reduce_fence_history()
+ * \brief Fetch STONITH history, optionally reducing it.
  *
  * \param[in]  st              The STONITH API object
  * \param[out] stonith_history Destination for storing the history
@@ -238,19 +237,4 @@ int pcmk__fence_validate(pcmk__output_t *out, stonith_t *st, const char *agent,
 int
 pcmk__get_fencing_history(stonith_t *st, stonith_history_t **stonith_history,
                           bool reduce);
-
-/**
- * \brief Reduce the STONITH history
- *
- * STONITH history is reduced as follows:
- *  - The last successful action of every action-type and target is kept
- *  - For failed actions, who failed is kept
- *  - All actions in progress are kept
- *
- * \param[in] history List of STONITH actions
- *
- * \return The reduced history
- */
-stonith_history_t *
-pcmk__reduce_fence_history(stonith_history_t *history);
 #endif
