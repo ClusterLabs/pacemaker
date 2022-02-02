f0fe7a396c213c05e15056e67c1abd141209eb3f
Ken Gaillot <kgaillot@redhat.com>
2022-02-01 18:18:10 -0600

    Refactor: scheduler: allocate and free data set every request
    The memory allocation of a pe_working_set_t is trivial compared to all the
    allocations done as part of the scheduling, not to mention it was reset for
    every request, so there's no real benefit to it.
    
    Refactor init_working_set() to always return a freshly allocated working set,
    and have handle_pecalc_op() free it when done processing the request.

--- daemons/schedulerd/pacemaker-schedulerd.h
+++ daemons/schedulerd/pacemaker-schedulerd.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2021 the Pacemaker project contributors
+ * Copyright 2004-2022 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -13,7 +13,6 @@
 #include <crm_internal.h>
 #include <crm/pengine/pe_types.h>
 
-extern pe_working_set_t *sched_data_set;
 extern pcmk__output_t *logger_out;
 extern pcmk__output_t *out;
 extern struct qb_ipcs_service_handlers ipc_callbacks;
