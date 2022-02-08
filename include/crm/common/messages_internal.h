3b3423ff922c242ac158ec958f8508e351ea7991
Ken Gaillot <kgaillot@redhat.com>
2022-02-08 10:03:34 -0600

    Refactor: libcrmcommon: separate internal message functions into own header
    ... for better organization and future reuse

---
+++ include/crm/common/messages_internal.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018-2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_MESSAGES_INTERNAL__H
+#define PCMK__CRM_COMMON_MESSAGES_INTERNAL__H
+
+const char *pcmk__message_name(const char *name);
+
+#endif // PCMK__CRM_COMMON_MESSAGES_INTERNAL__H
