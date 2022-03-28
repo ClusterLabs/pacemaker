9c44724073e617568c4cb0b03ed2e92e2fa220df
Chris Lumens <clumens@redhat.com>
2022-03-28 16:03:36 -0400

    Feature: tools: Add formatted output boilerplate to attrd_updater.
    This does not yet do anything with formatted output besides add the
    arguments, handle version output, and handle error output.

--- tools/attrd_updater.c
+++ tools/attrd_updater.c
@@ -20,12 +20,20 @@
 #include <crm/crm.h>
 #include <crm/msg_xml.h>
 #include <crm/common/cmdline_internal.h>
+#include <crm/common/output_internal.h>
 #include <crm/common/xml_internal.h>
 #include <crm/common/ipc.h>
 
 #include <crm/common/attrd_internal.h>
 
 #define SUMMARY "query and update Pacemaker node attributes"
+
+static pcmk__supported_format_t formats[] = {
+    PCMK__SUPPORTED_FORMAT_NONE,
+    PCMK__SUPPORTED_FORMAT_TEXT,
+    PCMK__SUPPORTED_FORMAT_XML,
+    { NULL, NULL, NULL }
+};
 
 GError *error = NULL;
 
@@ -167,10 +175,10 @@ static int do_update(char command, const char *attr_node, const char *attr_name,
                      const char *attr_set, const char *attr_dampen, int attr_options);
 
 static GOptionContext *
-build_arg_context(pcmk__common_args_t *args) {
+build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
     GOptionContext *context = NULL;
 
-    context = pcmk__build_arg_context(args, NULL, NULL, NULL);
+    context = pcmk__build_arg_context(args, "text (default), xml", group, NULL);
 
     pcmk__add_arg_group(context, "required", "Required Arguments:",
                         "Show required arguments", required_entries);
@@ -187,12 +195,17 @@ build_arg_context(pcmk__common_args_t *args) {
 int
 main(int argc, char **argv)
 {
+    int rc = pcmk_rc_ok;
     crm_exit_t exit_code = CRM_EX_OK;
 
+    pcmk__output_t *out = NULL;
+
+    GOptionGroup *output_group = NULL;
     pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
-    GOptionContext *context = build_arg_context(args);
+    GOptionContext *context = build_arg_context(args, &output_group);
     gchar **processed_args = pcmk__cmdline_preproc(argv, "dlnsvBNUS");
 
+    pcmk__register_formats(output_group, formats);
     if (!g_option_context_parse_strv(context, &processed_args, &error)) {
         exit_code = CRM_EX_USAGE;
         goto done;
@@ -200,9 +213,17 @@ main(int argc, char **argv)
 
     pcmk__cli_init_logging("attrd_updater", args->verbosity);
 
+    rc = pcmk__output_new(&out, args->output_ty, args->output_dest, argv);
+    if (rc != pcmk_rc_ok) {
+        exit_code = CRM_EX_ERROR;
+        g_set_error(&error, PCMK__EXITC_ERROR, exit_code, "Error creating output format %s: %s",
+                    args->output_ty, pcmk_rc_str(rc));
+        goto done;
+    }
+
     if (args->version) {
-        /* FIXME:  When attrd_updater is converted to use formatted output, this can go. */
-        pcmk__cli_help('v', CRM_EX_OK);
+        out->version(out, false);
+        goto done;
     }
 
     if (options.command != 'R' && options.attr_name == NULL) {
@@ -239,7 +260,12 @@ done:
     g_free(options.attr_set);
     free(options.attr_value);
 
-    pcmk__output_and_clear_error(error, NULL);
+    pcmk__output_and_clear_error(error, out);
+
+    if (out != NULL) {
+        out->finish(out, exit_code, true, NULL);
+        pcmk__output_free(out);
+    }
 
     crm_exit(exit_code);
 }
