6bf70065caceb5b1e3e512d7c98c4c720b33a884
Chris Lumens <clumens@redhat.com>
2022-04-25 16:26:55 -0400

    Refactor: tools: Convert iso8601 to using glib for command line.
    Fixes T99

--- tools/iso8601.c
+++ tools/iso8601.c
@@ -9,95 +9,100 @@
 
 #include <crm_internal.h>
 #include <crm/crm.h>
+#include <crm/common/cmdline_internal.h>
 #include <crm/common/iso8601.h>
 #include <crm/common/util.h>  /* CRM_ASSERT */
 #include <unistd.h>
 
+#define SUMMARY "Display and parse ISO 8601 dates and times"
+
 struct {
-    const char *date_time_s;
-    const char *duration_s;
-    const char *expected_s;
-    const char *period_s;
+    char *date_time_s;
+    gchar *duration_s;
+    gchar *expected_s;
+    gchar *period_s;
     int print_options;
 } options;
 
-static pcmk__cli_option_t long_options[] = {
-    // long option, argument type, storage, short option, description, flags
-    {
-        "help", no_argument, NULL, '?',
-        "\tThis text", pcmk__option_default
-    },
-    {
-        "version", no_argument, NULL, '$',
-        "\tVersion information", pcmk__option_default
-    },
-    {
-        "verbose", no_argument, NULL, 'V',
-        "\tIncrease debug output", pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nCommands:", pcmk__option_default
-    },
-    {
-        "now", no_argument, NULL, 'n',
-        "\tDisplay the current date/time", pcmk__option_default
-    },
-    {
-        "date", required_argument, NULL, 'd',
-        "Parse an ISO 8601 date/time (for example, "
-            "'2019-09-24 00:30:00 +01:00' or '2019-040')",
-        pcmk__option_default
-    },
-    {
-        "period", required_argument, NULL, 'p',
-        "Parse an ISO 8601 period (interval) with start time (for example, "
-            "'2005-040/2005-043')",
-        pcmk__option_default
-    },
-    {
-        "duration", required_argument, NULL, 'D',
-        "Parse an ISO 8601 duration (for example, 'P1M')", pcmk__option_default
-    },
-    {
-        "expected", required_argument, NULL, 'E',
-        "Exit with error status if result does not match this text. "
-            "Requires: -n or -d",
-        pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nOutput Modifiers:", pcmk__option_default
-    },
-    {
-        "seconds", no_argument, NULL, 's',
-        "\tShow result as a seconds since 0000-001 00:00:00Z",
-        pcmk__option_default
-    },
-    {
-        "epoch", no_argument, NULL, 'S',
-        "\tShow result as a seconds since EPOCH (1970-001 00:00:00Z)",
-        pcmk__option_default
-    },
-    {
-        "local", no_argument, NULL, 'L',
-        "\tShow result as a 'local' date/time", pcmk__option_default
-    },
-    {
-        "ordinal", no_argument, NULL, 'O',
-        "\tShow result as an 'ordinal' date/time", pcmk__option_default
-    },
-    {
-        "week", no_argument, NULL, 'W',
-        "\tShow result as an 'calendar week' date/time", pcmk__option_default
-    },
-    {
-        "-spacer-", no_argument, NULL, '-',
-        "\nFor more information on the ISO 8601 standard, see "
-            "https://en.wikipedia.org/wiki/ISO_8601",
-        pcmk__option_default
-    },
-    { 0, 0, 0, 0 }
+#define INDENT "                              "
+
+static gboolean
+date_now_cb(const gchar *option_name, const gchar *optarg, gpointer data, GError **error) {
+    if (pcmk__str_any_of(option_name, "--now", "-n", NULL)) {
+        pcmk__str_update(&options.date_time_s, "now");
+    } else if (pcmk__str_any_of(option_name, "--date", "-d", NULL)) {
+        pcmk__str_update(&options.date_time_s, optarg);
+    }
+
+    return TRUE;
+}
+
+static gboolean
+modifier_cb(const gchar *option_name, const gchar *optarg, gpointer data, GError **error) {
+    if (pcmk__str_any_of(option_name, "--seconds", "-s", NULL)) {
+        options.print_options |= crm_time_seconds;
+    } else if (pcmk__str_any_of(option_name, "--epoch", "-S", NULL)) {
+        options.print_options |= crm_time_epoch;
+    } else if (pcmk__str_any_of(option_name, "--local", "-L", NULL)) {
+        options.print_options |= crm_time_log_with_timezone;
+    } else if (pcmk__str_any_of(option_name, "--ordinal", "-O", NULL)) {
+        options.print_options |= crm_time_ordinal;
+    } else if (pcmk__str_any_of(option_name, "--week", "-W", NULL)) {
+        options.print_options |= crm_time_weeks;
+    }
+
+    return TRUE;
+}
+
+static GOptionEntry command_entries[] = {
+    { "now", 'n', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, date_now_cb,
+      "Display the current date/time",
+      NULL },
+
+    { "date", 'd', 0, G_OPTION_ARG_CALLBACK, date_now_cb,
+      "Parse an ISO 8601 date/time (for example,\n"
+      INDENT "'2019-09-24 00:30:00 +01:00' or '2019-040')",
+      "DATE" },
+
+    { "period", 'p', 0, G_OPTION_ARG_STRING, &options.period_s,
+      "Parse an ISO 8601 period (interval) with start time (for example,\n"
+      INDENT "'2005-040/2005-043')",
+      "PERIOD" },
+
+    { "duration", 'D', 0, G_OPTION_ARG_STRING, &options.duration_s,
+      "Parse an ISO 8601 duration (for example, 'P1M')",
+      "DURATION" },
+
+    { "expected", 'E', 0, G_OPTION_ARG_STRING, &options.expected_s,
+      "Exit with error status if result does not match this text.\n"
+      INDENT "Requires: -n or -d",
+      "TEXT" },
+
+    { NULL }
+};
+
+static GOptionEntry modifier_entries[] = {
+    { "seconds", 's', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, modifier_cb,
+      "Show result as a seconds since 0000-001 00:00:00Z",
+      NULL },
+
+    { "epoch", 'S', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, modifier_cb,
+      "Show result as a seconds since EPOCH (1970-001 00:00:00Z)",
+      NULL },
+
+    { "local", 'L', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, modifier_cb,
+      "Show result as a 'local' date/time",
+      NULL },
+
+    { "ordinal", 'O', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, modifier_cb,
+      "Show result as an 'ordinal' date/time",
+      NULL },
+
+    { "week", 'W', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, modifier_cb,
+      "Show result as an 'calendar week' date/time",
+      NULL },
+
+    { NULL }
 };
 
 static void
@@ -112,71 +117,49 @@ log_time_period(int log_level, crm_time_period_t * dtp, int flags)
     free(end);
 }
 
+static GOptionContext *
+build_arg_context(pcmk__common_args_t *args) {
+    GOptionContext *context = NULL;
+
+    const char *description = "For more information on the ISO 8601 standard, see " \
+                              "https://en.wikipedia.org/wiki/ISO_8601";
+
+    context = pcmk__build_arg_context(args, NULL, NULL, NULL);
+    g_option_context_set_description(context, description);
+
+    pcmk__add_arg_group(context, "commands", "Commands:",
+                        "Show command options", command_entries);
+    pcmk__add_arg_group(context, "modifiers", "Output modifiers:",
+                        "Show output modifiers", modifier_entries);
+
+    return context;
+}
+
 int
 main(int argc, char **argv)
 {
     crm_exit_t exit_code = CRM_EX_OK;
-    int argerr = 0;
-    int flag;
-    int index = 0;
     crm_time_t *duration = NULL;
     crm_time_t *date_time = NULL;
 
     GError *error = NULL;
 
-    pcmk__cli_init_logging("iso8601", 0);
-    pcmk__set_cli_options(NULL, "<command> [options] ", long_options,
-                          "display and parse ISO 8601 dates and times");
+    pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
+    GOptionContext *context = build_arg_context(args);
+    gchar **processed_args = pcmk__cmdline_preproc(argv, "dpDE");
 
-    if (argc < 2) {
-        argerr++;
+    if (!g_option_context_parse_strv(context, &processed_args, &error)) {
+        exit_code = CRM_EX_USAGE;
+        goto done;
     }
 
-    while (1) {
-        flag = pcmk__next_cli_option(argc, argv, &index, NULL);
-        if (flag == -1)
-            break;
+    pcmk__cli_init_logging("iso8601", args->verbosity);
 
-        switch (flag) {
-            case 'V':
-                crm_bump_log_level(argc, argv);
-                break;
-            case '?':
-            case '$':
-                pcmk__cli_help(flag, CRM_EX_OK);
-                break;
-            case 'n':
-                options.date_time_s = "now";
-                break;
-            case 'd':
-                options.date_time_s = optarg;
-                break;
-            case 'p':
-                options.period_s = optarg;
-                break;
-            case 'D':
-                options.duration_s = optarg;
-                break;
-            case 'E':
-                options.expected_s = optarg;
-                break;
-            case 'S':
-                options.print_options |= crm_time_epoch;
-                break;
-            case 's':
-                options.print_options |= crm_time_seconds;
-                break;
-            case 'W':
-                options.print_options |= crm_time_weeks;
-                break;
-            case 'O':
-                options.print_options |= crm_time_ordinal;
-                break;
-            case 'L':
-                options.print_options |= crm_time_log_with_timezone;
-                break;
-                break;
-        }
+    if (args->version) {
+        g_strfreev(processed_args);
+        pcmk__free_arg_context(context);
+        /* FIXME:  When iso8601 is converted to use formatted output, this can go. */
+        pcmk__cli_help('v', CRM_EX_OK);
     }
 
     if (pcmk__str_eq("now", options.date_time_s, pcmk__str_casei)) {
@@ -285,6 +268,14 @@ main(int argc, char **argv)
 done:
     crm_time_free(date_time);
     crm_time_free(duration);
+
+    g_strfreev(processed_args);
+    pcmk__free_arg_context(context);
+
+    free(options.date_time_s);
+    g_free(options.duration_s);
+    g_free(options.expected_s);
+    g_free(options.period_s);
 
     pcmk__output_and_clear_error(error, NULL);
     crm_exit(exit_code);
