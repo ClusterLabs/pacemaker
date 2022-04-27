f6edaf42e93a1391bf03f85a3d8fffcafe865947
Ken Gaillot <kgaillot@redhat.com>
2022-04-27 13:45:38 -0500

    API: libcrmcommon: deprecate crm_ipc_server_info
    It has long been unused

--- include/crm/common/ipc.h
+++ include/crm/common/ipc.h
@@ -155,9 +155,8 @@ enum crm_ipc_flags
     crm_ipc_server_free     = 0x00020000, /* Free the iovec after sending */
     crm_ipc_proxied_relay_response = 0x00040000, /* all replies to proxied connections are sent as events, this flag preserves whether the event should be treated as an actual event, or a response.*/
 
-    crm_ipc_server_info     = 0x00100000, /* Log failures as LOG_INFO */
-
 #if !defined(PCMK_ALLOW_DEPRECATED) || (PCMK_ALLOW_DEPRECATED == 1)
+    crm_ipc_server_info     = 0x00100000, //!< \deprecated Unused
     crm_ipc_server_error    = 0x00200000, //!< \deprecated Unused
 #endif
 };
