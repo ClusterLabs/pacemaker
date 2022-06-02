ClusterLabs/pacemaker

commit 4a6d187937a33d63f5bba95522778b904e7da6f0

Build: configure,CTS,tools: make corosync config location configurable

cts/lab/CIB.py
@@ -1,7 +1,7 @@
 """ CIB generator for Pacemaker's Cluster Test Suite (CTS)
 """
 
-__copyright__ = "Copyright 2008-2020 the Pacemaker project contributors"
+__copyright__ = "Copyright 2008-2021 the Pacemaker project contributors"
 __license__ = "GNU General Public License version 2 or later (GPLv2+) WITHOUT ANY WARRANTY"
 
 import os
@@ -127,8 +127,8 @@ class CIB12(ConfigBase):
         (rc, output) = self.Factory.rsh(self.Factory.target,
             r"""awk -v RS="}" """
             r"""'/^(\s*nodelist\s*{)?\s*node\s*{.*(ring0_addr|name):\s*%s(\s+|$)/"""
-            r"""{gsub(/.*nodeid:\s*/,"");gsub(/\s+.*$/,"");print}'"""
-            r""" /etc/corosync/corosync.conf""" % node_name, None)
+            r"""{gsub(/.*nodeid:\s*/,"");gsub(/\s+.*$/,"");print}'%s"""
+            % (node_name, CTSvars.COROSYNC_CONF), None)
 
         if rc == 0 and len(output) == 1:
             try:

configure.ac
@@ -400,6 +400,15 @@ AC_ARG_WITH([corosync],
 yes_no_try "$with_corosync" "try"
 with_corosync=$?
 
+dnl @TODO This should be a corosync pkgconfig variable
+PCMK__COROSYNC_CONF=""
+AC_ARG_WITH([corosync-conf],
+    [AS_HELP_STRING([--with-corosync-conf], m4_normalize([
+        location of Corosync configuration file
+        @<:@SYSCONFDIR/corosync/corosync.conf@:>@]))],
+    [ PCMK__COROSYNC_CONF="$withval" ]
+)
+
 AC_ARG_WITH([nagios],
     [AS_HELP_STRING([--with-nagios], [support nagios resources])]
 )
@@ -661,6 +670,9 @@ AC_SUBST(docdir)
 
 expand_path_option CONFIGDIR "${sysconfdir}/sysconfig"
 AC_SUBST(CONFIGDIR)
+
+expand_path_option PCMK__COROSYNC_CONF "${sysconfdir}/corosync/corosync.conf"
+AC_SUBST(PCMK__COROSYNC_CONF)
 
 expand_path_option CRM_LOG_DIR "${localstatedir}/log/pacemaker"
 AC_DEFINE_UNQUOTED(CRM_LOG_DIR,"$CRM_LOG_DIR", Location for Pacemaker log file)

cts/cts-fencing.in
@@ -19,6 +19,7 @@ import signal
 # Prefer the source tree if available
 BUILD_DIR = "@abs_top_builddir@"
 SCHEMA_DIR = "@CRM_SCHEMA_DIRECTORY@"
+COROSYNC_CONF = "@PCMK__COROSYNC_CONF@"
 TEST_DIR = sys.path[0]
 
 AUTOGEN_COROSYNC_TEMPLATE = """
@@ -544,7 +545,7 @@ class Tests(object):
         self.timeout = timeout
         self.force_wait = force_wait
         self.logdir = logdir
-        self.autogen_corosync_cfg = not os.path.exists("/etc/corosync/corosync.conf")
+        self.autogen_corosync_cfg = not os.path.exists(COROSYNC_CONF)
 
     def new_test(self, name, description, with_cpg=0):
         """ Create a named test """
@@ -1460,7 +1461,7 @@ class Tests(object):
         if use_corosync:
             if self.autogen_corosync_cfg:
                 logname = os.path.join(self.logdir, "corosync.log")
-                corosync_cfg = io.open("/etc/corosync/corosync.conf", "w")
+                corosync_cfg = io.open(COROSYNC_CONF, "w")
                 corosync_cfg.write(AUTOGEN_COROSYNC_TEMPLATE % (localname(), logname))
                 corosync_cfg.close()
 
@@ -1483,7 +1484,7 @@ class Tests(object):
                     for line in logfile.readlines():
                         print(line.strip())
                     logfile.close()
-                os.remove("/etc/corosync/corosync.conf")
+                os.remove(COROSYNC_CONF)
 
         subprocess.call(["cts-support", "uninstall"])
 

cts/lab/CTSvars.py.in
@@ -13,3 +13,4 @@ class CTSvars(object):
     CRM_DAEMON_USER="@CRM_DAEMON_USER@"
     CRM_DAEMON_DIR="@CRM_DAEMON_DIR@"
     OCF_ROOT_DIR="@OCF_ROOT_DIR@"
+    COROSYNC_CONF="@PCMK__COROSYNC_CONF@"

tools/report.common.in
@@ -1,7 +1,7 @@
 #
 # Originally based on hb_report
 # Copyright 2007 Dejan Muhamedagic <dmuhamedagic@suse.de>
-# Later changes copyright 2010-2018 the Pacemaker project contributors
+# Later changes copyright 2010-2021 the Pacemaker project contributors
 #
 # The version control history for this file may have further details.
 #
@@ -825,7 +825,7 @@ get_cluster_type() {
     # Now we're guessing...
 
     # TODO: Technically these could be anywhere :-/
-    elif [ -f /etc/corosync/corosync.conf ]; then
+    elif [ -f "@PCMK__COROSYNC_CONF@" ]; then
 	stack="corosync"
 
     else
@@ -845,7 +845,7 @@ find_cluster_cf() {
 	    best_file=""
 
 	    # TODO: Technically these could be anywhere :-/
-	    for cf in /etc/corosync/corosync.conf; do
+	    for cf in "@PCMK__COROSYNC_CONF@"; do
 		if [ -f $cf ]; then
 		    size=`wc -l $cf | awk '{print $1}'`
 		    if [ $size -gt $best_size ]; then

commit caf9a3bcd1b4c496b01bc5852373f6b103f72009

Low: CTS: Fix a bug in an awk command used in ctslab.
This command gets the node ID from a cluster node based on the corosync
config file.  4a6d187937 introduced a bug where the command can fail
because there's no longer a space between the command and the file name
awk is operating on.  So, add that space back.

No idea what has been broken as a result of this, if anything.

cts/lab/CIB.py
@@ -127,7 +127,7 @@ class CIB12(ConfigBase):
         (rc, output) = self.Factory.rsh(self.Factory.target,
             r"""awk -v RS="}" """
             r"""'/^(\s*nodelist\s*{)?\s*node\s*{.*(ring0_addr|name):\s*%s(\s+|$)/"""
-            r"""{gsub(/.*nodeid:\s*/,"");gsub(/\s+.*$/,"");print}'%s"""
+            r"""{gsub(/.*nodeid:\s*/,"");gsub(/\s+.*$/,"");print}' %s"""
             % (node_name, CTSvars.COROSYNC_CONF), None)
 
         if rc == 0 and len(output) == 1: