ClusterLabs/pacemaker

commit b09f16eb13558e84d860c5ef5cf441b54e132d04

Refactor: fencer: use an enum for topology level kind
The previous integer implementation was difficult to follow

daemons/fenced/fenced_commands.c
@@ -72,7 +72,7 @@ static void search_devices_record_result(struct device_search_s *search, const c
 
 static int get_agent_metadata(const char *agent, xmlNode **metadata);
 static void read_action_metadata(stonith_device_t *device);
-static int stonith_level_kind(xmlNode *level);
+static enum fenced_target_by unpack_level_kind(xmlNode *level);
 
 typedef struct async_command_s {
 
@@ -1505,55 +1505,52 @@ init_topology_list(void)
     }
 }
 
-char *stonith_level_key(xmlNode *level, int mode)
+char *
+stonith_level_key(xmlNode *level, enum fenced_target_by mode)
 {
-    if(mode == -1) {
-        mode = stonith_level_kind(level);
+    if (mode == fenced_target_by_unknown) {
+        mode = unpack_level_kind(level);
     }
-
-    switch(mode) {
-        case 0:
+    switch (mode) {
+        case fenced_target_by_name:
             return crm_element_value_copy(level, XML_ATTR_STONITH_TARGET);
-        case 1:
-            return crm_element_value_copy(level, XML_ATTR_STONITH_TARGET_PATTERN);
-        case 2:
-            {
-                const char *name = crm_element_value(level, XML_ATTR_STONITH_TARGET_ATTRIBUTE);
-                const char *value = crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE);
 
-                if(name && value) {
-                    return crm_strdup_printf("%s=%s", name, value);
-                }
-            }
+        case fenced_target_by_pattern:
+            return crm_element_value_copy(level, XML_ATTR_STONITH_TARGET_PATTERN);
+
+        case fenced_target_by_attribute:
+            return crm_strdup_printf("%s=%s",
+                crm_element_value(level, XML_ATTR_STONITH_TARGET_ATTRIBUTE),
+                crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE));
+
         default:
-            return crm_strdup_printf("Unknown-%d-%s", mode, ID(level));
+            return crm_strdup_printf("unknown-%s", ID(level));
     }
 }
 
+/*!
+ * \internal
+ * \brief Parse target identification from topology level XML
+ *
+ * \param[in] level  Topology level XML to parse
+ *
+ * \return How to identify target of \p level
+ */
 static int
-stonith_level_kind(xmlNode *level)
+unpack_level_kind(xmlNode *level)
 {
-    int mode = 0;
-    const char *target = crm_element_value(level, XML_ATTR_STONITH_TARGET);
-
-    if(target == NULL) {
-        mode++;
-        target = crm_element_value(level, XML_ATTR_STONITH_TARGET_PATTERN);
+    if (crm_element_value(level, XML_ATTR_STONITH_TARGET) != NULL) {
+        return fenced_target_by_name;
     }
-
-    if(stand_alone == FALSE && target == NULL) {
-
-        mode++;
-
-        if(crm_element_value(level, XML_ATTR_STONITH_TARGET_ATTRIBUTE) == NULL) {
-            mode++;
-
-        } else if(crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE) == NULL) {
-            mode++;
-        }
+    if (crm_element_value(level, XML_ATTR_STONITH_TARGET_PATTERN) != NULL) {
+        return fenced_target_by_pattern;
     }
-
-    return mode;
+    if (!stand_alone /* if standalone, there's no attribute manager */
+        && (crm_element_value(level, XML_ATTR_STONITH_TARGET_ATTRIBUTE) != NULL)
+        && (crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE) == NULL)) {
+        return fenced_target_by_attribute;
+    }
+    return fenced_target_by_unknown;
 }
 
 static stonith_key_value_t *
@@ -1601,7 +1598,7 @@ fenced_register_level(xmlNode *msg, char **desc, pcmk__action_result_t *result)
 {
     int id = 0;
     xmlNode *level;
-    int mode;
+    enum fenced_target_by mode;
     char *target;
 
     stonith_topology_t *tp;
@@ -1629,8 +1626,7 @@ fenced_register_level(xmlNode *msg, char **desc, pcmk__action_result_t *result)
         return;
     }
 
-    mode = stonith_level_kind(level);
-
+    mode = unpack_level_kind(level);
     target = stonith_level_key(level, mode);
     crm_element_value_int(level, XML_ATTR_STONITH_INDEX, &id);
 
@@ -1639,7 +1635,7 @@ fenced_register_level(xmlNode *msg, char **desc, pcmk__action_result_t *result)
     }
 
     // Ensure a valid target was specified
-    if ((mode < 0) || (mode > 2)) {
+    if (mode == fenced_target_by_unknown) {
         crm_warn("Ignoring topology level registration without valid target");
         free(target);
         crm_log_xml_warn(level, "Bad level");
@@ -1676,7 +1672,7 @@ fenced_register_level(xmlNode *msg, char **desc, pcmk__action_result_t *result)
 
         g_hash_table_replace(topology, tp->target, tp);
         crm_trace("Added %s (%d) to the topology (%d active entries)",
-                  target, mode, g_hash_table_size(topology));
+                  target, (int) mode, g_hash_table_size(topology));
     } else {
         free(target);
     }
@@ -1740,7 +1736,7 @@ fenced_unregister_level(xmlNode *msg, char **desc,
         return;
     }
 
-    target = stonith_level_key(level, -1);
+    target = stonith_level_key(level, fenced_target_by_unknown);
     crm_element_value_int(level, XML_ATTR_STONITH_INDEX, &id);
 
     // Ensure level ID is in allowed range

daemons/fenced/fenced_remote.c
@@ -829,8 +829,8 @@ topology_matches(const stonith_topology_t *tp, const char *node)
     regex_t r_patt;
 
     CRM_CHECK(node && tp && tp->target, return FALSE);
-    switch(tp->kind) {
-        case 2:
+    switch (tp->kind) {
+        case fenced_target_by_attribute:
             /* This level targets by attribute, so tp->target is a NAME=VALUE pair
              * of a permanent attribute applied to targeted nodes. The test below
              * relies on the locally cached copy of the CIB, so if fencing needs to
@@ -842,11 +842,11 @@ topology_matches(const stonith_topology_t *tp, const char *node)
                 return TRUE;
             }
             break;
-        case 1:
-            /* This level targets by name, so tp->target is a regular expression
-             * matching names of nodes to be targeted.
-             */
 
+        case fenced_target_by_pattern:
+            /* This level targets node names matching a pattern, so tp->target
+             * (and tp->target_pattern) is a regular expression.
+             */
             if (regcomp(&r_patt, tp->target_pattern, REG_EXTENDED|REG_NOSUB)) {
                 crm_info("Bad regex '%s' for fencing level", tp->target);
             } else {
@@ -859,9 +859,13 @@ topology_matches(const stonith_topology_t *tp, const char *node)
                 }
             }
             break;
-        case 0:
+
+        case fenced_target_by_name:
             crm_trace("Testing %s against %s", node, tp->target);
             return pcmk__str_eq(tp->target, node, pcmk__str_casei);
+
+        default:
+            break;
     }
     crm_trace("No match for %s with %s", node, tp->target);
     return FALSE;

daemons/fenced/pacemaker-fenced.c
@@ -502,7 +502,7 @@ handle_topology_change(xmlNode *match, bool remove)
 
     if(remove) {
         int index = 0;
-        char *key = stonith_level_key(match, -1);
+        char *key = stonith_level_key(match, fenced_target_by_unknown);
 
         crm_element_value_int(match, XML_ATTR_STONITH_INDEX, &index);
         topology_remove_helper(key, index);
@@ -527,7 +527,7 @@ remove_fencing_topology(xmlXPathObjectPtr xpathObj)
         if (match && crm_element_value(match, XML_DIFF_MARKER)) {
             /* Deletion */
             int index = 0;
-            char *target = stonith_level_key(match, -1);
+            char *target = stonith_level_key(match, fenced_target_by_unknown);
 
             crm_element_value_int(match, XML_ATTR_STONITH_INDEX, &index);
             if (target == NULL) {

daemons/fenced/pacemaker-fenced.h
@@ -167,6 +167,14 @@ enum st_client_flags {
     st_callback_notify_history_synced = (UINT64_C(1) << 6)
 };
 
+// How the user specified the target of a topology level
+enum fenced_target_by {
+    fenced_target_by_unknown = -1,  // Invalid or not yet parsed
+    fenced_target_by_name,          // By target name
+    fenced_target_by_pattern,       // By a pattern matching target names
+    fenced_target_by_attribute,     // By a node attribute/value on target
+};
+
 /*
  * Complex fencing requirements are specified via fencing topologies.
  * A topology consists of levels; each level is a list of fencing devices.
@@ -182,7 +190,7 @@ enum st_client_flags {
  * Topology levels start from 1, so levels[0] is unused and always NULL.
  */
 typedef struct stonith_topology_s {
-    int kind;
+    enum fenced_target_by kind; // How target was specified
 
     /*! Node name regex or attribute name=value for which topology applies */
     char *target;

commit 7f07edc0f8ef47527e55c4aafdd9d0c6388beaed

Fix: fencer: get target-by-attribute working again
Regression in 2.1.3 introduced by b09f16eb1

This was papered over by the separate regression in cts-lab introduced by
4a6d1879, which caused target-by-attribute to no longer be tested, and which
was recently fixed by caf9a3bc.

daemons/fenced/fenced_commands.c
@@ -1545,7 +1545,7 @@ unpack_level_kind(xmlNode *level)
     }
     if (!stand_alone /* if standalone, there's no attribute manager */
         && (crm_element_value(level, XML_ATTR_STONITH_TARGET_ATTRIBUTE) != NULL)
-        && (crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE) == NULL)) {
+        && (crm_element_value(level, XML_ATTR_STONITH_TARGET_VALUE) != NULL)) {
         return fenced_target_by_attribute;
     }
     return fenced_target_by_unknown;