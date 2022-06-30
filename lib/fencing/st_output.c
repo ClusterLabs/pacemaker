ClusterLabs/pacemaker

commit 91a2b2e417d0d3b4ed310b5003879b8d0e2fc831

Refactor: various: use new boolean-to-string functions where appropriate

lib/fencing/st_output.c
@@ -424,7 +424,7 @@ stonith__validate_agent_xml(pcmk__output_t *out, va_list args) {
     if (device != NULL) {
         xmlSetProp(node, (pcmkXmlStr) "device", (pcmkXmlStr) device);
     }
-    xmlSetProp(node, (pcmkXmlStr) "valid", (pcmkXmlStr) (rc ? "false" : "true"));
+    xmlSetProp(node, (pcmkXmlStr) "valid", (pcmkXmlStr) pcmk__btoa(rc));
 
     pcmk__output_xml_push_parent(out, node);
     out->subprocess_output(out, rc, output, error_output);

daemons/controld/controld_based.c
@@ -31,7 +31,7 @@ do_cib_updated(const char *event, xmlNode * msg)
 static void
 do_cib_replaced(const char *event, xmlNode * msg)
 {
-    crm_debug("Updating the CIB after a replace: DC=%s", AM_I_DC ? "true" : "false");
+    crm_debug("Updating the CIB after a replace: DC=%s", pcmk__btoa(AM_I_DC));
     if (AM_I_DC == FALSE) {
         return;
 

daemons/controld/controld_election.c
@@ -206,8 +206,9 @@ do_dc_takeover(long long action,
     fsa_cib_update(XML_TAG_CIB, cib, cib_quorum_override, rc, NULL);
     fsa_register_cib_callback(rc, FALSE, NULL, feature_update_callback);
 
-    update_attr_delegate(fsa_cib_conn, cib_none, XML_CIB_TAG_CRMCONFIG, NULL, NULL, NULL, NULL,
-                         XML_ATTR_HAVE_WATCHDOG, watchdog?"true":"false", FALSE, NULL, NULL);
+    update_attr_delegate(fsa_cib_conn, cib_none, XML_CIB_TAG_CRMCONFIG, NULL,
+                         NULL, NULL, NULL, XML_ATTR_HAVE_WATCHDOG,
+                         pcmk__btoa(watchdog), FALSE, NULL, NULL);
 
     update_attr_delegate(fsa_cib_conn, cib_none, XML_CIB_TAG_CRMCONFIG, NULL, NULL, NULL, NULL,
                          "dc-version", PACEMAKER_VERSION "-" BUILD_VERSION, FALSE, NULL, NULL);

daemons/controld/controld_execd.c
@@ -2287,7 +2287,7 @@ do_lrm_rsc_op(lrm_state_t *lrm_state, lrmd_rsc_info_t *rsc,
     if(send_nack) {
         crm_notice("Discarding attempt to perform action %s on %s in state %s (shutdown=%s)",
                    operation, rsc->id, fsa_state2string(fsa_state),
-                   is_set(fsa_input_register, R_SHUTDOWN)?"true":"false");
+                   pcmk__btoa(is_set(fsa_input_register, R_SHUTDOWN)));
 
         op->rc = PCMK_OCF_UNKNOWN_ERROR;
         op->op_status = PCMK_LRM_OP_INVALID;
@@ -2798,7 +2798,7 @@ process_lrm_event(lrm_state_t *lrm_state, lrmd_event_data_t *op,
                      crm_action_str(op->op_type, op->interval_ms),
                      op->rsc_id, node_name,
                      services_lrm_status_str(op->op_status),
-                     op->call_id, op_key, (removed? "true" : "false"));
+                     op->call_id, op_key, pcmk__btoa(removed));
             break;
 
         case PCMK_LRM_OP_DONE:
@@ -2807,8 +2807,7 @@ process_lrm_event(lrm_state_t *lrm_state, lrmd_event_data_t *op,
                        crm_action_str(op->op_type, op->interval_ms),
                        op->rsc_id, node_name,
                        services_ocf_exitcode_str(op->rc), op->rc,
-                       op->call_id, op_key, (removed? "true" : "false"),
-                       update_id);
+                       op->call_id, op_key, pcmk__btoa(removed), update_id);
             break;
 
         case PCMK_LRM_OP_TIMEOUT:
@@ -2826,7 +2825,7 @@ process_lrm_event(lrm_state_t *lrm_state, lrmd_event_data_t *op,
                     crm_action_str(op->op_type, op->interval_ms),
                     op->rsc_id, node_name,
                     services_lrm_status_str(op->op_status), op->call_id, op_key,
-                    (removed? "true" : "false"), op->op_status, update_id);
+                    pcmk__btoa(removed), op->op_status, update_id);
     }
 
     if (op->output) {

daemons/controld/controld_fsa.c
@@ -251,8 +251,7 @@ s_crmd_fsa(enum crmd_fsa_cause cause)
         || do_fsa_stall) {
         crm_debug("Exiting the FSA: queue=%d, fsa_actions=0x%llx, stalled=%s",
                   g_list_length(fsa_message_queue),
-                  (unsigned long long) fsa_actions,
-                  (do_fsa_stall? "true" : "false"));
+                  (unsigned long long) fsa_actions, pcmk__btoa(do_fsa_stall));
     } else {
         crm_trace("Exiting the FSA");
     }

daemons/controld/controld_membership.c
@@ -409,7 +409,8 @@ crm_update_quorum(gboolean quorum, gboolean force_update)
         crm_xml_add(update, XML_ATTR_DC_UUID, fsa_our_uuid);
 
         fsa_cib_update(XML_TAG_CIB, update, call_options, call_id, NULL);
-        crm_debug("Updating quorum status to %s (call=%d)", quorum ? "true" : "false", call_id);
+        crm_debug("Updating quorum status to %s (call=%d)",
+                  pcmk__btoa(quorum), call_id);
         fsa_register_cib_callback(call_id, FALSE, NULL, cib_quorum_update_complete);
         free_xml(update);
 

daemons/controld/controld_schedulerd.c
@@ -448,7 +448,7 @@ do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void
     crm_xml_add(output, XML_ATTR_DC_UUID, fsa_our_uuid);
     crm_xml_add_int(output, XML_ATTR_HAVE_QUORUM, fsa_has_quorum);
 
-    force_local_option(output, XML_ATTR_HAVE_WATCHDOG, watchdog?"true":"false");
+    force_local_option(output, XML_ATTR_HAVE_WATCHDOG, pcmk__btoa(watchdog));
 
     if (ever_had_quorum && crm_have_quorum == FALSE) {
         crm_xml_add_int(output, XML_ATTR_QUORUM_PANIC, 1);

daemons/controld/controld_te_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2019 the Pacemaker project contributors
+ * Copyright 2004-2020 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -195,7 +195,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
     if(reason == NULL) {
         do_crm_log(level, "Transition %d aborted: %s "CRM_XS" source=%s:%d complete=%s",
                    transition_graph->id, abort_text, fn, line,
-                   (transition_graph->complete? "true" : "false"));
+                   pcmk__btoa(transition_graph->complete));
 
     } else if(change == NULL) {
         char *local_path = xml_get_path(reason);
@@ -204,7 +204,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                    CRM_XS " cib=%d.%d.%d source=%s:%d path=%s complete=%s",
                    transition_graph->id, TYPE(reason), ID(reason), abort_text,
                    add[0], add[1], add[2], fn, line, local_path,
-                   (transition_graph->complete? "true" : "false"));
+                   pcmk__btoa(transition_graph->complete));
         free(local_path);
 
     } else {
@@ -233,7 +233,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                        transition_graph->id,
                        (shortpath? (shortpath + 1) : path), abort_text,
                        add[0], add[1], add[2], fn, line, path,
-                       (transition_graph->complete? "true" : "false"));
+                       pcmk__btoa(transition_graph->complete));
 
         } else if (pcmk__str_eq(XML_CIB_TAG_NVPAIR, kind, pcmk__str_casei)) { 
             do_crm_log(level, "Transition %d aborted by %s doing %s %s=%s: %s "
@@ -243,7 +243,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                        crm_element_value(reason, XML_NVPAIR_ATTR_NAME),
                        crm_element_value(reason, XML_NVPAIR_ATTR_VALUE),
                        abort_text, add[0], add[1], add[2], fn, line, path,
-                       (transition_graph->complete? "true" : "false"));
+                       pcmk__btoa(transition_graph->complete));
 
         } else if (pcmk__str_eq(XML_LRM_TAG_RSC_OP, kind, pcmk__str_casei)) {
             const char *magic = crm_element_value(reason, XML_ATTR_TRANSITION_MAGIC);
@@ -254,7 +254,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                        crm_element_value(reason, XML_LRM_ATTR_TASK_KEY), op,
                        crm_element_value(reason, XML_LRM_ATTR_TARGET), abort_text,
                        magic, add[0], add[1], add[2], fn, line,
-                       (transition_graph->complete? "true" : "false"));
+                       pcmk__btoa(transition_graph->complete));
 
         } else if (pcmk__strcase_any_of(kind, XML_CIB_TAG_STATE, XML_CIB_TAG_NODE, NULL)) {
             const char *uname = crm_peer_uname(ID(reason));
@@ -264,7 +264,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                        transition_graph->id,
                        kind, op, (uname? uname : ID(reason)), abort_text,
                        add[0], add[1], add[2], fn, line,
-                       (transition_graph->complete? "true" : "false"));
+                       pcmk__btoa(transition_graph->complete));
 
         } else {
             const char *id = ID(reason);
@@ -274,7 +274,7 @@ abort_transition_graph(int abort_priority, enum transition_action abort_action,
                        transition_graph->id,
                        TYPE(reason), (id? id : ""), (op? op : "change"),
                        abort_text, add[0], add[1], add[2], fn, line, path,
-                       (transition_graph->complete? "true" : "false"));
+                       pcmk__btoa(transition_graph->complete));
         }
     }
 

commit d6294dd28b6d95ad3844824996717f9959d97ac6

Fix: Use correct boolean in stonith__validate_agent_xml
This fixes a regression introduced by 91a2b2e that flips the boolean
values for "valid" in the XML output.

Resolves: RHBZ#2102292 (partial)

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/fencing/st_output.c
@@ -528,10 +528,9 @@ validate_agent_xml(pcmk__output_t *out, va_list args) {
     char *error_output = va_arg(args, char *);
     int rc = va_arg(args, int);
 
-    xmlNodePtr node = pcmk__output_create_xml_node(out, "validate",
-                                                   "agent", agent,
-                                                   "valid", pcmk__btoa(rc),
-                                                   NULL);
+    xmlNodePtr node = pcmk__output_create_xml_node(
+        out, "validate", "agent", agent, "valid", pcmk__btoa(rc == pcmk_ok),
+        NULL);
 
     if (device != NULL) {
         crm_xml_add(node, "device", device);