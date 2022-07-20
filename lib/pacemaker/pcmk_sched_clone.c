39d35ffaf283ee64f2e8b9a36448bca2b7bdb5b2
Ken Gaillot <kgaillot@redhat.com>
2022-07-20 17:15:51 -0500

    Refactor: scheduler: functionize considering a clone dependent's scores
    ... for readability

--- lib/pacemaker/pcmk_sched_clone.c
+++ lib/pacemaker/pcmk_sched_clone.c
@@ -16,6 +16,29 @@
 
 /*!
  * \internal
+ * \brief Consider a colocation dependent's node scores
+ *
+ * \param[in,out] data       Colocation constraint to consider
+ * \param[in,out] user_data  Clone resource that is colocation primary
+ */
+static void
+add_dependent_scores(gpointer data, gpointer user_data)
+{
+    pcmk__colocation_t *colocation = (pcmk__colocation_t *) data;
+    pe_resource_t *clone = (pe_resource_t *) user_data;
+
+    if (pcmk__colocation_has_influence(colocation, NULL)) {
+        pcmk__add_colocated_node_scores(colocation->dependent, clone->id,
+                                        &clone->allowed_nodes,
+                                        colocation->node_attribute,
+                                        colocation->score / (float) INFINITY,
+                                        pcmk__coloc_select_active
+                                        |pcmk__coloc_select_nonnegative);
+    }
+}
+
+/*!
+ * \internal
  * \brief Assign a clone resource to a node
  *
  * \param[in,out] rsc     Resource to assign to a node
@@ -56,21 +79,7 @@ pcmk__clone_allocate(pe_resource_t *rsc, const pe_node_t *prefer)
      * Because the with_this_colocations() method boils down to a copy of
      * rsc_cons_lhs for clones, we can use that here directly for efficiency.
      */
-    for (GList *gIter = rsc->rsc_cons_lhs; gIter != NULL; gIter = gIter->next) {
-        pcmk__colocation_t *constraint = (pcmk__colocation_t *) gIter->data;
-
-        if (pcmk__colocation_has_influence(constraint, NULL)) {
-            pe_resource_t *dependent = constraint->dependent;
-            const char *attr = constraint->node_attribute;
-            const float factor = constraint->score / (float) INFINITY;
-            const uint32_t flags = pcmk__coloc_select_active
-                                   |pcmk__coloc_select_nonnegative;
-
-            pcmk__add_colocated_node_scores(dependent, rsc->id,
-                                            &rsc->allowed_nodes, attr, factor,
-                                            flags);
-        }
-    }
+    g_list_foreach(rsc->rsc_cons_lhs, add_dependent_scores, rsc);
 
     pe__show_node_weights(!pcmk_is_set(rsc->cluster->flags, pe_flag_show_scores),
                           rsc, __func__, rsc->allowed_nodes, rsc->cluster);
