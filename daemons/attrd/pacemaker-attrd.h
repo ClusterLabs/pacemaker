1b2365de76f3e8e150d39e3702e9e2b15b878ba4
Chris Lumens <clumens@redhat.com>
2022-07-25 16:52:09 -0400

    Refactor: daemons: Move IPC-related message handlers into attrd_ipc.c
    Code changes are making send_attrd_message public and making
    minimum_protocol_version accessible as an extern.

--- daemons/attrd/pacemaker-attrd.h
+++ daemons/attrd/pacemaker-attrd.h
@@ -157,6 +157,7 @@ void attrd_client_update(xmlNode *xml);
 void attrd_client_refresh(void);
 void attrd_client_query(pcmk__client_t *client, uint32_t id, uint32_t flags,
                         xmlNode *query);
+gboolean send_attrd_message(crm_node_t * node, xmlNode * data);
 
 void free_attribute(gpointer data);
 
