d4358b0f9a6bb462e66f69268e36a4d96a36d247
Chris Lumens <clumens@redhat.com>
2022-08-04 15:28:36 -0400

    Test: libcrmcommon: Add unit tests for very basic list macros.

---
+++ lib/common/tests/lists/pcmk__list_of_1_test.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+#include <crm/common/unittest_internal.h>
+
+#include <glib.h>
+
+static void
+empty_list(void **state) {
+    assert_false(pcmk__list_of_1(NULL));
+}
+
+static void
+singleton_list(void **state) {
+    GList *lst = NULL;
+
+    lst = g_list_append(lst, strdup("abc"));
+    assert_true(pcmk__list_of_1(lst));
+
+    g_list_free_full(lst, free);
+}
+
+static void
+longer_list(void **state) {
+    GList *lst = NULL;
+
+    lst = g_list_append(lst, strdup("abc"));
+    lst = g_list_append(lst, strdup("xyz"));
+    assert_false(pcmk__list_of_1(lst));
+
+    g_list_free_full(lst, free);
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(empty_list),
+        cmocka_unit_test(singleton_list),
+        cmocka_unit_test(longer_list),
+    };
+
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
