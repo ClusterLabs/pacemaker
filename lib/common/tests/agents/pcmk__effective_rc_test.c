ed05cc21190a51309c549126ce01efc0ca209ca9
Chris Lumens <clumens@redhat.com>
2022-08-08 10:08:24 -0400

    Test: libcrmcommon: Add tests for the rest of agents.c.

---
+++ lib/common/tests/agents/pcmk__effective_rc_test.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+#include <crm/common/unittest_internal.h>
+#include <crm/common/agents.h>
+
+static void
+pcmk__effective_rc_test(void **state) {
+    /* All other PCMK_OCF_* values after UNKNOWN are deprecated and no longer used,
+     * so probably not worth testing them.
+     */
+    assert_int_equal(PCMK_OCF_OK, pcmk__effective_rc(PCMK_OCF_OK));
+    assert_int_equal(PCMK_OCF_OK, pcmk__effective_rc(PCMK_OCF_DEGRADED));
+    assert_int_equal(PCMK_OCF_RUNNING_PROMOTED, pcmk__effective_rc(PCMK_OCF_DEGRADED_PROMOTED));
+    assert_int_equal(PCMK_OCF_UNKNOWN, pcmk__effective_rc(PCMK_OCF_UNKNOWN));
+
+    /* There's nothing that says pcmk__effective_rc is restricted to PCMK_OCF_*
+     * values.  That's just how it's used.  Let's check some values outside
+     * that range just to be sure.
+     */
+    assert_int_equal(-1, pcmk__effective_rc(-1));
+    assert_int_equal(255, pcmk__effective_rc(255));
+    assert_int_equal(INT_MAX, pcmk__effective_rc(INT_MAX));
+    assert_int_equal(INT_MIN, pcmk__effective_rc(INT_MIN));
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(pcmk__effective_rc_test),
+    };
+
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ lib/common/tests/agents/pcmk_get_ra_caps_test.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+#include <crm/common/unittest_internal.h>
+#include <crm/common/agents.h>
+
+static void
+ocf_standard(void **state) {
+    uint32_t expected = pcmk_ra_cap_provider | pcmk_ra_cap_params |
+                        pcmk_ra_cap_unique | pcmk_ra_cap_promotable;
+
+    assert_int_equal(pcmk_get_ra_caps("ocf"), expected);
+    assert_int_equal(pcmk_get_ra_caps("OCF"), expected);
+}
+
+static void
+stonith_standard(void **state) {
+    uint32_t expected = pcmk_ra_cap_params | pcmk_ra_cap_unique |
+                        pcmk_ra_cap_stdin | pcmk_ra_cap_fence_params;
+
+    assert_int_equal(pcmk_get_ra_caps("stonith"), expected);
+    assert_int_equal(pcmk_get_ra_caps("StOnItH"), expected);
+}
+
+static void
+service_standard(void **state) {
+    assert_int_equal(pcmk_get_ra_caps("systemd"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("SYSTEMD"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("service"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("SeRvIcE"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("lsb"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("LSB"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("upstart"), pcmk_ra_cap_status);
+    assert_int_equal(pcmk_get_ra_caps("uPsTaRt"), pcmk_ra_cap_status);
+}
+
+static void
+nagios_standard(void **state) {
+    assert_int_equal(pcmk_get_ra_caps("nagios"), pcmk_ra_cap_params);
+    assert_int_equal(pcmk_get_ra_caps("NAGios"), pcmk_ra_cap_params);
+}
+
+static void
+unknown_standard(void **state) {
+    assert_int_equal(pcmk_get_ra_caps("blahblah"), pcmk_ra_cap_none);
+    assert_int_equal(pcmk_get_ra_caps(""), pcmk_ra_cap_none);
+    assert_int_equal(pcmk_get_ra_caps(NULL), pcmk_ra_cap_none);
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(ocf_standard),
+        cmocka_unit_test(stonith_standard),
+        cmocka_unit_test(service_standard),
+        cmocka_unit_test(nagios_standard),
+        cmocka_unit_test(unknown_standard),
+    };
+
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
