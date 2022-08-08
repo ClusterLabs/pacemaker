cf059d09f37c82ef4c2f68e8b98654e18c736fe6
Chris Lumens <clumens@redhat.com>
2022-08-08 13:07:33 -0400

    Test: libcrmcommon: Add unit tests for pcmk__unregister_formats.
    These are very basic tests, but it is a very basic function.

---
+++ lib/common/tests/output/pcmk__unregister_formats_test.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+#include <crm/common/unittest_internal.h>
+#include <crm/common/output_internal.h>
+
+static pcmk__output_t *
+null_create_fn(char **argv) {
+    return NULL;
+}
+
+static void
+invalid_params(void **state) {
+    /* This is basically just here to make sure that calling pcmk__unregister_formats
+     * with formatters=NULL doesn't segfault.
+     */
+    pcmk__unregister_formats();
+    assert_null(pcmk__output_formatters());
+}
+
+static void
+non_null_formatters(void **state) {
+    pcmk__register_format(NULL, "fake", null_create_fn, NULL);
+
+    pcmk__unregister_formats();
+    assert_null(pcmk__output_formatters());
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(invalid_params),
+        cmocka_unit_test(non_null_formatters),
+    };
+
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
