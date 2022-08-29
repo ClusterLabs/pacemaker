f2ba8a45019b3cd6d13f6087190fffe7e779fd35
Reid Wahl <nrwahl@protonmail.com>
2022-08-29 13:11:29 -0700

    Low: libpacemaker: Add copyright to pcmk_cluster_queries.h
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/pcmki/pcmki_cluster_queries.h
+++ include/pcmki/pcmki_cluster_queries.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2020-2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
 #ifndef PCMK__PCMKI_PCMKI_CLUSTER_QUERIES__H
 #  define PCMK__PCMKI_PCMKI_CLUSTER_QUERIES__H
 
