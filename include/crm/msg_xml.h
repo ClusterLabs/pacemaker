a7a79c6c62aa4683e19d48cc28f3ff8a81cbd4a9
Reid Wahl <nrwahl@protonmail.com>
2022-09-04 23:03:36 -0700

    API: libcrmcommon: New PCMK_XE_DATE_EXPRESSION constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml.h
+++ include/crm/msg_xml.h
@@ -343,6 +343,7 @@ extern "C" {
 #  define XML_RULE_ATTR_BOOLEAN_OP	"boolean-op"
 
 #  define XML_TAG_EXPRESSION		"expression"
+#  define PCMK_XE_DATE_EXPRESSION	"date_expression"
 #  define XML_EXPR_ATTR_ATTRIBUTE	"attribute"
 #  define XML_EXPR_ATTR_OPERATION	"operation"
 #  define XML_EXPR_ATTR_VALUE		"value"
