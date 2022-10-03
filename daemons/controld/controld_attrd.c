531f7c1912a4e961dedfff00592bdfaa7713ea8b
Ken Gaillot <kgaillot@redhat.com>
2022-10-03 11:41:00 -0500

    Refactor: controller: simplify attribute functions
    Drop enum attrd_command, log_attrd_error(), and update_attrd_helper().
    De-functionizing actually drops the file size by 34 lines and improves
    readability.

--- daemons/controld/controld_attrd.c
+++ daemons/controld/controld_attrd.c
@@ -19,12 +19,6 @@
 
 static pcmk_ipc_api_t *attrd_api = NULL;
 
-enum attrd_command {
-    cmd_clear,
-    cmd_purge,
-    cmd_update
-};
-
 void
 controld_close_attrd_ipc(void)
 {
@@ -64,88 +58,31 @@ handle_attr_error(void)
     }
 }
 
-static void
-log_attrd_error(const char *host, const char *name, const char *value,
-                gboolean is_remote, enum attrd_command command, int rc)
-{
-    switch (command) {
-        case cmd_clear:
-            crm_err("Could not clear failure attributes for %s on %s node %s%s: %s "
-                    CRM_XS " rc=%d", (name? name : "all resources"),
-                    node_type(is_remote), host, when(), pcmk_rc_str(rc), rc);
-            break;
-
-        case cmd_purge:
-            crm_err("Could not purge %s node %s in attribute manager%s: %s "
-                    CRM_XS " rc=%d",
-                    node_type(is_remote), host, when(), pcmk_rc_str(rc), rc);
-            break;
-
-        case cmd_update:
-            /* We weren't able to update an attribute after several retries,
-             * so something is horribly wrong with the attribute manager or the
-             * underlying system.
-             */
-            do_crm_log(AM_I_DC? LOG_CRIT : LOG_ERR,
-                       "Could not update attribute %s=%s for %s node %s%s: %s "
-                       CRM_XS " rc=%d", name, value, node_type(is_remote), host,
-                       when(), pcmk_rc_str(rc), rc);
-            handle_attr_error();
-            break;
-    }
-}
-
-static void
-update_attrd_helper(const char *host, const char *name, const char *value,
-                    const char *interval_spec, const char *user_name,
-                    gboolean is_remote_node, enum attrd_command command)
-{
-    int rc;
-    int attrd_opts = pcmk__node_attr_none;
-
-    if (is_remote_node) {
-        pcmk__set_node_attr_flags(attrd_opts, pcmk__node_attr_remote);
-    }
-
-    if (attrd_api == NULL) {
-        rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
-
-        if (rc != pcmk_rc_ok) {
-            log_attrd_error(host, name, value, is_remote_node, command, rc);
-            return;
-        }
-    }
-
-    switch (command) {
-        case cmd_clear:
-            /* name/value is really resource/operation */
-            rc = pcmk__attrd_api_clear_failures(attrd_api, host, name,
-                                                value, interval_spec,
-                                                user_name, attrd_opts);
-            break;
-
-        case cmd_update:
-            rc = pcmk__attrd_api_update(attrd_api, host, name, value,
-                                        NULL, NULL, user_name,
-                                        attrd_opts | pcmk__node_attr_value);
-            break;
-
-        case cmd_purge:
-            rc = pcmk__attrd_api_purge(attrd_api, host);
-            break;
-    }
-
-    if (rc != pcmk_rc_ok) {
-        log_attrd_error(host, name, value, is_remote_node, command, rc);
-    }
-}
-
 void
 update_attrd(const char *host, const char *name, const char *value,
              const char *user_name, gboolean is_remote_node)
 {
-    update_attrd_helper(host, name, value, NULL, user_name, is_remote_node,
-                        cmd_update);
+    int rc = pcmk_rc_ok;
+
+    if (attrd_api == NULL) {
+        rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
+    }
+    if (rc == pcmk_rc_ok) {
+        uint32_t attrd_opts = pcmk__node_attr_value;
+
+        if (is_remote_node) {
+            pcmk__set_node_attr_flags(attrd_opts, pcmk__node_attr_remote);
+        }
+        rc = pcmk__attrd_api_update(attrd_api, host, name, value,
+                                    NULL, NULL, user_name, attrd_opts);
+    }
+    if (rc != pcmk_rc_ok) {
+        do_crm_log(AM_I_DC? LOG_CRIT : LOG_ERR,
+                   "Could not update attribute %s=%s for %s node %s%s: %s "
+                   CRM_XS " rc=%d", name, value, node_type(is_remote_node),
+                   host, when(), pcmk_rc_str(rc), rc);
+        handle_attr_error();
+    }
 }
 
 void
@@ -171,26 +108,52 @@ update_attrd_list(GList *attrs, uint32_t opts)
 void
 update_attrd_remote_node_removed(const char *host, const char *user_name)
 {
-    crm_trace("Asking attribute manager to purge Pacemaker Remote node %s",
-              host);
-    update_attrd_helper(host, NULL, NULL, NULL, user_name, TRUE, cmd_purge);
+    int rc = pcmk_rc_ok;
+
+    if (attrd_api == NULL) {
+        rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
+    }
+    if (rc == pcmk_rc_ok) {
+        crm_trace("Asking attribute manager to purge Pacemaker Remote node %s",
+                  host);
+        rc = pcmk__attrd_api_purge(attrd_api, host);
+    }
+    if (rc != pcmk_rc_ok) {
+        crm_err("Could not purge Pacemaker Remote node %s "
+                "in attribute manager%s: %s " CRM_XS " rc=%d",
+                host, when(), pcmk_rc_str(rc), rc);
+    }
 }
 
 void
 update_attrd_clear_failures(const char *host, const char *rsc, const char *op,
                             const char *interval_spec, gboolean is_remote_node)
 {
-    const char *op_desc = NULL;
-    const char *interval_desc = NULL;
+    int rc = pcmk_rc_ok;
 
-    if (op) {
-        interval_desc = interval_spec? interval_spec : "nonrecurring";
-        op_desc = op;
-    } else {
-        interval_desc = "all";
-        op_desc = "operations";
+    if (attrd_api == NULL) {
+        rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
     }
-    crm_info("Asking pacemaker-attrd to clear failure of %s %s for %s on %s node %s",
-             interval_desc, op_desc, rsc, node_type(is_remote_node), host);
-    update_attrd_helper(host, rsc, op, interval_spec, NULL, is_remote_node, cmd_clear);
+    if (rc == pcmk_rc_ok) {
+        const char *op_desc = pcmk__s(op, "operations");
+        const char *interval_desc = "all";
+        uint32_t attrd_opts = pcmk__node_attr_none;
+
+        if (op != NULL) {
+            interval_desc = pcmk__s(interval_spec, "nonrecurring");
+        }
+        if (is_remote_node) {
+            pcmk__set_node_attr_flags(attrd_opts, pcmk__node_attr_remote);
+        }
+        crm_info("Asking attribute manager to clear failure of %s %s for %s "
+                 "on %s node %s", interval_desc, op_desc, rsc,
+                 node_type(is_remote_node), host);
+        rc = pcmk__attrd_api_clear_failures(attrd_api, host, rsc, op,
+                                            interval_spec, NULL, attrd_opts);
+    }
+    if (rc != pcmk_rc_ok) {
+        crm_err("Could not clear failure attributes for %s on %s node %s%s: %s "
+                CRM_XS " rc=%d", pcmk__s(rsc, "all resources"),
+                node_type(is_remote_node), host, when(), pcmk_rc_str(rc), rc);
+    }
 }
