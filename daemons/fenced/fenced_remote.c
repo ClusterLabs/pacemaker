5eff7cd08f699d1bed7a3081893fd211792d41b5
Ken Gaillot <kgaillot@redhat.com>
2022-10-04 17:41:49 -0500

    Low: fencer: skip "on" in remapped reboot when pcmk_reboot_action="off"
    Previously, when a topology reboot was remapped to "all off then all on",
    the "on" phase would still be attempted for a device even if it has
    pcmk_reboot_action set to "off".
    
    Now, skip the device in that situation.
    
    Ref T461

--- daemons/fenced/fenced_remote.c
+++ daemons/fenced/fenced_remote.c
@@ -1729,6 +1729,27 @@ request_peer_fencing(remote_fencing_op_t *op, peer_device_info_t *peer)
     crm_trace("Action %.8s targeting %s for %s is %s",
               op->id, op->target, op->client_name,
               stonith_op_state_str(op->state));
+
+    if ((op->phase == st_phase_on) && (op->devices != NULL)) {
+        /* We are in the "on" phase of a remapped topology reboot. If this
+         * device has pcmk_reboot_action="off", skip it.
+         *
+         * We can't check device properties at this point because we haven't
+         * chosen a peer for this stage yet. Instead, we check the local node's
+         * configuration for the device, which should be the same across all
+         * nodes.
+         */
+        device = op->devices->data;
+        if (pcmk__str_eq(fenced_device_reboot_action(device), "off",
+                         pcmk__str_none)) {
+            crm_info("Not turning %s back on using %s because the device is "
+                     "configured to stay off (pcmk_reboot_action='off')",
+                     op->target, device);
+            advance_topology_device_in_level(op, device, NULL);
+            return;
+        }
+    }
+
     timeout = op->base_timeout;
     if ((peer == NULL) && !pcmk_is_set(op->call_options, st_opt_topology)) {
         peer = stonith_choose_peer(op);
