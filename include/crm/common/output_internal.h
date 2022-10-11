494e64ce4b6936a7b29f1278a11d0d152b81770b
Reid Wahl <nrwahl@protonmail.com>
2022-10-10 21:53:02 -0700

    Low: schemas: Copy API schemas in preparation for changes
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/output_internal.h
+++ include/crm/common/output_internal.h
@@ -28,7 +28,7 @@ extern "C" {
  */
 
 
-#  define PCMK__API_VERSION "2.24"
+#  define PCMK__API_VERSION "2.25"
 
 #if defined(PCMK__WITH_ATTRIBUTE_OUTPUT_ARGS)
 #  define PCMK__OUTPUT_ARGS(ARGS...) __attribute__((output_args(ARGS)))
