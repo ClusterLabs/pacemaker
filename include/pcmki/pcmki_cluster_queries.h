f5afde1f5224415711588a1bdd4251c19c2672d6
Reid Wahl <nrwahl@protonmail.com>
2022-11-08 23:29:00 -0800

    Refactor: libpacemaker: New pcmk__query_node_name() function
    This is a simple wrapper for pcmk__query_node_info() that gets the node
    name and nothing more. It doesn't output anything and it doesn't have
    any other output arguments.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/pcmki/pcmki_cluster_queries.h
+++ include/pcmki/pcmki_cluster_queries.h
@@ -25,10 +25,40 @@ int pcmk__controller_status(pcmk__output_t *out, const char *node_name,
                             unsigned int message_timeout_ms);
 int pcmk__designated_controller(pcmk__output_t *out,
                                 unsigned int message_timeout_ms);
+int pcmk__pacemakerd_status(pcmk__output_t *out, const char *ipc_name,
+                            unsigned int message_timeout_ms, bool show_output,
+                            enum pcmk_pacemakerd_state *state);
 int pcmk__query_node_info(pcmk__output_t *out, uint32_t *node_id,
                           char **node_name, char **uuid, char **state,
                           bool *have_quorum, bool *is_remote, bool show_output,
                           unsigned int message_timeout_ms);
+
+/*!
+ * \internal
+ * \brief Get the node name corresponding to a node ID from the controller
+ *
+ * \param[in,out] out                 Output object
+ * \param[in]     node_id             ID of node whose name to get (or 0 for
+ *                                    the local node)
+ * \param[out]    node_name           If not \p NULL, where to store the node
+ *                                    name
+ * \param[in]     message_timeout_ms  How long to wait for a reply from the
+ *                                    \p pacemaker-controld API. If 0,
+ *                                    \p pcmk_ipc_dispatch_sync will be used.
+ *                                    Otherwise, \p pcmk_ipc_dispatch_poll will
+ *                                    be used.
+ *
+ * \return Standard Pacemaker return code
+ *
+ * \note The caller is responsible for freeing \p *node_name using \p free().
+ */
+static inline int
+pcmk__query_node_name(pcmk__output_t *out, uint32_t nodeid, char **node_name,
+                      unsigned int message_timeout_ms)
+{
+    return pcmk__query_node_info(out, &nodeid, node_name, NULL, NULL, NULL,
+                                 NULL, false, message_timeout_ms);
+}
 
 // pacemakerd queries
 int pcmk__pacemakerd_status(pcmk__output_t *out, const char *ipc_name,
