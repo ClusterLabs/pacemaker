ClusterLabs/pacemaker

commit b2937595b2e83a836edb2953104e896be0ac9f5a

Refactor: Add a pcmk__xe_add_node function.
This function just adds node name and node ID to an XML request.  And
then there's lots of places it can be used.

daemons/attrd/attrd_corosync.c
@@ -551,7 +551,7 @@ attrd_peer_update(crm_node_t *peer, xmlNode *xml, const char *host, bool filter)
              child = crm_next_same_xml(child)) {
             /* Set the node name on the child message, assuming it isn't already. */
             if (crm_element_value(child, PCMK__XA_ATTR_NODE_NAME) == NULL) {
-                crm_xml_add(child, PCMK__XA_ATTR_NODE_NAME, host);
+                pcmk__xe_add_node(xml, host, 0);
             }
 
             attrd_peer_update_one(peer, child, filter);

daemons/attrd/attrd_attributes.c
@@ -119,10 +119,7 @@ attrd_add_value_xml(xmlNode *parent, attribute_t *a, attribute_value_t *v,
     crm_xml_add(xml, PCMK__XA_ATTR_SET, a->set);
     crm_xml_add(xml, PCMK__XA_ATTR_UUID, a->uuid);
     crm_xml_add(xml, PCMK__XA_ATTR_USER, a->user);
-    crm_xml_add(xml, PCMK__XA_ATTR_NODE_NAME, v->nodename);
-    if (v->nodeid > 0) {
-        crm_xml_add_int(xml, PCMK__XA_ATTR_NODE_ID, v->nodeid);
-    }
+    pcmk__xe_add_node(xml, v->nodename, v->nodeid);
     if (v->is_remote != 0) {
         crm_xml_add_int(xml, PCMK__XA_ATTR_IS_REMOTE, 1);
     }

daemons/attrd/attrd_ipc.c
@@ -72,7 +72,7 @@ static xmlNode *build_query_reply(const char *attr, const char *host)
                 free_xml(reply);
                 return NULL;
             }
-            crm_xml_add(host_value, PCMK__XA_ATTR_NODE_NAME, host);
+            pcmk__xe_add_node(host_value, host, 0);
             crm_xml_add(host_value, PCMK__XA_ATTR_VALUE,
                         (v? v->current : NULL));
 
@@ -87,7 +87,7 @@ static xmlNode *build_query_reply(const char *attr, const char *host)
                     free_xml(reply);
                     return NULL;
                 }
-                crm_xml_add(host_value, PCMK__XA_ATTR_NODE_NAME, v->nodename);
+                pcmk__xe_add_node(host_value, v->nodename, 0);
                 crm_xml_add(host_value, PCMK__XA_ATTR_VALUE, v->current);
             }
         }
@@ -179,7 +179,7 @@ attrd_client_peer_remove(pcmk__request_t *request)
                 host_alloc = get_node_name(nodeid);
                 host = host_alloc;
             }
-            crm_xml_add(xml, PCMK__XA_ATTR_NODE_NAME, host);
+            pcmk__xe_add_node(xml, host, 0);
         }
     }
 
@@ -328,8 +328,7 @@ attrd_client_update(pcmk__request_t *request)
     if (host == NULL) {
         crm_trace("Inferring host");
         host = strdup(attrd_cluster->uname);
-        crm_xml_add(xml, PCMK__XA_ATTR_NODE_NAME, host);
-        crm_xml_add_int(xml, PCMK__XA_ATTR_NODE_ID, attrd_cluster->nodeid);
+        pcmk__xe_add_node(xml, host, attrd_cluster->nodeid);
     }
 
     a = g_hash_table_lookup(attributes, attr);

daemons/attrd/attrd_messages.c
@@ -228,11 +228,7 @@ attrd_handle_request(pcmk__request_t *request)
 void
 attrd_broadcast_protocol(void)
 {
-    char *host = strdup(attrd_cluster->uname);
-
     xmlNode *attrd_op = create_xml_node(NULL, __func__);
-
-    CRM_ASSERT(host != NULL);
 
     crm_xml_add(attrd_op, F_TYPE, T_ATTRD);
     crm_xml_add(attrd_op, F_ORIG, crm_system_name);
@@ -240,15 +236,13 @@ attrd_broadcast_protocol(void)
     crm_xml_add(attrd_op, PCMK__XA_ATTR_NAME, CRM_ATTR_PROTOCOL);
     crm_xml_add(attrd_op, PCMK__XA_ATTR_VALUE, ATTRD_PROTOCOL_VERSION);
     crm_xml_add_int(attrd_op, PCMK__XA_ATTR_IS_PRIVATE, 1);
-    crm_xml_add(attrd_op, PCMK__XA_ATTR_NODE_NAME, host);
-    crm_xml_add_int(attrd_op, PCMK__XA_ATTR_NODE_ID, attrd_cluster->nodeid);
+    pcmk__xe_add_node(attrd_op, attrd_cluster->uname, attrd_cluster->nodeid);
 
     crm_debug("Broadcasting attrd protocol version %s for node %s",
-              ATTRD_PROTOCOL_VERSION, host);
+              ATTRD_PROTOCOL_VERSION, attrd_cluster->uname);
 
     attrd_send_message(NULL, attrd_op); /* ends up at attrd_peer_message() */
 
-    free(host);
     free_xml(attrd_op);
 }
 

include/crm/common/internal.h
@@ -60,6 +60,19 @@ int pcmk__add_mainloop_ipc(crm_ipc_t *ipc, int priority, void *userdata,
 guint pcmk__mainloop_timer_get_period(mainloop_timer_t *timer);
 
 
+/* internal node-related XML utilities (from nodes.c) */
+
+/*!
+ * \internal
+ * \brief Add local node name and ID to an XML node
+ *
+ * \param[in,out] request  XML node to modify
+ * \param[in]     node     The local node's name
+ * \param[in]     nodeid   The local node's ID (can be 0)
+ */
+void pcmk__xe_add_node(xmlNode *xml, const char *node, int nodeid);
+
+
 /* internal name/value utilities (from nvpair.c) */
 
 int pcmk__scan_nvpair(const char *input, char **name, char **value);

lib/common/Makefile.am
@@ -73,6 +73,7 @@ libcrmcommon_la_SOURCES	+= lists.c
 libcrmcommon_la_SOURCES	+= logging.c
 libcrmcommon_la_SOURCES	+= mainloop.c
 libcrmcommon_la_SOURCES	+= messages.c
+libcrmcommon_la_SOURCES	+= nodes.c
 libcrmcommon_la_SOURCES	+= nvpair.c
 libcrmcommon_la_SOURCES	+= operations.c
 libcrmcommon_la_SOURCES	+= options.c

lib/common/ipc_attrd.c
@@ -215,7 +215,7 @@ pcmk__attrd_api_clear_failures(pcmk_ipc_api_t *api, const char *node,
     }
 
     crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_CLEAR_FAILURE);
-    crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node);
+    pcmk__xe_add_node(request, node, 0);
     crm_xml_add(request, PCMK__XA_ATTR_RESOURCE, resource);
     crm_xml_add(request, PCMK__XA_ATTR_OPERATION, operation);
     crm_xml_add(request, PCMK__XA_ATTR_INTERVAL, interval_spec);
@@ -293,7 +293,7 @@ pcmk__attrd_api_purge(pcmk_ipc_api_t *api, const char *node)
     request = create_attrd_op(NULL);
 
     crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_PEER_REMOVE);
-    crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node);
+    pcmk__xe_add_node(request, node, 0);
 
     if (api == NULL) {
         rc = create_api(&api);
@@ -341,7 +341,7 @@ pcmk__attrd_api_query(pcmk_ipc_api_t *api, const char *node, const char *name,
 
     crm_xml_add(request, PCMK__XA_ATTR_NAME, name);
     crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_QUERY);
-    crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node);
+    pcmk__xe_add_node(request, node, 0);
 
     rc = send_attrd_request(api, request);
     free_xml(request);
@@ -372,7 +372,7 @@ pcmk__attrd_api_refresh(pcmk_ipc_api_t *api, const char *node)
     request = create_attrd_op(NULL);
 
     crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_REFRESH);
-    crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node);
+    pcmk__xe_add_node(request, node, 0);
 
     if (api == NULL) {
         rc = create_api(&api);
@@ -424,7 +424,7 @@ populate_update_op(xmlNode *op, const char *node, const char *name, const char *
 
     crm_xml_add(op, PCMK__XA_ATTR_VALUE, value);
     crm_xml_add(op, PCMK__XA_ATTR_DAMPENING, dampen);
-    crm_xml_add(op, PCMK__XA_ATTR_NODE_NAME, node);
+    pcmk__xe_add_node(op, node, 0);
     crm_xml_add(op, PCMK__XA_ATTR_SET, set);
     crm_xml_add_int(op, PCMK__XA_ATTR_IS_REMOTE,
                     pcmk_is_set(options, pcmk__node_attr_remote));

lib/common/ipc_client.c
@@ -709,10 +709,7 @@ create_purge_node_request(pcmk_ipc_api_t *api, const char *node_name,
             crm_xml_add(request, F_TYPE, T_ATTRD);
             crm_xml_add(request, F_ORIG, crm_system_name);
             crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_PEER_REMOVE);
-            crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node_name);
-            if (nodeid > 0) {
-                crm_xml_add_int(request, PCMK__XA_ATTR_NODE_ID, (int) nodeid);
-            }
+            pcmk__xe_add_node(request, node_name, nodeid);
             break;
 
         case pcmk_ipc_controld:

commit 012ae78322cf57f1bb3c1de32a2cc092b54a7890

Low: daemons: Set an attribute on the child of a multi-attr message.
It's already set on the parent.  It doesn't need to be set there again.
This was a thinko introduced with b2937595b2e83a836edb2953104e896be0ac9f5a.

daemons/attrd/attrd_corosync.c
@@ -552,7 +552,7 @@ attrd_peer_update(const crm_node_t *peer, xmlNode *xml, const char *host,
              child = crm_next_same_xml(child)) {
             /* Set the node name on the child message, assuming it isn't already. */
             if (crm_element_value(child, PCMK__XA_ATTR_NODE_NAME) == NULL) {
-                pcmk__xe_add_node(xml, host, 0);
+                pcmk__xe_add_node(child, host, 0);
             }
 
             attrd_peer_update_one(peer, child, filter);