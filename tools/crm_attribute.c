ClusterLabs/pacemaker

commit 22cabbb07ca626cb17b8c5ee5ebfb842d97fbf69

Refactor: tools: Use pcmk__str_update in crm_attribute where possible.

tools/crm_attribute.c
@@ -105,12 +105,7 @@ struct {
 static gboolean
 delete_cb(const gchar *option_name, const gchar *optarg, gpointer data, GError **error) {
     options.command = 'D';
-
-    if (options.attr_value) {
-        free(options.attr_value);
-    }
-
-    options.attr_value = NULL;
+    pcmk__str_update(&options.attr_value, NULL);
     return TRUE;
 }
 
@@ -149,24 +144,14 @@ utilization_cb(const gchar *option_name, const gchar *optarg, gpointer data, GEr
     }
 
     options.type = g_strdup(XML_CIB_TAG_NODES);
-
-    if (options.set_type) {
-        free(options.set_type);
-    }
-
-    options.set_type = strdup(XML_TAG_UTILIZATION);
+    pcmk__str_update(&options.attr_value, XML_TAG_UTILIZATION);
     return TRUE;
 }
 
 static gboolean
 value_cb(const gchar *option_name, const gchar *optarg, gpointer data, GError **error) {
     options.command = 'G';
-
-    if (options.attr_value) {
-        free(options.attr_value);
-    }
-
-    options.attr_value = NULL;
+    pcmk__str_update(&options.attr_value, NULL);
     return TRUE;
 }
 

commit 566fb0c5cea8f8d3162091f73238a952a7ca4654

Low: tools: Fix setting utilization attrs with crm_attribute.
Regression in 2.1.3 introduced by 22cabbb07c

tools/crm_attribute.c
@@ -147,7 +147,7 @@ utilization_cb(const gchar *option_name, const gchar *optarg, gpointer data, GEr
     }
 
     options.type = g_strdup(XML_CIB_TAG_NODES);
-    pcmk__str_update(&options.attr_value, XML_TAG_UTILIZATION);
+    pcmk__str_update(&options.set_type, XML_TAG_UTILIZATION);
     return TRUE;
 }
 