4704d02b8a2a67c71433befc4ab7d75c3398d166
Reid Wahl <nrwahl@protonmail.com>
2022-11-19 14:36:52 -0800

    Feature: pacemaker-based: Use formatted output in pacemaker-based
    Closes T104
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/based/pacemaker-based.c
+++ daemons/based/pacemaker-based.c
@@ -24,6 +24,7 @@
 #include <crm/cluster/internal.h>
 #include <crm/common/cmdline_internal.h>
 #include <crm/common/mainloop.h>
+#include <crm/common/output_internal.h>
 #include <crm/common/xml.h>
 
 #include <pacemaker-based.h>
@@ -141,12 +142,20 @@ static GOptionEntry entries[] = {
     { NULL }
 };
 
+static pcmk__supported_format_t formats[] = {
+    PCMK__SUPPORTED_FORMAT_NONE,
+    PCMK__SUPPORTED_FORMAT_TEXT,
+    PCMK__SUPPORTED_FORMAT_XML,
+    { NULL, NULL, NULL }
+};
+
 static GOptionContext *
-build_arg_context(pcmk__common_args_t *args)
+build_arg_context(pcmk__common_args_t *args, GOptionGroup **group)
 {
     GOptionContext *context = NULL;
 
-    context = pcmk__build_arg_context(args, NULL, NULL, "[metadata]");
+    context = pcmk__build_arg_context(args, "text (default), xml", group,
+                                      "[metadata]");
     pcmk__add_main_args(context, entries);
     return context;
 }
@@ -157,27 +166,35 @@ main(int argc, char **argv)
     int rc = pcmk_rc_ok;
     crm_ipc_t *old_instance = NULL;
 
+    pcmk__output_t *out = NULL;
+
     GError *error = NULL;
 
+    GOptionGroup *output_group = NULL;
     pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
     gchar **processed_args = pcmk__cmdline_preproc(argv, "r");
-    GOptionContext *context = build_arg_context(args);
+    GOptionContext *context = build_arg_context(args, &output_group);
 
     crm_log_preinit(NULL, argc, argv);
 
+    pcmk__register_formats(output_group, formats);
     if (!g_option_context_parse_strv(context, &processed_args, &error)) {
         exit_code = CRM_EX_USAGE;
         goto done;
     }
 
-    if (args->version) {
-        g_strfreev(processed_args);
-        pcmk__free_arg_context(context);
+    rc = pcmk__output_new(&out, args->output_ty, args->output_dest, argv);
+    if (rc != pcmk_rc_ok) {
+        exit_code = CRM_EX_ERROR;
+        g_set_error(&error, PCMK__EXITC_ERROR, exit_code,
+                    "Error creating output format %s: %s",
+                    args->output_ty, pcmk_rc_str(rc));
+        goto done;
+    }
 
-        /* FIXME: When pacemaker-based is converted to use formatted output,
-         * this can go.
-         */
-        pcmk__cli_help('v', CRM_EX_OK);
+    if (args->version) {
+        out->version(out, false);
+        goto done;
     }
 
     mainloop_add_signal(SIGTERM, cib_shutdown);
@@ -259,7 +276,12 @@ done:
     g_strfreev(processed_args);
     pcmk__free_arg_context(context);
 
-    pcmk__output_and_clear_error(error, NULL);
+    pcmk__output_and_clear_error(error, out);
+
+    if (out != NULL) {
+        out->finish(out, exit_code, true, NULL);
+        pcmk__output_free(out);
+    }
     crm_exit(exit_code);
 }
 
