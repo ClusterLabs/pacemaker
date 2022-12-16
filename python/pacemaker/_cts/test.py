5c7cb22695706eeaaa426d6d1203bc699f746178
Chris Lumens <clumens@redhat.com>
2022-12-16 10:04:33 -0500

    Refactor: cts,python: Add a clean_environment method to the Test class.
    However, cleaning up is a very specialized operation so all it does is
    pass, leaving the child classes to do the real work.

--- python/pacemaker/_cts/test.py
+++ python/pacemaker/_cts/test.py
@@ -190,6 +190,10 @@ class Test:
 
         self._new_cmd(cmd, args, ExitStatus.OK, no_wait=True)
 
+    def clean_environment(self):
+        """ Clean up the host after executing a test """
+        raise NotImplementedError("clean_environment not provided by subclass")
+
     def print_result(self, filler):
         """ Print the result of the last test execution """
 
