a0e20d0ab6dd8df0da92f8000557250c61c82a39
Chris Lumens <clumens@redhat.com>
2022-12-21 15:44:32 -0500

    Refactor: cts,python: Move duplicate exception classes into _cts.

---
+++ python/pacemaker/_cts/errors.py
@@ -0,0 +1,50 @@
+""" A module providing custom exception classes used throughout the pacemaker library """
+
+__all__ = ["ExitCodeError", "OutputFoundError", "OutputNotFoundError", "XmlValidationError"]
+__copyright__ = "Copyright 2009-2023 the Pacemaker project contributors"
+__license__ = "LGPLv2.1+"
+
+
+class TestError(Exception):
+    """ Base class for exceptions in this module """
+    pass
+
+
+class ExitCodeError(TestError):
+    """ Exception raised when command exit status is unexpected """
+
+    def __init__(self, exit_code):
+        self.exit_code = exit_code
+
+    def __str__(self):
+        return repr(self.exit_code)
+
+
+class OutputNotFoundError(TestError):
+    """ Exception raised when command output does not contain wanted string """
+
+    def __init__(self, output):
+        self.output = output
+
+    def __str__(self):
+        return repr(self.output)
+
+
+class OutputFoundError(TestError):
+    """ Exception raised when command output contains unwanted string """
+
+    def __init__(self, output):
+        self.output = output
+
+    def __str__(self):
+        return repr(self.output)
+
+
+class XmlValidationError(TestError):
+    """ Exception raised when xmllint fails """
+
+    def __init__(self, output):
+        self.output = output
+
+    def __str__(self):
+        return repr(self.output)
