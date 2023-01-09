0c6471f74e7df5dd4539bc56c4c3bcef8976308f
VLiamin <LyaminVA@yandex.ru>
2023-01-09 01:34:05 -0500

    move hashTable to new file

---
+++ lib/pacemaker/pcmk_hashTable.c
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021-2022 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU General Public License version 2
+ * or later (GPLv2+) WITHOUT ANY WARRANTY.
+ */
+
+#include <pacemaker-internal.h>
+#include "libpacemaker_private.h"
+
+const int capacity = 200;
+
+int
+getHash(const char *S);
+
+void
+push(Node **head, pe_resource_t* data);
+
+void
+free_item(Node *item);
+
+void
+push(Node **head, pe_resource_t* data) {
+    Node *tmp = (Node *) malloc(sizeof(Node));
+    tmp->value = data;
+    tmp->next = (*head);
+    (*head) = tmp;
+}
+
+int
+getHash(const char *S)
+{
+    int i = 0;
+    int r = 0;
+
+    while(*S)
+    {
+        i++;
+        r+=(int)(*S);
+        S = S + 3;
+    }
+
+    return r % capacity;
+}
+
+void
+init_array(struct set **array)
+{
+    struct set *tmp = (struct set *) malloc(capacity * sizeof(struct set));
+    for (int i = 0; i < capacity; i++)
+    {
+        tmp[i].key = i;
+        tmp[i].head = NULL;
+    }
+    (*array) = tmp;
+}
+
+void
+free_item(Node *item) {
+    free(item);
+}
+
+void
+free_table(struct set *array) {
+    for (int i=0; i <capacity; i++) {
+        Node *item = array[i].head;
+        while (item != NULL) {
+            Node *tmp = item->next;
+            free_item(item);
+            item = tmp;
+        }
+    }
+    free(array);
+}
+
+void
+insert(char* key, pe_resource_t* data, struct set *array)
+{
+    int index = getHash(key);
+    if (array[index].head == NULL)
+    {
+        Node *head = NULL;
+        array[index].key = index;
+        push(&head, data);
+        array[index].head = head;
+    }
+    else if (array[index].key == index)
+    {
+        push(&array[index].head, data);
+    }
+}
+
+pe_resource_t *
+find(const char* key, struct set *array)
+{
+    int index = getHash(key);
+    if (array[index].head == 0)
+    {
+        return NULL;
+    }
+    else
+    {
+        Node *ptr = array[index].head;
+        while (ptr != NULL)
+        {
+            if (!strcmp(key, ptr->value->id)) {
+                return ptr->value;
+            }
+
+            ptr = ptr->next;
+        }
+
+        return NULL;
+    }
+}
+
--- lib/pacemaker/pcmk_sched_constraints.c
+++ lib/pacemaker/pcmk_sched_constraints.c
@@ -27,43 +27,7 @@
 #include <pacemaker-internal.h>
 #include "libpacemaker_private.h"
 
-typedef struct Node {
-    pe_resource_t* value;
-    struct Node *next;
-} Node;
-
-struct set
-{
-    int key;
-    Node* head;
-};
-
-int capacity = 200;
 static struct set *hashTable = NULL;
-
-pe_resource_t *
-find(const char* key, struct set *array);
-
-int
-getHash(const char *S);
-
-void
-push(Node **head, pe_resource_t* data);
-
-void
-insert(char* key, pe_resource_t* data, struct set *array);
-
-void
-init_array(struct set **array);
-
-void
-free_item(Node *item);
-
-void
-free_table(struct set *array);
-
-void
-insert_children(pe_resource_t * rsc, struct set *hashTable);
 
 static bool
 evaluate_lifetime(xmlNode *lifetime, pe_working_set_t *data_set)
@@ -81,102 +45,7 @@ evaluate_lifetime(xmlNode *lifetime, pe_working_set_t *data_set)
     return result;
 }
 
-void
-push(Node **head, pe_resource_t* data) {
-    Node *tmp = (Node *) malloc(sizeof(Node));
-    tmp->value = data;
-    tmp->next = (*head);
-    (*head) = tmp;
-}
-
-int
-getHash(const char *S)
-{
-    int i = 0;
-    int r = 0;
-
-    while(*S)
-    {
-        i++;
-        r+=(int)(*S);
-        S = S + 3;
-    }
-
-    return r % capacity;
-}
-
-void
-init_array(struct set **array)
-{
-    struct set *tmp = (struct set *) malloc(capacity * sizeof(struct set));
-    for (int i = 0; i < capacity; i++)
-    {
-        tmp[i].key = i;
-        tmp[i].head = NULL;
-    }
-    (*array) = tmp;
-}
-
-void
-free_item(Node *item) {
-    free(item);
-}
-
-void
-free_table(struct set *array) {
-    for (int i=0; i <capacity; i++) {
-        Node *item = array[i].head;
-        while (item != NULL) {
-            Node *tmp = item->next;
-            free_item(item);
-            item = tmp;
-        }
-    }
-    free(array);
-}
-
-void
-insert(char* key, pe_resource_t* data, struct set *array)
-{
-    int index = getHash(key);
-    if (array[index].head == NULL)
-    {
-        Node *head = NULL;
-        array[index].key = index;
-        push(&head, data);
-        array[index].head = head;
-    }
-    else if (array[index].key == index)
-    {
-        push(&array[index].head, data);
-    }
-}
-
-pe_resource_t *
-find(const char* key, struct set *array)
-{
-    int index = getHash(key);
-    if (array[index].head == 0)
-    {
-        return NULL;
-    }
-    else
-    {
-        Node *ptr = array[index].head;
-        while (ptr != NULL)
-        {
-            if (!strcmp(key, ptr->value->id)) {
-                return ptr->value;
-            }
-
-            ptr = ptr->next;
-        }
-
-        return NULL;
-    }
-}
-
-void
+static void
 insert_children(pe_resource_t * rsc, struct set *hashTable) {
     for (GList *gIter = rsc->children; gIter != NULL; gIter = gIter->next) {
         pe_resource_t *child = (pe_resource_t *) gIter->data;
