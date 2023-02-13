5dcf6a4dd4aefa74b1eae0db8359161074f791f3
Reid Wahl <nrwahl@protonmail.com>
2023-02-12 16:49:21 -0800

    Doc: libcrmcommon: Fix copyright in xml_display.c
    When we split this into its own file from xml.c, we added a copyright
    date of 2023. However, the code is older than that.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/xml_display.c
+++ lib/common/xml_display.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the Pacemaker project contributors
+ * Copyright 2004-2023 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
