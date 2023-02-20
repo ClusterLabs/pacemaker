ClusterLabs/pacemaker

commit eb20a65577c34175f32dabd72e3ac748ead3ea3f

Refactor: libcrmcommon: Call pcmk__node_attr_target from new API functions.

lib/common/ipc_attrd.c
@@ -159,6 +159,11 @@ pcmk__attrd_api_clear_failures(pcmk_ipc_api_t *api, const char *node,
     xmlNode *request = create_attrd_op(user_name);
     const char *interval_desc = NULL;
     const char *op_desc = NULL;
+    const char *target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
+    }
 
     crm_xml_add(request, PCMK__XA_TASK, PCMK__ATTRD_CMD_CLEAR_FAILURE);
     crm_xml_add(request, PCMK__XA_ATTR_NODE_NAME, node);
@@ -190,8 +195,16 @@ int
 pcmk__attrd_api_delete(pcmk_ipc_api_t *api, const char *node, const char *name,
                        uint32_t options)
 {
+    const char *target = NULL;
+
     if (name == NULL) {
         return EINVAL;
+    }
+
+    target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
     }
 
     /* Make sure the right update option is set. */
@@ -207,6 +220,11 @@ pcmk__attrd_api_purge(pcmk_ipc_api_t *api, const char *node)
     int rc = pcmk_rc_ok;
     xmlNode *request = NULL;
     const char *display_host = (node ? node : "localhost");
+    const char *target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
+    }
 
     request = create_attrd_op(NULL);
 
@@ -228,9 +246,16 @@ pcmk__attrd_api_query(pcmk_ipc_api_t *api, const char *node, const char *name,
 {
     int rc = pcmk_rc_ok;
     xmlNode *request = NULL;
+    const char *target = NULL;
 
     if (name == NULL) {
         return EINVAL;
+    }
+
+    target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
     }
 
     request = create_attrd_op(NULL);
@@ -259,6 +284,11 @@ pcmk__attrd_api_refresh(pcmk_ipc_api_t *api, const char *node)
     int rc = pcmk_rc_ok;
     xmlNode *request = NULL;
     const char *display_host = (node ? node : "localhost");
+    const char *target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
+    }
 
     request = create_attrd_op(NULL);
 
@@ -282,9 +312,16 @@ pcmk__attrd_api_update(pcmk_ipc_api_t *api, const char *node, const char *name,
     int rc = pcmk_rc_ok;
     xmlNode *request = NULL;
     const char *display_host = (node ? node : "localhost");
+    const char *target = NULL;
 
     if (name == NULL) {
         return EINVAL;
+    }
+
+    target = pcmk__node_attr_target(node);
+
+    if (target != NULL) {
+        node = target;
     }
 
     request = create_attrd_op(user_name);

tools/attrd_updater.c
@@ -263,10 +263,7 @@ main(int argc, char **argv)
          * However, it's not a big problem, because pacemaker-attrd will learn
          * and remember a node's "remoteness".
          */
-        const char *target = pcmk__node_attr_target(options.attr_node);
-
-        int rc = send_attrd_update(options.command,
-                                   target == NULL ? options.attr_node : target,
+        int rc = send_attrd_update(options.command, options.attr_node,
                                    options.attr_name, options.attr_value,
                                    options.attr_set, options.attr_dampen,
                                    options.attr_options);
@@ -366,11 +363,6 @@ send_attrd_query(pcmk__output_t *out, const char *attr_name, const char *attr_no
     /* Decide which node(s) to query */
     if (query_all == TRUE) {
         attr_node = NULL;
-    } else {
-        const char *target = pcmk__node_attr_target(attr_node);
-        if (target != NULL) {
-            attr_node = target;
-        }
     }
 
     rc = pcmk__attrd_api_query(attrd_api, attr_node, attr_name, 0);
@@ -398,7 +390,6 @@ send_attrd_update(char command, const char *attr_node, const char *attr_name,
     pcmk_ipc_api_t *attrd_api = NULL;
     int rc = pcmk_rc_ok;
     xmlNode *reply = NULL;
-    const char *target = NULL;
 
     // Create attrd IPC object
     rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
@@ -417,11 +408,6 @@ send_attrd_update(char command, const char *attr_node, const char *attr_name,
                 pcmk_rc_str(rc));
         pcmk_free_ipc_api(attrd_api);
         return rc;
-    }
-
-    target = pcmk__node_attr_target(attr_node);
-    if (target != NULL) {
-        attr_node = target;
     }
 
     switch (command) {

tools/notifyServicelogEvent.c
@@ -77,7 +77,6 @@ send_attrd_update(const char *attr_node, const char *attr_name,
 {
     pcmk_ipc_api_t *attrd_api = NULL;
     int rc = pcmk_rc_ok;
-    const char *target = NULL;
 
     // Create attrd IPC object
     rc = pcmk_new_ipc_api(&attrd_api, pcmk_ipc_attrd);
@@ -94,11 +93,6 @@ send_attrd_update(const char *attr_node, const char *attr_name,
                 pcmk_rc_str(rc));
         pcmk_free_ipc_api(attrd_api);
         return rc;
-    }
-
-    target = pcmk__node_attr_target(attr_node);
-    if (target != NULL) {
-        attr_node = target;
     }
 
     rc = pcmk__attrd_api_update(attrd_api, attr_node, attr_name, attr_value,

commit 160a6afc8294ddec3f5b5770b218f4aea6c53fcf

High: libcrmcommon: Fix handling node=NULL in pcmk__attrd_api_query.
According to the header file, if node is NULL, pcmk__attrd_api_query
should query the value of the given attribute on all cluster nodes.
This is also what the server expects and how attrd_updater is supposed
to work.

However, pcmk__attrd_api_query has no way of letting callers decide
whether they want to query all nodes or whether they want to use the
local node.  We were passing NULL for the node name, which it took to
mean it should look up the local node name.  This calls
pcmk__node_attr_target, which probes the local cluster name and returns
that to pcmk__attrd_api_query.  If it returns non-NULL, that value will
then be put into the XML IPC call which means the server will only
return the value for that node.

In testing this was usually fine.  However, in pratice, the methods
pcmk__node_attr_target uses to figure out the local cluster node name
involves checking the OCF_RESKEY_CRM_meta_on_node environment variable
among others.

This variable was never set in testing, but can be set in the real
world.  This leads to circumstances where the user did "attrd_updater -QA"
expecting to get the values on all nodes, but instead only got the value
on the local cluster node.

In pacemaker-2.1.4 and prior, pcmk__node_attr_target was simply never
called if the node was NULL but was called otherwise.

The fix is to modify pcmk__attrd_api_query to take an option for
querying all nodes.  If that's present, we'll query all nodes.  If it's
not present, we'll look at the given node name - NULL means look it up,
anything else means just that node.

Regression in 2.1.5 introduced by eb20a65577

include/crm/common/attrd_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the Pacemaker project contributors
+ * Copyright 2004-2023 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -26,6 +26,7 @@ enum pcmk__node_attr_opts {
     pcmk__node_attr_sync_local     = (1 << 6),
     pcmk__node_attr_sync_cluster   = (1 << 7),
     pcmk__node_attr_utilization    = (1 << 8),
+    pcmk__node_attr_query_all      = (1 << 9),
 };
 
 #define pcmk__set_node_attr_flags(node_attr_flags, flags_to_set) do {   \

include/crm/common/ipc_attrd_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the Pacemaker project contributors
+ * Copyright 2022-2023 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -110,9 +110,12 @@ int pcmk__attrd_api_purge(pcmk_ipc_api_t *api, const char *node);
  *
  * \param[in,out] api           Connection to pacemaker-attrd
  * \param[in]     node          Look up the attribute for this node
- *                              (or NULL for all nodes)
+ *                              (or NULL for the local node)
  * \param[in]     name          Attribute name
  * \param[in]     options       Bitmask of pcmk__node_attr_opts
+ *
+ * \note Passing pcmk__node_attr_query_all will cause the function to query
+ *       the value of \p name on all nodes, regardless of the value of \p node.
  *
  * \return Standard Pacemaker return code
  */

lib/common/ipc_attrd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2022 the Pacemaker project contributors
+ * Copyright 2011-2023 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -332,10 +332,14 @@ pcmk__attrd_api_query(pcmk_ipc_api_t *api, const char *node, const char *name,
         return EINVAL;
     }
 
-    target = pcmk__node_attr_target(node);
+    if (pcmk_is_set(options, pcmk__node_attr_query_all)) {
+        node = NULL;
+    } else {
+        target = pcmk__node_attr_target(node);
 
-    if (target != NULL) {
-        node = target;
+        if (target != NULL) {
+            node = target;
+        }
     }
 
     request = create_attrd_op(NULL);

tools/attrd_updater.c
@@ -431,6 +431,7 @@ static int
 send_attrd_query(pcmk__output_t *out, const char *attr_name,
                  const char *attr_node, gboolean query_all)
 {
+    uint32_t options = pcmk__node_attr_none;
     pcmk_ipc_api_t *attrd_api = NULL;
     int rc = pcmk_rc_ok;
 
@@ -455,10 +456,10 @@ send_attrd_query(pcmk__output_t *out, const char *attr_name,
 
     /* Decide which node(s) to query */
     if (query_all == TRUE) {
-        attr_node = NULL;
+        options |= pcmk__node_attr_query_all;
     }
 
-    rc = pcmk__attrd_api_query(attrd_api, attr_node, attr_name, 0);
+    rc = pcmk__attrd_api_query(attrd_api, attr_node, attr_name, options);
 
     if (rc != pcmk_rc_ok) {
         g_set_error(&error, PCMK__RC_ERROR, rc, "Could not query value of %s: %s (%d)",