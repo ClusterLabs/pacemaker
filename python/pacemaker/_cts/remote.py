f7401d2f306f2157cb79094d8ae3430b5b22febf
Chris Lumens <clumens@redhat.com>
2023-03-16 14:36:28 -0400

    Refactor: python: Add comments to the remote module.

--- python/pacemaker/_cts/remote.py
+++ python/pacemaker/_cts/remote.py
@@ -1,5 +1,4 @@
-""" Remote command runner for Pacemaker's Cluster Test Suite (CTS)
-"""
+""" Remote command runner for Pacemaker's Cluster Test Suite (CTS) """
 
 __all__ = ["RemoteExec", "RemoteFactory"]
 __copyright__ = "Copyright 2014-2023 the Pacemaker project contributors"
@@ -14,6 +13,10 @@ from threading import Thread
 from pacemaker._cts.logging import LogFactory
 
 def convert2string(lines):
+    """ Convert a byte string to a UTF-8 string, and a list of byte strings to
+        a list of UTF-8 strings.  All other text formats are passed through.
+    """
+
     if isinstance(lines, bytes):
         return lines.decode("utf-8")
 
@@ -30,7 +33,22 @@ def convert2string(lines):
     return lines
 
 class AsyncCmd(Thread):
+    """ A class for doing the hard work of running a command on another machine """
+
     def __init__(self, node, command, proc=None, delegate=None):
+        """ Create a new AsyncCmd instance
+
+            Arguments:
+
+            node     -- The remote machine to run on
+            command  -- The ssh command string to use for remote execution
+            proc     -- If not None, a process object previously created with Popen.
+                        Instead of spawning a new process, we will then wait on
+                        this process to finish and handle its output.
+            delegate -- When the command completes, call the async_complete method
+                        on this object
+        """
+
         self._command = command
         self._delegate = delegate
         self._logger = LogFactory()
@@ -40,6 +58,8 @@ class AsyncCmd(Thread):
         Thread.__init__(self)
 
     def run(self):
+        """ Run the previously instantiated AsyncCmd object """
+
         out = None
         err = None
 
@@ -72,13 +92,20 @@ class AsyncCmd(Thread):
             self._delegate.async_complete(self._proc.pid, self._proc.returncode, out, err)
 
 class RemoteExec:
-    '''This is an abstract remote execution class.  It runs a command on another
-       machine - somehow.  The somehow is up to us.  This particular
-       class uses ssh.
-       Most of the work is done by fork/exec of ssh or scp.
-    '''
+    """ An abstract class for remote execution.  It runs a command on another
+        machine using ssh and scp.
+    """
 
     def __init__(self, command, cp_command, silent=False):
+        """ Create a new RemoteExec instance
+
+            Arguments:
+
+            command    -- The ssh command string to use for remote execution
+            cp_command -- The scp command string to use for copying files
+            silent     -- Should we log command status?
+        """
+
         self._command = command
         self._cp_command = cp_command
         self._logger = LogFactory()
@@ -86,12 +113,14 @@ class RemoteExec:
         self._our_node = os.uname()[1].lower()
 
     def _fixcmd(self, cmd):
+        """ Perform shell escapes on certain characters in the input cmd string """
+
         return re.sub("\'", "'\\''", cmd)
 
     def _cmd(self, args):
-
-        '''Compute the string that will run the given command on the
-        given remote system'''
+        """ Given a list of arguments, return the string that will be run on the
+            remote system
+        """
 
         sysname = args[0]
         command = args[1]
@@ -104,14 +133,33 @@ class RemoteExec:
         return ret
 
     def _log(self, args):
+        """ Log a message """
+
         if not self._silent:
             self._logger.log(args)
 
     def _debug(self, args):
+        """ Log a message at the debug level """
+
         if not self._silent:
             self._logger.debug(args)
 
     def call_async(self, node, command, delegate=None):
+        """ Run the given command on the given remote system and do not wait for
+            it to complete.
+
+            Arguments:
+
+            node     -- The remote machine to run on
+            command  -- The command to run, as a string
+            delegate -- When the command completes, call the async_complete method
+                        on this object
+
+            Returns:
+
+            The running process object
+        """
+
         aproc = AsyncCmd(node, self._cmd([node, command]), delegate=delegate)
         aproc.start()
         return aproc
@@ -172,7 +220,14 @@ class RemoteExec:
         return (rc, result)
 
     def copy(self, source, target, silent=False):
-        '''Perform a remote copy'''
+        """ Perform a copy of the source file to the remote target, using the
+            cp_command provided when the RemoteExec object was created.
+
+            Returns:
+
+            The return code of the cp_command
+        """
+
         cmd = "%s '%s' '%s'" % (self._cp_command, source, target)
         rc = os.system(cmd)
 
@@ -193,6 +248,8 @@ class RemoteExec:
 
 
 class RemoteFactory:
+    """ A class for constructing a singleton instance of a RemoteExec object """
+
     # Class variables
 
     # -n: no stdin, -x: no X11,
@@ -208,6 +265,10 @@ class RemoteFactory:
     instance = None
 
     def getInstance(self):
+        """ Returns the previously created instance of RemoteExec, or creates a
+            new instance if one does not already exist.
+        """
+
         if not RemoteFactory.instance:
             RemoteFactory.instance = RemoteExec(RemoteFactory.command,
                                                 RemoteFactory.cp_command,
@@ -215,6 +276,8 @@ class RemoteFactory:
         return RemoteFactory.instance
 
     def enable_qarsh(self):
+        """ Enable the QA remote shell """
+
         # http://nstraz.wordpress.com/2008/12/03/introducing-qarsh/
         print("Using QARSH for connections to cluster nodes")
 
