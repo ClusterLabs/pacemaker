d0307bcd0168f21aa1522727f8348ee37329d98a
Chris Lumens <clumens@redhat.com>
2023-04-12 15:57:01 -0400

    Refactor: libpacemaker: Expect an int instead of a uint32_t for node_id.
    The value being passed to these messages is always an int anyway.

--- lib/pacemaker/pcmk_output.c
+++ lib/pacemaker/pcmk_output.c
@@ -1291,12 +1291,12 @@ node_action_xml(pcmk__output_t *out, va_list args)
     return pcmk_rc_ok;
 }
 
-PCMK__OUTPUT_ARGS("node-info", "uint32_t", "const char *", "const char *",
+PCMK__OUTPUT_ARGS("node-info", "int", "const char *", "const char *",
                   "const char *", "bool", "bool")
 static int
 node_info_default(pcmk__output_t *out, va_list args)
 {
-    uint32_t node_id = va_arg(args, uint32_t);
+    int node_id = va_arg(args, int);
     const char *node_name = va_arg(args, const char *);
     const char *uuid = va_arg(args, const char *);
     const char *state = va_arg(args, const char *);
@@ -1304,26 +1304,26 @@ node_info_default(pcmk__output_t *out, va_list args)
     bool is_remote = (bool) va_arg(args, int);
 
     return out->info(out,
-                     "Node %" PRIu32 ": %s "
+                     "Node %d: %s "
                      "(uuid=%s, state=%s, have_quorum=%s, is_remote=%s)",
                      node_id, pcmk__s(node_name, "unknown"),
                      pcmk__s(uuid, "unknown"), pcmk__s(state, "unknown"),
                      pcmk__btoa(have_quorum), pcmk__btoa(is_remote));
 }
 
-PCMK__OUTPUT_ARGS("node-info", "uint32_t", "const char *", "bool", "bool",
-                  "const char *", "const char *")
+PCMK__OUTPUT_ARGS("node-info", "int", "const char *", "const char *",
+                  "const char *", "bool", "bool")
 static int
 node_info_xml(pcmk__output_t *out, va_list args)
 {
-    uint32_t node_id = va_arg(args, uint32_t);
+    int node_id = va_arg(args, int);
     const char *node_name = va_arg(args, const char *);
     const char *uuid = va_arg(args, const char *);
     const char *state = va_arg(args, const char *);
     bool have_quorum = (bool) va_arg(args, int);
     bool is_remote = (bool) va_arg(args, int);
 
-    char *id_s = crm_strdup_printf("%" PRIu32, node_id);
+    char *id_s = crm_strdup_printf("%d", node_id);
 
     pcmk__output_create_xml_node(out, "node-info",
                                  "nodeid", id_s,
