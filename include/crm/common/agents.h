ec3216554ebf3c649d3abbb9c466a51f3e085708
Ken Gaillot <kgaillot@redhat.com>
2023-04-27 10:56:41 -0500

    API: libcrmcommon: deprecate PCMK_RESOURCE_CLASS_UPSTART

--- include/crm/common/agents.h
+++ include/crm/common/agents.h
@@ -28,11 +28,12 @@ extern "C" {
 #define PCMK_RESOURCE_CLASS_SERVICE "service"
 #define PCMK_RESOURCE_CLASS_LSB     "lsb"
 #define PCMK_RESOURCE_CLASS_SYSTEMD "systemd"
-#define PCMK_RESOURCE_CLASS_UPSTART "upstart"
 #define PCMK_RESOURCE_CLASS_STONITH "stonith"
 #define PCMK_RESOURCE_CLASS_ALERT   "alert"
 //! \deprecated Do not use
 #define PCMK_RESOURCE_CLASS_NAGIOS  "nagios"
+//! \deprecated Do not use
+#define PCMK_RESOURCE_CLASS_UPSTART "upstart"
 
 /* Special stonith-class agent parameters interpreted directly by Pacemaker
  * (not including the pcmk_ACTION_{action,retries,timeout} parameters)
