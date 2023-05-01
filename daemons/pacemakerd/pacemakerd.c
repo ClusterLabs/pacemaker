ClusterLabs/pacemaker

commit 64d4f9b5f600b08c18969299d5fd207b92cdafeb

Feature: daemons: Use the new IPC API in pacemakerd for shutdown.

daemons/pacemakerd/pacemakerd.c
@@ -23,6 +23,7 @@
 #include <crm/crm.h>  /* indirectly: CRM_EX_* */
 #include <crm/msg_xml.h>
 #include <crm/common/mainloop.h>
+#include <crm/common/ipc_pacemakerd.h>
 #include <crm/cluster/internal.h>
 #include <crm/cluster.h>
 
@@ -163,9 +164,36 @@ remove_core_file_limit(void)
     }
 }
 
+static void
+pacemakerd_event_cb(pcmk_ipc_api_t *pacemakerd_api,
+                    enum pcmk_ipc_event event_type, crm_exit_t status,
+                    void *event_data, void *user_data)
+{
+    pcmk_pacemakerd_api_reply_t *reply = event_data;
+
+    switch (event_type) {
+        case pcmk_ipc_event_reply:
+            break;
+
+        default:
+            return;
+    }
+
+    if (status != CRM_EX_OK) {
+        fprintf(stderr, "Bad reply from pacemakerd: %s", crm_exit_str(status));
+        return;
+    }
+
+    if (reply->reply_type != pcmk_pacemakerd_reply_shutdown) {
+        fprintf(stderr, "Unknown reply type %d from pacemakerd",
+                reply->reply_type);
+    }
+}
+
 int
 main(int argc, char **argv)
 {
+    int rc = pcmk_rc_ok;
     int flag;
     int argerr = 0;
 
@@ -173,7 +201,7 @@ main(int argc, char **argv)
     bool old_instance_connected = false;
     gboolean shutdown = FALSE;
 
-    crm_ipc_t *old_instance = NULL;
+    pcmk_ipc_api_t *old_instance = NULL;
     qb_ipcs_service_t *ipcs = NULL;
 
     crm_log_preinit(NULL, argc, argv);
@@ -237,29 +265,38 @@ main(int argc, char **argv)
     crm_log_init(NULL, LOG_INFO, TRUE, FALSE, argc, argv, FALSE);
 
     crm_debug("Checking for existing Pacemaker instance");
-    old_instance = crm_ipc_new(CRM_SYSTEM_MCP, 0);
-    old_instance_connected = crm_ipc_connect(old_instance);
+
+    rc = pcmk_new_ipc_api(&old_instance, pcmk_ipc_pacemakerd);
+    if (old_instance == NULL) {
+        fprintf(stderr, "Could not connect to pacemakerd: %s",
+                pcmk_rc_str(rc));
+        crm_exit(pcmk_rc2exitc(rc));
+    }
+
+    pcmk_register_ipc_callback(old_instance, pacemakerd_event_cb, NULL);
+    rc = pcmk_connect_ipc(old_instance, pcmk_ipc_dispatch_sync);
+    old_instance_connected = pcmk_ipc_is_connected(old_instance);
 
     if (shutdown) {
         if (old_instance_connected) {
-            crm_exit(request_shutdown(old_instance));
+            rc = pcmk_pacemakerd_api_shutdown(old_instance, crm_system_name);
+            pcmk_dispatch_ipc(old_instance);
+            pcmk_free_ipc_api(old_instance);
+            crm_exit(pcmk_rc2exitc(rc));
         } else {
             crm_err("Could not request shutdown of existing "
                     "Pacemaker instance: %s", strerror(errno));
-            crm_ipc_close(old_instance);
-            crm_ipc_destroy(old_instance);
+            pcmk_free_ipc_api(old_instance);
             crm_exit(CRM_EX_DISCONNECT);
         }
 
     } else if (old_instance_connected) {
-        crm_ipc_close(old_instance);
-        crm_ipc_destroy(old_instance);
+        pcmk_free_ipc_api(old_instance);
         crm_err("Aborting start-up because active Pacemaker instance found");
         crm_exit(CRM_EX_FATAL);
     }
 
-    crm_ipc_close(old_instance);
-    crm_ipc_destroy(old_instance);
+    pcmk_free_ipc_api(old_instance);
 
 #ifdef SUPPORT_COROSYNC
     if (mcp_read_config() == FALSE) {

daemons/pacemakerd/pacemakerd.h
@@ -46,4 +46,3 @@ void pcmk_shutdown(int nsig);
 void pcmk_handle_ping_request(pcmk__client_t *c, xmlNode *msg, uint32_t id);
 void pcmk_handle_shutdown_request(pcmk__client_t *c, xmlNode *msg, uint32_t id, uint32_t flags);
 void pcmkd_shutdown_corosync(void);
-crm_exit_t request_shutdown(crm_ipc_t *ipc);

daemons/pacemakerd/pcmkd_messages.c
@@ -188,60 +188,6 @@ pcmk_ipc_dispatch(qb_ipcs_connection_t * qbc, void *data, size_t size)
     return 0;
 }
 
-crm_exit_t
-request_shutdown(crm_ipc_t *ipc)
-{
-    xmlNode *request = NULL;
-    xmlNode *reply = NULL;
-    int rc = 0;
-    crm_exit_t status = CRM_EX_OK;
-
-    request = create_request(CRM_OP_QUIT, NULL, NULL, CRM_SYSTEM_MCP,
-                             CRM_SYSTEM_MCP, NULL);
-    if (request == NULL) {
-        crm_err("Unable to create shutdown request"); // Probably memory error
-        status = CRM_EX_TEMPFAIL;
-        goto done;
-    }
-
-    crm_notice("Requesting shutdown of existing Pacemaker instance");
-    rc = crm_ipc_send(ipc, request, crm_ipc_client_response, 0, &reply);
-    if (rc < 0) {
-        crm_err("Could not send shutdown request");
-        status = crm_errno2exit(rc);
-        goto done;
-    }
-
-    if ((rc == 0) || (reply == NULL)) {
-        crm_err("Unrecognized response to shutdown request");
-        status = CRM_EX_PROTOCOL;
-        goto done;
-    }
-
-    if ((crm_element_value_int(reply, "status", &rc) == 0)
-        && (rc != CRM_EX_OK)) {
-        crm_err("Shutdown request failed: %s", crm_exit_str(rc));
-        status = rc;
-        goto done;
-    }
-
-    // Wait for pacemakerd to shut down IPC (with 30-minute timeout)
-    status = CRM_EX_TIMEOUT;
-    for (int i = 0; i < 900; ++i) {
-        if (!crm_ipc_connected(ipc)) {
-            status = CRM_EX_OK;
-            break;
-        }
-        sleep(2);
-    }
-
-done:
-    free_xml(request);
-    crm_ipc_close(ipc);
-    crm_ipc_destroy(ipc);
-    return status;
-}
-
 struct qb_ipcs_service_handlers mcp_ipc_callbacks = {
     .connection_accept = pcmk_ipc_accept,
     .connection_created = NULL,

commit 6ec6bac351b4eaf74e4abd76d172190d60d016b6

Low: daemons: pacemakerd -S should wait for shutdown before returning
Previously, `pacemakerd -S` would request a shutdown and then sit in a
loop for 30 minutes, waiting for the IPC connection to drop.  Only then
would it return to the command line.  This gave the user a way of
knowing the shutdown completed.

There are potentially various ways of solving this problem.  However, a
shutdown works by adding an event to the main loop which makes it
difficult to know exactly when it happens.  So, just go with the easiest
fix and re-implement the loop checking the IPC connection status.

Regression in 2.1.1 introduced by 64d4f9b5f

daemons/pacemakerd/pacemakerd.c
@@ -337,9 +337,32 @@ main(int argc, char **argv)
         if (old_instance_connected) {
             rc = pcmk_pacemakerd_api_shutdown(old_instance, crm_system_name);
             pcmk_dispatch_ipc(old_instance);
-            pcmk_free_ipc_api(old_instance);
+
             exit_code = pcmk_rc2exitc(rc);
+
+            if (exit_code != CRM_EX_OK) {
+                pcmk_free_ipc_api(old_instance);
+                goto done;
+            }
+
+            /* We get the ACK immediately, and the response right after that,
+             * but it might take a while for pacemakerd to get around to
+             * shutting down.  Wait for that to happen (with 30-minute timeout).
+             */
+            for (int i = 0; i < 900; i++) {
+                if (!pcmk_ipc_is_connected(old_instance)) {
+                    exit_code = CRM_EX_OK;
+                    pcmk_free_ipc_api(old_instance);
+                    goto done;
+                }
+
+                sleep(2);
+            }
+
+            exit_code = CRM_EX_TIMEOUT;
+            pcmk_free_ipc_api(old_instance);
             goto done;
+
         } else {
             out->err(out, "Could not request shutdown "
                      "of existing Pacemaker instance: %s", pcmk_rc_str(rc));