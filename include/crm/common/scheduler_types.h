a8e3240bac891cbd5ff56407ddfb097a225299d8
Ken Gaillot <kgaillot@redhat.com>
2023-05-10 09:03:02 -0500

    API: libcrmcommon: add pcmk_node_t type
    This is equivalent to (and intended to replace) pe_node_t. The struct
    definition is still in libpe_rules/libpe_status, but C doesn't care as long as
    we don't try to use any members.

---
+++ include/crm/common/scheduler_types.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2023 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_SCHEDULER_TYPES__H
+#  define PCMK__CRM_COMMON_SCHEDULER_TYPES__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * \file
+ * \brief Type aliases needed to define scheduler objects
+ * \ingroup core
+ */
+
+//! Node object (including information that may vary depending on resource)
+typedef struct pe_node_s pcmk_node_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PCMK__CRM_COMMON_SCHEDULER_TYPES__H
