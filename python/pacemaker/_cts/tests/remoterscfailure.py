e9cba73264741cdd65508d2428292d95a360ea0b
Chris Lumens <clumens@redhat.com>
2023-06-01 16:25:09 -0400

    Refactor: python: Improve comments and whitespace in remoterscfailure.py.
    And because the only code changes are disabling some pylint warnings and
    a few minor things in is_applicable, add them into this commit too.

--- python/pacemaker/_cts/tests/remoterscfailure.py
+++ python/pacemaker/_cts/tests/remoterscfailure.py
@@ -1,5 +1,4 @@
-""" Test-specific classes for Pacemaker's Cluster Test Suite (CTS)
-"""
+""" Cause the Pacemaker Remote connection resource to fail """
 
 __all__ = ["RemoteRscFailure"]
 __copyright__ = "Copyright 2000-2023 the Pacemaker project contributors"
@@ -7,14 +6,33 @@ __license__ = "GNU General Public License version 2 or later (GPLv2+) WITHOUT AN
 
 from pacemaker._cts.tests.remotedriver import RemoteDriver
 
+# Disable various pylint warnings that occur in so many places throughout this
+# file it's easiest to just take care of them globally.  This does introduce the
+# possibility that we'll miss some other cause of the same warning, but we'll
+# just have to be careful.
+
+# pylint doesn't understand that self._env is subscriptable.
+# pylint: disable=unsubscriptable-object
+
 
 class RemoteRscFailure(RemoteDriver):
+    """ A concrete test that causes the Pacemaker Remote connection resource
+        to fail
+    """
+
     def __init__(self, cm):
+        """ Create a new RemoteRscFailure instance
+
+            Arguments:
+
+            cm -- A ClusterManager instance
+        """
+
         RemoteDriver.__init__(self, cm)
         self.name = "RemoteRscFailure"
 
     def __call__(self, node):
-        '''Perform the 'RemoteRscFailure' test. '''
+        """ Perform this test """
 
         if not self.start_new_test(node):
             return self.failure(self.fail_string)
@@ -42,11 +60,12 @@ class RemoteRscFailure(RemoteDriver):
                  r"Dummy.*: No process state file found" ] + super().errors_to_ignore
 
     def is_applicable(self):
+        """ Return True if this test is applicable in the current test configuration. """
+
         if not RemoteDriver.is_applicable(self):
-            return 0
+            return False
+
         # This test requires at least three nodes: one to convert to a
         # remote node, one to host the connection originally, and one
         # to migrate the connection to.
-        if len(self._env["nodes"]) < 3:
-            return 0
-        return 1
+        return len(self._env["nodes"]) >= 3
