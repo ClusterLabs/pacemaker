f18609ec3b992df54250f6aee4432202085f66a9
Chris Lumens <clumens@redhat.com>
2023-06-01 15:59:42 -0400

    Refactor: python: Improve comments and whitespace in remotebasic.py.
    No code changes.

--- python/pacemaker/_cts/tests/remotebasic.py
+++ python/pacemaker/_cts/tests/remotebasic.py
@@ -1,5 +1,4 @@
-""" Test-specific classes for Pacemaker's Cluster Test Suite (CTS)
-"""
+""" Start and stop a remote node """
 
 __all__ = ["RemoteBasic"]
 __copyright__ = "Copyright 2000-2023 the Pacemaker project contributors"
@@ -9,12 +8,22 @@ from pacemaker._cts.tests.remotedriver import RemoteDriver
 
 
 class RemoteBasic(RemoteDriver):
+    """ A concrete test that starts and stops a remote node """
+
     def __init__(self, cm):
+        """ Create a new RemoteBasic instance
+
+            Arguments:
+
+            cm -- A ClusterManager instance
+        """
+
         RemoteDriver.__init__(self, cm)
+
         self.name = "RemoteBasic"
 
     def __call__(self, node):
-        '''Perform the 'RemoteBaremetal' test. '''
+        """ Perform this test """
 
         if not self.start_new_test(node):
             return self.failure(self.fail_string)
