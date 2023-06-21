0f3a0dc4911edb05ae3b734cd18b700d3e0d310b
Ken Gaillot <kgaillot@redhat.com>
2023-06-21 15:53:00 -0500

    API: libcrmcommon: deprecate RSC_DEMOTE string constant

--- include/crm/crm_compat.h
+++ include/crm/crm_compat.h
@@ -52,6 +52,9 @@ extern "C" {
 //! \deprecated Use PCMK_ACTION_STOP instead
 #define CRMD_ACTION_STOP PCMK_ACTION_STOP
 
+//! \deprecated Use PCMK_ACTION_DEMOTE instead
+#define RSC_DEMOTE PCMK_ACTION_DEMOTE
+
 //! \deprecated Use PCMK_ACTION_META_DATA instead
 #define RSC_METADATA PCMK_ACTION_META_DATA
 
