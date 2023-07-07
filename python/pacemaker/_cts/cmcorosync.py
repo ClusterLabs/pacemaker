17ec4487d4569f18499344335304947fc4967cdd
Chris Lumens <clumens@redhat.com>
2023-07-07 16:06:33 -0400

    Refactor: python: Simplify list traversals in cmcorosync.py.

--- python/pacemaker/_cts/cmcorosync.py
+++ python/pacemaker/_cts/cmcorosync.py
@@ -23,7 +23,7 @@ class Corosync2(ClusterManager):
     @property
     def components(self):
         complist = []
-        if not len(list(self.fullcomplist.keys())):
+        if not self.fullcomplist:
             for c in [ "pacemaker-based", "pacemaker-controld", "pacemaker-attrd", "pacemaker-execd", "pacemaker-fenced" ]:
                 self.fullcomplist[c] = Process(
                     self, c, 
@@ -49,13 +49,13 @@ class Corosync2(ClusterManager):
         # Processes running under valgrind can't be shot with "killall -9 processname",
         # so don't include them in the returned list
         vgrind = self.env["valgrind-procs"].split()
-        for key in list(self.fullcomplist.keys()):
+        for (key, val) in self.fullcomplist.items():
             if self.env["valgrind-tests"]:
                 if key in vgrind:
                     self.log("Filtering %s from the component list as it is being profiled by valgrind" % key)
                     continue
             if key == "pacemaker-fenced" and not self.env["DoFencing"]:
                 continue
-            complist.append(self.fullcomplist[key])
+            complist.append(val)
 
         return complist
