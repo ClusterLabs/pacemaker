e30d3da6648ce4216545fba60911b8e9be474084
Reid Wahl <nrwahl@protonmail.com>
2023-08-26 15:08:56 -0700

    API: libcrmcommon: write_xml_fd()'s xml argument is now const
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/xml.h
+++ include/crm/common/xml.h
@@ -130,7 +130,8 @@ xmlNode *stdin2xml(void);
 
 xmlNode *string2xml(const char *input);
 
-int write_xml_fd(xmlNode * xml_node, const char *filename, int fd, gboolean compress);
+int write_xml_fd(const xmlNode *xml, const char *filename, int fd,
+                 gboolean compress);
 int write_xml_file(xmlNode * xml_node, const char *filename, gboolean compress);
 
 char *dump_xml_formatted(const xmlNode *xml);
