541ce5f04436ea744ae0a80312024f1bc8361051
Reid Wahl <nrwahl@protonmail.com>
2023-08-28 17:31:00 -0700

    Refactor: libcrmcommon: Move XML_RSC_OP_LAST_RUN to compat header
    Deprecated by 5fe89bd. Removed internally by fd00ba2.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml.h
+++ include/crm/msg_xml.h
@@ -328,7 +328,6 @@ extern "C" {
 #  define XML_LRM_ATTR_EXIT_REASON	"exit-reason"
 
 #  define XML_RSC_OP_LAST_CHANGE        "last-rc-change"
-#  define XML_RSC_OP_LAST_RUN           "last-run"  // deprecated since 2.0.3
 #  define XML_RSC_OP_T_EXEC             "exec-time"
 #  define XML_RSC_OP_T_QUEUE            "queue-time"
 
