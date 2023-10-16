4c8b6c262c2ae2b925b2ca393b729715f9edebcc
Reid Wahl <nrwahl@protonmail.com>
2023-10-16 12:45:59 -0700

    Refactor: libcrmcommon: New PCMK__ENV_SCHEMA_DIRECTORY
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -94,6 +94,7 @@ bool pcmk__valid_sbd_timeout(const char *value);
 #define PCMK__ENV_REMOTE_PID1               "remote_pid1"
 #define PCMK__ENV_REMOTE_PORT               "remote_port"
 #define PCMK__ENV_RESPAWNED                 "respawned"
+#define PCMK__ENV_SCHEMA_DIRECTORY          "schema_directory"
 #define PCMK__ENV_SERVICE                   "service"
 #define PCMK__ENV_SHUTDOWN_DELAY            "shutdown_delay"
 #define PCMK__ENV_STDERR                    "stderr"
