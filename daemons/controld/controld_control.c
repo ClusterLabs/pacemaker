ecd14bff445f38d19b1b78d9a29305d301b13d2c
Hideo Yamauchi <renayama19661014@ybb.ne.jp>
2023-10-31 16:22:15 +0900

    Mid: controld: Adding default value for node-pending-timeout.

--- daemons/controld/controld_control.c
+++ daemons/controld/controld_control.c
@@ -677,6 +677,16 @@ static pcmk__cluster_option_t controller_options[] = {
             "passed since the shutdown was initiated, even if the node has not "
             "rejoined.")
     },
+    {
+        XML_CONFIG_ATTR_NODE_PENDING_TIMEOUT, NULL, "time", NULL,
+        "2h", pcmk__valid_interval_spec,
+        N_("How long to wait for a node that has joined the cluster to join "
+           "the controller process group"),
+        N_("Fence nodes that do not join the controller process group within "
+           "this much time after joining the cluster, to allow the cluster "
+           "to continue managing resources. A value of 0 means never fence "
+           "pending nodes.")
+    },
 };
 
 void
