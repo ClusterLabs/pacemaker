ca77a9294cf399cc5d7b571b49535d612f09bd34
Ken Gaillot <kgaillot@redhat.com>
2023-12-14 14:56:11 -0600

    Low: pacemaker-attrd: drop "requesting shutdown" code
    This hasn't been needed since attrd_cib_updated_cb() started ignoring changes
    from trusted sources (!cib__client_triggers_refresh()). It's especially useless
    now that the attribute manager handles clearing of leaving nodes' attributes
    from the CIB.

--- daemons/attrd/attrd_cib.c
+++ daemons/attrd/attrd_cib.c
@@ -35,7 +35,7 @@ attrd_cib_destroy_cb(gpointer user_data)
 
     cib->cmds->signoff(cib);
 
-    if (attrd_shutting_down(false)) {
+    if (attrd_shutting_down()) {
         crm_info("Disconnected from the CIB manager");
 
     } else {
@@ -139,7 +139,7 @@ attrd_cib_updated_cb(const char *event, xmlNode *msg)
     }
 
     if (pcmk__cib_element_in_patchset(patchset, PCMK_XE_ALERTS)) {
-        if (attrd_shutting_down(true)) {
+        if (attrd_shutting_down()) {
             crm_debug("Ignoring alerts change in CIB during shutdown");
         } else {
             mainloop_set_trigger(attrd_config_read);
@@ -178,7 +178,7 @@ attrd_cib_updated_cb(const char *event, xmlNode *msg)
     if (status_changed
         || pcmk__cib_element_in_patchset(patchset, PCMK_XE_NODES)) {
 
-        if (attrd_shutting_down(true)) {
+        if (attrd_shutting_down()) {
             crm_debug("Ignoring node change in CIB during shutdown");
             return;
         }
