42bdad94c7cea846d0a3b9c707759ca2db82203a
Reid Wahl <nrwahl@protonmail.com>
2023-12-20 00:15:03 -0800

    API: libcrmcommon: Deprecate XML_CONFIG_ATTR_FORCE_QUIT
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml.h
+++ include/crm/msg_xml.h
@@ -428,7 +428,6 @@ extern "C" {
 
 #  define XML_NODE_ATTR_RSC_DISCOVERY   "resource-discovery-enabled"
 
-#  define XML_CONFIG_ATTR_FORCE_QUIT	"shutdown-escalation"
 #  define XML_CONFIG_ATTR_SHUTDOWN_LOCK         "shutdown-lock"
 #  define XML_CONFIG_ATTR_SHUTDOWN_LOCK_LIMIT   "shutdown-lock-limit"
 
