e8f647a8ddaff27b8adaee1117135894c04f5249
Reid Wahl <nrwahl@protonmail.com>
2024-01-07 20:28:41 -0800

    Test: libpe_rules: Use string constants for cron
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/pengine/tests/rules/pe_cron_range_satisfied_test.c
+++ lib/pengine/tests/rules/pe_cron_range_satisfied_test.c
@@ -51,98 +51,130 @@ time_satisfies_year_spec(void **state) {
 static void
 time_after_year_spec(void **state) {
     run_one_test("2020-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2019'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2019'/>",
                  pcmk_rc_after_range);
 }
 
 static void
 time_satisfies_year_range(void **state) {
     run_one_test("2020-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2010-2030'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2010-2030'/>",
                  pcmk_rc_ok);
 }
 
 static void
 time_before_year_range(void **state) {
     run_one_test("2000-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2010-2030'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2010-2030'/>",
                  pcmk_rc_before_range);
 }
 
 static void
 time_after_year_range(void **state) {
     run_one_test("2020-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2010-2015'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2010-2015'/>",
                  pcmk_rc_after_range);
 }
 
 static void
 range_without_start_year_passes(void **state) {
     run_one_test("2010-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='-2020'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='-2020'/>",
                  pcmk_rc_ok);
 }
 
 static void
 range_without_end_year_passes(void **state) {
     run_one_test("2010-01-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2000-'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2000-'/>",
                  pcmk_rc_ok);
     run_one_test("2000-10-01",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2000-'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2000-'/>",
                  pcmk_rc_ok);
 }
 
 static void
 yeardays_satisfies(void **state) {
     run_one_test("2020-01-30",
-                 "<date_spec " PCMK_XA_ID "='spec' yeardays='30'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARDAYS "='30'/>",
                  pcmk_rc_ok);
 }
 
 static void
 time_after_yeardays_spec(void **state) {
     run_one_test("2020-02-15",
-                 "<date_spec " PCMK_XA_ID "='spec' yeardays='40'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARDAYS "='40'/>",
                  pcmk_rc_after_range);
 }
 
 static void
 yeardays_feb_29_satisfies(void **state) {
     run_one_test("2016-02-29",
-                 "<date_spec " PCMK_XA_ID "='spec' yeardays='60'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARDAYS "='60'/>",
                  pcmk_rc_ok);
 }
 
 static void
 exact_ymd_satisfies(void **state) {
     run_one_test("2001-12-31",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2001' months='12' "
-                 "monthdays='31'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2001' "
+                     PCMK_XA_MONTHS "='12' "
+                     PCMK_XA_MONTHDAYS "='31'/>",
                  pcmk_rc_ok);
 }
 
 static void
 range_in_month_satisfies(void **state) {
     run_one_test("2001-06-10",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2001' months='6' "
-                 "monthdays='1-10'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2001' "
+                     PCMK_XA_MONTHS "='6' "
+                     PCMK_XA_MONTHDAYS "='1-10'/>",
                  pcmk_rc_ok);
 }
 
 static void
 exact_ymd_after_range(void **state) {
     run_one_test("2001-12-31",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2001' months='12' "
-                 "monthdays='30'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2001' "
+                     PCMK_XA_MONTHS "='12' "
+                     PCMK_XA_MONTHDAYS "='30'/>",
                  pcmk_rc_after_range);
 }
 
 static void
 time_after_monthdays_range(void **state) {
     run_one_test("2001-06-10",
-                 "<date_spec " PCMK_XA_ID "='spec' years='2001' months='6' "
-                 "monthdays='11-15'/>",
+                 "<" PCMK_XE_DATE_SPEC " "
+                     PCMK_XA_ID "='spec' "
+                     PCMK_XA_YEARS "='2001' "
+                     PCMK_XA_MONTHS "='6' "
+                     PCMK_XA_MONTHDAYS "='11-15'/>",
                  pcmk_rc_before_range);
 }
 
