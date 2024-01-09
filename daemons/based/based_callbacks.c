ClusterLabs/pacemaker

commit 618e77d7156649c81a40acc947ee7d31efb607a9

API: libcrmcommon: New PCMK_XA_NUM_UPDATES string constant
Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/based/based_callbacks.c
@@ -459,20 +459,22 @@ process_ping_reply(xmlNode *reply)
             xmlNode *remote_cib = get_message_xml(pong, F_CIB_CALLDATA);
             const char *admin_epoch_s = "_";
             const char *epoch_s = "_";
+            const char *num_updates_s = "_";
 
             if (remote_cib != NULL) {
                 admin_epoch_s = crm_element_value(remote_cib,
                                                   PCMK_XA_ADMIN_EPOCH);
                 epoch_s = crm_element_value(remote_cib, PCMK_XA_EPOCH);
+                num_updates_s = crm_element_value(remote_cib,
+                                                  PCMK_XA_NUM_UPDATES);
             }
 
             crm_notice("Local CIB %s.%s.%s.%s differs from %s: %s.%s.%s.%s %p",
                        crm_element_value(the_cib, PCMK_XA_ADMIN_EPOCH),
                        crm_element_value(the_cib, PCMK_XA_EPOCH),
-                       crm_element_value(the_cib, XML_ATTR_NUMUPDATES),
+                       crm_element_value(the_cib, PCMK_XA_NUM_UPDATES),
                        ping_digest, host,
-                       admin_epoch_s, epoch_s,
-                       remote_cib?crm_element_value(remote_cib, XML_ATTR_NUMUPDATES):"_",
+                       admin_epoch_s, epoch_s, num_updates_s,
                        digest, remote_cib);
 
             if(remote_cib && remote_cib->children) {
@@ -1134,10 +1136,12 @@ cib_process_request(xmlNode *request, gboolean privileged,
         const char *section = crm_element_value(request, F_CIB_SECTION);
         const char *admin_epoch_s = "0";
         const char *epoch_s = "0";
+        const char *num_updates_s = "0";
 
         if (the_cib != NULL) {
             admin_epoch_s = crm_element_value(the_cib, PCMK_XA_ADMIN_EPOCH);
             epoch_s = crm_element_value(the_cib, PCMK_XA_EPOCH);
+            num_updates_s = crm_element_value(the_cib, PCMK_XA_NUM_UPDATES);
         }
 
         rc = cib_process_command(request, operation, op_function, &op_reply,
@@ -1168,8 +1172,7 @@ cib_process_request(xmlNode *request, gboolean privileged,
                    "Completed %s operation for section %s: %s (rc=%d, origin=%s/%s/%s, version=%s.%s.%s)",
                    op, section ? section : "'all'", pcmk_strerror(rc), rc,
                    originator ? originator : "local", client_name, call_id,
-                   admin_epoch_s, epoch_s,
-                   the_cib ? crm_element_value(the_cib, XML_ATTR_NUMUPDATES) : "0");
+                   admin_epoch_s, epoch_s, num_updates_s);
 
         finished = time(NULL);
         if ((finished - now) > 3) {
@@ -1439,8 +1442,8 @@ cib_process_command(xmlNode *request, const cib__operation_t *operation,
             }
 
             crm_trace("Activating %s->%s%s",
-                      crm_element_value(the_cib, XML_ATTR_NUMUPDATES),
-                      crm_element_value(result_cib, XML_ATTR_NUMUPDATES),
+                      crm_element_value(the_cib, PCMK_XA_NUM_UPDATES),
+                      crm_element_value(result_cib, PCMK_XA_NUM_UPDATES),
                       (config_changed? " changed" : ""));
 
             rc = activateCibXml(result_cib, config_changed, op);
@@ -1487,7 +1490,7 @@ cib_process_command(xmlNode *request, const cib__operation_t *operation,
     } else {
         crm_trace("Not activating %d %d %s", rc,
                   pcmk_is_set(call_options, cib_dryrun),
-                  crm_element_value(result_cib, XML_ATTR_NUMUPDATES));
+                  crm_element_value(result_cib, PCMK_XA_NUM_UPDATES));
 
         if (result_cib != the_cib) {
             free_xml(result_cib);

daemons/based/based_io.c
@@ -289,7 +289,7 @@ readCibXmlFile(const char *dir, const char *file, gboolean discard_status)
         crm_xml_add_int(root, name, 0);
     }
 
-    name = XML_ATTR_NUMUPDATES;
+    name = PCMK_XA_NUM_UPDATES;
     value = crm_element_value(root, name);
     if (value == NULL) {
         crm_xml_add_int(root, name, 0);

daemons/based/based_messages.c
@@ -166,7 +166,7 @@ cib_process_ping(const char *op, int options, const char *section, xmlNode * req
              host, digest,
              crm_element_value(existing_cib, PCMK_XA_ADMIN_EPOCH),
              crm_element_value(existing_cib, PCMK_XA_EPOCH),
-             crm_element_value(existing_cib, XML_ATTR_NUMUPDATES));
+             crm_element_value(existing_cib, PCMK_XA_NUM_UPDATES));
 
     free(digest);
 

daemons/controld/controld_join_dc.c
@@ -413,7 +413,7 @@ do_dc_join_filter_offer(long long action,
         const char *attributes[] = {
             PCMK_XA_ADMIN_EPOCH,
             PCMK_XA_EPOCH,
-            XML_ATTR_NUMUPDATES,
+            PCMK_XA_NUM_UPDATES,
         };
 
         for (lpc = 0; cmp == 0 && lpc < PCMK__NELEM(attributes); lpc++) {

daemons/fenced/fenced_cib.c
@@ -227,7 +227,7 @@ cib_devices_update(void)
     crm_info("Updating devices to version %s.%s.%s",
              crm_element_value(local_cib, PCMK_XA_ADMIN_EPOCH),
              crm_element_value(local_cib, PCMK_XA_EPOCH),
-             crm_element_value(local_cib, XML_ATTR_NUMUPDATES));
+             crm_element_value(local_cib, PCMK_XA_NUM_UPDATES));
 
     g_hash_table_iter_init(&iter, device_list);
     while (g_hash_table_iter_next(&iter, NULL, (void **)&device)) {

include/crm/msg_xml.h
@@ -54,6 +54,7 @@ extern "C" {
 #define PCMK_XA_FORMAT                      "format"
 #define PCMK_XA_HAVE_QUORUM                 "have-quorum"
 #define PCMK_XA_NO_QUORUM_PANIC             "no-quorum-panic"
+#define PCMK_XA_NUM_UPDATES                 "num_updates"
 #define PCMK_XA_VALIDATE_WITH               "validate-with"
 
 /* These have been deprecated as CIB <clone> element attributes (aliases for

lib/cib/cib_file.c
@@ -876,8 +876,8 @@ cib_file_backup(const char *cib_dirname, const char *cib_filename)
  * \internal
  * \brief Prepare CIB XML to be written to disk
  *
- * Set num_updates to 0, set cib-last-written to the current timestamp,
- * and strip out the status section.
+ * Set \c PCMK_XA_NUM_UPDATES to 0, set cib-last-written to the current
+ * timestamp, and strip out the status section.
  *
  * \param[in,out] root  Root of CIB XML tree
  *
@@ -889,7 +889,7 @@ cib_file_prepare_xml(xmlNode *root)
     xmlNode *cib_status_root = NULL;
 
     /* Always write out with num_updates=0 and current last-written timestamp */
-    crm_xml_add(root, XML_ATTR_NUMUPDATES, "0");
+    crm_xml_add(root, PCMK_XA_NUM_UPDATES, "0");
     pcmk__xe_add_last_written(root);
 
     /* Delete status section before writing to file, because

lib/cib/cib_ops.c
@@ -286,7 +286,7 @@ cib_process_upgrade(const char *op, int options, const char *section, xmlNode * 
     if (new_version > current_version) {
         update_counter(*result_cib, PCMK_XA_ADMIN_EPOCH, false);
         update_counter(*result_cib, PCMK_XA_EPOCH, true);
-        update_counter(*result_cib, XML_ATTR_NUMUPDATES, true);
+        update_counter(*result_cib, PCMK_XA_NUM_UPDATES, true);
         return pcmk_ok;
     }
 
@@ -386,7 +386,7 @@ cib_process_replace(const char *op, int options, const char *section, xmlNode * 
             /* no more checks */
 
         } else if (replace_updates < updates) {
-            reason = XML_ATTR_NUMUPDATES;
+            reason = PCMK_XA_NUM_UPDATES;
         }
 
         if (reason != NULL) {

commit b23004445cd38a31fe34b208fe343ea9c5adaa83

Log: based: Fix CIB version values
939a408c, 8e9e6642, and 618e77d7 caused the CIB version attribute values
to get fetched too early in cib_process_request(). cib_process_command()
can change them.

This resulted in corruption (probably affecting only logging) after some
commands, with garbage values being logged. For example:

info: Completed cib_modify operation for section nodes: OK (rc=0, origin=laptop/crmd/4, version=<81>rcT.!^K9^H<9d>U.1^K9^H<9d>U)

We need to fetch the values after cib_process_command(). Here we do it
immediately before use.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/based/based_callbacks.c
@@ -1143,12 +1143,6 @@ cib_process_request(xmlNode *request, gboolean privileged,
         const char *epoch_s = "0";
         const char *num_updates_s = "0";
 
-        if (the_cib != NULL) {
-            admin_epoch_s = crm_element_value(the_cib, PCMK_XA_ADMIN_EPOCH);
-            epoch_s = crm_element_value(the_cib, PCMK_XA_EPOCH);
-            num_updates_s = crm_element_value(the_cib, PCMK_XA_NUM_UPDATES);
-        }
-
         rc = cib_process_command(request, operation, op_function, &op_reply,
                                  &result_diff, privileged);
 
@@ -1171,6 +1165,12 @@ cib_process_request(xmlNode *request, gboolean privileged,
 
         } else if (rc != pcmk_ok) {
             level = LOG_WARNING;
+        }
+
+        if (the_cib != NULL) {
+            admin_epoch_s = crm_element_value(the_cib, PCMK_XA_ADMIN_EPOCH);
+            epoch_s = crm_element_value(the_cib, PCMK_XA_EPOCH);
+            num_updates_s = crm_element_value(the_cib, PCMK_XA_NUM_UPDATES);
         }
 
         do_crm_log(level,