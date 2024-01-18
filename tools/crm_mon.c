ClusterLabs/pacemaker

commit bc91cc5d8b4257627d09103cf676cd83656bda8c

Refactor: tools: Split up connection teardown in crm_mon.
We don't necessarily want to tear down the fencing and CIB connections
at the same time always.  This can then immediately be used in
mon_refresh_display and do_mon_cib_connection_destroy.

tools/crm_mon.c
@@ -122,7 +122,8 @@ struct {
     .mon_ops = mon_op_default
 };
 
-static void clean_up_connections(void);
+static void clean_up_cib_connection(void);
+static void clean_up_fencing_connection(void);
 static crm_exit_t clean_up(crm_exit_t exit_code);
 static void crm_diff_update(const char *event, xmlNode * msg);
 static int mon_refresh_display(gpointer user_data);
@@ -712,12 +713,7 @@ do_mon_cib_connection_destroy(gpointer user_data, bool is_error)
         /* the client API won't properly reconnect notifications
          * if they are still in the table - so remove them
          */
-        st->cmds->remove_notification(st, T_STONITH_NOTIFY_DISCONNECT);
-        st->cmds->remove_notification(st, T_STONITH_NOTIFY_FENCE);
-        st->cmds->remove_notification(st, T_STONITH_NOTIFY_HISTORY);
-        if (st->state != stonith_disconnected) {
-            st->cmds->disconnect(st);
-        }
+        clean_up_fencing_connection();
     }
     if (cib) {
         cib->cmds->signoff(cib);
@@ -851,7 +847,8 @@ cib_connect(gboolean full)
 
         if (rc != pcmk_ok) {
             out->err(out, "Notification setup failed, could not monitor CIB actions");
-            clean_up_connections();
+            clean_up_cib_connection();
+            clean_up_fencing_connection();
         }
     }
     return rc;
@@ -1866,9 +1863,7 @@ mon_refresh_display(gpointer user_data)
     last_refresh = time(NULL);
 
     if (cli_config_update(&cib_copy, NULL, FALSE) == FALSE) {
-        if (cib) {
-            cib->cmds->signoff(cib);
-        }
+        clean_up_cib_connection();
         out->err(out, "Upgrade failed: %s", pcmk_strerror(-pcmk_err_schema_validation));
         clean_up(CRM_EX_CONFIG);
         return 0;
@@ -2040,24 +2035,33 @@ mon_st_callback_display(stonith_t * st, stonith_event_t * e)
 }
 
 static void
-clean_up_connections(void)
+clean_up_cib_connection(void)
 {
-    if (cib != NULL) {
-        cib->cmds->signoff(cib);
-        cib_delete(cib);
-        cib = NULL;
+    if (cib == NULL) {
+        return;
     }
 
-    if (st != NULL) {
-        if (st->state != stonith_disconnected) {
-            st->cmds->remove_notification(st, T_STONITH_NOTIFY_DISCONNECT);
-            st->cmds->remove_notification(st, T_STONITH_NOTIFY_FENCE);
-            st->cmds->remove_notification(st, T_STONITH_NOTIFY_HISTORY);
-            st->cmds->disconnect(st);
-        }
-        stonith_api_delete(st);
-        st = NULL;
+    cib->cmds->signoff(cib);
+    cib_delete(cib);
+    cib = NULL;
+}
+
+static void
+clean_up_fencing_connection(void)
+{
+    if (st == NULL) {
+        return;
     }
+
+    if (st->state != stonith_disconnected) {
+        st->cmds->remove_notification(st, T_STONITH_NOTIFY_DISCONNECT);
+        st->cmds->remove_notification(st, T_STONITH_NOTIFY_FENCE);
+        st->cmds->remove_notification(st, T_STONITH_NOTIFY_HISTORY);
+        st->cmds->disconnect(st);
+    }
+
+    stonith_api_delete(st);
+    st = NULL;
 }
 
 /*
@@ -2074,7 +2078,8 @@ clean_up(crm_exit_t exit_code)
     /* Quitting crm_mon is much more complicated than it ought to be. */
 
     /* (1) Close connections, free things, etc. */
-    clean_up_connections();
+    clean_up_cib_connection();
+    clean_up_fencing_connection();
     free(options.pid_file);
     free(options.neg_location_prefix);
     g_slist_free_full(options.includes_excludes, free);

commit 401f5d971f12db7792971aeec3aaba9f52d67626

Fix: tools: crm_mon segfaults when fencer connection is lost
This is easiest to observe when Pacemaker is stopping.

When crm_mon is running in interactive mode (the default) and the
cluster is stopped, crm_mon crashes with a segmentation fault. This is a
regression that was introduced in Pacemaker 2.1.0 by commit bc91cc5.
However, for some reason the crash doesn't happen on all platforms. In
particular, I can reproduce the crash on Fedora 38 and 39, but not on
RHEL 9.3 or Fedora 37. This is independent of the Pacemaker version.

The cause is a use-after-free. In detail, it is as follows:
1. crm_mon registers a notification via its stonith API client for
   disconnect events. This notification will call either
   mon_st_callback_event() or mon_st_callback_display(), depending on
   the CLI options. Both of these callbacks call
   mon_cib_connection_destroy() for disconnect notifications, so it
   doesn't matter which one is used.
2. When the fencer connection is lost, the mainloop calls the stonith
   API client's destroy callback (stonith_connection_destroy()).
3. stonith_connection_destroy() sets the state to stonith_disconnected
   and calls foreach_notify_entry(..., stonith_send_notification, blob),
   where blob contains a disconnect notification.
4. foreach_notify_entry() loops over all the registered notify entries,
   calling stonith_send_notification(entry, blob) for each notify entry.
5. For each notify client that's subscribed to disconnect notifications,
   stonith_send_notification() calls the registered callback function.
6. Based on the registration in step (1), stonith_send_notification()
   synchronously calls mon_st_callback_event()/display() for crm_mon.
7. mon_st_callback_event()/display() calls mon_cib_connection_destroy().
8. mon_cib_connection_destroy() calls stonith_api_delete(), which frees
   the stonith API client and its members, including the notification
   table.
9. Control returns to stonith_send_notification() and then back to
   foreach_notify_entry().
10. foreach_notify_entry() moves to the next entry in the list. But the
    entire list was freed in step (8). So when it tries to access a
    member of one of the entries, we get a segmentation fault.

Commit bc91cc5 introduced the regression by deleting the stonith API

tools/crm_mon.c
@@ -854,8 +854,12 @@ mon_cib_connection_destroy(gpointer user_data)
     /* the client API won't properly reconnect notifications if they are still
      * in the table - so remove them
      */
-    stonith_api_delete(st);
-    st = NULL;
+    if (st != NULL) {
+        if (st->state != stonith_disconnected) {
+            st->cmds->disconnect(st);
+        }
+        st->cmds->remove_notification(st, NULL);
+    }
 
     if (cib) {
         cib->cmds->signoff(cib);