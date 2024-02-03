2474d093ced0bc731b70e0027b7404f491ead1fa
Reid Wahl <nrwahl@protonmail.com>
2024-02-03 01:21:32 -0800

    Refactor: libcrmcommon: New PCMK__XA_LRMD_CALLDATA string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/execd/execd_commands.c
+++ daemons/execd/execd_commands.c
@@ -1779,7 +1779,7 @@ process_lrmd_get_recurring(xmlNode *request, int call_id)
     xmlNode *rsc_xml = NULL;
 
     // Resource ID is optional
-    rsc_xml = first_named_child(request, F_LRMD_CALLDATA);
+    rsc_xml = first_named_child(request, PCMK__XA_LRMD_CALLDATA);
     if (rsc_xml) {
         rsc_xml = first_named_child(rsc_xml, F_LRMD_RSC);
     }
@@ -1893,7 +1893,7 @@ process_lrmd_message(pcmk__client_t *client, uint32_t id, xmlNode *request)
         do_reply = 1;
     } else if (pcmk__str_eq(op, LRMD_OP_CHECK, pcmk__str_none)) {
         if (allowed) {
-            xmlNode *data = get_message_xml(request, F_LRMD_CALLDATA);
+            xmlNode *data = get_message_xml(request, PCMK__XA_LRMD_CALLDATA);
             const char *timeout = NULL;
 
             CRM_LOG_ASSERT(data != NULL);
--- include/crm/common/xml_names_internal.h
+++ include/crm/common/xml_names_internal.h
@@ -174,6 +174,7 @@ extern "C" {
 #define PCMK__XA_JOIN_ID                "join_id"
 #define PCMK__XA_LINE                   "line"
 #define PCMK__XA_LONG_ID                "long-id"
+#define PCMK__XA_LRMD_CALLDATA          "lrmd_calldata"
 #define PCMK__XA_LRMD_CALLID            "lrmd_callid"
 #define PCMK__XA_LRMD_CALLOPT           "lrmd_callopt"
 #define PCMK__XA_LRMD_CLIENTID          "lrmd_clientid"
