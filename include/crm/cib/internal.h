f4f29c3b490f74172f3a5b97d08fee4ab46d2635
Reid Wahl <nrwahl@protonmail.com>
2024-02-02 20:55:33 -0800

    Refactor: libcib: Drop F_CIB_TIMEOUT
    Unused internally. Also unused in Pacemaker-1.1.11.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/cib/internal.h
+++ include/crm/cib/internal.h
@@ -34,7 +34,6 @@
 #define PCMK__CIB_REQUEST_COMMIT_TRANSACT   "cib_commit_transact"
 #define PCMK__CIB_REQUEST_SCHEMAS       "cib_schemas"
 
-#  define F_CIB_TIMEOUT	"cib_timeout"
 #  define F_CIB_GLOBAL_UPDATE	"cib_update"
 #  define F_CIB_UPDATE_RESULT	"cib_update_result"
 #  define F_CIB_NOTIFY_TYPE	"cib_notify_type"
