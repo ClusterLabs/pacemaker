ClusterLabs/pacemaker

commit c187d8d7e065adaa2be8e592e9a4531a6dc1a316

Refactor: libcrmcommon,libpe_status: move and rename score adding function
Move pe__add_scores() from libpe_status to libcrmcommon, and rename it to
pcmk__add_scores(). There are no code changes.

include/crm/common/util.h
@@ -49,6 +49,7 @@ int crm_default_remote_port(void);
 int char2score(const char *score);
 char *score2char(int score);
 char *score2char_stack(int score, char *buf, size_t len);
+int pcmk__add_scores(int score1, int score2);
 
 /* public string functions (from strings.c) */
 gboolean crm_is_true(const char *s);

lib/common/scores.c
@@ -128,3 +128,73 @@ score2char(int score)
     }
     return result;
 }
+
+/*!
+ * \internal
+ * \brief Add two scores, bounding to +/-INFINITY
+ *
+ * \param[in] score1  First score to add
+ * \param[in] score2  Second score to add
+ */
+int
+pcmk__add_scores(int score1, int score2)
+{
+    int result = score1 + score2;
+
+    // First handle the cases where one or both is infinite
+
+    if (score1 <= -CRM_SCORE_INFINITY) {
+
+        if (score2 <= -CRM_SCORE_INFINITY) {
+            crm_trace("-INFINITY + -INFINITY = -INFINITY");
+        } else if (score2 >= CRM_SCORE_INFINITY) {
+            crm_trace("-INFINITY + +INFINITY = -INFINITY");
+        } else {
+            crm_trace("-INFINITY + %d = -INFINITY", score2);
+        }
+
+        return -CRM_SCORE_INFINITY;
+
+    } else if (score2 <= -CRM_SCORE_INFINITY) {
+
+        if (score1 >= CRM_SCORE_INFINITY) {
+            crm_trace("+INFINITY + -INFINITY = -INFINITY");
+        } else {
+            crm_trace("%d + -INFINITY = -INFINITY", score1);
+        }
+
+        return -CRM_SCORE_INFINITY;
+
+    } else if (score1 >= CRM_SCORE_INFINITY) {
+
+        if (score2 >= CRM_SCORE_INFINITY) {
+            crm_trace("+INFINITY + +INFINITY = +INFINITY");
+        } else {
+            crm_trace("+INFINITY + %d = +INFINITY", score2);
+        }
+
+        return CRM_SCORE_INFINITY;
+
+    } else if (score2 >= CRM_SCORE_INFINITY) {
+        crm_trace("%d + +INFINITY = +INFINITY", score1);
+        return CRM_SCORE_INFINITY;
+    }
+
+    /* As long as CRM_SCORE_INFINITY is less than half of the maximum integer,
+     * we can ignore the possibility of integer overflow
+     */
+
+    // Bound result to infinity
+
+    if (result >= CRM_SCORE_INFINITY) {
+        crm_trace("%d + %d = +INFINITY", score1, score2);
+        return CRM_SCORE_INFINITY;
+
+    } else if (result <= -CRM_SCORE_INFINITY) {
+        crm_trace("%d + %d = -INFINITY", score1, score2);
+        return -CRM_SCORE_INFINITY;
+    }
+
+    crm_trace("%d + %d = %d", score1, score2, result);
+    return result;
+}

include/crm/pengine/internal.h
@@ -213,7 +213,6 @@ typedef struct notify_data_s {
 
 bool pe_can_fence(pe_working_set_t *data_set, pe_node_t *node);
 
-int pe__add_scores(int score1, int score2);
 void add_hash_param(GHashTable * hash, const char *name, const char *value);
 
 char *native_parameter(pe_resource_t * rsc, pe_node_t * node, gboolean create, const char *name,

lib/pacemaker/pcmk_sched_colocation.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2021 the Pacemaker project contributors
+ * Copyright 2004-2022 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -974,7 +974,7 @@ pcmk__apply_coloc_to_weights(pe_resource_t *dependent, pe_resource_t *primary,
             pe_rsc_trace(dependent, "%s: %s@%s -= %d (%s inactive)",
                          constraint->id, dependent->id, node->details->uname,
                          constraint->score, primary->id);
-            node->weight = pe__add_scores(-constraint->score, node->weight);
+            node->weight = pcmk__add_scores(-constraint->score, node->weight);
 
         } else if (pcmk__str_eq(pe_node_attribute_raw(node, attribute), value,
                                 pcmk__str_casei)) {
@@ -982,14 +982,15 @@ pcmk__apply_coloc_to_weights(pe_resource_t *dependent, pe_resource_t *primary,
                 pe_rsc_trace(dependent, "%s: %s@%s += %d",
                              constraint->id, dependent->id,
                              node->details->uname, constraint->score);
-                node->weight = pe__add_scores(constraint->score, node->weight);
+                node->weight = pcmk__add_scores(constraint->score,
+                                                node->weight);
             }
 
         } else if (constraint->score >= CRM_SCORE_INFINITY) {
             pe_rsc_trace(dependent, "%s: %s@%s -= %d (%s mismatch)",
                          constraint->id, dependent->id, node->details->uname,
                          constraint->score, attribute);
-            node->weight = pe__add_scores(-constraint->score, node->weight);
+            node->weight = pcmk__add_scores(-constraint->score, node->weight);
         }
     }
 
@@ -1059,6 +1060,6 @@ pcmk__apply_coloc_to_priority(pe_resource_t *dependent, pe_resource_t *primary,
         score_multiplier = -1;
     }
 
-    dependent->priority = pe__add_scores(score_multiplier * constraint->score,
-                                         dependent->priority);
+    dependent->priority = pcmk__add_scores(score_multiplier * constraint->score,
+                                           dependent->priority);
 }

lib/pacemaker/pcmk_sched_location.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2021 the Pacemaker project contributors
+ * Copyright 2004-2022 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -164,7 +164,7 @@ generate_location_rule(pe_resource_t *rsc, xmlNode *rule_xml,
             }
 
             if (!do_and) {
-                local->weight = pe__add_scores(local->weight, score_f);
+                local->weight = pcmk__add_scores(local->weight, score_f);
             }
             crm_trace("node %s now has weight %d",
                       node->details->uname, local->weight);
@@ -657,8 +657,8 @@ pcmk__apply_location(pe__location_t *constraint, pe_resource_t *rsc)
         } else {
             pe_rsc_trace(rsc, "* + %d on %s",
                          node->weight, node->details->uname);
-            weighted_node->weight = pe__add_scores(weighted_node->weight,
-                                                   node->weight);
+            weighted_node->weight = pcmk__add_scores(weighted_node->weight,
+                                                     node->weight);
         }
 
         if (weighted_node->rsc_discover_mode < constraint->discover_mode) {

lib/pacemaker/pcmk_sched_native.c
@@ -313,7 +313,7 @@ add_node_scores_matching_attr(GHashTable *nodes, const pe_resource_t *rsc,
             }
         }
 
-        new_score = pe__add_scores(weight, node->weight);
+        new_score = pcmk__add_scores(weight, node->weight);
 
         if (only_positive && (new_score < 0) && (node->weight > 0)) {
             crm_trace("%s: Filtering %d + %f * %d = %d "

lib/pacemaker/pcmk_sched_nodes.c
@@ -270,7 +270,7 @@ add_node_health_value(gpointer key, gpointer value, gpointer user_data)
         int score = char2score((const char *) value);
         int *health = (int *) user_data;
 
-        *health = pe__add_scores(score, *health);
+        *health = pcmk__add_scores(score, *health);
     }
 }
 

lib/pacemaker/pcmk_sched_promotable.c
@@ -154,8 +154,8 @@ apply_promoted_location(pe_resource_t *child, GList *location_constraints,
             cons_node = pe_find_node_id(cons->node_list_rh, chosen->details->id);
         }
         if (cons_node != NULL) {
-            int new_priority = pe__add_scores(child->priority,
-                                              cons_node->weight);
+            int new_priority = pcmk__add_scores(child->priority,
+                                                cons_node->weight);
 
             pe_rsc_trace(child, "\t%s[%s]: %d -> %d (%d)",
                          child->id, cons_node->details->uname, child->priority,
@@ -329,7 +329,7 @@ promotion_order(pe_resource_t *rsc, pe_working_set_t *data_set)
         score2char_stack(child->sort_index, score, len);
         pe_rsc_trace(rsc, "Adding %s to %s from %s", score,
                      node->details->uname, child->id);
-        node->weight = pe__add_scores(child->sort_index, node->weight);
+        node->weight = pcmk__add_scores(child->sort_index, node->weight);
     }
 
     pe__show_node_weights(true, rsc, "Middle", rsc->allowed_nodes, data_set);
@@ -605,7 +605,7 @@ pcmk__add_promotion_scores(pe_resource_t *rsc)
 
             score = promotion_score(child_rsc, node, 0);
             if (score > 0) {
-                new_score = pe__add_scores(node->weight, score);
+                new_score = pcmk__add_scores(node->weight, score);
                 if (new_score != node->weight) {
                     pe_rsc_trace(rsc, "\t%s: Updating preference for %s (%d->%d)",
                                  child_rsc->id, node->details->uname, node->weight, new_score);
@@ -984,7 +984,7 @@ node_hash_update_one(GHashTable * hash, pe_node_t * other, const char *attr, int
 
         if (pcmk__str_eq(value, tmp, pcmk__str_casei)) {
             crm_trace("%s: %d + %d", node->details->uname, node->weight, other->weight);
-            node->weight = pe__add_scores(node->weight, score);
+            node->weight = pcmk__add_scores(node->weight, score);
         }
     }
 }
@@ -1042,8 +1042,8 @@ promotable_colocation_rh(pe_resource_t *dependent, pe_resource_t *primary,
             dependent->priority = -INFINITY;
 
         } else if (primary_instance != NULL) {
-            int new_priority = pe__add_scores(dependent->priority,
-                                              constraint->score);
+            int new_priority = pcmk__add_scores(dependent->priority,
+                                                constraint->score);
 
             pe_rsc_debug(dependent, "Applying %s to %s",
                          constraint->id, dependent->id);

commit 026eccc0caec780fbf8aae8fcfcf18ad546977d1

Refactor: libcrmcommon: separate score-related APIs into their own headers
... to parallel scores.c.

c187d8d7e mistakenly added pcmk__add_scores() to a public header, but the function
is documented as internal, so it is now moved to an internal header.

include/crm/common/Makefile.am
@@ -37,6 +37,8 @@ header_HEADERS = acl.h 			\
 		 rules.h		\
 		 scheduler.h		\
 		 scheduler_types.h	\
+		 scores.h		\
+		 scores_compat.h	\
 		 tags.h			\
 		 tickets.h		\
 		 util.h 		\

include/crm/common/internal.h
@@ -28,6 +28,7 @@
 #include <crm/common/results_internal.h>
 #include <crm/common/messages_internal.h>
 #include <crm/common/nvpair_internal.h>
+#include <crm/common/scores_internal.h>
 #include <crm/common/strings_internal.h>
 #include <crm/common/acl_internal.h>
 

include/crm/common/scores.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2004-2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_SCORES__H
+#define PCMK__CRM_COMMON_SCORES__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \file
+ * \brief Pacemaker APIs related to scores
+ * \ingroup core
+ */
+
+const char *pcmk_readable_score(int score);
+int char2score(const char *score);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PCMK__CRM_COMMON_SCORES__H

include/crm/common/scores_compat.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2004-2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_SCORES_COMPAT__H
+#define PCMK__CRM_COMMON_SCORES_COMPAT__H
+
+#include <sys/types.h>  // size_t
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \file
+ * \brief Deprecated Pacemaker score APIs
+ * \ingroup core
+ * \deprecated Do not include this header directly. The APIs in this header, and
+ *             the header itself, will be removed in a future release.
+ */
+
+//! \deprecated Use pcmk_readable_score() instead
+char *score2char(int score);
+
+//! \deprecated Use pcmk_readable_score() instead
+char *score2char_stack(int score, char *buf, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PCMK__CRM_COMMON_SCORES_COMPAT__H

include/crm/common/scores_internal.h
@@ -1,0 +1,15 @@
+/*
+ * Copyright 2022-2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_SCORES_INTERNAL__H
+#define PCMK__CRM_COMMON_SCORES_INTERNAL__H
+
+int pcmk__add_scores(int score1, int score2);
+
+#endif // PCMK__CRM_COMMON_SCORES_INTERNAL__H

include/crm/common/util.h
@@ -22,6 +22,7 @@
 #  include <crm/common/actions.h>
 #  include <crm/common/agents.h>
 #  include <crm/common/results.h>
+#  include <crm/common/scores.h>
 #  include <crm/common/nvpair.h>
 
 #ifdef __cplusplus
@@ -39,11 +40,6 @@ char *pcmk_promotion_score_name(const char *rsc_id);
 
 /* public Pacemaker Remote functions (from remote.c) */
 int crm_default_remote_port(void);
-
-/* public score-related functions (from scores.c) */
-const char *pcmk_readable_score(int score);
-int char2score(const char *score);
-int pcmk__add_scores(int score1, int score2);
 
 /* public string functions (from strings.c) */
 gboolean crm_is_true(const char *s);

include/crm/common/util_compat.h
@@ -158,12 +158,6 @@ crm_ttoa(time_t epoch_time)
 //! \deprecated Do not use Pacemaker libraries for generic I/O
 void crm_build_path(const char *path_c, mode_t mode);
 
-//! \deprecated Use pcmk_readable_score() instead
-char *score2char(int score);
-
-//! \deprecated Use pcmk_readable_score() instead
-char *score2char_stack(int score, char *buf, size_t len);
-
 //! \deprecated Use \c pcmk_parse_interval_spec() instead
 guint crm_parse_interval_spec(const char *input);
 

lib/common/scores.c
@@ -142,7 +142,7 @@ pcmk__add_scores(int score1, int score2)
 // Deprecated functions kept only for backward API compatibility
 // LCOV_EXCL_START
 
-#include <crm/common/util_compat.h>
+#include <crm/common/scores_compat.h>
 
 char *
 score2char(int score)