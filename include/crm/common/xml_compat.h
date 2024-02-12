c8f536ce242fb7832ea09c1c8aacaac66c1ea153
Reid Wahl <nrwahl@protonmail.com>
2024-02-11 21:19:26 -0800

    API: libcrmcommon: Deprecate crm_map_element_name()
    Will no longer be useful after "master" terminology is dropped at 3.0.0
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/xml_compat.h
+++ include/crm/common/xml_compat.h
@@ -166,6 +166,20 @@ gboolean add_message_xml(xmlNode *msg, const char *field, xmlNode *xml);
 //! \deprecated Do not use Pacemaker for general-purpose XML manipulation
 xmlNode *get_message_xml(const xmlNode *msg, const char *field);
 
+//! \deprecated Do not use
+static inline const char *
+crm_map_element_name(const xmlNode *xml)
+{
+    if (xml == NULL) {
+        return NULL;
+    } else if (strcmp((const char *) xml->name, "master") == 0) {
+        // Can't use PCMK__XE_PROMOTABLE_LEGACY because it's internal
+        return PCMK_XE_CLONE;
+    } else {
+        return (const char *) xml->name;
+    }
+}
+
 #ifdef __cplusplus
 }
 #endif
