ec7a123d20a3762caf599595241af6f428ff1354
Reid Wahl <nrwahl@protonmail.com>
2024-02-11 20:57:34 -0800

    API: libcrmcommon: Deprecate add_message_xml()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/xml_compat.h
+++ include/crm/common/xml_compat.h
@@ -136,6 +136,9 @@ int find_xml_children(xmlNode **children, xmlNode *root, const char *tag,
 xmlNode *get_xpath_object_relative(const char *xpath, xmlNode *xml_obj,
                                    int error_level);
 
+//! \deprecated Do not use Pacemaker for general-purpose XML manipulation
+gboolean add_message_xml(xmlNode *msg, const char *field, xmlNode *xml);
+
 #ifdef __cplusplus
 }
 #endif
