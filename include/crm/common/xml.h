403df6b2e49587f5cf1480e4ecddf7a26891d2ca
Reid Wahl <nrwahl@protonmail.com>
2024-02-12 21:43:34 -0800

    API: libcrmcommon: Deprecate free_xml()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/xml.h
+++ include/crm/common/xml.h
@@ -50,8 +50,6 @@ char *calculate_operation_digest(xmlNode * local_cib, const char *version);
 char *calculate_xml_versioned_digest(xmlNode * input, gboolean sort, gboolean do_filter,
                                      const char *version);
 
-void free_xml(xmlNode * child);
-
 xmlNode *sorted_xml(xmlNode * input, xmlNode * parent, gboolean recursive);
 xmlXPathObjectPtr xpath_search(const xmlNode *xml_top, const char *path);
 void crm_foreach_xpath_result(xmlNode *xml, const char *xpath,
