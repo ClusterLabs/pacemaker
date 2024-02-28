c2bd7b769b4788d2c14b3a56ce8f8ba6c2b2d387
Chris Lumens <clumens@redhat.com>
2024-02-28 15:01:19 -0500

    Test: build: Move common test environment stuff into tap.mk.
    I don't think there's any reason these settings can't always be applied.
    And while I'm at it, rework how the environment is set for consistency
    with other Makefile variables.

--- mk/tap.mk
+++ mk/tap.mk
@@ -7,10 +7,12 @@
 # or later (GPLv2+) WITHOUT ANY WARRANTY.
 #
 
-AM_TESTS_ENVIRONMENT= 					\
-	G_DEBUG=gc-friendly 				\
-	MALLOC_CHECK_=2 				\
-	MALLOC_PERTURB_=$$(($${RANDOM:-256} % 256))
+AM_TESTS_ENVIRONMENT = G_DEBUG=gc-friendly
+AM_TESTS_ENVIRONMENT += MALLOC_CHECK_=2
+AM_TESTS_ENVIRONMENT += MALLOC_PERTURB_=$$(($${RANDOM:-256} % 256))
+AM_TESTS_ENVIRONMENT += PCMK_CTS_CLI_DIR=$(top_srcdir)/cts/cli
+AM_TESTS_ENVIRONMENT += PCMK_schema_directory=$(top_builddir)/xml
+
 LOG_DRIVER = env AM_TAP_AWK='$(AWK)' $(SHELL) $(top_srcdir)/tests/tap-driver.sh
 LOG_COMPILER = $(top_srcdir)/tests/tap-test
 CLEANFILES = *.log *.trs
