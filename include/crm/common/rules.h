ff8a250e1d29dc3f0abcdda7bd94aea0b48ed5a6
Ken Gaillot <kgaillot@redhat.com>
2024-02-28 10:58:28 -0600

    API: libcrmcommon: add pcmk_rule_input_t type
    This new type can hold all data needed to evaluate a rule, and is intended to
    combine and replace the pe_re_match_data_t, pe_match_data_t,
    pe_rsc_eval_data_t, pe_op_eval_data_t, and pe_rule_eval_data_t types. Nothing
    uses it as of this commit.

--- include/crm/common/rules.h
+++ include/crm/common/rules.h
@@ -10,6 +10,10 @@
 #ifndef PCMK__CRM_COMMON_RULES__H
 #  define PCMK__CRM_COMMON_RULES__H
 
+#include <glib.h>                   // guint, GHashTable
+#include <regex.h>                  // regmatch_t
+#include <crm/common/iso8601.h>     // crm_time_t
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -47,6 +51,55 @@ enum expression_type {
 #endif
 };
 //!@}
+
+//! Data used to evaluate a rule (any NULL items are ignored)
+typedef struct pcmk_rule_input {
+    // Used to evaluate date expressions
+    const crm_time_t *now; //!< Current time for rule evaluation purposes
+
+    // Used to evaluate resource type expressions
+    const char *rsc_standard;   //!< Resource standard that rule applies to
+    const char *rsc_provider;   //!< Resource provider that rule applies to
+    const char *rsc_agent;      //!< Resource agent that rule applies to
+
+    // Used to evaluate operation type expressions
+    const char *op_name;    //! Operation name that rule applies to
+    guint op_interval_ms;   //! Operation interval that rule applies to
+
+    // Remaining members are used to evaluate node attribute expressions
+
+    /*!
+     * Node attributes for rule evaluation purposes
+     *
+     * \note Though not const, this is used only with g_hash_table_lookup().
+     */
+    GHashTable *node_attrs;
+
+    // Remaining members are used only within location constraint rules
+
+    /*!
+     * Resource parameters that can be used as the reference value source
+     *
+     * \note Though not const, this is used only with g_hash_table_lookup().
+     */
+    GHashTable *rsc_params;
+
+    /*!
+     * Resource meta-attributes that can be used as the reference value source
+     *
+     * \note Though not const, this is used only with g_hash_table_lookup().
+     */
+    GHashTable *rsc_meta;
+
+    //! Resource ID to compare against a location constraint's resource pattern
+    const char *rsc_id;
+
+    //! Resource pattern submatches (as set by regexec()) for rsc_id
+    const regmatch_t *rsc_id_submatches;
+
+    //! Number of entries in rsc_id_submatches
+    int rsc_id_nmatches;
+} pcmk_rule_input_t;
 
 #ifdef __cplusplus
 }
