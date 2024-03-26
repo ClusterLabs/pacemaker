a90ad4768e5c5cc832024f31b4d739baa7f18ac4
Reid Wahl <nrwahl@protonmail.com>
2024-03-25 18:22:43 -0700

    API: libcrmcommon: New PCMK_VALUE_SCORE string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options.h
+++ include/crm/common/options.h
@@ -196,6 +196,7 @@ extern     "C" {
 #define PCMK_VALUE_RESTART                      "restart"
 #define PCMK_VALUE_RESTART_CONTAINER            "restart-container"
 #define PCMK_VALUE_REVOKED                      "revoked"
+#define PCMK_VALUE_SCORE                        "score"
 #define PCMK_VALUE_SELECT                       "select"
 #define PCMK_VALUE_SERIALIZE                    "Serialize"
 #define PCMK_VALUE_STANDBY                      "standby"
