f2055b62a04f093ee6522f678a4f79a3543edccd
Reid Wahl <nrwahl@protonmail.com>
2024-04-04 02:26:25 -0700

    Refactor: libcrmcommon: PCMK__XA_ST_CALLDATA -> PCMK__XE_ST_CALLDATA
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/fenced/fenced_history.c
+++ daemons/fenced/fenced_history.c
@@ -53,7 +53,7 @@ stonith_send_broadcast_history(xmlNode *history,
     crm_xml_add_int(bcast, PCMK__XA_ST_CALLOPT, callopts);
 
     pcmk__xml_copy(data, history);
-    add_message_xml(bcast, PCMK__XA_ST_CALLDATA, data);
+    add_message_xml(bcast, PCMK__XE_ST_CALLDATA, data);
     send_cluster_message(NULL, crm_msg_stonith_ng, bcast, FALSE);
 
     free_xml(data);
