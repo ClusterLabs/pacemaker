ClusterLabs/pacemaker

commit 12710e6902c1cb308b2fb817fd7c3b929063d271

Refactor: libcrmcommon: Simplify loops in mark_xml_changes()
Neither old_child nor new_child gets removed from the tree during its
respective loop, so we can delay getting the next child until the next
iteration without changing behavior.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -1487,7 +1487,6 @@ mark_child_moved(xmlNode *old_child, xmlNode *new_parent, xmlNode *new_child,
 static void
 mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
 {
-    xmlNode *cIter = NULL;
     xml_node_private_t *nodepriv = NULL;
 
     CRM_CHECK(new_xml != NULL, return);
@@ -1509,11 +1508,11 @@ mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
     xml_diff_attrs(old_xml, new_xml);
 
     // Check for differences in the original children
-    for (cIter = pcmk__xml_first_child(old_xml); cIter != NULL; ) {
-        xmlNode *old_child = cIter;
-        xmlNode *new_child = pcmk__xml_match(new_xml, cIter, true);
+    for (xmlNode *old_child = pcmk__xml_first_child(old_xml); old_child != NULL;
+         old_child = pcmk__xml_next(old_child)) {
 
-        cIter = pcmk__xml_next(cIter);
+        xmlNode *new_child = pcmk__xml_match(new_xml, old_child, true);
+
         if(new_child) {
             mark_xml_changes(old_child, new_child, TRUE);
 
@@ -1523,11 +1522,11 @@ mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
     }
 
     // Check for moved or created children
-    for (cIter = pcmk__xml_first_child(new_xml); cIter != NULL; ) {
-        xmlNode *new_child = cIter;
-        xmlNode *old_child = pcmk__xml_match(old_xml, cIter, true);
+    for (xmlNode *new_child = pcmk__xml_first_child(new_xml); new_child != NULL;
+         new_child = pcmk__xml_next(new_child)) {
 
-        cIter = pcmk__xml_next(cIter);
+        xmlNode *old_child = pcmk__xml_match(old_xml, new_child, true);
+
         if(old_child == NULL) {
             // This is a newly created child
             nodepriv = new_child->_private;

commit 6b4052cc3b49231dc49886bb40887b49b7774162

Fix: libcrmcommon: Avoid use-after-free in mark_xml_changes()
mark_xml_changes() with check_top=true may free new_child via
pcmk__apply_creation_acl(). Calling pcmk__xml_next(new_child) after that
is a use-after-free error. This caused a seg fault on FreeBSD.

Regression introduced in 12710e69.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -1459,6 +1459,8 @@ mark_child_moved(xmlNode *old_child, xmlNode *new_parent, xmlNode *new_child,
 static void
 mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
 {
+    xmlNode *old_child = NULL;
+    xmlNode *new_child = NULL;
     xml_node_private_t *nodepriv = NULL;
 
     CRM_CHECK(new_xml != NULL, return);
@@ -1480,13 +1482,13 @@ mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
     xml_diff_attrs(old_xml, new_xml);
 
     // Check for differences in the original children
-    for (xmlNode *old_child = pcmk__xml_first_child(old_xml); old_child != NULL;
+    for (old_child = pcmk__xml_first_child(old_xml); old_child != NULL;
          old_child = pcmk__xml_next(old_child)) {
 
-        xmlNode *new_child = pcmk__xml_match(new_xml, old_child, true);
+        new_child = pcmk__xml_match(new_xml, old_child, true);
 
-        if(new_child) {
-            mark_xml_changes(old_child, new_child, TRUE);
+        if (new_child != NULL) {
+            mark_xml_changes(old_child, new_child, true);
 
         } else {
             mark_child_deleted(old_child, new_xml);
@@ -1494,16 +1496,19 @@ mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
     }
 
     // Check for moved or created children
-    for (xmlNode *new_child = pcmk__xml_first_child(new_xml); new_child != NULL;
-         new_child = pcmk__xml_next(new_child)) {
+    new_child = pcmk__xml_first_child(new_xml);
+    while (new_child != NULL) {
+        xmlNode *next = pcmk__xml_next(new_child);
 
-        xmlNode *old_child = pcmk__xml_match(old_xml, new_child, true);
+        old_child = pcmk__xml_match(old_xml, new_child, true);
 
-        if(old_child == NULL) {
+        if (old_child == NULL) {
             // This is a newly created child
             nodepriv = new_child->_private;
             pcmk__set_xml_flags(nodepriv, pcmk__xf_skip);
-            mark_xml_changes(old_child, new_child, TRUE);
+
+            // May free new_child
+            mark_xml_changes(old_child, new_child, true);
 
         } else {
             /* Check for movement, we already checked for differences */
@@ -1514,6 +1519,8 @@ mark_xml_changes(xmlNode *old_xml, xmlNode *new_xml, bool check_top)
                 mark_child_moved(old_child, new_xml, new_child, p_old, p_new);
             }
         }
+
+        new_child = next;
     }
 }
 