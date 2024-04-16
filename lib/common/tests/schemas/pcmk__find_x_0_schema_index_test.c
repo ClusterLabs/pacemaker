4b2bea559f125ee882527743cc746976a6577355
Ken Gaillot <kgaillot@redhat.com>
2024-04-16 15:51:47 -0500

    Test: libcrmcommon: avoid test vs production discrepancy in schema unit testing
    We have special logic in schemas.c for unit tests to get around the fact that
    the test for pcmk__find_x_0_schema_index() previously didn't call
    crm_schema_init() and thus didn't have the "pacemaker-next" and "none" schemas.
    
    However it's a bad idea to have differences between unit tests and production,
    and an even worse idea to change functional behavior for a single test that
    would fail if called by the unit test for any other function.
    
    Restructure the pcmk__find_x_0_schema_index() test so it calls
    crm_schema_init(). This requires moving RNG files around, so create a unique
    schema directory for this unit test, to avoid conflicting with other tests run
    in parallel.

--- lib/common/tests/schemas/pcmk__find_x_0_schema_index_test.c
+++ lib/common/tests/schemas/pcmk__find_x_0_schema_index_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the Pacemaker project contributors
+ * Copyright 2023-2024 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -8,105 +8,86 @@
  */
 
 #include <crm_internal.h>
-#include <crm/common/unittest_internal.h>
 
+#include <stdio.h>  // NULL, rename()
+#include <stdlib.h> // setenv(), unsetenv()
 #include <glib.h>
 
+#include <crm/common/unittest_internal.h>
 #include "crmcommon_private.h"
 
-static pcmk__schema_t *
-mk_schema(const char *name, unsigned char x, unsigned char y)
-{
-    pcmk__schema_t *schema = malloc(sizeof(pcmk__schema_t));
+#define SCHEMA_PREFIX PCMK__TEST_SCHEMA_DIR "/find_x_0/pacemaker-"
 
-    schema->name = strdup(name);
-    schema->version.v[0] = x;
-    schema->version.v[1] = y;
-    return schema;
+static int
+setup(void **state)
+{
+    // Use a unique schema directory so we can move files around
+    setenv("PCMK_schema_directory", PCMK__TEST_SCHEMA_DIR "/find_x_0", 1);
+    return 0;
+}
+
+static int
+teardown(void **state)
+{
+    unsetenv("PCMK_schema_directory");
+    return 0;
 }
 
 static void
-free_schema(void *data)
+last_is_0(void **state)
 {
-    pcmk__schema_t *schema = data;
-    free(schema->name);
-    free(schema);
+    /* This loads all the schemas normally linked for unit testing, so we have
+     * many 1.x and 2.x schemas and a single pacemaker-3.0 schema at index 14.
+     */
+    crm_schema_init();
+
+    assert_int_equal(14, pcmk__find_x_0_schema_index());
+    assert_string_equal(get_schema_name(14), "pacemaker-3.0");
+
+    crm_schema_cleanup();
 }
 
 static void
-empty_schema_list(void **state)
+last_is_not_0(void **state)
 {
-    pcmk__assert_asserts(pcmk__find_x_0_schema_index(NULL));
+    /* Disable the pacemaker-3.0 schema, so we now should get pacemaker-2.0 at
+     * index 3.
+     */
+    assert_int_equal(0, rename(SCHEMA_PREFIX "3.0.rng",
+                               SCHEMA_PREFIX "3.0.bak"));
+    crm_schema_init();
+
+    assert_int_equal(3, pcmk__find_x_0_schema_index());
+    assert_string_equal(get_schema_name(3), "pacemaker-2.0");
+
+    assert_int_equal(0, rename(SCHEMA_PREFIX "3.0.bak",
+                               SCHEMA_PREFIX "3.0.rng"));
+    crm_schema_cleanup();
 }
 
 static void
-singleton_schema_list(void **state)
+schema_0_missing(void **state)
 {
-    GList *schemas = NULL;
+    /* Disable the pacemaker-3.0 and pacemaker-2.0 schemas, so we now should get
+     * pacemaker-2.1 at index 3.
+     */
+    assert_int_equal(0, rename(SCHEMA_PREFIX "3.0.rng",
+                               SCHEMA_PREFIX "3.0.bak"));
+    assert_int_equal(0, rename(SCHEMA_PREFIX "2.0.rng",
+                               SCHEMA_PREFIX "2.0.bak"));
+    crm_schema_init();
 
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.0", 1, 0));
-    assert_int_equal(0, pcmk__find_x_0_schema_index(schemas));
-    g_list_free_full(schemas, free_schema);
+    assert_int_equal(3, pcmk__find_x_0_schema_index());
+    assert_string_equal(get_schema_name(3), "pacemaker-2.1");
+
+    assert_int_equal(0, rename(SCHEMA_PREFIX "2.0.bak",
+                               SCHEMA_PREFIX "2.0.rng"));
+    assert_int_equal(0, rename(SCHEMA_PREFIX "3.0.bak",
+                               SCHEMA_PREFIX "3.0.rng"));
+    crm_schema_cleanup();
 }
 
-static void
-one_major_version(void **state)
-{
-    GList *schemas = NULL;
-
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.0", 1, 0));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.2", 1, 2));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.3", 1, 3));
-    assert_int_equal(0, pcmk__find_x_0_schema_index(schemas));
-    g_list_free_full(schemas, free_schema);
-}
-
-static void
-first_version_is_not_0(void **state)
-{
-    GList *schemas = NULL;
-
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.1", 1, 1));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.2", 1, 2));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.3", 1, 3));
-    assert_int_equal(0, pcmk__find_x_0_schema_index(schemas));
-    g_list_free_full(schemas, free_schema);
-}
-
-static void
-multiple_major_versions(void **state)
-{
-    GList *schemas = NULL;
-
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.0", 1, 0));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.1", 1, 1));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-2.0", 2, 0));
-    assert_int_equal(2, pcmk__find_x_0_schema_index(schemas));
-    g_list_free_full(schemas, free_schema);
-}
-
-static void
-many_versions(void **state)
-{
-    GList *schemas = NULL;
-
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.0", 1, 0));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.1", 1, 1));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-1.2", 1, 2));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-2.0", 2, 0));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-2.1", 2, 1));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-2.2", 2, 2));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-3.0", 3, 0));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-3.1", 3, 1));
-    schemas = g_list_append(schemas, mk_schema("pacemaker-3.2", 3, 2));
-    assert_int_equal(6, pcmk__find_x_0_schema_index(schemas));
-    g_list_free_full(schemas, free_schema);
-}
-
-PCMK__UNIT_TEST(NULL, NULL,
-                cmocka_unit_test(empty_schema_list),
-                cmocka_unit_test(singleton_schema_list),
-                cmocka_unit_test(one_major_version),
-                cmocka_unit_test(first_version_is_not_0),
-                cmocka_unit_test(multiple_major_versions),
-                cmocka_unit_test(many_versions))
+PCMK__UNIT_TEST(setup, teardown,
+                cmocka_unit_test(last_is_0),
+                cmocka_unit_test(last_is_not_0),
+                cmocka_unit_test(schema_0_missing))
