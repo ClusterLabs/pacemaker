ClusterLabs/pacemaker

commit 3d16e0883b7e5d489a44f2ae570f63c67f708970

High: libcib: Fix handling the return value of cib_internal_op.
This function doesn't return a standard pacemaker return code.  It
returns a negative value on error and the positive call ID on success.
So, be more careful handling this return value.  Note that the find_attr
function that wraps it does return a standard pacemaker return code.

lib/cib/cib_attrs.c
@@ -151,13 +151,14 @@ find_attr(pcmk__output_t *out, cib_t *cib, const char *attr, const char *section
 
     rc = cib_internal_op(cib, CIB_OP_QUERY, NULL, xpath_string, NULL, &xml_search,
                          cib_sync_call | cib_scope_local | cib_xpath, user_name);
-    rc = pcmk_legacy2rc(rc);
-
-    if (rc != pcmk_rc_ok) {
+    if (rc < 0) {
+        rc = pcmk_legacy2rc(rc);
         crm_trace("Query failed for attribute %s (section=%s, node=%s, set=%s, xpath=%s): %s",
                   attr_name, section, crm_str(node_uuid), crm_str(set_name), xpath_string,
                   pcmk_rc_str(rc));
         goto done;
+    } else {
+        rc = pcmk_rc_ok;
     }
 
     crm_log_xml_debug(xml_search, "Match");
@@ -325,12 +326,14 @@ cib__update_node_attr(pcmk__output_t *out, cib_t *cib, int call_options, const c
     crm_log_xml_trace(xml_top, "update_attr");
     rc = cib_internal_op(cib, CIB_OP_MODIFY, NULL, section, xml_top, NULL,
                          call_options | cib_quorum_override, user_name);
-    rc = pcmk_legacy2rc(rc);
+    if (rc < 0) {
+        rc = pcmk_legacy2rc(rc);
 
-    if (rc != pcmk_rc_ok) {
         out->err(out, "Error setting %s=%s (section=%s, set=%s): %s",
                  attr_name, attr_value, section, crm_str(set_name), pcmk_rc_str(rc));
         crm_log_xml_info(xml_top, "Update");
+    } else {
+        rc = pcmk_rc_ok;
     }
 
     free(local_set_name);
@@ -391,9 +394,10 @@ cib__delete_node_attr(pcmk__output_t *out, cib_t *cib, int options, const char *
 
     rc = cib_internal_op(cib, CIB_OP_DELETE, NULL, section, xml_obj, NULL,
                          options | cib_quorum_override, user_name);
-    rc = pcmk_legacy2rc(rc);
-
-    if (rc == pcmk_rc_ok) {
+    if (rc < 0) {
+        rc = pcmk_legacy2rc(rc);
+    } else {
+        rc = pcmk_rc_ok;
         out->info(out, "Deleted %s %s: id=%s%s%s%s%s",
                   section, node_uuid ? "attribute" : "option", local_attr_id,
                   set_name ? " set=" : "", set_name ? set_name : "",

commit 05a8745dcea377cc2e9d3b270662660e6859fcd0

Low: libcib: Fix handling of return values in CIB attr update/delete
Fixes regression introduced by 3d16e088. That commit message says:

"This function doesn't return a standard pacemaker return code.  It
returns a negative value on error and the positive call ID on success."

But these may be synchronous calls, in which case the return value is
the (legacy) Pacemaker return code, not the call ID.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_attrs.c
@@ -133,14 +133,14 @@ find_attr(cib_t *cib, const char *section, const char *node_uuid,
     rc = cib_internal_op(cib, PCMK__CIB_REQUEST_QUERY, NULL,
                          (const char *) xpath->str, NULL, &xml_search,
                          cib_sync_call|cib_scope_local|cib_xpath, user_name);
-    if (rc < 0) {
-        rc = pcmk_legacy2rc(rc);
+    rc = pcmk_legacy2rc(rc);
+
+    if (rc != pcmk_rc_ok) {
         crm_trace("Query failed for attribute %s (section=%s, node=%s, set=%s, xpath=%s): %s",
                   attr_name, section, pcmk__s(node_uuid, "<null>"),
                   pcmk__s(set_name, "<null>"), (const char *) xpath->str,
                   pcmk_rc_str(rc));
     } else {
-        rc = pcmk_rc_ok;
         crm_log_xml_debug(xml_search, "Match");
     }
 
@@ -318,15 +318,20 @@ cib__update_node_attr(pcmk__output_t *out, cib_t *cib, int call_options, const c
     crm_log_xml_trace(xml_top, "update_attr");
     rc = cib_internal_op(cib, PCMK__CIB_REQUEST_MODIFY, NULL, section, xml_top,
                          NULL, call_options, user_name);
-    if (rc < 0) {
-        rc = pcmk_legacy2rc(rc);
 
+    if (!pcmk_is_set(call_options, cib_sync_call) && (cib->variant != cib_file)
+        && (rc >= 0)) {
+        // For async call, positive rc is the call ID (file always synchronous)
+        rc = pcmk_rc_ok;
+    } else {
+        rc = pcmk_legacy2rc(rc);
+    }
+
+    if (rc != pcmk_rc_ok) {
         out->err(out, "Error setting %s=%s (section=%s, set=%s): %s",
                  attr_name, attr_value, section, pcmk__s(set_name, "<null>"),
                  pcmk_rc_str(rc));
         crm_log_xml_info(xml_top, "Update");
-    } else {
-        rc = pcmk_rc_ok;
     }
 
     free(local_set_name);
@@ -394,16 +399,21 @@ cib__delete_node_attr(pcmk__output_t *out, cib_t *cib, int options, const char *
 
     rc = cib_internal_op(cib, PCMK__CIB_REQUEST_DELETE, NULL, section, xml_obj,
                          NULL, options, user_name);
-    if (rc < 0) {
-        rc = pcmk_legacy2rc(rc);
-    } else {
+
+    if (!pcmk_is_set(options, cib_sync_call) && (cib->variant != cib_file)
+        && (rc >= 0)) {
+        // For async call, positive rc is the call ID (file always synchronous)
         rc = pcmk_rc_ok;
+    } else {
+        rc = pcmk_legacy2rc(rc);
+    }
+
+    if (rc == pcmk_rc_ok) {
         out->info(out, "Deleted %s %s: id=%s%s%s%s%s",
                   section, node_uuid ? "attribute" : "option", local_attr_id,
                   set_name ? " set=" : "", set_name ? set_name : "",
                   attr_name ? " name=" : "", attr_name ? attr_name : "");
     }
-
     free(local_attr_id);
     free_xml(xml_obj);
     return rc;