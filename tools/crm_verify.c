ClusterLabs/pacemaker

commit 2aa8a9e2ee0bbd8e9601347794bd31a7da133524

Fix: libcrmcommon: Write crm_verify detailed messages to XML
...when requested

tools/crm_verify.c
@@ -159,6 +159,9 @@ main(int argc, char **argv)
 
     pcmk__register_lib_messages(out);
 
+    pcmk__set_config_error_handler((pcmk__config_error_func) out->err, out);
+    pcmk__set_config_warning_handler((pcmk__config_warning_func) out->err, out);
+
     crm_info("=#=#=#=#= Getting XML =#=#=#=#=");
 
     if (options.use_live_cib) {
@@ -216,7 +219,7 @@ main(int argc, char **argv)
         create_xml_node(cib_object, XML_CIB_TAG_STATUS);
     }
 
-    if (validate_xml(cib_object, NULL, FALSE) == FALSE) {
+    if (pcmk__validate_xml(cib_object, NULL, (xmlRelaxNGValidityErrorFunc) out->err, out) == FALSE) {
         pcmk__config_err("CIB did not pass schema validation");
         free_xml(cib_object);
         cib_object = NULL;
@@ -258,18 +261,18 @@ main(int argc, char **argv)
     if (crm_config_error) {
         rc = pcmk_rc_schema_validation;
 
-        if (args->verbosity > 0) {
+        if (args->verbosity > 0 || pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
             g_set_error(&error, PCMK__RC_ERROR, rc,
                         "Errors found during check: config not valid");
         } else {
             g_set_error(&error, PCMK__RC_ERROR, rc,
                         "Errors found during check: config not valid\n-V may provide more details");
-        }
+        } 
 
     } else if (crm_config_warning) {
         rc = pcmk_rc_schema_validation;
 
-        if (args->verbosity > 0) {
+        if (args->verbosity > 0 || pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
             g_set_error(&error, PCMK__RC_ERROR, rc,
                         "Warnings found during check: config may not be valid");
         } else {
@@ -289,7 +292,7 @@ main(int argc, char **argv)
         exit_code = pcmk_rc2exitc(rc);
     }
 
-    pcmk__output_and_clear_error(&error, NULL);
+    pcmk__output_and_clear_error(&error, out);
 
     if (out != NULL) {
         out->finish(out, exit_code, true, NULL);

include/crm/common/logging_internal.h
@@ -19,6 +19,18 @@ extern "C" {
 #  include <crm/common/logging.h>
 #  include <crm/common/output_internal.h>
 
+typedef void (*pcmk__config_error_func) (void *ctx, const char *msg, ...);
+typedef void (*pcmk__config_warning_func) (void *ctx, const char *msg, ...);
+
+extern pcmk__config_error_func pcmk__config_error_handler;
+extern pcmk__config_warning_func pcmk__config_warning_handler;
+
+extern void *pcmk__config_error_context;
+extern void *pcmk__config_warning_context;
+
+void pcmk__set_config_error_handler(pcmk__config_error_func error_handler, void *error_context);
+void pcmk__set_config_warning_handler(pcmk__config_warning_func warning_handler, void *warning_context);
+
 /*!
  * \internal
  * \brief Log a configuration error
@@ -26,9 +38,13 @@ extern "C" {
  * \param[in] fmt   printf(3)-style format string
  * \param[in] ...   Arguments for format string
  */
-#  define pcmk__config_err(fmt...) do {     \
-        crm_config_error = TRUE;            \
-        crm_err(fmt);                       \
+#  define pcmk__config_err(fmt...) do {                             \
+        crm_config_error = TRUE;                                    \
+        if (pcmk__config_error_handler == NULL) {                   \
+            crm_err(fmt);                                           \
+        } else {                                                    \
+            pcmk__config_error_handler(pcmk__config_error_context, fmt);   \
+        }                                                           \
     } while (0)
 
 /*!
@@ -38,9 +54,13 @@ extern "C" {
  * \param[in] fmt   printf(3)-style format string
  * \param[in] ...   Arguments for format string
  */
-#  define pcmk__config_warn(fmt...) do {    \
-        crm_config_warning = TRUE;          \
-        crm_warn(fmt);                      \
+#  define pcmk__config_warn(fmt...) do {                            \
+        crm_config_warning = TRUE;                                  \
+        if (pcmk__config_warning_handler == NULL) {                   \
+            crm_warn(fmt);                                           \
+        } else {                                                    \
+            pcmk__config_warning_handler(pcmk__config_warning_context, fmt);   \
+        }                                                           \
     } while (0)
 
 /*!

include/crm/common/xml_internal.h
@@ -21,6 +21,7 @@
 #  include <crm/crm.h>  /* transitively imports qblog.h */
 #  include <crm/common/output_internal.h>
 
+#  include <libxml/relaxng.h>
 
 /*!
  * \brief Base for directing lib{xml2,xslt} log into standard libqb backend
@@ -439,5 +440,9 @@ pcmk__xml_attr_value(const xmlAttr *attr)
     return ((attr == NULL) || (attr->children == NULL))? NULL
            : (const char *) attr->children->content;
 }
+
+gboolean pcmk__validate_xml(xmlNode *xml_blob, const char *validation,
+                            xmlRelaxNGValidityErrorFunc error_handler, 
+                            void *error_handler_context);
 
 #endif // PCMK__XML_INTERNAL__H

lib/common/logging.c
@@ -51,6 +51,11 @@ static unsigned int crm_log_priority = LOG_NOTICE;
 static GLogFunc glib_log_default = NULL;
 static pcmk__output_t *logger_out = NULL;
 
+pcmk__config_error_func pcmk__config_error_handler = NULL;
+pcmk__config_warning_func pcmk__config_warning_handler = NULL;
+void *pcmk__config_error_context = NULL;
+void *pcmk__config_warning_context = NULL;
+
 static gboolean crm_tracing_enabled(void);
 
 static void
@@ -1279,3 +1284,15 @@ pcmk_log_xml_impl(uint8_t level, const char *text, const xmlNode *xml)
 
 // LCOV_EXCL_STOP
 // End deprecated API
+
+void pcmk__set_config_error_handler(pcmk__config_error_func error_handler, void *error_context)
+{
+    pcmk__config_error_handler = error_handler;
+    pcmk__config_error_context = error_context;    
+}
+
+void pcmk__set_config_warning_handler(pcmk__config_warning_func warning_handler, void *warning_context)
+{
+    pcmk__config_warning_handler = warning_handler;
+    pcmk__config_warning_context = warning_context;   
+}

lib/common/schemas.c
@@ -433,7 +433,7 @@ crm_schema_init(void)
 }
 
 static gboolean
-validate_with_relaxng(xmlDocPtr doc, gboolean to_logs, const char *relaxng_file,
+validate_with_relaxng(xmlDocPtr doc, xmlRelaxNGValidityErrorFunc error_handler, void *error_handler_context, const char *relaxng_file,
                       relaxng_ctx_cache_t **cached_ctx)
 {
     int rc = 0;
@@ -454,11 +454,11 @@ validate_with_relaxng(xmlDocPtr doc, gboolean to_logs, const char *relaxng_file,
         ctx->parser = xmlRelaxNGNewParserCtxt(relaxng_file);
         CRM_CHECK(ctx->parser != NULL, goto cleanup);
 
-        if (to_logs) {
+        if (error_handler) {
             xmlRelaxNGSetParserErrors(ctx->parser,
-                                      (xmlRelaxNGValidityErrorFunc) xml_log,
-                                      (xmlRelaxNGValidityWarningFunc) xml_log,
-                                      GUINT_TO_POINTER(LOG_ERR));
+                                      (xmlRelaxNGValidityErrorFunc) error_handler,
+                                      (xmlRelaxNGValidityWarningFunc) error_handler,
+                                      error_handler_context);
         } else {
             xmlRelaxNGSetParserErrors(ctx->parser,
                                       (xmlRelaxNGValidityErrorFunc) fprintf,
@@ -474,11 +474,11 @@ validate_with_relaxng(xmlDocPtr doc, gboolean to_logs, const char *relaxng_file,
         ctx->valid = xmlRelaxNGNewValidCtxt(ctx->rng);
         CRM_CHECK(ctx->valid != NULL, goto cleanup);
 
-        if (to_logs) {
+        if (error_handler) {
             xmlRelaxNGSetValidErrors(ctx->valid,
-                                     (xmlRelaxNGValidityErrorFunc) xml_log,
-                                     (xmlRelaxNGValidityWarningFunc) xml_log,
-                                     GUINT_TO_POINTER(LOG_ERR));
+                                     (xmlRelaxNGValidityErrorFunc) error_handler,
+                                     (xmlRelaxNGValidityWarningFunc) error_handler,
+                                     error_handler_context);
         } else {
             xmlRelaxNGSetValidErrors(ctx->valid,
                                      (xmlRelaxNGValidityErrorFunc) fprintf,
@@ -561,7 +561,7 @@ crm_schema_cleanup(void)
 }
 
 static gboolean
-validate_with(xmlNode *xml, int method, gboolean to_logs)
+validate_with(xmlNode *xml, int method, xmlRelaxNGValidityErrorFunc error_handler, void* error_handler_context)
 {
     gboolean valid = FALSE;
     char *file = NULL;
@@ -590,7 +590,7 @@ validate_with(xmlNode *xml, int method, gboolean to_logs)
     switch (schema->validator) {
         case schema_validator_rng:
             cache = (relaxng_ctx_cache_t **) &(schema->cache);
-            valid = validate_with_relaxng(xml->doc, to_logs, file, cache);
+            valid = validate_with_relaxng(xml->doc, error_handler, error_handler_context, file, cache);
             break;
         default:
             crm_err("Unknown validator type: %d",
@@ -607,7 +607,7 @@ validate_with_silent(xmlNode *xml, int method)
 {
     bool rc, sl_backup = silent_logging;
     silent_logging = TRUE;
-    rc = validate_with(xml, method, TRUE);
+    rc = validate_with(xml, method, (xmlRelaxNGValidityErrorFunc) xml_log, GUINT_TO_POINTER(LOG_ERR));
     silent_logging = sl_backup;
     return rc;
 }
@@ -674,6 +674,12 @@ validate_xml_verbose(const xmlNode *xml_blob)
 gboolean
 validate_xml(xmlNode *xml_blob, const char *validation, gboolean to_logs)
 {
+    return pcmk__validate_xml(xml_blob, validation, to_logs ? (xmlRelaxNGValidityErrorFunc) xml_log : NULL, GUINT_TO_POINTER(LOG_ERR));
+}
+
+gboolean
+pcmk__validate_xml(xmlNode *xml_blob, const char *validation, xmlRelaxNGValidityErrorFunc error_handler, void* error_handler_context)
+{
     int version = 0;
 
     CRM_CHECK((xml_blob != NULL) && (xml_blob->doc != NULL), return FALSE);
@@ -687,7 +693,7 @@ validate_xml(xmlNode *xml_blob, const char *validation, gboolean to_logs)
         bool valid = FALSE;
 
         for (lpc = 0; lpc < xml_schema_max; lpc++) {
-            if (validate_with(xml_blob, lpc, FALSE)) {
+            if (validate_with(xml_blob, lpc, NULL, NULL)) {
                 valid = TRUE;
                 crm_xml_add(xml_blob, XML_ATTR_VALIDATION,
                             known_schemas[lpc].name);
@@ -705,7 +711,7 @@ validate_xml(xmlNode *xml_blob, const char *validation, gboolean to_logs)
     if (strcmp(validation, PCMK__VALUE_NONE) == 0) {
         return TRUE;
     } else if (version < xml_schema_max) {
-        return validate_with(xml_blob, version, to_logs);
+        return validate_with(xml_blob, version, error_handler, error_handler_context);
     }
 
     crm_err("Unknown validator: %s", validation);
@@ -988,6 +994,8 @@ update_validation(xmlNode **xml_blob, int *best, int max, gboolean transform,
     int max_stable_schemas = xml_latest_schema_index();
     int lpc = 0, match = -1, rc = pcmk_ok;
     int next = -1;  /* -1 denotes "inactive" value */
+    xmlRelaxNGValidityErrorFunc error_handler = 
+        to_logs ? (xmlRelaxNGValidityErrorFunc) xml_log : NULL;
 
     CRM_CHECK(best != NULL, return -EINVAL);
     *best = 0;
@@ -1024,7 +1032,7 @@ update_validation(xmlNode **xml_blob, int *best, int max, gboolean transform,
                   known_schemas[lpc].name ? known_schemas[lpc].name : "<unset>",
                   lpc, max_stable_schemas);
 
-        if (validate_with(xml, lpc, to_logs) == FALSE) {
+        if (validate_with(xml, lpc, error_handler, GUINT_TO_POINTER(LOG_ERR)) == FALSE) {
             if (next != -1) {
                 crm_info("Configuration not valid for schema: %s",
                          known_schemas[lpc].name);
@@ -1089,7 +1097,7 @@ update_validation(xmlNode **xml_blob, int *best, int max, gboolean transform,
                             known_schemas[lpc].transform);
                     rc = -pcmk_err_transform_failed;
 
-                } else if (validate_with(upgrade, next, to_logs)) {
+                } else if (validate_with(upgrade, next, error_handler, GUINT_TO_POINTER(LOG_ERR))) {
                     crm_info("Transformation %s.xsl successful",
                              known_schemas[lpc].transform);
                     lpc = next;

commit 987e0d2828e23f8b51e1d657315087a622ed0c20

Fix: tools: crm_verify respects verbosity for configuration issues
Because crm_verify previously set the configuration error/warning handlers to
out->err(), they were always displayed, regardless of quiet/verbose options.

Regression introduced by 2aa8a9e2ee0 (not yet released)

tools/crm_verify.c
@@ -42,6 +42,7 @@ struct {
     char *xml_file;
     gboolean xml_stdin;
     char *xml_string;
+    unsigned int verbosity;
 } options;
 
 static GOptionEntry data_entries[] = {
@@ -111,6 +112,30 @@ build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
     return context;
 }
 
+/*!
+ * \internal
+ * \brief Output a configuration issue
+ *
+ * \param[in] ctx  Output object
+ * \param[in] msg  printf(3)-style format string
+ * \param[in] ...  Format string arguments
+ */
+G_GNUC_PRINTF(2, 3)
+static void
+output_config_issue(void *ctx, const char *msg, ...)
+{
+    va_list ap;
+    char *buf = NULL;
+    pcmk__output_t *out = ctx;
+
+    va_start(ap, msg);
+    CRM_ASSERT(vasprintf(&buf, msg, ap) > 0);
+    if (options.verbosity > 0) {
+        out->err(out, "%s", buf);
+    }
+    va_end(ap);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -162,12 +187,13 @@ main(int argc, char **argv)
 
     pcmk__register_lib_messages(out);
 
-    pcmk__set_config_error_handler((pcmk__config_error_func) out->err, out);
-    pcmk__set_config_warning_handler((pcmk__config_warning_func) out->err, out);
+    pcmk__set_config_error_handler(output_config_issue, out);
+    pcmk__set_config_warning_handler(output_config_issue, out);
 
     if (pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
         args->verbosity = 1;
     }
+    options.verbosity = args->verbosity;
 
     if (options.xml_file != NULL) {
         cib_source = options.xml_file;
@@ -216,7 +242,7 @@ main(int argc, char **argv)
             failure_type = "";
           }
 
-          if (args->verbosity > 0) {
+          if (options.verbosity > 0) {
               out->err(out, "%sconfig not valid", failure_type);
           } else {
               out->err(out, "%sconfig not valid\n-V may provide more details", failure_type);