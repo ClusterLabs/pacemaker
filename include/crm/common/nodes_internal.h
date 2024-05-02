f7f1131be64896bad642a84771580ea91cb27d94
Ken Gaillot <kgaillot@redhat.com>
2024-05-02 10:20:50 -0500

    Refactor: libcrmcommon: move internal functions to proper header
    These were incorrectly added to a public header in 2.1.7. However they were
    named and documented as internal, so this does not break API backward
    compatibility.

--- include/crm/common/nodes_internal.h
+++ include/crm/common/nodes_internal.h
@@ -24,4 +24,47 @@
 
 pcmk_node_t *pcmk__find_node_in_list(const GList *nodes, const char *node_name);
 
+/*!
+ * \internal
+ * \brief Return a string suitable for logging as a node name
+ *
+ * \param[in] node  Node to return a node name string for
+ *
+ * \return Node name if available, otherwise node ID if available,
+ *         otherwise "unspecified node" if node is NULL or "unidentified node"
+ *         if node has neither a name nor ID.
+ */
+static inline const char *
+pcmk__node_name(const pcmk_node_t *node)
+{
+    if (node == NULL) {
+        return "unspecified node";
+
+    } else if (node->details->uname != NULL) {
+        return node->details->uname;
+
+    } else if (node->details->id != NULL) {
+        return node->details->id;
+
+    } else {
+        return "unidentified node";
+    }
+}
+
+/*!
+ * \internal
+ * \brief Check whether two node objects refer to the same node
+ *
+ * \param[in] node1  First node object to compare
+ * \param[in] node2  Second node object to compare
+ *
+ * \return true if \p node1 and \p node2 refer to the same node
+ */
+static inline bool
+pcmk__same_node(const pcmk_node_t *node1, const pcmk_node_t *node2)
+{
+    return (node1 != NULL) && (node2 != NULL)
+           && (node1->details == node2->details);
+}
+
 #endif  // PCMK__NODES_INTERNAL__H
