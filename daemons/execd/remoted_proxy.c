ClusterLabs/pacemaker

commit 7ebc31792926f90ff3267e1d61377cfa1173fa4c

Refactor: libcrmcommon: Drop add_message_xml() internally
It's short, so it's clearer to expand it in the caller. This also lets
us avoid making copies in some cases.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/execd/remoted_proxy.c
@@ -225,6 +225,7 @@ ipc_proxy_dispatch(qb_ipcs_connection_t * c, void *data, size_t size)
     uint32_t flags = 0;
     pcmk__client_t *client = pcmk__find_client(c);
     pcmk__client_t *ipc_proxy = pcmk__find_client_by_id(client->userdata);
+    xmlNode *wrapper = NULL;
     xmlNode *request = NULL;
     xmlNode *msg = NULL;
 
@@ -270,11 +271,13 @@ ipc_proxy_dispatch(qb_ipcs_connection_t * c, void *data, size_t size)
     crm_xml_add(msg, PCMK__XA_LRMD_IPC_USER, client->user);
     crm_xml_add_int(msg, PCMK__XA_LRMD_IPC_MSG_ID, id);
     crm_xml_add_int(msg, PCMK__XA_LRMD_IPC_MSG_FLAGS, flags);
-    add_message_xml(msg, PCMK__XE_LRMD_IPC_MSG, request);
-    lrmd_server_send_notify(ipc_proxy, msg);
-    free_xml(request);
-    free_xml(msg);
 
+    wrapper = pcmk__xe_create(msg, PCMK__XE_LRMD_IPC_MSG);
+    xmlAddChild(wrapper, request);
+
+    lrmd_server_send_notify(ipc_proxy, msg);
+
+    free_xml(msg);
     return 0;
 }
 

daemons/based/based_callbacks.c
@@ -168,8 +168,10 @@ create_cib_reply(const char *op, const char *call_id, const char *client_id,
     crm_xml_add_int(reply, PCMK__XA_CIB_RC, rc);
 
     if (call_data != NULL) {
+        xmlNode *wrapper = pcmk__xe_create(reply, PCMK__XE_CIB_CALLDATA);
+
         crm_trace("Attaching reply output");
-        add_message_xml(reply, PCMK__XE_CIB_CALLDATA, call_data);
+        pcmk__xml_copy(wrapper, call_data);
     }
 
     crm_log_xml_explicit(reply, "cib:reply");
@@ -981,6 +983,8 @@ send_peer_reply(xmlNode * msg, xmlNode * result_diff, const char *originator, gb
         const char *digest = NULL;
         int format = 1;
 
+        xmlNode *wrapper = NULL;
+
         CRM_LOG_ASSERT(result_diff != NULL);
         digest = crm_element_value(result_diff, PCMK__XA_DIGEST);
         crm_element_value_int(result_diff, PCMK_XA_FORMAT, &format);
@@ -1002,7 +1006,9 @@ send_peer_reply(xmlNode * msg, xmlNode * result_diff, const char *originator, gb
             CRM_ASSERT(digest != NULL);
         }
 
-        add_message_xml(msg, PCMK__XA_CIB_UPDATE_DIFF, result_diff);
+        wrapper = pcmk__xe_create(msg, PCMK__XA_CIB_UPDATE_DIFF);
+        pcmk__xml_copy(wrapper, result_diff);
+
         crm_log_xml_explicit(msg, "copy");
         return send_cluster_message(NULL, crm_msg_cib, msg, TRUE);
 

daemons/based/based_messages.c
@@ -142,6 +142,8 @@ cib_process_ping(const char *op, int options, const char *section, xmlNode * req
     const char *seq = crm_element_value(req, PCMK__XA_CIB_PING_ID);
     char *digest = calculate_xml_versioned_digest(the_cib, FALSE, TRUE, CRM_FEATURE_SET);
 
+    xmlNode *wrapper = NULL;
+
     crm_trace("Processing \"%s\" event %s from %s", op, seq, host);
     *answer = pcmk__xe_create(NULL, PCMK__XE_PING_RESPONSE);
 
@@ -149,21 +151,25 @@ cib_process_ping(const char *op, int options, const char *section, xmlNode * req
     crm_xml_add(*answer, PCMK__XA_DIGEST, digest);
     crm_xml_add(*answer, PCMK__XA_CIB_PING_ID, seq);
 
-    pcmk__if_tracing(
-        {
-            // Append additional detail so the receiver can log the differences
-            add_message_xml(*answer, PCMK__XE_CIB_CALLDATA, the_cib);
-        },
-        if (the_cib != NULL) {
-            // Always include at least the version details
-            xmlNode *shallow = pcmk__xe_create(NULL,
-                                               (const char *) the_cib->name);
+    wrapper = pcmk__xe_create(*answer, PCMK__XE_CIB_CALLDATA);
 
-            copy_in_properties(shallow, the_cib);
-            add_message_xml(*answer, PCMK__XE_CIB_CALLDATA, shallow);
-            free_xml(shallow);
-        }
-    );
+    if (the_cib != NULL) {
+        pcmk__if_tracing(
+            {
+                /* Append additional detail so the receiver can log the
+                 * differences
+                 */
+                pcmk__xml_copy(wrapper, the_cib);
+            },
+            {
+                // Always include at least the version details
+                const char *name = (const char *) the_cib->name;
+                xmlNode *shallow = pcmk__xe_create(wrapper, name);
+
+                copy_in_properties(shallow, the_cib);
+            }
+        );
+    }
 
     crm_info("Reporting our current digest to %s: %s for %s.%s.%s",
              host, digest,
@@ -420,6 +426,7 @@ sync_our_cib(xmlNode * request, gboolean all)
     const char *op = crm_element_value(request, PCMK__XA_CIB_OP);
     crm_node_t *peer = NULL;
     xmlNode *replace_request = NULL;
+    xmlNode *wrapper = NULL;
 
     CRM_CHECK(the_cib != NULL, return -EINVAL);
     CRM_CHECK(all || (host != NULL), return -EINVAL);
@@ -446,7 +453,8 @@ sync_our_cib(xmlNode * request, gboolean all)
     digest = calculate_xml_versioned_digest(the_cib, FALSE, TRUE, CRM_FEATURE_SET);
     crm_xml_add(replace_request, PCMK__XA_DIGEST, digest);
 
-    add_message_xml(replace_request, PCMK__XE_CIB_CALLDATA, the_cib);
+    wrapper = pcmk__xe_create(replace_request, PCMK__XE_CIB_CALLDATA);
+    pcmk__xml_copy(wrapper, the_cib);
 
     if (!all) {
         peer = pcmk__get_node(0, host, NULL, pcmk__node_search_cluster);

daemons/based/based_notify.c
@@ -128,13 +128,12 @@ cib_notify_send(const xmlNode *xml)
 static void
 attach_cib_generation(xmlNode *msg)
 {
-    xmlNode *generation = pcmk__xe_create(NULL, PCMK__XE_GENERATION_TUPLE);
+    xmlNode *wrapper = pcmk__xe_create(msg, PCMK__XE_CIB_GENERATION);
+    xmlNode *generation = pcmk__xe_create(wrapper, PCMK__XE_GENERATION_TUPLE);
 
     if (the_cib != NULL) {
         copy_in_properties(generation, the_cib);
     }
-    add_message_xml(msg, PCMK__XE_CIB_GENERATION, generation);
-    free_xml(generation);
 }
 
 void
@@ -153,6 +152,7 @@ cib_diff_notify(const char *op, int result, const char *call_id,
     uint8_t log_level = LOG_TRACE;
 
     xmlNode *update_msg = NULL;
+    xmlNode *wrapper = NULL;
     const char *type = NULL;
 
     if (diff == NULL) {
@@ -219,10 +219,12 @@ cib_diff_notify(const char *op, int result, const char *call_id,
 
     // @COMPAT Unused internally, drop at 3.0.0
     if (update != NULL) {
-        add_message_xml(update_msg, PCMK__XE_CIB_UPDATE, update);
+        wrapper = pcmk__xe_create(update_msg, PCMK__XE_CIB_UPDATE);
+        pcmk__xml_copy(wrapper, update);
     }
 
-    add_message_xml(update_msg, PCMK__XA_CIB_UPDATE_RESULT, diff);
+    wrapper = pcmk__xe_create(update_msg, PCMK__XA_CIB_UPDATE_RESULT);
+    pcmk__xml_copy(wrapper, diff);
 
     crm_log_xml_trace(update_msg, "diff-notify");
     cib_notify_send(update_msg);

daemons/fenced/fenced_commands.c
@@ -2509,8 +2509,10 @@ stonith_query_capable_device_cb(GList * devices, void *user_data)
     }
 
     if (list != NULL) {
+        xmlNode *wrapper = pcmk__xe_create(query->reply, PCMK__XE_ST_CALLDATA);
+
         crm_log_xml_trace(list, "Add query results");
-        add_message_xml(query->reply, PCMK__XE_ST_CALLDATA, list);
+        xmlAddChild(wrapper, list);
     }
 
     stonith_send_reply(query->reply, query->call_options, query->remote_peer,
@@ -2523,7 +2525,6 @@ done:
     free(query->target);
     free(query->action);
     free(query);
-    free_xml(list);
     g_list_free_full(devices, free);
 }
 
@@ -3005,7 +3006,9 @@ fenced_construct_reply(const xmlNode *request, xmlNode *data,
             crm_xml_add(reply, name, value);
         }
         if (data != NULL) {
-            add_message_xml(reply, PCMK__XE_ST_CALLDATA, data);
+            xmlNode *wrapper = pcmk__xe_create(reply, PCMK__XE_ST_CALLDATA);
+
+            pcmk__xml_copy(wrapper, data);
         }
     }
     return reply;

daemons/fenced/fenced_history.c
@@ -42,21 +42,21 @@ stonith_send_broadcast_history(xmlNode *history,
                                const char *target)
 {
     xmlNode *bcast = pcmk__xe_create(NULL, PCMK__XE_STONITH_COMMAND);
-    xmlNode *data = pcmk__xe_create(NULL, __func__);
+    xmlNode *wrapper = pcmk__xe_create(bcast, PCMK__XE_ST_CALLDATA);
+    xmlNode *call_data = pcmk__xe_create(wrapper, __func__);
 
-    if (target) {
-        crm_xml_add(data, PCMK__XA_ST_TARGET, target);
-    }
     crm_xml_add(bcast, PCMK__XA_T, PCMK__VALUE_STONITH_NG);
     crm_xml_add(bcast, PCMK__XA_SUBT, PCMK__VALUE_BROADCAST);
     crm_xml_add(bcast, PCMK__XA_ST_OP, STONITH_OP_FENCE_HISTORY);
     crm_xml_add_int(bcast, PCMK__XA_ST_CALLOPT, callopts);
 
-    pcmk__xml_copy(data, history);
-    add_message_xml(bcast, PCMK__XE_ST_CALLDATA, data);
+    pcmk__xml_copy(call_data, history);
+    if (target != NULL) {
+        crm_xml_add(call_data, PCMK__XA_ST_TARGET, target);
+    }
+
     send_cluster_message(NULL, crm_msg_stonith_ng, bcast, FALSE);
 
-    free_xml(data);
     free_xml(bcast);
 }
 

daemons/fenced/fenced_remote.c
@@ -403,7 +403,8 @@ fenced_broadcast_op_result(const remote_fencing_op_t *op, bool op_merged)
 {
     static int count = 0;
     xmlNode *bcast = pcmk__xe_create(NULL, PCMK__XE_ST_REPLY);
-    xmlNode *notify_data = fencing_result2xml(op);
+    xmlNode *wrapper = NULL;
+    xmlNode *notify_data = NULL;
 
     count++;
     crm_trace("Broadcasting result to peers");
@@ -416,11 +417,12 @@ fenced_broadcast_op_result(const remote_fencing_op_t *op, bool op_merged)
         pcmk__xe_set_bool_attr(bcast, PCMK__XA_ST_OP_MERGED, true);
     }
 
+    wrapper = pcmk__xe_create(bcast, PCMK__XE_ST_CALLDATA);
+    notify_data = fencing_result2xml(op);
     stonith__xe_set_result(notify_data, &op->result);
+    xmlAddChild(wrapper, notify_data);
 
-    add_message_xml(bcast, PCMK__XE_ST_CALLDATA, notify_data);
     send_cluster_message(NULL, crm_msg_stonith_ng, bcast, FALSE);
-    free_xml(notify_data);
     free_xml(bcast);
 
     return;

daemons/fenced/pacemaker-fenced.c
@@ -359,7 +359,9 @@ fenced_send_notification(const char *type, const pcmk__action_result_t *result,
     stonith__xe_set_result(update_msg, result);
 
     if (data != NULL) {
-        add_message_xml(update_msg, PCMK__XE_ST_CALLDATA, data);
+        xmlNode *wrapper = pcmk__xe_create(update_msg, PCMK__XE_ST_CALLDATA);
+
+        pcmk__xml_copy(wrapper, data);
     }
 
     crm_trace("Notifying clients");

commit 1b524d0ae7a341ff7952683e62c0df0d08a213f5

Fix: executor: avoid pacemaker-remoted crash
7ebc31792 introduced a memory error in pacemaker-remoted by using xmlAddChild()
instead of pcmk__xml_copy(), though I'm not sure why. It possibly just needed
the pcmk__xml_mark_created() that pcmk__xml_copy() does, but to be safe, go
back to pcmk__xml_copy().

daemons/execd/remoted_proxy.c
@@ -232,7 +232,6 @@ ipc_proxy_dispatch(qb_ipcs_connection_t * c, void *data, size_t size)
     xmlNode *wrapper = NULL;
     xmlNode *request = NULL;
     xmlNode *msg = NULL;
-    xmlDoc *old_doc = NULL;
 
     if (!ipc_proxy) {
         qb_ipcs_disconnect(client->ipcs);
@@ -279,12 +278,11 @@ ipc_proxy_dispatch(qb_ipcs_connection_t * c, void *data, size_t size)
 
     wrapper = pcmk__xe_create(msg, PCMK__XE_LRMD_IPC_MSG);
 
-    old_doc = request->doc;
-    xmlAddChild(wrapper, request);
-    xmlFreeDoc(old_doc);
+    pcmk__xml_copy(wrapper, request);
 
     lrmd_server_send_notify(ipc_proxy, msg);
 
+    free_xml(request);
     free_xml(msg);
     return 0;
 }