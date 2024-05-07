fbab6a30bc71712cbaa7ab4812c64fa13c927385
Ken Gaillot <kgaillot@redhat.com>
2024-05-07 12:43:22 -0500

    API: libpe_status,libpe_rules: drop pe_flag_have_remote_nodes constant
    This breaks API backward compatibility

--- include/crm/pengine/pe_types_compat.h
+++ include/crm/pengine/pe_types_compat.h
@@ -169,9 +169,6 @@ extern "C" {
 //! \deprecated Do not use
 #define pe_flag_have_status             pcmk_sched_have_status
 
-//! \deprecated Do not use
-#define pe_flag_have_remote_nodes       pcmk_sched_have_remote_nodes
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use pcmk_node_t instead
 typedef struct pe_node_s node_t;
