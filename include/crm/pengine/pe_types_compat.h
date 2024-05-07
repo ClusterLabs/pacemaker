5942604138dbb95c7309366666dfce57efc8e0be
Ken Gaillot <kgaillot@redhat.com>
2024-05-07 12:35:41 -0500

    API: libpe_status,libpe_rules: drop pe_tag_t type
    This breaks API backward compatibility

--- include/crm/pengine/pe_types_compat.h
+++ include/crm/pengine/pe_types_compat.h
@@ -240,9 +240,6 @@ typedef struct pe_resource_s resource_t;
 //! \deprecated Do not use
 typedef struct pe_tag_s tag_t;
 
-//! \deprecated Do not use
-typedef struct pe_tag_s pe_tag_t;
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use pcmk_scheduler_t instead
 typedef struct pe_working_set_s pe_working_set_t;
