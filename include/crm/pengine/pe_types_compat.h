db54c3c41875784e7736eb47c959aea4d89f769a
Ken Gaillot <kgaillot@redhat.com>
2024-05-07 12:42:21 -0500

    API: libpe_status,libpe_rules: drop pe_flag_no_counts constant
    This breaks API backward compatibility

--- include/crm/pengine/pe_types_compat.h
+++ include/crm/pengine/pe_types_compat.h
@@ -181,9 +181,6 @@ extern "C" {
 //! \deprecated Do not use
 #define pe_flag_stdout                  (1ULL << 22)
 
-//! \deprecated Do not use
-#define pe_flag_no_counts               pcmk_sched_no_counts
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use pcmk_node_t instead
 typedef struct pe_node_s node_t;
