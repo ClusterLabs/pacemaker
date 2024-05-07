cf0037869b9fb76c5226b931d648c9819f9d7707
Ken Gaillot <kgaillot@redhat.com>
2024-05-07 12:52:22 -0500

    API: libpe_status,libpe_rules: drop pe_rsc_allow_remote_remotes constant
    This breaks API backward compatibility

--- include/crm/pengine/pe_types_compat.h
+++ include/crm/pengine/pe_types_compat.h
@@ -68,9 +68,6 @@ extern "C" {
 //! \deprecated Do not use
 #define pe_rsc_reload                   pcmk_rsc_reload
 
-//! \deprecated Do not use
-#define pe_rsc_allow_remote_remotes     pcmk_rsc_remote_nesting_allowed
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Do not use
 #define pe_flag_have_quorum             pcmk_sched_quorate
