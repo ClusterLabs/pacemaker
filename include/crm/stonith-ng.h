7b39db6f6205a84fcc50f64182d17a6823897407
Ken Gaillot <kgaillot@redhat.com>
2024-05-14 10:26:28 -0500

    Refactor: libstonithd: drop stonith-ng_compat.h
    We can't use a separate compatibility header for the reason noted at the top of
    stonith-ng.h

--- include/crm/stonith-ng.h
+++ include/crm/stonith-ng.h
@@ -692,6 +692,18 @@ const char *stonith_action_str(const char *action);
  * we have to duplicate these declarations where they're implemented.
  */
 
+//! \deprecated Do not use
+#define T_STONITH_NOTIFY_DISCONNECT "st_notify_disconnect"
+
+//! \deprecated Do not use
+#define T_STONITH_NOTIFY_FENCE "st_notify_fence"
+
+//! \deprecated Do not use
+#define T_STONITH_NOTIFY_HISTORY "st_notify_history"
+
+//! \deprecated Do not use
+#define T_STONITH_NOTIFY_HISTORY_SYNCED "st_notify_history_synced"
+
 //! \deprecated Use stonith_get_namespace() instead
 const char *get_stonith_provider(const char *agent, const char *provider);
 
