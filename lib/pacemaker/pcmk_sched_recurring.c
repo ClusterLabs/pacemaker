ClusterLabs/pacemaker

commit 9f069800dabf751e5f7ea0a20ea1eb14e786491e

Refactor: libcrmcommon: New pcmk__str_copy()
Replace pcmk__str_update() with this when the assignee is guaranteed to
be NULL initially, and when the assignee is only a dummy variable for
returning a value.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/pacemaker/pcmk_sched_recurring.c
@@ -656,8 +656,8 @@ pcmk__new_cancel_action(pcmk_resource_t *rsc, const char *task,
     cancel_op = custom_action(rsc, key, PCMK_ACTION_CANCEL, node, FALSE,
                               rsc->cluster);
 
-    pcmk__str_update(&cancel_op->task, PCMK_ACTION_CANCEL);
-    pcmk__str_update(&cancel_op->cancel_task, task);
+    cancel_op->task = pcmk__str_copy(PCMK_ACTION_CANCEL);
+    cancel_op->cancel_task = pcmk__str_copy(task);
 
     interval_ms_s = crm_strdup_printf("%u", interval_ms);
     pcmk__insert_meta(cancel_op, PCMK_XA_OPERATION, task);

daemons/attrd/attrd_attributes.c
@@ -49,17 +49,15 @@ attrd_create_attribute(xmlNode *xml)
 
     a = pcmk__assert_alloc(1, sizeof(attribute_t));
 
+    a->id = pcmk__str_copy(name);
+    a->set_type = pcmk__str_copy(set_type);
+    a->set_id = crm_element_value_copy(xml, PCMK__XA_ATTR_SET);
+    a->user = crm_element_value_copy(xml, PCMK__XA_ATTR_USER);
+    a->values = pcmk__strikey_table(NULL, attrd_free_attribute_value);
+
     if (is_private) {
         attrd_set_attr_flags(a, attrd_attr_is_private);
     }
-
-    pcmk__str_update(&a->id, name);
-    pcmk__str_update(&a->set_type, set_type);
-
-    a->set_id = crm_element_value_copy(xml, PCMK__XA_ATTR_SET);
-    a->values = pcmk__strikey_table(NULL, attrd_free_attribute_value);
-
-    a->user = crm_element_value_copy(xml, PCMK__XA_ATTR_USER);
 
     if (dampen_s != NULL) {
         dampen = crm_get_msec(dampen_s);
@@ -245,7 +243,7 @@ attrd_set_id(const attribute_t *attr, const char *node_state_id)
          * attribute, which would break backward compatibility, pose design
          * challenges, and potentially cause problems in rolling upgrades.
          */
-        pcmk__str_update(&set_id, attr->set_id);
+        set_id = pcmk__str_copy(attr->set_id);
     }
     crm_xml_sanitize_id(set_id);
     return set_id;

daemons/attrd/attrd_cib.c
@@ -469,8 +469,8 @@ set_alert_attribute_value(GHashTable *t, attribute_value_t *v)
     attribute_value_t *a_v = pcmk__assert_alloc(1, sizeof(attribute_value_t));
 
     a_v->nodeid = v->nodeid;
-    a_v->nodename = strdup(v->nodename);
-    pcmk__str_update(&a_v->current, v->current);
+    a_v->nodename = pcmk__str_copy(v->nodename);
+    a_v->current = pcmk__str_copy(v->current);
 
     g_hash_table_replace(t, a_v->nodename, a_v);
 }
@@ -614,7 +614,7 @@ write_attribute(attribute_t *a, bool ignore_delay)
                  a->id, pcmk__s(a->set_id, "unspecified"));
     }
     if (cib_updates > 0) {
-        char *id = NULL;
+        char *id = pcmk__str_copy(a->id);
 
         // Commit transaction
         a->update = the_cib->cmds->end_transaction(the_cib, true, cib_none);
@@ -623,7 +623,6 @@ write_attribute(attribute_t *a, bool ignore_delay)
                  a->update, cib_updates, pcmk__plural_s(cib_updates),
                  a->id, pcmk__s(a->set_id, "unspecified"));
 
-        pcmk__str_update(&id, a->id);
         if (the_cib->cmds->register_callback_full(the_cib, a->update,
                                                   CIB_OP_TIMEOUT_S, FALSE, id,
                                                   "attrd_cib_callback",

daemons/attrd/attrd_corosync.c
@@ -241,7 +241,7 @@ update_attr_on_host(attribute_t *a, const crm_node_t *peer, const xmlNode *xml,
     if (v == NULL) {
         v = pcmk__assert_alloc(1, sizeof(attribute_value_t));
 
-        pcmk__str_update(&v->nodename, host);
+        v->nodename = pcmk__str_copy(host);
         g_hash_table_replace(a->values, v->nodename, v);
     }
 

daemons/attrd/attrd_sync.c
@@ -147,8 +147,6 @@ attrd_add_client_to_waitlist(pcmk__request_t *request)
 
     wl = pcmk__assert_alloc(1, sizeof(struct waitlist_node));
 
-    pcmk__str_update(&(wl->client_id), request->ipc_client->id);
-
     if (pcmk__str_eq(sync_point, PCMK__VALUE_LOCAL, pcmk__str_none)) {
         wl->sync_point = attrd_sync_point_local;
     } else if (pcmk__str_eq(sync_point, PCMK__VALUE_CLUSTER, pcmk__str_none)) {
@@ -158,6 +156,7 @@ attrd_add_client_to_waitlist(pcmk__request_t *request)
         return;
     }
 
+    wl->client_id = pcmk__str_copy(request->ipc_client->id);
     wl->ipc_id = request->ipc_id;
     wl->flags = request->flags;
 
@@ -498,10 +497,8 @@ attrd_expect_confirmations(pcmk__request_t *request, attrd_confirmation_action_f
     g_hash_table_iter_init(&iter, peer_protocol_vers);
     while (g_hash_table_iter_next(&iter, &host, &ver)) {
         if (ATTRD_SUPPORTS_CONFIRMATION(GPOINTER_TO_INT(ver))) {
-            char *s = NULL;
-
-            pcmk__str_update(&s, (char *) host);
-            respondents = g_list_prepend(respondents, s);
+            respondents = g_list_prepend(respondents,
+                                         pcmk__str_copy((char *) host));
         }
     }
 
@@ -510,9 +507,7 @@ attrd_expect_confirmations(pcmk__request_t *request, attrd_confirmation_action_f
     action->respondents = respondents;
     action->fn = fn;
     action->xml = pcmk__xml_copy(NULL, request->xml);
-
-    pcmk__str_update(&(action->client_id), request->ipc_client->id);
-
+    action->client_id = pcmk__str_copy(request->ipc_client->id);
     action->ipc_id = request->ipc_id;
     action->flags = request->flags;
 

daemons/attrd/attrd_utils.c
@@ -288,11 +288,9 @@ attrd_update_minimum_protocol_ver(const char *host, const char *value)
     pcmk__scan_min_int(value, &ver, 0);
 
     if (ver > 0) {
-        char *host_name = NULL;
-
         /* Record the peer attrd's protocol version. */
-        pcmk__str_update(&host_name, host);
-        g_hash_table_insert(peer_protocol_vers, host_name, GINT_TO_POINTER(ver));
+        g_hash_table_insert(peer_protocol_vers, pcmk__str_copy(host),
+                            GINT_TO_POINTER(ver));
 
         /* If the protocol version is a new minimum, record it as such. */
         if (minimum_protocol_version == -1 || ver < minimum_protocol_version) {

daemons/based/based_transaction.c
@@ -28,20 +28,15 @@
 char *
 based_transaction_source_str(const pcmk__client_t *client, const char *origin)
 {
-    char *source = NULL;
-
     if (client != NULL) {
-        source = crm_strdup_printf("client %s (%s)%s%s",
-                                   pcmk__client_name(client),
-                                   pcmk__s(client->id, "unidentified"),
-                                   ((origin != NULL)? " on " : ""),
-                                   pcmk__s(origin, ""));
-
+        return crm_strdup_printf("client %s (%s)%s%s",
+                                 pcmk__client_name(client),
+                                 pcmk__s(client->id, "unidentified"),
+                                 ((origin != NULL)? " on " : ""),
+                                 pcmk__s(origin, ""));
     } else {
-        pcmk__str_update(&source, pcmk__s(origin, "unknown source"));
+        return pcmk__str_copy(pcmk__s(origin, "unknown source"));
     }
-
-    return source;
 }
 
 /*!

daemons/controld/controld_execd.c
@@ -181,7 +181,7 @@ update_history_cache(lrm_state_t * lrm_state, lrmd_rsc_info_t * rsc, lrmd_event_
         entry->rsc.id = entry->id;
         entry->rsc.type = strdup(rsc->type);
         entry->rsc.standard = strdup(rsc->standard);
-        pcmk__str_update(&entry->rsc.provider, rsc->provider);
+        entry->rsc.provider = pcmk__str_copy(rsc->provider);
 
     } else if (entry == NULL) {
         crm_info("Resource %s no longer exists, not updating cache", op->rsc_id);
@@ -2031,7 +2031,7 @@ do_lrm_rsc_op(lrm_state_t *lrm_state, lrmd_rsc_info_t *rsc, xmlNode *msg,
         pending->op_key = strdup(op_id);
         pending->rsc_id = strdup(rsc->id);
         pending->start_time = time(NULL);
-        pcmk__str_update(&pending->user_data, op->user_data);
+        pending->user_data = pcmk__str_copy(op->user_data);
         if (crm_element_value_epoch(msg, PCMK_OPT_SHUTDOWN_LOCK,
                                     &(pending->lock_time)) != pcmk_ok) {
             pending->lock_time = 0;

commit bfd6a8cacee5c568e8be0827977d1611dd556a84

Low: scheduler: avoid memory leaks when updating action names
introduced in 9f06980 (never in a release)

lib/pacemaker/pcmk_sched_recurring.c
@@ -652,14 +652,16 @@ pcmk__new_cancel_action(pcmk_resource_t *rsc, const char *task,
 
     CRM_ASSERT((rsc != NULL) && (task != NULL) && (node != NULL));
 
-    // @TODO dangerous if possible to schedule another action with this key
     key = pcmk__op_key(rsc->id, task, interval_ms);
 
+    /* This finds an existing action by key, so custom_action() does not change
+     * cancel_op->task.
+     */
     cancel_op = custom_action(rsc, key, PCMK_ACTION_CANCEL, node, FALSE,
                               rsc->cluster);
 
-    cancel_op->task = pcmk__str_copy(PCMK_ACTION_CANCEL);
-    cancel_op->cancel_task = pcmk__str_copy(task);
+    pcmk__str_update(&(cancel_op->task), PCMK_ACTION_CANCEL);
+    pcmk__str_update(&(cancel_op->cancel_task), task);
 
     interval_ms_s = crm_strdup_printf("%u", interval_ms);
     pcmk__insert_meta(cancel_op, PCMK_XA_OPERATION, task);