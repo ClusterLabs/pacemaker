ae9eab80be6dd0c41bdaffb0f2d3485b5935ddb6
Ken Gaillot <kgaillot@redhat.com>
2024-05-15 15:55:16 -0500

    Refactor: libcrmcommon: move definitions to correct header
    No code changes

--- include/crm/common/resources_internal.h
+++ include/crm/common/resources_internal.h
@@ -10,11 +10,38 @@
 #ifndef PCMK__CRM_COMMON_RESOURCES_INTERNAL__H
 #define PCMK__CRM_COMMON_RESOURCES_INTERNAL__H
 
-#include <crm/common/resources.h>   // enum rsc_recovery_type
+#include <crm/common/resources.h>       // enum rsc_recovery_type
+#include <crm/common/scheduler_types.h> // pcmk_node_t, pcmk_resource_t
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/*!
+ * \internal
+ * \brief Set resource flags
+ *
+ * \param[in,out] resource      Resource to set flags for
+ * \param[in]     flags_to_set  Group of enum pcmk_rsc_flags to set
+ */
+#define pcmk__set_rsc_flags(resource, flags_to_set) do {                    \
+        (resource)->flags = pcmk__set_flags_as(__func__, __LINE__,          \
+            LOG_TRACE, "Resource", (resource)->id, (resource)->flags,       \
+            (flags_to_set), #flags_to_set);                                 \
+    } while (0)
+
+/*!
+ * \internal
+ * \brief Clear resource flags
+ *
+ * \param[in,out] resource        Resource to clear flags for
+ * \param[in]     flags_to_clear  Group of enum pcmk_rsc_flags to clear
+ */
+#define pcmk__clear_rsc_flags(resource, flags_to_clear) do {                \
+        (resource)->flags = pcmk__clear_flags_as(__func__, __LINE__,        \
+            LOG_TRACE, "Resource", (resource)->id, (resource)->flags,       \
+            (flags_to_clear), #flags_to_clear);                             \
+    } while (0)
 
 // Implementation of pcmk__resource_private_t
 struct pcmk__resource_private {
@@ -22,6 +49,23 @@ struct pcmk__resource_private {
 };
 
 const char *pcmk__multiply_active_text(enum rsc_recovery_type recovery);
+
+/*!
+ * \internal
+ * \brief Get node where resource is currently active (if any)
+ *
+ * \param[in] rsc  Resource to check
+ *
+ * \return Node that \p rsc is active on, if any, otherwise NULL
+ */
+static inline pcmk_node_t *
+pcmk__current_node(const pcmk_resource_t *rsc)
+{
+    if (rsc == NULL) {
+        return NULL;
+    }
+    return rsc->private->fns->active_node(rsc, NULL, NULL);
+}
 
 #ifdef __cplusplus
 }
--- include/crm/common/roles_internal.h
+++ include/crm/common/roles_internal.h
@@ -10,9 +10,6 @@
 #ifndef PCMK__CRM_COMMON_ROLES_INTERNAL__H
 #define PCMK__CRM_COMMON_ROLES_INTERNAL__H
 
-#include <crm/common/scheduler_types.h>     // pcmk_node_t, pcmk_resource_t
-#include <crm/common/resources_internal.h>  // struct pcmk__resource_private
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -21,49 +18,6 @@ extern "C" {
 #define PCMK__ROLE_UNKNOWN      "Unknown"
 #define PCMK__ROLE_UNPROMOTED_LEGACY    "Slave"
 #define PCMK__ROLE_PROMOTED_LEGACY      "Master"
-
-/*!
- * \internal
- * \brief Set resource flags
- *
- * \param[in,out] resource      Resource to set flags for
- * \param[in]     flags_to_set  Group of enum pcmk_rsc_flags to set
- */
-#define pcmk__set_rsc_flags(resource, flags_to_set) do {                    \
-        (resource)->flags = pcmk__set_flags_as(__func__, __LINE__,          \
-            LOG_TRACE, "Resource", (resource)->id, (resource)->flags,       \
-            (flags_to_set), #flags_to_set);                                 \
-    } while (0)
-
-/*!
- * \internal
- * \brief Clear resource flags
- *
- * \param[in,out] resource        Resource to clear flags for
- * \param[in]     flags_to_clear  Group of enum pcmk_rsc_flags to clear
- */
-#define pcmk__clear_rsc_flags(resource, flags_to_clear) do {                \
-        (resource)->flags = pcmk__clear_flags_as(__func__, __LINE__,        \
-            LOG_TRACE, "Resource", (resource)->id, (resource)->flags,       \
-            (flags_to_clear), #flags_to_clear);                             \
-    } while (0)
-
-/*!
- * \internal
- * \brief Get node where resource is currently active (if any)
- *
- * \param[in] rsc  Resource to check
- *
- * \return Node that \p rsc is active on, if any, otherwise NULL
- */
-static inline pcmk_node_t *
-pcmk__current_node(const pcmk_resource_t *rsc)
-{
-    if (rsc == NULL) {
-        return NULL;
-    }
-    return rsc->private->fns->active_node(rsc, NULL, NULL);
-}
 
 #ifdef __cplusplus
 }
