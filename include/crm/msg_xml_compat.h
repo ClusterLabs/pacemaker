f1866b3a6401b2d526c8686b93f608b4ee01d609
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:14:54 -0700

    API: libcrmcommon: Drop XML_TAG_PARAMS constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -644,9 +644,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_RESOURCE_DISCOVERY instead
 #define XML_LOCATION_ATTR_DISCOVERY PCMK_XA_RESOURCE_DISCOVERY
 
-//! \deprecated Use \c PCMK_XE_PARAMETERS instead
-#define XML_TAG_PARAMS PCMK_XE_PARAMETERS
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
