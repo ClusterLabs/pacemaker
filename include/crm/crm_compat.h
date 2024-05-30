80d6261c5c2f3a7f7d0c8023b64013103a7d209c
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:32:38 -0700

    API: libcrmcommon: Drop CRMD_ACTION_NOTIFY constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/crm_compat.h
+++ include/crm/crm_compat.h
@@ -101,9 +101,6 @@ extern "C" {
 //! \deprecated Use PCMK_ACTION_NOTIFIED instead
 #define CRMD_ACTION_NOTIFIED PCMK_ACTION_NOTIFIED
 
-//! \deprecated Use PCMK_ACTION_NOTIFY instead
-#define CRMD_ACTION_NOTIFY PCMK_ACTION_NOTIFY
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use GList * instead
 typedef GList *GListPtr;
