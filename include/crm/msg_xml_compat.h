8b7d0e8a5d71aa4f692c1536742ea9a4cccf3f59
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:13:15 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_PROMOTED_MAX constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -35,9 +35,6 @@ extern "C" {
 //! \deprecated Use PCMK_META_CLONE_NODE_MAX instead
 #define XML_RSC_ATTR_INCARNATION_NODEMAX PCMK_META_CLONE_NODE_MAX
 
-//! \deprecated Use PCMK_META_PROMOTED_MAX instead
-#define XML_RSC_ATTR_PROMOTED_MAX PCMK_META_PROMOTED_MAX
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
