e58ab14dd9f2b20f6ded29633252b5b0de5949ec
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:29:48 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_INTERVAL constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -503,9 +503,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_TIMESTAMP_FORMAT instead
 #define XML_ALERT_ATTR_TSTAMP_FORMAT PCMK_META_TIMESTAMP_FORMAT
 
-//! \deprecated Use \c PCMK_META_INTERVAL instead
-#define XML_LRM_ATTR_INTERVAL PCMK_META_INTERVAL
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
