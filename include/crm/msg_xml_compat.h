0b9a0635db292da543d94fc858c5d57c64cb6e53
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:23:10 -0700

    API: libcrmcommon: Drop XML_OP_ATTR_DIGESTS_ALL constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -530,9 +530,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_RECORD_PENDING instead
 #define XML_OP_ATTR_PENDING PCMK_META_RECORD_PENDING
 
-//! \deprecated Do not use
-#define XML_OP_ATTR_DIGESTS_ALL "digests-all"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
