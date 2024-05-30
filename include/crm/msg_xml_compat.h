9331ea96194adac3b2d74a5dd6f21e43dd9472e9
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:59:45 -0700

    API: libcrmcommon: Drop XML_ATTR_GENERATION_ADMIN constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -167,9 +167,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_EPOCH instead
 #define XML_ATTR_GENERATION PCMK_XA_EPOCH
 
-//! \deprecated Use \c PCMK_XA_ADMIN_EPOCH instead
-#define XML_ATTR_GENERATION_ADMIN PCMK_XA_ADMIN_EPOCH
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
