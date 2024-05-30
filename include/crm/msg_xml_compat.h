e26c4a1220e7c7d63587896bb207ac9ceaf59014
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:54:46 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_MULTIPLE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -227,9 +227,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_RESOURCE_STICKINESS instead
 #define XML_RSC_ATTR_STICKINESS PCMK_META_RESOURCE_STICKINESS
 
-//! \deprecated Use \c PCMK_META_MULTIPLE_ACTIVE instead
-#define XML_RSC_ATTR_MULTIPLE PCMK_META_MULTIPLE_ACTIVE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
