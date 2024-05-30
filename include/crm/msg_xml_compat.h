4274be16d382a042e5f23288b00386a7e2f48168
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:49:15 -0700

    API: libcrmcommon: Drop XML_OP_ATTR_ALLOW_MIGRATE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -254,9 +254,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_CRITICAL instead
 #define XML_RSC_ATTR_CRITICAL PCMK_META_CRITICAL
 
-//! \deprecated Use \c PCMK_META_ALLOW_MIGRATE instead
-#define XML_OP_ATTR_ALLOW_MIGRATE PCMK_META_ALLOW_MIGRATE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
