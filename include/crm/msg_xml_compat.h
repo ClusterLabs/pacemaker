b307c11b557016ebf10e38636549e4de29a366a2
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:10:29 -0700

    API: libcrmcommon: Drop XML_ACL_ATTR_TAG constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -698,9 +698,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_ACL_ATTR_REFv1 "ref"
 
-//! \deprecated Use \c PCMK_XA_OBJECT_TYPE instead
-#define XML_ACL_ATTR_TAG PCMK_XA_OBJECT_TYPE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
