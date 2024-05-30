cca858d6d17d9572ed3f819bacc203b962f48c74
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:58:38 -0700

    API: libcrmcommon: Drop XML_ATTR_TSTAMP constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -176,9 +176,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_CRM_DEBUG_ORIGIN instead
 #define XML_ATTR_ORIGIN PCMK_XA_CRM_DEBUG_ORIGIN
 
-//! \deprecated Use \c PCMK_XA_CRM_TIMESTAMP instead
-#define XML_ATTR_TSTAMP PCMK_XA_CRM_TIMESTAMP
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
