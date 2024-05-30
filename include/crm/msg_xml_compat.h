7b3f77ccbe6640fc87e208475a89c783403eb3ec
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:55:31 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_TARGET_ROLE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -215,9 +215,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_IS_MANAGED instead
 #define XML_RSC_ATTR_MANAGED PCMK_META_IS_MANAGED
 
-//! \deprecated Use \c PCMK_META_TARGET_ROLE instead
-#define XML_RSC_ATTR_TARGET_ROLE PCMK_META_TARGET_ROLE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
