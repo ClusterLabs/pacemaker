da2fd729888976ee75337806b838f8f0bb9aa7a0
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:56:07 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_INCARNATION constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -206,9 +206,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_INTERLEAVE instead
 #define XML_RSC_ATTR_INTERLEAVE PCMK_META_INTERLEAVE
 
-//! \deprecated Do not use
-#define XML_RSC_ATTR_INCARNATION "clone"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
