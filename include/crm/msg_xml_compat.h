2d99ba63653a0b692b5c13065d7c04b9d7f6aa98
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:36:26 -0700

    API: libcrmcommon: Drop F_CRM_JOIN_ID constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -428,9 +428,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_USER "crm_user"
 
-//! \deprecated Do not use
-#define F_CRM_JOIN_ID "join_id"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
