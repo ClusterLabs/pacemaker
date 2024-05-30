6b31638ba6215a79a164d357c5477dc1b62790f5
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:41:24 -0700

    API: libcrmcommon: Drop XML_ATTR_STONITH_DEVICES constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -362,9 +362,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_TARGET_ATTRIBUTE instead
 #define XML_ATTR_STONITH_TARGET_ATTRIBUTE PCMK_XA_TARGET_ATTRIBUTE
 
-//! \deprecated Use \c PCMK_XA_DEVICES instead
-#define XML_ATTR_STONITH_DEVICES PCMK_XA_DEVICES
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
