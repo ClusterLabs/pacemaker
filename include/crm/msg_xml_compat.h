1a00148909d21fc7b81c3a38a95a9ef047cb5713
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:10:09 -0700

    API: libcrmcommon: Drop XML_CIB_TAG_MASTER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -47,9 +47,6 @@ extern "C" {
 //! \deprecated Do not use
 #define PCMK_XE_PROMOTABLE_LEGACY "master"
 
-//! \deprecated Do not use
-#define XML_CIB_TAG_MASTER PCMK_XE_PROMOTABLE_LEGACY
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
