d29373cd1a6ac8665877620f5d12fe69ccbc9ca2
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:03:09 -0700

    API: libcrmcommon: Drop XML_CONFIG_ATTR_FENCE_REACTION constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -128,9 +128,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_OPT_ELECTION_TIMEOUT instead
 #define XML_CONFIG_ATTR_ELECTION_FAIL PCMK_OPT_ELECTION_TIMEOUT
 
-//! \deprecated Use \c PCMK_OPT_FENCE_REACTION instead
-#define XML_CONFIG_ATTR_FENCE_REACTION PCMK_OPT_FENCE_REACTION
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
