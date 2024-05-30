20282bd9c5a9dd3e5918758b87fe8da8cbc58f47
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:05:20 -0700

    API: libcrmcommon: Drop XML_CIB_ATTR_SOURCE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -98,9 +98,6 @@ extern "C" {
 //! \deprecated Do not use (will be removed in a future release)
 #define XML_CIB_TAG_DOMAINS "domains"
 
-//! \deprecated Do not use (will be removed in a future release)
-#define XML_CIB_ATTR_SOURCE "source"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
