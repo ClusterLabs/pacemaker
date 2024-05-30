2b163a4ce69cdd62cf4445b6372b48f380146a1a
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:35:23 -0700

    API: libcrmcommon: Drop F_CRM_ELECTION_OWNER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -443,9 +443,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_ELECTION_AGE_US "election-age-nano-sec"
 
-//! \deprecated Do not use
-#define F_CRM_ELECTION_OWNER "election-owner"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
