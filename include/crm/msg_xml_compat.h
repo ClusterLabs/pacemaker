88ef87df3994e31e83aaeb95c264e7b8ca82a7bf
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:49:05 -0700

    API: libcrmcommon: Drop XML_BOOLEAN_YES constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -257,9 +257,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_ALLOW_MIGRATE instead
 #define XML_OP_ATTR_ALLOW_MIGRATE PCMK_META_ALLOW_MIGRATE
 
-//! \deprecated Use \c PCMK_VALUE_TRUE instead
-#define XML_BOOLEAN_YES PCMK_VALUE_TRUE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
