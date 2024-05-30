8f2d58b1e2fb87ddb308a6a332397502c874a543
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:10:01 -0700

    API: libcrmcommon: Drop XML_ACL_ATTR_XPATH constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -704,9 +704,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_ACL_ATTR_TAGv1 "tag"
 
-//! \deprecated Use \c PCMK_XA_XPATH instead
-#define XML_ACL_ATTR_XPATH PCMK_XA_XPATH
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
