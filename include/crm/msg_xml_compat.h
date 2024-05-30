7d7622e8896ca7bcd2cf9dc7a195907e0f72ae64
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:45:19 -0700

    API: libcrmcommon: Drop XML_ATTR_UPDATE_ORIG constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -314,9 +314,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_DC_UUID instead
 #define XML_ATTR_DC_UUID PCMK_XA_DC_UUID
 
-//! \deprecated Use \c PCMK_XA_UPDATE_ORIGIN instead
-#define XML_ATTR_UPDATE_ORIG PCMK_XA_UPDATE_ORIGIN
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
