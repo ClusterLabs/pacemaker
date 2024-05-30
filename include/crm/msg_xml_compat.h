24264dfb0410de8b098c6fe7a96f7640f3839f03
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:04:23 -0700

    API: libcrmcommon: Drop XML_RSC_OP_LAST_RUN constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -113,9 +113,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_NODE_JOIN_STATE "join"
 
-//! \deprecated Do not use (will be removed in a future release)
-#define XML_RSC_OP_LAST_RUN "last-run"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
