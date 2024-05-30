129bb0c1c88deab91fa60c8a4834a7abd5ced094
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:17:03 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_MIGRATE_TARGET constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -611,9 +611,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_ATTR_MIGRATE_SOURCE "migrate_source"
 
-//! \deprecated Do not use
-#define XML_LRM_ATTR_MIGRATE_TARGET "migrate_target"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
