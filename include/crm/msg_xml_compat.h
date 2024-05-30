3267ba9ae427ac1dfd00efb0f2a395ddefa27086
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:30:32 -0700

    API: libcrmcommon: Drop XML_ALERT_ATTR_PATH constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -494,9 +494,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_TIMEOUT instead
 #define XML_ALERT_ATTR_TIMEOUT PCMK_META_TIMEOUT
 
-//! \deprecated Use \c PCMK_XA_PATH instead
-#define XML_ALERT_ATTR_PATH PCMK_XA_PATH
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
