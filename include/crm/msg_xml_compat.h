970d277aa9539d14d98b4720d1f5d642f2e79a7e
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:21:54 -0700

    API: libcrmcommon: Drop XML_ORDER_ATTR_FIRST_INSTANCE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -548,9 +548,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_ATTR_TARGET_UUID "on_node_uuid"
 
-//! \deprecated Do not use
-#define XML_ORDER_ATTR_FIRST_INSTANCE "first-instance"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
