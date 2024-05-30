90db7627b07d6b571fe39d4625318c6418436c2a
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:22:31 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_TASK_KEY constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -539,9 +539,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_CIB_ATTR_PRIORITY "priority"
 
-//! \deprecated Do not use
-#define XML_LRM_ATTR_TASK_KEY "operation_key"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
