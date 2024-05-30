170072b17e155e22486b9db76298299730fb4491
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:33:44 -0700

    API: libcrmcommon: Drop CRMD_ACTION_DEMOTE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/crm_compat.h
+++ include/crm/crm_compat.h
@@ -83,9 +83,6 @@ extern "C" {
 //! \deprecated Use PCMK_ACTION_DELETE instead
 #define CRMD_ACTION_DELETE PCMK_ACTION_DELETE
 
-//! \deprecated Use PCMK_ACTION_DEMOTE instead
-#define CRMD_ACTION_DEMOTE PCMK_ACTION_DEMOTE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use GList * instead
 typedef GList *GListPtr;
