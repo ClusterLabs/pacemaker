ee52aa7e103048e356d35817d5914563b6a4a14d
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:57:52 -0700

    API: libcrmcommon: Drop XML_ATTR_ID constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -188,9 +188,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_DESCRIPTION instead
 #define XML_ATTR_DESC PCMK_XA_DESCRIPTION
 
-//! \deprecated Use \c PCMK_XA_ID instead
-#define XML_ATTR_ID PCMK_XA_ID
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
