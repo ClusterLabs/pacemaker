619adf9f947304f476763d33be0e0c06321a40c7
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:17:41 -0700

    API: libcrmcommon: Drop XML_RSC_OP_T_EXEC constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -602,9 +602,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_LAST_RC_CHANGE instead
 #define XML_RSC_OP_LAST_CHANGE PCMK_XA_LAST_RC_CHANGE
 
-//! \deprecated Use \c PCMK_XA_EXEC_TIME instead
-#define XML_RSC_OP_T_EXEC PCMK_XA_EXEC_TIME
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
