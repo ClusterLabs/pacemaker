b5a354fe7e2a25f82ef05eb65ad2d2c350f1cd5e
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 19:14:23 -0700

    API: libcrmcommon: Drop F_CRM_DATA constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -945,9 +945,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XE_POSITION instead
 #define XML_DIFF_POSITION PCMK_XE_POSITION
 
-//! \deprecated Do not use
-#define F_CRM_DATA "crm_xml"
-
 #ifdef __cplusplus
 }
 #endif
