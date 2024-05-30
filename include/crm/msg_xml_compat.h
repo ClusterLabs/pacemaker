3fb9bef6e481495dcab6443e757cfe707db60946
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:19:12 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_OP_DIGEST constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -581,9 +581,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_ATTR_CALLID "call-id"
 
-//! \deprecated Do not use
-#define XML_LRM_ATTR_OP_DIGEST "op-digest"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
