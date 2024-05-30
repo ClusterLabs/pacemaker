39f57978d2bc6982360ae2f9c0131720f476c37c
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:14:43 -0700

    API: libcrmcommon: Drop XML_COLOC_ATTR_SOURCE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -647,9 +647,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XE_PARAMETERS instead
 #define XML_TAG_PARAMS PCMK_XE_PARAMETERS
 
-//! \deprecated Use \c PCMK_XA_RSC instead
-#define XML_COLOC_ATTR_SOURCE PCMK_XA_RSC
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
