1b59391d3b9aa9ce81dad3568d17821bac48f24e
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:04:47 -0700

    API: libcrmcommon: Drop XML_NODE_IS_PEER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -107,9 +107,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_NODE_IN_CLUSTER "in_ccm"
 
-//! \deprecated Do not use
-#define XML_NODE_IS_PEER "crmd"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
