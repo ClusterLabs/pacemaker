15b38b64b07690e76a3c251a081e476d74bea13e
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:11:42 -0700

    API: libcrmcommon: Drop XML_ORDER_ATTR_THEN_ACTION constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -680,9 +680,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_FIRST_ACTION instead
 #define XML_ORDER_ATTR_FIRST_ACTION PCMK_XA_FIRST_ACTION
 
-//! \deprecated Use \c PCMK_XA_THEN_ACTION instead
-#define XML_ORDER_ATTR_THEN_ACTION PCMK_XA_THEN_ACTION
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
