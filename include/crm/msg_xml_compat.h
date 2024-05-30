e8803dcdcfc36f32992c5831c934467ae573449c
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:38:24 -0700

    API: libcrmcommon: Drop XML_BOOLEAN_FALSE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -404,9 +404,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_VALUE_TRUE instead
 #define XML_BOOLEAN_TRUE PCMK_VALUE_TRUE
 
-//! \deprecated Use \c PCMK_VALUE_FALSE instead
-#define XML_BOOLEAN_FALSE PCMK_VALUE_FALSE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
