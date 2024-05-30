e1ea0d4a9621d3fb396755519b524bc6723fe898
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:29:22 -0700

    API: libcrmcommon: Drop XML_CIB_ATTR_REPLACE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -509,9 +509,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_INTERVAL instead
 #define XML_LRM_ATTR_INTERVAL_MS PCMK_META_INTERVAL
 
-//! \deprecated Do not use
-#define XML_CIB_ATTR_REPLACE "replace"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
