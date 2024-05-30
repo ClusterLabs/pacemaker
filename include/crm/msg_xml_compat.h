1dfd31820a2b5ec9c5ee49d2e7514fa1af266622
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:01:54 -0700

    API: libcrmcommon: Drop XML_CONFIG_ATTR_SHUTDOWN_LOCK constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -143,9 +143,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_OPT_SHUTDOWN_ESCALATION instead
 #define XML_CONFIG_ATTR_FORCE_QUIT PCMK_OPT_SHUTDOWN_ESCALATION
 
-//! \deprecated Use \c PCMK_OPT_SHUTDOWN_LOCK instead
-#define XML_CONFIG_ATTR_SHUTDOWN_LOCK PCMK_OPT_SHUTDOWN_LOCK
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
