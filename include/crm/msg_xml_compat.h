db735523b0eb5533148f8b29a9a0df4fb8843bb9
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:58:14 -0700

    API: libcrmcommon: Drop XML_ATTR_VERSION constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -182,9 +182,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_CIB_LAST_WRITTEN instead
 #define XML_CIB_ATTR_WRITTEN PCMK_XA_CIB_LAST_WRITTEN
 
-//! \deprecated Use \c PCMK_XA_VERSION instead
-#define XML_ATTR_VERSION PCMK_XA_VERSION
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
