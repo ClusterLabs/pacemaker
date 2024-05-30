5accba1b078aa0d41a6bb5d62258cf20dc050957
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:36:09 -0700

    API: libcrmcommon: Drop F_CRM_DC_LEAVING constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -431,9 +431,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_JOIN_ID "join_id"
 
-//! \deprecated Do not use
-#define F_CRM_DC_LEAVING "dc-leaving"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
