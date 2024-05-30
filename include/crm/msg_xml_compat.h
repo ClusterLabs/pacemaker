3948c3f26fdebd6aed5c1a6c99334b6c161e9923
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:40:17 -0700

    API: libcrmcommon: Drop F_CRM_MSG_TYPE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -383,9 +383,6 @@ extern "C" {
 #define F_SUBTYPE "subt"
 #endif
 
-//! \deprecated Do not use
-#define F_CRM_MSG_TYPE F_SUBTYPE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
