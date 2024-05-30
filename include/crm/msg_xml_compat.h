e432dcd616d07c1ae8b3d0d0570bc2e0f000313f
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:03:24 -0700

    API: libcrmcommon: Drop XML_CONFIG_ATTR_ELECTION_FAIL constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -125,9 +125,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_OPT_DC_DEADTIME instead
 #define XML_CONFIG_ATTR_DC_DEADTIME PCMK_OPT_DC_DEADTIME
 
-//! \deprecated Use \c PCMK_OPT_ELECTION_TIMEOUT instead
-#define XML_CONFIG_ATTR_ELECTION_FAIL PCMK_OPT_ELECTION_TIMEOUT
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
