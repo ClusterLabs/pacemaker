bc6c9bcd598f066391369b4c94ea2179f30ead06
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:56:33 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_ORDERED constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -200,9 +200,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_RSC_ATTR_RESTART "restart-type"
 
-//! \deprecated Use \c PCMK_META_ORDERED instead
-#define XML_RSC_ATTR_ORDERED PCMK_META_ORDERED
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
