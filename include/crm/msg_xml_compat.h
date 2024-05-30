bab8e78c3d2d9fb3711843dce7fc643ac96d626b
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:07:59 -0700

    API: libcrmcommon: Drop PCMK_XE_PROMOTED_NODE_MAX_LEGACY constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -62,9 +62,6 @@ extern "C" {
 //! \deprecated Do not use
 #define PCMK_XA_PROMOTED_NODE_MAX_LEGACY "master-node-max"
 
-//! \deprecated Do not use
-#define PCMK_XE_PROMOTED_NODE_MAX_LEGACY PCMK_XA_PROMOTED_NODE_MAX_LEGACY
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
