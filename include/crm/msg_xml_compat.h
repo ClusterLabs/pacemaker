ffcce8bf5e517633c1b5d2d0c2265945d482cb63
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:03:55 -0700

    API: libcrmcommon: Drop XML_CONFIG_ATTR_RECHECK constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -119,9 +119,6 @@ extern "C" {
 //! \deprecated Use name member directly
 #define TYPE(x) (((x) == NULL)? NULL : (const char *) ((x)->name))
 
-//! \deprecated Use \c PCMK_OPT_CLUSTER_RECHECK_INTERVAL instead
-#define XML_CONFIG_ATTR_RECHECK PCMK_OPT_CLUSTER_RECHECK_INTERVAL
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
