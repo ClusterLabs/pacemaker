0d4d08a36bffe27b4014b39707866b7b6e3fe76c
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:33:39 -0700

    API: libcrmcommon: Drop XML_PING_ATTR_STATUS constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -458,9 +458,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_THROTTLE_MAX "crm-limit-max"
 
-//! \deprecated Use \c PCMK_XA_RESULT instead
-#define XML_PING_ATTR_STATUS PCMK_XA_RESULT
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
