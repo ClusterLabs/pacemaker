2d60917055c81dfc9812a734f9a8f23900289f17
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:36:46 -0700

    API: libcrmcommon: Drop F_CRM_ORIGIN constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -422,9 +422,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_VERSION instead
 #define F_CRM_VERSION PCMK_XA_VERSION
 
-//! \deprecated Use \c PCMK_XA_ORIGIN instead
-#define F_CRM_ORIGIN PCMK_XA_ORIGIN
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
