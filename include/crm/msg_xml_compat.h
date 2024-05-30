d25d373fe107a1d9a63bf50f6c03091071198d93
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:50:15 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_CLEAR_INTERVAL constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -248,9 +248,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_RSC_ATTR_CLEAR_OP "clear_failure_op"
 
-//! \deprecated Do not use
-#define XML_RSC_ATTR_CLEAR_INTERVAL "clear_failure_interval"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
