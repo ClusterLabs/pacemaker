a0b00fa7ea49c25a7f188937123ed7596beddb97
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:08:23 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_MASTER_MAX constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -56,9 +56,6 @@ extern "C" {
 //! \deprecated Do not use
 #define PCMK_XE_PROMOTED_MAX_LEGACY PCMK_XA_PROMOTED_MAX_LEGACY
 
-//! \deprecated Do not use
-#define XML_RSC_ATTR_MASTER_MAX PCMK_XA_PROMOTED_MAX_LEGACY
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
