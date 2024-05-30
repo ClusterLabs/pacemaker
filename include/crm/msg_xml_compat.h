25a2daf14cc68d69f63d0e6ff0cdd5a82eff2d37
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:55:44 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_MANAGED constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -212,9 +212,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_PROMOTABLE instead
 #define XML_RSC_ATTR_PROMOTABLE PCMK_META_PROMOTABLE
 
-//! \deprecated Use \c PCMK_META_IS_MANAGED instead
-#define XML_RSC_ATTR_MANAGED PCMK_META_IS_MANAGED
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
