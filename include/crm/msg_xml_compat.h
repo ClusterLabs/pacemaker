40d2c4a8f761d352818d465d81fc6b829a960a79
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:55:19 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_UNIQUE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -218,9 +218,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_TARGET_ROLE instead
 #define XML_RSC_ATTR_TARGET_ROLE PCMK_META_TARGET_ROLE
 
-//! \deprecated Use \c PCMK_META_GLOBALLY_UNIQUE instead
-#define XML_RSC_ATTR_UNIQUE PCMK_META_GLOBALLY_UNIQUE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
