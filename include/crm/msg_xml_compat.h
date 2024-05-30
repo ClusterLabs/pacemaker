986be67aa8f31f06f21759c6f2819aa72e122148
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:10:21 -0700

    API: libcrmcommon: Drop PCMK_XE_PROMOTABLE_LEGACY constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -44,9 +44,6 @@ extern "C" {
 //! \deprecated Use PCMK_STONITH_PROVIDES instead
 #define XML_RSC_ATTR_PROVIDES PCMK_STONITH_PROVIDES
 
-//! \deprecated Do not use
-#define PCMK_XE_PROMOTABLE_LEGACY "master"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
