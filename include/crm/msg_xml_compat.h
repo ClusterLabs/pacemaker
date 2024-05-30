2bade49e0a41526938ad8779aafb9735e822e972
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:36:36 -0700

    API: libcrmcommon: Drop F_CRM_USER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -425,9 +425,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_ORIGIN instead
 #define F_CRM_ORIGIN PCMK_XA_ORIGIN
 
-//! \deprecated Do not use
-#define F_CRM_USER "crm_user"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
