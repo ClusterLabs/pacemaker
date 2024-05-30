bc70f89a6401260c4d292e90a62433456116f77e
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:37:27 -0700

    API: libcrmcommon: Drop F_CRM_SYS_FROM constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -416,9 +416,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_SYS_TO "crm_sys_to"
 
-//! \deprecated Do not use
-#define F_CRM_SYS_FROM "crm_sys_from"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
