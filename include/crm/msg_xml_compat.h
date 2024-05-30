8fc30f093cb64b66f0faff20172dd4bb2362e5ca
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:51:11 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_CONTAINER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -233,9 +233,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_REQUIRES instead
 #define XML_RSC_ATTR_REQUIRES PCMK_META_REQUIRES
 
-//! \deprecated Do not use
-#define XML_RSC_ATTR_CONTAINER "container"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
