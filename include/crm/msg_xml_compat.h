9090d6ef9479c4b40c276ec18dde8a383e127c02
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:45:56 -0700

    API: libcrmcommon: Drop XML_AGENT_ATTR_CLASS constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -305,9 +305,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_PROVIDER instead
 #define XML_AGENT_ATTR_PROVIDER PCMK_XA_PROVIDER
 
-//! \deprecated Use \c PCMK_XA_CLASS instead
-#define XML_AGENT_ATTR_CLASS PCMK_XA_CLASS
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
