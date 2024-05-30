e63158ac5fdb1fc788a078043370750c2d49b794
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:11:05 -0700

    API: libcrmcommon: Drop XML_TICKET_ATTR_TICKET constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -689,9 +689,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_KIND instead
 #define XML_ACL_ATTR_KIND PCMK_XA_KIND
 
-//! \deprecated Use \c PCMK_XA_TICKET instead
-#define XML_TICKET_ATTR_TICKET PCMK_XA_TICKET
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
