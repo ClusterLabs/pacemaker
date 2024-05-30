8bc760bb74ec64d7147984ea69f2dcbe7f96099a
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:35:37 -0700

    API: libcrmcommon: Drop F_CRM_ELECTION_AGE_US constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -440,9 +440,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_ELECTION_AGE_S "election-age-sec"
 
-//! \deprecated Do not use
-#define F_CRM_ELECTION_AGE_US "election-age-nano-sec"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
