15edc97faeff326a96e727602736e158082a45ad
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:01:53 -0700

    API: libcrmcommon: Drop XML_LRM_TAG_RESOURCE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -819,9 +819,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_TAG_RESOURCES "lrm_resources"
 
-//! \deprecated Do not use
-#define XML_LRM_TAG_RESOURCE "lrm_resource"
-
 #ifdef __cplusplus
 }
 #endif
