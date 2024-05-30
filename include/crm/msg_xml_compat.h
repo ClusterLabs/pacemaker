4853a13800e4d3ff3ce0b3878017f251b38eb05b
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:59:55 -0700

    API: libcrmcommon: Drop XML_ATTR_GENERATION constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -164,9 +164,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_HAVE_QUORUM instead
 #define XML_ATTR_HAVE_QUORUM PCMK_XA_HAVE_QUORUM
 
-//! \deprecated Use \c PCMK_XA_EPOCH instead
-#define XML_ATTR_GENERATION PCMK_XA_EPOCH
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
