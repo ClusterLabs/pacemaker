48d6f7a37ba5ffe9a14525746f407164edb1a5d9
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:58:26 -0700

    API: libcrmcommon: Drop XML_CIB_ATTR_WRITTEN constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -179,9 +179,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_CRM_TIMESTAMP instead
 #define XML_ATTR_TSTAMP PCMK_XA_CRM_TIMESTAMP
 
-//! \deprecated Use \c PCMK_XA_CIB_LAST_WRITTEN instead
-#define XML_CIB_ATTR_WRITTEN PCMK_XA_CIB_LAST_WRITTEN
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
