93ec73fa73131a3253d006ce9402833adfc1bf36
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:56:44 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_RESTART constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -197,9 +197,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_META_CONTAINER_ATTRIBUTE_TARGET instead
 #define XML_RSC_ATTR_TARGET PCMK_META_CONTAINER_ATTRIBUTE_TARGET
 
-//! \deprecated Do not use
-#define XML_RSC_ATTR_RESTART "restart-type"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
