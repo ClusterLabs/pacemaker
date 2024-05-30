4689c0bcb6249b4703989cfc229b9ce7be01ae42
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:04:35 -0700

    API: libcrmcommon: Drop XML_NODE_JOIN_STATE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -110,9 +110,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_NODE_IS_PEER "crmd"
 
-//! \deprecated Do not use
-#define XML_NODE_JOIN_STATE "join"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
