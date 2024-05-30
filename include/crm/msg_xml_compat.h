c6c672a13772278372d9a91853186257363615a7
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:50:04 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_CRITICAL constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -251,9 +251,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_RSC_ATTR_CLEAR_INTERVAL "clear_failure_interval"
 
-//! \deprecated Use \c PCMK_META_CRITICAL instead
-#define XML_RSC_ATTR_CRITICAL PCMK_META_CRITICAL
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
