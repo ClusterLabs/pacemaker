3a606d05eac124af22114fc2612fe2026744a5c3
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:30:06 -0700

    API: libcrmcommon: Drop XML_ALERT_ATTR_TSTAMP_FORMAT constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -500,9 +500,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_PATH instead
 #define XML_DIFF_PATH PCMK_XA_PATH
 
-//! \deprecated Use \c PCMK_META_TIMESTAMP_FORMAT instead
-#define XML_ALERT_ATTR_TSTAMP_FORMAT PCMK_META_TIMESTAMP_FORMAT
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
