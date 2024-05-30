02474a8a928edf7924e4704b596a43908f782560
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:17:27 -0700

    API: libcrmcommon: Drop XML_RSC_OP_T_QUEUE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -605,9 +605,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_EXEC_TIME instead
 #define XML_RSC_OP_T_EXEC PCMK_XA_EXEC_TIME
 
-//! \deprecated Use \c PCMK_XA_QUEUE_TIME instead
-#define XML_RSC_OP_T_QUEUE PCMK_XA_QUEUE_TIME
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
