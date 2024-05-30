4ef5666a74ed19a78dafebfadc7f6f307a7a7b00
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:42:41 -0700

    API: libcrmcommon: Drop XML_ATTR_STONITH_INDEX constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -347,9 +347,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_ATTR_TRANSITION_KEY	"transition-key"
 
-//! \deprecated Use \c PCMK_XA_INDEX instead
-#define XML_ATTR_STONITH_INDEX PCMK_XA_INDEX
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
