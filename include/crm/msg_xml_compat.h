67feaacb1e5399a71dd76388b30c53f2abff21b0
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:17:52 -0700

    API: libcrmcommon: Drop XML_RSC_OP_LAST_CHANGE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -599,9 +599,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_EXIT_REASON instead
 #define XML_LRM_ATTR_EXIT_REASON PCMK_XA_EXIT_REASON
 
-//! \deprecated Use \c PCMK_XA_LAST_RC_CHANGE instead
-#define XML_RSC_OP_LAST_CHANGE PCMK_XA_LAST_RC_CHANGE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
