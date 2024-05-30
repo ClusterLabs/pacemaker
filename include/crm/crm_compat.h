6e30bc534aa7cef8105561c49d291e9f1660eb2c
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:33:09 -0700

    API: libcrmcommon: Drop CRMD_ACTION_MIGRATE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/crm_compat.h
+++ include/crm/crm_compat.h
@@ -92,9 +92,6 @@ extern "C" {
 //! \deprecated Use PCMK_ACTION_META_DATA instead
 #define CRMD_ACTION_METADATA PCMK_ACTION_META_DATA
 
-//! \deprecated Use PCMK_ACTION_MIGRATE_TO instead
-#define CRMD_ACTION_MIGRATE PCMK_ACTION_MIGRATE_TO
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use GList * instead
 typedef GList *GListPtr;
