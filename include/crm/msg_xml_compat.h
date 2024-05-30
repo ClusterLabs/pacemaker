14f4ebea94d5d7b6140f6fa59b993ada9d6069bf
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:20:35 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_RSCID constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -569,9 +569,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_ATTR_ROUTER_NODE "router_node"
 
-//! \deprecated Do not use
-#define XML_LRM_ATTR_RSCID "rsc-id"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
