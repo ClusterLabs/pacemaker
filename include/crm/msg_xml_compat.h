2e16c135a58346b2afb99f6263b5e2b7c2db934f
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:34:15 -0700

    API: libcrmcommon: Drop F_CRM_TGRAPH_INPUT constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -449,9 +449,6 @@ extern "C" {
 //! \deprecated Do not use
 #define F_CRM_TGRAPH "crm-tgraph-file"
 
-//! \deprecated Do not use
-#define F_CRM_TGRAPH_INPUT "crm-tgraph-in"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
