a6b9896fa0b77b9c06b23d559c57c847645bbe09
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:21:18 -0700

    API: libcrmcommon: Drop XML_TAG_DIFF_REMOVED constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -557,9 +557,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_TAG_DIFF_ADDED "diff-added"
 
-//! \deprecated Do not use
-#define XML_TAG_DIFF_REMOVED "diff-removed"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
