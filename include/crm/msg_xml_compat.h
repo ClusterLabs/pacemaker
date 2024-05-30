a74b86d92731b1b887b25f1101803e8b024bc89a
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:44:56 -0700

    API: libcrmcommon: Drop XML_ATTR_UPDATE_USER constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -320,9 +320,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_UPDATE_CLIENT instead
 #define XML_ATTR_UPDATE_CLIENT PCMK_XA_UPDATE_CLIENT
 
-//! \deprecated Use \c PCMK_XA_UPDATE_USER instead
-#define XML_ATTR_UPDATE_USER PCMK_XA_UPDATE_USER
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
