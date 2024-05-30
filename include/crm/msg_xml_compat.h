795b1ed3a26374ab15b2e5d581af579dd7cde8c4
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:18:59 -0700

    API: libcrmcommon: Drop XML_LRM_ATTR_OP_RESTART constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -584,9 +584,6 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_LRM_ATTR_OP_DIGEST "op-digest"
 
-//! \deprecated Do not use
-#define XML_LRM_ATTR_OP_RESTART "op-force-restart"
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
