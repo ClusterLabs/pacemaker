7ad0c0d6295c0557a93e480c094c1d0550598356
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:13:18 -0700

    API: libcrmcommon: Drop XML_COLOC_ATTR_INFLUENCE constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -665,9 +665,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XA_NODE_ATTRIBUTE instead
 #define XML_COLOC_ATTR_NODE_ATTR PCMK_XA_NODE_ATTRIBUTE
 
-//! \deprecated Use \c PCMK_XA_INFLUENCE instead
-#define XML_COLOC_ATTR_INFLUENCE PCMK_XA_INFLUENCE
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
