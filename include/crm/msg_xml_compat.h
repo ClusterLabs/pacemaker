31c668c8918abdc689067d10705a9b46778ca43a
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 19:32:13 -0700

    API: libcrmcommon: Drop XML_DIFF_LIST constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -933,9 +933,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_XE_CHANGE instead
 #define XML_DIFF_CHANGE PCMK_XE_CHANGE
 
-//! \deprecated Use \c PCMK_XE_CHANGE_LIST instead
-#define XML_DIFF_LIST PCMK_XE_CHANGE_LIST
-
 #ifdef __cplusplus
 }
 #endif
