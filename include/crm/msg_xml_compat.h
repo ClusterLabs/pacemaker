96ec4f025671ec8d3632cefa8c8a6815d60b54dc
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 20:48:44 -0700

    API: libcrmcommon: Drop XML_RSC_ATTR_REMOTE_RA_ADDR constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -263,9 +263,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_VALUE_FALSE instead
 #define XML_BOOLEAN_NO PCMK_VALUE_FALSE
 
-//! \deprecated Use \c PCMK_REMOTE_RA_ADDR instead
-#define XML_RSC_ATTR_REMOTE_RA_ADDR PCMK_REMOTE_RA_ADDR
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
