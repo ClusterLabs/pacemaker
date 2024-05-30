e23f46fa3d991f4e1bf4eeaf304b824c18bb83f3
Reid Wahl <nrwahl@protonmail.com>
2024-05-29 21:03:39 -0700

    API: libcrmcommon: Drop XML_CONFIG_ATTR_DC_DEADTIME constant
    This breaks API backward compatibility.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -122,9 +122,6 @@ extern "C" {
 //! \deprecated Use \c PCMK_OPT_CLUSTER_RECHECK_INTERVAL instead
 #define XML_CONFIG_ATTR_RECHECK PCMK_OPT_CLUSTER_RECHECK_INTERVAL
 
-//! \deprecated Use \c PCMK_OPT_DC_DEADTIME instead
-#define XML_CONFIG_ATTR_DC_DEADTIME PCMK_OPT_DC_DEADTIME
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 //! \deprecated Use \c PCMK_XE_CIB instead
 #define XML_TAG_CIB PCMK_XE_CIB
