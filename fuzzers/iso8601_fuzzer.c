940130e26f079f5e14a1969669f386634a7eada8
Ken Gaillot <kgaillot@redhat.com>
2024-06-04 12:34:15 -0500

    Test: fuzzers: style conformance for iso8601_fuzzer.c

--- fuzzers/iso8601_fuzzer.c
+++ fuzzers/iso8601_fuzzer.c
@@ -6,40 +6,38 @@
  * This source code is licensed under the GNU Lesser General Public License
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
  */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <crm/common/util.h>
-#include <crm/common/iso8601.h>
-#include <crm/common/iso8601_internal.h>
-
+#include <crm/common/internal.h>
 
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-  char *ns;
-  char *result;
-  time_t epoch;
-  pcmk__time_hr_t *now;
+    char *ns = NULL;
+    char *result = NULL;
+    time_t epoch = 0;
+    pcmk__time_hr_t *now = NULL;
 
-  // Ensure we have enough data.
-  if (size < 10) {
+    // Ensure we have enough data.
+    if (size < 10) {
+        return 0;
+    }
+    ns = pcmk__assert_alloc(1, size + 1);
+    memcpy(ns, data, size);
+    ns[size] = '\0';
+
+    crm_time_parse_period(ns);
+    pcmk__time_hr_new(ns);
+
+    epoch = 0;
+    now = pcmk__time_hr_now(&epoch);
+    result = pcmk__time_format_hr(ns, now);
+    free(result);
+
+    free(ns);
     return 0;
-  }
-  ns = malloc(size+1);
-  memcpy(ns, data, size);
-  ns[size] = '\0';
-
-  crm_time_parse_period(ns);
-  pcmk__time_hr_new(ns);
-
-  epoch = 0;
-  now = NULL;
-  now = pcmk__time_hr_now(&epoch);
-  result = pcmk__time_format_hr(ns, now);
-  free(result);
-
-  free(ns);  
-  return 0;
 }
