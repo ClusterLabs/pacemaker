edb622f3cdd70751771c30e07046d3dd14066831
Ken Gaillot <kgaillot@redhat.com>
2024-06-04 15:58:09 -0500

    Test: fuzzers: style conformance for strings_fuzzer.c

--- fuzzers/strings_fuzzer.c
+++ fuzzers/strings_fuzzer.c
@@ -6,6 +6,7 @@
  * This source code is licensed under the GNU Lesser General Public License
  * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
  */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,27 +15,26 @@
 
 #include <crm/common/options.h>
 #include <crm/common/util.h>
-#include <crm/common/util_compat.h>
-#include <crm/common/strings_internal.h>
+#include <crm/common/internal.h>
 
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-  char *ns;
-  guint res;
+    char *ns = NULL;
+    guint res = 0U;
 
-  if (size < 10) {
+    if (size < 10) {
+        return 0;
+    }
+    ns = pcmk__assert_alloc(1, size + 1);
+    memcpy(ns, data, size);
+    ns[size] = '\0';
+
+    pcmk__numeric_strcasecmp(ns, ns);
+    pcmk__trim(ns);
+    pcmk_parse_interval_spec(ns, &res);
+    crm_get_msec(ns);
+
+    free(ns);
     return 0;
-  }
-  ns = malloc(size+1);
-  memcpy(ns, data, size);
-  ns[size] = '\0';
-
-  pcmk__numeric_strcasecmp(ns, ns);
-  pcmk__trim(ns);
-  pcmk_parse_interval_spec(ns, &res);
-  crm_get_msec(ns);
-
-  free(ns);  
-  return 0;
 }
