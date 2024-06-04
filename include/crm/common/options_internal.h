d74d17dc9a8fd0d1f1a2bbe95ece53fb47d97191
Reid Wahl <nrwahl@protonmail.com>
2024-06-04 13:37:30 -0700

    API: libcrmcommon: Drop PCMK_mcp and HA_mcp environment variables
    This has no effect except that if an external script like a resource
    agent checks the PCMK_mcp or HA_mcp environment variable, it will no
    longer find one set.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -158,9 +158,6 @@ bool pcmk__valid_stonith_watchdog_timeout(const char *value);
 // @COMPAT Drop at 3.0.0; default is plenty
 #define PCMK__ENV_CIB_TIMEOUT               "cib_timeout"
 
-// @COMPAT Drop at 3.0.0; likely last used in 1.1.24
-#define PCMK__ENV_MCP                       "mcp"
-
 /* @COMPAT Drop at 3.0.0; added to debug shutdown issues when Pacemaker is
  * managed by systemd, but no longer useful.
  */
