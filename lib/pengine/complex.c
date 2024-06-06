ClusterLabs/pacemaker

commit 8e3d1fb34ba6bb68fded3437525534e0fdc34a35

API: libcrmcommon: drop pcmk_resource_t variant member
This breaks API backward compatibility

lib/pengine/complex.c
@@ -707,8 +707,9 @@ pe__unpack_resource(xmlNode *xml_obj, pcmk_resource_t **rsc,
                                NULL);
     rsc_private->ops_xml = pcmk__xe_resolve_idref(ops, scheduler->input);
 
-    (*rsc)->variant = get_resource_type((const char *) rsc_private->xml->name);
-    if ((*rsc)->variant == pcmk_rsc_variant_unknown) {
+    rsc_private->variant = get_resource_type((const char *)
+                                             rsc_private->xml->name);
+    if (rsc_private->variant == pcmk_rsc_variant_unknown) {
         pcmk__config_err("Ignoring resource '%s' of unknown type '%s'",
                          id, rsc_private->xml->name);
         common_free(*rsc);
@@ -731,7 +732,7 @@ pe__unpack_resource(xmlNode *xml_obj, pcmk_resource_t **rsc,
 
     warn_about_deprecated_classes(*rsc);
 
-    rsc_private->fns = &resource_class_functions[(*rsc)->variant];
+    rsc_private->fns = &resource_class_functions[rsc_private->variant];
 
     get_meta_attributes((*rsc)->meta, *rsc, NULL, scheduler);
     (*rsc)->parameters = pe_rsc_params(*rsc, NULL, scheduler); // \deprecated
@@ -1063,7 +1064,8 @@ common_free(pcmk_resource_t * rsc)
         return;
     }
 
-    pcmk__rsc_trace(rsc, "Freeing %s %d", rsc->id, rsc->variant);
+    pcmk__rsc_trace(rsc, "Freeing %s %s",
+                    (const char *) rsc->private->xml->name, rsc->id);
 
     g_list_free(rsc->rsc_cons);
     g_list_free(rsc->rsc_cons_lhs);

include/crm/common/bundles_internal.h
@@ -45,7 +45,7 @@ typedef struct {
 static inline bool
 pcmk__is_bundle(const pcmk_resource_t *rsc)
 {
-    return (rsc != NULL) && (rsc->variant == pcmk_rsc_variant_bundle);
+    return (rsc != NULL) && (rsc->private->variant == pcmk_rsc_variant_bundle);
 }
 
 /*!
@@ -65,7 +65,7 @@ pcmk__is_bundled(const pcmk_resource_t *rsc)
     while (rsc->private->parent != NULL) {
         rsc = rsc->private->parent;
     }
-    return rsc->variant == pcmk_rsc_variant_bundle;
+    return rsc->private->variant == pcmk_rsc_variant_bundle;
 }
 
 /*!

include/crm/common/clone_internal.h
@@ -10,9 +10,10 @@
 #ifndef PCMK__CRM_COMMON_CLONE_INTERNAL__H
 #define PCMK__CRM_COMMON_CLONE_INTERNAL__H
 
-#include <stdbool.h>                    // bool
-#include <crm/common/scheduler_types.h> // pcmk_resource_t
-#include <crm/common/resources.h>       // pcmk_rsc_variant_clone
+#include <stdbool.h>                        // bool
+#include <crm/common/scheduler_types.h>     // pcmk_resource_t
+#include <crm/common/resources.h>           // pcmk_rsc_variant_clone
+#include <crm/common/resources_internal.h>  // struct pcmk__resource_private
 
 #ifdef __cplusplus
 extern "C" {
@@ -43,7 +44,7 @@ enum pcmk__clone_flags {
 static inline bool
 pcmk__is_clone(const pcmk_resource_t *rsc)
 {
-    return (rsc != NULL) && (rsc->variant == pcmk_rsc_variant_clone);
+    return (rsc != NULL) && (rsc->private->variant == pcmk_rsc_variant_clone);
 }
 
 /*!

include/crm/common/group_internal.h
@@ -10,9 +10,10 @@
 #ifndef PCMK__CRM_COMMON_GROUP_INTERNAL__H
 #define PCMK__CRM_COMMON_GROUP_INTERNAL__H
 
-#include <stdbool.h>                    // bool
-#include <crm/common/scheduler_types.h> // pcmk_resource_t
-#include <crm/common/resources.h>       // pcmk_rsc_variant_group
+#include <stdbool.h>                        // bool
+#include <crm/common/scheduler_types.h>     // pcmk_resource_t
+#include <crm/common/resources.h>           // pcmk_rsc_variant_group
+#include <crm/common/resources_internal.h>  // struct pcmk__resource_private
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,7 +38,7 @@ enum pcmk__group_flags {
 static inline bool
 pcmk__is_group(const pcmk_resource_t *rsc)
 {
-    return (rsc != NULL) && (rsc->variant == pcmk_rsc_variant_group);
+    return (rsc != NULL) && (rsc->private->variant == pcmk_rsc_variant_group);
 }
 
 #ifdef __cplusplus

include/crm/common/primitive_internal.h
@@ -10,9 +10,10 @@
 #ifndef PCMK__CRM_COMMON_PRIMITIVE_INTERNAL__H
 #define PCMK__CRM_COMMON_PRIMITIVE_INTERNAL__H
 
-#include <stdbool.h>                    // bool
-#include <crm/common/scheduler_types.h> // pcmk_resource_t
-#include <crm/common/resources.h>       // pcmk_rsc_variant_primitive
+#include <stdbool.h>                        // bool
+#include <crm/common/scheduler_types.h>     // pcmk_resource_t
+#include <crm/common/resources.h>           // pcmk_rsc_variant_primitive
+#include <crm/common/resources_internal.h>  // struct pcmk__resource_private
 
 #ifdef __cplusplus
 extern "C" {
@@ -29,7 +30,10 @@ extern "C" {
 static inline bool
 pcmk__is_primitive(const pcmk_resource_t *rsc)
 {
-    return (rsc != NULL) && (rsc->variant == pcmk_rsc_variant_primitive);
+    if (rsc == NULL) {
+        return false;
+    }
+    return rsc->private->variant == pcmk_rsc_variant_primitive;
 }
 
 #ifdef __cplusplus

include/crm/common/resources.h
@@ -241,7 +241,6 @@ struct pe_resource_s {
     //! \deprecated Call pcmk_resource_id() instead
     char *id;                           // Resource ID in configuration
 
-    enum pe_obj_types variant;          // Resource variant
     void *variant_opaque;               // Variant-specific (and private) data
 
     enum rsc_recovery_type recovery_type;   // How to recover if failed

include/crm/common/resources_internal.h
@@ -190,6 +190,7 @@ typedef struct {
 
 // Implementation of pcmk__resource_private_t
 struct pcmk__resource_private {
+    enum pe_obj_types variant;      // Resource variant
     char *history_id;               // Resource instance ID in history
     pcmk_resource_t *parent;        // Resource's parent resource, if any
     pcmk_scheduler_t *scheduler;    // Scheduler data containing resource

lib/pacemaker/pcmk_graph_producer.c
@@ -327,6 +327,7 @@ static void
 add_action_attributes(pcmk_action_t *action, xmlNode *action_xml)
 {
     xmlNode *args_xml = NULL;
+    pcmk_resource_t *rsc = action->rsc;
 
     /* We create free-standing XML to start, so we can sort the attributes
      * before adding it to action_xml, which keeps the scheduler regression
@@ -337,26 +338,25 @@ add_action_attributes(pcmk_action_t *action, xmlNode *action_xml)
     crm_xml_add(args_xml, PCMK_XA_CRM_FEATURE_SET, CRM_FEATURE_SET);
     g_hash_table_foreach(action->extra, hash2field, args_xml);
 
-    if ((action->rsc != NULL) && (action->node != NULL)) {
+    if ((rsc != NULL) && (action->node != NULL)) {
         // Get the resource instance attributes, evaluated properly for node
-        GHashTable *params = pe_rsc_params(action->rsc, action->node,
-                                           action->rsc->private->scheduler);
+        GHashTable *params = pe_rsc_params(rsc, action->node,
+                                           rsc->private->scheduler);
 
-        pcmk__substitute_remote_addr(action->rsc, params);
+        pcmk__substitute_remote_addr(rsc, params);
 
         g_hash_table_foreach(params, hash2smartfield, args_xml);
 
-    } else if ((action->rsc != NULL)
-               && (action->rsc->variant <= pcmk_rsc_variant_primitive)) {
-        GHashTable *params = pe_rsc_params(action->rsc, NULL,
-                                           action->rsc->private->scheduler);
+    } else if ((rsc != NULL)
+               && (rsc->private->variant <= pcmk_rsc_variant_primitive)) {
+        GHashTable *params = pe_rsc_params(rsc, NULL, rsc->private->scheduler);
 
         g_hash_table_foreach(params, hash2smartfield, args_xml);
     }
 
     g_hash_table_foreach(action->meta, hash2metafield, args_xml);
-    if (action->rsc != NULL) {
-        pcmk_resource_t *parent = action->rsc;
+    if (rsc != NULL) {
+        pcmk_resource_t *parent = rsc;
 
         while (parent != NULL) {
             parent->private->cmds->add_graph_meta(parent, args_xml);

commit b3a1a91ba87d088ff6945e8a18d2de8f4b36ca37

Low: scheduler: avoid core-dump on trace message
Bundles free implicit resource XML before calling common_free() on the
resource, so we can't assume the XML is non-NULL in common_free().

Regression introduced by 8e3d1fb34ba (never in a release)

lib/pengine/complex.c
@@ -1061,8 +1061,7 @@ common_free(pcmk_resource_t * rsc)
         return;
     }
 
-    pcmk__rsc_trace(rsc, "Freeing %s %s",
-                    (const char *) rsc->private->xml->name, rsc->id);
+    pcmk__rsc_trace(rsc, "Freeing %s", rsc->id);
 
     if (rsc->private->parameter_cache != NULL) {
         g_hash_table_destroy(rsc->private->parameter_cache);