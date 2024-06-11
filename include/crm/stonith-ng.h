5c3096f282254cb48205f64bdfaacb425f72748a
Ken Gaillot <kgaillot@redhat.com>
2024-06-11 14:38:59 -0500

    API: libstonithd: drop get_stonith_provider()
    This breaks API backward compatibility

--- include/crm/stonith-ng.h
+++ include/crm/stonith-ng.h
@@ -686,17 +686,6 @@ bool stonith_agent_exists(const char *agent, int timeout);
  */
 const char *stonith_action_str(const char *action);
 
-#if !defined(PCMK_ALLOW_DEPRECATED) || (PCMK_ALLOW_DEPRECATED == 1)
-/* Normally we'd put this section in a separate file (crm/fencing/compat.h), but
- * we can't do that for the reason noted at the top of this file. That does mean
- * we have to duplicate these declarations where they're implemented.
- */
-
-//! \deprecated Use stonith_get_namespace() instead
-const char *get_stonith_provider(const char *agent, const char *provider);
-
-#endif
-
 #ifdef __cplusplus
 }
 #endif
