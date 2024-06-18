3a2e9e31dbed7191e5c859c8e4e355de08c8389d
Ken Gaillot <kgaillot@redhat.com>
2024-06-17 19:28:40 -0500

    Test: test-headers.sh: test library-private headers

--- tests/test-headers.sh
+++ tests/test-headers.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 #
-# Check public headers for c++ compatibility and consistent protection #ifdefs
+# Copyright 2022-2024 the Pacemaker project contributors
 #
+# The version control history for this file may have further details.
+#
+# This source code is licensed under the GNU General Public License version 2
+# or later (GPLv2+) WITHOUT ANY WARRANTY.
+#
+
+#
+# Check public headers for C++ compatibility and consistent protection #ifdefs
 #
 
 : ${SRCDIR:-..}
 
-INCLUDE_FILES="$(find "${SRCDIR}/include/" -name \*.h \
+INCLUDE_FILES="$(find "${SRCDIR}/include/" "${SRCDIR}/lib/" -name \*.h \
                       -a \! -name config.h            \
                       -a \! -name gettext.h)"
 
@@ -14,19 +22,20 @@ TESTFILE="$(mktemp "${TMPDIR:-/tmp}/test-headers-XXXXXXXXXX.c")"
 
 for i in $INCLUDE_FILES
 do
-    NAME="$(echo $i | sed -e 's#^.*/include/##')"
+    NAME="$(echo $i | sed -e 's#^.*/include/##' | sed -e 's#^.*/lib/##')"
     PROTECT="PCMK__$(echo "$NAME" | tr '[:lower:]/\-\.' '[:upper:]___' | sed 's/_H$/__H/')"
 
     cat >"$TESTFILE" <<EOF
 #include <$NAME>
 #ifndef $PROTECT
-#error no header protector in file $i
+#error no $PROTECT header protector in file $i
 #endif
 int main(void) {return 0;}
 EOF
 
     # Not including ${CFLAGS} because it seems to break header detection. But we're not really building here
-    ${CC} -I "${SRCDIR}/include" -DHAVE_CONFIG_H ${CPPFLAGS} ${LIBS} "$TESTFILE" -o /dev/null
+    ${CC} -I "${SRCDIR}/include" -I"${SRCDIR}/lib" \
+        -DHAVE_CONFIG_H ${CPPFLAGS} ${LIBS} "$TESTFILE" -o /dev/null
     if [ $? -ne 0 ]
     then
         rm -f "$TESTFILE"
@@ -34,7 +43,8 @@ EOF
     fi
     if [ -n "$CXX" ]
     then
-        ${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LIBS} -I "${SRCDIR}/include" "$TESTFILE" -o /dev/null
+        ${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LIBS} \
+            -I "${SRCDIR}/include" -I"${SRCDIR}/lib" "$TESTFILE" -o /dev/null
         if [ $? -ne 0 ]
         then
             rm -f "$TESTFILE"
