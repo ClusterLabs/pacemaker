ClusterLabs/pacemaker

commit a744f1a8e61dedeab6ffadfb8093c10f6f6914ea

Low: libpacemaker: Move crm_verify functionality to libpacemaker
closes T113

include/pcmki/pcmki_verify.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__PCMKI_PCMKI_VERIFY__H
+#  define PCMK__PCMKI_PCMKI_VERIFY__H
+
+#include <crm/common/output_internal.h>
+#include <crm/common/scheduler.h>
+#include <libxml/tree.h>
+
+/*!
+ * \internal
+ * \brief Parse a CIB file
+ *
+ * This function parses a CIB file into a CIB object
+ *
+ * \param[in]     out          Output to use for logging and printing results
+ * \param[in]     cib_source   Source of the CIB: 
+ *                             NULL -> use live cib, "-" -> stdin
+ *                             "<..." -> xml str, otherwise -> xml file name                    
+ * \param[in,out] cib_object   The resulting, parsed CIB object
+ *
+ * \return Standard Pacemaker return code
+ */
+int pcmk__parse_cib(pcmk__output_t *out, const char *cib_source, xmlNodePtr *cib_object);
+
+/*!
+ * \internal
+ * \brief Verify that a CIB is error-free or output errors and warnings
+ *
+ * This high-level function essentially implements crm_verify(8). It operates
+ * on an input CIB file, which can be inputted through one of several ways. It
+ * can either write out XML-formatted output or plaintext output.
+ *
+ * \param[in,out] scheduler    Scheduler data
+ * \param[in]     out          Output to use for logging and printing results
+ * \param[in]     cib_object   The parsed CIB object
+ *
+ * \return Standard Pacemaker return code
+ */
+int pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_object);
+
+#endif

lib/pacemaker/pcmk_verify.c
@@ -1,0 +1,166 @@
+/*
+ * Copyright 2023-2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+#include <crm/cib/internal.h>
+#include <crm/common/output.h>
+#include <crm/common/results.h>
+#include <crm/common/scheduler.h>
+#include <pacemaker-internal.h>
+#include <pacemaker.h>
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "libpacemaker_private.h"
+
+int
+pcmk__parse_cib(pcmk__output_t *out, const char *cib_source, xmlNodePtr *cib_object)
+{
+    int rc = pcmk_rc_ok;
+
+    if (cib_source == NULL) {
+        crm_info("Reading XML from: live cluster");
+        rc = cib__signon_query(out, NULL, cib_object);
+
+    }  else if (cib_source[0] == '<') {
+        *cib_object = string2xml(cib_source);
+        if (*cib_object == NULL) {
+            rc = ENODATA;
+        }
+
+    } else if (strcmp(cib_source, "-") == 0) {
+        *cib_object = stdin2xml();
+        if (*cib_object == NULL) {
+            rc = ENODATA;
+        }
+
+    } else {
+        *cib_object = filename2xml(cib_source);
+        if (*cib_object == NULL) {
+            rc = ENODATA;
+        }
+    }
+
+    return rc;
+}
+
+int
+pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_object)
+{
+    int rc = pcmk_rc_ok;
+    xmlNode *status = NULL;
+    xmlNode *cib_object_copy = NULL;
+
+    if (!pcmk__xe_is(cib_object, XML_TAG_CIB)) {
+        rc = EBADMSG;
+        out->err(out, "This tool can only check complete configurations (i.e. those starting with <cib>).");
+        goto verify_done;
+    }
+
+    status = pcmk_find_cib_element(cib_object, XML_CIB_TAG_STATUS);
+    if (status == NULL) {
+        create_xml_node(cib_object, XML_CIB_TAG_STATUS);
+    }
+
+    if (pcmk__validate_xml(cib_object, NULL, (xmlRelaxNGValidityErrorFunc) out->err, out) == FALSE) {
+        crm_config_error = TRUE;
+        rc = pcmk_rc_schema_validation;
+        goto verify_done;
+
+    } else if (cli_config_update(&cib_object, NULL, FALSE) == FALSE) {
+        crm_config_error = TRUE;
+        out->err(out, "The cluster will NOT be able to use this configuration.\n"
+                 "Please manually update the configuration to conform to the %s syntax.",
+                 xml_latest_schema());
+        rc = pcmk_rc_schema_validation;
+        goto verify_done;
+    }
+
+    /* Process the configuration to set crm_config_error/crm_config_warning.
+     *
+     * @TODO Some parts of the configuration are unpacked only when needed (for
+     * example, action configuration), so we aren't necessarily checking those.
+     */
+    if (cib_object != NULL) {
+        unsigned long long flags = pcmk_sched_no_counts|pcmk_sched_no_compat;
+
+        if (status == NULL) {
+            // No status available, so do minimal checks
+            flags |= pcmk_sched_validate_only;
+        }
+        cib_object_copy = copy_xml(cib_object);
+
+        /* The scheduler takes ownership of the XML object and potentially
+         * frees it later. We want the caller of pcmk__verify to retain
+         * ownership of the passed-in XML object, hence we pass in a copy
+         * to the scheduler.
+         */ 
+        pcmk__schedule_actions(cib_object_copy, flags, scheduler);
+    }
+
+verify_done:
+    if (crm_config_error) {
+        rc = pcmk_rc_schema_validation;
+        pcmk__config_err("CIB did not pass schema validation");
+    } else if (crm_config_warning) {
+        rc = pcmk_rc_schema_validation;
+    }
+    return rc;
+}
+
+int
+pcmk_verify(xmlNodePtr *xml, const char *cib_source)
+{
+    pcmk_scheduler_t *scheduler = NULL;
+    pcmk__output_t *out = NULL;
+    int rc = pcmk_rc_ok;
+
+    xmlNode *cib_object = NULL;
+
+    rc = pcmk__xml_output_new(&out, xml);
+    if (rc != pcmk_rc_ok) {
+        return rc;
+    }
+
+    pe__register_messages(out);
+    pcmk__register_lib_messages(out);
+
+    rc = pcmk__parse_cib(out, cib_source, &cib_object);
+    
+    if (rc != pcmk_rc_ok) {
+        out->err(out, "Couldn't parse input");
+        goto done;
+    }
+
+    scheduler = pe_new_working_set();
+
+    if (scheduler == NULL) {
+        rc = errno;
+        out->err(out, "Could not allocate scheduler data: %s", pcmk_rc_str(rc));
+        goto done;
+    }
+    
+    scheduler->priv = out;
+
+    rc = pcmk__verify(scheduler, out, cib_object);
+    
+    pe_free_working_set(scheduler);
+
+done:
+    pcmk__xml_output_finish(out, pcmk_rc2exitc(rc), xml);
+
+    if (cib_object != NULL) {
+        free_xml(cib_object);      
+    }
+
+    return rc;
+}

tools/crm_verify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2023 the Pacemaker project contributors
+ * Copyright 2004-2024 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -115,8 +115,6 @@ build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
 int
 main(int argc, char **argv)
 {
-    xmlNode *cib_object = NULL;
-    xmlNode *status = NULL;
 
     pcmk_scheduler_t *scheduler = NULL;
 
@@ -127,7 +125,13 @@ main(int argc, char **argv)
 
     pcmk__output_t *out = NULL;
 
+    const char *cib_source = NULL;
+    xmlNode *cib_object = NULL;
+
     GOptionGroup *output_group = NULL;
+
+    const char *failure_type = NULL;
+
     pcmk__common_args_t *args = pcmk__new_common_args(SUMMARY);
     gchar **processed_args = pcmk__cmdline_preproc(argv, "xSX");
     GOptionContext *context = build_arg_context(args, &output_group);
@@ -162,51 +166,28 @@ main(int argc, char **argv)
     pcmk__set_config_error_handler((pcmk__config_error_func) out->err, out);
     pcmk__set_config_warning_handler((pcmk__config_warning_func) out->err, out);
 
-    crm_info("=#=#=#=#= Getting XML =#=#=#=#=");
+    if (pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
+        args->verbosity = 1;
+    }
 
-    if (options.use_live_cib) {
-        crm_info("Reading XML from: live cluster");
-        rc = cib__signon_query(out, NULL, &cib_object);
-
-        if (rc != pcmk_rc_ok) {
-            // cib__signon_query() outputs any relevant error
-            goto done;
-        }
-
-    } else if (options.xml_file != NULL) {
-        cib_object = filename2xml(options.xml_file);
-        if (cib_object == NULL) {
-            rc = ENODATA;
-            g_set_error(&error, PCMK__RC_ERROR, rc, "Couldn't parse input file: %s", options.xml_file);
-            goto done;
-        }
-
+    if (options.xml_file != NULL) {
+        cib_source = options.xml_file;
     } else if (options.xml_string != NULL) {
-        cib_object = string2xml(options.xml_string);
-        if (cib_object == NULL) {
-            rc = ENODATA;
-            g_set_error(&error, PCMK__RC_ERROR, rc, "Couldn't parse input string: %s", options.xml_string);
-            goto done;
-        }
+        cib_source = options.xml_string;
     } else if (options.xml_stdin) {
-        cib_object = stdin2xml();
-        if (cib_object == NULL) {
-            rc = ENODATA;
-            g_set_error(&error, PCMK__RC_ERROR, rc, "Couldn't parse input from STDIN.");
-            goto done;
-        }
-
+        cib_source = "-";
+    } else if (options.use_live_cib) {
+        cib_source = NULL;
     } else {
         rc = ENODATA;
-        g_set_error(&error, PCMK__RC_ERROR, rc,
-                    "No configuration source specified.  Use --help for usage information.");
+        g_set_error(&error, PCMK__EXITC_ERROR, exit_code, "No input specified");
         goto done;
     }
 
-    if (!pcmk__xe_is(cib_object, XML_TAG_CIB)) {
-        rc = EBADMSG;
-        g_set_error(&error, PCMK__RC_ERROR, rc,
-                    "This tool can only check complete configurations (i.e. those starting with <cib>).");
+    rc = pcmk__parse_cib(out, cib_source, &cib_object);
+
+    if (rc != pcmk_rc_ok) {
+        g_set_error(&error, PCMK__EXITC_ERROR, rc, "Couldn't parse input");
         goto done;
     }
 
@@ -214,72 +195,36 @@ main(int argc, char **argv)
         write_xml_file(cib_object, options.cib_save, FALSE);
     }
 
-    status = pcmk_find_cib_element(cib_object, XML_CIB_TAG_STATUS);
-    if (status == NULL) {
-        create_xml_node(cib_object, XML_CIB_TAG_STATUS);
-    }
-
-    if (pcmk__validate_xml(cib_object, NULL, (xmlRelaxNGValidityErrorFunc) out->err, out) == FALSE) {
-        pcmk__config_err("CIB did not pass schema validation");
-        free_xml(cib_object);
-        cib_object = NULL;
-
-    } else if (cli_config_update(&cib_object, NULL, FALSE) == FALSE) {
-        crm_config_error = TRUE;
-        free_xml(cib_object);
-        cib_object = NULL;
-        out->err(out, "The cluster will NOT be able to use this configuration.\n"
-                 "Please manually update the configuration to conform to the %s syntax.",
-                 xml_latest_schema());
-    }
-
     scheduler = pe_new_working_set();
+
     if (scheduler == NULL) {
         rc = errno;
         g_set_error(&error, PCMK__RC_ERROR, rc,
                     "Could not allocate scheduler data: %s", pcmk_rc_str(rc));
         goto done;
     }
+    
     scheduler->priv = out;
 
-    /* Process the configuration to set crm_config_error/crm_config_warning.
-     *
-     * @TODO Some parts of the configuration are unpacked only when needed (for
-     * example, action configuration), so we aren't necessarily checking those.
-     */
-    if (cib_object != NULL) {
-        unsigned long long flags = pcmk_sched_no_counts|pcmk_sched_no_compat;
+    rc = pcmk__verify(scheduler, out, cib_object);
 
-        if ((status == NULL) && !options.use_live_cib) {
-            // No status available, so do minimal checks
-            flags |= pcmk_sched_validate_only;
+    if (rc == pcmk_rc_schema_validation) {
+        if (crm_config_error) {
+            failure_type = "Errors found during check: ";
+          } else if (crm_config_warning) {
+            failure_type = "Warnings found during check: ";
+          } else {
+            failure_type = "";
+          }
+
+          if (args->verbosity > 0) {
+              out->err(out, "%sconfig not valid", failure_type);
+          } else {
+              out->err(out, "%sconfig not valid\n-V may provide more details", failure_type);
+          }
         }
-        pcmk__schedule_actions(cib_object, flags, scheduler);
-    }
+
     pe_free_working_set(scheduler);
-
-    if (crm_config_error) {
-        rc = pcmk_rc_schema_validation;
-
-        if (args->verbosity > 0 || pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
-            g_set_error(&error, PCMK__RC_ERROR, rc,
-                        "Errors found during check: config not valid");
-        } else {
-            g_set_error(&error, PCMK__RC_ERROR, rc,
-                        "Errors found during check: config not valid\n-V may provide more details");
-        } 
-
-    } else if (crm_config_warning) {
-        rc = pcmk_rc_schema_validation;
-
-        if (args->verbosity > 0 || pcmk__str_eq(args->output_ty, "xml", pcmk__str_none)) {
-            g_set_error(&error, PCMK__RC_ERROR, rc,
-                        "Warnings found during check: config may not be valid");
-        } else {
-            g_set_error(&error, PCMK__RC_ERROR, rc,
-                        "Warnings found during check: config may not be valid\n-V may provide more details");
-        }
-    }
 
   done:
     g_strfreev(processed_args);
@@ -287,6 +232,10 @@ main(int argc, char **argv)
     free(options.cib_save);
     free(options.xml_file);
     free(options.xml_string);
+
+    if (cib_object != NULL) {
+        free_xml(cib_object);      
+    }
 
     if (exit_code == CRM_EX_OK) {
         exit_code = pcmk_rc2exitc(rc);

Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright 2003-2023 the Pacemaker project contributors
+# Copyright 2003-2024 the Pacemaker project contributors
 #
 # The version control history for this file may have further details.
 #

cts/cli/regression.tools.exp
@@ -7904,7 +7904,8 @@ crm_shadow: Could not access shadow instance 'cts-cli': No such file or director
     <errors>
       <error>Resource test2:0 is of type systemd and therefore cannot be used as a promotable clone resource</error>
       <error>Ignoring &lt;clone&gt; resource 'test2-clone' because configuration is invalid</error>
-      <error>crm_verify: Errors found during check: config not valid</error>
+      <error>CIB did not pass schema validation</error>
+      <error>Errors found during check: config not valid</error>
     </errors>
   </status>
 </pacemaker-result>
@@ -7919,7 +7920,8 @@ crm_shadow: Could not access shadow instance 'cts-cli': No such file or director
       <error>NOTE: Clusters with shared data need STONITH to ensure data integrity</error>
       <error>Node pcmk-1 is unclean but cannot be fenced</error>
       <error>Node pcmk-2 is unclean but cannot be fenced</error>
-      <error>crm_verify: Errors found during check: config not valid</error>
+      <error>CIB did not pass schema validation</error>
+      <error>Errors found during check: config not valid</error>
     </errors>
   </status>
 </pacemaker-result>
@@ -7932,7 +7934,8 @@ unpack_config 	warning: Blind faith: not fencing unseen nodes
     <errors>
       <error>Resource test2:0 is of type systemd and therefore cannot be used as a promotable clone resource</error>
       <error>Ignoring &lt;clone&gt; resource 'test2-clone' because configuration is invalid</error>
-      <error>crm_verify: Errors found during check: config not valid</error>
+      <error>CIB did not pass schema validation</error>
+      <error>Errors found during check: config not valid</error>
     </errors>
   </status>
 </pacemaker-result>
@@ -7948,7 +7951,8 @@ cluster_status 	warning: Fencing and resource management disabled due to lack of
       <error>NOTE: Clusters with shared data need STONITH to ensure data integrity</error>
       <error>Node pcmk-1 is unclean but cannot be fenced</error>
       <error>Node pcmk-2 is unclean but cannot be fenced</error>
-      <error>crm_verify: Errors found during check: config not valid</error>
+      <error>CIB did not pass schema validation</error>
+      <error>Errors found during check: config not valid</error>
     </errors>
   </status>
 </pacemaker-result>

include/pacemaker-internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the Pacemaker project contributors
+ * Copyright 2019-2024 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -22,5 +22,6 @@
 #  include <pcmki/pcmki_simulate.h>
 #  include <pcmki/pcmki_status.h>
 #  include <pcmki/pcmki_transition.h>
+#  include <pcmki/pcmki_verify.h>
 
 #endif

include/pacemaker.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the Pacemaker project contributors
+ * Copyright 2019-2024 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -257,6 +257,22 @@ int pcmk_simulate(xmlNodePtr *xml, pcmk_scheduler_t *scheduler,
                   unsigned int section_opts, const char *use_date,
                   const char *input_file, const char *graph_file,
                   const char *dot_file);
+
+/*!
+ * \brief Verify that a CIB is error-free or output errors and warnings
+ *
+ * This high-level function essentially implements crm_verify(8). It operates
+ * on an input CIB file, which can be inputted through one of several ways. It
+ * writes out XML-formatted output.
+ *
+ * \param[in,out] xml          The destination for the result, as an XML tree
+ * \param[in]     cib_source   Source of the CIB: 
+ *                             NULL -> use live cib, "-" -> stdin
+ *                             "<..." -> xml str, otherwise -> xml file name
+ *
+ * \return Standard Pacemaker return code
+ */
+int pcmk_verify(xmlNodePtr *xml, const char *cib_source);
 
 /*!
  * \brief Get nodes list

lib/Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright 2003-2023 the Pacemaker project contributors
+# Copyright 2003-2024 the Pacemaker project contributors
 #
 # The version control history for this file may have further details.
 #

commit 7b68e220b0923b777fdcf33f73bf3549adab911b

Fix: tools: Don't double-free XML in crm_verify after schema update
Currently, if an updated schema is acceptable, the old CIB XML is freed
by pcmk_update_configured_schema() and then again by
crm_verify.c:main(). Here we fix this by passing a pointer-to-pointer
into pcmk__verify(), so that crm_verify gets a pointer to the new XML
object instead of to the freed one.

This fixes a regression introduced by a744f1a8.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

include/pcmki/pcmki_verify.h
@@ -44,11 +44,12 @@ int pcmk__parse_cib(pcmk__output_t *out, const char *cib_source, xmlNodePtr *cib
  *
  * \param[in,out] scheduler    Scheduler data
  * \param[in]     out          Output to use for logging and printing results
- * \param[in]     cib_object   The parsed CIB object
+ * \param[in,out] cib_object   The parsed CIB object
  *
  * \return Standard Pacemaker return code
  */
-int pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_object);
+int pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out,
+                 xmlNode **cib_object);
 
 #ifdef __cplusplus
 }

lib/pacemaker/pcmk_verify.c
@@ -47,31 +47,34 @@ pcmk__parse_cib(pcmk__output_t *out, const char *cib_source, xmlNodePtr *cib_obj
 }
 
 int
-pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_object)
+pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out,
+             xmlNode **cib_object)
 {
     int rc = pcmk_rc_ok;
     xmlNode *status = NULL;
     xmlNode *cib_object_copy = NULL;
 
-    if (!pcmk__xe_is(cib_object, PCMK_XE_CIB)) {
+    CRM_ASSERT(cib_object != NULL);
+
+    if (!pcmk__xe_is(*cib_object, PCMK_XE_CIB)) {
         rc = EBADMSG;
         out->err(out, "This tool can only check complete configurations (i.e. those starting with <cib>).");
         goto verify_done;
     }
 
-    status = pcmk_find_cib_element(cib_object, PCMK_XE_STATUS);
+    status = pcmk_find_cib_element(*cib_object, PCMK_XE_STATUS);
     if (status == NULL) {
-        pcmk__xe_create(cib_object, PCMK_XE_STATUS);
+        pcmk__xe_create(*cib_object, PCMK_XE_STATUS);
     }
 
-    if (!pcmk__validate_xml(cib_object, NULL,
+    if (!pcmk__validate_xml(*cib_object, NULL,
                             (xmlRelaxNGValidityErrorFunc) out->err, out)) {
         crm_config_error = TRUE;
         rc = pcmk_rc_schema_validation;
         goto verify_done;
     }
 
-    rc = pcmk_update_configured_schema(&cib_object, false);
+    rc = pcmk_update_configured_schema(cib_object, false);
     if (rc != pcmk_rc_ok) {
         crm_config_error = TRUE;
         out->err(out, "The cluster will NOT be able to use this configuration.\n"
@@ -85,14 +88,14 @@ pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_obje
      * @TODO Some parts of the configuration are unpacked only when needed (for
      * example, action configuration), so we aren't necessarily checking those.
      */
-    if (cib_object != NULL) {
+    if (*cib_object != NULL) {
         unsigned long long flags = pcmk__sched_no_counts|pcmk__sched_no_compat;
 
         if (status == NULL) {
             // No status available, so do minimal checks
             flags |= pcmk__sched_validate_only;
         }
-        cib_object_copy = pcmk__xml_copy(NULL, cib_object);
+        cib_object_copy = pcmk__xml_copy(NULL, *cib_object);
 
         /* The scheduler takes ownership of the XML object and potentially
          * frees it later. We want the caller of pcmk__verify to retain
@@ -143,7 +146,7 @@ pcmk_verify(xmlNodePtr *xml, const char *cib_source)
     }
 
     scheduler->priv->out = out;
-    rc = pcmk__verify(scheduler, out, cib_object);
+    rc = pcmk__verify(scheduler, out, &cib_object);
 
 done:
     pe_free_working_set(scheduler);

tools/crm_verify.c
@@ -255,7 +255,7 @@ main(int argc, char **argv)
 
     scheduler->priv->out = out;
 
-    rc = pcmk__verify(scheduler, out, cib_object);
+    rc = pcmk__verify(scheduler, out, &cib_object);
 
     if (rc == pcmk_rc_schema_validation) {
         if (crm_config_error) {