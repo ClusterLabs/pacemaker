ClusterLabs/pacemaker

commit 09d13ae759d83fad62690b154ab2461411f82e3d

Refactor: libcrmcommon: New pcmk__xe_set_id()
To replace crm_xml_set_id()

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cluster/corosync.c
@@ -646,7 +646,7 @@ pcmk__corosync_add_nodes(xmlNode *xml_parent)
             if (xml_parent) {
                 xmlNode *node = pcmk__xe_create(xml_parent, PCMK_XE_NODE);
 
-                crm_xml_set_id(node, "%u", nodeid);
+                pcmk__xe_set_id(node, "%u", nodeid);
                 crm_xml_add(node, PCMK_XA_UNAME, name);
             }
         }

lib/common/ipc_client.c
@@ -769,7 +769,7 @@ create_purge_node_request(const pcmk_ipc_api_t *api, const char *node_name,
             request = create_request(CRM_OP_RM_NODE_CACHE, NULL, NULL,
                                      pcmk_ipc_name(api, false), client, NULL);
             if (nodeid > 0) {
-                crm_xml_set_id(request, "%lu", (unsigned long) nodeid);
+                pcmk__xe_set_id(request, "%lu", (unsigned long) nodeid);
             }
             crm_xml_add(request, PCMK_XA_UNAME, node_name);
             break;

lib/common/ipc_controld.c
@@ -417,7 +417,7 @@ pcmk_controld_api_node_info(pcmk_ipc_api_t *api, uint32_t nodeid)
         return EINVAL;
     }
     if (nodeid > 0) {
-        crm_xml_set_id(request, "%lu", (unsigned long) nodeid);
+        pcmk__xe_set_id(request, "%lu", (unsigned long) nodeid);
     }
 
     rc = send_controller_request(api, request, true);

tools/crm_node.c
@@ -549,7 +549,7 @@ remove_from_section(cib_t *cib, const char *element, const char *section,
 
     crm_xml_add(xml, PCMK_XA_UNAME, node_name);
     if (node_id > 0) {
-        crm_xml_set_id(xml, "%ld", node_id);
+        pcmk__xe_set_id(xml, "%ld", node_id);
     }
 
     rc = cib->cmds->remove(cib, section, xml, cib_transaction);
@@ -689,7 +689,7 @@ purge_node_from_fencer(const char *node_name, long node_id)
     cmd = create_request(CRM_OP_RM_NODE_CACHE, NULL, NULL, "stonith-ng",
                          crm_system_name, NULL);
     if (node_id > 0) {
-        crm_xml_set_id(cmd, "%ld", node_id);
+        pcmk__xe_set_id(cmd, "%ld", node_id);
     }
     crm_xml_add(cmd, PCMK_XA_UNAME, node_name);
 

daemons/controld/controld_schedulerd.c
@@ -431,8 +431,8 @@ force_local_option(xmlNode *xml, const char *attr_name, const char *attr_value)
 
         xml = pcmk__xe_create(cluster_property_set, PCMK_XE_NVPAIR);
 
-        crm_xml_set_id(xml, "%s-%s",
-                       PCMK_VALUE_CIB_BOOTSTRAP_OPTIONS, attr_name);
+        pcmk__xe_set_id(xml, "%s-%s",
+                        PCMK_VALUE_CIB_BOOTSTRAP_OPTIONS, attr_name);
         crm_xml_add(xml, PCMK_XA_NAME, attr_name);
         crm_xml_add(xml, PCMK_XA_VALUE, attr_value);
     }

include/crm/common/xml_internal.h
@@ -456,6 +456,8 @@ enum pcmk__xa_flags {
 int pcmk__xe_copy_attrs(xmlNode *target, const xmlNode *src, uint32_t flags);
 
 void pcmk__xml_sanitize_id(char *id);
+void pcmk__xe_set_id(xmlNode *xml, const char *format, ...)
+    G_GNUC_PRINTF(2, 3);
 
 /*!
  * \internal

lib/common/actions.c
@@ -526,7 +526,7 @@ crm_create_op_xml(xmlNode *parent, const char *prefix, const char *task,
     CRM_CHECK(prefix && task && interval_spec, return NULL);
 
     xml_op = pcmk__xe_create(parent, PCMK_XE_OP);
-    crm_xml_set_id(xml_op, "%s-%s-%s", prefix, task, interval_spec);
+    pcmk__xe_set_id(xml_op, "%s-%s-%s", prefix, task, interval_spec);
     crm_xml_add(xml_op, PCMK_META_INTERVAL, interval_spec);
     crm_xml_add(xml_op, PCMK_XA_NAME, task);
     if (timeout) {

lib/common/nvpair.c
@@ -811,8 +811,8 @@ crm_create_nvpair_xml(xmlNode *parent, const char *id, const char *name,
     if (id) {
         crm_xml_add(nvp, PCMK_XA_ID, id);
     } else {
-        crm_xml_set_id(nvp, "%s-%s",
-                       pcmk__s(pcmk__xe_id(parent), PCMK_XE_NVPAIR), name);
+        pcmk__xe_set_id(nvp, "%s-%s",
+                        pcmk__s(pcmk__xe_id(parent), PCMK_XE_NVPAIR), name);
     }
     crm_xml_add(nvp, PCMK_XA_NAME, name);
     crm_xml_add(nvp, PCMK_XA_VALUE, value);

commit c8b20114009bcf06a79b66743851f16fd80fee9a

Fix: various: Don't set cluster-layer node ID as XML ID
Currently, we call the pcmk__xe_set_id() function using a stringified
version of the numeric cluster-layer node ID. However, pcmk__xe_set_id()
tries to sanitize its input to a valid XML ID. An XML ID cannot begin
with a digit.

The result is that the first character of the node ID string becomes an
underscore, making it impossible for the controller to parse it as an
int when it receives the node info request. In that case, the controller
will ignore the node ID and instead search for the passed-in node name
(if any) or the local node name. This yields the wrong result unless we
were trying to search for the local node.

This is a regression introduced by 09d13ae (not in any release yet).

Fixes T848

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/controld/controld_messages.c
@@ -909,7 +909,11 @@ handle_node_info_request(const xmlNode *msg)
                            pcmk_is_set(controld_globals.flags,
                                        controld_has_quorum));
 
-    // Check whether client requested node info by ID and/or name
+    /* Check whether client requested node info by ID and/or name
+     *
+     * @TODO A Corosync-layer node ID is of type uint32_t. We should be able to
+     * handle legitimate node IDs greater than INT_MAX, but currently we do not.
+     */
     crm_element_value_int(msg, PCMK_XA_ID, &node_id);
     if (node_id < 0) {
         node_id = 0;

lib/cluster/corosync.c
@@ -651,7 +651,7 @@ pcmk__corosync_add_nodes(xmlNode *xml_parent)
             if (xml_parent) {
                 xmlNode *node = pcmk__xe_create(xml_parent, PCMK_XE_NODE);
 
-                pcmk__xe_set_id(node, "%u", nodeid);
+                crm_xml_add_ll(node, PCMK_XA_ID, (long long) nodeid);
                 crm_xml_add(node, PCMK_XA_UNAME, name);
             }
         }

lib/common/ipc_client.c
@@ -764,7 +764,7 @@ create_purge_node_request(const pcmk_ipc_api_t *api, const char *node_name,
             request = create_request(CRM_OP_RM_NODE_CACHE, NULL, NULL,
                                      pcmk_ipc_name(api, false), client, NULL);
             if (nodeid > 0) {
-                pcmk__xe_set_id(request, "%lu", (unsigned long) nodeid);
+                crm_xml_add_ll(request, PCMK_XA_ID, (long long) nodeid);
             }
             crm_xml_add(request, PCMK_XA_UNAME, node_name);
             break;

lib/common/ipc_controld.c
@@ -9,9 +9,12 @@
 
 #include <crm_internal.h>
 
-#include <stdio.h>
-#include <stdbool.h>
 #include <errno.h>
+#include <inttypes.h>                   // PRIu32
+#include <stdbool.h>
+#include <stdint.h>                     // uint32_t
+#include <stdio.h>
+
 #include <libxml/tree.h>
 
 #include <crm/crm.h>
@@ -417,7 +420,7 @@ pcmk_controld_api_node_info(pcmk_ipc_api_t *api, uint32_t nodeid)
         return EINVAL;
     }
     if (nodeid > 0) {
-        pcmk__xe_set_id(request, "%lu", (unsigned long) nodeid);
+        crm_xml_add_ll(request, PCMK_XA_ID, nodeid);
     }
 
     rc = send_controller_request(api, request, true);

tools/crm_node.c
@@ -549,7 +549,7 @@ remove_from_section(cib_t *cib, const char *element, const char *section,
 
     crm_xml_add(xml, PCMK_XA_UNAME, node_name);
     if (node_id > 0) {
-        pcmk__xe_set_id(xml, "%ld", node_id);
+        crm_xml_add_ll(xml, PCMK_XA_ID, node_id);
     }
 
     rc = cib->cmds->remove(cib, section, xml, cib_transaction);
@@ -689,7 +689,7 @@ purge_node_from_fencer(const char *node_name, long node_id)
     cmd = create_request(CRM_OP_RM_NODE_CACHE, NULL, NULL, "stonith-ng",
                          crm_system_name, NULL);
     if (node_id > 0) {
-        pcmk__xe_set_id(cmd, "%ld", node_id);
+        crm_xml_add_ll(cmd, PCMK_XA_ID, node_id);
     }
     crm_xml_add(cmd, PCMK_XA_UNAME, node_name);
 