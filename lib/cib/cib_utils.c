ClusterLabs/pacemaker

commit 324e241d9d9666dfe6543ddf88d965b12d5a164f

Low: libcib: always use current values when unpacking config
Previously, cib_read_config() called unpack_instance_attributes() with
overwrite=FALSE. This meant that changes to an option would not take effect
unless the option was not set before.

The only significant use of cib_read_config() was in cib_acl_enabled(), which
used a new, empty hash table for every call, so the issue didn't matter.

The cib daemon also used cib_read_config() to maintain a global config_hash,
which was affected by the issue, but didn't matter because it was never used.
This change will allow config_hash to be used.

lib/cib/cib_utils.c
@@ -743,7 +743,7 @@ cib_read_config(GHashTable * options, xmlNode * current_cib)
     config = get_object_root(XML_CIB_TAG_CRMCONFIG, current_cib);
     if (config) {
         unpack_instance_attributes(current_cib, config, XML_CIB_TAG_PROPSET, NULL, options,
-                                   CIB_OPTIONS_FIRST, FALSE, now);
+                                   CIB_OPTIONS_FIRST, TRUE, now);
     }
 
     verify_cib_options(options);

commit 5d91cf665bde4e095ce0d02f0ddefe82a0d99011

Fix: libcib: Don't overwrite property values for CIB config hash
This is a regression introduced by commit 324e241d. However, it makes a
difference only when there are multiple properties with the same name in
the same cluster_property_set. This is allowed, but there's no valid use
case for it.

Currently, the value of the last property with a given name in a given
cluster_property_set, or in a group of cluster_property_sets with equal
scores, is applied. With this commit, the first one is applied instead.

The reasoning in the commit message for 324e241d seems to be mistaken:
the call to g_hash_table_remove_all() before pe_unpack_nvpairs() ensures
that the options table is empty, so there is nothing to overwrite.
sort_pairs() ensures that the cluster_property_sets that we're about to
process are processed in the correct order based on scores.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_utils.c
@@ -809,9 +809,9 @@ cib_read_config(GHashTable * options, xmlNode * current_cib)
 
     config = pcmk_find_cib_element(current_cib, PCMK_XE_CRM_CONFIG);
     if (config) {
-        pe_unpack_nvpairs(current_cib, config, PCMK_XE_CLUSTER_PROPERTY_SET,
-                          NULL, options, PCMK_VALUE_CIB_BOOTSTRAP_OPTIONS, TRUE,
-                          now, NULL);
+        pe_unpack_nvpairs(NULL, config, PCMK_XE_CLUSTER_PROPERTY_SET, NULL,
+                          options, PCMK_VALUE_CIB_BOOTSTRAP_OPTIONS, FALSE, now,
+                          NULL);
     }
 
     pcmk__validate_cluster_options(options);