33db267e6966748f0c22c35217ab2e53d442bfc6
Ken Gaillot <kgaillot@redhat.com>
2024-07-16 16:04:34 -0500

    Refactor: liblrmd: use pcmk__parse_server() in remote_proxy_new()
    ... instead of pcmk__message_name()

--- lib/lrmd/proxy_common.c
+++ lib/lrmd/proxy_common.c
@@ -191,8 +191,8 @@ remote_proxy_new(lrmd_t *lrmd, struct ipc_client_callbacks *proxy_callbacks,
     proxy->session_id = strdup(session_id);
     proxy->lrm = lrmd;
 
-    if (!strcmp(pcmk__message_name(crm_system_name), CRM_SYSTEM_CRMD)
-        && !strcmp(pcmk__message_name(channel), CRM_SYSTEM_CRMD)) {
+    if ((pcmk__parse_server(crm_system_name) == pcmk_ipc_controld)
+        && (pcmk__parse_server(channel) == pcmk_ipc_controld)) {
         // The controller doesn't need to connect to itself
         proxy->is_local = TRUE;
 
