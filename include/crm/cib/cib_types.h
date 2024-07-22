1d9348050df8a5c90921449a319683fe82d70dbd
Ken Gaillot <kgaillot@redhat.com>
2024-07-22 17:42:39 -0500

    API: libcib: drop cib_api_operations_t quit() member
    This breaks API backward compatibility
    
    Ref T657

--- include/crm/cib/cib_types.h
+++ include/crm/cib/cib_types.h
@@ -175,9 +175,6 @@ typedef struct cib_api_operations_s {
                    int call_options);
     int (*erase) (cib_t *cib, xmlNode **output_data, int call_options);
 
-    //! \deprecated This method is not implemented and should not be used
-    int (*quit) (cib_t *cib, int call_options);
-
     int (*register_notification) (cib_t *cib, const char *callback,
                                   int enabled);
     gboolean (*register_callback) (cib_t *cib, int call_id, int timeout,
