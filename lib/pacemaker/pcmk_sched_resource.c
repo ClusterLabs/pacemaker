1246f8d355d8e53956b6d2dc1e134b04756f5396
Ken Gaillot <kgaillot@redhat.com>
2024-07-30 17:07:14 -0500

    Log: various: ensure there are spaces around CRM_XS

--- lib/pacemaker/pcmk_sched_resource.c
+++ lib/pacemaker/pcmk_sched_resource.c
@@ -601,7 +601,7 @@ pcmk__threshold_reached(pcmk_resource_t *rsc, const pcmk_node_t *node,
 
     if (remaining_tries <= 0) {
         pcmk__sched_warn("%s cannot run on %s due to reaching migration "
-                         "threshold (clean up resource to allow again)"
+                         "threshold (clean up resource to allow again) "
                          CRM_XS " failures=%d "
                          PCMK_META_MIGRATION_THRESHOLD "=%d",
                          rsc_to_ban->id, pcmk__node_name(node), fail_count,
