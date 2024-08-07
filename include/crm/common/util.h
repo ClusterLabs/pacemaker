840b1052406b474fb4ac6fe3b592c673597b7a06
Ken Gaillot <kgaillot@redhat.com>
2024-08-07 17:26:14 -0500

    Doc: libcrmcommon: remove stray declaration
    ... so the one marked "deprecated" in util_compat.h is used

--- include/crm/common/util.h
+++ include/crm/common/util.h
@@ -51,8 +51,6 @@ int crm_str_to_boolean(const char *s, int *ret);
 // NOTE: sbd (as of at least 1.5.2) uses this
 long long crm_get_msec(const char *input);
 
-char * crm_strip_trailing_newline(char *str);
-
 // NOTE: sbd (as of at least 1.5.2) uses this
 char *crm_strdup_printf(char const *format, ...) G_GNUC_PRINTF(1, 2);
 
