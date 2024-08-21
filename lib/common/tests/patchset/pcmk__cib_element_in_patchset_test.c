6260da0661f056445be4d1b4ccd6b1f7c84a2d08
Chris Lumens <clumens@redhat.com>
2024-08-21 13:47:02 -0400

    Test: libcrmcommon: Add the unit test skeleton for pcmk__cib_element_in_patchset.
    For the moment, all this has is a test for a parameter being NULL.  More
    to come.
    
    Related T865

---
+++ lib/common/tests/patchset/pcmk__cib_element_in_patchset_test.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU General Public License version 2
+ * or later (GPLv2+) WITHOUT ANY WARRANTY.
+ */
+
+#include <crm_internal.h>
+
+#include <crm/common/unittest_internal.h>
+#include <crm/common/xml.h>
+#include <crm/common/xml_internal.h>
+
+static void
+empty_arguments(void **state) {
+    xmlNode *missing = NULL;
+
+    pcmk__assert_asserts(pcmk__cib_element_in_patchset(NULL, PCMK_XE_NODES));
+
+    missing = pcmk__xml_parse("<diff format=\"2\"/>");
+    assert_false(pcmk__cib_element_in_patchset(missing, NULL));
+    xmlFreeNode(missing);
+}
+
+PCMK__UNIT_TEST(NULL, NULL,
+                cmocka_unit_test(empty_arguments))
