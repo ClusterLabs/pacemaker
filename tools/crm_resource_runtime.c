ClusterLabs/pacemaker

commit 05e8f1b98a9bc6b4485df2a298a687ecf7faa41e

Refactor: libcrmcommon: make pcmk_update_configured_schema() return a standard code
The callers can now just return the code directly (which does change the code
in a few cases, but for the better)

tools/crm_resource_runtime.c
@@ -1237,19 +1237,16 @@ static void display_list(pcmk__output_t *out, GList *items, const char *tag)
  * \return Standard Pacemaker return code
  * \note On success, caller is responsible for freeing memory allocated for
  *       scheduler->now.
- * \todo This follows the example of other callers of
- *       pcmk_update_configured_schema() and returns ENOKEY ("Required key not
- *       available") if that fails, but perhaps pcmk_rc_schema_validation would
- *       be better in that case.
  */
 int
 update_scheduler_input(pcmk_scheduler_t *scheduler, xmlNode **xml)
 {
-    if (!pcmk_update_configured_schema(xml, false)) {
-        return ENOKEY;
+    int rc = pcmk_update_configured_schema(xml, false);
+
+    if (rc == pcmk_rc_ok) {
+        scheduler->input = *xml;
+        scheduler->now = crm_time_new(NULL);
     }
-    scheduler->input = *xml;
-    scheduler->now = crm_time_new(NULL);
     return pcmk_rc_ok;
 }
 

daemons/execd/cts-exec-helper.c
@@ -461,9 +461,9 @@ generate_params(void)
     if (rc != pcmk_rc_ok) {
         return rc;
     }
-    if (!pcmk_update_configured_schema(&cib_xml_copy, false)) {
-        crm_err("Could not update CIB");
-        return pcmk_rc_cib_corrupt;
+    rc = pcmk_update_configured_schema(&cib_xml_copy, false);
+    if (rc != pcmk_rc_ok) {
+        return rc;
     }
 
     // Calculate cluster status

daemons/schedulerd/schedulerd_messages.c
@@ -81,7 +81,7 @@ handle_pecalc_request(pcmk__request_t *request)
     digest = calculate_xml_versioned_digest(xml_data, FALSE, FALSE,
                                             CRM_FEATURE_SET);
     converted = pcmk__xml_copy(NULL, xml_data);
-    if (!pcmk_update_configured_schema(&converted, true)) {
+    if (pcmk_update_configured_schema(&converted, true) != pcmk_rc_ok) {
         scheduler->graph = pcmk__xe_create(NULL, PCMK__XE_TRANSITION_GRAPH);
         crm_xml_add_int(scheduler->graph, "transition_id", 0);
         crm_xml_add_int(scheduler->graph, PCMK_OPT_CLUSTER_DELAY, 0);

include/crm/common/schemas.h
@@ -23,7 +23,7 @@ extern "C" {
  * \ingroup core
  */
 
-bool pcmk_update_configured_schema(xmlNode **xml, bool to_logs);
+int pcmk_update_configured_schema(xmlNode **xml, bool to_logs);
 
 #ifdef __cplusplus
 }

lib/common/schemas.c
@@ -1243,12 +1243,12 @@ pcmk__update_schema(xmlNode **xml, const char *max_schema_name, bool transform,
  * \param[in]     to_logs  If false, certain validation errors will be
  *                         sent to stderr rather than logged
  *
- * \return true if XML was successfully updated, otherwise false
+ * \return Standard Pacemaker return code
  */
-bool
+int
 pcmk_update_configured_schema(xmlNode **xml, bool to_logs)
 {
-    bool rc = true;
+    int rc = pcmk_rc_ok;
     char *original_schema_name = NULL;
 
     // @COMPAT Not specifying a schema name is deprecated since 2.1.8
@@ -1257,6 +1257,8 @@ pcmk_update_configured_schema(xmlNode **xml, bool to_logs)
     int orig_version = -1;
     pcmk__schema_t *x_0_schema = pcmk__find_x_0_schema()->data;
     GList *entry = NULL;
+
+    CRM_CHECK(xml != NULL, return EINVAL);
 
     original_schema_name = crm_element_value_copy(*xml, PCMK_XA_VALIDATE_WITH);
     pcmk__warn_if_schema_deprecated(original_schema_name);
@@ -1326,7 +1328,7 @@ pcmk_update_configured_schema(xmlNode **xml, bool to_logs)
 
             free_xml(converted);
             converted = NULL;
-            rc = false;
+            rc = pcmk_rc_transform_failed;
 
         } else {
             // Updated configuration schema is acceptable
@@ -1718,7 +1720,7 @@ validate_xml_verbose(const xmlNode *xml_blob)
 gboolean
 cli_config_update(xmlNode **xml, int *best_version, gboolean to_logs)
 {
-    bool rc = pcmk_update_configured_schema(xml, to_logs);
+    int rc = pcmk_update_configured_schema(xml, to_logs);
 
     if (best_version != NULL) {
         const char *name = crm_element_value(*xml, PCMK_XA_VALIDATE_WITH);
@@ -1732,7 +1734,7 @@ cli_config_update(xmlNode **xml, int *best_version, gboolean to_logs)
             *best_version = (schema == NULL)? -1 : schema->schema_index;
         }
     }
-    return rc? TRUE: FALSE;
+    return (rc == pcmk_rc_ok)? TRUE: FALSE;
 }
 
 // LCOV_EXCL_STOP

lib/pacemaker/pcmk_simulate.c
@@ -345,7 +345,7 @@ profile_file(const char *xml_file, long long repeat,
         pcmk__xe_create(cib_object, PCMK_XE_STATUS);
     }
 
-    if (!pcmk_update_configured_schema(&cib_object, false)) {
+    if (pcmk_update_configured_schema(&cib_object, false) != pcmk_rc_ok) {
         free_xml(cib_object);
         return;
     }

lib/pacemaker/pcmk_status.c
@@ -86,10 +86,10 @@ pcmk__output_cluster_status(pcmk__output_t *out, stonith_t *stonith, cib_t *cib,
 
     int rc = pcmk_rc_ok;
 
-    if (!pcmk_update_configured_schema(&cib_copy, false)) {
+    rc = pcmk_update_configured_schema(&cib_copy, false);
+    if (rc != pcmk_rc_ok) {
         cib__clean_up_connection(&cib);
         free_xml(cib_copy);
-        rc = pcmk_rc_schema_validation;
         out->err(out, "Upgrade failed: %s", pcmk_rc_str(rc));
         return rc;
     }

lib/pacemaker/pcmk_verify.c
@@ -69,13 +69,14 @@ pcmk__verify(pcmk_scheduler_t *scheduler, pcmk__output_t *out, xmlNode *cib_obje
         crm_config_error = TRUE;
         rc = pcmk_rc_schema_validation;
         goto verify_done;
+    }
 
-    } else if (!pcmk_update_configured_schema(&cib_object, false)) {
+    rc = pcmk_update_configured_schema(&cib_object, false);
+    if (rc != pcmk_rc_ok) {
         crm_config_error = TRUE;
         out->err(out, "The cluster will NOT be able to use this configuration.\n"
                  "Please manually update the configuration to conform to the %s syntax.",
                  pcmk__highest_schema_name());
-        rc = pcmk_rc_schema_validation;
         goto verify_done;
     }
 

commit b1c84f7c35d1e2f0859f47aca3fb7a953822cdba

Fix: tools: crm_resource returns error on failed schema upgrade
This fixes a regression introduced by 05e8f1b9 in 2.1.8.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

tools/crm_resource_runtime.c
@@ -1331,7 +1331,7 @@ update_scheduler_input(pcmk_scheduler_t *scheduler, xmlNode **xml)
         scheduler->input = *xml;
         scheduler->priv->now = crm_time_new(NULL);
     }
-    return pcmk_rc_ok;
+    return rc;
 }
 
 /*!