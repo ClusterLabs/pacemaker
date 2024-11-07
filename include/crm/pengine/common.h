8513e9ae43d6fe7a7b1a708fd75ea9bd49a7bd5a
Ken Gaillot <kgaillot@redhat.com>
2024-11-07 15:15:23 -0600

    API: libpe_rules,libpe_status: deprecate struct pe_re_match_data and pe_re_match_data_t

--- include/crm/pengine/common.h
+++ include/crm/pengine/common.h
@@ -15,20 +15,6 @@
 #  include <crm/common/iso8601.h>
 #  include <crm/common/scheduler.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef struct pe_re_match_data {
-    char *string;
-    int nregs;
-    regmatch_t *pmatch;
-} pe_re_match_data_t;
-
-#ifdef __cplusplus
-}
-#endif
-
 #if !defined(PCMK_ALLOW_DEPRECATED) || (PCMK_ALLOW_DEPRECATED == 1)
 #include <crm/pengine/common_compat.h>
 #endif
