57ea681ccdc57bb08be815bf5eae68cfb29704da
Ken Gaillot <kgaillot@redhat.com>
2024-12-09 12:15:19 -0600

    Log: executor: avoid misleading log when cancelling a recurring monitor

--- daemons/execd/execd_commands.c
+++ daemons/execd/execd_commands.c
@@ -222,13 +222,19 @@ log_finished(const lrmd_cmd_t *cmd, int exec_time_ms, int queue_time_ms)
     if (cmd->last_pid != 0) {
         g_string_append_printf(str, ", PID %d", cmd->last_pid);
     }
-    if (cmd->result.execution_status == PCMK_EXEC_DONE) {
-        g_string_append_printf(str, ") exited with status %d",
-                               cmd->result.exit_status);
-    } else {
-        pcmk__g_strcat(str, ") could not be executed: ",
-                       pcmk_exec_status_str(cmd->result.execution_status),
-                       NULL);
+    switch (cmd->result.execution_status) {
+        case PCMK_EXEC_DONE:
+            g_string_append_printf(str, ") exited with status %d",
+                                   cmd->result.exit_status);
+            break;
+        case PCMK_EXEC_CANCELLED:
+            g_string_append_printf(str, ") cancelled");
+            break;
+        default:
+            pcmk__g_strcat(str, ") could not be executed: ",
+                           pcmk_exec_status_str(cmd->result.execution_status),
+                           NULL);
+            break;
     }
     if (cmd->result.exit_reason != NULL) {
         pcmk__g_strcat(str, " (", cmd->result.exit_reason, ")", NULL);
