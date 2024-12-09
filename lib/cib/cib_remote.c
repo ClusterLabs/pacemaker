813e25d5134f26972a2acd296ebe2488b558c98b
Chris Lumens <clumens@redhat.com>
2024-12-09 16:42:54 -0500

    Refactor: libcrmcommon: pcmk__x509_enabled doesn't need an arg.
    For Pacemaker Remote connections, both ends are daemons and will
    therefore read the TLS environment variables out of the sysconfig file.
    However, one end is a client and one end is a server.  So using that to
    decide where to read the variables is not helpful.
    
    Instead, just try both locations regardless of which end of the
    connection we're checking.  If the PCMK_ version of a variable isn't
    set, try the CIB_ version instead.

--- lib/cib/cib_remote.c
+++ lib/cib/cib_remote.c
@@ -375,7 +375,7 @@ cib_tls_signon(cib_t *cib, pcmk__remote_t *connection, gboolean event_channel)
     }
 
     if (private->encrypted) {
-        bool use_cert = pcmk__x509_enabled(false);
+        bool use_cert = pcmk__x509_enabled();
         int tls_rc = GNUTLS_E_SUCCESS;
 
         rc = pcmk__init_tls(&tls, false, use_cert ? GNUTLS_CRD_CERTIFICATE : GNUTLS_CRD_ANON);
