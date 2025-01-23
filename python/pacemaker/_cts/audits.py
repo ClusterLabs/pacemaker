3eb9450fa20b06b4e1aaa49992e32fd87e3ecb6d
Chris Lumens <clumens@redhat.com>
2025-01-23 15:43:26 -0500

    Refactor: python: Reduce code duplication looking for core dumps.

--- python/pacemaker/_cts/audits.py
+++ python/pacemaker/_cts/audits.py
@@ -264,30 +264,40 @@ class FileAudit(ClusterAudit):
         self.known = []
         self.name = "FileAudit"
 
+    def _output_has_core(self, output, node):
+        found = False
+
+        for line in output:
+            line = line.strip()
+
+            if line in self.known:
+                continue
+
+            found = True
+            self.known.append(line)
+            self._cm.log(f"Warning: core file on {node}: {line}")
+
+        return found
+
+    def _find_core_on_fs(self, node, path):
+        """Check for core dumps on the given node, under the given path."""
+        (_, lsout) = self._cm.rsh(node, f"ls -al {path} | grep core.[0-9]", verbose=1)
+        return self._output_has_core(lsout, node)
+
     def __call__(self):
         """Perform the audit action."""
         passed = True
 
         self._cm.ns.wait_for_all_nodes(self._cm.env["nodes"])
+
         for node in self._cm.env["nodes"]:
+            found = self._find_core_on_fs(node, "/var/lib/pacemaker/cores/*", "Pacemaker")
+            if found:
+                passed = False
 
-            (_, lsout) = self._cm.rsh(node, "ls -al /var/lib/pacemaker/cores/* | grep core.[0-9]", verbose=1)
-            for line in lsout:
-                line = line.strip()
-
-                if line not in self.known:
-                    passed = False
-                    self.known.append(line)
-                    self._cm.log(f"Warning: Pacemaker core file on {node}: {line}")
-
-            (_, lsout) = self._cm.rsh(node, "ls -al /var/lib/corosync | grep core.[0-9]", verbose=1)
-            for line in lsout:
-                line = line.strip()
-
-                if line not in self.known:
-                    passed = False
-                    self.known.append(line)
-                    self._cm.log(f"Warning: Corosync core file on {node}: {line}")
+            found = self._find_core_on_fs(node, "/var/lib/corosync", "Corosync")
+            if found:
+                passed = False
 
             if self._cm.expected_status.get(node) == "down":
                 clean = False
