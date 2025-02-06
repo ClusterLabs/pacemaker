4cd3841567d5d43267af7bfe44bf2f23ca02eaa0
Reid Wahl <nrwahl@protonmail.com>
2025-02-05 16:42:44 -0800

    Refactor: libcrmservice: New services__copy_result() function
    Ref T25
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/services/services.c
+++ lib/services/services.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 the Pacemaker project contributors
+ * Copyright 2010-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -1224,6 +1224,20 @@ services__set_result(svc_action_t *action, int agent_status,
         free(action->opaque->exit_reason);
         action->opaque->exit_reason = (reason == NULL)? NULL : strdup(reason);
     }
+}
+
+/*!
+ * \internal
+ * \brief Set a \c pcmk__action_result_t based on a \c svc_action_t
+ *
+ * \param[in]     action  Service action whose result to copy
+ * \param[in,out] result  Action result object to set
+ */
+void
+services__copy_result(const svc_action_t *action, pcmk__action_result_t *result)
+{
+    pcmk__set_result(result, action->rc, action->status,
+                     action->opaque->exit_reason);
 }
 
 /*!
