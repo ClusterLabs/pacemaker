fc86463a18b0f8f7311d54ff3d2b4d785c0ee552
Chris Lumens <clumens@redhat.com>
2025-02-19 10:13:12 -0500

    Refactor: libcrmcommon: pcmk__ipc_prepare_iov no longer takes a size.
    We no longer support specifying the buffer size, so the max send size is
    now always 128k.  It no longer needs to be passed as a parameter.

--- daemons/based/based_notify.c
+++ daemons/based/based_notify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 the Pacemaker project contributors
+ * Copyright 2004-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -110,7 +110,7 @@ cib_notify_send(const xmlNode *xml)
     struct cib_notification_s update;
 
     ssize_t bytes = 0;
-    int rc = pcmk__ipc_prepare_iov(0, xml, 0, &iov, &bytes);
+    int rc = pcmk__ipc_prepare_iov(0, xml, &iov, &bytes);
 
     if (rc == pcmk_rc_ok) {
         update.msg = xml;
