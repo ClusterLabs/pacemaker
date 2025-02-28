be5609993c3c77de02f53f19350d70dffc48fa1e
Chris Lumens <clumens@redhat.com>
2025-02-28 12:44:11 -0500

    Feature: daemons: Convert based to support multipart IPC messages.
    This is basically how every daemon is going to work.  The daemon needs
    to delay calling pcmk__client_data2xml until it has the complete
    message.  The hard work of reassembling the message and determining
    whether it's a multipart message in the first place is already done
    elsewhere.

--- daemons/based/based_notify.c
+++ daemons/based/based_notify.c
@@ -116,22 +116,32 @@ cib_notify_send(const xmlNode *xml)
     GString *iov_buffer = NULL;
     ssize_t bytes = 0;
     int rc = pcmk_rc_ok;
+    uint16_t index = 0;
 
     iov_buffer = g_string_sized_new(1024);
     pcmk__xml_string(xml, 0, iov_buffer, 0);
-    rc = pcmk__ipc_prepare_iov(0, iov_buffer, 0, &iov, &bytes);
 
-    if (rc == pcmk_rc_ok) {
+    do {
+        rc = pcmk__ipc_prepare_iov(0, iov_buffer, index, &iov, &bytes);
+
+        if ((rc != pcmk_rc_ok) && (rc != pcmk_rc_ipc_more)) {
+            crm_notice("Could not notify clients: %s " QB_XS " rc=%d",
+                       pcmk_rc_str(rc), rc);
+            break;
+        }
+
         update.msg = xml;
         update.iov = iov;
         update.iov_size = bytes;
         pcmk__foreach_ipc_client(cib_notify_send_one, &update);
         pcmk_free_ipc_event(iov);
 
-    } else {
-        crm_notice("Could not notify clients: %s " QB_XS " rc=%d",
-                   pcmk_rc_str(rc), rc);
-    }
+        if (rc == pcmk_rc_ok) {
+            break;
+        }
+
+        index++;
+    } while (true);
 
     g_string_free(iov_buffer, TRUE);
 }
