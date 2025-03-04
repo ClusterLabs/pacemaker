1b93694cc5ecad5ca1ae9706bf1866ef9f5bb638
Reid Wahl <nrwahl@protonmail.com>
2025-03-04 04:06:05 -0800

    Refactor: tools: New crm_resource.c:handle_list_active_ops()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- tools/crm_resource.c
+++ tools/crm_resource.c
@@ -1572,6 +1572,15 @@ handle_get_param(pcmk_resource_t *rsc)
     return rc;
 }
 
+static int
+handle_list_active_ops(const pcmk_node_t *node)
+{
+    const char *node_name = (node != NULL)? node->priv->name : NULL;
+
+    return cli_resource_print_operations(options.rsc_id, node_name, true,
+                                         scheduler);
+}
+
 static GOptionContext *
 build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
     GOptionContext *context = NULL;
@@ -2007,12 +2016,7 @@ main(int argc, char **argv)
             break;
 
         case cmd_list_active_ops:
-            {
-                const char *node_name = (node != NULL)? node->priv->name : NULL;
-
-                rc = cli_resource_print_operations(options.rsc_id, node_name,
-                                                   true, scheduler);
-            }
+            rc = handle_list_active_ops(node);
             break;
 
         case cmd_list_all_ops:
