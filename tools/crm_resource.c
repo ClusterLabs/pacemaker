126904d6ac01dfccc949d5ea3d6e4f9102d2ba4f
Reid Wahl <nrwahl@protonmail.com>
2025-03-04 04:23:29 -0800

    Refactor: tools: New crm_resource.c:handle_list_providers()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- tools/crm_resource.c
+++ tools/crm_resource.c
@@ -1604,6 +1604,12 @@ handle_list_options(void)
     }
 }
 
+static int
+handle_list_providers(void)
+{
+    return pcmk__list_providers(out, options.agent_spec);
+}
+
 static GOptionContext *
 build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
     GOptionContext *context = NULL;
@@ -1988,7 +1994,7 @@ main(int argc, char **argv)
             break;
 
         case cmd_list_providers:
-            rc = pcmk__list_providers(out, options.agent_spec);
+            rc = handle_list_providers();
             break;
 
         case cmd_metadata:
