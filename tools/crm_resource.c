8af3ae6321e261fea2e7349212368f6b67c48713
Reid Wahl <nrwahl@protonmail.com>
2025-03-04 04:21:40 -0800

    Refactor: tools: list_options() -> handle_list_options() in crm_resource
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- tools/crm_resource.c
+++ tools/crm_resource.c
@@ -939,26 +939,6 @@ initialize_scheduler_data(xmlNode **cib_xml_orig)
     return pcmk_rc_ok;
 }
 
-static void
-list_options(void)
-{
-    switch (options.opt_list) {
-        case pcmk__opt_fencing:
-            exit_code = pcmk_rc2exitc(pcmk__list_fencing_params(out,
-                                                                options.all));
-            break;
-        case pcmk__opt_primitive:
-            exit_code = pcmk_rc2exitc(pcmk__list_primitive_meta(out,
-                                                                options.all));
-            break;
-        default:
-            exit_code = CRM_EX_SOFTWARE;
-            g_set_error(&error, PCMK__EXITC_ERROR, exit_code,
-                        "BUG: Invalid option list type");
-            break;
-    }
-}
-
 static int
 refresh(pcmk__output_t *out, const pcmk_node_t *node)
 {
@@ -1608,6 +1588,22 @@ handle_list_instances(void)
     return pcmk_rc_ok;
 }
 
+static int
+handle_list_options(void)
+{
+    switch (options.opt_list) {
+        case pcmk__opt_fencing:
+            return pcmk__list_fencing_params(out, options.all);
+        case pcmk__opt_primitive:
+            return pcmk__list_primitive_meta(out, options.all);
+        default:
+            exit_code = CRM_EX_SOFTWARE;
+            g_set_error(&error, PCMK__EXITC_ERROR, exit_code,
+                        "BUG: Invalid option list type");
+            return pcmk_rc_ok;
+    }
+}
+
 static GOptionContext *
 build_arg_context(pcmk__common_args_t *args, GOptionGroup **group) {
     GOptionContext *context = NULL;
@@ -1976,7 +1972,7 @@ main(int argc, char **argv)
             break;
 
         case cmd_list_options:
-            list_options();
+            rc = handle_list_options();
             break;
 
         case cmd_list_alternatives:
