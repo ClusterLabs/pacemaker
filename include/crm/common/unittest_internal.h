71783ea27639af1998c0045abe87219e9e4fee24
Reid Wahl <nrwahl@protonmail.com>
2025-03-10 15:08:13 -0700

    Refactor: various: Drop includes of stddef.h
    Nothing seems to actually depend on it or include from it.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/unittest_internal.h
+++ include/crm/common/unittest_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the Pacemaker project contributors
+ * Copyright 2022-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -12,7 +12,6 @@
 
 #include <signal.h>
 #include <stdarg.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <setjmp.h>
 #include <sys/resource.h>
