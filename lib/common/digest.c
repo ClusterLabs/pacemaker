05a1383ebbe699800d89214e99e6b54deb208aa8
Reid Wahl <nrwahl@protonmail.com>
2025-03-17 13:46:07 -0700

    Refactor: libcrmcommon: pcmk__verify_digest() argument is now const
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/digest.c
+++ lib/common/digest.c
@@ -197,7 +197,7 @@ pcmk__digest_xml(const xmlNode *xml, bool filter)
  * \return true if digests match, false on mismatch or error
  */
 bool
-pcmk__verify_digest(xmlNode *input, const char *expected)
+pcmk__verify_digest(const xmlNode *input, const char *expected)
 {
     char *calculated = NULL;
     bool passed;
