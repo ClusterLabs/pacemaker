ClusterLabs/pacemaker

commit bf7ffcdb988441bdf7bb6c2ff48ea3c1653aaf66

Low: scheduler: create local node after unpacking everything
Previously, the scheduler create a fake node for the local host name if
one didn't already exist after unpacking the nodes section. However, a
Pacemaker Remote node can be created after unpacking the resources and
status sections, so move the creation after all that.

This fixes a regression introduced by fd8fb8677a (not in a release)
where crm_mon on a Pacemaker Remote node showed the local node as an
offline cluster node.

lib/pengine/status.c
@@ -192,6 +192,15 @@ cluster_status(pcmk_scheduler_t * scheduler)
                   scheduler->priv->blocked_resources);
     }
 
+    if ((scheduler->priv->local_node_name != NULL)
+        && (pcmk_find_node(scheduler,
+                           scheduler->priv->local_node_name) == NULL)) {
+        crm_info("Creating a fake local node for %s",
+                 scheduler->priv->local_node_name);
+        pe_create_node(scheduler->priv->local_node_name,
+                       scheduler->priv->local_node_name, NULL, 0, scheduler);
+    }
+
     pcmk__set_scheduler_flags(scheduler, pcmk__sched_have_status);
     return TRUE;
 }

lib/pengine/unpack.c
@@ -656,14 +656,6 @@ unpack_nodes(xmlNode *xml_nodes, pcmk_scheduler_t *scheduler)
         }
     }
 
-    if ((scheduler->priv->local_node_name != NULL)
-        && (pcmk_find_node(scheduler,
-                           scheduler->priv->local_node_name) == NULL)) {
-        crm_info("Creating a fake local node");
-        pe_create_node(scheduler->priv->local_node_name,
-                       scheduler->priv->local_node_name, NULL, 0, scheduler);
-    }
-
     return TRUE;
 }
 

commit 39aab7b3e87cc9172ab05d3a9b0c82bf4fb6400d

Fix: libpe_status: Add fake local node to all resources' allowed_nodes
This fixes a regression introduced by bf7ffcd. As of that commit, the
fake local node is created after all resources have been unpacked. So it
doesn't get added to resources' allowed_nodes tables.

This prevents registration of fencing devices when the fencer receives a
CIB diff that doesn't contain the local node. For example, the user may
have replaced the CIB with a boilerplate configuration that has an empty
nodes section.

See the following pull requests from Hideo Yamauchi and their
discussions:
https://github.com/ClusterLabs/pacemaker/pull/3849
https://github.com/ClusterLabs/pacemaker/pull/3852

Thanks to Hideo for the report and finding the cause.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/pengine/status.c
@@ -167,10 +167,25 @@ cluster_status(pcmk_scheduler_t * scheduler)
     if ((scheduler->priv->local_node_name != NULL)
         && (pcmk_find_node(scheduler,
                            scheduler->priv->local_node_name) == NULL)) {
-        crm_info("Creating a fake local node for %s",
-                 scheduler->priv->local_node_name);
-        pe_create_node(scheduler->priv->local_node_name,
-                       scheduler->priv->local_node_name, NULL, 0, scheduler);
+        const char *local_node_name = scheduler->priv->local_node_name;
+        pcmk_node_t *node = NULL;
+
+        crm_info("Creating a fake local node for %s", local_node_name);
+        node = pe_create_node(local_node_name, local_node_name, NULL, 0,
+                              scheduler);
+
+        for (GList *iter = scheduler->priv->resources; iter != NULL;
+             iter = iter->next) {
+            pcmk_resource_t *rsc = iter->data;
+            pcmk_node_t *local = g_hash_table_lookup(rsc->priv->allowed_nodes,
+                                                     local_node_name);
+
+            if (local == NULL) {
+                local = pe__copy_node(node);
+                g_hash_table_insert(rsc->priv->allowed_nodes,
+                                    (gpointer) local->priv->id, local);
+            }
+        }
     }
 
     pcmk__set_scheduler_flags(scheduler, pcmk__sched_have_status);