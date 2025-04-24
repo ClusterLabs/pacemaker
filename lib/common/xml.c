460f49d9803ad9315d3f9f953bf4f0bee66ee8c5
Reid Wahl <nrwahl@protonmail.com>
2025-04-24 11:10:33 -0700

    Refactor: libcrmcommon: Drop pcmk__xml_cleanup()
    xmlCleanupParser() is supposed to be called only immediately before
    program exit (or more precisely, when the program is completely finished
    using libxml2). So even though we usually isolate libxml2 functions to
    appropriate source files, I think it makes sense to call this directly
    in pcmk_common_cleanup() alongside qb_log_fini(), as part of cleaning up
    external library memory/state.
    
    This means we also must call it directly in crm_xml_cleanup() for
    backwards compatibility. And at least for now, we call it in the unit
    test cleanup function, since crm_exit()/pcmk_common_cleanup() doesn't
    get called when exiting a unit test.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -18,7 +18,6 @@
 #include <sys/types.h>
 
 #include <glib.h>                       // gboolean, GString
-#include <libxml/parser.h>              // xmlCleanupParser()
 #include <libxml/tree.h>                // xmlNode, etc.
 #include <libxml/xmlstring.h>           // xmlGetUTF8Char()
 
@@ -1439,19 +1438,6 @@ pcmk__xml_init(void)
     }
 }
 
-/*!
- * \internal
- * \brief Tear down the Pacemaker XML environment
- *
- * Destroy schema cache and clean up memory allocated by libxml2.
- */
-void
-pcmk__xml_cleanup(void)
-{
-    pcmk__schema_cleanup();
-    xmlCleanupParser();
-}
-
 char *
 pcmk__xml_artefact_root(enum pcmk__xml_artefact_ns ns)
 {
@@ -1532,6 +1518,8 @@ pcmk__xml_artefact_path(enum pcmk__xml_artefact_ns ns, const char *filespec)
 // Deprecated functions kept only for backward API compatibility
 // LCOV_EXCL_START
 
+#include <libxml/parser.h>              // xmlCleanupParser()
+
 #include <crm/common/xml_compat.h>
 
 xmlNode *
@@ -1557,7 +1545,8 @@ crm_xml_init(void)
 void
 crm_xml_cleanup(void)
 {
-    pcmk__xml_cleanup();
+    pcmk__schema_cleanup();
+    xmlCleanupParser();
 }
 
 void
