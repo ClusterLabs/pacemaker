b0e6544bbf578285918b69ff9c9b35d2c9f54713
Chris Lumens <clumens@redhat.com>
2025-06-09 14:23:53 -0400

    Med: scheduler: Require a cluster option for old remote fencing behavior.
    If the user wants to preserve the old fencing behavior, where a node
    without quorum is allowed to fence remote nodes in the same partition
    even if they've been restarted, they need to add
    fence-remote-without-quorum="true" to their CIB.  Omitting this option
    or setting it to false will get the new remote fencing behavior.

--- lib/pengine/utils.c
+++ lib/pengine/utils.c
@@ -70,7 +70,8 @@ pe_can_fence(const pcmk_scheduler_t *scheduler, const pcmk_node_t *node)
          * node, not when they are actually started (see remote_connection_assigned)
          * so the above test by itself isn't good enough.
          */
-        if (pcmk__is_pacemaker_remote_node(node)) {
+        if (pcmk__is_pacemaker_remote_node(node)
+            && !pcmk_is_set(scheduler->flags, pcmk__sched_fence_remote_no_quorum)) {
             /* If we're on a system without quorum, it's entirely possible that
              * the remote resource was automatically moved to a node on the
              * partition with quorum.  We can't tell that from this node - the
