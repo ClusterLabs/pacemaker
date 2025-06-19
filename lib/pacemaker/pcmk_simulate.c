ClusterLabs/pacemaker

commit 8b9caadbdfe7063ef3d15e54a5d8eca7dcdf8137

Low: libpacemaker: Fix mem leak in pcmk__profile_dir()
Previously, we leaked cib_object in profile_file() if repeat > 1.

Currently the only caller is crm_simulate, and we know the scheduler
object has no relevant state at call time. Thus we can reset the
scheduler before doing the simulation.

We can avoid unnecessary copies and special case handling by setting
scheduler->input to cib_object and then setting it back to NULL before
resetting the scheduler.

Then we can make sure cib_object is freed unconditionally at the end.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/pacemaker/pcmk_simulate.c
@@ -345,13 +345,11 @@ profile_file(const char *xml_file, long long repeat,
     }
 
     if (pcmk__update_configured_schema(&cib_object, false) != pcmk_rc_ok) {
-        pcmk__xml_free(cib_object);
-        return;
+        goto done;
     }
 
     if (!pcmk__validate_xml(cib_object, NULL, NULL, NULL)) {
-        pcmk__xml_free(cib_object);
-        return;
+        goto done;
     }
 
     if (pcmk_is_set(scheduler->flags, pcmk__sched_output_scores)) {
@@ -362,22 +360,24 @@ profile_file(const char *xml_file, long long repeat,
     }
 
     for (int i = 0; i < repeat; ++i) {
-        xmlNode *input = cib_object;
-
-        if (repeat > 1) {
-            input = pcmk__xml_copy(NULL, cib_object);
-        }
         pcmk_reset_scheduler(scheduler);
-        scheduler->input = input;
+
+        scheduler->input = cib_object;
         pcmk__set_scheduler_flags(scheduler, scheduler_flags);
         set_effective_date(scheduler, false, use_date);
         cluster_status(scheduler);
         pcmk__schedule_actions(NULL, pcmk__sched_none, scheduler);
+
+        // Avoid freeing cib_object in pcmk_reset_scheduler()
+        scheduler->input = NULL;
     }
 
     pcmk_reset_scheduler(scheduler);
     end = clock();
     out->message(out, "profile", xml_file, start, end);
+
+done:
+    pcmk__xml_free(cib_object);
 }
 
 void

commit f40b4fa8e04e8f74c8339410da46f03abf4f83c9

Fix: libpacemaker: Prevent CIB growth during crm_simulate --profile
This fixes a regression introduced by commit 8b9caad. It has not made it
into a release yet but may make it into 3.0.1.

That commit assumed that cluster_status() and pcmk__schedule_actions()
did not modify scheduler->input. However, if scheduler->input contains
guest nodes, then cluster_status() modifies it by creating a primitive
element for each guest node:

cluster_status
-> unpack_remote_nodes()
   -> expand_remote_rsc_meta()
      -> pe_create_remote_xml()

As a result, if we run crm_simulate --profile --repeat=N, the
scheduler->input CIB grows with each repeat. At each iteration, any
guest nodes are unpacked into primitive elements, creating an increasing
number of duplicates.

The increasing input CIB size causes superlinear slowdowns in
crm_simulate --profile as the number of repeats increases.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/pacemaker/pcmk_simulate.c
@@ -405,13 +405,10 @@ profile_file(const char *xml_file, unsigned int repeat,
     for (int i = 0; i < repeat; ++i) {
         pcmk_reset_scheduler(scheduler);
 
-        scheduler->input = cib_object;
+        scheduler->input = pcmk__xml_copy(NULL, cib_object);
         pcmk__set_scheduler_flags(scheduler, flags);
         set_effective_date(scheduler, false, use_date);
         pcmk__schedule_actions(scheduler);
-
-        // Avoid freeing cib_object in pcmk_reset_scheduler()
-        scheduler->input = NULL;
     }
 
     pcmk_reset_scheduler(scheduler);