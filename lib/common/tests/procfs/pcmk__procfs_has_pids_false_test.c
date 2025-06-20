8eb30ac3bc78001e09db33de6499cd30fcf3ffbc
Reid Wahl <nrwahl@protonmail.com>
2025-06-20 02:11:49 -0700

    Refactor: libcrmcommon: Avoid snprintf() in no_pids()/has_pids()
    To avoid the need to error-check snprintf() explicitly, which we have
    not been doing.
    
    Ref T974
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/tests/procfs/pcmk__procfs_has_pids_false_test.c
+++ lib/common/tests/procfs/pcmk__procfs_has_pids_false_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the Pacemaker project contributors
+ * Copyright 2022-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -17,18 +17,15 @@
 #include <string.h>
 #include <errno.h>
 
-
 static void
 no_pids(void **state)
 {
-    char path[PATH_MAX];
-
-    snprintf(path, PATH_MAX, "/proc/%u/exe", getpid());
+    char *exe_path = crm_strdup_printf("/proc/%lld/exe", (long long) getpid());
 
     // Set readlink() errno and link contents (for /proc/PID/exe)
     pcmk__mock_readlink = true;
 
-    expect_string(__wrap_readlink, path, path);
+    expect_string(__wrap_readlink, path, exe_path);
     expect_any(__wrap_readlink, buf);
     expect_value(__wrap_readlink, bufsize, PATH_MAX - 1);
     will_return(__wrap_readlink, ENOENT);
@@ -37,6 +34,7 @@ no_pids(void **state)
     assert_false(pcmk__procfs_has_pids());
 
     pcmk__mock_readlink = false;
+    free(exe_path);
 }
 
 PCMK__UNIT_TEST(NULL, NULL, cmocka_unit_test(no_pids))
