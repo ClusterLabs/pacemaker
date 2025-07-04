ClusterLabs/pacemaker

commit 9cd1ccbb5a3c9ee7fd555333a75f5b76f38de773

Feature: libcrmcommon: Require version info when applying CIB patches
Previously, if a CIB patch was missing source or target version numbers,
we treated the missing numbers as zero when checking whether applying
the patch would be allowed. Now, we treat missing version numbers as an
error and decide that applying the patch is not allowed.

This should not affect typical callers or users. When Pacemaker
generates a CIB patchset (using, for example, crm_diff or
xml_create_patchset()), it adds the version numbers automatically. So
version numbers should be present in a patchset they were removed
manually.

--

This commit adds a new pcmk__xml_patchset_versions() function.

Preserve the body of xml_patch_versions() because the new function
returns EINVAL in the case where we fail to parse version numbers as
integers.

The order of arguments changes. I think it's clearer to use "source" and
then "target". In v1 patchsets, we used "add"/"remove", but we can start
moving away from that.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/patchset.c
@@ -310,6 +310,69 @@ patchset_process_digest(xmlNode *patch, xmlNode *source, xmlNode *target,
     return;
 }
 
+/*!
+ * \internal
+ * \brief Get the source and target CIB versions from an XML patchset
+ *
+ * Each output object will contain, in order, the following version fields from
+ * the source and target, respectively:
+ * * \c PCMK_XA_ADMIN_EPOCH
+ * * \c PCMK_XA_EPOCH
+ * * \c PCMK_XA_NUM_UPDATES
+ *
+ * \param[in]  patchset  XML patchset
+ * \param[out] source    Where to store versions from source CIB
+ * \param[out] target    Where to store versions from target CIB
+ *
+ * \return Standard Pacemaker return code
+ */
+int
+pcmk__xml_patchset_versions(const xmlNode *patchset, int source[3],
+                            int target[3])
+{
+    static const char *const vfields[] = {
+        PCMK_XA_ADMIN_EPOCH,
+        PCMK_XA_EPOCH,
+        PCMK_XA_NUM_UPDATES,
+    };
+
+    int format = 0;
+    const xmlNode *version = NULL;
+    const xmlNode *source_xml = NULL;
+    const xmlNode *target_xml = NULL;
+
+    CRM_CHECK((patchset != NULL) && (source != NULL) && (target != NULL),
+              return EINVAL);
+
+    crm_element_value_int(patchset, PCMK_XA_FORMAT, &format);
+    if (format != 2) {
+        crm_err("Unknown patch format: %d", format);
+        return EINVAL;
+    }
+
+    version = pcmk__xe_first_child(patchset, PCMK_XE_VERSION, NULL, NULL);
+    source_xml = pcmk__xe_first_child(version, PCMK_XE_SOURCE, NULL, NULL);
+    target_xml = pcmk__xe_first_child(version, PCMK_XE_TARGET, NULL, NULL);
+
+    if ((source_xml == NULL) || (target_xml == NULL)) {
+        return EINVAL;
+    }
+
+    for (int i = 0; i < PCMK__NELEM(vfields); i++) {
+        if (crm_element_value_int(source_xml, vfields[i], &(source[i])) != 0) {
+            return EINVAL;
+        }
+        crm_trace("Got %d for source[%s]", source[i], vfields[i]);
+
+        if (crm_element_value_int(target_xml, vfields[i], &(target[i])) != 0) {
+            return EINVAL;
+        }
+        crm_trace("Got %d for target[%s]", target[i], vfields[i]);
+    }
+
+    return pcmk_rc_ok;
+}
+
 // Get CIB versions used for additions and deletions in a patchset
 // Return value of true means failure; false means success
 bool
@@ -369,6 +432,7 @@ xml_patch_version_check(const xmlNode *xml, const xmlNode *patchset)
     int this[] = { 0, 0, 0 };
     int add[] = { 0, 0, 0 };
     int del[] = { 0, 0, 0 };
+    int rc = pcmk_rc_ok;
 
     const char *vfields[] = {
         PCMK_XA_ADMIN_EPOCH,
@@ -392,7 +456,10 @@ xml_patch_version_check(const xmlNode *xml, const xmlNode *patchset)
         del[lpc] = this[lpc];
     }
 
-    xml_patch_versions(patchset, add, del);
+    rc = pcmk__xml_patchset_versions(patchset, del, add);
+    if (rc != pcmk_rc_ok) {
+        return rc;
+    }
 
     for (lpc = 0; lpc < PCMK__NELEM(vfields); lpc++) {
         if (this[lpc] < del[lpc]) {
@@ -830,6 +897,18 @@ xml_apply_patchset(xmlNode *xml, xmlNode *patchset, bool check_version)
     return rc;
 }
 
+/*!
+ * \internal
+ * \brief Check whether a given CIB element was modified in a CIB patchset
+ *
+ * \param[in] patchset  CIB XML patchset
+ * \param[in] element   XML tag of CIB element to check (\c NULL is equivalent
+ *                      to \c PCMK_XE_CIB). Supported values include any CIB
+ *                      element supported by \c pcmk__cib_abs_xpath_for().
+ *
+ * \retval \c true if \p element was modified
+ * \retval \c false otherwise
+ */
 bool
 pcmk__cib_element_in_patchset(const xmlNode *patchset, const char *element)
 {

tools/crm_diff.c
@@ -150,7 +150,7 @@ log_patch_cib_versions(xmlNode *patch)
     const char *fmt = NULL;
     const char *digest = NULL;
 
-    xml_patch_versions(patch, add, del);
+    pcmk__xml_patchset_versions(patch, del, add);
     fmt = crm_element_value(patch, PCMK_XA_FORMAT);
     digest = crm_element_value(patch, PCMK__XA_DIGEST);
 

daemons/controld/controld_te_callbacks.c
@@ -389,7 +389,7 @@ te_update_diff(const char *event, xmlNode * msg)
     wrapper = pcmk__xe_first_child(msg, PCMK__XE_CIB_UPDATE_RESULT, NULL, NULL);
     diff = pcmk__xe_first_child(wrapper, NULL, NULL, NULL);
 
-    xml_patch_versions(diff, p_add, p_del);
+    pcmk__xml_patchset_versions(diff, p_del, p_add);
     crm_debug("Processing (%s) diff: %d.%d.%d -> %d.%d.%d (%s)", op,
               p_del[0], p_del[1], p_del[2], p_add[0], p_add[1], p_add[2],
               fsa_state2string(controld_globals.fsa_state));

daemons/controld/controld_te_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 the Pacemaker project contributors
+ * Copyright 2004-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -390,7 +390,7 @@ abort_transition_graph(int abort_priority, enum pcmk__graph_next abort_action,
         }
 
         if(diff) {
-            xml_patch_versions(diff, add, del);
+            pcmk__xml_patchset_versions(diff, del, add);
             for(search = reason; search; search = search->parent) {
                 if (pcmk__xe_is(search, PCMK_XE_CHANGE)) {
                     change = search;

daemons/fenced/fenced_cib.c
@@ -400,7 +400,7 @@ update_fencing_topology(const char *event, xmlNode *msg)
         return;
     }
 
-    xml_patch_versions(patchset, add, del);
+    pcmk__xml_patchset_versions(patchset, del, add);
 
     for (xmlNode *change = pcmk__xe_first_child(patchset, NULL, NULL, NULL);
          change != NULL; change = pcmk__xe_next(change, NULL)) {

include/crm/common/xml_internal.h
@@ -433,17 +433,9 @@ pcmk__xml_attr_value(const xmlAttr *attr)
            : (const char *) attr->children->content;
 }
 
-/*!
- * \internal
- * \brief Check whether a given CIB element was modified in a CIB patchset
- *
- * \param[in] patchset  CIB XML patchset
- * \param[in] element   XML tag of CIB element to check (\c NULL is equivalent
- *                      to \c PCMK_XE_CIB). Supported values include any CIB
- *                      element supported by \c pcmk__cib_abs_xpath_for().
- *
- * \return \c true if \p element was modified, or \c false otherwise
- */
+int pcmk__xml_patchset_versions(const xmlNode *patchset, int source[3],
+                                int target[3]);
+
 bool pcmk__cib_element_in_patchset(const xmlNode *patchset,
                                    const char *element);
 

lib/cib/cib_utils.c
@@ -48,7 +48,7 @@ cib_diff_version_details(xmlNode * diff, int *admin_epoch, int *epoch, int *upda
     int add[] = { 0, 0, 0 };
     int del[] = { 0, 0, 0 };
 
-    xml_patch_versions(diff, add, del);
+    pcmk__xml_patchset_versions(diff, del, add);
 
     *admin_epoch = add[0];
     *epoch = add[1];

lib/common/patchset_display.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 the Pacemaker project contributors
+ * Copyright 2004-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -44,7 +44,7 @@ xml_show_patchset_header(pcmk__output_t *out, const xmlNode *patchset)
     int add[] = { 0, 0, 0 };
     int del[] = { 0, 0, 0 };
 
-    xml_patch_versions(patchset, add, del);
+    pcmk__xml_patchset_versions(patchset, del, add);
 
     if ((add[0] != del[0]) || (add[1] != del[1]) || (add[2] != del[2])) {
         const char *fmt = crm_element_value(patchset, PCMK_XA_FORMAT);

commit ec4b9ff8d00279609baee8f03c91925b8c0d4269

Fix: libcrmcommon: Don't require version info when applying CIB patches
This fixes a regression introduced by 9cd1ccbb, which broke updates by
pcs. pcs generates a CIB diff after editing the CIB XML directly, rather
than using Pacemaker tools that automatically bump the CIB version.

This regression has not made it into any release.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/patchset.c
@@ -321,6 +321,10 @@ pcmk__xml_patchset_add_digest(xmlNode *patchset, const xmlNode *target)
  * * \c PCMK_XA_EPOCH
  * * \c PCMK_XA_NUM_UPDATES
  *
+ * If source versions or target versions are absent from the patchset, then
+ * \p source and \p target (respectively) are left unmodified. This is not
+ * treated as an error. An unparsable version is an error, however.
+ *
  * \param[in]  patchset  XML patchset
  * \param[out] source    Where to store versions from source CIB
  * \param[out] target    Where to store versions from target CIB
@@ -355,20 +359,51 @@ pcmk__xml_patchset_versions(const xmlNode *patchset, int source[3],
     source_xml = pcmk__xe_first_child(version, PCMK_XE_SOURCE, NULL, NULL);
     target_xml = pcmk__xe_first_child(version, PCMK_XE_TARGET, NULL, NULL);
 
-    if ((source_xml == NULL) || (target_xml == NULL)) {
-        return EINVAL;
-    }
-
+    /* @COMPAT Consider requiring source_xml and target_xml to be non-NULL. As
+     * of pcs version 0.10.8, pcs creates a patchset using crm_diff
+     * --no-version. The behavior and documentation of the crm_diff options
+     * --cib and --no-version are questionable and should be re-examined. Even
+     * without --no-version, crm_diff does not update the target version in the
+     * generated patchset. So a diff based on a manual CIB XML edit is likely to
+     * have unchanged version numbers. (Pacemaker tools bump the CIB versions
+     * automatically when editing the CIB.)
+     *
+     * Until then, we may be applying a patchset that has no version info. We
+     * will allow either source version or target version to be missing (even
+     * though both should be present or both should be missing). However, return
+     * an error if any of the three vfields is missing from a source or target
+     * version element that is present. That level of sanity check should be
+     * okay.
+     *
+     * We leave the destination arrays unmodified in case of absent versions,
+     * instead of setting them to some default value like { 0, 0, 0 }.
+     * xml_patch_version_check() sets its own defaults in case of absent
+     * versions.
+     */
     for (int i = 0; i < PCMK__NELEM(vfields); i++) {
-        if (crm_element_value_int(source_xml, vfields[i], &(source[i])) != 0) {
-            return EINVAL;
-        }
-        crm_trace("Got %d for source[%s]", source[i], vfields[i]);
+        if (source_xml != NULL) {
+            if (crm_element_value_int(source_xml, vfields[i],
+                                      &(source[i])) != 0) {
+                return EINVAL;
+            }
+            crm_trace("Got source[%s]=%d", vfields[i], source[i]);
 
-        if (crm_element_value_int(target_xml, vfields[i], &(target[i])) != 0) {
-            return EINVAL;
+        } else {
+            crm_trace("No source versions found; keeping source[%s]=%d",
+                      vfields[i], source[i]);
         }
-        crm_trace("Got %d for target[%s]", target[i], vfields[i]);
+
+        if (target_xml != NULL) {
+            if (crm_element_value_int(target_xml, vfields[i],
+                                      &(target[i])) != 0) {
+                return EINVAL;
+            }
+            crm_trace("Got target[%s]=%d", vfields[i], target[i]);
+
+        } else {
+            crm_trace("No target versions found; keeping target[%s]=%d",
+                      vfields[i], target[i]);
+        }
     }
 
     return pcmk_rc_ok;

tools/crm_diff.c
@@ -68,6 +68,13 @@ static GOptionEntry operation_entries[] = {
 };
 
 static GOptionEntry addl_entries[] = {
+    /* @FIXME Revisit --cib and --no-version and consider deprecating or
+     * renaming --cib, as well as improving the help text. --cib does things
+     * other than include version details. Also, even with --cib, we call
+     * xml_create_patchset() with manage_versions=false in generate_patch(). So
+     * we don't update the target versions in the patchset, and we don't drop
+     * the versions from the patchset unless --no-version is given.
+     */
     { "cib", 'c', 0, G_OPTION_ARG_NONE, &options.as_cib,
       "Compare/patch the inputs as a CIB (includes versions details)",
       NULL },