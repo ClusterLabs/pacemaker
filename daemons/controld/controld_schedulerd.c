ClusterLabs/pacemaker

commit 676b54728fa6fbd1550129f06e358d80cd9a92d3

Avoid blocking wait in controller for CIB re-request
closes T966

daemons/controld/controld_schedulerd.c
@@ -27,6 +27,8 @@ static void handle_disconnect(void);
 
 static pcmk_ipc_api_t *schedulerd_api = NULL;
 
+static mainloop_timer_t *cib_retry_timer = NULL;
+
 /*!
  * \internal
  * \brief Close any scheduler connection and free associated memory
@@ -444,6 +446,15 @@ force_local_option(xmlNode *xml, const char *attr_name, const char *attr_value)
     xmlXPathFreeObject(xpathObj);
 }
 
+static gboolean
+sleep_timer(gpointer data)
+{
+    controld_set_fsa_action_flags(A_PE_INVOKE);
+    controld_trigger_fsa();
+    mainloop_timer_del(cib_retry_timer);
+    return G_SOURCE_REMOVE;
+}
+
 static void
 do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void *user_data)
 {
@@ -475,10 +486,8 @@ do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void
     } else if (num_cib_op_callbacks() > 1) {
         crm_debug("Re-asking for the CIB: %d other peer updates still pending",
                   (num_cib_op_callbacks() - 1));
-        sleep(1);
-        controld_set_fsa_action_flags(A_PE_INVOKE);
-        controld_trigger_fsa();
-        return;
+        cib_retry_timer = mainloop_timer_add("cib_retry", 1000, FALSE, sleep_timer, NULL);
+        mainloop_timer_start(cib_retry_timer);
     }
 
     CRM_LOG_ASSERT(output != NULL);

commit 45363ca9fbed1445749ea1447999a2527eedb4ba

Fix: controller: Avoid crash due to GSource double-free
And rename cib_retry_timer to controld_cib_retry_timer so that it
doesn't look like public CIB API.

This fixes a regression introduced by 676b547 and/or f91848e.

Issues have been observed occasionally during cts-lab runs. For example:

Jul 30 16:20:21 BadNews: 2025-07-30T16:19:46-0400 rhel9-ctslab-3 pacemaker-controld[54218]:  error: crm_glib_handler: Forked child [73686] to record non-fatal assertion at logging.c:83 : Source ID 2307737477 was not found when attempting to remove it
Jul 30 16:20:21 BadNews: 2025-07-30T16:19:47-0400 rhel9-ctslab-3 pacemaker-controld[54218]:  crit: GLib: Source ID 2307737477 was not found when attempting to remove it
Jul 30 16:20:23 BadNews: 2025-07-30T16:19:58-0400 rhel9-ctslab-3 pacemakerd[54211]:  error: pacemaker-controld[54218] terminated with signal 6 (Aborted)

We believe this is happening because somehow we're assigning a new value
to cib_retry_timer before we run the sleep_timer callback for the first
one. So, for example:
1. do_pe_invoke_callback() creates cib_retry_timer. Call it timer-1.
2. do_pe_invoke_callback() creates cib_retry_timer. Call it timer-2.
   Now cib_retry_timer no longer points to timer-1.
3. sleep_timer gets called for timer-1 . It calls
   mainloop_timer_del(cib_retry_timer). This destroys timer-2.
4. sleep_timer gets called for timer-2. It calls
   mainloop_timer_del(cib_retry_timer). This tries to destroy timer-2
   again, which fails.

Commit 676b547 removed a blocking wait, so there's probably a way for us
to re-enter do_pe_invoke_callback() before the first wait expires.

So I think we should be NULL-ing cib_retry_timer after we delete it, and
also making sure it's currently NULL before we assign to it in
do_pe_invoke_callback(). We can do this by returning early from
do_pe_invoke(). That way we don't lose the reference to our existing
timer.

I hope the controller doesn't end up in a busy-loop of just trying to

daemons/controld/controld_schedulerd.c
@@ -27,7 +27,7 @@ static void handle_disconnect(void);
 
 static pcmk_ipc_api_t *schedulerd_api = NULL;
 
-static mainloop_timer_t *cib_retry_timer = NULL;
+static mainloop_timer_t *controld_cib_retry_timer = NULL;
 
 /*!
  * \internal
@@ -362,6 +362,10 @@ do_pe_invoke(long long action,
         register_fsa_input_before(C_FSA_INTERNAL, I_ELECTION, NULL);
         return;
     }
+    if (controld_cib_retry_timer != NULL) {
+        crm_debug("Not invoking scheduler until CIB retry timer expires");
+        return;
+    }
 
     fsa_pe_query = cib_conn->cmds->query(cib_conn, NULL, NULL, cib_none);
 
@@ -451,7 +455,8 @@ sleep_timer(gpointer data)
 {
     controld_set_fsa_action_flags(A_PE_INVOKE);
     controld_trigger_fsa();
-    mainloop_timer_del(cib_retry_timer);
+    mainloop_timer_del(controld_cib_retry_timer);
+    controld_cib_retry_timer = NULL;
     return G_SOURCE_REMOVE;
 }
 
@@ -486,8 +491,9 @@ do_pe_invoke_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void
     } else if (num_cib_op_callbacks() > 1) {
         crm_debug("Re-asking for the CIB: %d other peer updates still pending",
                   (num_cib_op_callbacks() - 1));
-        cib_retry_timer = mainloop_timer_add("cib_retry", 1000, FALSE, sleep_timer, NULL);
-        mainloop_timer_start(cib_retry_timer);
+        controld_cib_retry_timer = mainloop_timer_add("cib_retry", 1000, false,
+                                                      sleep_timer, NULL);
+        mainloop_timer_start(controld_cib_retry_timer);
         return;
     }
 