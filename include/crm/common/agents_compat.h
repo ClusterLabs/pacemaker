57524b6ec6dcaebb236e40660694c8744278d2ec
Reid Wahl <nrwahl@protonmail.com>
2025-08-10 00:23:46 -0700

    API: libcrmcommon: Deprecate PCMK_STONITH_ACTION_LIMIT
    Ref T895
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

---
+++ include/crm/common/agents_compat.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2004-2025 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef PCMK__CRM_COMMON_AGENTS_COMPAT__H
+#define PCMK__CRM_COMMON_AGENTS_COMPAT__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \file
+ * \brief Deprecated Pacemaker resource agents API
+ * \ingroup core
+ * \deprecated Do not include this header directly. The nvpair APIs in this
+ *             header, and the header itself, will be removed in a future
+ *             release.
+ */
+
+//! \deprecated Use \c PCMK_FENCING_ACTION_LIMIT instead
+#define PCMK_STONITH_ACTION_LIMIT PCMK_FENCING_ACTION_LIMIT
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PCMK__CRM_COMMON_AGENTS_COMPAT__H
