ClusterLabs/pacemaker

commit 0ac9beeabe1b3955b8fdbf1296edcc948a692d47

Refactor: tools: exit_on_invalid_cib should check for NULL first.
This means it can unconditionally be called, which means I can
un-indent a bunch of code.

tools/crm_mon.c
@@ -1388,6 +1388,10 @@ one_shot(void)
 static void
 exit_on_invalid_cib(void)
 {
+    if (cib != NULL) {
+        return;
+    }
+
     // Shouldn't really be possible
     g_set_error(&error, PCMK__EXITC_ERROR, CRM_EX_ERROR, "Invalid CIB source");
     clean_up(CRM_EX_ERROR);
@@ -1452,39 +1456,36 @@ main(int argc, char **argv)
          */
         cib = cib_new();
 
-        if (cib == NULL) {
-            exit_on_invalid_cib();
-        } else {
-            switch (cib->variant) {
+        exit_on_invalid_cib();
 
-                case cib_native:
-                    /* cib & fencing - everything available */
-                    use_cib_native = TRUE;
-                    break;
+        switch (cib->variant) {
+            case cib_native:
+                /* cib & fencing - everything available */
+                use_cib_native = TRUE;
+                break;
 
-                case cib_file:
-                    /* Don't try to connect to fencing as we
-                     * either don't have a running cluster or
-                     * the fencing-information would possibly
-                     * not match the cib data from a file.
-                     * As we don't expect cib-updates coming
-                     * in enforce one-shot. */
-                    fence_history_cb("--fence-history", "0", NULL, NULL);
-                    options.one_shot = TRUE;
-                    break;
+            case cib_file:
+                /* Don't try to connect to fencing as we
+                 * either don't have a running cluster or
+                 * the fencing-information would possibly
+                 * not match the cib data from a file.
+                 * As we don't expect cib-updates coming
+                 * in enforce one-shot. */
+                fence_history_cb("--fence-history", "0", NULL, NULL);
+                options.one_shot = TRUE;
+                break;
 
-                case cib_remote:
-                    /* updates coming in but no fencing */
-                    fence_history_cb("--fence-history", "0", NULL, NULL);
-                    break;
+            case cib_remote:
+                /* updates coming in but no fencing */
+                fence_history_cb("--fence-history", "0", NULL, NULL);
+                break;
 
-                case cib_undefined:
-                case cib_database:
-                default:
-                    /* something is odd */
-                    exit_on_invalid_cib();
-                    break;
-            }
+            case cib_undefined:
+            case cib_database:
+            default:
+                /* something is odd */
+                exit_on_invalid_cib();
+                break;
         }
 
         if (options.one_shot) {
@@ -1546,9 +1547,7 @@ main(int argc, char **argv)
         cib = NULL;
         pcmk__daemonize(crm_system_name, options.pid_file);
         cib = cib_new();
-        if (cib == NULL) {
-            exit_on_invalid_cib();
-        }
+        exit_on_invalid_cib();
     }
 
     show = default_includes(output_format);

commit b5730805cf55da77f32536ba250ae5d2bef9891a

Low: tools: Fix CIB validation in crm_mon.c
0ac9beea introduced a bug wherein we stopped exiting on non-NULL cib. So
exit_on_invalid_cib() works if cib is NULL but does nothing if
cib->variant is invalid. The latter case should never happen, but we use
exit_on_invalid_cib() to handle exactly that.

But even b71146d9, which introduced exit_on_invalid_cib(), had a
misleading error message. "Invalid CIB source" isn't accurate when cib
is NULL.

The duplication will go away in an upcoming commit.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

tools/crm_mon.c
@@ -1394,18 +1394,6 @@ one_shot(void)
     }
 }
 
-static void
-exit_on_invalid_cib(void)
-{
-    if (cib != NULL) {
-        return;
-    }
-
-    // Shouldn't really be possible
-    g_set_error(&error, PCMK__EXITC_ERROR, CRM_EX_ERROR, "Invalid CIB source");
-    clean_up(CRM_EX_ERROR);
-}
-
 int
 main(int argc, char **argv)
 {
@@ -1454,8 +1442,16 @@ main(int argc, char **argv)
          * decisions based on the cib-source
          */
         cib = cib_new();
-
-        exit_on_invalid_cib();
+        if (cib == NULL) {
+            /* For the foreseeable future, out-of-memory is the only possible
+             * reason for NULL return value
+             */
+            rc = ENOMEM;
+            g_set_error(&error, PCMK__RC_ERROR, rc,
+                        "Failed to create CIB API connection object: %s",
+                        pcmk_rc_str(rc));
+            clean_up(pcmk_rc2exitc(rc));
+        }
 
         switch (cib->variant) {
             case cib_native:
@@ -1478,8 +1474,8 @@ main(int argc, char **argv)
                 break;
 
             default:
-                /* something is odd */
-                exit_on_invalid_cib();
+                // Should not be possible; would indicate a bug in CIB library
+                CRM_CHECK(false, clean_up(CRM_EX_ERROR));
                 break;
         }
 
@@ -1532,7 +1528,15 @@ main(int argc, char **argv)
         cib = NULL;
         pcmk__daemonize(crm_system_name, options.pid_file);
         cib = cib_new();
-        exit_on_invalid_cib();
+
+        // @TODO Remove this duplication
+        if (cib == NULL) {
+            rc = ENOMEM;
+            g_set_error(&error, PCMK__RC_ERROR, rc,
+                        "Failed to create CIB API connection object: %s",
+                        pcmk_rc_str(rc));
+            clean_up(pcmk_rc2exitc(rc));
+        }
     }
 
     show = default_includes(output_format);