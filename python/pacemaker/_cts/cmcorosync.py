ClusterLabs/pacemaker

commit a58a4000f4154b3a0c11b1b29c07b4adb757f82a

Refactor: cts: Remove --valgrind-tests
This option was meant to enable a special ValgrindTest class in cts-lab.
However, when I rearranged all the cts-lab code, I didn't bring that
test class along.  This is probably because in order for it to do
anything, you would have to enable valgrind on each lab node, but it
still wouldn't do anything because the test's __call__ method was empty.

I also find this option's presence confusing.  I expected it to actually
enable valgrind for me, which it did not do.  So just remove it.

Related T989

python/pacemaker/_cts/cmcorosync.py
@@ -63,7 +63,7 @@ class Corosync2(ClusterManager):
         # so don't include them in the returned list
         vgrind = self.env["valgrind-procs"].split()
         for (key, val) in self._fullcomplist.items():
-            if self.env["valgrind-tests"] and key in vgrind:
+            if key in vgrind:
                 self.log(f"Filtering {key} from the component list as it is being profiled by valgrind")
                 continue
 

python/pacemaker/_cts/environment.py
@@ -453,9 +453,6 @@ class Environment:
                           metavar="PROCS",
                           default="pacemaker-attrd pacemaker-based pacemaker-controld pacemaker-execd pacemaker-fenced pacemaker-schedulerd",
                           help="Run valgrind against the given space-separated list of processes")
-        grp4.add_argument("--valgrind-tests",
-                          action="store_true",
-                          help="Include tests using valgrind")
         grp4.add_argument("--warn-inactive",
                           action="store_true",
                           help="Warn if a resource is assigned to an inactive node")
@@ -492,7 +489,6 @@ class Environment:
         self["stonith-type"] = args.stonith_type
         self["unsafe-tests"] = not args.no_unsafe_tests
         self["valgrind-procs"] = args.valgrind_procs
-        self["valgrind-tests"] = args.valgrind_tests
         self["warn-inactive"] = args.warn_inactive
 
         # Nodes and groups are mutually exclusive, so their defaults cannot be

python/pacemaker/_cts/tests/ctstest.py
@@ -60,7 +60,6 @@ class CTSTest:
         self.is_experimental = False
         self.is_loop = False
         self.is_unsafe = False
-        self.is_valgrind = False
         self.passed = True
 
     def log(self, args):
@@ -227,9 +226,6 @@ class CTSTest:
             return False
 
         if self.is_unsafe and not self._env["unsafe-tests"]:
-            return False
-
-        if self.is_valgrind and not self._env["valgrind-tests"]:
             return False
 
         if self.is_experimental and not self._env["experimental-tests"]:

commit 3b2571c2c7a7915c2542870672d734e9ce6ca5ef

Test: cts: Fix regression in ComponentFail test
Commit a58a4000 caused a regression such that the ComponentFail test
always chooses to kill corosync. It never kills any of the other
Pacemaker daemons, which all get filtered out because they're in
valgrind-procs.

Prior to that commit, they got filtered out only if valgrind-tests was
set. It defaulted to False. The 'if self.env["valgrind-tests"]' check
was removed by that commit.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

python/pacemaker/_cts/cmcorosync.py
@@ -59,14 +59,7 @@ class Corosync2(ClusterManager):
                            badnews_ignore=badnews)
             self._fullcomplist["corosync"] = proc
 
-        # Processes running under valgrind can't be shot with "killall -9 processname",
-        # so don't include them in the returned list
-        vgrind = self.env["valgrind-procs"].split()
         for (key, val) in self._fullcomplist.items():
-            if key in vgrind:
-                self.log(f"Filtering {key} from the component list as it is being profiled by valgrind")
-                continue
-
             if key == "pacemaker-fenced" and not self.env["DoFencing"]:
                 continue
 