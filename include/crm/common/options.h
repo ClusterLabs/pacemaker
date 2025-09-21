4cbbe5158cd12e8801eb165dc16b2dc3638bb592
Reid Wahl <nrwahl@protonmail.com>
2025-09-20 23:43:11 -0700

    API: libcrmcommon: Drop PCMK_OPT_STOP_REMOVED_ACTIONS
    It has been replaced by PCMK_OPT_CANCEL_REMOVED_ACTIONS.
    PCMK_OPT_STOP_REMOVED_ACTIONS has not made it into any release yet, so
    there is no need for a deprecation period.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options.h
+++ include/crm/common/options.h
@@ -68,7 +68,6 @@ extern     "C" {
 #define PCMK_OPT_START_FAILURE_IS_FATAL         "start-failure-is-fatal"
 #define PCMK_OPT_STARTUP_FENCING                "startup-fencing"
 #define PCMK_OPT_STOP_ALL_RESOURCES             "stop-all-resources"
-#define PCMK_OPT_STOP_REMOVED_ACTIONS           "stop-removed-actions"
 #define PCMK_OPT_STOP_REMOVED_RESOURCES         "stop-removed-resources"
 #define PCMK_OPT_SYMMETRIC_CLUSTER              "symmetric-cluster"
 #define PCMK_OPT_TRANSITION_DELAY               "transition-delay"
