cff6f16e98a6eb590b0f3db9650554fec33a9b94
Reid Wahl <nrwahl@protonmail.com>
2025-09-24 17:52:13 -0700

    Refactor: libcrmcommon: Drop unused include from actions.h
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/actions.h
+++ include/crm/common/actions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 the Pacemaker project contributors
+ * Copyright 2004-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -11,7 +11,6 @@
 #define PCMK__CRM_COMMON_ACTIONS__H
 
 #include <stdbool.h>                    // bool
-#include <strings.h>                    // strcasecmp()
 #include <glib.h>                       // gboolean, guint
 #include <libxml/tree.h>                // xmlNode
 
