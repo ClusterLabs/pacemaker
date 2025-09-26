feffb5d4e085d833783de604c7944cc789b47c82
Chris Lumens <clumens@redhat.com>
2025-09-26 09:17:13 -0400

    Refactor: libcrmcommon: Convert the pcmk__group_flags enum...
    ...to use the UINT32_C style of declaration.
    
    Ref T222

--- include/crm/common/group_internal.h
+++ include/crm/common/group_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 the Pacemaker project contributors
+ * Copyright 2004-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -12,6 +12,7 @@
 
 #include <stdio.h>                          // NULL
 #include <stdbool.h>                        // bool
+#include <stdint.h>                         // UINT32_C
 #include <crm/common/scheduler_types.h>     // pcmk_resource_t
 #include <crm/common/resources_internal.h>  // pcmk__rsc_variant_group etc.
 
@@ -19,10 +20,16 @@
 extern "C" {
 #endif
 
-// Group resource flags (used in variant data)
+/*!
+ * \internal
+ * \brief Group resource flags (used in variant data)
+ */
 enum pcmk__group_flags {
-    pcmk__group_ordered     = (1 << 0), // Members start sequentially
-    pcmk__group_colocated   = (1 << 1), // Members must be on same node
+    //! Members start sequentially
+    pcmk__group_ordered     = (UINT32_C(1) << 0),
+
+    //! Members must be on same node
+    pcmk__group_colocated   = (UINT32_C(1) << 1),
 };
 
 /*!
