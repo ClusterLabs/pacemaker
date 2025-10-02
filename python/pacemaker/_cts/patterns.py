d3237a14b4f456f6a740b7250c90769f39e1e506
Chris Lumens <clumens@redhat.com>
2025-10-02 11:10:05 -0400

    Low: cts: Fix the BadNews messages that the SplitBrain test ignores.
    This has been broken since the message changed in 07a19bcf3be, but we
    didn't notice because the SplitBrain test has been disabled for a long
    time.
    
    Additionally, I've removed the "stonith " part from the beginning of the
    string.  Other resources can cause the same message to be printed in the
    SplitBrain test.  We could have a whole separate message for this case,
    but I'm not sure it's worth it.

--- python/pacemaker/_cts/patterns.py
+++ python/pacemaker/_cts/patterns.py
@@ -76,7 +76,7 @@ class BasePatterns:
             "Pat:Fencing_start": r"Requesting peer fencing .* targeting %s",
             "Pat:Fencing_ok": r"pacemaker-fenced.*:\s*Operation .* targeting %s by .* for .*@.*: OK",
             "Pat:Fencing_recover": r"pacemaker-schedulerd.*: Recover\s+%s",
-            "Pat:Fencing_active": r"stonith resource .* is active on 2 nodes (attempting recovery)",
+            "Pat:Resource_active": r"resource .* might be active on \d+ nodes \(attempting recovery\)",
             "Pat:Fencing_probe": r"pacemaker-controld.* Result of probe operation for %s on .*: Error",
 
             "Pat:RscOpOK": r"pacemaker-controld.*:\s+Result of %s operation for %s.*: (0 \()?OK",
