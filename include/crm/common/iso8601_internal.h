d0ea4f2721afb28fc7ea54db7611363644c4c14d
Reid Wahl <nrwahl@protonmail.com>
2025-11-17 22:48:36 -0800

    Refactor: libcrmcommon: New pcmk__time_get_ywd()
    To replace crm_time_get_isoweek(), which doesn't seem as if it needs to
    be part of public API.
    
    We could make a case for keeping this public because we allow the use of
    ISO week dates in date specifications within rules. Some hypothetical
    user might want to get the ISO week date from a crm_time_t and use it to
    create a rule. However, that feels a bit contrived, and I think I would
    be in favor of deprecating ISO week dates from rules... it's not a ton
    of trouble to keep, but it's hard to imagine anyone using it.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/iso8601_internal.h
+++ include/crm/common/iso8601_internal.h
@@ -20,6 +20,8 @@
 extern "C" {
 #endif
 
+void pcmk__time_get_ywd(const crm_time_t *dt, uint32_t *y, uint32_t *w,
+                        uint32_t *d);
 char *pcmk__time_format_hr(const char *format, const crm_time_t *dt, int usec);
 char *pcmk__epoch2str(const time_t *source, uint32_t flags);
 char *pcmk__timespec2str(const struct timespec *ts, uint32_t flags);
