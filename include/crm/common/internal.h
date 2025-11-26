052d8dee83e30e896a0c3e35160e39fe67642d11
Chris Lumens <clumens@redhat.com>
2025-11-26 13:25:03 -0500

    Refactor: include: Move failure-related functions out of internal.h.
    ...and into failcounts_internal.h where they belong.  Additionally,
    logging.h needs to include results.h because CRM_CHECK uses crm_abort.
    It would be worth going through all these header files and make sure
    they include everything they need, but that's a project for a different
    day.
    
    Ref T777

--- include/crm/common/internal.h
+++ include/crm/common/internal.h
@@ -10,12 +10,13 @@
 #ifndef PCMK__CRM_COMMON_INTERNAL__H
 #define PCMK__CRM_COMMON_INTERNAL__H
 
-#include <glib.h>               // guint, GHashTable
+#include <glib.h>               // GHashTable
 
 #include <crm/common/agents_internal.h>
 #include <crm/common/acl_internal.h>
 #include <crm/common/actions_internal.h>
 #include <crm/common/digest_internal.h>
+#include <crm/common/failcounts_internal.h>
 #include <crm/common/flags_internal.h>
 #include <crm/common/health_internal.h>
 #include <crm/common/io_internal.h>
@@ -53,52 +54,6 @@ extern bool pcmk__is_daemon;
 
 int pcmk__substitute_secrets(const char *rsc_id, GHashTable *params);
 #endif
-
-
-/* convenience functions for failure-related node attributes */
-
-#define PCMK__FAIL_COUNT_PREFIX   "fail-count"
-#define PCMK__LAST_FAILURE_PREFIX "last-failure"
-
-/*!
- * \internal
- * \brief Generate a failure-related node attribute name for a resource
- *
- * \param[in] prefix       Start of attribute name
- * \param[in] rsc_id       Resource name
- * \param[in] op           Operation name
- * \param[in] interval_ms  Operation interval
- *
- * \return Newly allocated string with attribute name
- *
- * \note Failure attributes are named like PREFIX-RSC#OP_INTERVAL (for example,
- *       "fail-count-myrsc#monitor_30000"). The '#' is used because it is not
- *       a valid character in a resource ID, to reliably distinguish where the
- *       operation name begins. The '_' is used simply to be more comparable to
- *       action labels like "myrsc_monitor_30000".
- */
-static inline char *
-pcmk__fail_attr_name(const char *prefix, const char *rsc_id, const char *op,
-                   guint interval_ms)
-{
-    CRM_CHECK(prefix && rsc_id && op, return NULL);
-    return pcmk__assert_asprintf("%s-%s#%s_%u", prefix, rsc_id, op,
-                                 interval_ms);
-}
-
-static inline char *
-pcmk__failcount_name(const char *rsc_id, const char *op, guint interval_ms)
-{
-    return pcmk__fail_attr_name(PCMK__FAIL_COUNT_PREFIX, rsc_id, op,
-                                interval_ms);
-}
-
-static inline char *
-pcmk__lastfailure_name(const char *rsc_id, const char *op, guint interval_ms)
-{
-    return pcmk__fail_attr_name(PCMK__LAST_FAILURE_PREFIX, rsc_id, op,
-                                interval_ms);
-}
 
 #ifdef __cplusplus
 }
