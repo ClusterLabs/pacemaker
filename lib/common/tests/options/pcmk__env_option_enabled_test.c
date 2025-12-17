1ff4286904a80d534079085c879f78ccb703eb4d
Reid Wahl <nrwahl@protonmail.com>
2025-12-17 15:15:06 -0600

    Refactor: libcrmcommon: Use constants in pcmk__env_option_enabled tests
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/tests/options/pcmk__env_option_enabled_test.c
+++ lib/common/tests/options/pcmk__env_option_enabled_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the Pacemaker project contributors
+ * Copyright 2022-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -11,6 +11,9 @@
 #include <crm/common/unittest_internal.h>
 
 #include "mock_private.h"
+
+#define OPTION "env_var"
+#define ENV_VAR "PCMK_" OPTION
 
 static void
 disabled_null_value(void **state)
@@ -26,13 +29,13 @@ enabled_true_value(void **state)
     // Return true if option value is true, with or without daemon name
     pcmk__mock_getenv = true;
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, "true");
-    assert_true(pcmk__env_option_enabled(NULL, "env_var"));
+    assert_true(pcmk__env_option_enabled(NULL, OPTION));
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, "true");
-    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
     pcmk__mock_getenv = false;
 }
@@ -43,13 +46,13 @@ disabled_false_value(void **state)
     // Return false if option value is false (no daemon list)
     pcmk__mock_getenv = true;
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, "false");
-    assert_false(pcmk__env_option_enabled(NULL, "env_var"));
+    assert_false(pcmk__env_option_enabled(NULL, OPTION));
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, "false");
-    assert_false(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_false(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
     pcmk__mock_getenv = false;
 }
@@ -60,23 +63,23 @@ enabled_daemon_in_list(void **state)
     // Return true if daemon is in the option's value
     pcmk__mock_getenv = true;
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, PCMK__SERVER_EXECD);
-    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, PCMK__SERVER_EXECD "," PCMK__SERVER_FENCED);
-    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, PCMK__SERVER_CONTROLD "," PCMK__SERVER_EXECD);
-    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv,
                 PCMK__SERVER_CONTROLD "," PCMK__SERVER_EXECD
                 "," PCMK__SERVER_FENCED);
-    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_true(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
     pcmk__mock_getenv = false;
 }
@@ -87,9 +90,9 @@ disabled_daemon_not_in_list(void **state)
     // Return false if value is not true and daemon is not in the option's value
     pcmk__mock_getenv = true;
 
-    expect_string(__wrap_getenv, name, "PCMK_env_var");
+    expect_string(__wrap_getenv, name, ENV_VAR);
     will_return(__wrap_getenv, PCMK__SERVER_CONTROLD "," PCMK__SERVER_FENCED);
-    assert_false(pcmk__env_option_enabled(PCMK__SERVER_EXECD, "env_var"));
+    assert_false(pcmk__env_option_enabled(PCMK__SERVER_EXECD, OPTION));
 
     pcmk__mock_getenv = false;
 }
