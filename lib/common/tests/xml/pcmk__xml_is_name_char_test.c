22ea5833ee66f81ce001b4f0a4b09c6c745059ff
Reid Wahl <nrwahl@protonmail.com>
2025-12-19 11:03:26 -0800

    Refactor: libcrmcommon: Split unit test helpers into true/false cases
    With a function, the CMocka assert macros showed unhelpful line numbers
    on error.
    
    We want to convert these to macros. Splitting into multiple helpers has
    two goals.
    * Make each helper a bit easier to read.
    * Avoid complaints from the compiler or Coverity. I started making
      splits after compilation failed due to a NULL strcmp() argument, even
      though strcmp() would have never actually been *called* with a NULL
      argument. Macro expansion has unpleasant results sometimes.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/tests/xml/pcmk__xml_is_name_char_test.c
+++ lib/common/tests/xml/pcmk__xml_is_name_char_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the Pacemaker project contributors
+ * Copyright 2024-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -18,27 +18,37 @@
 
 /*!
  * \internal
- * \brief Assert that a Unicode character is (not) a valid XML \c NameChar
+ * \brief Assert that a Unicode character is a valid XML \c NameChar
  *
- * \param[in] c          Unicode code point of character to check
- * \param[in] reference  If \c true, \p c should be considered a valid
- *                       \c NameChar; if \c false, it should not
+ * \param[in] code_pt  Unicode code point of character to check
  */
 static void
-assert_name_char(int c, bool reference)
+assert_name_char(int code_pt)
 {
     gchar utf8_buf[6] = { 0, };
     int len = 4;
-    int ref_len = g_unichar_to_utf8(c, utf8_buf);
-    bool result = pcmk__xml_is_name_char(utf8_buf, &len);
+    int ref_len = g_unichar_to_utf8(code_pt, utf8_buf);
 
-    if (reference) {
-        assert_true(result);
-    } else {
-        assert_false(result);
-    }
+    assert_true(pcmk__xml_is_name_char(utf8_buf, &len));
+    assert_int_equal(len, ref_len);
+}
 
-    if ((c < 0xD800) || (c > 0xDFFF)) {
+/*!
+ * \internal
+ * \brief Assert that a Unicode character is not a valid XML \c NameChar
+ *
+ * \param[in] code_pt  Unicode code point of character to check
+ */
+static void
+assert_not_name_char(int code_pt)
+{
+    gchar utf8_buf[6] = { 0, };
+    int len = 4;
+    int ref_len = g_unichar_to_utf8(code_pt, utf8_buf);
+
+    assert_false(pcmk__xml_is_name_char(utf8_buf, &len));
+
+    if ((code_pt < 0xD800) || (code_pt > 0xDFFF)) {
         /* Unicode code points in the range D800 to DFFF are UTF-16 surrogate
          * pair halves. They can be represented in UTF-8, but they shouldn't
          * appear in valid UTF-8-encoded text. RFC 3629 (Nov 2003) says they
@@ -69,9 +79,9 @@ ascii(void **state)
         if (g_ascii_isalnum(c)
             || c == ':' || c == '_' || c == '-' || c == '.') {
 
-            assert_name_char(c, true);
+            assert_name_char(c);
         } else {
-            assert_name_char(c, false);
+            assert_not_name_char(c);
         }
     }
 }
@@ -80,21 +90,21 @@ static void
 unicode_0x80_to_0xB6(void **state)
 {
     for (int c = 0x80; c <= 0xB6; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
 static void
 unicode_0xB7(void **state)
 {
-    assert_name_char(0xB7, true);
+    assert_name_char(0xB7);
 }
 
 static void
 unicode_0xB8_to_0xBF(void **state)
 {
     for (int c = 0xB8; c <= 0xBF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -102,35 +112,35 @@ static void
 unicode_0xC0_to_0xD6(void **state)
 {
     for (int c = 0xC0; c <= 0xD6; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
 static void
 unicode_0xD7(void **state)
 {
-    assert_name_char(0xD7, false);
+    assert_not_name_char(0xD7);
 }
 
 static void
 unicode_0xD8_to_0xF6(void **state)
 {
     for (int c = 0xD8; c <= 0xF6; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
 static void
 unicode_0xF7(void **state)
 {
-    assert_name_char(0xF7, false);
+    assert_not_name_char(0xF7);
 }
 
 static void
 unicode_0xF8_to_0x2FF(void **state)
 {
     for (int c = 0xF8; c <= 0x2FF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -138,7 +148,7 @@ static void
 unicode_0x300_to_0x36F(void **state)
 {
     for (int c = 0x300; c <= 0x36F; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -146,21 +156,21 @@ static void
 unicode_0x370_to_0x37D(void **state)
 {
     for (int c = 0x370; c <= 0x37D; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
 static void
 unicode_0x37E(void **state)
 {
-    assert_name_char(0x37E, false);
+    assert_not_name_char(0x37E);
 }
 
 static void
 unicode_0x37F_to_0x1FFF(void **state)
 {
     for (int c = 0x37F; c <= 0x1FFF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -168,7 +178,7 @@ static void
 unicode_0x2000_to_0x200B(void **state)
 {
     for (int c = 0x2000; c <= 0x200B; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -176,7 +186,7 @@ static void
 unicode_0x200C_to_0x200D(void **state)
 {
     for (int c = 0x200C; c <= 0x200D; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -184,7 +194,7 @@ static void
 unicode_0x200E_to_0x203E(void **state)
 {
     for (int c = 0x200E; c <= 0x203E; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -192,7 +202,7 @@ static void
 unicode_0x203F_to_0x2040(void **state)
 {
     for (int c = 0x203F; c <= 0x2040; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -200,7 +210,7 @@ static void
 unicode_0x2041_to_0x206F(void **state)
 {
     for (int c = 0x2041; c <= 0x206F; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -208,7 +218,7 @@ static void
 unicode_0x2070_to_0x218F(void **state)
 {
     for (int c = 0x2070; c <= 0x218F; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -216,7 +226,7 @@ static void
 unicode_0x2190_to_0x2BFF(void **state)
 {
     for (int c = 0x2190; c <= 0x2BFF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -224,7 +234,7 @@ static void
 unicode_0x2C00_to_0x2FEF(void **state)
 {
     for (int c = 0x2C00; c <= 0x2FEF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -232,7 +242,7 @@ static void
 unicode_0x2FF0_to_0x3000(void **state)
 {
     for (int c = 0x2FF0; c <= 0x3000; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -240,7 +250,7 @@ static void
 unicode_0x3001_to_0xD7FF(void **state)
 {
     for (int c = 0x3001; c <= 0xD7FF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -248,7 +258,7 @@ static void
 unicode_0xD800_to_0xF8FF(void **state)
 {
     for (int c = 0xD800; c <= 0xF8FF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -256,7 +266,7 @@ static void
 unicode_0xF900_to_0xFDCF(void **state)
 {
     for (int c = 0xF900; c <= 0xFDCF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -264,7 +274,7 @@ static void
 unicode_0xFDD0_to_0xFDEF(void **state)
 {
     for (int c = 0xFDD0; c <= 0xFDEF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -272,7 +282,7 @@ static void
 unicode_0xFDF0_to_0xFFFD(void **state)
 {
     for (int c = 0xFDF0; c <= 0xFFFD; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -280,7 +290,7 @@ static void
 unicode_0xFFFE_to_0xFFFF(void **state)
 {
     for (int c = 0xFFFE; c <= 0xFFFF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
@@ -288,7 +298,7 @@ static void
 unicode_0x10000_to_0xEFFFF(void **state)
 {
     for (int c = 0x10000; c <= 0xEFFFF; c++) {
-        assert_name_char(c, true);
+        assert_name_char(c);
     }
 }
 
@@ -296,7 +306,7 @@ static void
 unicode_0xF0000_to_0x10FFFF(void **state)
 {
     for (int c = 0xF0000; c <= 0x10FFFF; c++) {
-        assert_name_char(c, false);
+        assert_not_name_char(c);
     }
 }
 
--- lib/common/tests/xml/pcmk__xml_is_name_start_char_test.c
+++ lib/common/tests/xml/pcmk__xml_is_name_start_char_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the Pacemaker project contributors
+ * Copyright 2024-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -10,7 +10,6 @@
 #include <crm_internal.h>
 
 #include <glib.h>                           // gchar, g_ascii_isalpha(), etc.
-#include <libxml/xmlstring.h>               // xmlGetUTF8Char()
 
 #include <crm/common/unittest_internal.h>
 #include <crm/common/xml_internal.h>
@@ -19,27 +18,37 @@
 
 /*!
  * \internal
- * \brief Assert that a Unicode character is (not) a valid XML \c NameStartChar
+ * \brief Assert that a Unicode character is a valid XML \c NameStartChar
  *
- * \param[in] c          Unicode code point of character to check
- * \param[in] reference  If \c true, \p c should be considered a valid
- *                       \c NameStartChar; if \c false, it should not
+ * \param[in] code_pt  Unicode code point of character to check
  */
 static void
-assert_name_start_char(int c, bool reference)
+assert_name_start_char(int code_pt)
 {
     gchar utf8_buf[6] = { 0, };
     int len = 4;
-    int ref_len = g_unichar_to_utf8(c, utf8_buf);
-    bool result = pcmk__xml_is_name_start_char(utf8_buf, &len);
+    int ref_len = g_unichar_to_utf8(code_pt, utf8_buf);
 
-    if (reference) {
-        assert_true(result);
-    } else {
-        assert_false(result);
-    }
+    assert_true(pcmk__xml_is_name_start_char(utf8_buf, &len));
+    assert_int_equal(len, ref_len);
+}
 
-    if ((c < 0xD800) || (c > 0xDFFF)) {
+/*!
+ * \internal
+ * \brief Assert that a Unicode character is not a valid XML \c NameStartChar
+ *
+ * \param[in] code_pt  Unicode code point of character to check
+ */
+static void
+assert_not_name_start_char(int code_pt)
+{
+    gchar utf8_buf[6] = { 0, };
+    int len = 4;
+    int ref_len = g_unichar_to_utf8(code_pt, utf8_buf);
+
+    assert_false(pcmk__xml_is_name_start_char(utf8_buf, &len));
+
+    if ((code_pt < 0xD800) || (code_pt > 0xDFFF)) {
         /* Unicode code points in the range D800 to DFFF are UTF-16 surrogate
          * pair halves. They can be represented in UTF-8, but they shouldn't
          * appear in valid UTF-8-encoded text. RFC 3629 (Nov 2003) says they
@@ -68,9 +77,9 @@ ascii(void **state)
 {
     for (int c = 0x00; c <= 0x7F; c++) {
         if (g_ascii_isalpha(c) || c == ':' || c == '_') {
-            assert_name_start_char(c, true);
+            assert_name_start_char(c);
         } else {
-            assert_name_start_char(c, false);
+            assert_not_name_start_char(c);
         }
     }
 }
@@ -79,7 +88,7 @@ static void
 unicode_0x80_to_0xBF(void **state)
 {
     for (int c = 0x80; c <= 0xBF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -87,35 +96,35 @@ static void
 unicode_0xC0_to_0xD6(void **state)
 {
     for (int c = 0xC0; c <= 0xD6; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
 static void
 unicode_0xD7(void **state)
 {
-    assert_name_start_char(0xD7, false);
+    assert_not_name_start_char(0xD7);
 }
 
 static void
 unicode_0xD8_to_0xF6(void **state)
 {
     for (int c = 0xD8; c <= 0xF6; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
 static void
 unicode_0xF7(void **state)
 {
-    assert_name_start_char(0xF7, false);
+    assert_not_name_start_char(0xF7);
 }
 
 static void
 unicode_0xF8_to_0x2FF(void **state)
 {
     for (int c = 0xF8; c <= 0x2FF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -123,7 +132,7 @@ static void
 unicode_0x300_to_0x36F(void **state)
 {
     for (int c = 0x300; c <= 0x36F; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -131,21 +140,21 @@ static void
 unicode_0x370_to_0x37D(void **state)
 {
     for (int c = 0x370; c <= 0x37D; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
 static void
 unicode_0x37E(void **state)
 {
-    assert_name_start_char(0x37E, false);
+    assert_not_name_start_char(0x37E);
 }
 
 static void
 unicode_0x37F_to_0x1FFF(void **state)
 {
     for (int c = 0x37F; c <= 0x1FFF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -153,7 +162,7 @@ static void
 unicode_0x2000_to_0x200B(void **state)
 {
     for (int c = 0x2000; c <= 0x200B; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -161,7 +170,7 @@ static void
 unicode_0x200C_to_0x200D(void **state)
 {
     for (int c = 0x200C; c <= 0x200D; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -169,7 +178,7 @@ static void
 unicode_0x200E_to_0x206F(void **state)
 {
     for (int c = 0x200E; c <= 0x206F; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -177,7 +186,7 @@ static void
 unicode_0x2070_to_0x218F(void **state)
 {
     for (int c = 0x2070; c <= 0x218F; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -185,7 +194,7 @@ static void
 unicode_0x2190_to_0x2BFF(void **state)
 {
     for (int c = 0x2190; c <= 0x2BFF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -193,7 +202,7 @@ static void
 unicode_0x2C00_to_0x2FEF(void **state)
 {
     for (int c = 0x2C00; c <= 0x2FEF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -201,7 +210,7 @@ static void
 unicode_0x2FF0_to_0x3000(void **state)
 {
     for (int c = 0x2FF0; c <= 0x3000; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -209,7 +218,7 @@ static void
 unicode_0x3001_to_0xD7FF(void **state)
 {
     for (int c = 0x3001; c <= 0xD7FF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -217,7 +226,7 @@ static void
 unicode_0xD800_to_0xF8FF(void **state)
 {
     for (int c = 0xD800; c <= 0xF8FF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -225,7 +234,7 @@ static void
 unicode_0xF900_to_0xFDCF(void **state)
 {
     for (int c = 0xF900; c <= 0xFDCF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -233,7 +242,7 @@ static void
 unicode_0xFDD0_to_0xFDEF(void **state)
 {
     for (int c = 0xFDD0; c <= 0xFDEF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -241,7 +250,7 @@ static void
 unicode_0xFDF0_to_0xFFFD(void **state)
 {
     for (int c = 0xFDF0; c <= 0xFFFD; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -249,7 +258,7 @@ static void
 unicode_0xFFFE_to_0xFFFF(void **state)
 {
     for (int c = 0xFFFE; c <= 0xFFFF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
@@ -257,7 +266,7 @@ static void
 unicode_0x10000_to_0xEFFFF(void **state)
 {
     for (int c = 0x10000; c <= 0xEFFFF; c++) {
-        assert_name_start_char(c, true);
+        assert_name_start_char(c);
     }
 }
 
@@ -265,7 +274,7 @@ static void
 unicode_0xF0000_to_0x10FFFF(void **state)
 {
     for (int c = 0xF0000; c <= 0x10FFFF; c++) {
-        assert_name_start_char(c, false);
+        assert_not_name_start_char(c);
     }
 }
 
