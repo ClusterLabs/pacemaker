194c291cad8519d202c4be995a6e1885d5d1fe9e
Reid Wahl <nrwahl@protonmail.com>
2025-12-19 01:08:21 -0800

    Refactor: libcrmcommon: Reduce duplication in pcmk__lookup_user() test
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/tests/utils/pcmk__lookup_user_test.c
+++ lib/common/tests/utils/pcmk__lookup_user_test.c
@@ -40,6 +40,10 @@ assert_lookup_user(uid_t *uid, gid_t *gid, int expected_rc, uid_t expected_uid,
     uid_t uid_orig = ((uid != NULL)? *uid : 0);
     gid_t gid_orig = ((gid != NULL)? *gid : 0);
 
+    pcmk__mock_getpwnam = true;
+
+    expect_string(__wrap_getpwnam, name, "hauser");
+
     assert_int_equal(pcmk__lookup_user("hauser", uid, gid), expected_rc);
 
     if (uid != NULL) {
@@ -50,6 +54,8 @@ assert_lookup_user(uid_t *uid, gid_t *gid, int expected_rc, uid_t expected_uid,
         assert_int_equal(*gid, expected_gid);
         *gid = gid_orig;
     }
+
+    pcmk__mock_getpwnam = false;
 }
 
 static void
@@ -78,14 +84,9 @@ getpwnam_fails(void **state)
     uid_t uid = 0;
     gid_t gid = 0;
 
-    pcmk__mock_getpwnam = true;
-
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, EIO);  // errno
     will_return(__wrap_getpwnam, NULL); // return value
     assert_lookup_user(&uid, &gid, EIO, 0, 0);
-
-    pcmk__mock_getpwnam = false;
 }
 
 static void
@@ -94,20 +95,15 @@ no_matching_pwent(void **state)
     uid_t uid = 0;
     gid_t gid = 0;
 
-    pcmk__mock_getpwnam = true;
-
     /* errno may or may not be set when no matching passwd entry is found.
      * However, if the return value is NULL and errno == 0, then we can be sure
      * no entry was found. In other words, it's sufficient but not necessary. So
      * this is our test case for "no matching entry," and we should return
      * ENOENT.
      */
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, 0);    // errno
     will_return(__wrap_getpwnam, NULL); // return value
     assert_lookup_user(&uid, &gid, ENOENT, 0, 0);
-
-    pcmk__mock_getpwnam = false;
 }
 
 static void
@@ -119,33 +115,25 @@ entry_found(void **state)
     // We don't care about the other fields of the passwd entry
     struct passwd returned_ent = { .pw_uid = 1000, .pw_gid = 1000 };
 
-    pcmk__mock_getpwnam = true;
-
     // NULL uid and NULL gid
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, 0);
     will_return(__wrap_getpwnam, &returned_ent);
     assert_lookup_user(NULL, NULL, pcmk_rc_ok, 0, 0);
 
     // Non-NULL uid and NULL gid
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, 0);
     will_return(__wrap_getpwnam, &returned_ent);
     assert_lookup_user(&uid, NULL, pcmk_rc_ok, 1000, 0);
 
     // NULL uid and non-NULL gid
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, 0);
     will_return(__wrap_getpwnam, &returned_ent);
     assert_lookup_user(NULL, &gid, pcmk_rc_ok, 0, 1000);
 
     // Non-NULL uid and non-NULL gid
-    expect_string(__wrap_getpwnam, name, "hauser");
     will_return(__wrap_getpwnam, 0);
     will_return(__wrap_getpwnam, &returned_ent);
     assert_lookup_user(&uid, &gid, pcmk_rc_ok, 1000, 1000);
-
-    pcmk__mock_getpwnam = false;
 }
 
 PCMK__UNIT_TEST(NULL, NULL,
