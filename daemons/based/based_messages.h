7aa15397888702800b7df6beabb81293f8e066fb
Reid Wahl <nrwahl@protonmail.com>
2025-12-22 02:29:14 -0800

    Refactor: based: cib_process_replace_svr() -> based_process_replace()
    Avoid using the public API prefix. Also move the definition to match the
    order in the operation table.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/based/based_messages.h
+++ daemons/based/based_messages.h
@@ -49,6 +49,10 @@ int based_process_primary(const char *op, int options, const char *section,
                           xmlNode *req, xmlNode *input, xmlNode *existing_cib,
                           xmlNode **result_cib, xmlNode **answer);
 
+int based_process_replace(const char *op, int options, const char *section,
+                          xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                          xmlNode **result_cib, xmlNode **answer);
+
 int cib_process_shutdown_req(const char *op, int options, const char *section,
                              xmlNode *req, xmlNode *input,
                              xmlNode *existing_cib, xmlNode **result_cib,
@@ -57,10 +61,6 @@ int cib_process_shutdown_req(const char *op, int options, const char *section,
 int cib_process_readwrite(const char *op, int options, const char *section,
                           xmlNode *req, xmlNode *input, xmlNode *existing_cib,
                           xmlNode **result_cib, xmlNode **answer);
-
-int cib_process_replace_svr(const char *op, int options, const char *section,
-                            xmlNode *req, xmlNode *input, xmlNode *existing_cib,
-                            xmlNode **result_cib, xmlNode **answer);
 
 int cib_process_sync(const char *op, int options, const char *section,
                      xmlNode *req, xmlNode *input, xmlNode *existing_cib,
