c91c23d32fe9b8a3b4ab5ee7f5433a875c8d6cfc
Reid Wahl <nrwahl@protonmail.com>
2025-12-21 23:43:57 -0800

    Refactor: based: New based_messages.h
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

---
+++ daemons/based/based_messages.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2004-2025 the Pacemaker project contributors
+ *
+ * The version control history for this file may have further details.
+ *
+ * This source code is licensed under the GNU Lesser General Public License
+ * version 2.1 or later (LGPLv2.1+) WITHOUT ANY WARRANTY.
+ */
+
+#ifndef BASED_MESSAGES__H
+#define BASED_MESSAGES__H
+
+#include <stdbool.h>
+
+#include <libxml/tree.h>            // xmlNode *
+
+extern bool based_is_primary;
+extern xmlNode *the_cib;
+
+int cib_process_shutdown_req(const char *op, int options, const char *section,
+                             xmlNode *req, xmlNode *input,
+                             xmlNode *existing_cib, xmlNode **result_cib,
+                             xmlNode **answer);
+
+int cib_process_noop(const char *op, int options, const char *section,
+                     xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                     xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_ping(const char *op, int options, const char *section,
+                     xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                     xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_readwrite(const char *op, int options, const char *section,
+                          xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                          xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_replace_svr(const char *op, int options, const char *section,
+                            xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                            xmlNode **result_cib, xmlNode **answer);
+
+int cib_server_process_diff(const char *op, int options, const char *section,
+                            xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                            xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_sync(const char *op, int options, const char *section,
+                     xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                     xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_sync_one(const char *op, int options, const char *section,
+                         xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                         xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_delete_absolute(const char *op, int options,
+                                const char *section, xmlNode *req,
+                                xmlNode *input, xmlNode *existing_cib,
+                                xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_upgrade_server(const char *op, int options, const char *section,
+                               xmlNode *req, xmlNode *input,
+                               xmlNode *existing_cib, xmlNode **result_cib,
+                               xmlNode **answer);
+
+int cib_process_commit_transaction(const char *op, int options,
+                                   const char *section, xmlNode *req,
+                                   xmlNode *input, xmlNode *existing_cib,
+                                   xmlNode **result_cib, xmlNode **answer);
+
+int cib_process_schemas(const char *op, int options, const char *section,
+                        xmlNode *req, xmlNode *input, xmlNode *existing_cib,
+                        xmlNode **result_cib, xmlNode **answer);
+
+void send_sync_request(const char *host);
+int sync_our_cib(xmlNode *request, bool all);
+
+#endif // BASED_MESSAGES__H
