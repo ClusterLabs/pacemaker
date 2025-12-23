ebd42791f48039157c7bffdd41b529edccb64caa
Reid Wahl <nrwahl@protonmail.com>
2025-12-23 00:15:53 -0800

    Refactor: based: Rename cib_peer_callback() to based_peer_callback()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/based/based_callbacks.c
+++ daemons/based/based_callbacks.c
@@ -1205,7 +1205,7 @@ done:
 }
 
 void
-cib_peer_callback(xmlNode * msg, void *private_data)
+based_peer_callback(xmlNode *msg, void *private_data)
 {
     const char *reason = NULL;
     const char *originator = pcmk__xe_get(msg, PCMK__XA_SRC);
--- daemons/based/pacemaker-based.c
+++ daemons/based/pacemaker-based.c
@@ -326,7 +326,7 @@ cib_cs_dispatch(cpg_handle_t handle,
         return;
     }
     pcmk__xe_set(xml, PCMK__XA_SRC, from);
-    cib_peer_callback(xml, NULL);
+    based_peer_callback(xml, NULL);
 
     pcmk__xml_free(xml);
     free(data);
