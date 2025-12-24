a24f7d3635a28f86dfa1bff1d485c8bc4bcf3b0e
Reid Wahl <nrwahl@protonmail.com>
2025-12-23 16:18:46 -0800

    Refactor: based: Functionize updating client notify flags
    Also move enum based_notify_flags and based_parse_notify_flags into
    based_notify.c. No other file needs them now.
    
    Technically we don't need based_nf_none or based_parse_notify_flags()
    since we only have one meaningful flag. Those seemed clearer than
    checking directly for PCMK__VALUE_CIB_DIFF_NOTIFY and setting the
    based_nf_diff flag directly in based_update_notify_flags().
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/based/based_notify.h
+++ daemons/based/based_notify.h
@@ -10,25 +10,11 @@
 #ifndef BASED_NOTIFY__H
 #define BASED_NOTIFY__H
 
-#include <stdint.h>                 // UINT64_C
-
 #include <libxml/tree.h>            // xmlNode
 
-/*!
- * \internal
- * \brief Flags for CIB manager client notification types
- *
- * These are used for setting the \c flags field of a \c pcmk__client_t.
- */
-enum based_notify_flags {
-    //! This flag has no effect
-    based_nf_none = UINT64_C(0),
+#include <crm/common/internal.h>    // pcmk__client_t
 
-    //! Notify when the CIB changes
-    based_nf_diff = (UINT64_C(1) << 0),
-};
-
-enum based_notify_flags based_parse_notify_flag(const char *text);
+int based_update_notify_flags(const xmlNode *xml, pcmk__client_t *client);
 
 void based_diff_notify(const char *op, int result, const char *call_id,
                        const char *client_id, const char *client_name,
