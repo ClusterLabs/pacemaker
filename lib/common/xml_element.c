26b5111a927c6712bced382b5e6105114e0950c9
Reid Wahl <nrwahl@protonmail.com>
2025-12-23 18:10:30 -0800

    Refactor: libcrmcommon: Correct the Doxygen for pcmk__xe_attr_is_true()
    And simplify the body. The value doesn't have to be "true"
    (PCMK_VALUE_TRUE); it can be anything that pcmk__parse_bool() parses to
    true.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/xml_element.c
+++ lib/common/xml_element.c
@@ -1530,17 +1530,17 @@ pcmk__xe_set_bool(xmlNode *xml, const char *attr, bool value)
  * \param[in] node XML node to get attribute from
  * \param[in] name XML attribute to get
  *
- * \return True if the given \p name is an attribute on \p node and has
- *         the value \c PCMK_VALUE_TRUE, False in all other cases
+ * \return \c true if the given \p name is an attribute on \p node whose value
+ *         parses to \c true (see \c pcmk__parse_bool()), or \c false otherwise
  */
 bool
 pcmk__xe_attr_is_true(const xmlNode *node, const char *name)
 {
     bool value = false;
-    int rc;
 
-    rc = pcmk__xe_get_bool(node, name, &value);
-    return rc == pcmk_rc_ok && value == true;
+    // value remains false on error, so don't check return value
+    (void) pcmk__xe_get_bool(node, name, &value);
+    return value;
 }
 
 // Deprecated functions kept only for backward API compatibility
