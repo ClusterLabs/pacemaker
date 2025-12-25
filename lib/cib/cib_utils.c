ClusterLabs/pacemaker

commit 53d4d3474ed94132313cbca4ee566f670e33daf8

Refactor: various: Remove internal uses of crm_xml_replace()
crm_xml_replace() is basically a version of crm_xml_add() that removes
the attribute if the value argument is NULL.

It logs an assertion and doesn't update the attribute if the old value
matches the new value. This has the potential to cause some strange
memory errors, and it's difficult to update callers to be sure they're
safe.

cib_perform_op() is the only caller where it's unknown whether the value
argument is NULL. There, we now call xml_remove_prop() if value is NULL
or crm_xml_add() otherwise. In the other callers, we know whether value
is NULL, and we can unconditionally call the appropriate function.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_utils.c
@@ -485,13 +485,29 @@ cib_perform_op(const char *op, int call_options, cib__op_fn_t fn, bool is_query,
 
             /* Does the CIB support the "update-*" attributes... */
             if (current_schema >= minimum_schema) {
+                /* Ensure values of origin, client, and user in scratch match
+                 * the values in req
+                 */
                 const char *origin = crm_element_value(req, F_ORIG);
+                const char *client = crm_element_value(req, F_CIB_CLIENTNAME);
 
-                CRM_LOG_ASSERT(origin != NULL);
-                crm_xml_replace(scratch, XML_ATTR_UPDATE_ORIG, origin);
-                crm_xml_replace(scratch, XML_ATTR_UPDATE_CLIENT,
-                                crm_element_value(req, F_CIB_CLIENTNAME));
-                crm_xml_replace(scratch, XML_ATTR_UPDATE_USER, crm_element_value(req, F_CIB_USER));
+                if (origin != NULL) {
+                    crm_xml_add(scratch, XML_ATTR_UPDATE_ORIG, origin);
+                } else {
+                    xml_remove_prop(scratch, XML_ATTR_UPDATE_ORIG);
+                }
+
+                if (client != NULL) {
+                    crm_xml_add(scratch, XML_ATTR_UPDATE_CLIENT, user);
+                } else {
+                    xml_remove_prop(scratch, XML_ATTR_UPDATE_CLIENT);
+                }
+
+                if (user != NULL) {
+                    crm_xml_add(scratch, XML_ATTR_UPDATE_USER, user);
+                } else {
+                    xml_remove_prop(scratch, XML_ATTR_UPDATE_USER);
+                }
             }
         }
     }

daemons/attrd/attrd_corosync.c
@@ -476,9 +476,7 @@ attrd_peer_clear_failure(pcmk__request_t *request)
     crm_xml_add(xml, PCMK__XA_TASK, PCMK__ATTRD_CMD_UPDATE);
 
     /* Make sure value is not set, so we delete */
-    if (crm_element_value(xml, PCMK__XA_ATTR_VALUE)) {
-        crm_xml_replace(xml, PCMK__XA_ATTR_VALUE, NULL);
-    }
+    xml_remove_prop(xml, PCMK__XA_ATTR_VALUE);
 
     g_hash_table_iter_init(&iter, attributes);
     while (g_hash_table_iter_next(&iter, (gpointer *) &attr, NULL)) {

daemons/attrd/attrd_ipc.c
@@ -140,12 +140,8 @@ attrd_client_clear_failure(pcmk__request_t *request)
     }
 
     /* Make sure attribute and value are not set, so we delete via regex */
-    if (crm_element_value(xml, PCMK__XA_ATTR_NAME)) {
-        crm_xml_replace(xml, PCMK__XA_ATTR_NAME, NULL);
-    }
-    if (crm_element_value(xml, PCMK__XA_ATTR_VALUE)) {
-        crm_xml_replace(xml, PCMK__XA_ATTR_VALUE, NULL);
-    }
+    xml_remove_prop(xml, PCMK__XA_ATTR_NAME);
+    xml_remove_prop(xml, PCMK__XA_ATTR_VALUE);
 
     return attrd_client_update(request);
 }
@@ -283,7 +279,7 @@ expand_regexes(xmlNode *xml, const char *attr, const char *value, const char *re
                  * regex and replace it with the name.
                  */
                 attrd_copy_xml_attributes(xml, child);
-                crm_xml_replace(child, PCMK__XA_ATTR_PATTERN, NULL);
+                xml_remove_prop(child, PCMK__XA_ATTR_PATTERN);
                 crm_xml_add(child, PCMK__XA_ATTR_NAME, attr);
             }
         }

lib/common/xml.c
@@ -1714,6 +1714,10 @@ pcmk__xml2fd(int fd, xmlNode *cur)
 void
 xml_remove_prop(xmlNode * obj, const char *name)
 {
+    if (crm_element_value(obj, name) == NULL) {
+        return;
+    }
+
     if (pcmk__check_acl(obj, NULL, pcmk__xf_acl_write) == FALSE) {
         crm_trace("Cannot remove %s from %s", name, obj->name);
 

lib/pengine/complex.c
@@ -297,7 +297,7 @@ unpack_template(xmlNode * xml_obj, xmlNode ** expanded_xml, pe_working_set_t * d
 
     new_xml = copy_xml(template);
     xmlNodeSetName(new_xml, xml_obj->name);
-    crm_xml_replace(new_xml, XML_ATTR_ID, id);
+    crm_xml_add(new_xml, XML_ATTR_ID, id);
 
     clone = crm_element_value(xml_obj, XML_RSC_ATTR_INCARNATION);
     if(clone) {

commit 47b31a6203523e98f1986e1754e17f12cc809203

Fix: libcib: Fix setting origin attributes in CIB updates
This fixes a regression introduced by 53d4d347. The update-client
attribute was being set to the user instead of the update client.

This affects the outputs of the following public API functions and CLI
tools (via the cluster-status message with pcmk_section_times set):
* pcmk_simulate()
* pcmk_status()
* crm_mon
* crm_simulate

Also fix an indentation issue found while investigating what this
affects.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_utils.c
@@ -519,7 +519,7 @@ cib_perform_op(cib_t *cib, const char *op, uint32_t call_options,
             }
 
             if (client != NULL) {
-                pcmk__xe_set(scratch, PCMK_XA_UPDATE_CLIENT, user);
+                pcmk__xe_set(scratch, PCMK_XA_UPDATE_CLIENT, client);
             } else {
                 pcmk__xe_remove_attr(scratch, PCMK_XA_UPDATE_CLIENT);
             }

lib/pengine/pe_output.c
@@ -457,7 +457,7 @@ cluster_summary(pcmk__output_t *out, va_list args) {
         const char *client = pcmk__xe_get(scheduler->input,
                                           PCMK_XA_UPDATE_CLIENT);
         const char *origin = pcmk__xe_get(scheduler->input,
-                                               PCMK_XA_UPDATE_ORIGIN);
+                                          PCMK_XA_UPDATE_ORIGIN);
 
         PCMK__OUTPUT_LIST_HEADER(out, false, rc, "Cluster Summary");
         out->message(out, "cluster-times", scheduler->priv->local_node_name,