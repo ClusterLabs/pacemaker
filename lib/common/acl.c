1fa9e2ba4dbb5cdb93f81e826a1d5153032c9ec0
Reid Wahl <nrwahl@protonmail.com>
2025-12-24 17:03:32 -0800

    Refactor: libcrmcommon: Unindent a block of xml_acl_filtered_copy()
    If the "else"/"docpriv->acls == NULL" block was executed, then target
    was set to NULL, which meant we wouldn't do anything else before
    returning true.
    
    Also drop some redundancies:
    * The dropped trace message doesn't seem very helpul.
    * pcmk__xml_copy() is guaranteed to return non-NULL if its second
      argument is non-NULL.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/acl.c
+++ lib/common/acl.c
@@ -484,20 +484,16 @@ xml_acl_filtered_copy(const char *user, xmlNode *acl_source, xmlNode *xml,
 
     *result = NULL;
     if ((xml == NULL) || !pcmk_acl_required(user)) {
-        pcmk__trace("Not filtering XML because ACLs not required for user '%s'",
-                    user);
         return false;
     }
 
-    pcmk__trace("Filtering XML copy using user '%s' ACLs", user);
     target = pcmk__xml_copy(NULL, xml);
-    if (target == NULL) {
-        return true;
-    }
+    docpriv = target->doc->_private;
 
     pcmk__enable_acl(acl_source, target, user);
 
-    docpriv = target->doc->_private;
+    pcmk__trace("Filtering XML copy using user '%s' ACLs", user);
+
     for (const GList *iter = docpriv->acls; (iter != NULL) && (target != NULL);
          iter = iter->next) {
 
@@ -543,19 +539,17 @@ xml_acl_filtered_copy(const char *user, xmlNode *acl_source, xmlNode *xml,
         return true;
     }
 
-    if (docpriv->acls) {
-        g_list_free_full(docpriv->acls, free_acl);
-        docpriv->acls = NULL;
-
-    } else {
+    if (docpriv->acls == NULL) {
         pcmk__trace("User '%s' without ACLs denied access to entire XML "
-                    "document",
-                    user);
+                    "document", user);
         pcmk__xml_free(target);
-        target = NULL;
+        return true;
     }
 
-    if (target) {
+    g_list_free_full(docpriv->acls, free_acl);
+    docpriv->acls = NULL;
+
+    if (target != NULL) {
         *result = target;
     }
 
