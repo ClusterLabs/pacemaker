91befb85766b8094274df5abfeea5b69a0c97e22
Reid Wahl <nrwahl@protonmail.com>
2025-12-31 23:50:20 -0800

    Refactor: libcrmcommon: Drop redundant check in xml_create_patchset_v2()
    This is a static function with one caller (xml_create_patchset()), which
    already checks whether the doc has the dirty flag set. This lets us
    clearly make two guarantees:
    * xml_create_patchset_v2() returns non-NULL.
    * xml_create_patchset() doesn't change *config_changed if its return
      value is NULL.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/patchset.c
+++ lib/common/patchset.c
@@ -182,6 +182,7 @@ is_config_change(xmlNode *xml)
     return FALSE;
 }
 
+// Guaranteed to return non-NULL
 static xmlNode *
 xml_create_patchset_v2(const xmlNode *source, xmlNode *target)
 {
@@ -194,11 +195,6 @@ xml_create_patchset_v2(const xmlNode *source, xmlNode *target)
     xmlNode *patchset = NULL;
 
     pcmk__assert(target != NULL);
-
-    if (!pcmk__xml_doc_all_flags_set(target->doc, pcmk__xf_dirty)) {
-        return NULL;
-    }
-
     pcmk__assert(target->doc != NULL);
     docpriv = target->doc->_private;
 
@@ -242,6 +238,7 @@ xml_create_patchset_v2(const xmlNode *source, xmlNode *target)
     return patchset;
 }
 
+// *config_changed is unchanged if the return value is NULL
 xmlNode *
 xml_create_patchset(int format, const xmlNode *source, xmlNode *target,
                     bool *config_changed, bool manage_version)
