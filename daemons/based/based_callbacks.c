7ab528ba3f3fc47f544392d0c734b22687f65838
Reid Wahl <nrwahl@protonmail.com>
2026-01-02 01:20:07 -0800

    Refactor: libcib: Drop op arg from cib__perform_query()/cib_perform_op()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/based/based_callbacks.c
+++ daemons/based/based_callbacks.c
@@ -597,7 +597,7 @@ cib_process_command(xmlNode *request, const cib__operation_t *operation,
     input = prepare_input(request, operation->type, &section);
 
     if (!pcmk__is_set(operation->flags, cib__op_attr_modifies)) {
-        rc = cib__perform_query(op, call_options, op_function, section, request,
+        rc = cib__perform_query(call_options, op_function, section, request,
                                 input, &the_cib, &output);
         goto done;
     }
@@ -609,7 +609,7 @@ cib_process_command(xmlNode *request, const cib__operation_t *operation,
      * It's not important whether the client variant is cib_native or
      * cib_remote.
      */
-    rc = cib_perform_op(cib_undefined, op, call_options, op_function, section,
+    rc = cib_perform_op(cib_undefined, call_options, op_function, section,
                         request, input, &config_changed, &the_cib, &result_cib,
                         &cib_diff, &output);
 
