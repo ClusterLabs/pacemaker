1cc930e623576d0f4c591ab8d30e7a4d32d3726c
Reid Wahl <nrwahl@protonmail.com>
2026-01-11 20:18:32 -0800

    Refactor: libcrmcommon: Use getgrnam() in pcmk__is_user_in_group()
    This matches the way we do it in the CIB manager
    (is_daemon_group_member()) and will let us get rid of that function and
    use this instead.
    
    This required a mocking overhaul, which I'm not thrilled about, but it
    seems correct and is simpler than before.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/tests/utils/pcmk__is_user_in_group_test.c
+++ lib/common/tests/utils/pcmk__is_user_in_group_test.c
@@ -14,39 +14,67 @@
 #include "../../crmcommon_private.h"
 #include "mock_private.h"
 
+#define assert_user_in_group(user, group, expected)             \
+    do {                                                        \
+        /* Primary group: grp1 */                               \
+        const struct passwd entry = { .pw_gid = 1 };            \
+                                                                \
+        expect_string(__wrap_getpwnam, name, user);             \
+        will_return(__wrap_getpwnam, 0);                        \
+        will_return(__wrap_getpwnam, &entry);                   \
+                                                                \
+        if (expected) {                                         \
+            assert_true(pcmk__is_user_in_group(user, group));   \
+        } else {                                                \
+            assert_false(pcmk__is_user_in_group(user, group));  \
+        }                                                       \
+    } while (0);
+
 static int
 setup(void **state)
 {
-    pcmk__mock_grent = true;
+    pcmk__mock_getgrnam = true;
+    pcmk__mock_getpwnam = true;
     return 0;
 }
 
 static int
 teardown(void **state)
 {
-    pcmk__mock_grent = false;
+    pcmk__mock_getgrnam = false;
+    pcmk__mock_getpwnam = false;
     return 0;
 }
 
 static void
 null_args(void **state)
 {
-    assert_false(pcmk__is_user_in_group(NULL, NULL));
-    assert_false(pcmk__is_user_in_group(NULL, "grp0"));
-    assert_false(pcmk__is_user_in_group("user0", NULL));
+    pcmk__assert_asserts(pcmk__is_user_in_group(NULL, NULL));
+    pcmk__assert_asserts(pcmk__is_user_in_group(NULL, "grp0"));
+    pcmk__assert_asserts(pcmk__is_user_in_group("user0", NULL));
 }
 
 static void
 user_in_group(void **state)
 {
-    assert_true(pcmk__is_user_in_group("user0", "grp0"));
+    // user0 is not in grp1's member list
+    assert_user_in_group("user0", "grp1", true);
+
+    // user1 has grp1 as primary group and is also in grp1's member list
+    assert_user_in_group("user1", "grp1", true);
+
+    // user1 has grp1 as primary group but is in grp0's member list
+    assert_user_in_group("user1", "grp0", true);
 }
 
 static void
 user_not_in_group(void **state)
 {
-    assert_false(pcmk__is_user_in_group("user0", "nonexistent_group"));
-    assert_false(pcmk__is_user_in_group("user2", "grp0"));
+    // Group does not exist
+    assert_user_in_group("user0", "nonexistent_group", false);
+
+    // Group exists but user is not a member
+    assert_user_in_group("user2", "grp0", false);
 }
 
 PCMK__UNIT_TEST(setup, teardown,
