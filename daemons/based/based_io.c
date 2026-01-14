ClusterLabs/pacemaker

commit 863f5bb1cd38f53ff5146272474da6604a119e55

Refactor: based: Use G_SOURCE_{CONTINUE,REMOVE} in write_cib_contents()
Also, drop the return statement after calling _exit(), and rename
exit_rc to rc.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/based/based_io.c
@@ -66,7 +66,7 @@ cib_diskwrite_complete(mainloop_child_t * p, pid_t pid, int core, int signo, int
 static int
 write_cib_contents(gpointer p)
 {
-    int exit_rc = pcmk_ok;
+    int rc = pcmk_ok;
     xmlNode *cib_local = NULL;
     crm_exit_t exit_code = CRM_EX_OK;
 
@@ -87,7 +87,7 @@ write_cib_contents(gpointer p)
         pcmk__err("Disabling disk writes after fork failure: %s",
                   pcmk_rc_str(errno));
         cib_writes_enabled = FALSE;
-        return FALSE;
+        return G_SOURCE_REMOVE;
     }
 
     if (pid) {
@@ -98,7 +98,7 @@ write_cib_contents(gpointer p)
             qb_log_ctl(QB_LOG_BLACKBOX, QB_LOG_CONF_ENABLED, QB_TRUE);
         }
 
-        return -1;          /* -1 means 'still work to do' */
+        return G_SOURCE_CONTINUE;
     }
 
     /* Asynchronous write-out after a fork() */
@@ -109,12 +109,12 @@ write_cib_contents(gpointer p)
     cib_local = pcmk__xml_copy(NULL, the_cib);
 
     /* Write the CIB */
-    exit_rc = cib_file_write_with_digest(cib_local, cib_root, "cib.xml");
+    rc = cib_file_write_with_digest(cib_local, cib_root, "cib.xml");
 
     /* A nonzero exit code will cause further writes to be disabled */
     pcmk__xml_free(cib_local);
 
-    switch (exit_rc) {
+    switch (rc) {
         case pcmk_ok:
             exit_code = CRM_EX_OK;
             break;
@@ -133,8 +133,6 @@ write_cib_contents(gpointer p)
     /* Use _exit() because exit() could affect the parent adversely */
     pcmk_common_cleanup();
     _exit(exit_code);
-
-    return exit_rc;
 }
 
 /*!

commit 48bba91a6756d14b463c2d8daaee496321427ee8

Fix: based: Return correct code from write_cib_async()
This fixes a regression introduced by 863f5bb1. The bug has not made it
into any release.

The issue was detected by cts-lab. On some tests, and pretty
consistently when the cts-lab was starting up just before the first
test, we saw messages like the following:

Jan 13 19:32:06	BadNews: 1768361495.879763 fastvm-fedora42-24 pacemaker-based[245023]:  error: Digest comparison failed: expected 0b48f5d25147555a151659941880d50e, calculated fb2765a99c8dd65d0054350b4892c171
Jan 13 19:32:06	BadNews: 1768361495.879822 fastvm-fedora42-24 pacemaker-based[245023]:  error: /var/lib/pacemaker/cib/cib.xml was manually modified while the cluster was active!
Jan 13 19:32:06	BadNews: 1768361495.885858 fastvm-fedora42-24 pacemaker-based[243474]:  error: Disabling CIB disk writes after failure: process 245023 exited 104
Jan 13 19:32:07	BadNews: 1768361460.711935 fastvm-fedora42-22 pacemaker-based[261565]:  error: Digest comparison failed: expected 8d27368b7f2ee1c5f2a8947e5a630c78, calculated 35539cbd81959081be2ed02e04ea0c44
Jan 13 19:32:07	BadNews: 1768361460.712250 fastvm-fedora42-22 pacemaker-based[261565]:  error: /var/lib/pacemaker/cib/cib.xml was manually modified while the cluster was active!
Jan 13 19:32:07	BadNews: 1768361460.736050 fastvm-fedora42-22 pacemaker-based[261378]:  error: Disabling CIB disk writes after failure: process 261565 exited 104

It's not clear exactly how or why this was happening, but a trigger
callback function is supposed to return -1 when the job is not yet
complete but should not be removed from the main loop. Commit 863r5bb1
replaced the -1 return value with G_SOURCE_CONTINUE, based on the
misunderstanding that -1 was being cast to TRUE as it would be in a
GSourceFunc. The -1 was correct and necessary, however.

I presume that the issue arose in crm_trigger_dispatch() when the
trigger was set and dispatched again. If the running flag is set, we
don't call the callback function.

daemons/based/based_io.c
@@ -106,7 +106,9 @@ write_cib_async(gpointer user_data)
         pcmk__err("Disabling disk writes after fork failure: %s",
                   strerror(errno));
         writes_enabled = false;
-        return G_SOURCE_REMOVE;
+
+        // Remove trigger from main loop
+        return 0;
     }
 
     if (pid > 0) {
@@ -117,7 +119,10 @@ write_cib_async(gpointer user_data)
             qb_log_ctl(QB_LOG_BLACKBOX, QB_LOG_CONF_ENABLED, QB_TRUE);
         }
 
-        return G_SOURCE_CONTINUE;
+        /* Mark job as running and keep trigger. write_cib_cb() will mark it as
+         * complete.
+         */
+        return -1;
     }
 
     /* Write the CIB. Note that this modifies the_cib, but this child is about