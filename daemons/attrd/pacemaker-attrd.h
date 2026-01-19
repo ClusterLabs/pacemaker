352df17178126f84cd3bbe43ca042ebfa9ffd475
Reid Wahl <nrwahl@protonmail.com>
2026-01-19 15:23:00 -0700

    Refactor: attrd: Return void from attrd_send_message()
    There are nine calls and none of them check the return value.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- daemons/attrd/pacemaker-attrd.h
+++ daemons/attrd/pacemaker-attrd.h
@@ -8,7 +8,7 @@
  */
 
 #ifndef PACEMAKER_ATTRD__H
-#  define PACEMAKER_ATTRD__H
+#define PACEMAKER_ATTRD__H
 
 #include <regex.h>
 #include <stdbool.h>
@@ -210,8 +210,8 @@ void attrd_client_clear_failure(pcmk__request_t *request);
 void attrd_client_update(pcmk__request_t *request);
 void attrd_client_refresh(pcmk__request_t *request);
 xmlNode *attrd_client_query(pcmk__request_t *request);
-gboolean attrd_send_message(const pcmk__node_status_t *node, xmlNode *data,
-                            bool confirm);
+void attrd_send_message(const pcmk__node_status_t *node, xmlNode *data,
+                        bool confirm);
 
 xmlNode *attrd_add_value_xml(xmlNode *parent, const attribute_t *a,
                              const attribute_value_t *v, bool force_write);
@@ -267,4 +267,4 @@ void attrd_set_node_xml_id(const char *node_name, const char *node_xml_id);
 void attrd_forget_node_xml_id(const char *node_name);
 void attrd_cleanup_xml_ids(void);
 
-#endif /* PACEMAKER_ATTRD__H */
+#endif  // PACEMAKER_ATTRD__H
