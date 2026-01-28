0ec3b8cd6271c27c7ca4713cfaacccfe4da71d94
Chris Lumens <clumens@redhat.com>
2026-01-28 12:46:25 -0500

    Refactor: python: Duplicate the LogFactory class at the module level
    This is the first step in getting rid of the LogFactory class entirely.
    First, we'll just duplicate all its functions as module-level functions
    and then modify those in the class to use the same global variables.
    
    Ref T680

--- python/pacemaker/_cts/logging.py
+++ python/pacemaker/_cts/logging.py
@@ -1,12 +1,51 @@
 """Logging classes for Pacemaker's Cluster Test Suite (CTS)."""
 
-__all__ = ["LogFactory"]
-__copyright__ = "Copyright 2014-2025 the Pacemaker project contributors"
+__all__ = ["LogFactory", "add_file", "add_stderr", "log", "debug", "traceback"]
+__copyright__ = "Copyright 2014-2026 the Pacemaker project contributors"
 __license__ = "GNU General Public License version 2 or later (GPLv2+) WITHOUT ANY WARRANTY"
 
 import os
 import sys
 import time
+
+
+_log_methods = []
+_HAVE_STDERR = False
+
+
+def add_file(filename):
+    """When logging messages, log them to specified file."""
+    if filename:
+        _log_methods.append(FileLog(filename))
+
+
+def add_stderr():
+    """When logging messages, log them to standard error."""
+    # pylint: disable=global-statement
+    global _HAVE_STDERR
+
+    if not _HAVE_STDERR:
+        _HAVE_STDERR = True
+        _log_methods.append(StdErrLog(None))
+
+
+def log(args):
+    """Log a message (to all configured log destinations)."""
+    for logfn in _log_methods:
+        logfn(args.strip())
+
+
+def debug(args):
+    """Log a debug message (to all configured log destinations)."""
+    for logfn in _log_methods:
+        if logfn.is_debug_target:
+            logfn(f"debug: {args.strip()}")
+
+
+def traceback(traceback_obj):
+    """Log a stack trace (to all configured log destinations)."""
+    for logfn in _log_methods:
+        traceback_obj.print_exc(50, logfn)
 
 
 class Logger:
@@ -82,32 +121,32 @@ class FileLog(Logger):
 class LogFactory:
     """Singleton to log messages to various destinations."""
 
-    log_methods = []
-    have_stderr = False
-
     def add_file(self, filename):
         """When logging messages, log them to specified file."""
         if filename:
-            LogFactory.log_methods.append(FileLog(filename))
+            _log_methods.append(FileLog(filename))
 
     def add_stderr(self):
         """When logging messages, log them to standard error."""
-        if not LogFactory.have_stderr:
-            LogFactory.have_stderr = True
-            LogFactory.log_methods.append(StdErrLog(None))
+        # pylint: disable=global-statement
+        global _HAVE_STDERR
+
+        if not _HAVE_STDERR:
+            _HAVE_STDERR = True
+            _log_methods.append(StdErrLog(None))
 
     def log(self, args):
         """Log a message (to all configured log destinations)."""
-        for logfn in LogFactory.log_methods:
+        for logfn in _log_methods:
             logfn(args.strip())
 
     def debug(self, args):
         """Log a debug message (to all configured log destinations)."""
-        for logfn in LogFactory.log_methods:
+        for logfn in _log_methods:
             if logfn.is_debug_target:
                 logfn(f"debug: {args.strip()}")
 
-    def traceback(self, traceback):
+    def traceback(self, traceback_obj):
         """Log a stack trace (to all configured log destinations)."""
-        for logfn in LogFactory.log_methods:
-            traceback.print_exc(50, logfn)
+        for logfn in _log_methods:
+            traceback_obj.print_exc(50, logfn)
