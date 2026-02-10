ClusterLabs/pacemaker

commit bcc7c90cdf688b0d8e4016824ff538d9326a8e23

Refactor: based: Drop redundant checks in is_daemon_group_member()
Previously, we were looking up the user, making sure we found it,
looking up the user's primary group, and checking whether it was
CRM_DAEMON_GROUP. If not, then we fell back to looking up
CRM_DAEMON_GROUP and checking whether user was in its member list.

That fallback is all we need. The additional checks were added by commit
14d9ae4. No reasoning was provided.

I'm guessing they were added in the name of efficiency in the common
case. But they required two additional system calls, so I doubt that was
any more efficient than iterating over the list of group members and
doing string comparisons. And since this is done only once per remote
client, efficiency gains probably don't matter much.

Also add Doxygen.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/based/based_remote.c
@@ -19,7 +19,6 @@
 #include <stdlib.h>                 // calloc, free, getenv
 #include <string.h>                 // memset
 #include <sys/socket.h>             // sockaddr{,_storage}, AF_INET, etc.
-#include <sys/types.h>              // gid_t
 #include <unistd.h>                 // close
 
 #include <glib.h>                   // gboolean, gpointer, g_source_remove, etc.
@@ -84,47 +83,36 @@ remote_auth_timeout_cb(gpointer data)
     return G_SOURCE_REMOVE;
 }
 
+/*!
+ * \internal
+ * \brief Check whether a given user is a member of \c CRM_DAEMON_GROUP
+ *
+ * \param[in] user  User name
+ *
+ * \return \c true if \p user is a member of \c CRM_DAEMON_GROUP, or \c false
+ *         otherwise
+ */
 static bool
-is_daemon_group_member(const char *usr)
+is_daemon_group_member(const char *user)
 {
-    int index = 0;
-    gid_t gid = 0;
-    struct group *group = NULL;
-    int rc = pcmk_rc_ok;
+    const struct group *group = getgrnam(CRM_DAEMON_GROUP);
 
-    rc = pcmk__lookup_user(usr, NULL, &gid);
-    if (rc != pcmk_rc_ok) {
-        pcmk__notice("Rejecting remote client: could not find user '%s': %s",
-                     usr, pcmk_rc_str(rc));
-        return false;
-    }
-
-    group = getgrgid(gid);
-    if ((group != NULL)
-        && pcmk__str_eq(group->gr_name, CRM_DAEMON_GROUP, pcmk__str_none)) {
-        return true;
-    }
-
-    group = getgrnam(CRM_DAEMON_GROUP);
     if (group == NULL) {
         pcmk__err("Rejecting remote client: " CRM_DAEMON_GROUP " is not a "
                   "valid group");
         return false;
     }
 
-    while (true) {
-        char *member = group->gr_mem[index++];
+    for (const char *const *member = (const char *const *) group->gr_mem;
+         *member != NULL; member++) {
 
-        if (member == NULL) {
-            break;
-
-        } else if (pcmk__str_eq(usr, member, pcmk__str_none)) {
+        if (pcmk__str_eq(user, *member, pcmk__str_none)) {
             return true;
         }
     }
 
-    pcmk__notice("Rejecting remote client: User %s is not a member of group "
-                 CRM_DAEMON_GROUP, usr);
+    pcmk__notice("Rejecting remote client: User %s is not a member of group %s",
+                 user, CRM_DAEMON_GROUP);
     return false;
 }
 

commit 07db76e5c8b0902b1728a65d2c13aae4bbde5660

Med: daemons: Fix user/group checking in based.
This fixes a bug introduced by bcc7c90cdf6.  The group members returned
by getgrnam will only include those users that are listed in /etc/group
for that group.  It won't include any users for which a group is their
primary.

In other words, if this is what's in /etc/passwd:

    hacluster:x:189:189:cluster user:/var/lib/pacemaker:/sbin/nologin

And this is what's in /etc/group:

    haclient:x:189:

Then getgrnam will not list hacluster as a member of the haclient group
and is_daemon_group_member will return false.  We need to re-introduce
the primary group check to fix this.

daemons/based/based_remote.c
@@ -95,14 +95,36 @@ remote_auth_timeout_cb(gpointer data)
 static bool
 is_daemon_group_member(const char *user)
 {
-    const struct group *group = getgrnam(CRM_DAEMON_GROUP);
+    int rc = pcmk_rc_ok;
+    gid_t gid = 0;
+    const struct group *group = NULL;
 
+    /* group->gr_mem only contains those users that are listed in /etc/group.
+     * It won't list the user if the group is their primary (that is, it's in
+     * the GID field in /etc/passwd (or passwd->pw_gid as returned by getpwent).
+     * So, we first need to perform a primary group check.
+     */
+    rc = pcmk__lookup_user(user, NULL, &gid);
+    if (rc != pcmk_rc_ok) {
+        pcmk__notice("Rejecting remote client: could not find user '%s': %s",
+                     user, pcmk_rc_str(rc));
+        return false;
+    }
+
+    group = getgrnam(CRM_DAEMON_GROUP);
     if (group == NULL) {
         pcmk__err("Rejecting remote client: " CRM_DAEMON_GROUP " is not a "
                   "valid group");
         return false;
     }
 
+    if (group->gr_gid == gid) {
+        return true;
+    }
+
+    /* If that didn't work, check if CRM_DAEMON_GROUP is a secondary group for
+     * the user.
+     */
     for (const char *const *member = (const char *const *) group->gr_mem;
          *member != NULL; member++) {
 