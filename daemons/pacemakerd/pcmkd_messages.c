ClusterLabs/pacemaker

commit 6f7dd0b2bc87c1b92281a76f6551f4eea93eef57

Refactor: pacemakerd: Use g_str_has_prefix() in handle_ping_request()
As far as I can tell, the clients that set crm_system_name to a value
that contains "sbd" set it to a value that STARTS with "sbd".

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/pacemakerd/pcmkd_messages.c
@@ -76,7 +76,7 @@ handle_ping_request(pcmk__request_t *request)
     }
 
     /* just proceed state on sbd pinging us */
-    if (from && strstr(from, "sbd")) {
+    if ((from != NULL) && g_str_has_prefix(from, "sbd")) {
         if (pcmk__str_eq(pacemakerd_state, PCMK__VALUE_SHUTDOWN_COMPLETE,
                          pcmk__str_none)) {
             if (pcmk__get_sbd_sync_resource_startup()) {

commit 19967cf3ebb49d4736aa08d528eaa93fafd8d98b

Fix: pacemakerd: Correctly detect ping requests from sbd
This fixes a regression introduced by 6f7dd0b2bc, which has not made it
into any release. Even though the ipc_name is set to "sbd:pcmk" by sbd,
do_pacemakerd_api_call() generates the sender_system in the format
"<pid>_sbd:pcmk".

daemons/pacemakerd/pcmkd_messages.c
@@ -75,8 +75,10 @@ handle_ping_request(pcmk__request_t *request)
         pcmk__set_result(&request->result, CRM_EX_OK, PCMK_EXEC_DONE, NULL);
     }
 
-    /* just proceed state on sbd pinging us */
-    if ((from != NULL) && g_str_has_prefix(from, "sbd")) {
+    /* Just proceed state on sbd pinging us.
+     * The "from" string is in the format "<pid>_sbd:pcmk".
+     */
+    if ((from != NULL) && (strstr(from, "sbd") != NULL)) {
         if (pcmk__str_eq(pacemakerd_state, PCMK__VALUE_SHUTDOWN_COMPLETE,
                          pcmk__str_none)) {
             if (pcmk__get_sbd_sync_resource_startup()) {