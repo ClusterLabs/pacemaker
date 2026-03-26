ClusterLabs/pacemaker

commit 5aba8f7b4788393b99a7368d00d07ae82fbe067b

Refactor: libcrmcommon: Simplify pcmk__add_logfile()
Don't bother opening in append mode with fopen() unless we're root. In
that case, do it in set_logfile_permissions() to limit scope of the file
descriptor. We use that file descriptor only for passing to fstat(),
fchown(), and fchmod().

This also lets us simplify the error message, since the user must be
root, and lets us drop a fileno() error check.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/logging.c
@@ -367,32 +367,44 @@ chmod_logfile(const char *filename, int logfd)
     }
 }
 
-// If we're root, correct a log file's permissions if needed
+/*!
+ * \internal
+ * \brief If we're root, ensure a log file has correct permissions and ownership
+ *
+ * \param[in] filename  Log file name
+ *
+ * \return Standard Pacemaker return code
+ */
 static int
-set_logfile_permissions(const char *filename, FILE *logfile)
+set_logfile_permissions(const char *filename)
 {
     int fd = 0;
     int rc = pcmk_rc_ok;
 
     if (geteuid() != 0) {
-        return pcmk_rc_ok;
+        goto done;
     }
 
-    fd = fileno(logfile);
+    fd = open(filename, O_RDWR);
     if (fd < 0) {
         rc = errno;
-        pcmk__warn("Logging to '%s' is disabled because couldn't get file "
-                   "descriptor: %s", filename, strerror(rc));
-        return rc;
+        pcmk__warn("Logging to '%s' is disabled because open() failed as root: "
+                   "%s", filename, strerror(rc));
+        goto done;
     }
 
     rc = chown_logfile(filename, fd);
     if (rc != pcmk_rc_ok) {
-        return rc;
+        goto done;
     }
 
     chmod_logfile(filename, fd);
-    return pcmk_rc_ok;
+
+done:
+    if (fd >= 0) {
+        close(fd);
+    }
+    return rc;
 }
 
 // Enable libqb logging to a new log file
@@ -444,7 +456,6 @@ pcmk__add_logfile(const char *filename)
 {
     int fd = 0;
     int rc = pcmk_rc_ok;
-    FILE *logfile = NULL;
     bool is_default = false;
 
     static int default_fd = -1;
@@ -471,26 +482,14 @@ pcmk__add_logfile(const char *filename)
         return pcmk_rc_ok;
     }
 
-    // Check whether we have write access to the file
-    logfile = fopen(filename, "a");
-    if (logfile == NULL) {
-        rc = errno;
-        pcmk__warn("Logging to '%s' is disabled because couldn't open for "
-                   "append: %s " QB_XS " uid=%lld gid=%lld",
-                   filename, strerror(rc), (long long) geteuid(),
-                   (long long) getegid());
-        return rc;
-    }
-
-    rc = set_logfile_permissions(filename, logfile);
+    // If we're root, ensure ownership and permissions are correct
+    rc = set_logfile_permissions(filename);
     if (rc != pcmk_rc_ok) {
         // Warning has already been logged
-        fclose(logfile);
         return rc;
     }
 
-    // Close and reopen as libqb logging target
-    fclose(logfile);
+    // Open as a libqb log file
     fd = qb_log_file_open(filename);
     if (fd < 0) {
         rc = -fd;   // fd == -errno

commit ecf6ceafe0b6da8086c724ffbfb034186524a4a9

Low: libcrmcommon: Create a log file if it doesn't exist.
This fixes a regression caused by 5aba8f7b47.  Prior to that patch,
pcmk__log_file would attempt to open the file it was given with
fopen(..., "a").  This would cause the file to be created if it did not
already exist.  However, 5aba8f7b47 uses open() instead, which will not
create the log file unless told to do so.

Additionally, the commit message mentions opening in append mode in
set_logfile_permissions, but no flag is being passed to open() to make
that happen.  This doesn't matter since we're not writing to the log
file before closing it.  It will be opened again by qb_log_file_open
which will use the proper append flag.

The permissions we're creating with don't matter too much since the next
thing to happen is calling {chown,chmod}_logfile.

This is at least needed to run the attrd regression tests, and likely
other tests as well.

lib/common/logging.c
@@ -10,7 +10,7 @@
 #include <crm_internal.h>
 
 #include <errno.h>                  // errno
-#include <fcntl.h>                  // open, O_RDWR
+#include <fcntl.h>                  // open, O_CREAT, O_RDWR, S_*
 #include <libgen.h>                 // basename
 #include <signal.h>                 // raise, SIG*
 #include <stdbool.h>
@@ -386,7 +386,7 @@ set_logfile_permissions(const char *filename)
         goto done;
     }
 
-    fd = open(filename, O_RDWR);
+    fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd < 0) {
         rc = errno;
         pcmk__warn("Logging to '%s' is disabled because open() failed as root: "