767a6837a7037cd07debd17da5983790ea4d9bd8
Reid Wahl <nrwahl@protonmail.com>
2026-04-24 21:54:47 -0700

    Refactor: various: Stack-allocate pcmk_node_t in unit tests
    Other tests already do this. Dynamic allocation would make more sense if
    we had a generic constructor for pcmk_node_t objects. But all we have is
    pe__create_node(), which requires a pcmk_scheduler_t argument. That
    seems like overkill.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/pengine/tests/status/pe_find_node_id_test.c
+++ lib/pengine/tests/status/pe_find_node_id_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the Pacemaker project contributors
+ * Copyright 2022-2026 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -13,37 +13,30 @@
 #include <crm/pengine/internal.h>
 
 static void
-empty_list(void **state) {
+empty_list(void **state)
+{
     assert_null(pe_find_node_id(NULL, NULL));
     assert_null(pe_find_node_id(NULL, "id1"));
 }
 
 static void
-non_null_list(void **state) {
+non_null_list(void **state)
+{
+    struct pcmk__node_private node1_priv = { .id = "id1" };
+    struct pcmk__node_private node2_priv = { .id = "id2" };
+    pcmk_node_t node1 = { .priv = &node1_priv };
+    pcmk_node_t node2 = { .priv = &node2_priv };
     GList *nodes = NULL;
 
-    pcmk_node_t *a = pcmk__assert_alloc(1, sizeof(pcmk_node_t));
-    pcmk_node_t *b = pcmk__assert_alloc(1, sizeof(pcmk_node_t));
+    nodes = g_list_prepend(nodes, &node1);
+    nodes = g_list_prepend(nodes, &node2);
 
-    a->priv = pcmk__assert_alloc(1, sizeof(struct pcmk__node_private));
-    b->priv = pcmk__assert_alloc(1, sizeof(struct pcmk__node_private));
-
-    a->priv->id = "id1";
-    b->priv->id = "id2";
-
-    nodes = g_list_append(nodes, a);
-    nodes = g_list_append(nodes, b);
-
-    assert_ptr_equal(a, pe_find_node_id(nodes, "id1"));
+    assert_ptr_equal(&node1, pe_find_node_id(nodes, "id1"));
     assert_null(pe_find_node_id(nodes, "id10"));
     assert_null(pe_find_node_id(nodes, "nodeid1"));
-    assert_ptr_equal(b, pe_find_node_id(nodes, "ID2"));
+    assert_ptr_equal(&node2, pe_find_node_id(nodes, "ID2"));
     assert_null(pe_find_node_id(nodes, "xyz"));
 
-    free(a->priv);
-    free(a);
-    free(b->priv);
-    free(b);
     g_list_free(nodes);
 }
 
