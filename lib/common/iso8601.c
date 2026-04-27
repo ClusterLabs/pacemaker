88f6a0f6066852014fa6113aaf81378e028ef358
Reid Wahl <nrwahl@protonmail.com>
2026-04-27 03:23:22 -0700

    API: libcrmcommon: Deprecate crm_time_free_period()
    There is no longer a constructor for crm_time_period_t and no reason to
    create one.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/iso8601.c
+++ lib/common/iso8601.c
@@ -1354,22 +1354,6 @@ invalid:
 }
 
 /*!
- * \brief Free a dynamically allocated time period object
- *
- * \param[in,out] period  Time period to free
- */
-void
-crm_time_free_period(crm_time_period_t *period)
-{
-    if (period) {
-        crm_time_free(period->start);
-        crm_time_free(period->end);
-        crm_time_free(period->diff);
-        free(period);
-    }
-}
-
-/*!
  * \internal
  * \brief Set one time object to another if the other is earlier
  *
@@ -2323,6 +2307,17 @@ crm_time_log_alias(int log_level, const char *file, const char *function,
 {
     pcmk__time_log_as(file, function, line, pcmk__clip_log_level(log_level),
                       prefix, date_time, flags);
+}
+
+void
+crm_time_free_period(crm_time_period_t *period)
+{
+    if (period) {
+        crm_time_free(period->start);
+        crm_time_free(period->end);
+        crm_time_free(period->diff);
+        free(period);
+    }
 }
 
 crm_time_period_t *
