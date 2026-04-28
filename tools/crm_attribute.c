046613ed2b436e7d5e8959a8e924b4f9cfe7fc6e
Chris Lumens <clumens@redhat.com>
2026-04-28 16:38:00 -0400

    Refactor: tools: Simplify crm_attribute help output.
    Ref T638

--- tools/crm_attribute.c
+++ tools/crm_attribute.c
@@ -175,22 +175,20 @@ wait_cb (const gchar *option_name, const gchar *optarg, gpointer data, GError **
 
 static GOptionEntry selecting_entries[] = {
     { "all", 'a', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &options.all,
-      "With -L/--list-options, include advanced and deprecated options in the\n"
-      INDENT "output. This is always treated as true when --output-as=xml is\n"
-      INDENT "specified.",
+      "Include advanced and deprecated options in the\n"
+      INDENT "output\n"
+      INDENT "(with -L/--list-options)",
       NULL,
     },
 
     { "id", 'i', 0, G_OPTION_ARG_STRING, &options.attr_id,
-      "(Advanced) Operate on instance of specified attribute with this\n"
-      INDENT "XML ID",
+      "(Advanced) Operate on instance of specified\n"
+      INDENT "attribute with this XML ID",
       "XML_ID"
     },
 
     { "name", 'n', G_OPTION_FLAG_NONE, G_OPTION_ARG_CALLBACK, attr_name_cb,
-      "Operate on attribute or option with this name.  For queries, this\n"
-      INDENT "is optional, in which case all matching attributes will be\n"
-      INDENT "returned.",
+      "Operate on attribute or option with this name",
       "NAME"
     },
 
@@ -201,16 +199,15 @@ static GOptionEntry selecting_entries[] = {
     },
 
     { "promotion", 'p', G_OPTION_FLAG_OPTIONAL_ARG, G_OPTION_ARG_CALLBACK, promotion_cb,
-      "Operate on node attribute used as promotion score for specified\n"
-      INDENT "resource, or resource given in OCF_RESOURCE_INSTANCE environment\n"
-      INDENT "variable if none is specified; this also defaults -l/--lifetime\n"
-      INDENT "to reboot (normally invoked from an OCF resource agent)",
+      "Operate on node attribute used as promotion score\n"
+      INDENT "for specified resource",
       "RESOURCE"
     },
 
     { "set-name", 's', 0, G_OPTION_ARG_STRING, &options.set_name,
-      "(Advanced) Operate on instance of specified attribute that is\n"
-      INDENT "within set with this XML ID",
+      "(Advanced) Operate on instance of specified\n"
+      INDENT "attribute that is within set with this\n"
+      INDENT "XML ID",
       "NAME"
     },
 
@@ -219,24 +216,26 @@ static GOptionEntry selecting_entries[] = {
 
 static GOptionEntry command_entries[] = {
     { "list-options", 'L', G_OPTION_FLAG_NONE, G_OPTION_ARG_CALLBACK, list_cb,
-      "List all available options of the given type.\n"
-      INDENT "Allowed values: " PCMK__VALUE_CLUSTER,
+      "List all available options of the given type\n"
+      INDENT "Values: '" PCMK__VALUE_CLUSTER "'",
       "TYPE"
     },
 
     { "delete", 'D', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, delete_cb,
-      "Delete the attribute/option (with -n or -P)",
+      "Delete the attribute/option\n"
+      INDENT "(with -n or -P)",
       NULL
     },
 
     { "query", 'G', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, value_cb,
-      "Query the current value of the attribute/option.\n"
+      "Query the current value of the attribute/option\n"
       INDENT "See also: -n, -P",
       NULL
     },
 
     { "update", 'v', 0, G_OPTION_ARG_CALLBACK, update_cb,
-      "Update the value of the attribute/option (with -n or -P)",
+      "Update the value of the attribute/option\n"
+      INDENT "(with -n or -P)",
       "VALUE"
     },
 
@@ -245,69 +244,45 @@ static GOptionEntry command_entries[] = {
 
 static GOptionEntry addl_entries[] = {
     { "default", 'd', 0, G_OPTION_ARG_STRING, &options.attr_default,
-      "(Advanced) Default value to display if none is found in configuration",
+      "(Advanced) Default value to display if none is\n"
+      INDENT "found in configuration",
       "VALUE"
     },
 
     { "lifetime", 'l', 0, G_OPTION_ARG_STRING, &options.type,
-      "Lifetime of the node attribute.\n"
-      INDENT "Valid values: reboot, forever",
+      "Lifetime of the node attribute\n"
+      INDENT "Values: 'forever', 'reboot'",
       "LIFETIME"
     },
 
     { "node", 'N', 0, G_OPTION_ARG_STRING, &options.dest_uname,
-      "Set a node attribute for named node (instead of a cluster option).\n"
+      "Set a node attribute for named node (instead of\n"
+      INDENT "a cluster option)\n"
       INDENT "See also: -l",
       "NODE"
     },
 
     { "type", 't', 0, G_OPTION_ARG_STRING, &options.type,
-      "Which part of the configuration to update/delete/query the option in.\n"
-      INDENT "Valid values: crm_config, rsc_defaults, op_defaults, tickets",
+      "Which part of the configuration to update, delete,\n"
+      INDENT "or query the option in\n"
+      INDENT "Values: 'crm_config', 'rsc_defaults',\n"
+      INDENT "        'op_defaults', 'tickets'",
       "SECTION"
     },
 
     { "score", 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &options.score_update,
-      "Treat new attribute values as atomic score updates where possible\n"
-      INDENT "(with --update/-v, when running against a CIB file or updating\n"
-      INDENT "an attribute outside the " PCMK_XE_STATUS " section; enabled\n"
-      INDENT "by default if --promotion/-p is specified)\n\n"
-
-      INDENT "This currently happens by default and cannot be disabled, but\n"
-      INDENT "this default behavior is deprecated and will be removed in a\n"
-      INDENT "future release (exception: this will remain the default with\n"
-      INDENT "--promotion/-p). Set this flag if this behavior is desired.\n\n"
-
-      INDENT "This option takes effect when updating XML attributes. For an\n"
-      INDENT "attribute named \"name\", if the new value is \"name++\" or\n"
-      INDENT "\"name+=X\" for some score X, the new value is set as follows:\n"
-      INDENT " * If attribute \"name\" is not already set to some value in\n"
-      INDENT "   the element being updated, the new value is set as a literal\n"
-      INDENT "   string.\n"
-      INDENT " * If the new value is \"name++\", then the attribute is set to\n"
-      INDENT "   its existing value (parsed as a score) plus 1.\n"
-      INDENT " * If the new value is \"name+=X\" for some score X, then the\n"
-      INDENT "   attribute is set to its existing value plus X, where the\n"
-      INDENT "   existing value and X are parsed and added as scores.\n\n"
-
-      INDENT "Scores are integer values capped at INFINITY and -INFINITY.\n"
-      INDENT "Refer to Pacemaker Explained for more details on scores,\n"
-      INDENT "including how they are parsed and added.",
+      "Treat new attribute values as atomic score updates\n"
+      INDENT "where possible",
       NULL },
 
     { "wait", 'W', 0, G_OPTION_ARG_CALLBACK, wait_cb,
-      "Wait for some event to occur before returning.  Values are 'no' (wait\n"
-      INDENT "only for the attribute daemon to acknowledge the request),\n"
-      INDENT "'local' (wait until the change has propagated to where a local\n"
-      INDENT "query will return the request value, or the value set by a\n"
-      INDENT "later request), or 'cluster' (wait until the change has propagated\n"
-      INDENT "to where a query anywhere on the cluster will return the requested\n"
-      INDENT "value, or the value set by a later request).  Default is 'no'.\n"
-      INDENT "(with -N, and one of -D or -u)",
+      "Wait for some event to occur before returning\n"
+      INDENT "Values: 'no' (default), 'local', 'cluster'\n"
+      INDENT "(with -N, and one of -D or -v)",
       "UNTIL" },
 
     { "utilization", 'z', G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, utilization_cb,
-      "Set an utilization attribute for the node.",
+      "Set an utilization attribute for the node",
       NULL
     },
 
