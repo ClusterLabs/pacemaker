d5379fdb915d7fd049d5d82a5026572b4146e26e
Reid Wahl <nrwahl@protonmail.com>
2026-04-30 18:47:29 -0700

    API: libcrmcommon: Deprecate crm_time_log_with_timezone
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/iso8601_compat.h
+++ include/crm/common/iso8601_compat.h
@@ -36,6 +36,9 @@ extern "C" {
 #define crm_time_log_timeofday 0x002
 
 //! \deprecated Do not use
+#define crm_time_log_with_timezone 0x004
+
+//! \deprecated Do not use
 typedef struct crm_time_period_s {
     crm_time_t *start;
     crm_time_t *end;
