f5ddcce7591afd574f9b2acbcd714d55d54cbbc2
Reid Wahl <nrwahl@protonmail.com>
2026-04-30 18:51:40 -0700

    Refactor: libcrmcommon: New pcmk__time_fmt_ordinal
    To replace crm_time_ordinal.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- tools/iso8601.c
+++ tools/iso8601.c
@@ -61,7 +61,7 @@ modifier_cb(const char *option_name, const char *optarg, void *data,
     } else if (pcmk__str_any_of(option_name, "--local", "-L", NULL)) {
         options.print_options |= pcmk__time_fmt_timezone;
     } else if (pcmk__str_any_of(option_name, "--ordinal", "-O", NULL)) {
-        options.print_options |= crm_time_ordinal;
+        options.print_options |= pcmk__time_fmt_ordinal;
     } else if (pcmk__str_any_of(option_name, "--week", "-W", NULL)) {
         options.print_options |= crm_time_weeks;
     }
