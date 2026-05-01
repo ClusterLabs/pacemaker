65279c9683ebe0f8ea26e5d82eb1e9ea836e98c2
Reid Wahl <nrwahl@protonmail.com>
2026-04-30 22:22:51 -0700

    API: libcrmcommon: Deprecate crm_time_get_gregorian()
    External callers have no need to inspect a crm_time_t object. Pacemaker
    should not be used for general-purpose date/time manipulation.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/iso8601.h
+++ include/crm/common/iso8601.h
@@ -54,8 +54,6 @@ typedef struct crm_time_s crm_time_t;
 crm_time_t *crm_time_new(const char *string);
 void crm_time_free(crm_time_t * dt);
 
-int crm_time_get_gregorian(const crm_time_t *dt, uint32_t *y, uint32_t *m,
-                           uint32_t *d);
 int crm_time_get_ordinal(const crm_time_t *dt, uint32_t *y, uint32_t *d);
 
 /* Time in seconds since 0000-01-01 00:00:00Z */
