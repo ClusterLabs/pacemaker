ClusterLabs/pacemaker

commit ed200011102731fad27a5cde7c98c9cc5dc6983a

Refactor: libcib: Sanity-check the cib__op_fn_t assumptions
As noted in the comments, these sanity checks are only partial.

It's fairly easy to make a mistake when modifying cib__op_fn_t
functions, and to end up with the cib in a different document. So this
at least catches those issues, as well as cib no longer being the root.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_utils.c
@@ -197,6 +197,8 @@ cib__perform_query(cib__op_fn_t fn, xmlNode *req, xmlNode **current_cib,
 
     xmlNode *cib = NULL;
     xmlNode *cib_filtered = NULL;
+    const xmlNode *saved_cib = NULL;
+    const xmlDoc *saved_doc = NULL;
 
     pcmk__assert((fn != NULL) && (req != NULL)
                  && (current_cib != NULL) && (*current_cib != NULL)
@@ -226,7 +228,16 @@ cib__perform_query(cib__op_fn_t fn, xmlNode *req, xmlNode **current_cib,
                 pcmk__s(section, "(null)"), pcmk__s(user, "(null)"));
     pcmk__log_xml_trace(req, "request");
 
+    saved_cib = cib;
+    saved_doc = cib->doc;
+
     rc = fn(req, input, &cib, output);
+
+    /* Sanity check: op should be read-only (but this does not check children,
+     * attributes, or private data)
+     */
+    pcmk__assert((cib == saved_cib) && (cib->doc == saved_doc)
+                 && (cib == xmlDocGetRootElement(cib->doc)));
 
     if (*output == NULL) {
         // Do nothing
@@ -483,6 +494,7 @@ cib_perform_op(enum cib_variant variant, cib__op_fn_t fn, xmlNode *req,
     xmlNode *old_versions = NULL;
 
     xmlNode *top = NULL;
+    const xmlDoc *saved_doc = NULL;
 
     pcmk__assert((fn != NULL) && (req != NULL)
                  && (config_changed != NULL) && (!*config_changed)
@@ -530,6 +542,8 @@ cib_perform_op(enum cib_variant variant, cib__op_fn_t fn, xmlNode *req,
         }
     }
 
+    saved_doc = (*cib)->doc;
+
     rc = fn(req, input, cib, output);
     if (rc != pcmk_rc_ok) {
         goto done;
@@ -539,6 +553,10 @@ cib_perform_op(enum cib_variant variant, cib__op_fn_t fn, xmlNode *req,
         rc = EINVAL;
         goto done;
     }
+
+    // Sanity check: doc should not change and *cib must be the root
+    pcmk__assert(((*cib)->doc == saved_doc)
+                 && (*cib == xmlDocGetRootElement((*cib)->doc)));
 
     // Tracking flag should still be set after operation
     pcmk__assert(pcmk__xml_doc_all_flags_set((*cib)->doc, pcmk__xf_tracking));

commit e8333fe487bbb8adffb257bdf43b8e1854ead8c1

Fix: libcib: Avoid assertion when processing a shutdown request
This fixes a regression introduced by ed200011. It has not made it into
any release.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/cib/cib_utils.c
@@ -236,8 +236,14 @@ cib__perform_query(cib__op_fn_t fn, xmlNode *req, xmlNode **current_cib,
     /* Sanity check: op should be read-only (but this does not check children,
      * attributes, or private data)
      */
-    pcmk__assert((cib == saved_cib) && (cib->doc == saved_doc)
-                 && (cib == xmlDocGetRootElement(cib->doc)));
+    if (!pcmk__str_eq(op, PCMK__CIB_REQUEST_SHUTDOWN, pcmk__str_none)) {
+        /* @TODO based_terminate() frees the_cib during
+         * based_process_shutdown(). Consolidate the based cleanup logic and
+         * remove the if guard here.
+         */
+        pcmk__assert((cib == saved_cib) && (cib->doc == saved_doc)
+                     && (cib == xmlDocGetRootElement(cib->doc)));
+    }
 
     if (*output == NULL) {
         // Do nothing