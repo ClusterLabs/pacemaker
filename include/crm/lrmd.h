ClusterLabs/pacemaker

commit dc986d9bb439c8284c4fec9dbb41674c1512d044

Refactor: libcrmcommon: New pcmk__compare_versions()
To replace compare_versions(). Seems a bit more readable and a bit less
error-prone.

I started working on an implementation that just uses
pcmk__numeric_strcasecmp(), but it started getting ugly for things like
"1" vs. "1.0", since it treats all non-digit characters equally (in
particular, it doesn't treat dots specially).

pcmk__compare_versions() could also reasonably go into strings.c, by the
way.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

include/crm/lrmd.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the Pacemaker project contributors
+ * Copyright 2012-2025 the Pacemaker project contributors
  *
  * The version control history for this file may have further details.
  *
@@ -15,6 +15,8 @@
 #include <crm_config.h>
 #include <crm/lrmd_events.h>
 #include <crm/services.h>
+
+#include <crm/common/internal.h>    // pcmk__compare_versions()
 
 #ifdef __cplusplus
 extern "C" {
@@ -43,7 +45,7 @@ typedef struct lrmd_key_value_s {
  */
 #define LRMD_PROTOCOL_VERSION "1.2"
 
-#define LRMD_SUPPORTS_SCHEMA_XFER(x) (compare_version((x), "1.2") >= 0)
+#define LRMD_SUPPORTS_SCHEMA_XFER(x) (pcmk__compare_versions((x), "1.2") >= 0)
 
 /* The major protocol version the client and server both need to support for
  * the connection to be successful.  This should only ever be the major

daemons/based/based_callbacks.c
@@ -1180,8 +1180,9 @@ cib_process_command(xmlNode *request, const cib__operation_t *operation,
          */
         if ((operation->type == cib__op_commit_transact)
             && pcmk__str_eq(originator, OUR_NODENAME, pcmk__str_casei)
-            && compare_version(pcmk__xe_get(the_cib, PCMK_XA_CRM_FEATURE_SET),
-                               "3.19.0") < 0) {
+            && (pcmk__compare_versions(pcmk__xe_get(the_cib,
+                                                    PCMK_XA_CRM_FEATURE_SET),
+                                       "3.19.0") < 0)) {
 
             sync_our_cib(request, TRUE);
         }

daemons/controld/controld_cib.c
@@ -481,8 +481,9 @@ build_parameter_list(const lrmd_event_data_t *op,
     /* Consider all parameters only except private ones to be consistent with
      * what scheduler does with calculate_secure_digest().
      */
-    if (param_type == ra_param_private
-        && compare_version(controld_globals.dc_version, "3.16.0") >= 0) {
+    if ((param_type == ra_param_private)
+        && (pcmk__compare_versions(controld_globals.dc_version,
+                                   "3.16.0") >= 0)) {
         g_hash_table_foreach(op->params, hash2field, *result);
         pcmk__filter_op_for_digest(*result);
     }

daemons/controld/controld_execd.c
@@ -2190,7 +2190,7 @@ process_lrm_event(lrm_state_t *lrm_state, lrmd_event_data_t *op,
     CRM_CHECK(op->rsc_id != NULL, return);
 
     // Remap new status codes for older DCs
-    if (compare_version(controld_globals.dc_version, "3.2.0") < 0) {
+    if (pcmk__compare_versions(controld_globals.dc_version, "3.2.0") < 0) {
         switch (op->op_status) {
             case PCMK_EXEC_NOT_CONNECTED:
                 lrmd__set_result(op, PCMK_OCF_CONNECTION_DIED,

daemons/controld/controld_membership.c
@@ -161,7 +161,8 @@ create_node_state_update(pcmk__node_status_t *node, uint32_t flags,
      * "< 3.18.0" code here and below.
      */
     if (pcmk__is_set(flags, controld_node_update_cluster)) {
-        if (compare_version(controld_globals.dc_version, "3.18.0") >= 0) {
+        if (pcmk__compare_versions(controld_globals.dc_version,
+                                   "3.18.0") >= 0) {
             // A value 0 means the node is not a cluster member.
             pcmk__xe_set_ll(node_state, PCMK__XA_IN_CCM, node->when_member);
 
@@ -174,7 +175,8 @@ create_node_state_update(pcmk__node_status_t *node, uint32_t flags,
 
     if (!pcmk__is_set(node->flags, pcmk__node_status_remote)) {
         if (pcmk__is_set(flags, controld_node_update_peer)) {
-            if (compare_version(controld_globals.dc_version, "3.18.0") >= 0) {
+            if (pcmk__compare_versions(controld_globals.dc_version,
+                                       "3.18.0") >= 0) {
                 // A value 0 means the peer is offline in CPG.
                 pcmk__xe_set_ll(node_state, PCMK_XA_CRMD, node->when_online);
 

daemons/controld/controld_metadata.c
@@ -97,12 +97,12 @@ log_ra_ocf_version(const char *ra_key, const char *ra_ocf_version)
     if (pcmk__str_empty(ra_ocf_version)) {
         crm_warn("%s does not advertise OCF version supported", ra_key);
 
-    } else if (compare_version(ra_ocf_version, "2") >= 0) {
+    } else if (pcmk__compare_versions(ra_ocf_version, "2") >= 0) {
         crm_warn("%s supports OCF version %s (this Pacemaker version supports "
                  PCMK_OCF_VERSION " and might not work properly with agent)",
                  ra_key, ra_ocf_version);
 
-    } else if (compare_version(ra_ocf_version, PCMK_OCF_VERSION) > 0) {
+    } else if (pcmk__compare_versions(ra_ocf_version, PCMK_OCF_VERSION) > 0) {
         crm_info("%s supports OCF version %s (this Pacemaker version supports "
                  PCMK_OCF_VERSION " and might not use all agent features)",
                  ra_key, ra_ocf_version);
@@ -151,7 +151,8 @@ controld_cache_metadata(GHashTable *mdc, const lrmd_rsc_info_t *rsc,
         }
         log_ra_ocf_version(key, (const char *) content);
         if (content != NULL) {
-            ocf1_1 = (compare_version((const char *) content, "1.1") >= 0);
+            ocf1_1 = (pcmk__compare_versions((const char *) content,
+                                             "1.1") >= 0);
             xmlFree(content);
         }
     }

daemons/execd/execd_commands.c
@@ -1519,7 +1519,8 @@ execd_process_signon(pcmk__client_t *client, xmlNode *request, int call_id,
                                                 PCMK__XA_LRMD_PROTOCOL_VERSION);
     const char *start_state = pcmk__env_option(PCMK__ENV_NODE_START_STATE);
 
-    if (compare_version(protocol_version, LRMD_COMPATIBLE_PROTOCOL) < 0) {
+    if (pcmk__compare_versions(protocol_version,
+                               LRMD_COMPATIBLE_PROTOCOL) < 0) {
         crm_err("Cluster API version must be greater than or equal to %s, not %s",
                 LRMD_COMPATIBLE_PROTOCOL, protocol_version);
         rc = EPROTO;

include/crm/cib.h
@@ -26,7 +26,7 @@ extern "C" {
  * \ingroup cib
  */
 
-// Use compare_version() for doing comparisons
+// Use pcmk__compare_versions() for doing comparisons
 #  define CIB_FEATURE_SET "2.0"
 
 /* Core functions */

commit 537494aeb481a1164114846f8c2193d3ecf88bd9

Fix: liblrmd: Don't include internal header in lrmd.h
This fixes a regression introduced by dc986d9b. We don't package or
install internal headers like lrmd_internal.h.

The new lrmd_compare_versions() is immediately deprecated. It will be
moved to a compat header in an upcoming commit. Its only purpose is to
wrap pcmk__compare_versions() so that lrmd.h doesn't require an internal
header.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

include/crm/lrmd.h
@@ -16,7 +16,6 @@
 #include <glib.h>                   // GList
 
 #include <crm_config.h>
-#include <crm/common/internal.h>    // pcmk__compare_versions()
 #include <crm/lrmd_events.h>
 #include <crm/services.h>
 
@@ -52,7 +51,7 @@ typedef struct lrmd_key_value_s {
  */
 #define LRMD_PROTOCOL_VERSION "1.2"
 
-#define LRMD_SUPPORTS_SCHEMA_XFER(x) (pcmk__compare_versions((x), "1.2") >= 0)
+#define LRMD_SUPPORTS_SCHEMA_XFER(x) (lrmd_compare_versions((x), "1.2") >= 0)
 
 /* The major protocol version the client and server both need to support for
  * the connection to be successful.  This should only ever be the major
@@ -518,6 +517,9 @@ lrmd_event_type2str(enum lrmd_callback_event type)
     }
     return "unknown";
 }
+
+//! \deprecated Do not use
+int lrmd_compare_versions(const char *version1, const char *version2);
 
 #ifdef __cplusplus
 }

lib/lrmd/lrmd_client.c
@@ -2505,3 +2505,10 @@ lrmd__proxy_send(lrmd_t *lrmd, xmlNode *msg)
 
     return lrmd_send_xml_no_reply(lrmd, msg);
 }
+
+// @COMPAT This exists only as a helper for LRMD_SUPPORTS_SCHEMA_XFER()
+int
+lrmd_compare_versions(const char *version1, const char *version2)
+{
+    return pcmk__compare_versions(version1, version2);
+}