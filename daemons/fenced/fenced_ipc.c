1391e9cf79f2edecd4aca905ccc5fc48a360d3bd
Chris Lumens <clumens@redhat.com>
2026-08-18 13:18:06 -0400

    Refactor: daemons: Standardize connection_accept in fenced.

--- daemons/fenced/fenced_ipc.c
+++ daemons/fenced/fenced_ipc.c
@@ -9,7 +9,6 @@
 
 #include <crm_internal.h>
 
-#include <errno.h>                            // ECONNREFUSED, ENOMEM
 #include <inttypes.h>                         // int32_t, uint32_t, PRIu32
 #include <stdio.h>                            // NULL, size_t
 #include <sys/types.h>                        // gid_t, uid_t
@@ -49,30 +48,10 @@ handle_ipc_reply(pcmk__client_t *client, xmlNode *request)
                 pcmk__client_name(client));
 }
 
-/*!
- * \internal
- * \brief Accept a new client IPC connection
- *
- * \param[in,out] c    New connection
- * \param[in]     uid  Client user id
- * \param[in]     gid  Client group id
- *
- * \return 0 on success, -errno otherwise
- */
 static int32_t
-fenced_ipc_accept(qb_ipcs_connection_t *c, uid_t uid, gid_t gid)
+ipc_accept(qb_ipcs_connection_t *c, uid_t uid, gid_t gid)
 {
-    pcmk__trace("New client connection %p", c);
-    if (fenced.shutting_down) {
-        pcmk__info("Ignoring new connection from pid %d during shutdown",
-                   pcmk__client_pid(c));
-        return -ECONNREFUSED;
-    }
-
-    if (pcmk__new_client(c, uid, gid) == NULL) {
-        return -ENOMEM;
-    }
-    return 0;
+    return pcmk__daemon_ipc_accept(&fenced, c, uid, gid);
 }
 
 /*!
@@ -250,7 +229,7 @@ fenced_ipc_destroy(qb_ipcs_connection_t *c)
 }
 
 struct qb_ipcs_service_handlers ipc_callbacks = {
-    .connection_accept = fenced_ipc_accept,
+    .connection_accept = ipc_accept,
     .connection_created = NULL,
     .msg_process = fenced_ipc_dispatch,
     .connection_closed = fenced_ipc_closed,
