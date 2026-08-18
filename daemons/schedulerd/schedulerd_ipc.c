63cea1ebb61bc87a4875c295bdd25e05024a65a7
Chris Lumens <clumens@redhat.com>
2026-08-18 16:14:55 -0400

    Refactor: daemons: Standardize connection_accept in schedulerd.

--- daemons/schedulerd/schedulerd_ipc.c
+++ daemons/schedulerd/schedulerd_ipc.c
@@ -9,7 +9,6 @@
 
 #include <crm_internal.h>
 
-#include <errno.h>                      // ENOMEM
 #include <stddef.h>                     // NULL, size_t
 #include <stdint.h>                     // int32_t, uint32_t
 #include <sys/types.h>                  // gid_t, uid_t
@@ -23,24 +22,10 @@
 
 #include "pacemaker-schedulerd.h"       // schedulerd_handle_request
 
-/*!
- * \internal
- * \brief Accept a new client IPC connection
- *
- * \param[in,out] c    New connection
- * \param[in]     uid  Client user id
- * \param[in]     gid  Client group id
- *
- * \return 0 on success, -errno otherwise
- */
 static int32_t
-schedulerd_ipc_accept(qb_ipcs_connection_t *c, uid_t uid, gid_t gid)
+ipc_accept(qb_ipcs_connection_t *c, uid_t uid, gid_t gid)
 {
-    pcmk__trace("New client connection %p", c);
-    if (pcmk__new_client(c, uid, gid) == NULL) {
-        return -ENOMEM;
-    }
-    return 0;
+    return pcmk__daemon_ipc_accept(&schedulerd, c, uid, gid);
 }
 
 /*!
@@ -180,7 +165,7 @@ schedulerd_ipc_destroy(qb_ipcs_connection_t *c)
 }
 
 struct qb_ipcs_service_handlers ipc_callbacks = {
-    .connection_accept = schedulerd_ipc_accept,
+    .connection_accept = ipc_accept,
     .connection_created = NULL,
     .msg_process = schedulerd_ipc_dispatch,
     .connection_closed = schedulerd_ipc_closed,
